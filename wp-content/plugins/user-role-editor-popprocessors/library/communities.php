67161048975822f7145cfd0a933b4f1558ccc176
Leonardo Losoviz <leo@getpop.org>
2017-04-20 18:32:26 +0800

    Fixed bug in which a user was in his/her user network

--- wp-content/plugins/user-role-editor-popprocessors/library/communities.php
+++ wp-content/plugins/user-role-editor-popprocessors/library/communities.php
@@ -35,6 +35,12 @@ function gd_ure_get_user_networkusers($usernetwork, $user_id) {
 
 		// Remove duplicates
 		$usernetwork = array_unique($usernetwork);
+
+		// Remove the user him/herself (since the user is also a member of his/her communities)
+		$pos = array_search($user_id, $usernetwork);
+		if ($pos > -1) {
+			array_splice($usernetwork, $pos, 1);
+		}
 	}
 
 	return $usernetwork;
