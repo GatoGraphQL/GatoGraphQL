ae7f4e1f0dbab5e9e131d032f2a6008df709424c
Leo <losoviz@gmail.com>
2017-08-10 13:48:48 +0200

    Fixed error "Refused to display" '...' in a frame because it set 'X-Frame-Options' to 'sameorigin'" after having logged in

--- wp-content/plugins/poptheme-wassup/library/header.php
+++ wp-content/plugins/poptheme-wassup/library/header.php
@@ -6,6 +6,12 @@
  *
  * ---------------------------------------------------------------------------------------------------------------*/
 
+// Comment Leo 10/08/2017: we need to allow other websites to embed our content:
+// Multidomain (so we can, in GetPoP, display a page from MESYM with an iframe, such as the post-link posts)
+// Otherwise, after logging in, we can't load iframes anymore
+remove_action('login_init', 'send_frame_options_header', 10, 0);
+remove_action('admin_init', 'send_frame_options_header', 10, 0);
+
 // add_action('send_headers', 'gd_wp_headers');
 // function gd_wp_headers() {
 
