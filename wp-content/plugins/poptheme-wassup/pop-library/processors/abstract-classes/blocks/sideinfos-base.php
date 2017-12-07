61ffabe89c95cb71dd7489e7ac7466c1ceae9e32
Leo <losoviz@gmail.com>
2017-12-08 01:45:59 +0800

    Fixed sideinfo opening for Verticals homepage and then closing after a few seconds. Solution: make JS function `closePageSectionOnTabpaneShown` critical, so the sideinfo is closed immediately

--- wp-content/plugins/poptheme-wassup/pop-library/processors/abstract-classes/blocks/sideinfos-base.php
+++ wp-content/plugins/poptheme-wassup/pop-library/processors/abstract-classes/blocks/sideinfos-base.php
@@ -10,7 +10,10 @@ class GD_Template_Processor_CustomSideInfoBlocksBase extends GD_Template_Process
 	function get_pagesection_jsmethod($template_id, $atts) {
 
 		$ret = parent::get_pagesection_jsmethod($template_id, $atts);
-		$this->add_jsmethod($ret, 'closePageSectionOnTabpaneShown');
+
+		// Comment Leo 07/12/2017: this function closes the sideinfo for GD_TEMPLATE_BLOCK_EMPTYSIDEINFO, and it must take place immediately,
+		// or otherwise the sideinfo will show and then disappear a few seconds later and it looks ugly (eg: in Verticals homepage, where there is no sideinfo)
+		$this->add_jsmethod($ret, 'closePageSectionOnTabpaneShown', '', false, POP_PROGRESSIVEBOOTING_CRITICAL);
 		return $ret;
 	}
 }
