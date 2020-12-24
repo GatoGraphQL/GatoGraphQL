3b9a4b882d0b947ee4ba1b0e1d4c777b3e91e950
Leonardo Losoviz <leo@getpop.org>
2020-12-24 23:21:42 +0800

    Added again removed classes

---
+++ layers/GraphQLByPoP/packages/graphql-parser/src/Validator/RequestValidator/RequestValidatorInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Date: 10/24/16
+ *
+ * @author Portey Vasil <portey@gmail.com>
+ */
+
+namespace GraphQLByPoP\GraphQLParser\Validator\RequestValidator;
+
+
+use GraphQLByPoP\GraphQLParser\Execution\Request;
+
+interface RequestValidatorInterface
+{
+
+    public function validate(Request $request);
+
+}
