2131a2ef87ce852c67f82744582c974bbd48a6d0
Leonardo Losoviz <leo@getpop.org>
2021-01-09 15:25:15 +0800

    Created command

---
+++ src/Extensions/Symplify/MonorepoBuilder/CustomDependencyUpdater.php
@@ -0,0 +1,127 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\PoP\Extensions\Symplify\MonorepoBuilder;
+
+use Nette\Utils\Strings;
+use Symplify\ComposerJsonManipulator\FileSystem\JsonFileManager;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class CustomDependencyUpdater
+{
+    /**
+     * @var JsonFileManager
+     */
+    private $jsonFileManager;
+
+    public function __construct(JsonFileManager $jsonFileManager)
+    {
+        $this->jsonFileManager = $jsonFileManager;
+    }
+
+    /**
+     * @param SmartFileInfo[] $smartFileInfos
+     * @param string[] $packageNames
+     */
+    public function updateFileInfosWithPackagesAndVersion(
+        array $smartFileInfos,
+        array $packageNames,
+        string $version
+    ): void {
+        foreach ($smartFileInfos as $packageComposerFileInfo) {
+            $json = $this->jsonFileManager->loadFromFileInfo($packageComposerFileInfo);
+
+            $json = $this->processSectionWithPackages($json, $packageNames, $version, ComposerJsonSection::REQUIRE);
+
+            $json = $this->processSectionWithPackages($json, $packageNames, $version, ComposerJsonSection::REQUIRE_DEV);
+
+            $this->jsonFileManager->printJsonToFileInfo($json, $packageComposerFileInfo);
+        }
+    }
+
+    /**
+     * @param string[] $packageReplacements
+     * @param SmartFileInfo[] $smartFileInfos
+     */
+    public function updateFileInfosWithVendorAndVersion(
+        array $smartFileInfos,
+        array $packageReplacements,
+        string $version
+    ): void {
+        foreach ($smartFileInfos as $packageComposerFileInfo) {
+            $json = $this->jsonFileManager->loadFromFileInfo($packageComposerFileInfo);
+
+            $json = $this->processSection($json, $packageReplacements, $version, ComposerJsonSection::REQUIRE);
+            $json = $this->processSection($json, $packageReplacements, $version, ComposerJsonSection::REQUIRE_DEV);
+
+            $this->jsonFileManager->printJsonToFileInfo($json, $packageComposerFileInfo);
+        }
+    }
+
+    /**
+     * @param mixed[] $json
+     * @param string[] $packageNames
+     * @return mixed[]
+     */
+    private function processSectionWithPackages(
+        array $json,
+        array $packageNames,
+        string $targetVersion,
+        string $section
+    ): array {
+        if (! isset($json[$section])) {
+            return $json;
+        }
+
+        $sectionKeys = array_keys($json[$section]);
+        foreach ($sectionKeys as $packageName) {
+            if (! in_array($packageName, $packageNames, true)) {
+                continue;
+            }
+
+            $json[$section][$packageName] = $targetVersion;
+        }
+
+        return $json;
+    }
+
+    /**
+     * @param mixed[] $json
+     * @param string[] $packageReplacements
+     * @return mixed[]
+     */
+    private function processSection(array $json, array $packageReplacements, string $targetVersion, string $section): array
+    {
+        if (! isset($json[$section])) {
+            return $json;
+        }
+
+        foreach ($json[$section] as $packageName => $packageVersion) {
+            if ($this->shouldSkip($packageReplacements, $targetVersion, $packageName, $packageVersion)) {
+                continue;
+            }
+
+            $json[$section][$packageName] = $targetVersion;
+        }
+
+        return $json;
+    }
+
+    /**
+     * @param string[] $packageReplacements
+     */
+    private function shouldSkip(
+        array $packageReplacements,
+        string $targetVersion,
+        string $packageName,
+        string $packageVersion
+    ): bool {
+        if (! in_array($packageName, $packageReplacements)) {
+            return true;
+        }
+
+        return $packageVersion === $targetVersion;
+    }
+}
