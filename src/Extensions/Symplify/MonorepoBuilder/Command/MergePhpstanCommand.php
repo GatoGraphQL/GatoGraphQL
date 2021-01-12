bf056e39900b47a0564928d101166cf771d564e1
Leonardo Losoviz <leo@getpop.org>
2021-01-12 15:49:26 +0800

    Created command

---
+++ src/Extensions/Symplify/MonorepoBuilder/Command/MergePhpstanCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\PoP\Extensions\Symplify\MonorepoBuilder\Command;
+
+use PoP\PoP\Extensions\Symplify\MonorepoBuilder\Neon\NeonFilePrinter;
+use PoP\PoP\Extensions\Symplify\MonorepoBuilder\PHPStanNeonContentProvider;
+use PoP\PoP\Extensions\Symplify\MonorepoBuilder\ValueObject\Option;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
+use Symplify\PackageBuilder\Console\ShellCode;
+
+final class MergePhpstanCommand extends AbstractSymplifyCommand
+{
+    /**
+     * @var PHPStanNeonContentProvider
+     */
+    private $phpstanNeonContentProvider;
+
+    /**
+     * @var NeonFilePrinter
+     */
+    private $neonFilePrinter;
+
+    public function __construct(
+        PHPStanNeonContentProvider $phpstanNeonContentProvider,
+        NeonFilePrinter $neonFilePrinter
+    ) {
+        $this->phpstanNeonContentProvider = $phpstanNeonContentProvider;
+        $this->neonFilePrinter = $neonFilePrinter;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Create the phpstan.neon.dist config for the monorepo, including all phpstan.neon.dist files from all packages');
+        $this->addOption(
+            Option::OUTPUT_FILE,
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Path to dump root phpstan.neon.dist to',
+            getcwd() . DIRECTORY_SEPARATOR . 'phpstan.neon.dist'
+        );
+        $this->addOption(
+            Option::SKIP_UNMIGRATED,
+            null,
+            InputOption::VALUE_NONE,
+            'Skip the not-yet-migrated to PSR-4 packages.'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $skipUnmigrated = (bool) $input->getOption(Option::SKIP_UNMIGRATED);
+        $neonFileContent = $this->phpstanNeonContentProvider->provideContent($skipUnmigrated);
+
+        $outputFilePath = (string) $input->getOption(Option::OUTPUT_FILE);
+        $this->neonFilePrinter->printContentToOutputFile($neonFileContent, $outputFilePath);
+
+        return ShellCode::SUCCESS;
+    }
+}
---
+++ src/Extensions/Symplify/MonorepoBuilder/PHPStanNeonContentProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\PoP\Extensions\Symplify\MonorepoBuilder;
+
+use PoP\PoP\Extensions\Symplify\MonorepoBuilder\Json\SourcePackagesProvider;
+use Symplify\PackageBuilder\Neon\NeonPrinter;
+use Symplify\SymplifyKernel\Exception\ShouldNotHappenException;
+
+final class PHPStanNeonContentProvider
+{
+    private SourcePackagesProvider $sourcePackagesProvider;
+
+    /**
+     * @var NeonPrinter
+     */
+    private $neonPrinter;
+
+    public function __construct(
+        SourcePackagesProvider $sourcePackagesProvider,
+        NeonPrinter $neonPrinter
+    ) {
+        $this->sourcePackagesProvider = $sourcePackagesProvider;
+        $this->neonPrinter = $neonPrinter;
+    }
+
+    public function provideContent(bool $skipUnmigrated): string
+    {
+        $sourcePackages = $this->sourcePackagesProvider->provideSourcePackages(true, $skipUnmigrated);
+
+        $phpstanConfigIncludes = [];
+        foreach ($sourcePackages as $package) {
+            $packagePHPStanConfigFilePath = $package . DIRECTORY_SEPARATOR . 'phpstan.neon.dist';
+            if (!file_exists($packagePHPStanConfigFilePath)) {
+                throw new ShouldNotHappenException(sprintf(
+                    'PHPStan config file "%s" does not exist',
+                    $packagePHPStanConfigFilePath
+                ));
+            }
+
+            $phpstanConfigIncludes[] = $packagePHPStanConfigFilePath;
+        }
+        $phpStanNeon = [
+            'includes' => $phpstanConfigIncludes,
+        ];
+
+        return $this->neonPrinter->printNeon($phpStanNeon);
+    }
+}
