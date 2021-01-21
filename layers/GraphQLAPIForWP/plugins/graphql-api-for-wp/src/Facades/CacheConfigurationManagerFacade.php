3482b9ee0be9826be8b67835e9027e74580511ef
Leonardo Losoviz <leo@getpop.org>
2021-01-21 14:50:00 +0800

    Moved setting the namespace to ComponentModel

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Facades/CacheConfigurationManagerFacade.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Facades/CacheConfigurationManagerFacade.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace GraphQLAPI\GraphQLAPI\Facades;
 
 use GraphQLAPI\GraphQLAPI\Cache\CacheConfigurationManager;
-use GraphQLAPI\GraphQLAPI\Cache\CacheConfigurationManagerInterface;
+use PoP\ComponentModel\Cache\CacheConfigurationManagerInterface;
 
 /**
  * Obtain an instance of the CacheConfigurationManager.
