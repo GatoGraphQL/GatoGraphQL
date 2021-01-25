c67aa5509c9079be843188069afb8d72e72d28d0
Leonardo Losoviz <leo@getpop.org>
2021-01-25 18:40:16 +0800

    Use the dist repo for GitHub Updater

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/graphql-api.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/graphql-api.php
@@ -12,8 +12,7 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 Text Domain: graphql-api
 Domain Path: /languages
-GitHub Plugin URI: https://github.com/GraphQLAPI/graphql-api-for-wp
-Release Asset: true
+GitHub Plugin URI: GraphQLAPI/graphql-api-for-wp-dist
 */
 
 // Exit if accessed directly
