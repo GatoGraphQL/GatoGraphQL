e12ccbeb4d2117ba0b5c3cfa8a4b55438ba90877
Leonardo Losoviz <leo@getpop.org>
2021-01-27 14:28:01 +0800

    Don't call functions from root, or they get scoped

--- layers/Schema/packages/migrate-media/migrate/initialization.php
+++ layers/Schema/packages/migrate-media/migrate/initialization.php
@@ -5,7 +5,7 @@ class Initialization
 {
     public function initialize()
     {
-        \load_plugin_textdomain('pop-media', false, dirname(\plugin_basename(__FILE__)).'/languages');
+        load_plugin_textdomain('pop-media', false, dirname(plugin_basename(__FILE__)).'/languages');
 
         /**
          * Load the Contracts
