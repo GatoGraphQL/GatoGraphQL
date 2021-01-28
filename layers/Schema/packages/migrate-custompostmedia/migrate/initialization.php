a6c3dffb4ec5ab3a61d93c8194c4d6755d216f56
Leonardo Losoviz <leo@getpop.org>
2021-01-28 16:30:03 +0800

    Removed POP_MEDIA_IMAGE_NOFEATUREDIMAGEPOST

--- layers/Schema/packages/migrate-custompostmedia/migrate/initialization.php
+++ layers/Schema/packages/migrate-custompostmedia/migrate/initialization.php
@@ -6,11 +6,6 @@ class Initialization
     public function initialize()
     {
         /**
-         * Load the Config
-         */
-        include_once 'config/load.php';
-
-        /**
          * Load the Kernel
          */
         include_once 'kernel/load.php';
