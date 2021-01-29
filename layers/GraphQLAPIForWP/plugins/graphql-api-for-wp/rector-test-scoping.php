537c5ea826a44619314e2639dcec678b80ddde0a
Leonardo Losoviz <leo@getpop.org>
2021-01-30 02:27:57 +0800

    Removed commented code

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/rector-test-scoping.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/rector-test-scoping.php
@@ -26,10 +26,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::AUTOLOAD_PATHS, [
         __DIR__ . '/vendor/scoper-autoload.php',
         __DIR__ . '/vendor/erusev/parsedown/Parsedown.php',
-        // // full directory
-        // __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php',
-        // // Avoid error: "Class EM_Event not found"
-        // __DIR__ . '/stubs/wpackagist-plugin/events-manager/em-stubs.php',
     ]);
 
     // files to rector
@@ -57,19 +53,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/vendor/symfony/string/Slugger/AsciiSlugger.php',
         __DIR__ . '/vendor/symfony/yaml/Command/LintCommand.php',
     ]);
-
-    // /**
-    //  * This constant is defined in wp-load.php, but never loaded.
-    //  * It is read when resolving class WP_Upgrader in Plugin.php.
-    //  * Define it here again, or otherwise Rector fails with message:
-    //  *
-    //  * "PHP Warning:  Use of undefined constant ABSPATH -
-    //  * assumed 'ABSPATH' (this will throw an Error in a future version of PHP)
-    //  * in .../graphql-api-for-wp/vendor/wordpress/wordpress/wp-admin/includes/class-wp-upgrader.php
-    //  * on line 13"
-    //  */
-    // /** Define ABSPATH as this file's directory */
-    // if (!defined('ABSPATH')) {
-    //     define('ABSPATH', __DIR__ . '/vendor/wordpress/wordpress/');
-    // }
 };
