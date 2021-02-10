2039cf8252efe3d0ec99acf0eba6977bc87e1fff
Leonardo Losoviz <leo@getpop.org>
2021-02-10 19:03:00 +0800

    Use PHP config for DI

--- layers/Engine/packages/root/src/Component/YAMLServicesTrait.php
+++ layers/Engine/packages/root/src/Component/YAMLServicesTrait.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace PoP\Root\Component;
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use PoP\Root\Container\ContainerBuilderFactory;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 trait YAMLServicesTrait
 {
@@ -27,9 +29,51 @@ trait YAMLServicesTrait
         // First check if the container has been cached. If so, do nothing
         if (!ContainerBuilderFactory::isCached()) {
             // Initialize the ContainerBuilder with this component's service implementations
+            /** @var ContainerBuilder */
             $containerBuilder = ContainerBuilderFactory::getInstance();
-            $componentPath = $componentDir . '/config' . ($configPath ? '/' . trim($configPath, '/') : '');
+            $componentPath = self::getComponentPath($componentDir, $configPath);
             $loader = new YamlFileLoader($containerBuilder, new FileLocator($componentPath));
+            $loader->load($fileName);
+        }
+    }
+
+    /**
+     * Initialize the services defiend in the YAML configuration file.
+     * If not provided, use "services.yaml"
+     *
+     * @param string $componentDir
+     * @param string $configPath
+     * @param string $fileName
+     * @return void
+     */
+    protected static function getComponentPath(
+        string $componentDir,
+        string $configPath = ''
+    ): string {
+        return $componentDir . '/config' . ($configPath ? '/' . trim($configPath, '/') : '');
+    }
+
+    /**
+     * Initialize the services defiend in the PHP configuration file.
+     * If not provided, use "services.yaml"
+     *
+     * @param string $componentDir
+     * @param string $configPath
+     * @param string $fileName
+     * @return void
+     */
+    public static function initPHPServices(
+        string $componentDir,
+        string $configPath = '',
+        string $fileName = 'services.php'
+    ): void {
+        // First check if the container has been cached. If so, do nothing
+        if (!ContainerBuilderFactory::isCached()) {
+            // Initialize the ContainerBuilder with this component's service implementations
+            /** @var ContainerBuilder */
+            $containerBuilder = ContainerBuilderFactory::getInstance();
+            $componentPath = self::getComponentPath($componentDir, $configPath);
+            $loader = new PhpFileLoader($containerBuilder, new FileLocator($componentPath));
             $loader->load($fileName);
         }
     }
@@ -54,5 +98,27 @@ trait YAMLServicesTrait
             return;
         }
         self::initYAMLServices($componentDir, $configPath, $fileName);
+    }
+
+    /**
+     * If param `$skipSchema` is `true`, initialize the schema services defiend in the YAML configuration file.
+     * If not provided, use "schema-services.yaml"
+     *
+     * @param string $componentDir
+     * @param boolean $skipSchema
+     * @param string $configPath
+     * @param string $fileName
+     * @return void
+     */
+    public static function maybeInitPHPSchemaServices(
+        string $componentDir,
+        bool $skipSchema,
+        string $configPath = '',
+        string $fileName = 'schema-services.php'
+    ): void {
+        if ($skipSchema) {
+            return;
+        }
+        self::initPHPServices($componentDir, $configPath, $fileName);
     }
 }
