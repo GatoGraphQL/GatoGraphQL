3b0c7742219b7728609bf4945396c2105e0403ac
Leonardo Losoviz <leo@getpop.org>
2021-02-14 16:19:35 +0800

    Created CompilerPass to inject service IDs

---
+++ layers/Engine/packages/root/src/Container/CompilerPasses/AbstractInjectServiceDefinitionIDIntoRegistryCompilerPass.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Root\Container\CompilerPasses;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+abstract class AbstractInjectServiceDefinitionIDIntoRegistryCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $containerBuilder): void
+    {
+        if (!$this->enabled()) {
+            return;
+        }
+        $registryDefinition = $containerBuilder->getDefinition($this->getRegistryServiceDefinition());
+        $definitions = $containerBuilder->getDefinitions();
+        foreach ($definitions as $definitionID => $definition) {
+            $definitionClass = $definition->getClass();
+            if ($definitionClass === null || !is_a($definitionClass, $this->getServiceClass(), true)) {
+                continue;
+            }
+
+            // Register the service in the corresponding registry
+            $registryDefinition->addMethodCall(
+                $this->getRegistryMethodCallName(),
+                [$definitionID]
+            );
+        }
+    }
+
+    abstract protected function getRegistryServiceDefinition(): string;
+    abstract protected function getServiceClass(): string;
+    abstract protected function getRegistryMethodCallName(): string;
+
+    protected function enabled(): bool
+    {
+        return true;
+    }
+}
