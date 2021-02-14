3f36c5af3d3e7ebbae2ac928c9084bb7ccbc5c83
Leonardo Losoviz <leo@getpop.org>
2021-02-14 19:21:21 +0800

    Commented out overriding clients

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/config/ConditionalOnEnvironment/GraphiQLExplorerInSingleEndpointPublicClient/Overrides/services.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/config/ConditionalOnEnvironment/GraphiQLExplorerInSingleEndpointPublicClient/Overrides/services.php
@@ -3,17 +3,15 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use GraphQLByPoP\GraphQLClientsForWP\Clients\GraphiQLClient as UpstreamGraphiQLClient;
-use GraphQLAPI\GraphQLAPI\ConditionalOnEnvironment\GraphiQLExplorerInSingleEndpointPublicClient\Overrides\Services\Clients\GraphiQLWithExplorerClient;
 
 return function (ContainerConfigurator $configurator) {
     $services = $configurator->services()
         ->defaults()
             ->public()
             ->autowire();
-    // Override the GraphiQL clients
-    $services->set(
-        UpstreamGraphiQLClient::class,
-        GraphiQLWithExplorerClient::class
-    );
+    // // Override the GraphiQL clients
+    // $services->set(
+    //     UpstreamGraphiQLClient::class,
+    //     GraphiQLWithExplorerClient::class
+    // );
 };
--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/config/Overrides/services.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/config/Overrides/services.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use GraphQLByPoP\GraphQLClientsForWP\Clients\GraphiQLClient as UpstreamGraphiQLClient;
-use GraphQLAPI\GraphQLAPI\Overrides\Services\Clients\GraphiQLClient;
 
 return function (ContainerConfigurator $configurator) {
     $services = $configurator->services()
@@ -12,8 +10,8 @@ return function (ContainerConfigurator $configurator) {
             ->public()
             ->autowire();
     // Override the GraphiQL clients
-    $services->set(
-        UpstreamGraphiQLClient::class,
-        GraphiQLClient::class
-    );
+    // $services->set(
+    //     UpstreamGraphiQLClient::class,
+    //     GraphiQLClient::class
+    // );
 };
