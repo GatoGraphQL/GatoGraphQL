6d085de5eb2105d0bac56cde250481c58877f0f2
Leonardo Losoviz <leo@getpop.org>
2021-02-18 09:28:17 +0800

    Initialize directly

--- layers/Engine/packages/component-model/src/DataStructure/DataStructureManager.php
+++ layers/Engine/packages/component-model/src/DataStructure/DataStructureManager.php
@@ -9,34 +9,18 @@ use PoP\ComponentModel\State\ApplicationState;
 
 class DataStructureManager implements DataStructureManagerInterface
 {
-    protected bool $initialized = false;
     /**
      * @var array<string, DataStructureFormatterInterface>
      */
     public array $formatters = [];
-    /**
-     * @var DataStructureFormatterInterface[]
-     */
-    public array $dataStructureFormatters = [];
-
-    protected function maybeInitialize(): void
-    {
-        if (!$this->initialized) {
-            $this->initialized = true;
-            foreach ($this->dataStructureFormatters as $formatter) {
-                $this->formatters[$formatter::getName()] = $formatter;
-            }
-        }
-    }
 
     public function addDataStructureFormatter(DataStructureFormatterInterface $formatter): void
     {
-        $this->dataStructureFormatters[] = $formatter;
+        $this->formatters[$formatter::getName()] = $formatter;
     }
 
     public function getDataStructureFormatter(string $name = null): DataStructureFormatterInterface
     {
-        $this->maybeInitialize();
         // Return the formatter if it exists
         if ($name && isset($this->formatters[$name])) {
             return $this->formatters[$name];
