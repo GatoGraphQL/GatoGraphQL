67a488bc0e0a50c7151681443e6b14ffc25ad755
Leonardo Losoviz <leo@getpop.org>
2021-02-26 07:46:17 +0800

    Inject ModuleRegistry

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/HybridServices/ModuleResolvers/AbstractModuleResolver.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/HybridServices/ModuleResolvers/AbstractModuleResolver.php
@@ -7,9 +7,16 @@ namespace GraphQLAPI\GraphQLAPI\HybridServices\ModuleResolvers;
 // use GraphQLAPI\GraphQLAPI\ComponentConfiguration;
 
 use GraphQLAPI\GraphQLAPI\HybridServices\ModuleResolvers\ModuleResolverInterface;
+use GraphQLAPI\GraphQLAPI\Registries\ModuleRegistryInterface;
 
 abstract class AbstractModuleResolver implements ModuleResolverInterface
 {
+    protected ModuleRegistryInterface $moduleRegistry;
+
+    function __construct(ModuleRegistryInterface $moduleRegistry)
+    {
+        $this->moduleRegistry = $moduleRegistry;
+    }
     /**
      * @return array<array> List of entries that must be satisfied, each entry is an array where at least 1 module must be satisfied
      */
