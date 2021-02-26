4ab1775078501e9623dc5b98ae91b59f9ec61385
Leonardo Losoviz <leo@getpop.org>
2021-02-26 10:26:56 +0800

    Removed initPHPServices

--- layers/Engine/packages/root/src/Component/InitializeContainerServicesInComponentTrait.php
+++ layers/Engine/packages/root/src/Component/InitializeContainerServicesInComponentTrait.php
@@ -9,7 +9,6 @@ use PoP\Root\Container\Loader\ForceAutoconfigureYamlFileLoader;
 use PoP\Root\Container\SystemContainerBuilderFactory;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 trait InitializeContainerServicesInComponentTrait
@@ -71,31 +70,6 @@ trait InitializeContainerServicesInComponentTrait
         string $configPath = ''
     ): string {
         return $componentDir . '/config' . ($configPath ? '/' . trim($configPath, '/') : '');
-    }
-
-    /**
-     * Initialize the services defined in the PHP configuration file.
-     * If not provided, use "services.yaml"
-     *
-     * @param string $componentDir
-     * @param string $configPath
-     * @param string $fileName
-     * @return void
-     */
-    public static function initPHPServices(
-        string $componentDir,
-        string $configPath = '',
-        string $fileName = 'services.php'
-    ): void {
-        // First check if the container has been cached. If so, do nothing
-        if (!ContainerBuilderFactory::isCached()) {
-            // Initialize the ContainerBuilder with this component's service implementations
-            /** @var ContainerBuilder */
-            $containerBuilder = ContainerBuilderFactory::getInstance();
-            $componentPath = self::getComponentPath($componentDir, $configPath);
-            $loader = new PhpFileLoader($containerBuilder, new FileLocator($componentPath));
-            $loader->load($fileName);
-        }
     }
 
     /**
