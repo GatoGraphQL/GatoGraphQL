5770ed247daab2757f39b08be53cd1c1c116e382
Leonardo Losoviz <leo@getpop.org>
2021-03-10 17:56:01 +0800

    Removed `overrideClass`

--- layers/Engine/packages/component-model/src/Instances/InstanceManagerTrait.php
+++ layers/Engine/packages/component-model/src/Instances/InstanceManagerTrait.php
@@ -12,15 +12,6 @@ trait InstanceManagerTrait
      * @var array<string, object>
      */
     private array $instances = [];
-    /**
-     * @var array<string, string>
-     */
-    private array $overridingClasses = [];
-
-    public function overrideClass(string $overrideClass, string $withClass): void
-    {
-        $this->overridingClasses[$overrideClass] = $withClass;
-    }
 
     protected function hasClassBeenLoaded(string $class): bool
     {
@@ -29,17 +20,14 @@ trait InstanceManagerTrait
 
     public function getImplementationClass(string $class): string
     {
-        // Allow a class to take the place of another one
-        return $this->overridingClasses[$class] ?? $class;
+        $containerBuilder = ContainerBuilderFactory::getInstance();
+        $instance = $containerBuilder->get($class);
+        return get_class($instance);
     }
 
     public function getClassInstance(string $class): object
     {
         if (!$this->hasClassBeenLoaded($class)) {
-            // Allow a class to take the place of another one
-            $class = $this->getImplementationClass($class);
-
-            // First ask the ContainerBuilder to handle the class as a Service
             $containerBuilder = ContainerBuilderFactory::getInstance();
             if ($containerBuilder->has($class)) {
                 $instance = $containerBuilder->get($class);
