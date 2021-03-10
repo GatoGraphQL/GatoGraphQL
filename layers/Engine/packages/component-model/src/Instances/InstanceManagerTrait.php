779beaf215f757d6ac95feafdad4003ca917b6da
Leonardo Losoviz <leo@getpop.org>
2021-03-10 18:01:13 +0800

    Let the container handle the instances, always

--- layers/Engine/packages/component-model/src/Instances/InstanceManagerTrait.php
+++ layers/Engine/packages/component-model/src/Instances/InstanceManagerTrait.php
@@ -8,41 +8,14 @@ use PoP\Root\Container\ContainerBuilderFactory;
 
 trait InstanceManagerTrait
 {
-    /**
-     * @var array<string, object>
-     */
-    private array $instances = [];
-
-    protected function hasClassBeenLoaded(string $class): bool
-    {
-        return !is_null($this->instances[$class] ?? null);
-    }
-
     public function getImplementationClass(string $class): string
     {
-        $containerBuilder = ContainerBuilderFactory::getInstance();
-        $instance = $containerBuilder->get($class);
-        return get_class($instance);
-    }
-
-    public function getClassInstance(string $class): object
-    {
-        if (!$this->hasClassBeenLoaded($class)) {
-            $containerBuilder = ContainerBuilderFactory::getInstance();
-            if ($containerBuilder->has($class)) {
-                $instance = $containerBuilder->get($class);
-            } else {
-                // Otherwise, assume the class needs no parameters
-                $instance = new $class();
-            }
-            $this->instances[$class] = $instance;
-        }
-
-        return $this->instances[$class];
+        return get_class($this->getInstance($class));
     }
 
     public function getInstance(string $class): object
     {
-        return $this->getClassInstance($class);
+        $containerBuilder = ContainerBuilderFactory::getInstance();
+        return $containerBuilder->get($class);
     }
 }
