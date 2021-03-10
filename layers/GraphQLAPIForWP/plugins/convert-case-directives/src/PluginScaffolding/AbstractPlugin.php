eab20fc7a7bd2e1215d4a61f1e3385be6ec8f71a
Leonardo Losoviz <leo@getpop.org>
2021-03-10 15:37:03 +0800

    Removed PluginConfiguration

--- layers/GraphQLAPIForWP/plugins/convert-case-directives/src/PluginScaffolding/AbstractPlugin.php
+++ layers/GraphQLAPIForWP/plugins/convert-case-directives/src/PluginScaffolding/AbstractPlugin.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\ConvertCaseDirectives\PluginScaffolding;
 
+use GraphQLAPI\GraphQLAPI\Facades\Registries\SystemModuleRegistryFacade;
 use GraphQLAPI\GraphQLAPI\Facades\UserSettingsManagerFacade;
 use GraphQLAPI\GraphQLAPI\Plugin as GraphQLAPIPlugin;
+use PoP\ComponentModel\Misc\GeneralUtils;
 use PoP\Engine\AppLoader;
 
 abstract class AbstractPlugin
@@ -128,8 +130,41 @@ abstract class AbstractPlugin
      */
     public function getSchemaComponentClassesToSkip(): array
     {
-        return [];
+        return static::getSkippingSchemaComponentClasses();
     }
+
+    /**
+     * Provide the classes of the components whose
+     * schema initialization must be skipped
+     *
+     * @return array
+     */
+    protected static function getSkippingSchemaComponentClasses(): array
+    {
+        $moduleRegistry = SystemModuleRegistryFacade::getInstance();
+
+        // Component classes are skipped if the module is disabled
+        $skipSchemaModuleComponentClasses = array_filter(
+            static::getModuleComponentClasses(),
+            function ($module) use ($moduleRegistry) {
+                return !$moduleRegistry->isModuleEnabled($module);
+            },
+            ARRAY_FILTER_USE_KEY
+        );
+        return GeneralUtils::arrayFlatten(
+            array_values(
+                $skipSchemaModuleComponentClasses
+            )
+        );
+    }
+
+    /**
+     * Provide the list of modules to check if they are enabled and,
+     * if they are not, what component classes must skip initialization
+     *
+     * @return array
+     */
+    abstract protected static function getModuleComponentClasses(): array;
 
     /**
      * Plugin's initialization
