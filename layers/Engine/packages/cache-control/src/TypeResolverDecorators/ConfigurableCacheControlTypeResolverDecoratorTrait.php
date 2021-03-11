625f8d471fb1f8757b82c0a5a280fa1ab2ea09ba
Leonardo Losoviz <leo@getpop.org>
2021-03-11 17:04:32 +0800

    Imported missing class

--- layers/Engine/packages/cache-control/src/TypeResolverDecorators/ConfigurableCacheControlTypeResolverDecoratorTrait.php
+++ layers/Engine/packages/cache-control/src/TypeResolverDecorators/ConfigurableCacheControlTypeResolverDecoratorTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PoP\CacheControl\TypeResolverDecorators;
 
 use PoP\CacheControl\DirectiveResolvers\AbstractCacheControlDirectiveResolver;
+use PoP\CacheControl\DirectiveResolvers\CacheControlDirectiveResolver;
 use PoP\ComponentModel\DirectiveResolvers\DirectiveResolverInterface;
 use PoP\ComponentModel\Facades\Instances\InstanceManagerFacade;
 use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
