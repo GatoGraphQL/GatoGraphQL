73bc691112af2e8745635ad28889d0ee923324d8
Leonardo Losoviz <leo@getpop.org>
2021-03-12 14:17:14 +0800

    Typed function

--- layers/Engine/packages/migrate-component-model/migrate/kernel/library/input-utils.php
+++ layers/Engine/packages/migrate-component-model/migrate/kernel/library/input-utils.php
@@ -3,11 +3,11 @@ namespace PoP\ComponentModel;
 
 class PoP_InputUtils
 {
-    public static function getMultipleInputName($name, $subname)
+    public static function getMultipleInputName(string $name, string $subname): string
     {
         // Use "_" instead of "-" so it's compatible with GraphQL
         // Then, a query field will be called "date_from" (allowed) instead of "date-from" (forbidden)
         // Otherwise it throws error: "Error: Names must match /^[_a-zA-Z][_a-zA-Z0-9]*$/ but "date-from" does not."
-        return $name.'_'.$subname;
+        return $name . '_' . $subname;
     }
 }
