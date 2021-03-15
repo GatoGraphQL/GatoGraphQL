82b9aa302ae68b5db8b971b30fa67283e63dcf92
Leonardo Losoviz <leo@getpop.org>
2021-03-15 17:38:37 +0800

    Added test

---
+++ layers/GraphQLAPIForWP/packages/plugin-skeleton/tests/Extension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace GraphQLAPI\PluginSkeleton;
+
+class Extension extends AbstractGraphQLAPIExtension
+{
+    /**
+     * Plugin main file
+     */
+    protected function getPluginFile(): string
+    {
+        return '';
+    }
+
+    /**
+     * Plugin name
+     */
+    protected function getPluginName(): string
+    {
+        return 'Some extension for testing';
+    }
+}
