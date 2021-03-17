28d280ef86954bd20c6c38bdac82caeda5d2f70f
Leonardo Losoviz <leo@getpop.org>
2021-03-17 15:50:53 +0800

    Removed overriding configure

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginSkeleton/AbstractExtension.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginSkeleton/AbstractExtension.php
@@ -108,14 +108,6 @@ abstract class AbstractExtension extends AbstractPlugin
     }
 
     /**
-     * Plugin's configuration
-     */
-    public function configure(): void
-    {
-        // Function to override
-    }
-
-    /**
      * Plugin's booting
      */
     public function boot(): void
