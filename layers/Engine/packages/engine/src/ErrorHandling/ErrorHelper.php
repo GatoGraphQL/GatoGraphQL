3c63685f1a899dd405c9bf152a12f2b9c94c360f
Leonardo Losoviz <leo@getpop.org>
2021-03-24 17:07:25 +0800

    Converted ErrorUtils to service ErrorHelper

---
+++ layers/Engine/packages/engine/src/ErrorHandling/ErrorHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Engine\ErrorHandling;
+
+class ErrorHelper implements ErrorHelperInterface
+{
+    public function __construct(protected ErrorManagerInterface $errorManager)
+    {
+    }
+
+    public function returnResultOrConvertError(mixed $result): mixed
+    {
+        if ($this->errorManager->isCMSError($result)) {
+            return $this->errorManager->convertFromCMSToPoPError($result);
+        }
+        return $result;
+    }
+}
