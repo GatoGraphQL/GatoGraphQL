4c18767eb35197ef62e8ed4e91ad77e457e1b6d9
Leonardo Losoviz <leo@getpop.org>
2021-03-24 14:20:19 +0800

    Fixed type

--- layers/Schema/packages/customposts/src/TypeAPIs/CustomPostTypeAPIInterface.php
+++ layers/Schema/packages/customposts/src/TypeAPIs/CustomPostTypeAPIInterface.php
@@ -15,7 +15,7 @@ interface CustomPostTypeAPIInterface extends CustomPostTypeInterface
      * Get the custom post with provided ID or, if it doesn't exist, null
      */
     public function getCustomPost(int | string $id): ?object;
-    public function getCustomPostType(mixed $objectOrID): string;
+    public function getCustomPostType(string | int | object $objectOrID): string;
     /**
      * @param array<string, mixed> $query
      * @param array<string, mixed> $options
