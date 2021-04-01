5ac375d0dc440ffe84e5c5b86e361839d0a3f87f
Leonardo Losoviz <leo@getpop.org>
2021-04-01 15:05:44 +0800

    Removed obsolete files

--- layers/Schema/packages/migrate-tags-wp/migrate/contractimplementations/cms-apis/load.php
+++ layers/Schema/packages/migrate-tags-wp/migrate/contractimplementations/cms-apis/load.php
@@ -3,6 +3,3 @@
 require_once 'cms-functionapi.php';
 require_once 'cms-objectpropertyresolver.php';
 require_once 'cms-routingstate-hooks.php';
-// require_once 'obsolete-cms-functionapi-categories.php';
-// require_once 'obsolete-cms-objectpropertyresolver-categories.php';
-// require_once 'obsolete-cms-routingstate-hooks-categories.php';
