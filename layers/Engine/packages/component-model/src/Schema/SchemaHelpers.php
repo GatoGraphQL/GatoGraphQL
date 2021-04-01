9ba6488ef4229e8bc37eadc1b7c6a242bfb465f5
Leonardo Losoviz <leo@getpop.org>
2021-04-01 17:25:42 +0800

    Removed unused import

--- layers/Engine/packages/component-model/src/Schema/SchemaHelpers.php
+++ layers/Engine/packages/component-model/src/Schema/SchemaHelpers.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace PoP\ComponentModel\Schema;
 
 use InvalidArgumentException;
-use PoP\ComponentModel\Environment;
 use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\Translation\Facades\TranslationAPIFacade;
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
