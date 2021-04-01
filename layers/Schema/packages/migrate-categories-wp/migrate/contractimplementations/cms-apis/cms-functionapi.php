1f0d486914f944bc2874dfb29d8fa91629341129
Leonardo Losoviz <leo@getpop.org>
2021-04-01 14:25:15 +0800

    Fixed getCategoryURL

--- layers/Schema/packages/migrate-categories-wp/migrate/contractimplementations/cms-apis/cms-functionapi.php
+++ layers/Schema/packages/migrate-categories-wp/migrate/contractimplementations/cms-apis/cms-functionapi.php
@@ -18,30 +18,68 @@ abstract class AbstractFunctionAPI extends \PoPSchema\Taxonomies\WP\FunctionAPI 
         \PoPSchema\PostCategories\FunctionAPIFactory::setInstance($this);
     }
 
-    // protected function returnCategoryObjectsOrIDs($categories, $options = []): array
-    // {
-    //     $return_type = $options['return-type'] ?? null;
-    //     if ($return_type == ReturnTypes::IDS) {
-    //         return array_map(
-    //             function ($category) {
-    //                 return $category->term_id;
-    //             },
-    //             $categories
-    //         );
-    //     }
+    /**
+     * Implement this function by the actual service
+     */
+    abstract protected function getTaxonomyName(): string;
+    /**
+     * Implement this function by the actual service
+     */
+    abstract protected function getCategoryBaseOption(): string;
 
-    //     return $categories;
-    // }
-    // public function getCategories($query, $options = []): array
-    // {
-    //     if (isset($query['hide-empty'])) {
-    //         $query['hide_empty'] = $query['hide-empty'];
-    //         unset($query['hide-empty']);
-    //     }
-    //     return $this->returnCategoryObjectsOrIDs((array) get_categories($query));
-    // }
+    public function getCategoryName($category_id)
+    {
+        $category = get_term($category_id, $this->getTaxonomyName());
+        return $category->name;
+    }
+    public function getCategory($category_id)
+    {
+        return get_category($category_id, $this->getTaxonomyName());
+    }
+    public function getCategoryByName($category_name)
+    {
+        return get_term_by('name', $category_name, $this->getTaxonomyName());
+    }
+    public function getCustomPostCategories($post_id, array $query = [], array $options = []): array
+    {
+        $query = $this->convertCategoriesQuery($query, $options);
 
+        return \wp_get_post_terms($post_id, $this->getTaxonomyName(), $query);
+    }
+    public function getCustomPostCategoryCount($post_id, array $query = [], array $options = []): int
+    {
+        // There is no direct way to calculate the total
+        // (Documentation mentions to pass arg "count" => `true` to `wp_get_post_categories`,
+        // but it doesn't work)
+        // So execute a normal `wp_get_post_categories` retrieving all the IDs, and count them
+        $options['return-type'] = ReturnTypes::IDS;
+        $query = $this->convertCategoriesQuery($query, $options);
 
+        // All results, no offset
+        $query['number'] = 0;
+        unset($query['offset']);
+
+        // Resolve and count
+        $categories = \wp_get_post_terms($post_id, $this->getTaxonomyName(), $query);
+        return count($categories);
+    }
+    public function getCategoryCount($query = [], $options = []): int
+    {
+        // There is no direct way to calculate the total
+        // (Documentation mentions to pass arg "count" => `true` to `get_categories`,
+        // but it doesn't work)
+        // So execute a normal `get_categories` retrieving all the IDs, and count them
+        $options['return-type'] = ReturnTypes::IDS;
+        $query = $this->convertCategoriesQuery($query, $options);
+
+        // All results, no offset
+        $query['number'] = 0;
+        unset($query['offset']);
+
+        // Resolve and count
+        $categories = get_categories($query, ['taxonomy' => $this->getTaxonomyName()]);
+        return count($categories);
+    }
     public function getCategories($query, $options = []): array
     {
         $query = $this->convertCategoriesQuery($query, $options);
@@ -60,6 +98,11 @@ abstract class AbstractFunctionAPI extends \PoPSchema\Taxonomies\WP\FunctionAPI 
 
         // Accept field atts to filter the API fields
         $this->maybeFilterDataloadQueryArgs($query, $options);
+
+        if (isset($query['hide-empty'])) {
+            $query['hide_empty'] = $query['hide-empty'];
+            unset($query['hide-empty']);
+        }
 
         // Convert the parameters
         if (isset($query['include'])) {
@@ -107,44 +150,81 @@ abstract class AbstractFunctionAPI extends \PoPSchema\Taxonomies\WP\FunctionAPI 
             $options
         );
     }
-
-    public function getCategoryCount($query, $options = []): int
+    public function getCategoryURL($category_id)
     {
-        $options['return-type'] = ReturnTypes::IDS;
-
-        // All results, no offset
-        $query['number'] = 0;
-        unset($query['offset']);
-
-        return count($this->getCategories($query, $options));
+        return get_term_link($category_id, $this->getTaxonomyName());
+    }
+    public function getCategoryBase()
+    {
+        $cmsService = CMSServiceFacade::getInstance();
+        return $cmsService->getOption($this->getCategoryBaseOption());
     }
 
-    public function getCustomPostCategories($post_id, array $options = []): array
+    public function setPostCategories($post_id, array $categories, bool $append = false)
     {
-        $query = [];
-        if ($return_type = $options['return-type'] ?? null) {
-            if ($return_type == ReturnTypes::IDS) {
-                $query['fields'] = 'ids';
-            } elseif ($return_type == ReturnTypes::SLUGS) {
-                $query['fields'] = 'slugs';
-            }
-        }
-        return (array) wp_get_post_categories($post_id, $query);
+        return wp_set_post_terms($post_id, $categories, $this->getTaxonomyName(), $append);
     }
-    public function getCustomPostCategoryCount($query, $options = []): int
-    {
-        $options['return-type'] = ReturnTypes::IDS;
 
-        // All results, no offset
-        $query['number'] = 0;
-        unset($query['offset']);
+    // protected function returnCategoryObjectsOrIDs($categories, $options = []): array
+    // {
+    //     $return_type = $options['return-type'] ?? null;
+    //     if ($return_type == ReturnTypes::IDS) {
+    //         return array_map(
+    //             function ($category) {
+    //                 return $category->term_id;
+    //             },
+    //             $categories
+    //         );
+    //     }
 
-        return count($this->getCustomPostCategories($query, $options));
-    }
-    public function getCategoryName($cat_id)
-    {
-        return get_cat_name($cat_id);
-    }
+    //     return $categories;
+    // }
+    // public function getCategories($query, $options = []): array
+    // {
+    //     if (isset($query['hide-empty'])) {
+    //         $query['hide_empty'] = $query['hide-empty'];
+    //         unset($query['hide-empty']);
+    //     }
+    //     return $this->returnCategoryObjectsOrIDs((array) get_categories($query));
+    // }
+
+    // public function getCategoryCount($query, $options = []): int
+    // {
+    //     $options['return-type'] = ReturnTypes::IDS;
+
+    //     // All results, no offset
+    //     $query['number'] = 0;
+    //     unset($query['offset']);
+
+    //     return count($this->getCategories($query, $options));
+    // }
+
+    // public function getCustomPostCategories($post_id, array $options = []): array
+    // {
+    //     $query = [];
+    //     if ($return_type = $options['return-type'] ?? null) {
+    //         if ($return_type == ReturnTypes::IDS) {
+    //             $query['fields'] = 'ids';
+    //         } elseif ($return_type == ReturnTypes::SLUGS) {
+    //             $query['fields'] = 'slugs';
+    //         }
+    //     }
+    //     return (array) wp_get_post_categories($post_id, $query);
+    // }
+    // public function getCustomPostCategoryCount($query, $options = []): int
+    // {
+    //     $options['return-type'] = ReturnTypes::IDS;
+
+    //     // All results, no offset
+    //     $query['number'] = 0;
+    //     unset($query['offset']);
+
+    //     return count($this->getCustomPostCategories($query, $options));
+    // }
+    // public function getCategoryName($cat_id)
+    // {
+    //     return get_cat_name($cat_id);
+    // }
     public function getCategoryParent($cat_id)
     {
         $category = get_category($cat_id);
@@ -177,9 +257,9 @@ abstract class AbstractFunctionAPI extends \PoPSchema\Taxonomies\WP\FunctionAPI 
     {
         return has_category($cat_id, $post_id);
     }
-    public function getCategoryURL($cat)
-    {
-        return get_term_link($cat->term_id, 'category');
-    }
+    // public function getCategoryURL($cat)
+    // {
+    //     return get_term_link($cat->term_id, 'category');
+    // }
 }
 
