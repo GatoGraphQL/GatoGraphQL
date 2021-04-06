f945c5a1d1f718c6fc5ef7aa8ea36f6edf09e1eb
Leonardo Losoviz <leo@getpop.org>
2021-04-06 13:40:16 +0800

    Reordered functions

--- layers/Schema/packages/tags/src/TypeAPIs/TagTypeAPIInterface.php
+++ layers/Schema/packages/tags/src/TypeAPIs/TagTypeAPIInterface.php
@@ -16,19 +16,20 @@ interface TagTypeAPIInterface extends TaxonomyTypeAPIInterface
      */
     public function isInstanceOfTagType(object $object): bool;
 
-    public function getCustomPostTags(string | int $customPostID, array $query = [], array $options = []): array;
-    public function getCustomPostTagCount(string | int $customPostID, array $query = [], array $options = []): int;
+    public function getTagID(object $tag): string | int;
     public function getTag(string | int $tagID): object;
     public function getTagByName(string $tagName): object;
     public function getTags(array $query, array $options = []): array;
     public function getTagCount(array $query = [], array $options = []): int;
     public function getTagURL(string | int $tagID): string;
     public function getTagName(string | int | object $tagObjectOrID): string;
-    public function getTagBase(): string;
-    public function setPostTags(string | int $customPostID, array $tags, bool $append = false);
-
     public function getTagSlug(string | int | object $tagObjectOrID): string;
     public function getTagDescription(string | int | object $tagObjectOrID): string;
     public function getTagItemCount(string | int | object $tagObjectOrID): int;
-    public function getTagID(object $tag): string | int;
+
+    public function getCustomPostTags(string | int $customPostID, array $query = [], array $options = []): array;
+    public function getCustomPostTagCount(string | int $customPostID, array $query = [], array $options = []): int;
+
+    public function getTagBase(): string;
+    public function setPostTags(string | int $customPostID, array $tags, bool $append = false);
 }
