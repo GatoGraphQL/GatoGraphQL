4db78038b2b2891df2aa6ca730ea47a5b05d89ba
Leonardo Losoviz <leo@getpop.org>
2021-04-07 10:22:51 +0800

    Use code from upstream package

---
+++ layers/Schema/packages/post-tag-mutations/src/FieldResolvers/SetTagsOnPostFieldResolverTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\PostTagMutations\FieldResolvers;
+
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoPSchema\Posts\TypeResolvers\PostTypeResolver;
+use PoPSchema\PostTagMutations\MutationResolvers\SetTagsOnPostMutationResolver;
+
+trait SetTagsOnPostFieldResolverTrait
+{
+    protected function getTypeResolverClass(): string
+    {
+        return PostTypeResolver::class;
+    }
+
+    protected function getTypeMutationResolverClass(): string
+    {
+        return SetTagsOnPostMutationResolver::class;
+    }
+
+    protected function getEntityName(): string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        return $translationAPI->__('post', 'post-tag-mutations');
+    }
+}
