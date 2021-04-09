bd71ff99b605ebfe2ea5a0bd1a5467a5f1fe41b7
Leonardo Losoviz <leo@getpop.org>
2021-04-09 18:24:15 +0800

    Made `posts(order: "title")` work (with no "|ASC")

--- layers/Schema/packages/schema-commons/src/FormInputs/OrderFormInput.php
+++ layers/Schema/packages/schema-commons/src/FormInputs/OrderFormInput.php
@@ -13,9 +13,11 @@ class OrderFormInput extends SelectFormInput
         if ($value = parent::getValue($source)) {
             // There must be exactly 2 elements: orderby|order
             $elems = explode('|', $value);
-            if (count($elems) == 2) {
+            if (count($elems) >= 2) {
                 return array('orderby' => $elems[0], 'order' => $elems[1]);
             }
+            // Passing only 1 item also works: orderby
+            return array('orderby' => $elems[0]);
         }
 
         return null;
