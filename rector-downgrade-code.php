4c0f3b6dd69ee4e69e5c66c2750c07237f67dd4f
Leonardo Losoviz <leo@getpop.org>
2021-04-10 15:55:28 +0800

    Skip DowngradePregUnmatchedAsNullConstantRector

--- rector-downgrade-code.php
+++ rector-downgrade-code.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
-use Rector\DowngradePhp72\Rector\FuncCall\DowngradePregUnmatchedAsNullConstantRector;
 use Rector\DowngradePhp72\Rector\FunctionLike\DowngradeObjectTypeDeclarationRector;
 use Rector\Set\ValueObject\DowngradeSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -22,17 +21,18 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     ]);
 
     /**
-     * Downgrade PHP 7.2:
-     * Run all rules except DowngradeParameterTypeWideningRector, which is buggy
-     *
-     * @see https://github.com/leoloso/PoP/issues/597
+     * Downgrade PHP 7.2 - Temporarily skip buggy rules:
+     * - DowngradeParameterTypeWideningRector
+     *   @see https://github.com/leoloso/PoP/issues/597)
+     * - DowngradePregUnmatchedAsNullConstantRector
+     *   @see https://github.com/leoloso/PoP/issues/598
      *
      * Source code from: vendor/rector/rector/config/set/downgrade-php72.php
      */
     $services = $containerConfigurator->services();
     $services->set(DowngradeObjectTypeDeclarationRector::class);
     // $services->set(DowngradeParameterTypeWideningRector::class);
-    $services->set(DowngradePregUnmatchedAsNullConstantRector::class);
+    // $services->set(DowngradePregUnmatchedAsNullConstantRector::class);
 
     // is your PHP version different from the one your refactor to? [default: your PHP version]
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_71);
