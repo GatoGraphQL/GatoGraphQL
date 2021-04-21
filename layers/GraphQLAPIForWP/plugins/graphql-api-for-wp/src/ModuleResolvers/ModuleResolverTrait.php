fb6b0ebf983119ae5c044501cba3b947d9776e1f
Leonardo Losoviz <leo@getpop.org>
2021-04-21 12:09:40 +0800

    Use trait

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ModuleResolvers/ModuleResolverTrait.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ModuleResolvers/ModuleResolverTrait.php
@@ -4,26 +4,11 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\ModuleResolvers;
 
+use GraphQLAPI\GraphQLAPI\ContentProcessors\PluginMarkdownContentRetrieverTrait;
 use GraphQLAPI\GraphQLAPI\ModuleResolvers\HasMarkdownDocumentationModuleResolverTrait;
-use GraphQLAPI\GraphQLAPI\PluginInfo;
 
 trait ModuleResolverTrait
 {
     use HasMarkdownDocumentationModuleResolverTrait;
-
-    /**
-     * Get the dir where to look for the documentation.
-     */
-    protected function getBaseDir(): string
-    {
-        return PluginInfo::get('dir');
-    }
-
-    /**
-     * Get the URL where to look for the documentation.
-     */
-    protected function getBaseURL(): string
-    {
-        return PluginInfo::get('url');
-    }
+    use PluginMarkdownContentRetrieverTrait;
 }
