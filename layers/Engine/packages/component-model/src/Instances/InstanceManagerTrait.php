b986d6bda4ba4bc20cc121fb73a679117f20450b
Leonardo Losoviz <leo@getpop.org>
2021-04-23 13:26:50 +0800

    Created SystemInstanceManager

--- layers/Engine/packages/component-model/src/Instances/InstanceManagerTrait.php
+++ layers/Engine/packages/component-model/src/Instances/InstanceManagerTrait.php
@@ -4,18 +4,10 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Instances;
 
-use PoP\Root\Container\ContainerBuilderFactory;
-
 trait InstanceManagerTrait
 {
     public function getInstanceClass(string $class): string
     {
         return get_class($this->getInstance($class));
-    }
-
-    public function getInstance(string $class): object
-    {
-        $containerBuilder = ContainerBuilderFactory::getInstance();
-        return $containerBuilder->get($class);
     }
 }
