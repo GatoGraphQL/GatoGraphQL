02299b18682420db0cc9ddd9a0fabaca30ca61b7
Leonardo Losoviz <leo@getpop.org>
2021-05-26 03:50:00 +0800

    Created RequestHelperService

---
+++ layers/Engine/packages/component-model/src/Facades/HelperServices/RequestHelperServiceFacade.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\Facades\HelperServices;
+
+use PoP\ComponentModel\HelperServices\RequestHelperServiceInterface;
+use PoP\Root\Container\ContainerBuilderFactory;
+
+class RequestHelperServiceFacade
+{
+    public static function getInstance(): RequestHelperServiceInterface
+    {
+        /**
+         * @var RequestHelperServiceInterface
+         */
+        $service = ContainerBuilderFactory::getInstance()->get(RequestHelperServiceInterface::class);
+        return $service;
+    }
+}
