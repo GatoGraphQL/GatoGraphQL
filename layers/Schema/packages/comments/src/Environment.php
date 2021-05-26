decdf6531bae3a12b46335797e9822269849a104
Leonardo Losoviz <leo@getpop.org>
2021-05-26 09:18:01 +0800

    Created `mustUserBeLoggedInToAddComment`

---
+++ layers/Schema/packages/comments/src/Environment.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\Comments;
+
+class Environment
+{
+    public const MUST_USER_BE_LOGGED_IN_TO_ADD_COMMENT = 'MUST_USER_BE_LOGGED_IN_TO_ADD_COMMENT';
+}
