a7018f3a6e54153183036a1c5db3db2f11001d9e
Leonardo Losoviz <leo@getpop.org>
2021-05-26 13:46:02 +0800

    Removed dependency on migrate

--- layers/Schema/packages/taxonomyquery/src/Component.php
+++ layers/Schema/packages/taxonomyquery/src/Component.php
@@ -22,13 +22,4 @@ class Component extends AbstractComponent
             \PoPSchema\TaxonomyMeta\Component::class,
         ];
     }
-
-    public static function getDependedMigrationPlugins(): array
-    {
-        $packageName = basename(dirname(__DIR__));
-        $folder = dirname(__DIR__, 2);
-        return [
-            $folder . '/migrate-' . $packageName . '/initialize.php',
-        ];
-    }
 }
