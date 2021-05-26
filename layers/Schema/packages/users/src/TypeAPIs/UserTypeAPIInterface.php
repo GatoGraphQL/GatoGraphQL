ff8ae92a7b0ef33acabf6b2ab97506e112d8e492
Leonardo Losoviz <leo@getpop.org>
2021-05-26 16:42:11 +0800

    Moved functions

--- layers/Schema/packages/users/src/TypeAPIs/UserTypeAPIInterface.php
+++ layers/Schema/packages/users/src/TypeAPIs/UserTypeAPIInterface.php
@@ -13,4 +13,16 @@ interface UserTypeAPIInterface
      * Indicates if the passed object is of type User
      */
     public function isInstanceOfUserType(object $object): bool;
+
+    public function getUserById($value);
+    public function getUserByEmail($value);
+    public function getUsers($query = array(), array $options = []): array;
+    public function getUserCount(array $query = [], array $options = []): int;
+    public function getUserDisplayName($user_id);
+    public function getUserEmail($user_id);
+    public function getUserFirstname($user_id);
+    public function getUserLastname($user_id);
+    public function getUserLogin($user_id);
+    public function getUserDescription($user_id);
+    public function getUserURL($user_id);
 }
