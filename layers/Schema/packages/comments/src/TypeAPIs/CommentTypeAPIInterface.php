04742b9ecc99e78c5ba41b549a6c9a8c67d8c653
Leonardo Losoviz <leo@getpop.org>
2021-05-26 09:28:41 +0800

    Migrated TypeAPI for Comments

--- layers/Schema/packages/comments/src/TypeAPIs/CommentTypeAPIInterface.php
+++ layers/Schema/packages/comments/src/TypeAPIs/CommentTypeAPIInterface.php
@@ -13,4 +13,9 @@ interface CommentTypeAPIInterface
      * Indicates if the passed object is of type Comment
      */
     public function isInstanceOfCommentType(object $object): bool;
+
+    public function getComments(array $query, array $options = []): array;
+    public function getComment(string | int $comment_id): ?object;
+    public function getCommentNumber(string | int $post_id): int;
+    public function areCommentsOpen(string | int $post_id): bool;
 }
