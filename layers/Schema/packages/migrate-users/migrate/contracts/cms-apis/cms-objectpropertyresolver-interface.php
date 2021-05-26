cbea2c8248034ea9db81f1caf6b75a59142e5fb1
Leonardo Losoviz <leo@getpop.org>
2021-05-26 17:08:31 +0800

    Removed migrated API

--- layers/Schema/packages/migrate-users/migrate/contracts/cms-apis/cms-objectpropertyresolver-interface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace PoPSchema\Users;
-
-interface ObjectPropertyResolver
-{
-    public function getUserNicename($user);
-    public function getUserSlug($user);
-    public function getUserId($user);
-}
