d615f19af2ec3ecb7fc331fec6129556af67e676
Leonardo Losoviz <leo@getpop.org>
2021-05-26 13:36:28 +0800

    Removed migrate-metaquery(-wp)

--- layers/Schema/packages/migrate-metaquery-wp/migrate/plugins/pop-users/kernel/cms-functionapi-hooks.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace PoPSchema\MetaQuery\WP;
-use PoP\Hooks\Facades\HooksAPIFacade;
-
-HooksAPIFacade::getInstance()->addFilter(
-    'CMSAPI:users:query',
-    array(FunctionAPIUtils::class, 'convertMetaQuery')
-);
