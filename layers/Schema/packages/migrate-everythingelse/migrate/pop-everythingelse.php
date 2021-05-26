0742d94759dced933519100d717dee8beb386834
Leonardo Losoviz <leo@getpop.org>
2021-05-26 08:10:59 +0800

    Removed migrate-commentmeta(-wp)

--- layers/Schema/packages/migrate-everythingelse/migrate/pop-everythingelse.php
+++ layers/Schema/packages/migrate-everythingelse/migrate/pop-everythingelse.php
@@ -285,8 +285,6 @@ class Plugins
         require_once (dirname(__DIR__, 2) . '/migrate-custompostmeta-wp/migrate/pop-custompostmeta-wp.php');
         require_once (dirname(__DIR__, 2) . '/migrate-usermeta/migrate/pop-usermeta.php');
         require_once (dirname(__DIR__, 2) . '/migrate-usermeta-wp/migrate/pop-usermeta-wp.php');
-        require_once (dirname(__DIR__, 2) . '/migrate-commentmeta/migrate/pop-commentmeta.php');
-        require_once (dirname(__DIR__, 2) . '/migrate-commentmeta-wp/migrate/pop-commentmeta-wp.php');
         require_once (dirname(__DIR__, 2) . '/migrate-taxonomymeta/migrate/pop-taxonomymeta.php');
         require_once (dirname(__DIR__, 2) . '/migrate-taxonomymeta-wp/migrate/pop-taxonomymeta-wp.php');
         require_once (dirname(__DIR__, 2) . '/migrate-events-wp-em/migrate/pop-events-wp-em.php');
