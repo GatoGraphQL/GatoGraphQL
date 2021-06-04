d6525792996db6a316c6246f889fb383bce46e67
Leonardo Losoviz <leo@getpop.org>
2021-06-05 07:48:11 +0800

    Created ExtensionManager

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginManagement/ExtensionManager.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\PluginManagement;
+
+use GraphQLAPI\GraphQLAPI\PluginSkeleton\AbstractExtension;
+
+class ExtensionManager
+{
+    /**
+     * @var array<string, AbstractExtension>
+     */
+    private static array $extensionClassInstances = [];
+
+    public static function register(AbstractExtension $extension): void
+    {
+        self::$extensionClassInstances[get_class($extension)] = $extension;
+    }
+}
