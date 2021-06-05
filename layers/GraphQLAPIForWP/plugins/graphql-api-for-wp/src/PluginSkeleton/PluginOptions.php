1b123d21b0e5d52bcf133a1a3dec03afbe436a52
Leonardo Losoviz <leo@getpop.org>
2021-06-05 11:37:59 +0800

    Added comment

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginSkeleton/PluginOptions.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginSkeleton/PluginOptions.php
@@ -8,7 +8,9 @@ class PluginOptions
 {
     /**
      * Store the plugin/extension versions in the Options table,
-     * to track when each of them is installed/updated
+     * to track when each of them is installed/updated.
+     *
+     * It is stored as an array of "plugin/extension baseName" => "version"
      */
     public const PLUGIN_VERSIONS = 'graphql-api-plugin-versions';
 
