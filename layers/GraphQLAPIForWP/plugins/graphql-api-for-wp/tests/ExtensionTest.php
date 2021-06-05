116e22ae2dc61dcd25b89eb097e9585339d5b13d
Leonardo Losoviz <leo@getpop.org>
2021-06-05 14:40:47 +0800

    Removed wrong comment

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/tests/ExtensionTest.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/tests/ExtensionTest.php
@@ -6,9 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 class ExtensionTest extends TestCase
 {
-    /**
-     * The component must have some dependency (only the root has not)
-     */
     public function testExtensionNamespace(): void
     {
         $extension = new Extension('', '');
