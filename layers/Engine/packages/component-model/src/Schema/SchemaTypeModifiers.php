1ca99b24f425993de374a1c5369f403383f2688d
Leonardo Losoviz <leo@getpop.org>
2021-06-10 19:46:03 +0800

    Added doc

--- layers/Engine/packages/component-model/src/Schema/SchemaTypeModifiers.php
+++ layers/Engine/packages/component-model/src/Schema/SchemaTypeModifiers.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Schema;
 
+/**
+ * Values to extract using a bitwise operation
+ * 
+ * @see https://www.php.net/manual/en/language.operators.bitwise.php#91291
+ */
 class SchemaTypeModifiers
 {
     public const NON_NULLABLE = 1;
