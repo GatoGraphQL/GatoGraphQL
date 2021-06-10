e891434e37fa97fa72faf5555784e7fc73557919
Leonardo Losoviz <leo@getpop.org>
2021-06-10 22:37:58 +0800

    Removed unneeded functions

--- layers/Engine/packages/component-model/src/Schema/TypeCastingHelpers.php
+++ layers/Engine/packages/component-model/src/Schema/TypeCastingHelpers.php
@@ -20,28 +20,4 @@ class TypeCastingHelpers
         }
         return $type;
     }
-
-    /**
-     * Return the current type combination element, which is simply the first element, always
-     * Eg: if passing "string", it is "string"; for "array:string", it is "array";
-     */
-    public static function getTypeCombinationCurrentElement(string $type): string
-    {
-        $maybeCombinationElems = explode(self::TYPE_SEPARATOR, $type);
-        return $maybeCombinationElems[0];
-    }
-    /**
-     * If the type is a combination of 2 or more types, then return the string containing all of them except the first one
-     * Eg: if passing "string", it is null; for "array:string", it is "string"; for "array:array:string", it is "array:string"
-     */
-    public static function getTypeCombinationNestedElements(string $type): ?string
-    {
-        $maybeCombinationElems = explode(self::TYPE_SEPARATOR, $type);
-        if (count($maybeCombinationElems) >= 2) {
-            // Remove the first element
-            return substr($type, strlen($maybeCombinationElems[0]) + 1);
-        }
-        // There are no others
-        return null;
-    }
 }
