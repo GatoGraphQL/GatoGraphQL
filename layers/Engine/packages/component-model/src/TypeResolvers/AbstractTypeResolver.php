773d9960cafed4bd27666df7799b7b4bd669ab19
Leonardo Losoviz <leo@getpop.org>
2021-06-13 14:44:51 +0800

    Fixed typehint

--- layers/Engine/packages/component-model/src/TypeResolvers/AbstractTypeResolver.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/AbstractTypeResolver.php
@@ -42,7 +42,7 @@ abstract class AbstractTypeResolver implements TypeResolverInterface
     /**
      * Cache of which fieldResolvers will process the given field
      *
-     * @var FieldResolverInterface[]
+     * @var array<string, FieldResolverInterface>
      */
     protected array $fieldResolvers = [];
     /**
