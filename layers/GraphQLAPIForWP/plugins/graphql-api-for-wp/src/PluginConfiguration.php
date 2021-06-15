906f6474c2fa7dd7ae7f3282094315683ce9a7c5
Leonardo Losoviz <leo@getpop.org>
2021-06-15 16:30:30 +0800

    Set `REMOVE_FIELD_IF_DIRECTIVE_FAILED` on true

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginConfiguration.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginConfiguration.php
@@ -744,6 +744,10 @@ class PluginConfiguration
              * If a directive fails, then stop execution of the directive pipeline altogether
              */
             ComponentModelEnvironment::STOP_DIRECTIVE_PIPELINE_EXECUTION_IF_DIRECTIVE_FAILED => true,
+            /**
+             * If a directive fails, then remove the affected IDs/fields from the upcoming stages of the directive pipeline execution
+             */
+            ComponentModelEnvironment::REMOVE_FIELD_IF_DIRECTIVE_FAILED => true,
         ];
         $componentClassConfiguration[\GraphQLByPoP\GraphQLClientsForWP\Component::class] = [
             \GraphQLByPoP\GraphQLClientsForWP\Environment::GRAPHQL_CLIENTS_COMPONENT_URL => $mainPluginURL . 'vendor/graphql-by-pop/graphql-clients-for-wp',
