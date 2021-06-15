1bdc76b77bc3041a9f0d417b3dee1eb2b8d210f5
Leonardo Losoviz <leo@getpop.org>
2021-06-15 17:06:16 +0800

    env var is implicit

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginConfiguration.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginConfiguration.php
@@ -741,10 +741,6 @@ class PluginConfiguration
              */
             ComponentModelEnvironment::SET_FAILING_FIELD_RESPONSE_AS_NULL => true,
             /**
-             * If a directive fails, then stop execution of the directive pipeline altogether
-             */
-            ComponentModelEnvironment::STOP_DIRECTIVE_PIPELINE_EXECUTION_IF_DIRECTIVE_FAILED => true,
-            /**
              * If a directive fails, then remove the affected IDs/fields from the upcoming stages of the directive pipeline execution
              */
             ComponentModelEnvironment::REMOVE_FIELD_IF_DIRECTIVE_FAILED => true,
