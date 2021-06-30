d8398eed20d65b0110013a368207778f4fb89574
Leonardo Losoviz <leo@getpop.org>
2021-06-30 13:24:36 +0800

    Define symbols via env constant

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginEnvironment.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginEnvironment.php
@@ -10,6 +10,7 @@ class PluginEnvironment
 {
     public const DISABLE_CACHING = 'DISABLE_CACHING';
     public const CACHE_DIR = 'CACHE_DIR';
+    public const REDEFINED_FIELD_ARGS_SYMBOLS = 'REDEFINED_FIELD_ARGS_SYMBOLS';
 
     /**
      * If the information is provided by either environment variable
@@ -45,5 +46,30 @@ class PluginEnvironment
         }
         
         return dirname(__FILE__, 2) . \DIRECTORY_SEPARATOR . 'cache';
+    }
+
+    /**
+     * Make it difficult to have the string be considered a field (eg: title()),
+     * by changing the field args `()` symbols into something different.
+     * 
+     * Must pass a string of exactly two characters:
+     * 
+     *   1st: QuerySyntax::$SYMBOL_FIELDARGS_OPENING (eg: `{`)
+     *   2nd: QuerySyntax::$SYMBOL_FIELDARGS_CLOSING (eg: `}`)
+     * 
+     * Eg: REDEFINED_FIELD_ARGS_SYMBOLS={}
+     */
+    public static function getRedefinedFieldArgsSymbols(): ?array
+    {
+        $chars = null;
+        if (getenv(self::REDEFINED_FIELD_ARGS_SYMBOLS) !== false) {
+            $chars = getenv(self::REDEFINED_FIELD_ARGS_SYMBOLS);
+        } elseif (PluginConfigurationHelpers::isWPConfigConstantDefined(self::REDEFINED_FIELD_ARGS_SYMBOLS)) {
+            $chars = PluginConfigurationHelpers::getWPConfigConstantValue(self::REDEFINED_FIELD_ARGS_SYMBOLS);
+        }
+        if ($chars !== null && strlen($chars) === 2) {
+            return [$chars[0], $chars[1]];
+        }
+        return null;
     }
 }
