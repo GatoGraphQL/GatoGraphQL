fa8f4cc3a34bfef1f5b536d32a66d999a7755ddb
Leonardo Losoviz <leo@getpop.org>
2021-07-02 13:10:03 +0800

    Removed env-var "checkout-submodules"

--- src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
@@ -7,18 +7,10 @@ namespace PoP\PoP\Config\Symplify\MonorepoBuilder\DataSources;
 class EnvironmentVariablesDataSource
 {
     /**
-     * @var string
-     */
-    public const CHECKOUT_SUBMODULES = 'checkout-submodules';
-
-    /**
      * @return array<string,string>
      */
     public function getEnvironmentVariables(): array
     {
-        return [
-            // [multi-monorepo] Env var overriden by downstream monorepo
-            self::CHECKOUT_SUBMODULES => '',
-        ];
+        return [];
     }
 }
