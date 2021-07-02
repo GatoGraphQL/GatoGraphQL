d9071cbc3d942b618c8defbc23e74b80ea31c99b
Leonardo Losoviz <leo@getpop.org>
2021-07-02 12:37:37 +0800

    Added empty entry "checkout-submodules"

--- src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
@@ -7,10 +7,18 @@ namespace PoP\PoP\Config\Symplify\MonorepoBuilder\DataSources;
 class EnvironmentVariablesDataSource
 {
     /**
+     * @var string
+     */
+    public const CHECKOUT_SUBMODULES = 'checkout-submodules';
+
+    /**
      * @return array<string,string>
      */
     public function getEnvironmentVariables(): array
     {
-        return [];
+        return [
+            // [multi-monorepo] Env var overriden by downstream monorepo
+            self::CHECKOUT_SUBMODULES => '',
+        ];
     }
 }
