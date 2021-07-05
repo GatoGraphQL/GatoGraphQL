603eca1921980d9743189c3abc8d32cddce9363c
Leonardo Losoviz <leo@getpop.org>
2021-07-05 13:59:03 +0800

    Pass retention_days via env-var

--- src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
@@ -7,6 +7,7 @@ namespace PoP\PoP\Config\Symplify\MonorepoBuilder\DataSources;
 class EnvironmentVariablesDataSource
 {
     public const GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE = 'GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE';
+    public const RETENTION_DAYS_FOR_GENERATED_PLUGINS = 'RETENTION_DAYS_FOR_GENERATED_PLUGINS';
 
     /**
      * @return array<string,string>
@@ -15,6 +16,7 @@ class EnvironmentVariablesDataSource
     {
         return [
             self::GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE => false,
+            self::RETENTION_DAYS_FOR_GENERATED_PLUGINS => 30,
         ];
     }
 }
