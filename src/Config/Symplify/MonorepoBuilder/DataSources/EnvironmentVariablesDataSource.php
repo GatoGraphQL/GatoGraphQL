e988c478381b0f69d940014ea8d9c750a30a5eb0
Leonardo Losoviz <leo@getpop.org>
2021-07-05 13:46:32 +0800

    Added const GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE

--- src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
@@ -6,11 +6,15 @@ namespace PoP\PoP\Config\Symplify\MonorepoBuilder\DataSources;
 
 class EnvironmentVariablesDataSource
 {
+    public const GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE = 'GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE';
+
     /**
      * @return array<string,string>
      */
     public function getEnvironmentVariables(): array
     {
-        return [];
+        return [
+            self::GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE => true,
+        ];
     }
 }
