c57f92a8eda1ee54f2eb5f03d2cfe1a5fbd43015
Leonardo Losoviz <leo@getpop.org>
2021-07-08 07:42:06 +0800

    Removed package to skip

--- src/Config/Symplify/MonorepoBuilder/DataSources/PHPStanDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/PHPStanDataSource.php
@@ -17,7 +17,6 @@ class PHPStanDataSource
     public function getUnmigratedFailingPackages(): array
     {
         return [
-            'layers/Engine/packages/component-model',
             'layers/GraphQLByPoP/packages/graphql-parser',
             'layers/Schema/packages/block-metadata-for-wp',
             'layers/Schema/packages/categories',
