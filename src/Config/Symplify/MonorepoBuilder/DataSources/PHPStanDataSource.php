ece85f26665dfe9da1cfe5369c4004760ed99710
Leonardo Losoviz <leo@getpop.org>
2021-07-07 17:50:22 +0800

    Removed package to skip

--- src/Config/Symplify/MonorepoBuilder/DataSources/PHPStanDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/PHPStanDataSource.php
@@ -21,7 +21,6 @@ class PHPStanDataSource
             'layers/Engine/packages/engine-wp',
             'layers/Engine/packages/engine',
             'layers/GraphQLByPoP/packages/graphql-parser',
-            'layers/GraphQLByPoP/packages/graphql-server',
             'layers/Schema/packages/block-metadata-for-wp',
             'layers/Schema/packages/categories',
             'layers/Schema/packages/categories',
