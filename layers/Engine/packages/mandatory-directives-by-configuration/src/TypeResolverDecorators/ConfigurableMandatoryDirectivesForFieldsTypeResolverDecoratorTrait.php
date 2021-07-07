74b32b86ef4ad76fb1590013bb339ff03e4255a0
Leonardo Losoviz <leo@getpop.org>
2021-07-07 16:08:56 +0800

    Import missing class

--- layers/Engine/packages/mandatory-directives-by-configuration/src/TypeResolverDecorators/ConfigurableMandatoryDirectivesForFieldsTypeResolverDecoratorTrait.php
+++ layers/Engine/packages/mandatory-directives-by-configuration/src/TypeResolverDecorators/ConfigurableMandatoryDirectivesForFieldsTypeResolverDecoratorTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PoP\MandatoryDirectivesByConfiguration\TypeResolverDecorators;
 
 use PoP\ComponentModel\Facades\Instances\InstanceManagerFacade;
+use PoP\ComponentModel\FieldInterfaceResolvers\FieldInterfaceResolverInterface;
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
 use PoP\MandatoryDirectivesByConfiguration\ConfigurationEntries\ConfigurableMandatoryDirectivesForFieldsTrait;
 
