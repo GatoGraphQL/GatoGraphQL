03ff0bc92ad04ea08a2c0ba2a1ac2b5718b51c32
Leonardo Losoviz <leo@getpop.org>
2021-07-08 16:44:16 +0800

    Removed stale imports, tidied up phpdoc

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginManagement/PluginConfigurationHelper.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginManagement/PluginConfigurationHelper.php
@@ -4,86 +4,13 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\PluginManagement;
 
-use GraphQLAPI\GraphQLAPI\ComponentConfiguration;
-use GraphQLAPI\GraphQLAPI\Config\PluginConfigurationHelpers;
-use GraphQLAPI\GraphQLAPI\Environment;
-use GraphQLAPI\GraphQLAPI\Facades\CacheConfigurationManagerFacade;
 use GraphQLAPI\GraphQLAPI\Facades\Registries\SystemModuleRegistryFacade;
-use GraphQLAPI\GraphQLAPI\Facades\UserSettingsManagerFacade;
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\CacheFunctionalityModuleResolver;
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\ClientFunctionalityModuleResolver;
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\EndpointFunctionalityModuleResolver;
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\OperationalFunctionalityModuleResolver;
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\PerformanceFunctionalityModuleResolver;
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\PluginManagementFunctionalityModuleResolver;
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\SchemaConfigurationFunctionalityModuleResolver;
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\SchemaTypeModuleResolver;
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\UserInterfaceFunctionalityModuleResolver;
-use GraphQLAPI\GraphQLAPI\PluginManagement\MainPluginManager;
-use GraphQLAPI\GraphQLAPI\Services\Helpers\EndpointHelpers;
 use GraphQLAPI\GraphQLAPI\Services\MenuPages\SettingsMenuPage;
-use GraphQLByPoP\GraphQLClientsForWP\ComponentConfiguration as GraphQLClientsForWPComponentConfiguration;
-use GraphQLByPoP\GraphQLClientsForWP\Environment as GraphQLClientsForWPEnvironment;
-use GraphQLByPoP\GraphQLEndpointForWP\ComponentConfiguration as GraphQLEndpointForWPComponentConfiguration;
-use GraphQLByPoP\GraphQLEndpointForWP\Environment as GraphQLEndpointForWPEnvironment;
-use GraphQLByPoP\GraphQLQuery\Environment as GraphQLQueryEnvironment;
-use GraphQLByPoP\GraphQLServer\ComponentConfiguration as GraphQLServerComponentConfiguration;
-use GraphQLByPoP\GraphQLServer\Configuration\MutationSchemes;
-use GraphQLByPoP\GraphQLServer\Environment as GraphQLServerEnvironment;
-use PoP\AccessControl\ComponentConfiguration as AccessControlComponentConfiguration;
-use PoP\AccessControl\Environment as AccessControlEnvironment;
-use PoP\AccessControl\Schema\SchemaModes;
 use PoP\APIEndpoints\EndpointUtils;
-use PoP\CacheControl\ComponentConfiguration as CacheControlComponentConfiguration;
-use PoP\CacheControl\Environment as CacheControlEnvironment;
-use PoP\ComponentModel\ComponentConfiguration as ComponentModelComponentConfiguration;
-use PoP\ComponentModel\ComponentConfiguration\ComponentConfigurationHelpers;
-use PoP\ComponentModel\Environment as ComponentModelEnvironment;
 use PoP\ComponentModel\Facades\Instances\InstanceManagerFacade;
-use PoP\ComponentModel\Facades\Instances\SystemInstanceManagerFacade;
-use PoP\ComponentModel\Misc\GeneralUtils;
-use PoP\Engine\ComponentConfiguration as EngineComponentConfiguration;
-use PoP\Engine\Environment as EngineEnvironment;
-use PoP\Root\Environment as RootEnvironment;
-use PoPSchema\Categories\ComponentConfiguration as CategoriesComponentConfiguration;
-use PoPSchema\Categories\Environment as CategoriesEnvironment;
-use PoPSchema\CommentMeta\ComponentConfiguration as CommentMetaComponentConfiguration;
-use PoPSchema\CommentMeta\Environment as CommentMetaEnvironment;
-use PoPSchema\CustomPostMeta\ComponentConfiguration as CustomPostMetaComponentConfiguration;
-use PoPSchema\CustomPostMeta\Environment as CustomPostMetaEnvironment;
-use PoPSchema\CustomPosts\ComponentConfiguration as CustomPostsComponentConfiguration;
-use PoPSchema\CustomPosts\Environment as CustomPostsEnvironment;
-use PoPSchema\GenericCustomPosts\ComponentConfiguration as GenericCustomPostsComponentConfiguration;
-use PoPSchema\GenericCustomPosts\Environment as GenericCustomPostsEnvironment;
-use PoPSchema\Pages\ComponentConfiguration as PagesComponentConfiguration;
-use PoPSchema\Pages\Environment as PagesEnvironment;
-use PoPSchema\Posts\ComponentConfiguration as PostsComponentConfiguration;
-use PoPSchema\Posts\Environment as PostsEnvironment;
-use PoPSchema\SchemaCommons\Constants\Behaviors;
-use PoPSchema\Settings\ComponentConfiguration as SettingsComponentConfiguration;
-use PoPSchema\Settings\Environment as SettingsEnvironment;
-use PoPSchema\Tags\ComponentConfiguration as TagsComponentConfiguration;
-use PoPSchema\Tags\Environment as TagsEnvironment;
-use PoPSchema\TaxonomyMeta\ComponentConfiguration as TaxonomyMetaComponentConfiguration;
-use PoPSchema\TaxonomyMeta\Environment as TaxonomyMetaEnvironment;
-use PoPSchema\UserMeta\ComponentConfiguration as UserMetaComponentConfiguration;
-use PoPSchema\UserMeta\Environment as UserMetaEnvironment;
-use PoPSchema\Users\ComponentConfiguration as UsersComponentConfiguration;
-use PoPSchema\Users\Environment as UsersEnvironment;
 
 /**
  * Helper class with functions to set the configuration in PoP components.
- *
- * To set the value for properties, it uses this order:
- *
- * 1. Retrieve it as an environment value, if defined
- * 2. Retrieve as a constant `GRAPHQL_API_...` from wp-config.php, if defined
- * 3. Retrieve it from the user settings, if stored
- * 4. Use the default value
- *
- * If a slug is set or updated in the environment variable or wp-config constant,
- * it is necessary to flush the rewrite rules for the change to take effect.
- * For that, on the WordPress admin, go to Settings => Permalinks and click on Save changes
  */
 class PluginConfigurationHelper
 {
@@ -101,7 +28,7 @@ class PluginConfigurationHelper
      */
     public static function getNormalizedOptionValues(): array
     {
-        if (is_null(self::$normalizedOptionValuesCache)) {
+        if (self::$normalizedOptionValuesCache === null) {
             $instanceManager = InstanceManagerFacade::getInstance();
             /**
              * @var SettingsMenuPage
