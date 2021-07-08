58f02525dbaa7778fada884a5a1fc9bd4d396253
Leonardo Losoviz <leo@getpop.org>
2021-07-08 16:46:13 +0800

    Removed stale imports, tidied up phpdoc

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginSkeleton/AbstractExtensionConfiguration.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginSkeleton/AbstractExtensionConfiguration.php
@@ -4,24 +4,8 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\PluginSkeleton;
 
-use GraphQLAPI\GraphQLAPI\Facades\Registries\SystemModuleRegistryFacade;
-use GraphQLAPI\GraphQLAPI\Services\Helpers\EndpointHelpers;
-use PoP\ComponentModel\Facades\Instances\SystemInstanceManagerFacade;
-use PoP\ComponentModel\Misc\GeneralUtils;
-
 /**
- * Sets the configuration in all the PoP components.
- *
- * To set the value for properties, it uses this order:
- *
- * 1. Retrieve it as an environment value, if defined
- * 2. Retrieve as a constant `GRAPHQL_API_...` from wp-config.php, if defined
- * 3. Retrieve it from the user settings, if stored
- * 4. Use the default value
- *
- * If a slug is set or updated in the environment variable or wp-config constant,
- * it is necessary to flush the rewrite rules for the change to take effect.
- * For that, on the WordPress admin, go to Settings => Permalinks and click on Save changes
+ * Base class to set the configuration in all the PoP components for the Extension.
  */
 abstract class AbstractExtensionConfiguration extends AbstractPluginConfiguration
 {    
