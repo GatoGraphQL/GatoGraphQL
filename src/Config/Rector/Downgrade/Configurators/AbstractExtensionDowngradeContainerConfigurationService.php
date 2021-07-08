ec50fd2af0dbba2151c9cc529f3003921aa756dd
Leonardo Losoviz <leo@getpop.org>
2021-07-08 17:32:42 +0800

    reorganize ContainerConfigurationService

---
+++ src/Config/Rector/Downgrade/Configurators/AbstractExtensionDowngradeContainerConfigurationService.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\PoP\Config\Rector\Downgrade\Configurators;
+
+abstract class AbstractExtensionDowngradeContainerConfigurationService extends AbstractPluginDowngradeContainerConfigurationService
+{
+}
---
+++ src/Config/Rector/Downgrade/Configurators/AbstractMainPluginDowngradeContainerConfigurationService.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\PoP\Config\Rector\Downgrade\Configurators;
+
+abstract class AbstractMainPluginDowngradeContainerConfigurationService extends AbstractPluginDowngradeContainerConfigurationService
+{
+    public function configureContainer(): void
+    {
+        parent::configureContainer();
+
+        /**
+         * This constant is defined in wp-load.php, but never loaded.
+         * It is read when resolving class WP_Upgrader in Plugin.php.
+         * Define it here again, or otherwise Rector fails with message:
+         *
+         * "PHP Warning:  Use of undefined constant ABSPATH -
+         * assumed 'ABSPATH' (this will throw an Error in a future version of PHP)
+         * in .../graphql-api-for-wp/vendor/wordpress/wordpress/wp-admin/includes/class-wp-upgrader.php
+         * on line 13"
+         */
+        /** Define ABSPATH as this file's directory */
+        if (!defined('ABSPATH')) {
+            define('ABSPATH', $this->pluginDir . '/vendor/wordpress/wordpress/');
+        }
+    }
+}
