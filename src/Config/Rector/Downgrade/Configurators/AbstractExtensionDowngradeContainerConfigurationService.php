ee8ed3d68fb3f961d343a7bb34d6ffde8a365212
Leonardo Losoviz <leo@getpop.org>
2021-07-08 17:34:03 +0800

    Moved `getSkip` upward

--- src/Config/Rector/Downgrade/Configurators/AbstractExtensionDowngradeContainerConfigurationService.php
+++ src/Config/Rector/Downgrade/Configurators/AbstractExtensionDowngradeContainerConfigurationService.php
@@ -6,4 +6,17 @@ namespace PoP\PoP\Config\Rector\Downgrade\Configurators;
 
 abstract class AbstractExtensionDowngradeContainerConfigurationService extends AbstractPluginDowngradeContainerConfigurationService
 {
+    /**
+     * @return string[]
+     */
+    protected function getSkip(): array
+    {
+        return array_merge(
+            parent::getSkip(),
+            [
+                // Skip since they are not needed and they fail
+                $this->pluginDir . '/vendor/composer/*',
+            ]
+        );
+    }
 }
