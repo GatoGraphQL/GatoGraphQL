2002ce773f494de9389fd8ed177ad04ab85ca44a
Leonardo Losoviz <leo@getpop.org>
2021-07-10 10:01:38 +0800

    Spread config into several files

---
+++ webservers/graphql-by-pop/setup/configure.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wp config set WP_DEBUG true --raw --path=/app/wordpress
+wp rewrite structure '/%postname%/' --hard --path=/app/wordpress
+cp /app/assets/.htaccess /app/wordpress
--- webservers/graphql-by-pop/setup/setup.sh
+++ webservers/graphql-by-pop/setup/setup.sh
@@ -5,14 +5,7 @@ if wp core is-installed --path=/app/wordpress; then
     exit
 fi
 echo "Installing WordPress..."
-# Install
-wp core install --url="$SITE_URL" --title="$SITE_TITLE" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --path=/app/wordpress 
-# Configure
-wp config set WP_DEBUG true --raw --path=/app/wordpress
-wp rewrite structure '/%postname%/' --hard --path=/app/wordpress
-cp /app/assets/.htaccess /app/wordpress
-# Load custom code
-sed "s#require_once ABSPATH . 'wp-settings.php';#require_once(__DIR__ . '/../vendor/autoload.php'); require_once(__DIR__ . '/../code-src/initialize-pop-components.php'); require_once  ABSPATH  .  'wp-settings.php' ;#g" /app/wordpress/wp-config.php > /app/wp-config.php.tmp
-mv /app/wp-config.php.tmp /app/wordpress/wp-config.php
-# Activate own plugins
-wp plugin activate engine-wp-bootloader --path=/app/wordpress
+/bash/sh ./install.sh
+/bash/sh ./configure.sh
+/bash/sh ./load-custom-code.sh
+/bash/sh ./activate-plugins.sh
