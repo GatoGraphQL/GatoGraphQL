0cc71b084d5108b78ce05e936789575f0ae026f1
Leonardo Losoviz <leo@getpop.org>
2021-07-10 09:37:01 +0800

    Use full path to /app/wordpress

--- webservers/graphql-by-pop/setup/setup.sh
+++ webservers/graphql-by-pop/setup/setup.sh
@@ -10,9 +10,9 @@ wp core install --url="$SITE_URL" --title="$SITE_TITLE" --admin_user=admin --adm
 # Configure
 wp config set WP_DEBUG true --raw --path=/app/wordpress
 wp rewrite structure '/%postname%/' --hard --path=/app/wordpress
-cp ../assets/.htaccess ../wordpress
+cp /app/assets/.htaccess /app/wordpress
 # Load custom code
-sed "s#require_once ABSPATH . 'wp-settings.php';#require_once(__DIR__ . '/../vendor/autoload.php'); require_once(__DIR__ . '/../code-src/initialize-pop-components.php'); require_once  ABSPATH  .  'wp-settings.php' ;#g" ../wordpress/wp-config.php > ../wp-config.php.tmp
-mv ../wp-config.php.tmp ../wordpress/wp-config.php
+sed "s#require_once ABSPATH . 'wp-settings.php';#require_once(__DIR__ . '/../vendor/autoload.php'); require_once(__DIR__ . '/../code-src/initialize-pop-components.php'); require_once  ABSPATH  .  'wp-settings.php' ;#g" /app/wordpress/wp-config.php > /app/wp-config.php.tmp
+mv /app/wp-config.php.tmp /app/wordpress/wp-config.php
 # Activate own plugins
 wp plugin activate engine-wp-bootloader --path=/app/wordpress
