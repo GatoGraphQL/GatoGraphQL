522b84ee5be74b038e6ddd37d57cd49eecad9e7e
Leonardo Losoviz <leo@getpop.org>
2021-07-10 09:28:53 +0800

    Moved commands to .sh

---
+++ webservers/graphql-by-pop/setup/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=database --skip-check --force --path=/app/wordpress
+if wp core is-installed --path=/app/wordpress; then
+    echo "WordPress is already installed"
+    exit
+fi
+echo "Installing WordPress..."
+# Install
+wp core install --url=graphql-by-pop.lndo.site --title="GraphQL by PoP" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --path=/app/wordpress 
+# Configure
+wp config set WP_DEBUG true --raw --path=/app/wordpress
+wp rewrite structure '/%postname%/' --hard --path=/app/wordpress
+cp ../assets/.htaccess ../wordpress
+# Load custom code
+sed "s#require_once ABSPATH . 'wp-settings.php';#require_once(__DIR__ . '/../vendor/autoload.php'); require_once(__DIR__ . '/../code-src/initialize-pop-components.php'); require_once  ABSPATH  .  'wp-settings.php' ;#g" ../wordpress/wp-config.php > ../wp-config.php.tmp
+mv ../wp-config.php.tmp ../wordpress/wp-config.php
+# Activate own plugins
+wp plugin activate engine-wp-bootloader --path=/app/wordpress
