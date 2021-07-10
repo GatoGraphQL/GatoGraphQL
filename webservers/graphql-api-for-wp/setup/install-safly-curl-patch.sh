3bf4c1f2f222cd583b1c39cfedea076434ed1658
Leonardo Losoviz <leo@getpop.org>
2021-07-10 13:57:38 +0800

    Added script to install SaFly Curl Patch

---
+++ webservers/graphql-api-for-wp/setup/install-safly-curl-patch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Fix for https://github.com/lando/lando/issues/2913
+cd /tmp
+curl -O https://downloads.wordpress.org/plugin/safly-curl-patch.1.0.0.zip
+unzip safly-curl-patch.1.0.0.zip
+mv safly-curl-patch /app/wordpress/wp-content/plugins/
+wp plugin activate safly-curl-patch --path=/app/wordpress
