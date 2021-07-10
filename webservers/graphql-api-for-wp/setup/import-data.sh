0506de1d35647cf40fe8a7315161fefa1bc1f6f2
Leonardo Losoviz <leo@getpop.org>
2021-07-10 11:47:19 +0800

    Distributed commands into .sh files

---
+++ webservers/graphql-api-for-wp/setup/import-data.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wp import /app/assets/graphql-api-sample-data.xml --authors=create --path=/app/wordpress
---
+++ webservers/graphql-api-for-wp/setup/install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wp core install --url="$SITE_NAME.$LANDO_DOMAIN" --title="$SITE_TITLE" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --path=/app/wordpress 
