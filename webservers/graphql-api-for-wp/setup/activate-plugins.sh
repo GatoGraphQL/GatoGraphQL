a8c85ae62fe63ab758aa7e45a3947af558ac969c
Leonardo Losoviz <leo@getpop.org>
2021-07-10 11:50:33 +0800

    Moved plugin install around

--- webservers/graphql-api-for-wp/setup/activate-plugins.sh
+++ webservers/graphql-api-for-wp/setup/activate-plugins.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-# Activate own plugins
 wp plugin activate graphql-api --path=/app/wordpress
-# Download and activate external plugins
-wp plugin install wordpress-importer --activate --path=/app/wordpress
