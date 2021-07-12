781d6d8b2a40125fd3474c7e151e6a050b3b9877
Leonardo Losoviz <leo@getpop.org>
2021-07-12 17:19:28 +0800

    Removed stale import

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ComponentConfiguration.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ComponentConfiguration.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI;
 
-use GraphQLAPI\GraphQLAPI\Security\AccessSchemes;
 use PoP\ComponentModel\ComponentConfiguration\ComponentConfigurationTrait;
 use PoP\ComponentModel\ComponentConfiguration\EnvironmentValueHelpers;
 
