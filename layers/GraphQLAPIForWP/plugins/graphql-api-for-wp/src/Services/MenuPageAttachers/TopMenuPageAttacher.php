cc5e5d262036d11f0694b0094061e2a48788e55c
Leonardo Losoviz <leo@getpop.org>
2021-07-12 08:49:17 +0800

    Fixed cs

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/MenuPageAttachers/TopMenuPageAttacher.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/MenuPageAttachers/TopMenuPageAttacher.php
@@ -35,7 +35,7 @@ class TopMenuPageAttacher extends AbstractPluginMenuPageAttacher
     public function addMenuPages(): void
     {
         $schemaEditorAccessCapability = $this->userAuthorization->getSchemaEditorAccessCapability();
-        
+
         /**
          * @var GraphiQLMenuPage
          */
