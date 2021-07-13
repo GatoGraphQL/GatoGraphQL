33577a7106b91c5744a2bda1575f72eb1c7908d7
Leonardo Losoviz <leo@getpop.org>
2021-07-13 17:52:35 +0800

    Abstracted CPTFieldResolver

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ConditionalOnContext/Admin/ConditionalOnContext/Editor/SchemaServices/FieldResolvers/CPTFieldResolver.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ConditionalOnContext/Admin/ConditionalOnContext/Editor/SchemaServices/FieldResolvers/CPTFieldResolver.php
@@ -8,36 +8,14 @@ use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\GraphQLAccessControlListCusto
 use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\GraphQLCacheControlListCustomPostType;
 use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\GraphQLFieldDeprecationListCustomPostType;
 use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\GraphQLSchemaConfigurationCustomPostType;
-use PoP\ComponentModel\FieldResolvers\AbstractQueryableFieldResolver;
-use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
-use PoP\Engine\TypeResolvers\RootTypeResolver;
-use PoPSchema\CustomPosts\Facades\CustomPostTypeAPIFacade;
-use PoPSchema\CustomPosts\TypeResolvers\CustomPostTypeResolver;
-use PoPSchema\CustomPosts\Types\Status;
-use PoPSchema\SchemaCommons\DataLoading\ReturnTypes;
 
 /**
  * FieldResolver for the Custom Post Types from this plugin
  */
-class CPTFieldResolver extends AbstractQueryableFieldResolver
+class CPTFieldResolver extends AbstractCPTFieldResolver
 {
-    /**
-     * Option to tell the hook to not remove the private CPTs when querying
-     */
-    public const QUERY_OPTION_ALLOW_QUERYING_PRIVATE_CPTS = 'allow-querying-private-cpts';
-
-    /**
-     * @return string[]
-     */
-    public function getClassesToAttachTo(): array
-    {
-        return [
-            RootTypeResolver::class,
-        ];
-    }
-
     /**
      * @return string[]
      */
@@ -49,19 +27,6 @@ class CPTFieldResolver extends AbstractQueryableFieldResolver
             'fieldDeprecationLists',
             'schemaConfigurations',
         ];
-    }
-
-    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): string
-    {
-        return match ($fieldName) {
-            'accessControlLists',
-            'cacheControlLists',
-            'fieldDeprecationLists',
-            'schemaConfigurations'
-                => SchemaDefinition::TYPE_ID,
-            default
-                => parent::getSchemaFieldType($typeResolver, $fieldName),
-        };
     }
 
     public function getSchemaFieldTypeModifiers(TypeResolverInterface $typeResolver, string $fieldName): ?int
@@ -88,140 +53,14 @@ class CPTFieldResolver extends AbstractQueryableFieldResolver
         };
     }
 
-    /**
-     * @return array<string, mixed>
-     */
-    public function getSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): array
+    function getFieldCustomPostType(string $fieldName): string
     {
-        $schemaFieldArgs = parent::getSchemaFieldArgs($typeResolver, $fieldName);
-        switch ($fieldName) {
-            case 'accessControlLists':
-            case 'cacheControlLists':
-            case 'fieldDeprecationLists':
-            case 'schemaConfigurations':
-                $schemaFieldArgs = array_merge(
-                    $schemaFieldArgs,
-                    $this->getFieldArgumentsSchemaDefinitions($typeResolver, $fieldName)
-                );
-                // Remove the "customPostTypes" field argument
-                $schemaFieldArgs = array_filter(
-                    $schemaFieldArgs,
-                    fn ($schemaFieldArg) => $schemaFieldArg[SchemaDefinition::ARGNAME_NAME] != 'customPostTypes'
-                );
-                break;
-        }
-        return $schemaFieldArgs;
-    }
-
-    public function enableOrderedSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): bool
-    {
-        switch ($fieldName) {
-            case 'accessControlLists':
-            case 'cacheControlLists':
-            case 'fieldDeprecationLists':
-            case 'schemaConfigurations':
-                return false;
-        }
-        return parent::enableOrderedSchemaFieldArgs($typeResolver, $fieldName);
-    }
-
-    /**
-     * @param array<string, mixed> $fieldArgs
-     * @return array<string, mixed>
-     */
-    protected function getQuery(
-        TypeResolverInterface $typeResolver,
-        object $resultItem,
-        string $fieldName,
-        array $fieldArgs = []
-    ): array {
-        switch ($fieldName) {
-            case 'accessControlLists':
-            case 'cacheControlLists':
-            case 'fieldDeprecationLists':
-            case 'schemaConfigurations':
-                $query = [
-                    'limit' => -1,
-                    'status' => [
-                        Status::PUBLISHED,
-                    ],
-                ];
-                return $query;
-        }
-        return [];
-    }
-
-    /**
-     * @param array<string, mixed> $fieldArgs
-     * @param array<string, mixed>|null $variables
-     * @param array<string, mixed>|null $expressions
-     * @param array<string, mixed> $options
-     */
-    public function resolveValue(
-        TypeResolverInterface $typeResolver,
-        object $resultItem,
-        string $fieldName,
-        array $fieldArgs = [],
-        ?array $variables = null,
-        ?array $expressions = null,
-        array $options = []
-    ): mixed {
-        $customPostTypeAPI = CustomPostTypeAPIFacade::getInstance();
-        switch ($fieldName) {
-            case 'accessControlLists':
-            case 'cacheControlLists':
-            case 'fieldDeprecationLists':
-            case 'schemaConfigurations':
-                // Remove the "customPostTypes" field argument
-                unset($fieldArgs['customPostTypes']);
-                $query = $this->getQuery($typeResolver, $resultItem, $fieldName, $fieldArgs);
-                // Execute for the corresponding field name
-                $customPostTypes = [
-                    'accessControlLists' => GraphQLAccessControlListCustomPostType::CUSTOM_POST_TYPE,
-                    'cacheControlLists' => GraphQLCacheControlListCustomPostType::CUSTOM_POST_TYPE,
-                    'fieldDeprecationLists' => GraphQLFieldDeprecationListCustomPostType::CUSTOM_POST_TYPE,
-                    'schemaConfigurations' => GraphQLSchemaConfigurationCustomPostType::CUSTOM_POST_TYPE,
-                ];
-                $query['custompost-types'] = [
-                    $customPostTypes[$fieldName],
-                ];
-                $options = [
-                    'return-type' => ReturnTypes::IDS,
-                    // Do not use the limit set in the settings for custom posts
-                    'skip-max-limit' => true,
-                    // With this flag, the hook will not remove the private CPTs
-                    self::QUERY_OPTION_ALLOW_QUERYING_PRIVATE_CPTS => true,
-                ];
-                $this->addFilterDataloadQueryArgs($options, $typeResolver, $fieldName, $fieldArgs);
-                return $customPostTypeAPI->getCustomPosts($query, $options);
-        }
-
-        return parent::resolveValue(
-            $typeResolver,
-            $resultItem,
-            $fieldName,
-            $fieldArgs,
-            $variables,
-            $expressions,
-            $options
-        );
-    }
-
-    public function resolveFieldTypeResolverClass(
-        TypeResolverInterface $typeResolver,
-        string $fieldName
-    ): ?string {
-        switch ($fieldName) {
-            case 'accessControlLists':
-            case 'cacheControlLists':
-            case 'fieldDeprecationLists':
-            case 'schemaConfigurations':
-                return CustomPostTypeResolver::class;
-        }
-
-        return parent::resolveFieldTypeResolverClass(
-            $typeResolver,
-            $fieldName,
-        );
+        return match ($fieldName) {
+            'accessControlLists' => GraphQLAccessControlListCustomPostType::CUSTOM_POST_TYPE,
+            'cacheControlLists' => GraphQLCacheControlListCustomPostType::CUSTOM_POST_TYPE,
+            'fieldDeprecationLists' => GraphQLFieldDeprecationListCustomPostType::CUSTOM_POST_TYPE,
+            'schemaConfigurations' => GraphQLSchemaConfigurationCustomPostType::CUSTOM_POST_TYPE,
+            default => '', // It will never reach here
+        };
     }
 }
