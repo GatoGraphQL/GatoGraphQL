a9c6235451ce04e29737481905b7866145811881
Leonardo Losoviz <leo@getpop.org>
2021-07-13 17:35:26 +0800

    Removed block

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/field-deprecation/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '814b7d95c0464cc750414cd04ad98bd3');
