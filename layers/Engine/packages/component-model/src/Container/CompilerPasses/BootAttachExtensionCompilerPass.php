0d4a61d01ad375c09325395fdf606d09bfde0891
Leonardo Losoviz <leo@getpop.org>
2021-07-15 07:56:04 +0800

    Initialize schema on Boot event! To enable SchemaConfigurationExecuters to modify env vars via hooks

---
+++ layers/Engine/packages/component-model/src/Container/CompilerPasses/BootAttachExtensionCompilerPass.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\Container\CompilerPasses;
+
+use PoP\ComponentModel\AttachableExtensions\AttachableExtensionGroups;
+use PoP\Root\Component\ApplicationEvents;
+use PoP\ComponentModel\DirectiveResolvers\DirectiveResolverInterface;
+use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
+use PoP\ComponentModel\TypeResolverPickers\TypeResolverPickerInterface;
+
+class BootAttachExtensionCompilerPass extends AbstractAttachExtensionCompilerPass
+{
+    protected function getAttachExtensionEvent(): string
+    {
+        return ApplicationEvents::BOOT;
+    }
+
+    /**
+     * @return array<string,string>
+     */
+    protected function getAttachableClassGroups(): array
+    {
+        return [
+            FieldResolverInterface::class => AttachableExtensionGroups::FIELDRESOLVERS,
+            DirectiveResolverInterface::class => AttachableExtensionGroups::DIRECTIVERESOLVERS,
+            TypeResolverPickerInterface::class => AttachableExtensionGroups::TYPERESOLVERPICKERS,
+        ];
+    }
+}
