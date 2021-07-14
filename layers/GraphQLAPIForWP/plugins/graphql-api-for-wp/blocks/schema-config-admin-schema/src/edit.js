02a163a05ec981cea73874ad85eb6ab253c5fedd
Leonardo Losoviz <leo@getpop.org>
2021-07-15 04:34:08 +0800

    Removed local data vars

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-admin-schema/src/edit.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-admin-schema/src/edit.js
@@ -1,28 +1,13 @@
 /**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
- */
-
-/**
  * Application imports
  */
 import SchemaConfigAdminSchemaCard from './schema-config-admin-schema-card';
-
-const isPublicPrivateSchemaEnabled = window.graphqlApiSchemaConfigOptions ? window.graphqlApiSchemaConfigOptions.isPublicPrivateSchemaEnabled : true;
-const isAdminSchemaEnabled = window.graphqlApiSchemaConfigOptions ? window.graphqlApiSchemaConfigOptions.isAdminSchemaEnabled : true;
-const isSchemaNamespacingEnabled = window.graphqlApiSchemaConfigOptions ? window.graphqlApiSchemaConfigOptions.isSchemaNamespacingEnabled : true;
-const isNestedMutationsEnabled = window.graphqlApiSchemaConfigOptions ? window.graphqlApiSchemaConfigOptions.isNestedMutationsEnabled : true;
 
 const EditBlock = ( props ) => {
 	const { className } = props;
 	return (
 		<div class={ className }>
 			<SchemaConfigAdminSchemaCard
-				isPublicPrivateSchemaEnabled={ isPublicPrivateSchemaEnabled }
-				isAdminSchemaEnabled={ isAdminSchemaEnabled }
-				isSchemaNamespacingEnabled={ isSchemaNamespacingEnabled }
-				isNestedMutationsEnabled={ isNestedMutationsEnabled }
 				{ ...props }
 			/>
 		</div>
--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-mutation-scheme/src/edit.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-mutation-scheme/src/edit.js
@@ -1,28 +1,13 @@
 /**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
- */
-
-/**
  * Application imports
  */
 import SchemaConfigMutationSchemeCard from './schema-config-mutation-scheme-card';
-
-const isPublicPrivateSchemaEnabled = window.graphqlApiSchemaConfigOptions ? window.graphqlApiSchemaConfigOptions.isPublicPrivateSchemaEnabled : true;
-const isAdminSchemaEnabled = window.graphqlApiSchemaConfigOptions ? window.graphqlApiSchemaConfigOptions.isAdminSchemaEnabled : true;
-const isSchemaNamespacingEnabled = window.graphqlApiSchemaConfigOptions ? window.graphqlApiSchemaConfigOptions.isSchemaNamespacingEnabled : true;
-const isNestedMutationsEnabled = window.graphqlApiSchemaConfigOptions ? window.graphqlApiSchemaConfigOptions.isNestedMutationsEnabled : true;
 
 const EditBlock = ( props ) => {
 	const { className } = props;
 	return (
 		<div class={ className }>
 			<SchemaConfigMutationSchemeCard
-				isPublicPrivateSchemaEnabled={ isPublicPrivateSchemaEnabled }
-				isAdminSchemaEnabled={ isAdminSchemaEnabled }
-				isSchemaNamespacingEnabled={ isSchemaNamespacingEnabled }
-				isNestedMutationsEnabled={ isNestedMutationsEnabled }
 				{ ...props }
 			/>
 		</div>
