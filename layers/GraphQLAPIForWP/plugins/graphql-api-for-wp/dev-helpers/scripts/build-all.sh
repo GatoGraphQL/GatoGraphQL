1c4ffbf9b855c8b9a05d3040d6c43a77356cb8f3
Leonardo Losoviz <leo@getpop.org>
2021-07-14 17:20:45 +0800

    Install deps via arg

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/dev-helpers/scripts/build-all.sh
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/dev-helpers/scripts/build-all.sh
@@ -6,6 +6,9 @@
 # @see: https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself#comment16925670_59895
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+# To install the dependencies, exec script with arg "true"
+INSTALL_DEPS="$1"
+
 # Function `buildScripts` will run `npm run build`
 # on all folders in the current directory
 buildScripts(){
@@ -18,7 +21,10 @@ buildScripts(){
             echo "In subfolder '$file'"
             cd "$file"
             # Install node_modules/ dependencies
-            npm install --legacy-peer-deps
+            if [ -n "$INSTALL_DEPS" ]; then
+                echo "Installing dependencies"
+                npm install --legacy-peer-deps
+            fi
             npm run build
             cd ..
         fi
