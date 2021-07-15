0c4ee87bfce2bc7e663ac6c1cfe9e6f231ec95b4
Leonardo Losoviz <leo@getpop.org>
2021-07-15 08:06:44 +0800

    Fixed cs

--- layers/GraphQLByPoP/packages/graphql-server/src/FieldResolvers/EchoOperatorGlobalFieldResolver.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/FieldResolvers/EchoOperatorGlobalFieldResolver.php
@@ -16,7 +16,7 @@ class EchoOperatorGlobalFieldResolver extends OperatorGlobalFieldResolver
 {
     /**
      * Only use it when "embeddable fields" is enabled.
-     * 
+     *
      * Check on runtime (not via container) since this option can be
      * assigned to the Schema Configuration in the GraphQL API plugin.
      */
