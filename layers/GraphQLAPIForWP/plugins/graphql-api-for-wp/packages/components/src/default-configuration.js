510eed0d1dc93c0c59e37c3c59afb9332561758e
Leonardo Losoviz <leo@getpop.org>
2021-07-15 12:15:49 +0800

    Improved text

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/packages/components/src/default-configuration.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/packages/components/src/default-configuration.js
@@ -15,4 +15,4 @@ export const EMPTY_LABEL = __('---', 'graphql-api');
 /**
  * Same default value as for environment variable `SETTINGS_VALUE_LABEL` in PHP
  */
-export const SETTINGS_VALUE_LABEL = __('Default (as defined in the Settings)', 'graphql-api');
+export const SETTINGS_VALUE_LABEL = __('Default (use value defined in the Settings page)', 'graphql-api');
