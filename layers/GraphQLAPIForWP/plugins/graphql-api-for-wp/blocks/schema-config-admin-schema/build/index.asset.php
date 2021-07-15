a46682eee416b2aa0615ad1496df55343888dc09
Leonardo Losoviz <leo@getpop.org>
2021-07-15 11:37:15 +0800

    npm run build

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-admin-schema/build/index.asset.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-admin-schema/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '2171be8a67fc71109d709d9a1cdb00a3');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c73e3d6733acfb897f4ddcfd8a9d1db8');
