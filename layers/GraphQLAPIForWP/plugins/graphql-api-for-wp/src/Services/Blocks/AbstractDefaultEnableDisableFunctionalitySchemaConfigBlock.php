ab76925d08fb0a17711fd638d136c84a97ff119b
Leonardo Losoviz <leo@getpop.org>
2021-07-15 11:56:20 +0800

    Created base class

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/AbstractDefaultEnableDisableFunctionalitySchemaConfigBlock.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\Services\Blocks;
+
+use GraphQLAPI\GraphQLAPI\ComponentConfiguration;
+use GraphQLAPI\GraphQLAPI\Constants\BlockAttributeNames;
+
+abstract class AbstractDefaultEnableDisableFunctionalitySchemaConfigBlock extends AbstractSchemaConfigBlock
+{
+    use OptionsBlockTrait;
+
+    protected abstract function getBlockLabel(): string;
+    protected abstract function getBlockTitle(): string;
+
+    /**
+     * @param array<string, mixed> $attributes
+     */
+    public function renderBlock(array $attributes, string $content): string
+    {
+        // Append "-front" because this style must be used only on the client, not on the admin
+        $className = $this->getBlockClassName() . '-front';
+
+        $blockContentPlaceholder = '<p><strong>%s</strong> %s</p>';
+
+        $enabledDisabledLabels = $this->getEnabledDisabledLabels();
+        $blockContent = sprintf(
+            $blockContentPlaceholder,
+            $this->getBlockLabel(),
+            $enabledDisabledLabels[$attributes[BlockAttributeNames::ENABLE] ?? ''] ?? ComponentConfiguration::getSettingsValueLabel()
+        );
+
+        $blockContentPlaceholder = <<<EOT
+            <div class="%s">
+                <h3 class="%s">%s</h3>
+                %s
+            </div>
+        EOT;
+        return sprintf(
+            $blockContentPlaceholder,
+            $className . ' ' . $this->getAlignClass(),
+            $className . '__title',
+            $this->getBlockTitle(),
+            $blockContent
+        );
+    }
+
+    /**
+     * Register index.css
+     */
+    protected function registerEditorCSS(): bool
+    {
+        return true;
+    }
+    /**
+     * Register style-index.css
+     */
+    protected function registerCommonStyleCSS(): bool
+    {
+        return true;
+    }
+}
