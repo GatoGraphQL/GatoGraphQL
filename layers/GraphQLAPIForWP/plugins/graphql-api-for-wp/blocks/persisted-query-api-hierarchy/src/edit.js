2f42971fc1003d303253a4ab005d0db60787e23f
Leonardo Losoviz <leo@getpop.org>
2021-07-15 17:22:43 +0800

    Removed commented code

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/persisted-query-api-hierarchy/src/edit.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/persisted-query-api-hierarchy/src/edit.js
@@ -1,11 +1,4 @@
 /**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
- */
-// import { __ } from '@wordpress/i18n';
-
-/**
  * Application imports
  */
 import PersistedQueryAPIHierarchy from './persisted-query-api-hierarchy';
--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-cache-control-lists/src/edit.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-cache-control-lists/src/edit.js
@@ -1,11 +1,4 @@
 /**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
- */
-// import { __ } from '@wordpress/i18n';
-
-/**
  * Application imports
  */
 import { CacheControlListEditableOnFocusMultiSelectControl } from '@graphqlapi/components';
