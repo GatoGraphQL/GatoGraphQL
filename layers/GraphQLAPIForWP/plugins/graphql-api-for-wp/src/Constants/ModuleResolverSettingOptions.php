7b251bc2c9f2f07705a7e177c92ff2f89c1df414
Leonardo Losoviz <leo@getpop.org>
2021-07-15 10:31:40 +0800

    Created ModuleResolverSettingOptions

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Constants/ModuleResolverSettingOptions.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\ModuleResolvers;
+
+class ModuleResolverSettingOptions
+{
+    public const ENABLE = 'enable';
+    public const LIST_DEFAULT_LIMIT = 'list-default-limit';
+    public const LIST_MAX_LIMIT = 'list-max-limit';
+    public const CUSTOMPOST_TYPES = 'custompost-types';
+    public const ENTRIES = 'entries';
+    public const BEHAVIOR = 'behavior';
+}
