b4de2fda047bf08a10c0d6d445297d55b2211704
Leonardo Losoviz <leo@getpop.org>
2021-07-16 23:33:56 +0800

    Moved function under Executer

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/CustomPostTypes/GraphQLPersistedQueryEndpointCustomPostType.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/CustomPostTypes/GraphQLPersistedQueryEndpointCustomPostType.php
@@ -15,7 +15,6 @@ use GraphQLAPI\GraphQLAPI\Registries\PersistedQueryEndpointBlockRegistryInterfac
 use GraphQLAPI\GraphQLAPI\Registries\PersistedQueryEndpointExecuterRegistryInterface;
 use GraphQLAPI\GraphQLAPI\Security\UserAuthorizationInterface;
 use GraphQLAPI\GraphQLAPI\Services\Blocks\AbstractEndpointOptionsBlock;
-use GraphQLAPI\GraphQLAPI\Services\Blocks\PersistedQueryEndpointGraphiQLBlock;
 use GraphQLAPI\GraphQLAPI\Services\Blocks\PersistedQueryEndpointOptionsBlock;
 use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\AbstractGraphQLEndpointCustomPostType;
 use GraphQLAPI\GraphQLAPI\Services\Helpers\BlockContentHelpers;
@@ -184,70 +183,6 @@ class GraphQLPersistedQueryEndpointCustomPostType extends AbstractGraphQLEndpoin
     public function usePostExcerptAsDescription(): bool
     {
         return true;
-    }
-
-    /**
-     * Add the parent query to the rendering of the GraphQL Query CPT
-     */
-    protected function getGraphQLQuerySourceContent(string $content, WP_Post $graphQLQueryPost): string
-    {
-        $content = parent::getGraphQLQuerySourceContent($content, $graphQLQueryPost);
-
-        /**
-         * If the GraphQL query has a parent, possibly it is missing the query/variables/acl/ccl attributes,
-         * which inherits from some parent
-         * In that case, render the block twice:
-         * 1. The current block, with missing attributes
-         * 2. The final block, completing the missing attributes from its parent
-         */
-        if ($graphQLQueryPost->post_parent) {
-            /**
-             * @var PersistedQueryEndpointGraphiQLBlock
-             */
-            $graphiQLBlock = $this->instanceManager->getInstance(PersistedQueryEndpointGraphiQLBlock::class);
-
-            // Check if the user is authorized to see the content
-            $ancestorContent = null;
-            if ($this->userAuthorization->canAccessSchemaEditor()) {
-                /**
-                 * If the query has a parent, also render the inherited output
-                 */
-                list(
-                    $inheritQuery
-                ) = $this->blockContentHelpers->getSinglePersistedQueryOptionsBlockAttributesFromPost($graphQLQueryPost);
-                if ($inheritQuery) {
-                    // Fetch the attributes using inheritance
-                    list(
-                        $inheritedGraphQLQuery,
-                        $inheritedGraphQLVariables
-                    ) = $this->graphQLQueryPostTypeHelpers->getGraphQLQueryPostAttributes($graphQLQueryPost, true);
-                    // To render the variables in the block, they must be json_encoded
-                    if ($inheritedGraphQLVariables) {
-                        $inheritedGraphQLVariables = json_encode($inheritedGraphQLVariables);
-                    }
-                    // Render the block again, using the inherited attributes
-                    $inheritedGraphQLBlockAttributes = [
-                        PersistedQueryEndpointGraphiQLBlock::ATTRIBUTE_NAME_QUERY => $inheritedGraphQLQuery,
-                        PersistedQueryEndpointGraphiQLBlock::ATTRIBUTE_NAME_VARIABLES => $inheritedGraphQLVariables,
-                    ];
-                    // Add the new rendering to the output, and a description for each
-                    $ancestorContent = $graphiQLBlock->renderBlock($inheritedGraphQLBlockAttributes, '');
-                }
-            } else {
-                $ancestorContent = $graphiQLBlock->renderUnauthorizedAccess();
-            }
-            if (!is_null($ancestorContent)) {
-                $content = sprintf(
-                    '%s%s<hr/>%s%s',
-                    \__('<p><u>GraphQL query, inheriting properties from ancestor(s): </u></p>'),
-                    $ancestorContent,
-                    \__('<p><u>GraphQL query, as defined in this level: </u></p>'),
-                    $content
-                );
-            }
-        }
-
-        return $content;
     }
 
     protected function getEndpointOptionsBlock(): AbstractEndpointOptionsBlock
--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/EndpointExecuters/ViewPersistedQueryEndpointSourceEndpointExecuter.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/EndpointExecuters/ViewPersistedQueryEndpointSourceEndpointExecuter.php
@@ -6,9 +6,11 @@ namespace GraphQLAPI\GraphQLAPI\Services\EndpointExecuters;
 
 use GraphQLAPI\GraphQLAPI\ModuleResolvers\EndpointFunctionalityModuleResolver;
 use GraphQLAPI\GraphQLAPI\Registries\ModuleRegistryInterface;
+use GraphQLAPI\GraphQLAPI\Services\Blocks\PersistedQueryEndpointGraphiQLBlock;
 use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\AbstractGraphQLEndpointCustomPostType;
 use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\GraphQLPersistedQueryEndpointCustomPostType;
 use PoP\ComponentModel\Instances\InstanceManagerInterface;
+use WP_Post;
 
 class ViewPersistedQueryEndpointSourceEndpointExecuter extends AbstractViewSourceEndpointExecuter implements PersistedQueryEndpointExecuterServiceTagInterface
 {
@@ -31,5 +33,69 @@ class ViewPersistedQueryEndpointSourceEndpointExecuter extends AbstractViewSourc
     protected function getCustomPostType(): AbstractGraphQLEndpointCustomPostType
     {
         return $this->graphQLPersistedQueryEndpointCustomPostType;
+    }
+
+    /**
+     * Add the parent query to the rendering of the GraphQL Query CPT
+     */
+    protected function getGraphQLQuerySourceContent(string $content, WP_Post $graphQLQueryPost): string
+    {
+        $content = parent::getGraphQLQuerySourceContent($content, $graphQLQueryPost);
+
+        /**
+         * If the GraphQL query has a parent, possibly it is missing the query/variables/acl/ccl attributes,
+         * which inherits from some parent
+         * In that case, render the block twice:
+         * 1. The current block, with missing attributes
+         * 2. The final block, completing the missing attributes from its parent
+         */
+        if ($graphQLQueryPost->post_parent) {
+            /**
+             * @var PersistedQueryEndpointGraphiQLBlock
+             */
+            $graphiQLBlock = $this->instanceManager->getInstance(PersistedQueryEndpointGraphiQLBlock::class);
+
+            // Check if the user is authorized to see the content
+            $ancestorContent = null;
+            if ($this->userAuthorization->canAccessSchemaEditor()) {
+                /**
+                 * If the query has a parent, also render the inherited output
+                 */
+                list(
+                    $inheritQuery
+                ) = $this->blockContentHelpers->getSinglePersistedQueryOptionsBlockAttributesFromPost($graphQLQueryPost);
+                if ($inheritQuery) {
+                    // Fetch the attributes using inheritance
+                    list(
+                        $inheritedGraphQLQuery,
+                        $inheritedGraphQLVariables
+                    ) = $this->graphQLQueryPostTypeHelpers->getGraphQLQueryPostAttributes($graphQLQueryPost, true);
+                    // To render the variables in the block, they must be json_encoded
+                    if ($inheritedGraphQLVariables) {
+                        $inheritedGraphQLVariables = json_encode($inheritedGraphQLVariables);
+                    }
+                    // Render the block again, using the inherited attributes
+                    $inheritedGraphQLBlockAttributes = [
+                        PersistedQueryEndpointGraphiQLBlock::ATTRIBUTE_NAME_QUERY => $inheritedGraphQLQuery,
+                        PersistedQueryEndpointGraphiQLBlock::ATTRIBUTE_NAME_VARIABLES => $inheritedGraphQLVariables,
+                    ];
+                    // Add the new rendering to the output, and a description for each
+                    $ancestorContent = $graphiQLBlock->renderBlock($inheritedGraphQLBlockAttributes, '');
+                }
+            } else {
+                $ancestorContent = $graphiQLBlock->renderUnauthorizedAccess();
+            }
+            if (!is_null($ancestorContent)) {
+                $content = sprintf(
+                    '%s%s<hr/>%s%s',
+                    \__('<p><u>GraphQL query, inheriting properties from ancestor(s): </u></p>'),
+                    $ancestorContent,
+                    \__('<p><u>GraphQL query, as defined in this level: </u></p>'),
+                    $content
+                );
+            }
+        }
+
+        return $content;
     }
 }
