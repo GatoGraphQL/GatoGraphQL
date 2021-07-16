e37de295824ef3a978d53332ecfb5d2ca0e1c965
Leonardo Losoviz <leo@getpop.org>
2021-07-16 21:57:48 +0800

    Annotator for PersistedQuery

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Registries/PersistedQueryEndpointAnnotatorRegistry.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\Registries;
+
+class PersistedQueryEndpointAnnotatorRegistry extends AbstractEndpointAnnotatorRegistry implements PersistedQueryEndpointAnnotatorRegistryInterface
+{
+}
