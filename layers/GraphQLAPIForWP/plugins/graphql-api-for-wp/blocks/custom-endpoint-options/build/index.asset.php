84e96cef00f328c4b2159fc65acd2563bcf48864
Leonardo Losoviz <leo@getpop.org>
2021-07-19 05:13:53 +0800

    npm run build

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/custom-endpoint-options/build/index.asset.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/custom-endpoint-options/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '38a0cef8b9249b1c497f7aad06c220e7');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '006353c21015da8a71583d90dae86b7e');
