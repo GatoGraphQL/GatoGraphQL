60737fdc918aa3a877527d283d52be1faae5f807
Leonardo Losoviz <leo@getpop.org>
2021-07-19 19:26:27 +0800

    Imported classes

--- src/Config/Symplify/MonorepoBuilder/DataSources/ReleaseWorkersDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/ReleaseWorkersDataSource.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace PoP\PoP\Config\Symplify\MonorepoBuilder\DataSources;
 
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
+
 class ReleaseWorkersDataSource
 {
     /**
