959cadce4302ca7b6f279bfc588be3a804c358fd
Leonardo Losoviz <leo@getpop.org>
2021-07-30 16:52:26 +0800

    Get avatar from registry

--- layers/Schema/packages/user-avatars/src/TypeDataLoaders/UserAvatarTypeDataLoader.php
+++ layers/Schema/packages/user-avatars/src/TypeDataLoaders/UserAvatarTypeDataLoader.php
@@ -4,14 +4,31 @@ declare(strict_types=1);
 
 namespace PoPSchema\UserAvatars\TypeDataLoaders;
 
+use PoP\ComponentModel\Instances\InstanceManagerInterface;
 use PoP\ComponentModel\TypeDataLoaders\AbstractTypeDataLoader;
+use PoP\Hooks\HooksAPIInterface;
+use PoP\LooseContracts\NameResolverInterface;
+use PoPSchema\UserAvatars\RuntimeRegistries\UserAvatarRuntimeRegistryInterface;
 
 class UserAvatarTypeDataLoader extends AbstractTypeDataLoader
 {
+    public function __construct(
+        HooksAPIInterface $hooksAPI,
+        InstanceManagerInterface $instanceManager,
+        NameResolverInterface $nameResolver,
+        protected UserAvatarRuntimeRegistryInterface $userAvatarRuntimeRegistry,
+    ) {
+        parent::__construct(
+            $hooksAPI,
+            $instanceManager,
+            $nameResolver,
+        );
+    }
+
     public function getObjects(array $ids): array
     {
         return array_map(
-            fn (string | int $id) => null/*$this->menuItemRuntimeRegistry->getMenuItem($id)*/,
+            fn (string | int $id) => $this->userAvatarRuntimeRegistry->getUserAvatar($id),
             $ids
         );
     }
