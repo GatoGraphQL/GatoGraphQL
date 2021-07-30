2f2dcb9467678873777a9e7f1e6b6e7e81b905ca
Leonardo Losoviz <leo@getpop.org>
2021-07-30 16:08:52 +0800

    Moved services

--- layers/Schema/packages/user-avatars-wp/src/Component.php
+++ layers/Schema/packages/user-avatars-wp/src/Component.php
@@ -46,6 +46,5 @@ class Component extends AbstractComponent
         array $skipSchemaComponentClasses = []
     ): void {
         self::initServices(dirname(__DIR__));
-        self::initSchemaServices(dirname(__DIR__), $skipSchema);
     }
 }
