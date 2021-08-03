58881e04557690428c5a443073cdcea6b2b39e7f
Leonardo Losoviz <leo@getpop.org>
2021-08-03 17:52:19 +0800

    Removed `AddTagToChangelogReleaseWorker`

--- src/Config/Symplify/MonorepoBuilder/DataSources/ReleaseWorkersDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/ReleaseWorkersDataSource.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PoP\PoP\Config\Symplify\MonorepoBuilder\DataSources;
 
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualConflictsReleaseWorker;
@@ -25,7 +24,6 @@ class ReleaseWorkersDataSource
             UpdateReplaceReleaseWorker::class,
             SetCurrentMutualConflictsReleaseWorker::class,
             SetCurrentMutualDependenciesReleaseWorker::class,
-            AddTagToChangelogReleaseWorker::class,
             TagVersionReleaseWorker::class,
             PushTagReleaseWorker::class,
             SetNextMutualDependenciesReleaseWorker::class,
