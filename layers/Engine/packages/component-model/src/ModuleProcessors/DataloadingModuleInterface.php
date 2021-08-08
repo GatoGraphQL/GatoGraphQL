dcb80c97cb215d54af834954aa7ca3c64764e708
Leonardo Losoviz <leo@getpop.org>
2021-08-08 18:53:01 +0800

    Removed commented code

--- layers/Engine/packages/component-model/src/ModuleProcessors/DataloadingModuleInterface.php
+++ layers/Engine/packages/component-model/src/ModuleProcessors/DataloadingModuleInterface.php
@@ -6,5 +6,4 @@ namespace PoP\ComponentModel\ModuleProcessors;
 
 interface DataloadingModuleInterface extends FormattableModuleInterface
 {
-    // public function getFilterSubmodule(array $module): ?array;
 }
