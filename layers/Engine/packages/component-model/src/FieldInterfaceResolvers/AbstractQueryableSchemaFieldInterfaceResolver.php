3f41b89391fa57935f72be5e967f98b08ae0a837
Leonardo Losoviz <leo@getpop.org>
2021-08-08 20:39:26 +0800

    Add `getFieldDataFilteringModule` to `FieldInterfaceResolver`

--- layers/Engine/packages/component-model/src/FieldInterfaceResolvers/AbstractQueryableSchemaFieldInterfaceResolver.php
+++ layers/Engine/packages/component-model/src/FieldInterfaceResolvers/AbstractQueryableSchemaFieldInterfaceResolver.php
@@ -9,4 +9,24 @@ use PoP\ComponentModel\Resolvers\QueryableFieldResolverTrait;
 abstract class AbstractQueryableSchemaFieldInterfaceResolver extends AbstractSchemaFieldInterfaceResolver
 {
     use QueryableFieldResolverTrait;
+
+    protected function getFieldDataFilteringModule(string $fieldName): ?array
+    {
+        return null;
+    }
+
+    public function getSchemaFieldArgs(string $fieldName): array
+    {
+        $schemaFieldArgs = parent::getSchemaFieldArgs($fieldName);
+        
+        // Retrieve all the schema definitions for the filter inputs
+        if ($filterDataloadingModule = $this->getFieldDataFilteringModule($fieldName)) {
+            return array_merge(
+                $schemaFieldArgs,
+                $this->getFilterSchemaDefinitionItems($filterDataloadingModule)
+            );
+        }
+        
+        return $schemaFieldArgs;
+    }
 }
