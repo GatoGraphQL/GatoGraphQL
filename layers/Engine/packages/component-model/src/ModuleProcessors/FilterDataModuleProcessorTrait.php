5a26ac44877b49c13c1d1d297d5a307df5fe1220
Leonardo Losoviz <leo@getpop.org>
2021-08-08 15:36:16 +0800

    Undo visibility

--- layers/Engine/packages/component-model/src/ModuleProcessors/FilterDataModuleProcessorTrait.php
+++ layers/Engine/packages/component-model/src/ModuleProcessors/FilterDataModuleProcessorTrait.php
@@ -34,7 +34,7 @@ trait FilterDataModuleProcessorTrait
         }
     }
 
-    protected function getActiveDataloadQueryArgsFilteringModules(array $module, array $source = null): array
+    public function getActiveDataloadQueryArgsFilteringModules(array $module, array $source = null): array
     {
         // Search for cached result
         $cacheKey = json_encode($source ?? []);
