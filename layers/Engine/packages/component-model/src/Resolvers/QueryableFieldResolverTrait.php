a5db81e5ade7bfa9b1078f3552af5e85b3144b52
Leonardo Losoviz <leo@getpop.org>
2021-08-08 15:03:08 +0800

    Typed function

--- layers/Engine/packages/component-model/src/Resolvers/QueryableFieldResolverTrait.php
+++ layers/Engine/packages/component-model/src/Resolvers/QueryableFieldResolverTrait.php
@@ -4,19 +4,22 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Resolvers;
 
-use PoP\ComponentModel\Misc\GeneralUtils;
 use PoP\ComponentModel\Facades\ModuleProcessors\ModuleProcessorManagerFacade;
+use PoP\ComponentModel\Misc\GeneralUtils;
+use PoP\ComponentModel\ModuleProcessors\DataloadQueryArgsFilterInputModuleProcessorInterface;
 
 trait QueryableFieldResolverTrait
 {
     protected function getFilterSchemaDefinitionItems(array $filterDataloadingModule): array
     {
         $moduleprocessor_manager = ModuleProcessorManagerFacade::getInstance();
-        $filterqueryargs_modules = $moduleprocessor_manager->getProcessor((array)$filterDataloadingModule)->getDataloadQueryArgsFilteringModules($filterDataloadingModule);
+        $filterqueryargs_modules = $moduleprocessor_manager->getProcessor($filterDataloadingModule)->getDataloadQueryArgsFilteringModules($filterDataloadingModule);
         return GeneralUtils::arrayFlatten(
             array_map(
-                function ($module) use ($moduleprocessor_manager) {
-                    return $moduleprocessor_manager->getProcessor($module)->getFilterInputSchemaDefinitionItems($module);
+                function (array $module) use ($moduleprocessor_manager) {
+                    /** @var DataloadQueryArgsFilterInputModuleProcessorInterface */
+                    $dataloadQueryArgsFilterInputModuleProcessor = $moduleprocessor_manager->getProcessor($module);
+                    return $dataloadQueryArgsFilterInputModuleProcessor->getFilterInputSchemaDefinitionItems($module);
                 },
                 $filterqueryargs_modules
             )
