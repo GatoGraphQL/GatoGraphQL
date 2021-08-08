86925decac67b263534f922d101b15a8837c1b97
Leonardo Losoviz <leo@getpop.org>
2021-08-08 15:17:24 +0800

    Typed vars

--- layers/Engine/packages/component-model/src/ModuleProcessors/QueryDataModuleProcessorInterface.php
+++ layers/Engine/packages/component-model/src/ModuleProcessors/QueryDataModuleProcessorInterface.php
@@ -7,4 +7,5 @@ namespace PoP\ComponentModel\ModuleProcessors;
 interface QueryDataModuleProcessorInterface
 {
     public function getDataloadQueryArgsFilteringModules(array $module): array;
+    public function filterHeadmoduleDataloadQueryArgs(array $module, array &$query, array $source = null): void;
 }
