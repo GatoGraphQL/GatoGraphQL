a3cf1801f16c401f4ed16221621f9ece48cf236a
Leonardo Losoviz <leo@getpop.org>
2021-08-09 15:38:47 +0800

    Inject for CustomPosts, not Posts

--- layers/Schema/packages/users/src/ConditionalOnComponent/CustomPosts/SchemaHooks/FilterInputHookSet.php
+++ layers/Schema/packages/users/src/ConditionalOnComponent/CustomPosts/SchemaHooks/FilterInputHookSet.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace PoPSchema\Users\ConditionalOnComponent\CustomPosts\SchemaHooks;
 
 use PoP\Hooks\AbstractHookSet;
-use PoPSchema\Posts\ModuleProcessors\PostFilterInputContainerModuleProcessor;
+use PoPSchema\CustomPosts\ModuleProcessors\AbstractCustomPostFilterInputContainerModuleProcessor;
 use PoPSchema\Users\ConditionalOnComponent\CustomPosts\ModuleProcessors\FormInputs\FilterInputModuleProcessor;
 
 class FilterInputHookSet extends AbstractHookSet
@@ -13,7 +13,7 @@ class FilterInputHookSet extends AbstractHookSet
     protected function init(): void
     {
         $this->hooksAPI->addAction(
-            PostFilterInputContainerModuleProcessor::HOOK_FILTER_INPUTS,
+            AbstractCustomPostFilterInputContainerModuleProcessor::HOOK_FILTER_INPUTS,
             [$this, 'getFilterInputSubmodules']
         );
     }
