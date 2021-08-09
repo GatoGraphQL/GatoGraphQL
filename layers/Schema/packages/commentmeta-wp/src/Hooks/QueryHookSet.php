82b232165fd07b03f3251a943b2b4a4827db4de4
Leonardo Losoviz <leo@getpop.org>
2021-08-09 17:45:01 +0800

    Replaced 'CMSAPI:comments:query' with const

--- layers/Schema/packages/commentmeta-wp/src/Hooks/QueryHookSet.php
+++ layers/Schema/packages/commentmeta-wp/src/Hooks/QueryHookSet.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PoPSchema\CommentMetaWP\Hooks;
 
 use PoP\Hooks\AbstractHookSet;
+use PoPSchema\CommentsWP\TypeAPIs\CommentTypeAPI;
 use PoPSchema\MetaQueryWP\Helpers\MetaQueryHelpers;
 
 class QueryHookSet extends AbstractHookSet
@@ -12,7 +13,7 @@ class QueryHookSet extends AbstractHookSet
     protected function init(): void
     {
         $this->hooksAPI->addFilter(
-            'CMSAPI:comments:query',
+            CommentTypeAPI::HOOK_QUERY,
             [MetaQueryHelpers::class, 'convertMetaQuery']
         );
     }
