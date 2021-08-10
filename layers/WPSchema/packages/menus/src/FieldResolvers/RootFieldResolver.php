05ff824d82df669317ae63a3fe05c917dd00ffc3
Leonardo Losoviz <leo@getpop.org>
2021-08-10 19:10:47 +0800

    Added fields menuByLocation and menuBySlug

---
+++ layers/WPSchema/packages/menus/src/FieldResolvers/RootFieldResolver.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Menus\FieldResolvers;
+
+use PoP\ComponentModel\FieldResolvers\AbstractQueryableFieldResolver;
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\Engine\TypeResolvers\RootTypeResolver;
+use PoPSchema\Menus\TypeResolvers\MenuTypeResolver;
+
+class RootFieldResolver extends AbstractQueryableFieldResolver
+{
+    public function getClassesToAttachTo(): array
+    {
+        return array(RootTypeResolver::class);
+    }
+
+    public function getFieldNamesToResolve(): array
+    {
+        return [
+            'menuByLocation',
+            'menuBySlug',
+        ];
+    }
+
+    public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $descriptions = [
+            'menuByLocation' => $this->translationAPI->__('Get a menu by its location', 'menus'),
+            'menuBySlug' => $this->translationAPI->__('Get a menu by its slug', 'menus'),
+        ];
+        return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): string
+    {
+        $types = [
+            'menuByLocation' => SchemaDefinition::TYPE_ID,
+            'menuBySlug' => SchemaDefinition::TYPE_ID,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): array
+    {
+        switch ($fieldName) {
+            case 'menuByLocation':
+                return [
+                    [
+                        SchemaDefinition::ARGNAME_NAME => 'location',
+                        SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
+                        SchemaDefinition::ARGNAME_DESCRIPTION => $this->translationAPI->__('The location of the menu', 'menus'),
+                        SchemaDefinition::ARGNAME_MANDATORY => true,
+                    ],
+                ];
+            case 'menuBySlug':
+                return [
+                    [
+                        SchemaDefinition::ARGNAME_NAME => 'slug',
+                        SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
+                        SchemaDefinition::ARGNAME_DESCRIPTION => $this->translationAPI->__('The slug of the menu', 'menus'),
+                        SchemaDefinition::ARGNAME_MANDATORY => true,
+                    ],
+                ];
+        }
+        return parent::getSchemaFieldArgs($typeResolver, $fieldName);
+    }
+
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @param array<string, mixed>|null $variables
+     * @param array<string, mixed>|null $expressions
+     * @param array<string, mixed> $options
+     */
+    public function resolveValue(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = [],
+        ?array $variables = null,
+        ?array $expressions = null,
+        array $options = []
+    ): mixed {
+        $locations = \get_nav_menu_locations();
+        switch ($fieldName) {
+            case 'menuByLocation':
+            case 'menuBySlug':
+                $menuParam = null;
+                if ($fieldName === 'menuByLocation') {
+                    $location = $fieldArgs['location'];
+                    $menuParam = $locations[$location] ?? null;
+                } elseif ($fieldName === 'menuBySlug') {
+                    $menuParam = $fieldArgs['slug'];
+                }
+                if ($menuParam === null) {
+                    return null;
+                }
+                $menu = wp_get_nav_menu_object($menuParam);
+                if ($menu === false) {
+                    return null;
+                }
+                return $menu->term_id;
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+
+    public function resolveFieldTypeResolverClass(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        switch ($fieldName) {
+            case 'menuByLocation':
+            case 'menuBySlug':
+                return MenuTypeResolver::class;
+        }
+
+        return parent::resolveFieldTypeResolverClass($typeResolver, $fieldName);
+    }
+}
