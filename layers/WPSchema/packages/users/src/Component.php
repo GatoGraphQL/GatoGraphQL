ea601097ffe7bfa9618ba862e4321b2895b224b3
Leonardo Losoviz <leo@getpop.org>
2021-08-10 13:45:49 +0800

    Added pop-wp-schema/users package

---
+++ layers/WPSchema/packages/users/src/Component.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Users;
+
+use PoP\Root\Component\AbstractComponent;
+
+/**
+ * Initialize component
+ */
+class Component extends AbstractComponent
+{
+    /**
+     * Classes from PoP components that must be initialized before this component
+     *
+     * @return string[]
+     */
+    public static function getDependedComponentClasses(): array
+    {
+        return [
+            \PoPSchema\UsersWP\Component::class,
+        ];
+    }
+
+    /**
+     * Initialize services
+     *
+     * @param array<string, mixed> $configuration
+     * @param string[] $skipSchemaComponentClasses
+     */
+    protected static function initializeContainerServices(
+        array $configuration = [],
+        bool $skipSchema = false,
+        array $skipSchemaComponentClasses = []
+    ): void {
+        self::initSchemaServices(dirname(__DIR__), $skipSchema);
+    }
+}
