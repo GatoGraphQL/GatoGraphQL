61e46614234a81b5eee890dd24a330722129a5e0
Leonardo Losoviz <leo@getpop.org>
2021-08-11 10:54:10 +0800

    Moved ParentIDs filterInput to SchemaCommons

--- layers/Schema/packages/schema-commons/src/FilterInputProcessors/FilterInputProcessor.php
+++ layers/Schema/packages/schema-commons/src/FilterInputProcessors/FilterInputProcessor.php
@@ -14,6 +14,7 @@ class FilterInputProcessor extends AbstractFilterInputProcessor
     public const FILTERINPUT_SEARCH = 'filterinput-search';
     public const FILTERINPUT_DATES = 'filterinput-dates';
     public const FILTERINPUT_INCLUDE = 'filterinput-include';
+    public const FILTERINPUT_PARENT_IDS = 'filterinput-parent-ids';
 
     public function getFilterInputsToProcess(): array
     {
@@ -24,6 +25,7 @@ class FilterInputProcessor extends AbstractFilterInputProcessor
             [self::class, self::FILTERINPUT_SEARCH],
             [self::class, self::FILTERINPUT_DATES],
             [self::class, self::FILTERINPUT_INCLUDE],
+            [self::class, self::FILTERINPUT_PARENT_IDS],
         );
     }
 
@@ -49,6 +51,9 @@ class FilterInputProcessor extends AbstractFilterInputProcessor
                 break;
             case self::FILTERINPUT_INCLUDE:
                 $query['include'] = $value;
+                break;
+            case self::FILTERINPUT_PARENT_IDS:
+                $query['parent-ids'] = $value;
                 break;
         }
     }
