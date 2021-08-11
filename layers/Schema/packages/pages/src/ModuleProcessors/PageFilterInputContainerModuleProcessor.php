23f7a129d88834f982bd32c2c0ec1902f00a8c0a
Leonardo Losoviz <leo@getpop.org>
2021-08-11 11:00:13 +0800

    Inherit the FilterInputs

--- layers/Schema/packages/pages/src/ModuleProcessors/PageFilterInputContainerModuleProcessor.php
+++ layers/Schema/packages/pages/src/ModuleProcessors/PageFilterInputContainerModuleProcessor.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace PoPSchema\Pages\ModuleProcessors;
 
-use PoPSchema\CustomPosts\ModuleProcessors\AbstractCustomPostFilterInputContainerModuleProcessor;
-use PoPSchema\CustomPosts\ModuleProcessors\FormInputs\FilterInputModuleProcessor;
+use PoPSchema\CustomPosts\ModuleProcessors\CustomPostFilterInputContainerModuleProcessor;
 use PoPSchema\SchemaCommons\ModuleProcessors\FormInputs\CommonFilterInputModuleProcessor;
-use PoPSchema\SchemaCommons\ModuleProcessors\FormInputs\CommonFilterMultipleInputModuleProcessor;
 
-class PageFilterInputContainerModuleProcessor extends AbstractCustomPostFilterInputContainerModuleProcessor
+class PageFilterInputContainerModuleProcessor extends CustomPostFilterInputContainerModuleProcessor
 {
     public const HOOK_FILTER_INPUTS = __CLASS__ . ':filter-inputs';
 
@@ -30,37 +28,17 @@ class PageFilterInputContainerModuleProcessor extends AbstractCustomPostFilterIn
 
     public function getFilterInputModules(array $module): array
     {
-        $filterInputModules = match ($module[1]) {
-            self::MODULE_FILTERINNER_PAGELISTLIST,
-            self::MODULE_FILTERINNER_ADMINPAGELISTLIST =>
-                [
-                    [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_SEARCH],
-                    [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_ORDER],
-                    [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_LIMIT],
-                    [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_OFFSET],
-                    [CommonFilterMultipleInputModuleProcessor::class, CommonFilterMultipleInputModuleProcessor::MODULE_FILTERINPUT_DATES],
-                    [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_IDS],
-                    [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_ID],
-                ],
-                self::MODULE_FILTERINNER_PAGELISTCOUNT,
-                self::MODULE_FILTERINNER_ADMINPAGELISTCOUNT =>
-                [
-                    [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_SEARCH],
-                    [CommonFilterMultipleInputModuleProcessor::class, CommonFilterMultipleInputModuleProcessor::MODULE_FILTERINPUT_DATES],
-                    [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_IDS],
-                    [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_ID],
-                ],
-                default => [],
+        // Get the original config from above
+        $targetModule = match ($module[1]) {
+            self::MODULE_FILTERINNER_PAGELISTLIST => [self::class, self::MODULE_FILTERINNER_CUSTOMPOSTLISTLIST],
+            self::MODULE_FILTERINNER_PAGELISTCOUNT => [self::class, self::MODULE_FILTERINNER_CUSTOMPOSTLISTCOUNT],
+            self::MODULE_FILTERINNER_ADMINPAGELISTLIST => [self::class, self::MODULE_FILTERINNER_ADMINCUSTOMPOSTLISTLIST],
+            self::MODULE_FILTERINNER_ADMINPAGELISTCOUNT => [self::class, self::MODULE_FILTERINNER_ADMINCUSTOMPOSTLISTCOUNT],
+            default => null,
         };
-        // "Admin" fields also have the "status" filter
-        if (
-            in_array($module[1], [
-                self::MODULE_FILTERINNER_ADMINPAGELISTLIST,
-                self::MODULE_FILTERINNER_ADMINPAGELISTCOUNT,
-            ])
-        ) {
-            $filterInputModules[] = [FilterInputModuleProcessor::class, FilterInputModuleProcessor::MODULE_FILTERINPUT_CUSTOMPOSTSTATUS];
-        }
+        $filterInputModules = parent::getFilterInputModules($targetModule);
+        // Add the parentIDs filterInput
+        $filterInputModules[] = [CommonFilterInputModuleProcessor::class, CommonFilterInputModuleProcessor::MODULE_FILTERINPUT_PARENT_IDS];
         return $filterInputModules;
     }
 
