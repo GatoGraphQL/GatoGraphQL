d0b31388b1e2997966b74d7a0846db9a7b8a0c8f
Leonardo Losoviz <leo@getpop.org>
2021-08-12 17:25:18 +0800

    No need to check for null in TypeAPI

--- layers/Schema/packages/categories-wp/src/TypeAPIs/AbstractCategoryTypeAPI.php
+++ layers/Schema/packages/categories-wp/src/TypeAPIs/AbstractCategoryTypeAPI.php
@@ -166,8 +166,7 @@ abstract class AbstractCategoryTypeAPI extends TaxonomyTypeAPI implements Catego
             $query['slug'] = $query['slugs'];
             unset($query['slugs']);
         }
-        // Watch out: "parent-id" can also be null
-        if (array_key_exists('parent-id', $query)) {
+        if (isset($query['parent-id'])) {
             $query['parent'] = $query['parent-id'];
             unset($query['parent-id']);
         }
