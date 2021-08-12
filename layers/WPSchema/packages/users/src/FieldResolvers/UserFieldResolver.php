c1de608e3e22ce0a192db03811180a7fc5b78406
Leonardo Losoviz <leo@getpop.org>
2021-08-12 19:24:48 +0800

    Added field `User.registeredDate`

--- layers/WPSchema/packages/users/src/FieldResolvers/UserFieldResolver.php
+++ layers/WPSchema/packages/users/src/FieldResolvers/UserFieldResolver.php
@@ -5,14 +5,43 @@ declare(strict_types=1);
 namespace PoPWPSchema\Users\FieldResolvers;
 
 use PoP\ComponentModel\FieldResolvers\AbstractDBDataFieldResolver;
+use PoP\ComponentModel\HelperServices\SemverHelperServiceInterface;
+use PoP\ComponentModel\Instances\InstanceManagerInterface;
+use PoP\ComponentModel\Schema\FieldQueryInterpreterInterface;
 use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\Engine\CMS\CMSServiceInterface;
+use PoP\Engine\Formatters\DateFormatterInterface;
+use PoP\Hooks\HooksAPIInterface;
+use PoP\LooseContracts\NameResolverInterface;
+use PoP\Translation\TranslationAPIInterface;
 use PoPSchema\Users\TypeResolvers\UserTypeResolver;
 use WP_User;
 
 class UserFieldResolver extends AbstractDBDataFieldResolver
 {
+    public function __construct(
+        TranslationAPIInterface $translationAPI,
+        HooksAPIInterface $hooksAPI,
+        InstanceManagerInterface $instanceManager,
+        FieldQueryInterpreterInterface $fieldQueryInterpreter,
+        NameResolverInterface $nameResolver,
+        CMSServiceInterface $cmsService,
+        SemverHelperServiceInterface $semverHelperService,
+        protected DateFormatterInterface $dateFormatter
+    ) {
+        parent::__construct(
+            $translationAPI,
+            $hooksAPI,
+            $instanceManager,
+            $fieldQueryInterpreter,
+            $nameResolver,
+            $cmsService,
+            $semverHelperService,
+        );
+    }
+
     public function getClassesToAttachTo(): array
     {
         return array(UserTypeResolver::class);
@@ -24,6 +53,14 @@ class UserFieldResolver extends AbstractDBDataFieldResolver
             'nicename',
             'nickname',
             'locale',
+            'registeredDate',
+        ];
+    }
+
+    public function getAdminFieldNames(): array
+    {
+        return [
+            'registeredDate',
         ];
     }
 
@@ -33,6 +70,7 @@ class UserFieldResolver extends AbstractDBDataFieldResolver
             'nicename' => SchemaDefinition::TYPE_STRING,
             'nickname' => SchemaDefinition::TYPE_STRING,
             'locale' => SchemaDefinition::TYPE_STRING,
+            'registeredDate' => SchemaDefinition::TYPE_STRING,
             default => parent::getSchemaFieldType($typeResolver, $fieldName),
         };
     }
@@ -42,7 +80,8 @@ class UserFieldResolver extends AbstractDBDataFieldResolver
         return match ($fieldName) {
             'nicename',
             'nickname',
-            'locale'
+            'locale',
+            'registeredDate'
                 => SchemaTypeModifiers::NON_NULLABLE,
             default
                 => parent::getSchemaFieldTypeModifiers($typeResolver, $fieldName),
@@ -55,8 +94,33 @@ class UserFieldResolver extends AbstractDBDataFieldResolver
             'nicename' => $this->translationAPI->__('User\'s nicename', 'pop-users'),
             'nickname' => $this->translationAPI->__('User\'s nickname', 'pop-users'),
             'locale' => $this->translationAPI->__('Retrieves the locale of a user', 'pop-users'),
+            'registeredDate' => $this->translationAPI->__('The date the user registerd on the site', 'pop-users'),
             default => parent::getSchemaFieldDescription($typeResolver, $fieldName),
         };
+    }
+
+    public function getSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): array
+    {
+        $schemaFieldArgs = parent::getSchemaFieldArgs($typeResolver, $fieldName);
+        switch ($fieldName) {
+            case 'registeredDate':
+                return array_merge(
+                    $schemaFieldArgs,
+                    [
+                        [
+                            SchemaDefinition::ARGNAME_NAME => 'format',
+                            SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
+                            SchemaDefinition::ARGNAME_DESCRIPTION => sprintf(
+                                $this->translationAPI->__('Date format, as defined in %s', 'media'),
+                                'https://www.php.net/manual/en/function.date.php'
+                            ),
+                            SchemaDefinition::ARGNAME_DEFAULT_VALUE => $this->cmsService->getOption($this->nameResolver->getName('popcms:option:dateFormat')),
+                        ],
+                    ]
+                );
+        }
+
+        return $schemaFieldArgs;
     }
 
     /**
@@ -83,6 +147,11 @@ class UserFieldResolver extends AbstractDBDataFieldResolver
                 return $user->nickname;
             case 'locale':
                 return \get_user_locale($user);
+            case 'registeredDate':
+                return $this->dateFormatter->format(
+                    $fieldArgs['format'],
+                    $user->user_registered
+                );
         }
 
         return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
