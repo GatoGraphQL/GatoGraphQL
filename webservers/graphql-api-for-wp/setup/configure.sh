6ad22a4e2a8f4a182e4bfeba38981a4c3c14dedd
Leonardo Losoviz <leo@getpop.org>
2021-08-12 11:12:29 +0800

    Set `ENABLE_UNSAFE_DEFAULTS=true` when creating wp-config.php

--- webservers/graphql-api-for-wp/setup/configure.sh
+++ webservers/graphql-api-for-wp/setup/configure.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 wp config set WP_DEBUG true --raw --path=/app/wordpress
+wp config set GRAPHQL_API_ENABLE_UNSAFE_DEFAULTS true --raw --path=/app/wordpress
 wp rewrite structure '/%postname%/' --hard --path=/app/wordpress
 cp /app/assets/.htaccess /app/wordpress
