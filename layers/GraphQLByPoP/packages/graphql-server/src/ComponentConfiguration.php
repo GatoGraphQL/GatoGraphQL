f2fedfd3f91548a2fee6653776f8b8aabd2c6a46
Leonardo Losoviz <leo@getpop.org>
2021-08-12 08:41:45 +0800

    Added env vars and ComponentConfig

--- layers/GraphQLByPoP/packages/graphql-server/src/ComponentConfiguration.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/ComponentConfiguration.php
@@ -20,6 +20,13 @@ class ComponentConfiguration
     private static bool $enableProactiveFeedbackLogs = true;
     private static bool $enableNestedMutations = false;
     private static ?bool $enableGraphQLIntrospection = null;
+    private static bool $addGlobalFieldsToSchema = false;
+    private static bool $addSelfFieldToSchema = false;
+    private static bool $addFullSchemaFieldToSchema = false;
+    private static bool $addVersionToSchemaFieldDescription = false;
+    private static bool $enableSettingMutationSchemeByURLParam = false;
+    private static bool $enableEnablingGraphQLIntrospectionByURLParam = false;
+    private static bool $addGraphQLIntrospectionPersistedQuery = false;
 
     public static function addSelfFieldForRootTypeToSchema(): bool
     {
@@ -171,6 +178,132 @@ class ComponentConfiguration
         $envVariable = Environment::ENABLE_GRAPHQL_INTROSPECTION;
         $selfProperty = &self::$enableGraphQLIntrospection;
         $defaultValue = null;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
+
+        // Initialize property from the environment/hook
+        self::maybeInitializeConfigurationValue(
+            $envVariable,
+            $selfProperty,
+            $defaultValue,
+            $callback
+        );
+        return $selfProperty;
+    }
+
+    public static function addGlobalFieldsToSchema(): bool
+    {
+        // Define properties
+        $envVariable = Environment::ADD_GLOBAL_FIELDS_TO_SCHEMA;
+        $selfProperty = &self::$addGlobalFieldsToSchema;
+        $defaultValue = false;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
+
+        // Initialize property from the environment/hook
+        self::maybeInitializeConfigurationValue(
+            $envVariable,
+            $selfProperty,
+            $defaultValue,
+            $callback
+        );
+        return $selfProperty;
+    }
+
+    public static function addSelfFieldToSchema(): bool
+    {
+        // Define properties
+        $envVariable = Environment::ADD_SELF_FIELD_TO_SCHEMA;
+        $selfProperty = &self::$addSelfFieldToSchema;
+        $defaultValue = false;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
+
+        // Initialize property from the environment/hook
+        self::maybeInitializeConfigurationValue(
+            $envVariable,
+            $selfProperty,
+            $defaultValue,
+            $callback
+        );
+        return $selfProperty;
+    }
+
+    public static function addFullSchemaFieldToSchema(): bool
+    {
+        // Define properties
+        $envVariable = Environment::ADD_FULLSCHEMA_FIELD_TO_SCHEMA;
+        $selfProperty = &self::$addFullSchemaFieldToSchema;
+        $defaultValue = false;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
+
+        // Initialize property from the environment/hook
+        self::maybeInitializeConfigurationValue(
+            $envVariable,
+            $selfProperty,
+            $defaultValue,
+            $callback
+        );
+        return $selfProperty;
+    }
+
+    public static function addVersionToSchemaFieldDescription(): bool
+    {
+        // Define properties
+        $envVariable = Environment::ADD_VERSION_TO_SCHEMA_FIELD_DESCRIPTION;
+        $selfProperty = &self::$addVersionToSchemaFieldDescription;
+        $defaultValue = false;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
+
+        // Initialize property from the environment/hook
+        self::maybeInitializeConfigurationValue(
+            $envVariable,
+            $selfProperty,
+            $defaultValue,
+            $callback
+        );
+        return $selfProperty;
+    }
+
+    public static function enableSettingMutationSchemeByURLParam(): bool
+    {
+        // Define properties
+        $envVariable = Environment::ENABLE_SETTING_MUTATION_SCHEME_BY_URL_PARAM;
+        $selfProperty = &self::$enableSettingMutationSchemeByURLParam;
+        $defaultValue = false;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
+
+        // Initialize property from the environment/hook
+        self::maybeInitializeConfigurationValue(
+            $envVariable,
+            $selfProperty,
+            $defaultValue,
+            $callback
+        );
+        return $selfProperty;
+    }
+
+    public static function enableEnablingGraphQLIntrospectionByURLParam(): bool
+    {
+        // Define properties
+        $envVariable = Environment::ENABLE_ENABLING_GRAPHQL_INTROSPECTION_BY_URL_PARAM;
+        $selfProperty = &self::$enableEnablingGraphQLIntrospectionByURLParam;
+        $defaultValue = false;
+        $callback = [EnvironmentValueHelpers::class, 'toBool'];
+
+        // Initialize property from the environment/hook
+        self::maybeInitializeConfigurationValue(
+            $envVariable,
+            $selfProperty,
+            $defaultValue,
+            $callback
+        );
+        return $selfProperty;
+    }
+
+    public static function addGraphQLIntrospectionPersistedQuery(): bool
+    {
+        // Define properties
+        $envVariable = Environment::ADD_GRAPHQL_INTROSPECTION_PERSISTED_QUERY;
+        $selfProperty = &self::$addGraphQLIntrospectionPersistedQuery;
+        $defaultValue = false;
         $callback = [EnvironmentValueHelpers::class, 'toBool'];
 
         // Initialize property from the environment/hook
