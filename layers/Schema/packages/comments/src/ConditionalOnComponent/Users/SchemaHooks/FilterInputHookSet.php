8d523df733bc20473ea9ae2e091441ef4063acf9
Leonardo Losoviz <leo@getpop.org>
2021-08-13 17:12:55 +0800

    Added FilterInput to Comments filter

---
+++ layers/Schema/packages/comments/src/ConditionalOnComponent/Users/SchemaHooks/FilterInputHookSet.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\Comments\ConditionalOnComponent\Users\SchemaHooks;
+
+use PoP\Hooks\AbstractHookSet;
+use PoPSchema\Users\ConditionalOnComponent\CustomPosts\ModuleProcessors\FormInputs\FilterInputModuleProcessor;
+use PoPSchema\Comments\ModuleProcessors\CommentFilterInputContainerModuleProcessor;
+
+class FilterInputHookSet extends AbstractHookSet
+{
+
+    protected function init(): void
+    {
+        $this->hooksAPI->addFilter(
+            CommentFilterInputContainerModuleProcessor::HOOK_FILTER_INPUTS,
+            [$this, 'getFilterInputModules']
+        );
+    }
+
+    public function getFilterInputModules(array $filterInputModules): array
+    {
+        return [
+            ...$filterInputModules,
+            [
+                FilterInputModuleProcessor::class,
+                FilterInputModuleProcessor::MODULE_FILTERINPUT_AUTHOR_IDS
+            ],
+        ];
+    }
+}
