ee60dfd4f5a11c2df9b220226d829faaab5c5980
Leonardo Losoviz <leo@getpop.org>
2021-08-14 13:02:24 +0800

    Get modules from service

--- layers/Schema/packages/post-mutations/src/ConditionalOnComponent/Users/SchemaHooks/FilterInputHookSet.php
+++ layers/Schema/packages/post-mutations/src/ConditionalOnComponent/Users/SchemaHooks/FilterInputHookSet.php
@@ -4,12 +4,28 @@ declare(strict_types=1);
 
 namespace PoPSchema\PostMutations\ConditionalOnComponent\Users\SchemaHooks;
 
+use PoP\ComponentModel\Instances\InstanceManagerInterface;
 use PoP\Hooks\AbstractHookSet;
+use PoP\Hooks\HooksAPIInterface;
+use PoP\Translation\TranslationAPIInterface;
 use PoPSchema\PostMutations\ModuleProcessors\PostMutationFilterInputContainerModuleProcessor;
-use PoPSchema\Users\ConditionalOnComponent\CustomPosts\ModuleProcessors\FormInputs\FilterInputModuleProcessor;
+use PoPSchema\Users\ConditionalOnComponent\CustomPosts\SchemaHooks\FilterInputHookSet as UserCustomPostFilterInputHookSet;
 
 class FilterInputHookSet extends AbstractHookSet
 {
+    public function __construct(
+        HooksAPIInterface $hooksAPI,
+        TranslationAPIInterface $translationAPI,
+        InstanceManagerInterface $instanceManager,
+        protected UserCustomPostFilterInputHookSet $userCustomPostFilterInputHookSet,
+    ) {
+        parent::__construct(
+            $hooksAPI,
+            $translationAPI,
+            $instanceManager,
+        );
+    }
+
     protected function init(): void
     {
         $this->hooksAPI->addFilter(
@@ -19,24 +35,11 @@ class FilterInputHookSet extends AbstractHookSet
     }
 
     /**
-     * Remove arguments "author-ids" and "author-slug" from field "myPosts"
+     * Remove author fieldArgs from field "myPosts"
      */
     public function getFilterInputModules(array $filterInputModules): array
     {
-        $modules = [
-            [
-                FilterInputModuleProcessor::class,
-                FilterInputModuleProcessor::MODULE_FILTERINPUT_AUTHOR_IDS
-            ],
-            [
-                FilterInputModuleProcessor::class,
-                FilterInputModuleProcessor::MODULE_FILTERINPUT_AUTHOR_SLUG
-            ],
-            [
-                FilterInputModuleProcessor::class,
-                FilterInputModuleProcessor::MODULE_FILTERINPUT_EXCLUDE_AUTHOR_IDS
-            ],
-        ];
+        $modules = $this->userCustomPostFilterInputHookSet->getAuthorFilterInputModules();
         foreach ($modules as $module) {
             $pos = array_search($module, $filterInputModules);
             if ($pos !== false) {
