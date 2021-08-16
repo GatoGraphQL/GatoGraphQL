9c1bf47757e7a67ba3586fc1a712a3f2064035b7
Leonardo Losoviz <leo@getpop.org>
2021-08-16 14:28:37 +0800

    Use camelCase for fieldArg names

--- layers/Engine/packages/component-model/src/HelperServices/FormInputHelperService.php
+++ layers/Engine/packages/component-model/src/HelperServices/FormInputHelperService.php
@@ -8,9 +8,9 @@ class FormInputHelperService implements FormInputHelperServiceInterface
 {
     public function getMultipleInputName(string $name, string $subname): string
     {
-        // Use "_" instead of "-" so it's compatible with GraphQL
-        // Then, a query field will be called "date_from" (allowed) instead of "date-from" (forbidden)
+        // Use camelCase instead of separating with "-", so it's compatible with GraphQL
+        // Then, a query field will be called "dateFrom" (allowed) instead of "date-from" (forbidden)
         // Otherwise it throws error: "Error: Names must match /^[_a-zA-Z][_a-zA-Z0-9]*$/ but "date-from" does not."
-        return $name . '_' . $subname;
+        return strtolower($name) . ucwords($subname);
     }
 }
