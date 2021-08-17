cab080c142087682897e727ecbc7122337ffd644
Leonardo Losoviz <leo@getpop.org>
2021-08-17 17:56:00 +0800

    Created MODULE_FILTERINPUT_COMMASEPARATED_IDS

--- layers/Schema/packages/schema-commons/src/ModuleProcessors/FormInputs/CommonFilterInputModuleProcessor.php
+++ layers/Schema/packages/schema-commons/src/ModuleProcessors/FormInputs/CommonFilterInputModuleProcessor.php
@@ -25,6 +25,7 @@ class CommonFilterInputModuleProcessor extends AbstractFormInputModuleProcessor 
     public const MODULE_FILTERINPUT_SEARCH = 'filterinput-search';
     public const MODULE_FILTERINPUT_IDS = 'filterinput-ids';
     public const MODULE_FILTERINPUT_ID = 'filterinput-id';
+    public const MODULE_FILTERINPUT_COMMASEPARATED_IDS = 'filterinput-commaseparated-ids';
     public const MODULE_FILTERINPUT_EXCLUDE_IDS = 'filterinput-exclude-ids';
     public const MODULE_FILTERINPUT_PARENT_IDS = 'filterinput-parent-ids';
     public const MODULE_FILTERINPUT_PARENT_ID = 'filterinput-parent-id';
@@ -39,6 +40,7 @@ class CommonFilterInputModuleProcessor extends AbstractFormInputModuleProcessor 
             [self::class, self::MODULE_FILTERINPUT_SEARCH],
             [self::class, self::MODULE_FILTERINPUT_IDS],
             [self::class, self::MODULE_FILTERINPUT_ID],
+            [self::class, self::MODULE_FILTERINPUT_COMMASEPARATED_IDS],
             [self::class, self::MODULE_FILTERINPUT_EXCLUDE_IDS],
             [self::class, self::MODULE_FILTERINPUT_PARENT_IDS],
             [self::class, self::MODULE_FILTERINPUT_PARENT_ID],
@@ -55,6 +57,7 @@ class CommonFilterInputModuleProcessor extends AbstractFormInputModuleProcessor 
             self::MODULE_FILTERINPUT_SEARCH => [FilterInputProcessor::class, FilterInputProcessor::FILTERINPUT_SEARCH],
             self::MODULE_FILTERINPUT_IDS => [FilterInputProcessor::class, FilterInputProcessor::FILTERINPUT_INCLUDE],
             self::MODULE_FILTERINPUT_ID => [FilterInputProcessor::class, FilterInputProcessor::FILTERINPUT_INCLUDE],
+            self::MODULE_FILTERINPUT_COMMASEPARATED_IDS => [FilterInputProcessor::class, FilterInputProcessor::FILTERINPUT_INCLUDE],
             self::MODULE_FILTERINPUT_EXCLUDE_IDS => [FilterInputProcessor::class, FilterInputProcessor::FILTERINPUT_EXCLUDE_IDS],
             self::MODULE_FILTERINPUT_PARENT_IDS => [FilterInputProcessor::class, FilterInputProcessor::FILTERINPUT_PARENT_IDS],
             self::MODULE_FILTERINPUT_PARENT_ID => [FilterInputProcessor::class, FilterInputProcessor::FILTERINPUT_PARENT_ID],
@@ -73,7 +76,7 @@ class CommonFilterInputModuleProcessor extends AbstractFormInputModuleProcessor 
             case self::MODULE_FILTERINPUT_PARENT_IDS:
             case self::MODULE_FILTERINPUT_EXCLUDE_PARENT_IDS:
                 return FormMultipleInput::class;
-            case self::MODULE_FILTERINPUT_ID:
+            case self::MODULE_FILTERINPUT_COMMASEPARATED_IDS:
                 return MultiValueFromStringFormInput::class;
         }
 
@@ -90,6 +93,7 @@ class CommonFilterInputModuleProcessor extends AbstractFormInputModuleProcessor 
             self::MODULE_FILTERINPUT_SEARCH => 'searchfor',
             self::MODULE_FILTERINPUT_IDS => 'ids',
             self::MODULE_FILTERINPUT_ID => 'id',
+            self::MODULE_FILTERINPUT_COMMASEPARATED_IDS => 'id',
             self::MODULE_FILTERINPUT_EXCLUDE_IDS => 'excludeIDs',
             self::MODULE_FILTERINPUT_PARENT_IDS => 'parentIDs',
             self::MODULE_FILTERINPUT_PARENT_ID => 'parentID',
@@ -107,6 +111,7 @@ class CommonFilterInputModuleProcessor extends AbstractFormInputModuleProcessor 
             self::MODULE_FILTERINPUT_SEARCH => SchemaDefinition::TYPE_STRING,
             self::MODULE_FILTERINPUT_IDS => SchemaDefinition::TYPE_ID,
             self::MODULE_FILTERINPUT_ID => SchemaDefinition::TYPE_ID,
+            self::MODULE_FILTERINPUT_COMMASEPARATED_IDS => SchemaDefinition::TYPE_STRING,
             self::MODULE_FILTERINPUT_EXCLUDE_IDS => SchemaDefinition::TYPE_ID,
             self::MODULE_FILTERINPUT_PARENT_IDS => SchemaDefinition::TYPE_ID,
             self::MODULE_FILTERINPUT_PARENT_ID => SchemaDefinition::TYPE_ID,
@@ -149,7 +154,8 @@ class CommonFilterInputModuleProcessor extends AbstractFormInputModuleProcessor 
             self::MODULE_FILTERINPUT_OFFSET => $this->translationAPI->__('Offset the results by how many places (required for pagination)', 'schema-commons'),
             self::MODULE_FILTERINPUT_SEARCH => $this->translationAPI->__('Search for elements containing the given string', 'schema-commons'),
             self::MODULE_FILTERINPUT_IDS => $this->translationAPI->__('Limit results to elements with the given IDs', 'schema-commons'),
-            self::MODULE_FILTERINPUT_ID => sprintf(
+            self::MODULE_FILTERINPUT_ID => $this->translationAPI->__('Fetch the element with the given ID', 'schema-commons'),
+            self::MODULE_FILTERINPUT_COMMASEPARATED_IDS => sprintf(
                 $this->translationAPI->__('Limit results to elements with the given ID, or IDs (separated by \'%s\')', 'schema-commons'),
                 Param::VALUE_SEPARATOR
             ),
