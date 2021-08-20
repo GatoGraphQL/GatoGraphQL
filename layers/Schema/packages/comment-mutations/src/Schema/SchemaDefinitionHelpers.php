92cdfd148616d29b32d7152b95f0c75d5c2590ab
Leonardo Losoviz <leo@getpop.org>
2021-08-20 17:53:29 +0800

    Set default values from logged-in user

--- layers/Schema/packages/comment-mutations/src/Schema/SchemaDefinitionHelpers.php
+++ layers/Schema/packages/comment-mutations/src/Schema/SchemaDefinitionHelpers.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace PoPSchema\CommentMutations\Schema;
 
 use PoP\ComponentModel\Schema\SchemaDefinition;
-use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\State\ApplicationState;
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\Translation\Facades\TranslationAPIFacade;
 use PoPSchema\CommentMutations\MutationResolvers\MutationInputProperties;
 use PoPSchema\Comments\ComponentConfiguration;
+use PoPSchema\Users\Facades\UserTypeAPIFacade;
 
 class SchemaDefinitionHelpers
 {
@@ -49,7 +51,36 @@ class SchemaDefinitionHelpers
                 ];
             }
             if (!ComponentConfiguration::mustUserBeLoggedInToAddComment()) {
-
+                $vars = ApplicationState::getVars();
+                $defaultAuthorName = null;
+                $defaultAuthorEmail = null;
+                $defaultAuthorURL = null;
+                // If the user is logged-in, take his/her properties to set as default
+                if ($vars['global-userstate']['is-user-logged-in']) {
+                    $userTypeAPI = UserTypeAPIFacade::getInstance();
+                    $userID = $vars['global-userstate']['current-user-id'];
+                    $defaultAuthorName = $userTypeAPI->getUserDisplayName($userID);
+                    $defaultAuthorEmail = $userTypeAPI->getUserEmail($userID);
+                    $defaultAuthorURL = $userTypeAPI->getUserURL($userID);
+                }
+                $schemaFieldArgs[] = [
+                    SchemaDefinition::ARGNAME_NAME => MutationInputProperties::AUTHOR_NAME,
+                    SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
+                    SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The comment author\'s name', 'comment-mutations'),
+                    SchemaDefinition::ARGNAME_DEFAULT_VALUE => $defaultAuthorName,
+                ];
+                $schemaFieldArgs[] = [
+                    SchemaDefinition::ARGNAME_NAME => MutationInputProperties::AUTHOR_EMAIL,
+                    SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
+                    SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The comment author\'s email', 'comment-mutations'),
+                    SchemaDefinition::ARGNAME_DEFAULT_VALUE => $defaultAuthorEmail,
+                ];
+                $schemaFieldArgs[] = [
+                    SchemaDefinition::ARGNAME_NAME => MutationInputProperties::AUTHOR_URL,
+                    SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
+                    SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The comment author\'s site URL', 'comment-mutations'),
+                    SchemaDefinition::ARGNAME_DEFAULT_VALUE => $defaultAuthorURL,
+                ];
             }
             self::$schemaFieldArgsCache[$key] = $schemaFieldArgs;
         }
