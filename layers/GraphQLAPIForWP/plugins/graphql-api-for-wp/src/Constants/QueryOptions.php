9106c12190869b0fb2e91db367e24f8d66bb679b
Leonardo Losoviz <leo@getpop.org>
2021-08-20 15:31:57 +0800

    Created QueryOptions class

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Constants/QueryOptions.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\Constants;
+
+class QueryOptions
+{
+    /**
+     * Option to tell the hook to not remove the private CPTs when querying
+     */
+    public const ALLOW_QUERYING_PRIVATE_CPTS = 'allow-querying-private-cpts';
+}
