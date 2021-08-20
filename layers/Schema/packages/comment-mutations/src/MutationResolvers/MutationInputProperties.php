a3cb4a75d173ace766b3b111e661dd2f31abe27d
Leonardo Losoviz <leo@getpop.org>
2021-08-20 17:40:57 +0800

    Added consts

--- layers/Schema/packages/comment-mutations/src/MutationResolvers/MutationInputProperties.php
+++ layers/Schema/packages/comment-mutations/src/MutationResolvers/MutationInputProperties.php
@@ -9,4 +9,7 @@ class MutationInputProperties
     public const CUSTOMPOST_ID = 'customPostID';
     public const COMMENT = 'comment';
     public const PARENT_COMMENT_ID = 'parentCommentID';
+    public const AUTHOR_NAME = 'authorName';
+    public const AUTHOR_EMAIL = 'authorEmail';
+    public const AUTHOR_URL = 'authorURL';
 }
