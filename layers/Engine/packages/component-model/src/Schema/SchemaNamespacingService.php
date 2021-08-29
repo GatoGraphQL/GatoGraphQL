53618cbefa98c1a7e219a3e92281acee197d5963
Leonardo Losoviz <leo@getpop.org>
2021-08-30 06:16:33 +0800

    Renamed vars

--- layers/Engine/packages/component-model/src/Schema/SchemaNamespacingService.php
+++ layers/Engine/packages/component-model/src/Schema/SchemaNamespacingService.php
@@ -9,28 +9,28 @@ class SchemaNamespacingService implements SchemaNamespacingServiceInterface
     /**
      * @var array<string,string>
      */
-    protected array $ownerAndProjectSchemaNamespaces = [];
+    protected array $classOwnerAndProjectNamespaceSchemaNamespaces = [];
 
-    public function addSchemaNamespaceForOwnerAndProject(string $ownerAndProject, $schemaNamespace): void
+    public function addSchemaNamespaceForClassOwnerAndProjectNamespace(string $classOwnerAndProjectNamespace, $schemaNamespace): void
     {
-        $this->ownerAndProjectSchemaNamespaces[$ownerAndProject] = $schemaNamespace;
+        $this->classOwnerAndProjectNamespaceSchemaNamespaces[$classOwnerAndProjectNamespace] = $schemaNamespace;
     }
 
     public function getSchemaNamespace(string $class): string
     {
-        $ownerAndProject = $this->getOwnerAndProjectFromClass($class);
+        $classOwnerAndProjectNamespace = $this->getPSR4ClassOwnerAndProject($class);
         // Check if an entry for this combination of Owner + class has been provided
-        if (isset($this->ownerAndProjectSchemaNamespaces[$ownerAndProject])) {
-            return $this->ownerAndProjectSchemaNamespaces[$ownerAndProject];
+        if (isset($this->classOwnerAndProjectNamespaceSchemaNamespaces[$classOwnerAndProjectNamespace])) {
+            return $this->classOwnerAndProjectNamespaceSchemaNamespaces[$classOwnerAndProjectNamespace];
         }
-        return $this->convertNamespace($ownerAndProject);
+        return $this->convertClassNamespaceToSchemaNamespace($classOwnerAndProjectNamespace);
     }
 
     /**
      * Following PSR-4, namespaces must contain the owner (eg: "PoP") and project name (eg: "ComponentModel")
      * Extract these 2 elements to namespace the types/interfaces
      */
-    protected function getOwnerAndProjectFromClass(string $class): string
+    protected function getPSR4ClassOwnerAndProject(string $class): string
     {
         // First slash: between owner and project name
         $firstSlashPos = strpos($class, '\\');
@@ -47,9 +47,9 @@ class SchemaNamespacingService implements SchemaNamespacingServiceInterface
         return '';
     }
 
-    protected function convertNamespace(string $namespace): string
+    protected function convertClassNamespaceToSchemaNamespace(string $classNamespace): string
     {
-        return str_replace('\\', SchemaDefinition::TOKEN_NAMESPACE_SEPARATOR, $namespace);
+        return str_replace('\\', SchemaDefinition::TOKEN_NAMESPACE_SEPARATOR, $classNamespace);
     }
 
     public function getSchemaNamespacedName(string $schemaNamespace, string $name): string
