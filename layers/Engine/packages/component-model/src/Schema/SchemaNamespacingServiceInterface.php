82b3003fda1663a6f36ef1b122b5d8979f51bc24
Leonardo Losoviz <leo@getpop.org>
2021-08-30 05:47:57 +0800

    Created SchemaNamespacingService

---
+++ layers/Engine/packages/component-model/src/Schema/SchemaNamespacingServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\Schema;
+
+use PoP\ComponentModel\FieldInterfaceResolvers\FieldInterfaceResolverInterface;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+
+interface SchemaNamespacingServiceInterface
+{
+}
