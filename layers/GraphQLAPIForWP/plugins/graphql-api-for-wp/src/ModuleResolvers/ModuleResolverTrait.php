ad9ce7d5ccb3048429fbe202aa35eade00c350b5
Leonardo Losoviz <leo@getpop.org>
2021-08-31 06:58:16 +0800

    Moved functions across traits

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ModuleResolvers/ModuleResolverTrait.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ModuleResolvers/ModuleResolverTrait.php
@@ -11,4 +11,19 @@ trait ModuleResolverTrait
 {
     use HasMarkdownDocumentationModuleResolverTrait;
     use PluginMarkdownContentRetrieverTrait;
+
+    protected function getDefaultValueLabel(): string
+    {
+        return \__('Default value for the Schema Configuration', 'graphql-api');
+    }
+
+    protected function getDefaultValueDescription(): string
+    {
+        return \__('This value will be used when option <code>"Default"</code> is selected in the Schema Configuration', 'graphql-api');
+    }
+
+    protected function getAdminClientDescription(): string
+    {
+        return \__('It will be applied on the GraphiQL and Interactive Schema clients, configuration in Access/Cache Control Lists, others', 'graphql-api');
+    }
 }
