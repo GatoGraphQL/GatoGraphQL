829e1059451844dba56eb120652eb2c43892cf69
Leonardo Losoviz <leo@getpop.org>
2021-08-30 09:23:31 +0800

    Build JS blocks

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-admin-schema/build/index.asset.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-admin-schema/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '324f998dc52cd4883ad820f9da837367');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '38a23db99111dc8639012a3b2d74d480');
