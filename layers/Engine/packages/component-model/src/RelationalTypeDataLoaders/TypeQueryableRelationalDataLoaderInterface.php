7708f532a8b4f9ac8b2ad059e4c6cb7b97d72f54
Leonardo Losoviz <leo@getpop.org>
2021-09-03 18:57:02 +0800

    Renamed TypeQueryableRelationalDataLoaderInterface

--- layers/Engine/packages/component-model/src/RelationalTypeDataLoaders/TypeQueryableRelationalDataLoaderInterface.php
+++ layers/Engine/packages/component-model/src/RelationalTypeDataLoaders/TypeQueryableRelationalDataLoaderInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\RelationalTypeDataLoaders;
 
-interface TypeQueryableDataLoaderInterface
+interface TypeQueryableRelationalDataLoaderInterface
 {
     public function findIDs(array $data_properties): array;
 }
