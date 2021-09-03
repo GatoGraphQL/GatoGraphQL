a8fc1d0cf443bda1e17908a5b1658cd44eb4a3bb
Leonardo Losoviz <leo@getpop.org>
2021-09-04 07:35:32 +0800

    Created service

---
+++ layers/Engine/packages/component-model/src/Facades/DirectivePipeline/DirectivePipelineServiceFacade.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\Facades\DirectivePipeline;
+
+use PoP\ComponentModel\DirectivePipeline\DirectivePipelineServiceInterface;
+use PoP\Root\Container\ContainerBuilderFactory;
+
+class DirectivePipelineServiceFacade
+{
+    public static function getInstance(): DirectivePipelineServiceInterface
+    {
+        /**
+         * @var DirectivePipelineServiceInterface
+         */
+        $service = ContainerBuilderFactory::getInstance()->get(DirectivePipelineServiceInterface::class);
+        return $service;
+    }
+}
