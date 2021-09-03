b11c0e1f40308fd29754dec76fdf50a059adc2fd
Leonardo Losoviz <leo@getpop.org>
2021-09-03 19:03:32 +0800

    Fixed namespace

--- layers/Engine/packages/component-model/src/RelationalTypeDataLoaders/Object/ObjectTypeQueryableDataLoaderInterface.php
+++ layers/Engine/packages/component-model/src/RelationalTypeDataLoaders/Object/ObjectTypeQueryableDataLoaderInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace PoP\ComponentModel\RelationalTypeDataLoaders;
+namespace PoP\ComponentModel\RelationalTypeDataLoaders\Object;
 
 interface ObjectTypeQueryableDataLoaderInterface
 {
