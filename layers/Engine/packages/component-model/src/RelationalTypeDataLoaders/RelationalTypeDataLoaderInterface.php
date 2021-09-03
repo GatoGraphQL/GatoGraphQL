461372f9b796ee24d4d88f8cc16b498738496a20
Leonardo Losoviz <leo@getpop.org>
2021-09-03 18:56:27 +0800

    Renamed RelationalTypeDataLoaderInterface

--- layers/Engine/packages/component-model/src/RelationalTypeDataLoaders/RelationalTypeDataLoaderInterface.php
+++ layers/Engine/packages/component-model/src/RelationalTypeDataLoaders/RelationalTypeDataLoaderInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\RelationalTypeDataLoaders;
 
-interface TypeDataLoaderInterface
+interface RelationalTypeDataLoaderInterface
 {
     public function getObjects(array $ids): array;
 }
