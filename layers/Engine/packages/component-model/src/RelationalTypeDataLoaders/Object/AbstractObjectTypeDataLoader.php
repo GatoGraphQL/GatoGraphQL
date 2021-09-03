3faf4996d954d19f995c9a5383f7dcd12dab7043
Leonardo Losoviz <leo@getpop.org>
2021-09-03 19:04:54 +0800

    Added AbstractObjectTypeDataLoader

---
+++ layers/Engine/packages/component-model/src/RelationalTypeDataLoaders/Object/AbstractObjectTypeDataLoader.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\RelationalTypeDataLoaders\Object;
+
+use PoP\ComponentModel\RelationalTypeDataLoaders\AbstractRelationalTypeDataLoader;
+
+abstract class AbstractObjectTypeDataLoader extends AbstractRelationalTypeDataLoader
+{
+}
