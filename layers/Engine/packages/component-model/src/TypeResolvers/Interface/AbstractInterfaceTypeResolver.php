ee63c9fe082548d8bbd6ce0a855dddff8bab92d8
Leonardo Losoviz <leo@getpop.org>
2021-09-07 15:12:02 +0800

    Added InterfaceTypeResolver classes

---
+++ layers/Engine/packages/component-model/src/TypeResolvers/Interface/AbstractInterfaceTypeResolver.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\TypeResolvers\Interface;
+
+use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
+
+abstract class AbstractInterfaceTypeResolver extends AbstractTypeResolver implements InterfaceTypeResolverInterface
+{
+}
---
+++ layers/Engine/packages/component-model/src/TypeResolvers/Interface/InterfaceTypeResolverInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\TypeResolvers\Interface;
+
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+
+interface InterfaceTypeResolverInterface extends TypeResolverInterface
+{
+}
