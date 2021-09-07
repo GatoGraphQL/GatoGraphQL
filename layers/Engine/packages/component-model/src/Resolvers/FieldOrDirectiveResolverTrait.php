bf2f685c441705e219fd86392d93304232b1ac19
Leonardo Losoviz <leo@getpop.org>
2021-09-07 10:36:16 +0800

    Removed stale import

--- layers/Engine/packages/component-model/src/Resolvers/FieldOrDirectiveResolverTrait.php
+++ layers/Engine/packages/component-model/src/Resolvers/FieldOrDirectiveResolverTrait.php
@@ -9,7 +9,6 @@ use PoP\ComponentModel\Misc\GeneralUtils;
 use PoP\ComponentModel\Schema\FieldQueryUtils;
 use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\ComponentModel\Schema\SchemaHelpers;
-use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
 use PoP\Translation\Facades\TranslationAPIFacade;
 
 trait FieldOrDirectiveResolverTrait
