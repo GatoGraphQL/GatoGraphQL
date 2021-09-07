63c953fc6dbedefdc97c8e96aba2a9858ca7aca5
Leonardo Losoviz <leo@getpop.org>
2021-09-07 11:02:29 +0800

    Removed AbstractSchemaDirectiveResolver

--- layers/Engine/packages/component-model/src/DirectiveResolvers/AbstractGlobalDirectiveResolver.php
+++ layers/Engine/packages/component-model/src/DirectiveResolvers/AbstractGlobalDirectiveResolver.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\DirectiveResolvers;
 
-abstract class AbstractGlobalDirectiveResolver extends AbstractSchemaDirectiveResolver
+abstract class AbstractGlobalDirectiveResolver extends AbstractDirectiveResolver
 {
     use GlobalDirectiveResolverTrait;
 }
