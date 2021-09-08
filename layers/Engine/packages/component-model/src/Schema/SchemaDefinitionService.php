12883d44f3e814f4bc7894971f6f8a6f2a26638d
Leonardo Losoviz <leo@getpop.org>
2021-09-08 17:19:34 +0800

    Removed getTypeSchemaKey

--- layers/Engine/packages/component-model/src/Schema/SchemaDefinitionService.php
+++ layers/Engine/packages/component-model/src/Schema/SchemaDefinitionService.php
@@ -5,15 +5,9 @@ declare(strict_types=1);
 namespace PoP\ComponentModel\Schema;
 
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
-use PoP\ComponentModel\FieldInterfaceResolvers\FieldInterfaceResolverInterface;
 
 class SchemaDefinitionService implements SchemaDefinitionServiceInterface
 {
-    public function getInterfaceSchemaKey(FieldInterfaceResolverInterface $interfaceResolver): string
-    {
-        // By default, use the type name
-        return $interfaceResolver->getMaybeNamespacedInterfaceName();
-    }
     public function getTypeSchemaKey(TypeResolverInterface $typeResolver): string
     {
         // By default, use the type name
