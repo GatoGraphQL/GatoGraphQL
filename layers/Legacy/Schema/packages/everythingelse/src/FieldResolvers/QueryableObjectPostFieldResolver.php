21bde89cac853b1971b06e957dd0c232c203c854
Leonardo Losoviz <leo@getpop.org>
2021-09-08 18:39:25 +0800

    Fixed namespace

--- layers/Legacy/Schema/packages/everythingelse/src/FieldResolvers/QueryableObjectPostFieldResolver.php
+++ layers/Legacy/Schema/packages/everythingelse/src/FieldResolvers/QueryableObjectPostFieldResolver.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoPSchema\EverythingElse\FieldResolvers;
 
-use PoP\API\APIUtils;
+use PoP\SiteBuilderAPI\Helpers\APIUtils;
 use PoP\ComponentModel\FieldResolvers\AbstractDBDataFieldResolver;
 use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
