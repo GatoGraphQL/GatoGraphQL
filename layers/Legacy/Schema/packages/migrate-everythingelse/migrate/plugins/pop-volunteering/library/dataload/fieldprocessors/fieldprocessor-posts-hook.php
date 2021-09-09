b8350ef3760cd0effce47c6fe6fb53342a756b8d
Leonardo Losoviz <leo@getpop.org>
2021-09-09 17:43:21 +0800

    FieldResolver: getClassesToAttachTo => getObjectTypeResolverClassesToAttachTo

--- layers/Legacy/Schema/packages/migrate-everythingelse/migrate/plugins/pop-volunteering/library/dataload/fieldprocessors/fieldprocessor-posts-hook.php
+++ layers/Legacy/Schema/packages/migrate-everythingelse/migrate/plugins/pop-volunteering/library/dataload/fieldprocessors/fieldprocessor-posts-hook.php
@@ -4,14 +4,14 @@ use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
 use PoP\Translation\Facades\TranslationAPIFacade;
-use PoPSchema\CustomPosts\TypeResolvers\Interface\IsCustomPostInterfaceTypeResolver;
+use PoPSchema\CustomPosts\TypeResolvers\Object\AbstractCustomPostTypeResolver;
 
 class PoP_Volunteering_DataLoad_FieldResolver_Posts extends AbstractDBDataFieldResolver
 {
-    public function getClassesToAttachTo(): array
+    public function getObjectTypeResolverClassesToAttachTo(): array
     {
         return [
-            IsCustomPostInterfaceTypeResolver::class,
+            AbstractCustomPostTypeResolver::class,
         ];
     }
 
