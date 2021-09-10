ec85757d65c95680b3faaf55b81466647c0e71c6
Leonardo Losoviz <leo@getpop.org>
2021-09-10 16:52:22 +0800

    Removed stale import

--- layers/Engine/packages/mandatory-directives-by-configuration/src/ConfigurationEntries/ConfigurableMandatoryDirectivesForDirectivesTrait.php
+++ layers/Engine/packages/mandatory-directives-by-configuration/src/ConfigurationEntries/ConfigurableMandatoryDirectivesForDirectivesTrait.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace PoP\MandatoryDirectivesByConfiguration\ConfigurationEntries;
 
-use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
-
 trait ConfigurableMandatoryDirectivesForDirectivesTrait
 {
     /**
