979b6c760e2700f4dc5a5c73f66e4d9404a58a8f
Leonardo Losoviz <leo@getpop.org>
2021-09-13 15:30:35 +0800

    Moved FieldResolvers under ObjectType/

--- src/Config/Rector/Downgrade/Configurators/ChainedRules/GraphQLAPIArrowFnUnionTypeChainedRuleContainerConfigurationService.php
+++ src/Config/Rector/Downgrade/Configurators/ChainedRules/GraphQLAPIArrowFnUnionTypeChainedRuleContainerConfigurationService.php
@@ -19,7 +19,7 @@ class GraphQLAPIArrowFnUnionTypeChainedRuleContainerConfigurationService extends
             $this->pluginDir . '/vendor/pop-schema/menus/src/RelationalTypeDataLoaders/ObjectType/MenuItemTypeDataLoader.php',
             $this->pluginDir . '/vendor/pop-schema/menus/src/RelationalTypeDataLoaders/ObjectType/MenuTypeDataLoader.php',
             $this->pluginDir . '/vendor/pop-schema/user-avatars/src/RelationalTypeDataLoaders/ObjectType/UserAvatarTypeDataLoader.php',
-            $this->pluginDir . '/vendor/pop-wp-schema/menus/src/FieldResolvers/MenuFieldResolver.php',
+            $this->pluginDir . '/vendor/pop-wp-schema/menus/src/FieldResolvers/ObjectType/MenuFieldResolver.php',
         ];
     }
 }
