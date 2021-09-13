3ed07f02c6b3b8ab0f93e1ddfc6a48f6f73297a3
Leonardo Losoviz <leo@getpop.org>
2021-09-13 18:01:32 +0800

    Created AbstractGlobalObjectTypeDirectiveResolver

---
+++ layers/Engine/packages/component-model/src/DirectiveResolvers/AbstractGlobalObjectTypeDirectiveResolver.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\DirectiveResolvers;
+
+abstract class AbstractGlobalObjectTypeDirectiveResolver extends AbstractDirectiveResolver
+{
+    use GlobalObjectTypeDirectiveResolverTrait;
+}
---
+++ layers/Engine/packages/component-model/src/DirectiveResolvers/GlobalObjectTypeDirectiveResolverTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\DirectiveResolvers;
+
+use PoP\ComponentModel\TypeResolvers\ObjectType\AbstractObjectTypeResolver;
+use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
+
+trait GlobalObjectTypeDirectiveResolverTrait
+{
+    public function getRelationalTypeOrInterfaceTypeResolverClassesToAttachTo(): array
+    {
+        // Global: Be attached to all ObjectTypeResolvers
+        return [
+            AbstractObjectTypeResolver::class,
+        ];
+    }
+
+    public function isGlobal(RelationalTypeResolverInterface $relationalTypeResolver): bool
+    {
+        return true;
+    }
+}
