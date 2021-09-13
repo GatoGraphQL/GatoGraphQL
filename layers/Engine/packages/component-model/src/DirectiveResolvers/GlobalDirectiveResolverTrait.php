7ce1757fc5529466a823d1de5d37b29b86b1ed19
Leonardo Losoviz <leo@getpop.org>
2021-09-13 19:18:54 +0800

    Renamed GlobalRelationalTypeDirectiveResolverTrait

--- layers/Engine/packages/component-model/src/DirectiveResolvers/GlobalDirectiveResolverTrait.php
+++ layers/Engine/packages/component-model/src/DirectiveResolvers/GlobalDirectiveResolverTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\DirectiveResolvers;
 
+use PoP\ComponentModel\TypeResolvers\AbstractRelationalTypeResolver;
 use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
 
 trait GlobalDirectiveResolverTrait
@@ -11,5 +12,13 @@ trait GlobalDirectiveResolverTrait
     public function isGlobal(RelationalTypeResolverInterface $relationalTypeResolver): bool
     {
         return true;
+    }
+
+    public function getRelationalTypeOrInterfaceTypeResolverClassesToAttachTo(): array
+    {
+        // Global: Be attached to all RelationalTypeResolvers
+        return [
+            AbstractRelationalTypeResolver::class,
+        ];
     }
 }
