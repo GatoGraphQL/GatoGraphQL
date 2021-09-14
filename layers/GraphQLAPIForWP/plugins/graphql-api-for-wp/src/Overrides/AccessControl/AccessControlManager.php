78767a80b30aa009f3599a0bc41b5677591ef16b
Leonardo Losoviz <leo@getpop.org>
2021-09-14 12:16:06 +0800

    Implemented ACL override

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Overrides/AccessControl/AccessControlManager.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Overrides/AccessControl/AccessControlManager.php
@@ -7,40 +7,95 @@ namespace GraphQLAPI\GraphQLAPI\Overrides\AccessControl;
 use GraphQLByPoP\GraphQLServer\TypeResolvers\ObjectType\MutationRootObjectTypeResolver;
 use GraphQLByPoP\GraphQLServer\TypeResolvers\ObjectType\QueryRootObjectTypeResolver;
 use PoP\AccessControl\Services\AccessControlManager as UpstreamAccessControlManager;
+use PoP\ComponentModel\Instances\InstanceManagerInterface;
 
 class AccessControlManager extends UpstreamAccessControlManager
 {
+    public function __construct(
+        protected InstanceManagerInterface $instanceManager,
+    ) {
+    }
+
     /**
-     * Add an additional entry: whenever QueryRoot and MutationRoot is used,
-     * duplicate it also as Root, so that the user needs to set
-     * the configuration only once
+     * @var array<string, array>
      */
+    protected array $overriddenFieldEntries = [];
+    
     public function addEntriesForFields(string $group, array $fieldEntries): void
     {
         parent::addEntriesForFields($group, $fieldEntries);
 
-        // Find all entries set to QueryRoot or MutationRoot
-        $rootFieldEntries = array_filter(
+        // Make sure to reset getting the entries
+        unset($this->overriddenFieldEntries[$group]);
+    }
+
+    /**
+     * Add additional entries: whenever Root is used,
+     * duplicate it also for both QueryRoot and MutationRoot,
+     * so that the user needs to set the configuration only once.
+     * 
+     * Add this logic when retrieving the entries because by then
+     * the container is compiled and we can access the TypeResolver
+     * services. In contrast, `addEntriesForFields` can be called
+     * with a CompilerPass, so this logic wouldn't work.
+     */
+    public function getEntriesForFields(string $group): array
+    {
+        if (isset($this->overriddenFieldEntries[$group])) {
+            return $this->overriddenFieldEntries[$group];
+        }
+        $fieldEntries = parent::getEntriesForFields($group);
+
+        // Find all entries set to Root
+        if ($rootFieldEntries = array_filter(
             $fieldEntries,
-            fn (array $fieldEntry) => in_array($fieldEntry[0], [
-                QueryRootObjectTypeResolver::class,
-                MutationRootObjectTypeResolver::class,
-            ])
-        );
+            fn (array $fieldEntry) => $fieldEntry[0] === RootObjectTypeResolver::class
+        )) {
+            $fieldEntries = array_merge(
+                $fieldEntries,
+                $this->getAdditionalRootEntriesForFields($rootFieldEntries)
+            );
+        }
 
-        // Replace the type to Root
-        $rootFieldEntries = array_map(
-            function (array $rootFieldEntry): array {
-                $rootFieldEntry[0] = RootObjectTypeResolver::class;
-                return $rootFieldEntry;
-            },
-            $rootFieldEntries
-        );
+        $this->overriddenFieldEntries[$group] = $fieldEntries;
+        return $this->overriddenFieldEntries[$group];
+    }
 
-        // Add the entries
-        $this->fieldEntries[$group] = array_merge(
-            $this->fieldEntries[$group] ?? [],
-            $rootFieldEntries
-        );
+    /**
+     * For each of the rootFieldEntries, add an additional QueryRoot and/or MutationRoot.
+     * Fields "id", "self" and "__typename" can belong to both types.
+     * Otherwise, the field is added to MutationRoot if it has a MutationResolver,
+     * or to QueryRoot otherwise.
+     * 
+     * @param array $rootFieldEntries Entries where the class is set to Root
+     */
+    protected function getAdditionalRootEntriesForFields(array $rootFieldEntries): array
+    {
+        $additionalFieldEntries = [];
+
+        /** @var RootObjectTypeResolver */
+        $rootObjectTypeResolver = $this->instanceManager->getInstance(RootObjectTypeResolver::class);
+
+        foreach ($rootFieldEntries as $rootFieldEntry) {
+            $fieldName = $rootFieldEntry[1];
+            if (in_array($fieldName, ['id', 'self', '__typename'])) {
+                $rootFieldEntry[0] = QueryRootObjectTypeResolver::class;
+                $additionalFieldEntries[] = $rootFieldEntry;
+                $rootFieldEntry[0] = MutationRootObjectTypeResolver::class;
+                $additionalFieldEntries[] = $rootFieldEntry;
+                continue;
+            }
+            // If it has a MutationResolver for that field then add entry for MutationRoot
+            if ($rootObjectTypeResolver->getFieldMutationResolverClass($fieldName) !== null) {
+                $rootFieldEntry[0] = MutationRootObjectTypeResolver::class;
+                $additionalFieldEntries[] = $rootFieldEntry;
+                continue;
+            }
+            // It's a field for QueryRoot
+            $rootFieldEntry[0] = QueryRootObjectTypeResolver::class;
+            $additionalFieldEntries[] = $rootFieldEntry;
+        }
+
+        return $additionalFieldEntries;
     }
 }
