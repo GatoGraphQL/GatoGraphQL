f1932cec5ff116a5cb9dd144627cfa0fb038e51d
Leonardo Losoviz <leo@getpop.org>
2021-09-16 16:42:21 +0800

    Updated phpdoc

--- layers/Engine/packages/component-model/src/TypeResolvers/ScalarType/ScalarTypeResolverInterface.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/ScalarType/ScalarTypeResolverInterface.php
@@ -32,9 +32,12 @@ interface ScalarTypeResolverInterface extends TypeResolverInterface
      * Called by the (GraphQL) engine to convert an input
      * (such as field argument `"Hallo!"` in `{ echo(msg: "Hallo!") }`)
      * into the corresponding scalar entity (in this case, a String).
+     * 
+     * Return an instance of Error if the coercing cannot be done,
+     * with a descriptive error message.
      *
      * @param inputValue mixed the (custom) scalar in any format: itself (eg: an object) or its representation (eg: as a string)
-     * @return mixed the (custom) scalar
+     * @return mixed the coerced (custom) scalar, or an instance of Error if it can't be done
      * 
      * @see https://spec.graphql.org/draft/#sec-Input-Values
      */
