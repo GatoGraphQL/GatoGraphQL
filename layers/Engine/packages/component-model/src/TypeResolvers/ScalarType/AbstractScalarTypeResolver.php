671f4070fbdadc708eee8e96819ef29a22594b36
Leonardo Losoviz <leo@getpop.org>
2021-09-16 16:36:24 +0800

    Created interface

---
+++ layers/Engine/packages/component-model/src/TypeResolvers/ScalarType/AbstractScalarTypeResolver.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\TypeResolvers\ScalarType;
+
+use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
+
+abstract class AbstractScalarTypeResolver extends AbstractTypeResolver implements ScalarTypeResolverInterface
+{
+}
---
+++ layers/Engine/packages/component-model/src/TypeResolvers/ScalarType/ScalarTypeResolverInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\TypeResolvers\ScalarType;
+
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+
+interface ScalarTypeResolverInterface extends TypeResolverInterface
+{
+    
+}
