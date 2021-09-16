4001a0b8e834f0869d69681a996c860e197fc67d
Leonardo Losoviz <leo@getpop.org>
2021-09-16 17:03:02 +0800

    Added GraphQL Built-in Scalars

---
+++ layers/Engine/packages/engine/src/TypeResolvers/ScalarType/BooleanTypeResolver.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Engine\TypeResolvers\ScalarType;
+
+use PoP\ComponentModel\TypeResolvers\ScalarType\AbstractScalarTypeResolver;
+
+/**
+ * GraphQL Built-in Scalar
+ * 
+ * @see https://spec.graphql.org/draft/#sec-Scalars.Built-in-Scalars
+ */
+class BooleanTypeResolver extends AbstractScalarTypeResolver
+{
+    public function getTypeName(): string
+    {
+        return 'Boolean';
+    }
+    
+    public function serialize(mixed $scalarValue): string|int|float|bool|array
+    {
+        return $scalarValue;
+    }
+
+    public function coerceValue(mixed $inputValue): mixed
+    {
+        return $inputValue;
+    }
+}
---
+++ layers/Engine/packages/engine/src/TypeResolvers/ScalarType/IntTypeResolver.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Engine\TypeResolvers\ScalarType;
+
+use PoP\ComponentModel\TypeResolvers\ScalarType\AbstractScalarTypeResolver;
+
+/**
+ * GraphQL Built-in Scalar
+ * 
+ * @see https://spec.graphql.org/draft/#sec-Scalars.Built-in-Scalars
+ */
+class IntTypeResolver extends AbstractScalarTypeResolver
+{
+    public function getTypeName(): string
+    {
+        return 'Int';
+    }
+    
+    public function serialize(mixed $scalarValue): string|int|float|bool|array
+    {
+        return $scalarValue;
+    }
+
+    public function coerceValue(mixed $inputValue): mixed
+    {
+        return $inputValue;
+    }
+}
