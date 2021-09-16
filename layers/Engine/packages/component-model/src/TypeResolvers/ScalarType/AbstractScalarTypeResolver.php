60c657afe201b65fafeff9d2757635621c35abec
Leonardo Losoviz <leo@getpop.org>
2021-09-16 17:20:07 +0800

    Added validateIsNotArrayOrObject

--- layers/Engine/packages/component-model/src/TypeResolvers/ScalarType/AbstractScalarTypeResolver.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/ScalarType/AbstractScalarTypeResolver.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\TypeResolvers\ScalarType;
 
+use PoP\ComponentModel\ErrorHandling\Error;
 use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
 
 abstract class AbstractScalarTypeResolver extends AbstractTypeResolver implements ScalarTypeResolverInterface
@@ -14,5 +15,22 @@ abstract class AbstractScalarTypeResolver extends AbstractTypeResolver implement
     public function serialize(mixed $scalarValue): string|int|float|bool|array
     {
         return $scalarValue;
+    }
+
+    protected function validateIsNotArrayOrObject(mixed $inputValue): ?Error
+    {
+        // Fail if passing an array for unsupporting types
+        if (is_array($inputValue) || is_object($inputValue)) {
+            $entity = is_array($inputValue) ? 'array' : 'object';
+            return new Error(
+                sprintf('%s-cast', $entity),
+                sprintf(
+                    $this->translationAPI->__('An %s cannot be casted to type \'%s\'', 'component-model'),
+                    $entity,
+                    $this->getMaybeNamespacedTypeName()
+                )
+            );
+        }
+        return null;
     }
 }
