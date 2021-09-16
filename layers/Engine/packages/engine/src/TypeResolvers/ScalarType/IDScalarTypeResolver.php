fb905cde747dbdcbfa1f766c2a1f224247515773
Leonardo Losoviz <leo@getpop.org>
2021-09-16 18:00:54 +0800

    Added phpdoc

--- layers/Engine/packages/engine/src/TypeResolvers/ScalarType/IDScalarTypeResolver.php
+++ layers/Engine/packages/engine/src/TypeResolvers/ScalarType/IDScalarTypeResolver.php
@@ -18,6 +18,17 @@ class IDScalarTypeResolver extends AbstractScalarTypeResolver
         return 'ID';
     }
 
+    /**
+     * From the GraphQL spec, for section "ID > Input Coercion":
+     * 
+     *   When expected as an input type, any string (such as "4")
+     *   or integer (such as 4 or -4) input value should be coerced to ID
+     *   as appropriate for the ID formats a given GraphQL service expects.
+     *   Any other input value, including float input values (such as 4.0),
+     *   must raise a request error indicating an incorrect type.
+     *
+     * @see https://spec.graphql.org/draft/#sec-ID.Input-Coercion
+     */
     public function coerceValue(mixed $inputValue): mixed
     {
         if ($error = $this->validateIsNotArrayOrObject($inputValue)) {
