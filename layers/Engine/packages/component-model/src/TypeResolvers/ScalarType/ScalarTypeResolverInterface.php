401d97e011cc662aa0af579fe17b2a04c5dec698
Leonardo Losoviz <leo@getpop.org>
2021-09-16 16:39:29 +0800

    Added interface methods

--- layers/Engine/packages/component-model/src/TypeResolvers/ScalarType/ScalarTypeResolverInterface.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/ScalarType/ScalarTypeResolverInterface.php
@@ -6,7 +6,59 @@ namespace PoP\ComponentModel\TypeResolvers\ScalarType;
 
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
 
+/**
+ * Based on GraphQL custom scalars.
+ *
+ * @see https://www.graphql.de/blog/scalars-in-depth/
+ */
 interface ScalarTypeResolverInterface extends TypeResolverInterface
 {
-    
+    /**
+     * Result coercion. Called by the (GraphQL) engine when printing the response.
+     *
+     * It takes the scalar entity as an input and it is converted
+     * into a format that can be output on the response.
+     * 
+     * `array` is supported as an output type, as to support `JSONObject`.
+     *
+     * @return string|int|float|bool|array formatted representation of the custom scalar
+     */
+    public function serialize(mixed $scalarValue): string|int|float|bool|array;
+
+    /**
+     * It handles both "Literal input coercion" and "Value input coercion"
+     * from the GraphQL spec.
+     * 
+     * Called by the (GraphQL) engine to convert an input
+     * (such as field argument `"Hallo!"` in `{ echo(msg: "Hallo!") }`)
+     * into the corresponding scalar entity (in this case, a String).
+     *
+     * @param inputValue mixed the (custom) scalar in any format: itself (eg: an object) or its representation (eg: as a string)
+     * @return mixed the (custom) scalar
+     * 
+     * @see https://spec.graphql.org/draft/#sec-Input-Values
+     */
+    public function coerceValue(mixed $inputValue): mixed;
+
+    // /**
+    //  * Literal input coercion. Called by the (GraphQL) engine to convert an input
+    //  * (such as field argument `"Hallo!"` in `{ echo(msg: "Hallo!") }`)
+    //  * into the corresponding scalar entity (in this case, a String).
+    //  *
+    //  * @return mixed the (custom) scalar
+    //  * @see https://spec.graphql.org/draft/#sec-Input-Values
+    //  */
+    // public function parseLiteral(string|int|float|bool|array|null $inputValue): mixed;
+
+    // /**
+    //  * Value input coercion.
+    //  * 
+    //  * Similar to `serialize` in that it can take any input: the (custom)
+    //  * scalar itself, or a representation of it (as string, int, etc).
+    //  * 
+    //  * Similar to `parseLiteral` in that it must return the scalar entity
+    //  *
+    //  * @return mixed the (custom) scalar
+    //  */
+    // public function parseValue(mixed $scalarValue): mixed;
 }
