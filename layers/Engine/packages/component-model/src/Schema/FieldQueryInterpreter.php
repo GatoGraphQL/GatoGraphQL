d81219a7dc7a25df07843e85368737fb2d99be5e
Leonardo Losoviz <leo@getpop.org>
2021-09-17 18:49:10 +0800

    Fixed cs

--- layers/Engine/packages/component-model/src/Schema/FieldQueryInterpreter.php
+++ layers/Engine/packages/component-model/src/Schema/FieldQueryInterpreter.php
@@ -149,10 +149,10 @@ class FieldQueryInterpreter extends \PoP\FieldQuery\FieldQueryInterpreter implem
      * If the TypeResolver is of Union type, and we don't have the object
      * (eg: when printing the configuration), then generate a list of the
      * unique field outputs for all the target ObjectTypeResolvers.
-     * 
+     *
      * If the TypeResolver is an Object type, to respect the same response,
      * return an array of a single element, with its own unique field output.
-     * 
+     *
      * @return array<string,string>
      */
     final public function getTargetObjectTypeUniqueFieldOutputKeys(
--- layers/Engine/packages/component-model/src/Schema/FieldQueryInterpreterInterface.php
+++ layers/Engine/packages/component-model/src/Schema/FieldQueryInterpreterInterface.php
@@ -33,10 +33,10 @@ interface FieldQueryInterpreterInterface extends \PoP\FieldQuery\FieldQueryInter
      * If the TypeResolver is of Union type, and we don't have the object
      * (eg: when printing the configuration), then generate a list of the
      * unique field outputs for all the target ObjectTypeResolvers.
-     * 
+     *
      * If the TypeResolver is an Object type, to respect the same response,
      * return an array of a single element, with its own unique field output.
-     * 
+     *
      * @return array<string,string>
      */
     public function getTargetObjectTypeUniqueFieldOutputKeys(
