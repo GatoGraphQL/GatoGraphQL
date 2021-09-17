15f75312727fccea503f38a0844ea45236d0ae32
Leonardo Losoviz <leo@getpop.org>
2021-09-17 10:45:10 +0800

    Added phpdoc

--- layers/Engine/packages/component-model/src/TypeResolvers/ConcreteTypeResolverInterface.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/ConcreteTypeResolverInterface.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\TypeResolvers;
 
+/**
+ * Concrete types are those that can resolve their fields to an actual value:
+ * 
+ * - ObjectType
+ * - UnionType
+ * - ScalarType
+ * - EnumType
+ * 
+ * The InterfaceType indicates of what type the field is, without being able to
+ * resolve it, hence it's not Concrete
+ */
 interface ConcreteTypeResolverInterface extends TypeResolverInterface
 {
 }
