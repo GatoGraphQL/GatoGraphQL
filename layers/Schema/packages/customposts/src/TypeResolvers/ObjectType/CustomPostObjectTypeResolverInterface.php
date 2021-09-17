efb92903ca8799f7a501bad522746d301e7d4e86
Leonardo Losoviz <leo@getpop.org>
2021-09-17 10:39:32 +0800

    extend ObjectTypeResolverInterface

--- layers/Schema/packages/customposts/src/TypeResolvers/ObjectType/CustomPostObjectTypeResolverInterface.php
+++ layers/Schema/packages/customposts/src/TypeResolvers/ObjectType/CustomPostObjectTypeResolverInterface.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace PoPSchema\CustomPosts\TypeResolvers\ObjectType;
 
-interface CustomPostObjectTypeResolverInterface
+use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
+
+interface CustomPostObjectTypeResolverInterface extends ObjectTypeResolverInterface
 {
 
 }
