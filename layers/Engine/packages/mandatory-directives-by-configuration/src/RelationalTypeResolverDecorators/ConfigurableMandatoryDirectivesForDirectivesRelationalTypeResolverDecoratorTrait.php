5db044e78d2b0bc7eb383ffc6ba6ac0b030be715
Leonardo Losoviz <leo@getpop.org>
2021-09-18 21:38:07 +0800

    Validate the configured entry exists [skip ci]

--- layers/Engine/packages/mandatory-directives-by-configuration/src/RelationalTypeResolverDecorators/ConfigurableMandatoryDirectivesForDirectivesRelationalTypeResolverDecoratorTrait.php
+++ layers/Engine/packages/mandatory-directives-by-configuration/src/RelationalTypeResolverDecorators/ConfigurableMandatoryDirectivesForDirectivesRelationalTypeResolverDecoratorTrait.php
@@ -22,8 +22,23 @@ trait ConfigurableMandatoryDirectivesForDirectivesRelationalTypeResolverDecorato
         foreach ($this->getEntries() as $entry) {
             $directiveResolverClass = $entry[0];
             $entryValue = $entry[1] ?? null; // this might be any value (string, array, etc) or, if not defined, null
-            /** @var DirectiveResolverInterface */
+            /**
+             * Because the entries can be stored in DB, we run the risk that the
+             * configured DirectiveResolver class will not exist anymore.
+             * So check that the instance exists, and if it doesn't, then
+             * skip processing the entry
+             */
+            if (!$instanceManager->hasInstance($directiveResolverClass)) {
+                continue;
+            }
+            /**
+             * Just to be on the safe side, also validate the instance is a directive
+             */
             $directiveResolver = $instanceManager->getInstance($directiveResolverClass);
+            if (!($directiveResolver instanceof DirectiveResolverInterface)) {
+                continue;
+            }
+            /** @var DirectiveResolverInterface $directiveResolver */
             $directiveName = $directiveResolver->getDirectiveName();
             $mandatoryDirectivesForDirectives[$directiveName] = $this->getMandatoryDirectives($entryValue);
         }
