33a5c45f0bc8c21c4dabd063544518fd53129ec9
Leonardo Losoviz <leo@getpop.org>
2021-09-19 00:28:22 +0800

    Fixed params

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/SchemaConfigurators/PersistedQueryEndpointSchemaConfigurator.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/SchemaConfigurators/PersistedQueryEndpointSchemaConfigurator.php
@@ -8,6 +8,8 @@ use GraphQLAPI\GraphQLAPI\ModuleResolvers\EndpointFunctionalityModuleResolver;
 use GraphQLAPI\GraphQLAPI\Registries\ModuleRegistryInterface;
 use GraphQLAPI\GraphQLAPI\Registries\PersistedQueryEndpointSchemaConfigurationExecuterRegistryInterface;
 use GraphQLAPI\GraphQLAPI\Registries\SchemaConfigurationExecuterRegistryInterface;
+use GraphQLAPI\GraphQLAPI\Services\Blocks\EndpointSchemaConfigurationBlock;
+use GraphQLAPI\GraphQLAPI\Services\Helpers\BlockHelpers;
 use PoP\ComponentModel\Instances\InstanceManagerInterface;
 
 class PersistedQueryEndpointSchemaConfigurator extends AbstractCustomPostEndpointSchemaConfigurator
@@ -15,11 +17,15 @@ class PersistedQueryEndpointSchemaConfigurator extends AbstractCustomPostEndpoin
     public function __construct(
         InstanceManagerInterface $instanceManager,
         ModuleRegistryInterface $moduleRegistry,
+        BlockHelpers $blockHelpers,
+        EndpointSchemaConfigurationBlock $endpointSchemaConfigurationBlock,
         protected PersistedQueryEndpointSchemaConfigurationExecuterRegistryInterface $persistedQueryEndpointSchemaConfigurationExecuterRegistry
     ) {
         parent::__construct(
             $instanceManager,
             $moduleRegistry,
+            $blockHelpers,
+            $endpointSchemaConfigurationBlock,
         );
     }
 
