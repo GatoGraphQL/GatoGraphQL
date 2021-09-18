30d386b079501f4087e5cbff758f44120a200876
Leonardo Losoviz <leo@getpop.org>
2021-09-18 12:06:33 +0800

    Removed AbstractItemProcessorManager

--- layers/Engine/packages/component-model/src/ModuleProcessors/ModuleProcessorManager.php
+++ layers/Engine/packages/component-model/src/ModuleProcessors/ModuleProcessorManager.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\ModuleProcessors;
 
-use PoP\ComponentModel\ItemProcessors\AbstractItemProcessorManager;
+use PoP\ComponentModel\ItemProcessors\ItemProcessorManagerTrait;
 
-class ModuleProcessorManager extends AbstractItemProcessorManager implements ModuleProcessorManagerInterface
+class ModuleProcessorManager implements ModuleProcessorManagerInterface
 {
+    use ItemProcessorManagerTrait;
 }
