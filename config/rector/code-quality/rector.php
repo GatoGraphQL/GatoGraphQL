befb8c92d69d1bbd0dd4ea860dc016932cd12314
Leonardo Losoviz <leo@getpop.org>
2021-09-20 18:25:46 +0800

    Reverted

--- config/rector/code-quality/rector.php
+++ config/rector/code-quality/rector.php
@@ -22,7 +22,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
     $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
 
-    $monorepoDir = dirname(__DIR__, 5);
+    $monorepoDir = dirname(__DIR__, 3);
 
     // Rector relies on autoload setup of your project; Composer autoload is included by default; to add more:
     $parameters->set(Option::BOOTSTRAP_FILES, [
@@ -32,33 +32,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     // files to skip downgrading
     $parameters->set(Option::PATHS, [
-        $monorepoDir . '/layers/Schema/packages/events/src/TypeResolvers/ObjectType/EventObjectTypeResolver.php',
-        $monorepoDir . '/layers/Schema/packages/locations/src/TypeResolvers/ObjectType/LocationObjectTypeResolver.php',
-        $monorepoDir . '/layers/Wassup/packages/locationposts/src/TypeResolvers/ObjectType/LocationPostObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Engine/packages/engine/src/TypeResolvers/ObjectType/RootObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/ObjectType/AbstractUseRootAsSourceForSchemaObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/ObjectType/DirectiveObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/ObjectType/EnumValueObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/ObjectType/FieldObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/ObjectType/InputValueObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/ObjectType/MutationRootObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/ObjectType/QueryRootObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/ObjectType/TypeObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/comments/src/TypeResolvers/ObjectType/CommentObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/customposts/src/TypeResolvers/ObjectType/CustomPostObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/customposts/src/TypeResolvers/UnionType/CustomPostUnionTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/generic-customposts/src/TypeResolvers/ObjectType/GenericCustomPostObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/media/src/TypeResolvers/ObjectType/MediaObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/menus/src/TypeResolvers/ObjectType/MenuItemObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/menus/src/TypeResolvers/ObjectType/MenuObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/pages/src/TypeResolvers/ObjectType/PageObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/post-categories/src/TypeResolvers/ObjectType/PostCategoryObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/post-tags/src/TypeResolvers/ObjectType/PostTagObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/posts/src/TypeResolvers/ObjectType/PostObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/user-avatars/src/TypeResolvers/ObjectType/UserAvatarObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/user-roles-wp/src/TypeResolvers/ObjectType/UserRoleObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/Schema/packages/users/src/TypeResolvers/ObjectType/UserObjectTypeResolver.php',
-        $monorepoDir . '/submodules/PoP/layers/SiteBuilder/packages/multisite/src/TypeResolvers/ObjectType/SiteObjectTypeResolver.php',
+        $monorepoDir . '/src/*',
+        $monorepoDir . '/layers/*',
     ]);
 
     // files to skip downgrading
