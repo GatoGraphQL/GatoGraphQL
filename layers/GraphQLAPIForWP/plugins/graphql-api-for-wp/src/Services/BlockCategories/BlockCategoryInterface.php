cdcc0338a1dc69d3805ab584ccc868a665e69379
Leonardo Losoviz <leo@getpop.org>
2021-09-20 22:52:16 +0800

    Exchanged Abstract with Interface

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/BlockCategories/BlockCategoryInterface.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/BlockCategories/BlockCategoryInterface.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Services\BlockCategories;
 
-use WP_Block_Editor_Context;
-use WP_Post;
-
 interface BlockCategoryInterface
 {
     /**
@@ -15,20 +12,4 @@ interface BlockCategoryInterface
      * @return string[]
      */
     public function getCustomPostTypes(): array;
-
-    /**
-     * Register the category when in the corresponding CPT
-     *
-     * @param array<array> $categories List of categories, each item is an array with props "slug" and "title"
-     * @return array<array> List of categories, each item is an array with props "slug" and "title"
-     */
-    public function getBlockCategoriesViaBlockEditorContext(array $categories, WP_Block_Editor_Context $blockEditorContext): array;
-
-    /**
-     * Register the category when in the corresponding CPT
-     *
-     * @param array<array> $categories List of categories, each item is an array with props "slug" and "title"
-     * @return array<array> List of categories, each item is an array with props "slug" and "title"
-     */
-    public function getBlockCategories(array $categories, WP_Post $post): array;
 }
