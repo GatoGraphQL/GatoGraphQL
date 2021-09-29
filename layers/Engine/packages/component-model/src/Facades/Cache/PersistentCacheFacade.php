d8813284b36f77acf7b931a56d141335e147058d
Leonardo Losoviz <leo@getpop.org>
2021-09-29 17:22:13 +0800

    Replaced "persistent_cache" with interface

--- layers/Engine/packages/component-model/src/Facades/Cache/PersistentCacheFacade.php
+++ layers/Engine/packages/component-model/src/Facades/Cache/PersistentCacheFacade.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PoP\ComponentModel\Facades\Cache;
 
 use PoP\ComponentModel\Cache\CacheInterface;
+use PoP\ComponentModel\Cache\PersistentCacheInterface;
 use PoP\Root\Container\ContainerBuilderFactory;
 
 class PersistentCacheFacade
@@ -12,8 +13,8 @@ class PersistentCacheFacade
     public static function getInstance(): ?CacheInterface
     {
         $containerBuilderFactory = ContainerBuilderFactory::getInstance();
-        if ($containerBuilderFactory->has('persistent_cache')) {
-            return $containerBuilderFactory->get('persistent_cache');
+        if ($containerBuilderFactory->has(PersistentCacheInterface::class)) {
+            return $containerBuilderFactory->get(PersistentCacheInterface::class);
         }
         return null;
     }
