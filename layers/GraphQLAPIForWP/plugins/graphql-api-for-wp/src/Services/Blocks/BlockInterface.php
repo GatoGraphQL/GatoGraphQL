12196e24178aedd72d56d09490d18205847c00b4
Leonardo Losoviz <leo@getpop.org>
2021-09-29 11:57:14 +0800

    Added BlockInterface

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/BlockInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\Services\Blocks;
+
+use PoP\Root\Services\AutomaticallyInstantiatedServiceInterface;
+
+interface BlockInterface extends AutomaticallyInstantiatedServiceInterface
+{
+}
