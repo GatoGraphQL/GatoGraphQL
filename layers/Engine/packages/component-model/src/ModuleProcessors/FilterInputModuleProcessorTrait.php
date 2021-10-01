125615ee588cae96b878c81e8d8311038d9463bd
Leonardo Losoviz <leo@getpop.org>
2021-10-01 16:11:11 +0800

    Reuse FieldOrDirectiveSchemaDefinitionResolverTrait

--- layers/Engine/packages/component-model/src/ModuleProcessors/FilterInputModuleProcessorTrait.php
+++ layers/Engine/packages/component-model/src/ModuleProcessors/FilterInputModuleProcessorTrait.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\ModuleProcessors;
 
+use PoP\ComponentModel\Resolvers\FieldOrDirectiveSchemaDefinitionResolverTrait;
 use PoP\ComponentModel\Schema\SchemaDefinition;
-use PoP\ComponentModel\TypeResolvers\EnumType\EnumTypeResolverInterface;
 
 trait FilterInputModuleProcessorTrait
 {
+    use FieldOrDirectiveSchemaDefinitionResolverTrait;
+
     /**
      * Return an array of elements, instead of a single element, to enable filters with several inputs (such as "date", with inputs "date-from" and "date-to") to document all of them
      */
@@ -28,60 +30,26 @@ trait FilterInputModuleProcessorTrait
     {
     }
 
-    public function getFilterInputSchemaDefinitionResolver(array $module): ?DataloadQueryArgsSchemaFilterInputModuleProcessorInterface
-    {
-        return null;
-    }
-
     /**
      * Documentation for the module
      */
     protected function getFilterInputSchemaDefinition(array $module): array
     {
-        $schemaDefinition = [
-            SchemaDefinition::ARGNAME_NAME => $this->getName($module),
-        ];
-        if ($filterSchemaDefinitionResolver = $this->getFilterInputSchemaDefinitionResolver($module)) {
-            $inputTypeResolver = $filterSchemaDefinitionResolver->getSchemaFilterInputTypeResolver($module);
-            if ($inputTypeResolver instanceof EnumTypeResolverInterface) {
-                $type = SchemaDefinition::TYPE_ENUM;
-            } else {
-                $type = $inputTypeResolver->getMaybeNamespacedTypeName();
-            }
-            $schemaDefinition[SchemaDefinition::ARGNAME_TYPE] = $type;
-            if ($description = $filterSchemaDefinitionResolver->getSchemaFilterInputDescription($module)) {
-                $schemaDefinition[SchemaDefinition::ARGNAME_DESCRIPTION] = $description;
-            }
-            if ($deprecationDescription = $filterSchemaDefinitionResolver->getSchemaFilterInputDeprecationDescription($module)) {
-                $schemaDefinition[SchemaDefinition::ARGNAME_DEPRECATED] = true;
-                $schemaDefinition[SchemaDefinition::ARGNAME_DEPRECATIONDESCRIPTION] = $deprecationDescription;
-            }
-            $defaultValue = $filterSchemaDefinitionResolver->getSchemaFilterInputDefaultValue($module);
-            if ($defaultValue !== null) {
-                $schemaDefinition[SchemaDefinition::ARGNAME_DEFAULT_VALUE] = $defaultValue;
-            }
-            if ($filterSchemaDefinitionResolver->getSchemaFilterInputMandatory($module)) {
-                $schemaDefinition[SchemaDefinition::ARGNAME_MANDATORY] = true;
-            }
-            // If setting the "array of arrays" flag, there's no need to set the "array" flag
-            $isArrayOfArrays = $filterSchemaDefinitionResolver->getSchemaFilterInputIsArrayOfArraysType($module);
-            if (
-                $filterSchemaDefinitionResolver->getSchemaFilterInputIsArrayType($module)
-                || $isArrayOfArrays
-            ) {
-                $schemaDefinition[SchemaDefinition::ARGNAME_IS_ARRAY] = true;
-                if ($filterSchemaDefinitionResolver->getSchemaFilterInputIsNonNullableItemsInArrayType($module)) {
-                    $schemaDefinition[SchemaDefinition::ARGNAME_IS_NON_NULLABLE_ITEMS_IN_ARRAY] = true;
-                }
-                if ($isArrayOfArrays) {
-                    $schemaDefinition[SchemaDefinition::ARGNAME_IS_ARRAY_OF_ARRAYS] = true;
-                    if ($filterSchemaDefinitionResolver->getSchemaFilterInputIsNonNullableItemsInArrayOfArraysType($module)) {
-                        $schemaDefinition[SchemaDefinition::ARGNAME_IS_NON_NULLABLE_ITEMS_IN_ARRAY_OF_ARRAYS] = true;
-                    }
-                }
-            }
-            $filterSchemaDefinitionResolver->addSchemaDefinitionForFilter($schemaDefinition, $module);
+        $filterSchemaDefinitionResolver = $this->getFilterInputSchemaDefinitionResolver($module);
+        $inputTypeResolver = $filterSchemaDefinitionResolver->getSchemaFilterInputTypeResolver($module);
+        $schemaDefinition = $this->getFieldOrDirectiveArgSchemaDefinition(
+            $this->getName($module),
+            $inputTypeResolver,
+            $filterSchemaDefinitionResolver->getSchemaFilterInputDescription($module),
+            $filterSchemaDefinitionResolver->getSchemaFilterInputDefaultValue($module),
+            $filterSchemaDefinitionResolver->getSchemaFilterInputTypeModifiers($module)
+        );
+        if ($deprecationDescription = $filterSchemaDefinitionResolver->getSchemaFilterInputDeprecationDescription($module)) {
+            $schemaDefinition[SchemaDefinition::ARGNAME_DEPRECATED] = true;
+            $schemaDefinition[SchemaDefinition::ARGNAME_DEPRECATIONDESCRIPTION] = $deprecationDescription;
         }
+        $filterSchemaDefinitionResolver->addSchemaDefinitionForFilter($schemaDefinition, $module);
+        
         return $schemaDefinition;
     }
 }
