7ec1d756b9ff451cac790315952a1f8e8f0273b7
Leonardo Losoviz <leo@getpop.org>
2021-10-01 22:21:14 +0800

    Added new functions to interface

--- layers/Engine/packages/component-model/src/ModuleProcessors/FilterInputContainerModuleProcessorInterface.php
+++ layers/Engine/packages/component-model/src/ModuleProcessors/FilterInputContainerModuleProcessorInterface.php
@@ -6,5 +6,9 @@ namespace PoP\ComponentModel\ModuleProcessors;
 
 interface FilterInputContainerModuleProcessorInterface extends FilterDataModuleProcessorInterface
 {
+    public function getFieldFilterInputNameResolvers(array $module): array;
+    public function getFieldFilterInputDescription(array $module, string $fieldArgName): ?string;
+    public function getFieldFilterInputDefaultValue(array $module, string $fieldArgName): mixed;
+    public function getFieldFilterInputTypeModifiers(array $module, string $fieldArgName): int;
     public function getFilterInputModules(array $module): array;
 }
