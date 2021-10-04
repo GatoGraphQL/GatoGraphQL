148ab403a7804e8ba2414d0364aeb9534559733c
Leonardo Losoviz <leo@getpop.org>
2021-10-04 18:04:53 +0800

    Made service nullable

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ContentProcessors/MarkdownContentRetrieverTrait.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ContentProcessors/MarkdownContentRetrieverTrait.php
@@ -9,11 +9,19 @@ use Symfony\Contracts\Service\Attribute\Required;
 
 trait MarkdownContentRetrieverTrait
 {
-    protected MarkdownContentParserInterface $markdownContentParser;
+    protected ?MarkdownContentParserInterface $markdownContentParser;
 
+    /**
+     * Make all properties nullable, becase the ModuleRegistry is registered
+     * in the SystemContainer, where there are no typeResolvers so it will be null,
+     * and in the ApplicationContainer, from where the "Modules" page is resolved
+     * and which does have all the typeResolvers.
+     * Function `getDescription` will only be accessed from the Application Container,
+     * so the properties will not be null in that situation.
+     */
     #[Required]
     final public function autowireMarkdownContentRetrieverTrait(
-        MarkdownContentParserInterface $markdownContentParser,
+        ?MarkdownContentParserInterface $markdownContentParser,
     ): void {
         $this->markdownContentParser = $markdownContentParser;
     }
