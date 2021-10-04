8fb712da40750cb07012c58ba19306775395a2d1
Leonardo Losoviz <leo@getpop.org>
2021-10-04 20:42:16 +0800

    Reverted changes

--- layers/Engine/packages/routing/src/RoutesTrait.php
+++ layers/Engine/packages/routing/src/RoutesTrait.php
@@ -4,28 +4,19 @@ declare(strict_types=1);
 
 namespace PoP\Routing;
 
-use PoP\Definitions\DefinitionManagerInterface;
-use Symfony\Contracts\Service\Attribute\Required;
+use PoP\Definitions\Facades\DefinitionManagerFacade;
 
 trait RoutesTrait
 {
-    protected DefinitionManagerInterface $definitionManager;
-
-    #[Required]
-    public function autowireRoutesTrait(
-        DefinitionManagerInterface $definitionManager,
-    ): void {
-        $this->definitionManager = $definitionManager;
-    }
-    
     /**
      * Construct all the routes, each of them having a unique definition
      * (if the same "name" is used for 2 different routes, it throws an exception)
      */
     public static function init(): void
     {
+        $definitionManager = DefinitionManagerFacade::getInstance();
         foreach (self::getRouteNameAndVariableRefs() as $route => &$var) {
-            $var = $this->definitionManager->getUniqueDefinition($route, DefinitionGroups::ROUTES);
+            $var = $definitionManager->getUniqueDefinition($route, DefinitionGroups::ROUTES);
         }
     }
     /**
