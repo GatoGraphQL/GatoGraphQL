2ad3beae68c446e95ebffe99a3801ff07940a680
Leonardo Losoviz <leo@getpop.org>
2021-10-05 17:52:01 +0800

    Removed "ARGNAME_"

--- layers/Engine/packages/cache-control/src/Schema/SchemaDefinition.php
+++ layers/Engine/packages/cache-control/src/Schema/SchemaDefinition.php
@@ -8,5 +8,5 @@ use PoP\ComponentModel\Schema\SchemaDefinition as UpstreamSchemaDefinition;
 
 class SchemaDefinition extends UpstreamSchemaDefinition
 {
-    // const ARGNAME_MAX_AGE = 'maxAge';
+    // const MAX_AGE = 'maxAge';
 }
