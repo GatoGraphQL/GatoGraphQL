c30c63370a3a2d27c5c2eab3a4d60b86f071756a
Leonardo Losoviz <leo@getpop.org>
2021-10-07 14:34:27 +0800

    Added interface and type

---
+++ layers/Engine/packages/component-model/src/TypeResolvers/InputObjectType/AbstractInputObjectTypeTypeResolver.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\TypeResolvers\InputObjectType;
+
+use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
+
+abstract class AbstractInputObjectTypeResolver extends AbstractTypeResolver implements InputObjectTypeResolverInterface
+{
+    
+}
