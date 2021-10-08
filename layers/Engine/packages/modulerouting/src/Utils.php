c2e2a72ed3c2227f1e885ef36a30343aa4cf7f71
Leonardo Losoviz <leo@getpop.org>
2021-10-08 18:09:09 +0800

    Added arrayDiffRecursive for PHP 8

--- layers/Engine/packages/modulerouting/src/Utils.php
+++ layers/Engine/packages/modulerouting/src/Utils.php
@@ -40,4 +40,49 @@ class Utils
         // If no values, then must return null, so it's avoided when asking is_null() above
         return $ret ? $ret : null;
     }
+
+    /**
+     * `array_diff` doesn't work with multidimensional arrays,
+     * this functions does.
+     * 
+     * @see https://stackoverflow.com/a/29526501
+     *
+     * @param mixed[] $arr1
+     * @param mixed[] $arr2
+     * @return mixed[]
+     */
+    public static function arrayDiffRecursive(array $arr1, array $arr2): array
+    {
+        $outputDiff = [];
+
+        foreach ($arr1 as $key => $value)
+        {
+            //if the key exists in the second array, recursively call this function 
+            //if it is an array, otherwise check if the value is in arr2
+            if (array_key_exists($key, $arr2))
+            {
+                if (is_array($value))
+                {
+                    $recursiveDiff = self::arrayDiffRecursive($value, $arr2[$key]);
+
+                    if (count($recursiveDiff))
+                    {
+                        $outputDiff[$key] = $recursiveDiff;
+                    }
+                }
+                else if (!in_array($value, $arr2))
+                {
+                    $outputDiff[$key] = $value;
+                }
+            }
+            //if the key is not in the second array, check if the value is in 
+            //the second array (this is a quirk of how array_diff works)
+            else if (!in_array($value, $arr2))
+            {
+                $outputDiff[$key] = $value;
+            }
+        }
+
+        return $outputDiff;
+    }
 }
