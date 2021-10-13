bc4137e51cd5d1c26d48b101bb73cd2b71f284c2
Leonardo Losoviz <leo@getpop.org>
2021-10-13 17:51:30 +0800

    Added MEMBER_OBJECT_TYPES

--- layers/API/packages/api/src/Schema/SchemaDefinition.php
+++ layers/API/packages/api/src/Schema/SchemaDefinition.php
@@ -10,6 +10,7 @@ class SchemaDefinition extends UpstreamSchemaDefinition
 {
     const TYPE_TYPE = 'typeType';
     const IMPLEMENTED_INTERFACES = 'implementedInterfaces';
+    const MEMBER_OBJECT_TYPES = 'memberObjectTypes';
 
     const TYPE_OBJECT = 'object';
     const TYPE_INTERFACE = 'interface';
@@ -17,7 +18,7 @@ class SchemaDefinition extends UpstreamSchemaDefinition
     const TYPE_SCALAR = 'scalar';
     const TYPE_ENUM = 'enum';
     const TYPE_INPUT_OBJECT = 'inputObject';
-    
+
     const PERSISTED_FRAGMENTS = 'persistedFragments';
     const PERSISTED_QUERIES = 'persistedQueries';
     const FRAGMENT_RESOLUTION = 'resolution';
