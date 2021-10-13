0363d98772dbe15ecc6a82fb3ddfc48450b28130
Leonardo Losoviz <leo@getpop.org>
2021-10-13 18:06:58 +0800

    Completed enum

--- layers/API/packages/api/src/ObjectModels/SchemaDefinition/EnumTypeSchemaDefinitionProvider.php
+++ layers/API/packages/api/src/ObjectModels/SchemaDefinition/EnumTypeSchemaDefinitionProvider.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace PoP\API\ObjectModels\SchemaDefinition;
 
 use PoP\API\Schema\SchemaDefinition;
+use PoP\ComponentModel\Resolvers\EnumTypeSchemaDefinitionResolverTrait;
 use PoP\ComponentModel\TypeResolvers\EnumType\EnumTypeResolverInterface;
 
 class EnumTypeSchemaDefinitionProvider extends AbstractTypeSchemaDefinitionProvider
 {
+    use EnumTypeSchemaDefinitionResolverTrait;
+    
     public function __construct(
         protected EnumTypeResolverInterface $enumTypeResolver,
     ) {
@@ -24,7 +27,11 @@ class EnumTypeSchemaDefinitionProvider extends AbstractTypeSchemaDefinitionProvi
     {
         $schemaDefinition = parent::getSchemaDefinition();
 
-        
+        $this->doAddSchemaDefinitionEnumValuesForField(
+            $schemaDefinition,
+            $this->enumTypeResolver
+        );
+
         return $schemaDefinition;
     }
 }
