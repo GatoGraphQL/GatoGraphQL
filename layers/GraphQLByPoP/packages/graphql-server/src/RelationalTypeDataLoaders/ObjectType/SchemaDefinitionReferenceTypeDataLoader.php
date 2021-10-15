8cffbd60889e2bfadac996e2fcf868d0ccb06238
Leonardo Losoviz <leo@getpop.org>
2021-10-15 11:29:42 +0800

    Implemented wrappers

--- layers/GraphQLByPoP/packages/graphql-server/src/RelationalTypeDataLoaders/ObjectType/SchemaDefinitionReferenceTypeDataLoader.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/RelationalTypeDataLoaders/ObjectType/SchemaDefinitionReferenceTypeDataLoader.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace GraphQLByPoP\GraphQLServer\RelationalTypeDataLoaders\ObjectType;
 
+use GraphQLByPoP\GraphQLServer\ObjectModels\ListType;
+use GraphQLByPoP\GraphQLServer\ObjectModels\NonNullType;
 use GraphQLByPoP\GraphQLServer\ObjectModels\SchemaDefinitionReferenceObjectInterface;
 use GraphQLByPoP\GraphQLServer\Registries\SchemaDefinitionReferenceRegistryInterface;
+use GraphQLByPoP\GraphQLServer\Syntax\SyntaxHelpers;
 use PoP\ComponentModel\RelationalTypeDataLoaders\ObjectType\AbstractObjectTypeDataLoader;
 use Symfony\Contracts\Service\Attribute\Required;
 
@@ -21,14 +24,42 @@ class SchemaDefinitionReferenceTypeDataLoader extends AbstractObjectTypeDataLoad
     }
 
     /**
+     * The IDs can contain GraphQL's type wrappers, such as `[String]!`
+     * 
      * @return SchemaDefinitionReferenceObjectInterface[]
      */
     public function getObjects(array $ids): array
     {
-        // Filter out potential `null` results
-        return array_filter(array_map(
-            fn ($schemaDefinitionID) => $this->schemaDefinitionReferenceRegistry->getSchemaDefinitionReference($schemaDefinitionID),
+        return array_map(
+            fn (string $typeID) => $this->getSchemaDefinitionReferenceObject($typeID),
             $ids
-        ));
+        );
+    }
+
+    protected function getSchemaDefinitionReferenceObject(string $typeID): SchemaDefinitionReferenceObjectInterface
+    {
+        // Check if the type is non-null
+        if (SyntaxHelpers::isNonNullType($typeID)) {
+            return new NonNullType(
+                $this->fullSchemaDefinition,
+                $this->schemaDefinitionPath,
+                $this->getSchemaDefinitionReferenceObject(
+                    SyntaxHelpers::getNonNullTypeNestedTypeName($typeID)
+                )
+            );
+        }
+
+        // Check if it is an array
+        if (SyntaxHelpers::isListType($typeID)) {
+            return new ListType(
+                $this->fullSchemaDefinition,
+                $this->schemaDefinitionPath,
+                $this->getSchemaDefinitionReferenceObject(
+                    SyntaxHelpers::getListTypeNestedTypeName($typeID)
+                )
+            );
+        }
+
+        return $this->schemaDefinitionReferenceRegistry->getSchemaDefinitionReference($typeID);
     }
 }
