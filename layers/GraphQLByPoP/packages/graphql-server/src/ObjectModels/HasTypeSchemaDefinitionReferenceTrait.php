52931658882eb36f07d2e5bd9b13c2cc1e03bb26
Leonardo Losoviz <leo@getpop.org>
2021-10-15 11:22:25 +0800

    Add SDL notation in typeID

--- layers/GraphQLByPoP/packages/graphql-server/src/ObjectModels/HasTypeSchemaDefinitionReferenceTrait.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/ObjectModels/HasTypeSchemaDefinitionReferenceTrait.php
@@ -5,18 +5,30 @@ declare(strict_types=1);
 namespace GraphQLByPoP\GraphQLServer\ObjectModels;
 
 use GraphQLByPoP\GraphQLServer\Schema\SchemaDefinitionHelpers;
+use GraphQLByPoP\GraphQLServer\Schema\SchemaHelpers;
 use PoP\API\Schema\SchemaDefinition;
 
 trait HasTypeSchemaDefinitionReferenceTrait
 {
     use ResolveTypeSchemaDefinitionReferenceTrait;
-    
+
+    /**
+     * Append the GraphQL wrappers to the ID
+     */
     public function getTypeID(): string
     {
-        return SchemaDefinitionHelpers::getID([
+        $typeID = SchemaDefinitionHelpers::getID([
             SchemaDefinition::TYPES,
             $this->schemaDefinition[SchemaDefinition::TYPE_KIND],
             $this->schemaDefinition[SchemaDefinition::TYPE_NAME],
         ]);
+        return SchemaHelpers::getTypeToOutputInSchema(
+            $typeID,
+            $this->schemaDefinition[SchemaDefinition::NON_NULLABLE] ?? null,
+            $this->schemaDefinition[SchemaDefinition::IS_ARRAY] ?? false,
+            $this->schemaDefinition[SchemaDefinition::IS_NON_NULLABLE_ITEMS_IN_ARRAY] ?? false,
+            $this->schemaDefinition[SchemaDefinition::IS_ARRAY_OF_ARRAYS] ?? false,
+            $this->schemaDefinition[SchemaDefinition::IS_NON_NULLABLE_ITEMS_IN_ARRAY_OF_ARRAYS] ?? false,
+        );
     }
 }
