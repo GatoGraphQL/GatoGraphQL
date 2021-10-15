f92a62235fffac0e35c097cef855c7feb6d45dac
Leonardo Losoviz <leo@getpop.org>
2021-10-15 10:11:22 +0800

    Removed unneeded props for Scalar

--- layers/GraphQLByPoP/packages/graphql-server/src/ObjectModels/ScalarType.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/ObjectModels/ScalarType.php
@@ -10,20 +10,6 @@ class ScalarType extends AbstractType
 {
     use NonDocumentableTypeTrait;
 
-    public function __construct(
-        array &$fullSchemaDefinition,
-        array $schemaDefinitionPath,
-        protected string $name,
-        array $customDefinition = []
-    ) {
-        parent::__construct($fullSchemaDefinition, $schemaDefinitionPath, $customDefinition);
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
     public function getKind(): string
     {
         return TypeKinds::SCALAR;
