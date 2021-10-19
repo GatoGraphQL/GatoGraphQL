c326697234d955a02f5c9dcbfffcaedb475ec32c
Leonardo Losoviz <leo@getpop.org>
2021-10-19 15:42:27 +0800

    Removed getSchemaDefinitionForFieldHookName

--- layers/Engine/packages/component-model/src/TypeResolvers/HookHelpers.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/HookHelpers.php
@@ -8,7 +8,6 @@ class HookHelpers
 {
     public const HOOK_ENABLED_FIELD_NAMES = __CLASS__ . ':enabled_field_names';
     public const HOOK_ENABLED_DIRECTIVE_NAMES = __CLASS__ . ':resolved_directives_names';
-    public const HOOK_SCHEMA_DEFINITION_FOR_FIELD = __CLASS__ . ':schema_definition_for_field:%s:%s';
 
     public static function getHookNameToFilterDirective(?string $directiveName = null): string
     {
@@ -18,14 +17,5 @@ class HookHelpers
     public static function getHookNameToFilterField(?string $fieldName = null): string
     {
         return self::HOOK_ENABLED_FIELD_NAMES . ($fieldName ? ':' . $fieldName : '');
-    }
-
-    public static function getSchemaDefinitionForFieldHookName(string $typeOrInterfaceTypeFieldResolverClass, string $fieldName): string
-    {
-        return sprintf(
-            self::HOOK_SCHEMA_DEFINITION_FOR_FIELD,
-            $typeOrInterfaceTypeFieldResolverClass,
-            $fieldName
-        );
     }
 }
