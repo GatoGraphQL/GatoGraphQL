614d5a8d35ab6ef728ae27ab7efaffa4d7ddf59a
Leonardo Losoviz <leo@getpop.org>
2021-10-28 12:54:02 +0800

    Implement $this->id

--- layers/GraphQLByPoP/packages/graphql-server/src/ObjectModels/NonNullType.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/ObjectModels/NonNullType.php
@@ -6,19 +6,21 @@ namespace GraphQLByPoP\GraphQLServer\ObjectModels;
 
 class NonNullType extends AbstractWrappingType
 {
+    public function __construct(
+        TypeInterface $wrappedType,
+    ) {
+        parent::__construct($wrappedType);
+        $this->id = sprintf(
+            '%s!',
+            $this->wrappedType->getID()
+        );
+    }
+
     public function getName(): string
     {
         return sprintf(
             '%s!',
             $this->wrappedType->getName()
-        );
-    }
-
-    public function getID(): string
-    {
-        return sprintf(
-            '%s!',
-            $this->wrappedType->getID()
         );
     }
 
