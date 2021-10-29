580eaee69735e895cdfc1a9a93f5ce29316a3888
Leonardo Losoviz <leo@getpop.org>
2021-10-29 22:16:29 +0800

    Added basic service trait

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Clients/CustomEndpointVoyagerClient.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Clients/CustomEndpointVoyagerClient.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace GraphQLAPI\GraphQLAPI\Services\Clients;
 
 use GraphQLByPoP\GraphQLClientsForWP\Clients\VoyagerClient;
+use PoP\ComponentModel\Services\BasicServiceTrait;
 
 class CustomEndpointVoyagerClient extends VoyagerClient
 {
     use CustomEndpointClientTrait;
+    use BasicServiceTrait;
 }
