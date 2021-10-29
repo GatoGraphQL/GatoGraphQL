fc1c69e04b7bd50ee2f98049f7df6d81ff5ecd3e
Leonardo Losoviz <leo@getpop.org>
2021-10-29 23:39:26 +0800

    Made instanceManager lazy

--- layers/Engine/packages/root/src/Services/WithInstanceManagerServiceTrait.php
+++ layers/Engine/packages/root/src/Services/WithInstanceManagerServiceTrait.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace PoP\Root\Services;
 
+use PoP\Root\Facades\Instances\InstanceManagerFacade;
 use PoP\Root\Instances\InstanceManagerInterface;
 use Symfony\Contracts\Service\Attribute\Required;
 
 trait WithInstanceManagerServiceTrait
 {
-    protected InstanceManagerInterface $instanceManager;
+    private ?InstanceManagerInterface $instanceManager = null;
 
-    #[Required]
+    //#[Required]
     public function setInstanceManager(InstanceManagerInterface $instanceManager): void
     {
         $this->instanceManager = $instanceManager;
+    }
+    protected function getInstanceManager(): InstanceManagerInterface
+    {
+        // return $this->instanceManager ??= $this->instanceManager->getInstance(InstanceManagerInterface::class);
+        return $this->instanceManager ??= InstanceManagerFacade::getInstance();
     }
 }
