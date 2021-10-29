faddb6f5395d956ab395d4bb4f3328be6ec5f26d
Leonardo Losoviz <leo@getpop.org>
2021-10-29 17:54:43 +0800

    Added Hooks to BasicService

--- layers/Engine/packages/component-model/src/Services/BasicServiceTrait.php
+++ layers/Engine/packages/component-model/src/Services/BasicServiceTrait.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Services;
 
+use PoP\Hooks\Services\WithHooksAPIServiceTrait;
+
 trait BasicServiceTrait
 {
     use WithInstanceManagerServiceTrait;
     use WithTranslationAPIServiceTrait;
+    use WithHooksAPIServiceTrait;
 }
