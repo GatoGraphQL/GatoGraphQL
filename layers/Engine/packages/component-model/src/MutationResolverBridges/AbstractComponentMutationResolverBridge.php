404d82c1703bec23821628ae1a3c3dc33380d528
Leonardo Losoviz <leo@getpop.org>
2021-10-29 23:31:40 +0800

    Added missing property

--- layers/Engine/packages/component-model/src/MutationResolverBridges/AbstractComponentMutationResolverBridge.php
+++ layers/Engine/packages/component-model/src/MutationResolverBridges/AbstractComponentMutationResolverBridge.php
@@ -7,6 +7,7 @@ namespace PoP\ComponentModel\MutationResolverBridges;
 use PoP\ComponentModel\ErrorHandling\Error;
 use PoP\ComponentModel\Misc\GeneralUtils;
 use PoP\ComponentModel\ModuleProcessors\DataloadingConstants;
+use PoP\ComponentModel\ModuleProcessors\ModuleProcessorManagerInterface;
 use PoP\ComponentModel\MutationResolution\MutationResolutionManagerInterface;
 use PoP\ComponentModel\MutationResolvers\ErrorTypes;
 use PoP\ComponentModel\QueryInputOutputHandlers\ResponseConstants;
@@ -18,6 +19,7 @@ abstract class AbstractComponentMutationResolverBridge implements ComponentMutat
     use BasicServiceTrait;
 
     private ?MutationResolutionManagerInterface $mutationResolutionManager = null;
+    private ?ModuleProcessorManagerInterface $moduleProcessorManager = null;
 
     public function setMutationResolutionManager(MutationResolutionManagerInterface $mutationResolutionManager): void
     {
@@ -26,6 +28,14 @@ abstract class AbstractComponentMutationResolverBridge implements ComponentMutat
     protected function getMutationResolutionManager(): MutationResolutionManagerInterface
     {
         return $this->mutationResolutionManager ??= $this->instanceManager->getInstance(MutationResolutionManagerInterface::class);
+    }
+    public function setModuleProcessorManager(ModuleProcessorManagerInterface $moduleProcessorManager): void
+    {
+        $this->moduleProcessorManager = $moduleProcessorManager;
+    }
+    protected function getModuleProcessorManager(): ModuleProcessorManagerInterface
+    {
+        return $this->moduleProcessorManager ??= $this->instanceManager->getInstance(ModuleProcessorManagerInterface::class);
     }
 
     public function getSuccessString(string | int $result_id): ?string
