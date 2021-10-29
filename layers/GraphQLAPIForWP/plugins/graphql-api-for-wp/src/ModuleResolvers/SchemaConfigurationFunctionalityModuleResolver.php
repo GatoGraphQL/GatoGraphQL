3290ff7a3eb419378f015d16f99049373dc76cd6
Leonardo Losoviz <leo@getpop.org>
2021-10-29 23:24:37 +0800

    Removed stale autowire functions in ModuleResolvers

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ModuleResolvers/SchemaConfigurationFunctionalityModuleResolver.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ModuleResolvers/SchemaConfigurationFunctionalityModuleResolver.php
@@ -37,21 +37,6 @@ class SchemaConfigurationFunctionalityModuleResolver extends AbstractFunctionali
     }
 
     /**
-     * Make all properties nullable, becase the ModuleRegistry is registered
-     * in the SystemContainer, where there are no typeResolvers so it will be null,
-     * and in the ApplicationContainer, from where the "Modules" page is resolved
-     * and which does have all the typeResolvers.
-     * Function `getDescription` will only be accessed from the Application Container,
-     * so the properties will not be null in that situation.
-     */
-    //#[Required]
-    final public function autowireSchemaConfigurationFunctionalityModuleResolver(
-        ?GraphQLSchemaConfigurationCustomPostType $graphQLSchemaConfigurationCustomPostType,
-    ): void {
-        $this->graphQLSchemaConfigurationCustomPostType = $graphQLSchemaConfigurationCustomPostType;
-    }
-
-    /**
      * Setting options
      */
     public const OPTION_MODE = 'mode';
