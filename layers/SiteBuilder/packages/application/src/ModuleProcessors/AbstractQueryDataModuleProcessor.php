8ef21ac584dad6bc2de5cbaad17c627b101bb2d4
Leonardo Losoviz <leo@getpop.org>
2021-10-30 12:26:49 +0800

    Migrated setting services in class, not trait

--- layers/SiteBuilder/packages/application/src/ModuleProcessors/AbstractQueryDataModuleProcessor.php
+++ layers/SiteBuilder/packages/application/src/ModuleProcessors/AbstractQueryDataModuleProcessor.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace PoP\Application\ModuleProcessors;
 
+use PoP\ComponentModel\FilterInputProcessors\FilterInputProcessorManagerInterface;
 use PoP\ComponentModel\ModuleProcessors\QueryDataModuleProcessorInterface;
 use PoP\ComponentModel\ModuleProcessors\QueryDataModuleProcessorTrait;
 
 abstract class AbstractQueryDataModuleProcessor extends AbstractModuleProcessor implements QueryDataModuleProcessorInterface
 {
     use QueryDataModuleProcessorTrait;
+
+    private ?FilterInputProcessorManagerInterface $filterInputProcessorManager = null;
+
+    final public function setFilterInputProcessorManager(FilterInputProcessorManagerInterface $filterInputProcessorManager): void
+    {
+        $this->filterInputProcessorManager = $filterInputProcessorManager;
+    }
+    final protected function getFilterInputProcessorManager(): FilterInputProcessorManagerInterface
+    {
+        return $this->filterInputProcessorManager ??= $this->instanceManager->getInstance(FilterInputProcessorManagerInterface::class);
+    }
 }
