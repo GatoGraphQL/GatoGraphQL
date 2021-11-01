29d8e261d6a4b4ff9b75c252a08865360a117621
Leonardo Losoviz <leo@getpop.org>
2021-11-01 14:52:08 +0800

    Fixed PHPStan

--- layers/Engine/packages/component-model/src/CheckpointProcessors/CheckpointProcessorManager.php
+++ layers/Engine/packages/component-model/src/CheckpointProcessors/CheckpointProcessorManager.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace PoP\ComponentModel\CheckpointProcessors;
 
 use PoP\ComponentModel\ItemProcessors\ItemProcessorManagerTrait;
+use PoP\ComponentModel\Services\BasicServiceTrait;
 
 class CheckpointProcessorManager implements CheckpointProcessorManagerInterface
 {
     use ItemProcessorManagerTrait;
+    use BasicServiceTrait;
 
     public function getProcessor(array $item): CheckpointProcessorInterface
     {
