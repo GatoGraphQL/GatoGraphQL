682b4695651cd05c6a85be859ecd43b611dc88ca
Leonardo Losoviz <leo@getpop.org>
2021-11-08 15:58:19 +0800

    Created AutomaticallyInstantiatedServiceTrait

---
+++ layers/Engine/packages/root/src/Services/AutomaticallyInstantiatedServiceTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Root\Services;
+
+use PoP\Root\Component\ApplicationEvents;
+
+/**
+ * A service which must always be instantiated,
+ * so it's done automatically by the application.
+ * Eg: hooks.
+ */
+trait AutomaticallyInstantiatedServiceTrait
+{
+    public function initialize(): void
+    {
+        // By default, do nothing
+    }
+
+    public function getInstantiationEvent(): string
+    {
+        return ApplicationEvents::BEFORE_BOOT;
+    }
+}
