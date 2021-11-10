892742e41fa80339de37df252a1225c352ed6dc4
Leonardo Losoviz <leo@getpop.org>
2021-11-11 00:00:43 +0800

    Added values to PostOrderBy

--- layers/Schema/packages/posts/src/Constants/PostOrderBy.php
+++ layers/Schema/packages/posts/src/Constants/PostOrderBy.php
@@ -6,7 +6,11 @@ namespace PoPSchema\Posts\Constants;
 
 class PostOrderBy
 {
+    public const ID = 'ID';
     public const TITLE = 'TITLE';
     public const DATE = 'DATE';
-    public const NUMBER_COMMENTS = 'NUMBER_COMMENTS';
+    public const COMMENT_COUNT = 'COMMENT_COUNT';
+    public const RAND = 'RAND';
+    public const MODIFIED_DATE = 'MODIFIED_DATE';
+    public const RELEVANCE = 'RELEVANCE';
 }
