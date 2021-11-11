cf6b7977f78920af9042a353e1e1863a8d5d09a9
Leonardo Losoviz <leo@getpop.org>
2021-11-11 22:42:28 +0800

    Improved phpdoc

--- layers/Engine/packages/component-model/src/TypeResolvers/ConcreteTypeResolverInterface.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/ConcreteTypeResolverInterface.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace PoP\ComponentModel\TypeResolvers;
 
 /**
+ * A "concrete" type is different than "output" type, because
+ * the output type includes the InterfaceType:
+ *
+ * @see https://spec.graphql.org/draft/#sec-Input-and-Output-Types
+ *
  * Concrete types are those that can resolve their fields to an actual value:
  *
  * - ObjectType
@@ -13,7 +18,10 @@ namespace PoP\ComponentModel\TypeResolvers;
  * - EnumType
  *
  * The InterfaceType indicates of what type the field is, without being able to
- * resolve it, hence it's not Concrete
+ * resolve it, hence it's not Concrete.
+ *
+ * Currently there is no OutputTypeResolver because
+ * there is no need for it.
  */
 interface ConcreteTypeResolverInterface extends TypeResolverInterface
 {
