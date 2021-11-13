037d75509316cef39167ba5f49562e8aff8bbe81
Leonardo Losoviz <leo@getpop.org>
2021-11-13 19:41:55 +0800

    AddedLeafOutput

---
+++ layers/Engine/packages/component-model/src/TypeResolvers/LeafOutputTypeResolverInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\TypeResolvers;
+
+/**
+ * This definition of "LeafOutput" type is different than the "Output"
+ * type defined in the GraphQL spec:
+ *
+ * @see https://spec.graphql.org/draft/#sec-Input-and-Output-Types
+ *
+ * In the GraphQL spec, the OutputType also includes ObjectType and InterfaceType.
+ * In this case, "LeafOutput" types do not include them. It includes
+ * only the types which can reach a leaf, and will be printed on the response:
+ *
+ * - ScalarType
+ * - EnumType
+ *
+ * Only these types need be called ->serialize() on them
+ */
+interface LeafOutputTypeResolverInterface extends OutputTypeResolverInterface
+{
+    /**
+     * Result coercion. Called by the (GraphQL) engine when printing the response.
+     *
+     * It takes the scalar entity as an input and it is converted
+     * into a format that can be output on the response.
+     *
+     * `array` is supported as an output type, as to support `JSONObject`.
+     *
+     * @return string|int|float|bool|array formatted representation of the custom scalar
+     */
+    public function serialize(string|int|float|bool|object $scalarValue): string|int|float|bool|array;
+}
