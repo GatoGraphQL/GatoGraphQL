c39abafba4f1291f1127fb305522e6e40ed89584
Leonardo Losoviz <leo@getpop.org>
2021-11-24 17:31:47 +0800

    Added package

---
+++ layers/WPSchema/packages/comments/src/Component.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Comments;
+
+use PoP\Root\Component\AbstractComponent;
+
+/**
+ * Initialize component
+ */
+class Component extends AbstractComponent
+{
+    /**
+     * Classes from PoP components that must be initialized before this component
+     *
+     * @return string[]
+     */
+    public static function getDependedComponentClasses(): array
+    {
+        return [
+            \PoPSchema\CommentsWP\Component::class,
+        ];
+    }
+
+    /**
+     * Initialize services
+     *
+     * @param array<string, mixed> $configuration
+     * @param string[] $skipSchemaComponentClasses
+     */
+    protected static function initializeContainerServices(
+        array $configuration = [],
+        bool $skipSchema = false,
+        array $skipSchemaComponentClasses = []
+    ): void {
+        self::initServices(dirname(__DIR__));
+        self::initSchemaServices(dirname(__DIR__), $skipSchema);
+        self::initSchemaServices(dirname(__DIR__), $skipSchema, '/Overrides');
+    }
+}
