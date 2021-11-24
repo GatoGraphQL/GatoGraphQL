48c515d0d745039c137e156ce1131663b4b1cb18
Leonardo Losoviz <leo@getpop.org>
2021-11-24 17:06:52 +0800

    Migrated Comment responses

---
+++ layers/Schema/packages/comments/src/TypeResolvers/InputObjectType/CommentResponsePaginationInputObjectTypeResolver.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\Comments\TypeResolvers\InputObjectType;
+
+use PoPSchema\Comments\ComponentConfiguration;
+use PoPSchema\SchemaCommons\TypeResolvers\InputObjectType\PaginationInputObjectTypeResolver;
+
+class CommentResponsePaginationInputObjectTypeResolver extends PaginationInputObjectTypeResolver
+{
+    public function getTypeName(): string
+    {
+        return 'CommentResponsePaginationInput';
+    }
+
+    public function getTypeDescription(): ?string
+    {
+        return $this->getTranslationAPI()->__('Input to paginate response comments', 'comments');
+    }
+
+    protected function getDefaultLimit(): ?int
+    {
+        return ComponentConfiguration::getCustomPostCommentOrCommentResponseListDefaultLimit();
+    }
+
+    protected function getMaxLimit(): ?int
+    {
+        return ComponentConfiguration::getCommentListMaxLimit();
+    }
+}
