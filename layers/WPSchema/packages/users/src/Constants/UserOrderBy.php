ad4326f1231a6b0eaf2c8da24e82db6dba5fb4c0
Leonardo Losoviz <leo@getpop.org>
2021-11-26 13:31:39 +0800

    Added WP-specific entries

---
+++ layers/WPSchema/packages/users/src/Constants/UserOrderBy.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Users\Constants;
+
+use PoPSchema\Users\Constants\UserOrderBy as UpstreamUserOrderBy;
+
+class UserOrderBy extends UpstreamUserOrderBy
+{
+    public const USER_URL = 'USER_URL';
+}
---
+++ layers/WPSchema/packages/users/src/Overrides/TypeResolvers/EnumType/UserOrderByEnumTypeResolver.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Users\Overrides\TypeResolvers\EnumType;
+
+use PoPSchema\Users\TypeResolvers\EnumType\UserOrderByEnumTypeResolver as UpstreamUserOrderByEnumTypeResolver;
+use PoPWPSchema\Users\Constants\UserOrderBy;
+
+/**
+ * The "order by" parameters are defined here:
+ *
+ * @see https://developer.wordpress.org/reference/classes/wp_query/#order-orderby-parameters
+ */
+class UserOrderByEnumTypeResolver extends UpstreamUserOrderByEnumTypeResolver
+{
+    /**
+     * @return string[]
+     */
+    public function getEnumValues(): array
+    {
+        return array_merge(
+            parent::getEnumValues(),
+            [
+                UserOrderBy::USER_URL,
+            ]
+        );
+    }
+
+    public function getEnumValueDescription(string $enumValue): ?string
+    {
+        return match ($enumValue) {
+            UserOrderBy::USER_URL => $this->getTranslationAPI()->__('Order by user URL', 'users'),
+            default => parent::getEnumValueDescription($enumValue),
+        };
+    }
+}
