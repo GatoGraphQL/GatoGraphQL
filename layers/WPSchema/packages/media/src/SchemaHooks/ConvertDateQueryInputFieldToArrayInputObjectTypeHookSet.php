2b269e384cd8afaeddbfe600d04305fd5d0bab2c
Leonardo Losoviz <leo@getpop.org>
2021-11-26 15:27:03 +0800

    Convert dateQuery to array for Media

---
+++ layers/WPSchema/packages/media/src/SchemaHooks/ConvertDateQueryInputFieldToArrayInputObjectTypeHookSet.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Media\SchemaHooks;
+
+use PoP\ComponentModel\TypeResolvers\InputObjectType\InputObjectTypeResolverInterface;
+use PoPSchema\Media\TypeResolvers\InputObjectType\AbstractMediaItemsFilterInputObjectTypeResolver;
+use PoPWPSchema\SchemaCommons\SchemaHooks\AbstractConvertDateQueryInputFieldToArrayInputObjectTypeHookSet;
+
+class ConvertDateQueryInputFieldToArrayInputObjectTypeHookSet extends AbstractConvertDateQueryInputFieldToArrayInputObjectTypeHookSet
+{
+    protected function isInputObjectTypeResolver(InputObjectTypeResolverInterface $inputObjectTypeResolver): bool
+    {
+        return $inputObjectTypeResolver instanceof AbstractMediaItemsFilterInputObjectTypeResolver;
+    }
+}
