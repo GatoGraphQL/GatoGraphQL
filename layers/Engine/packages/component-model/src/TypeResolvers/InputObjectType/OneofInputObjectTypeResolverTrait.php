a5e306638a62500c2cf0f405d63d343d9c5fef5a
Leonardo Losoviz <leo@getpop.org>
2021-11-26 12:03:02 +0800

    Added isOneInputValueMandatory

--- layers/Engine/packages/component-model/src/TypeResolvers/InputObjectType/OneofInputObjectTypeResolverTrait.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/InputObjectType/OneofInputObjectTypeResolverTrait.php
@@ -15,27 +15,48 @@ trait OneofInputObjectTypeResolverTrait
     abstract public function getMaybeNamespacedTypeName(): string;
 
     /**
+     * The oneof input can be used for different uses, such as:
+     *
+     *   1. Get a specific user: `{ user(by:{id: 1}) { name } }`
+     *   2. Search users: `{ users(searchBy:{name: "leo"}) { id } }`
+     *
+     * In the first case, the input is mandatory
+     * In the second case, it is not
+     *
+     * Because InputObjects with no value in the query are initialized as {} (via `new stdClass`),
+     * then we must explicitly check if the oneof input requires the one value or not.
+     */
+    protected function isOneInputValueMandatory(): bool
+    {
+        return true;
+    }
+
+    /**
      * Validate that there is exactly one input set
      */
     protected function validateOneofInputObjectValue(stdClass $inputValue): ?Error
     {
         $inputValueSize = count((array)$inputValue);
-        if ($inputValueSize !== 1) {
+        if ($inputValueSize > 1) {
             return new Error(
                 $this->getErrorCode(),
                 sprintf(
-                    $this->getTranslationAPI()->__('The oneof input object \'%s\' must receive exactly 1 input, but %s', 'component-model'),
+                    $this->getTranslationAPI()->__('The oneof input object \'%s\' must receive exactly 1 input, but \'%s\' inputs were provided (\'%s\')', 'component-model'),
                     $this->getMaybeNamespacedTypeName(),
-                    $inputValueSize === 0 ?
-                        $this->getTranslationAPI()->__('no input was provided', 'component-model')
-                        : sprintf(
-                            $this->getTranslationAPI()->__('\'%s\' inputs were provided (\'%s\')', 'component-model'),
-                            $inputValueSize,
-                            implode(
-                                $this->getTranslationAPI()->__('\', \'', 'component-model'),
-                                array_keys((array)$inputValue)
-                            )
-                        )
+                    $inputValueSize,
+                    implode(
+                        $this->getTranslationAPI()->__('\', \'', 'component-model'),
+                        array_keys((array)$inputValue)
+                    )
+                )
+            );
+        }
+        if ($inputValueSize === 0 && $this->isOneInputValueMandatory()) {
+            return new Error(
+                $this->getErrorCode(),
+                sprintf(
+                    $this->getTranslationAPI()->__('No input value was provided to the oneof input object \'%s\'', 'component-model'),
+                    $this->getMaybeNamespacedTypeName()
                 )
             );
         }
