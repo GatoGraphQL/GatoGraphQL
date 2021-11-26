47e7ff59eeb6994308159765c87d68c5839e3099
Leonardo Losoviz <leo@getpop.org>
2021-11-26 14:21:35 +0800

    Added "admin" enum values

--- layers/Engine/packages/component-model/src/TypeResolvers/EnumType/HookNames.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/EnumType/HookNames.php
@@ -7,6 +7,7 @@ namespace PoP\ComponentModel\TypeResolvers\EnumType;
 class HookNames
 {
     public const ENUM_VALUES = __CLASS__ . ':enum-values';
+    public const ADMIN_ENUM_VALUES = __CLASS__ . ':admin-enum-values';
     public const ENUM_VALUE_DESCRIPTION = __CLASS__ . ':enum-value-description';
     public const ENUM_VALUE_DEPRECATION_MESSAGE = __CLASS__ . ':enum-value-deprecation-message';
 }
