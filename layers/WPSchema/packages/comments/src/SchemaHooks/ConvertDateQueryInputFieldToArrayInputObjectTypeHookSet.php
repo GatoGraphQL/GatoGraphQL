91c57ac45490d9c018e6e4afb560782e6c351664
Leonardo Losoviz <leo@getpop.org>
2021-11-26 15:29:34 +0800

    Convert dateQuery to array for Comments

---
+++ layers/WPSchema/packages/comments/src/SchemaHooks/ConvertDateQueryInputFieldToArrayInputObjectTypeHookSet.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Comments\SchemaHooks;
+
+use PoP\ComponentModel\TypeResolvers\InputObjectType\InputObjectTypeResolverInterface;
+use PoPSchema\Comments\TypeResolvers\InputObjectType\AbstractCommentsFilterInputObjectTypeResolver;
+use PoPWPSchema\SchemaCommons\SchemaHooks\AbstractConvertDateQueryInputFieldToArrayInputObjectTypeHookSet;
+
+class ConvertDateQueryInputFieldToArrayInputObjectTypeHookSet extends AbstractConvertDateQueryInputFieldToArrayInputObjectTypeHookSet
+{
+    protected function isInputObjectTypeResolver(InputObjectTypeResolverInterface $inputObjectTypeResolver): bool
+    {
+        return $inputObjectTypeResolver instanceof AbstractCommentsFilterInputObjectTypeResolver;
+    }
+}
