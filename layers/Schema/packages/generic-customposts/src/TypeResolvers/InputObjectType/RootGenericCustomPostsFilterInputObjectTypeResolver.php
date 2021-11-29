803e0a2b69f4cd1e393425ae3ce2de3882b8d8e1
Leonardo Losoviz <leo@getpop.org>
2021-11-29 14:24:29 +0800

    Fixed inputs to "genericCustomPosts"

--- layers/Schema/packages/generic-customposts/src/TypeResolvers/InputObjectType/RootGenericCustomPostsFilterInputObjectTypeResolver.php
+++ layers/Schema/packages/generic-customposts/src/TypeResolvers/InputObjectType/RootGenericCustomPostsFilterInputObjectTypeResolver.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 namespace PoPSchema\GenericCustomPosts\TypeResolvers\InputObjectType;
 
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoPSchema\CustomPosts\TypeResolvers\InputObjectType\AbstractCustomPostsFilterInputObjectTypeResolver;
+use PoPSchema\GenericCustomPosts\FilterInputProcessors\FilterInputProcessor;
+use PoPSchema\GenericCustomPosts\TypeResolvers\EnumType\GenericCustomPostEnumTypeResolver;
 
 class RootGenericCustomPostsFilterInputObjectTypeResolver extends AbstractCustomPostsFilterInputObjectTypeResolver
 {
+    private ?GenericCustomPostEnumTypeResolver $customPostEnumTypeResolver = null;
+    
+    final public function setGenericCustomPostEnumTypeResolver(GenericCustomPostEnumTypeResolver $customPostEnumTypeResolver): void
+    {
+        $this->customPostEnumTypeResolver = $customPostEnumTypeResolver;
+    }
+    final protected function getGenericCustomPostEnumTypeResolver(): GenericCustomPostEnumTypeResolver
+    {
+        return $this->customPostEnumTypeResolver ??= $this->instanceManager->getInstance(GenericCustomPostEnumTypeResolver::class);
+    }
+
     public function getTypeName(): string
     {
         return 'RootGenericCustomPostsFilterInput';
@@ -15,11 +29,48 @@ class RootGenericCustomPostsFilterInputObjectTypeResolver extends AbstractCustom
 
     public function getTypeDescription(): ?string
     {
-        return $this->getTranslationAPI()->__('Input to filter custom posts', 'customposts');
+        return $this->getTranslationAPI()->__('Input to filter generic custom posts', 'genericcustomposts');
     }
 
-    protected function addCustomPostInputFields(): bool
+    public function getInputFieldNameTypeResolvers(): array
     {
-        return true;
+        return array_merge(
+            parent::getInputFieldNameTypeResolvers(),
+            [
+                'customPostTypes' => $this->getGenericCustomPostEnumTypeResolver(),
+            ]
+        );
+    }
+
+    public function getInputFieldDescription(string $inputFieldName): ?string
+    {
+        return match ($inputFieldName) {
+            'customPostTypes' => $this->getTranslationAPI()->__('Filter generic custom posts of given types', 'genericcustomposts'),
+            default => parent::getInputFieldDescription($inputFieldName),
+        };
+    }
+
+    public function getInputFieldDefaultValue(string $inputFieldName): mixed
+    {
+        return match ($inputFieldName) {
+            'customPostTypes' => $this->getGenericCustomPostEnumTypeResolver()->getConsolidatedEnumValues(),
+            default => parent::getInputFieldDefaultValue($inputFieldName)
+        };
+    }
+
+    public function getInputFieldTypeModifiers(string $inputFieldName): int
+    {
+        return match ($inputFieldName) {
+            'customPostTypes' => SchemaTypeModifiers::IS_ARRAY | SchemaTypeModifiers::IS_NON_NULLABLE_ITEMS_IN_ARRAY,
+            default => parent::getInputFieldTypeModifiers($inputFieldName)
+        };
+    }
+
+    public function getInputFieldFilterInput(string $inputFieldName): ?array
+    {
+        return match ($inputFieldName) {
+            'customPostTypes' => [FilterInputProcessor::class, FilterInputProcessor::FILTERINPUT_GENERICCUSTOMPOSTTYPES],
+            default => parent::getInputFieldFilterInput($inputFieldName),
+        };
     }
 }
