087b76d0ee3919c715ee36ceb3c57a66f5610114
Leonardo Losoviz <leo@getpop.org>
2021-12-02 17:35:27 +0800

    Make sure there's at least 1 result in MenuLocationEnum [skip ci]

--- layers/WPSchema/packages/menus/src/TypeResolvers/EnumType/MenuLocationEnumTypeResolver.php
+++ layers/WPSchema/packages/menus/src/TypeResolvers/EnumType/MenuLocationEnumTypeResolver.php
@@ -23,6 +23,11 @@ class MenuLocationEnumTypeResolver extends AbstractEnumTypeResolver
      */
     public function getEnumValues(): array
     {
-        return array_keys(\get_registered_nav_menus());
+        // Make sure there's at least 1 result, to avoid GraphQL throwing
+        // errors from an empty Enum
+        if ($enumValues = array_keys(\get_registered_nav_menus())) {
+            return $enumValues;
+        }
+        return ['empty'];
     }
 }
