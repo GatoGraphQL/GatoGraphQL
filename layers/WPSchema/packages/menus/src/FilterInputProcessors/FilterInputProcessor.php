01e95db9ad60e8aaeca8e56eb8efc7a884cb76eb
Leonardo Losoviz <leo@getpop.org>
2021-12-02 20:00:41 +0800

    Moved logic to MenuTypeAPI

--- layers/WPSchema/packages/menus/src/FilterInputProcessors/FilterInputProcessor.php
+++ layers/WPSchema/packages/menus/src/FilterInputProcessors/FilterInputProcessor.php
@@ -21,26 +21,7 @@ class FilterInputProcessor extends AbstractFilterInputProcessor
     {
         switch ($filterInput[1]) {
             case self::FILTERINPUT_LOCATIONS:
-                if (empty($value)) {
-                    return;
-                }
-                // Include all menu IDs using the given locations
-                $locations = \get_nav_menu_locations();
-                $menuIDs = [];
-                foreach ($value as $location) {
-                    $menuID = $locations[$location] ?? null;
-                    // If the location doesn't have a menu assigned, it will be assigned "0"
-                    if ($menuID === null || (int) $menuID === 0) {
-                        continue;
-                    }
-                    $menuIDs[] = $menuID;
-                }
-                if ($menuIDs !== []) {
-                    $query['include'] = implode(',', $menuIDs);
-                    return;
-                }
-                // No menu was found with the location, then return nothing
-                $query['include'] = -1;
+                $query['locations'] = $value;
                 break;
         }
     }
---
+++ layers/WPSchema/packages/menus/src/Hooks/QueryHookSet.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Menus\Hooks;
+
+use PoP\Hooks\AbstractHookSet;
+use PoPSchema\MenusWP\TypeAPIs\MenuTypeAPI;
+
+class QueryHookSet extends AbstractHookSet
+{
+    protected function init(): void
+    {
+        $this->getHooksAPI()->addFilter(
+            MenuTypeAPI::HOOK_QUERY,
+            [$this, 'convertMenuQuery'],
+            10,
+            2
+        );
+    }
+
+    public function convertMenuQuery(array $query, array $options): array
+    {
+        if (isset($query['locations'])) {
+            if ($query['locations'] !== []) {
+                // Include all menu IDs using the given locations
+                $locationMenuIDs = \get_nav_menu_locations();
+                $menuIDs = [];
+                foreach ($query['locations'] as $location) {
+                    $menuID = $locationMenuIDs[$location] ?? null;
+                    // If the location doesn't have a menu assigned, it will be assigned "0"
+                    if ($menuID === null || (int) $menuID === 0) {
+                        continue;
+                    }
+                    $menuIDs[] = $menuID;
+                }
+                if ($menuIDs === []) {
+                    // If no menu was found with the location, then return no results
+                    // via ID '-1', which does not exist
+                    $query['include'] = -1;
+                } elseif ($query['include']) {
+                    // If other IDs had been requested already, do the intersection among them
+                    $intersectedMenuIDs = array_intersect(
+                        explode(',', $query['include']),
+                        $menuIDs
+                    );
+                    $query['include'] = $intersectedMenuIDs === [] ? -1 : implode(',', $intersectedMenuIDs);
+                } else {
+                    // Limit to the found IDs
+                    $query['include'] = implode(',', $menuIDs);
+                }
+            }
+            unset($query['locations']);
+        }
+        return $query;
+    }
+}
