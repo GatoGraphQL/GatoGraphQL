127de45ef6e9b26580533c61fcd1d51627b51611
Leonardo Losoviz <leo@getpop.org>
2021-12-06 16:41:41 +0800

    Added "specifiedBy" for URL [skip ci]

--- layers/Schema/packages/schema-commons/src/TypeResolvers/ScalarType/URLScalarTypeResolver.php
+++ layers/Schema/packages/schema-commons/src/TypeResolvers/ScalarType/URLScalarTypeResolver.php
@@ -24,6 +24,11 @@ class URLScalarTypeResolver extends AbstractScalarTypeResolver
         return $this->getTranslationAPI()->__('URL scalar, such as https://mysite.com/my-fabulous-page', 'component-model');
     }
 
+    public function getSpecifiedByURL(): ?string
+    {
+        return 'https://datatracker.ietf.org/doc/rfc1738/';
+    }
+
     public function coerceValue(string|int|float|bool|stdClass $inputValue): string|int|float|bool|object
     {
         if ($error = $this->validateIsString($inputValue)) {
