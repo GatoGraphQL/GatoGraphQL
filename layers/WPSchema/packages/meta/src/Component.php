d12027da66709ece1bf72570b39461d37bf52ae5
Leonardo Losoviz <leo@getpop.org>
2021-12-08 17:06:05 +0800

    Added meta package

---
+++ layers/WPSchema/packages/meta/src/Component.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Meta;
+
+use PoP\Root\Component\AbstractComponent;
+
+/**
+ * Initialize component
+ */
+class Component extends AbstractComponent
+{
+    /**
+     * Classes from PoP components that must be initialized before this component
+     *
+     * @return string[]
+     */
+    public static function getDependedComponentClasses(): array
+    {
+        return [
+            \PoPSchema\Meta\Component::class,
+        ];
+    }
+}
