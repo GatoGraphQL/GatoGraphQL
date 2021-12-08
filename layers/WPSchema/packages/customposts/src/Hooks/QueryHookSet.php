0a539c85c82573b1d5641242376f02dcaf9b34b8
Leonardo Losoviz <leo@getpop.org>
2021-12-08 14:24:47 +0800

    Added "password" filter input for custom posts

--- layers/WPSchema/packages/customposts/src/Hooks/QueryHookSet.php
+++ layers/WPSchema/packages/customposts/src/Hooks/QueryHookSet.php
@@ -24,12 +24,19 @@ class QueryHookSet extends AbstractHookSet
         );
     }
 
+    /**
+     * @see https://developer.wordpress.org/reference/classes/wp_query/#password-parameters
+     */
     public function convertCustomPostsQuery($query, array $options): array
     {
         // `null` is an accepted value
         if (array_key_exists('has-password', $query)) {
             $query['has_password'] = $query['has-password'];
             unset($query['has-password']);
+        }
+        if (isset($query['password'])) {
+            $query['post_password'] = $query['password'];
+            unset($query['password']);
         }
         return $query;
     }
