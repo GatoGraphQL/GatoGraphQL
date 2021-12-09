6b2f786906a614b36653e01f2264d47343a9a507
Leonardo Losoviz <leo@getpop.org>
2021-12-09 17:46:37 +0800

    Fixed comment_meta_value

--- layers/WPSchema/packages/commentmeta/src/Hooks/CommentMetaOrderByQueryHookSet.php
+++ layers/WPSchema/packages/commentmeta/src/Hooks/CommentMetaOrderByQueryHookSet.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PoPWPSchema\CommentMeta\Hooks;
 
 use PoPSchema\CommentsWP\TypeAPIs\CommentTypeAPI;
+use PoPWPSchema\Meta\Constants\MetaOrderBy;
 use PoPWPSchema\Meta\Hooks\AbstractMetaOrderByQueryHookSet;
 
 class CommentMetaOrderByQueryHookSet extends AbstractMetaOrderByQueryHookSet
@@ -12,5 +13,13 @@ class CommentMetaOrderByQueryHookSet extends AbstractMetaOrderByQueryHookSet
     protected function getHookName(): string
     {
         return CommentTypeAPI::HOOK_ORDERBY_QUERY_ARG_VALUE;
+    }
+
+    public function getOrderByQueryArgValue(string $orderBy): string
+    {
+        return match ($orderBy) {
+            MetaOrderBy::META_VALUE => 'comment_meta_value',
+            default => parent::getOrderByQueryArgValue($orderBy),
+        };
     }
 }
