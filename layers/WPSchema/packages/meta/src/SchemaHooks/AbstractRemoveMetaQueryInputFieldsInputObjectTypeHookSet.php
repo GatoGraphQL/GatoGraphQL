af4867132966ee0b3012827b3457ff30d743c8de
Leonardo Losoviz <leo@getpop.org>
2021-12-09 12:46:36 +0800

    Remove "metaQuery" from CPTs

---
+++ layers/WPSchema/packages/meta/src/SchemaHooks/AbstractRemoveMetaQueryInputFieldsInputObjectTypeHookSet.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Meta\SchemaHooks;
+
+use PoP\ComponentModel\TypeResolvers\InputObjectType\HookNames;
+use PoP\ComponentModel\TypeResolvers\InputObjectType\InputObjectTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
+use PoP\Hooks\AbstractHookSet;
+
+abstract class AbstractRemoveMetaQueryInputFieldsInputObjectTypeHookSet extends AbstractHookSet
+{
+    protected function init(): void
+    {
+        $this->getHooksAPI()->addFilter(
+            HookNames::INPUT_FIELD_NAME_TYPE_RESOLVERS,
+            [$this, 'getInputFieldNameTypeResolvers'],
+            100,
+            2
+        );
+    }
+
+    /**
+     * Indicate if to remove the fields added by the SchemaHookSet
+     */
+    abstract protected function removeMetaQueryInputFields(
+        InputObjectTypeResolverInterface $inputObjectTypeResolver,
+    ): bool;
+
+    /**
+     * Remove the fields added by the SchemaHookSet
+     *
+     * @param array<string, InputTypeResolverInterface> $inputFieldNameTypeResolvers
+     */
+    public function getInputFieldNameTypeResolvers(
+        array $inputFieldNameTypeResolvers,
+        InputObjectTypeResolverInterface $inputObjectTypeResolver,
+    ): array {
+        if (!$this->removeMetaQueryInputFields($inputObjectTypeResolver)) {
+            return $inputFieldNameTypeResolvers;
+        }
+        unset($inputFieldNameTypeResolvers['metaQuery']);
+        return $inputFieldNameTypeResolvers;
+    }
+}
