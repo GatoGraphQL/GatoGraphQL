ff53dbd3448cc743d6a0ac95b9ae21ff721a3c28
Leonardo Losoviz <leo@getpop.org>
2021-12-13 21:24:20 +0800

    Don't override service

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Clients/CustomEndpointGraphiQLClient.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Clients/CustomEndpointGraphiQLClient.php
@@ -6,7 +6,6 @@ namespace GraphQLAPI\GraphQLAPI\Services\Clients;
 
 use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\GraphQLCustomEndpointCustomPostType;
 use GraphQLByPoP\GraphQLClientsForWP\Clients\GraphiQLClient;
-use PoP\ComponentModel\HelperServices\RequestHelperServiceInterface;
 use PoP\ComponentModel\Services\BasicServiceTrait;
 
 class CustomEndpointGraphiQLClient extends GraphiQLClient
@@ -15,7 +14,6 @@ class CustomEndpointGraphiQLClient extends GraphiQLClient
     use BasicServiceTrait;
 
     private ?GraphQLCustomEndpointCustomPostType $graphQLCustomEndpointCustomPostType = null;
-    private ?RequestHelperServiceInterface $requestHelperService = null;
 
     final public function setGraphQLCustomEndpointCustomPostType(GraphQLCustomEndpointCustomPostType $graphQLCustomEndpointCustomPostType): void
     {
@@ -24,13 +22,5 @@ class CustomEndpointGraphiQLClient extends GraphiQLClient
     final protected function getGraphQLCustomEndpointCustomPostType(): GraphQLCustomEndpointCustomPostType
     {
         return $this->graphQLCustomEndpointCustomPostType ??= $this->instanceManager->getInstance(GraphQLCustomEndpointCustomPostType::class);
-    }
-    final public function setRequestHelperService(RequestHelperServiceInterface $requestHelperService): void
-    {
-        $this->requestHelperService = $requestHelperService;
-    }
-    final protected function getRequestHelperService(): RequestHelperServiceInterface
-    {
-        return $this->requestHelperService ??= $this->instanceManager->getInstance(RequestHelperServiceInterface::class);
     }
 }
