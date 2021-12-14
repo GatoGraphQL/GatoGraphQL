76f50c09afedfb5f29f05a0b6021d93d8e0e9be7
Leonardo Losoviz <leo@getpop.org>
2021-12-14 09:55:01 +0800

    Show message "Safly already exists" [skip ci]

--- webservers/graphql-api-for-wp/setup/install-safly-curl-patch.sh
+++ webservers/graphql-api-for-wp/setup/install-safly-curl-patch.sh
@@ -2,14 +2,17 @@
 #Fix for https://github.com/lando/lando/issues/2913
 FILE=/tmp/safly-curl-patch.1.0.0.zip
 if [ ! -f "$FILE" ]; then
-    echo "Downloading and installing SaFly Curl Patch"
+    echo "Downloading SaFly Curl Patch"
     cd /tmp
     curl -O https://downloads.wordpress.org/plugin/safly-curl-patch.1.0.0.zip
     unzip safly-curl-patch.1.0.0.zip
+else
+    echo "SaFly Curl Patch already exists"
 fi
 
 DIR=/app/wordpress/wp-content/plugins/safly-curl-patch
 if [ ! -d "$DIR" ]; then
+    echo "Installing SaFly Curl Patch in WordPress"
     mv safly-curl-patch /app/wordpress/wp-content/plugins/
     wp plugin activate safly-curl-patch --path=/app/wordpress
 fi
