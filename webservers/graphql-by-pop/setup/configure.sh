9b3faf839231a79e92def96ae07a29f6397867f8
Leonardo Losoviz <leo@getpop.org>
2021-12-14 09:52:54 +0800

    Copy phpinfo to Lando webserver [skip ci]

--- webservers/graphql-by-pop/setup/configure.sh
+++ webservers/graphql-by-pop/setup/configure.sh
@@ -2,3 +2,4 @@
 wp config set WP_DEBUG true --raw --path=/app/wordpress
 wp rewrite structure '/%postname%/' --hard --path=/app/wordpress
 cp /app/assets/.htaccess /app/wordpress
+cp /app/assets/phpinfo.php /app/wordpress
