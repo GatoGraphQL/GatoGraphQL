b81d63e05009643fd219f99c467012ba471b0a1a
Leonardo Losoviz <leo@getpop.org>
2021-12-16 01:12:46 +0800

    Sort lines

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,18 +2,18 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->in([
-        __DIR__ . '/layers/WPSchema/packages/*/src',
+        __DIR__ . '/layers/API/packages/*/src',
+        __DIR__ . '/layers/Engine/packages/*/src',
+        __DIR__ . '/layers/GraphQLAPIForWP/packages/*/src',
+        __DIR__ . '/layers/GraphQLAPIForWP/plugins/*/src',
+        __DIR__ . '/layers/GraphQLByPoP/packages/*/src',
+        __DIR__ . '/layers/Legacy/Engine/packages/*/src',
+        __DIR__ . '/layers/Legacy/Schema/packages/*/src',
+        __DIR__ . '/layers/Legacy/Wassup/packages/*/src',
+        __DIR__ . '/layers/Schema/packages/*/src',
         __DIR__ . '/layers/SiteBuilder/packages/*/src',
         __DIR__ . '/layers/Wassup/packages/*/src',
-        __DIR__ . '/layers/Legacy/Wassup/packages/*/src',
-        __DIR__ . '/layers/Legacy/Schema/packages/*/src',
-        __DIR__ . '/layers/Legacy/Engine/packages/*/src',
-        __DIR__ . '/layers/Schema/packages/*/src',
-        __DIR__ . '/layers/API/packages/*/src',
-        __DIR__ . '/layers/GraphQLByPoP/packages/*/src',
-        __DIR__ . '/layers/GraphQLAPIForWP/plugins/*/src',
-        __DIR__ . '/layers/GraphQLAPIForWP/packages/*/src',
-        __DIR__ . '/layers/Engine/packages/*/src',
+        __DIR__ . '/layers/WPSchema/packages/*/src',
     ])
 ;
 
