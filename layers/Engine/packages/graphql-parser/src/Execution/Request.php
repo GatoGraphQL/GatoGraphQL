cfb8b5aa196de7c07636ad446f497a520b15a970
Leonardo Losoviz <leo@getpop.org>
2021-12-17 14:49:25 +0800

    Use WithTranslationAPITrait

--- layers/Engine/packages/graphql-parser/src/Execution/Request.php
+++ layers/Engine/packages/graphql-parser/src/Execution/Request.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace PoP\GraphQLParser\Execution;
 
+use PoP\Translation\Services\WithTranslationAPITrait;
 use PoPBackbone\GraphQLParser\Execution\Request as UpstreamRequest;
 
 class Request extends UpstreamRequest
 {
-
+    use WithTranslationAPITrait;
 }
