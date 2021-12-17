1fe3fbcd2ef8f11b202733977738f636e9607476
Leonardo Losoviz <leo@getpop.org>
2021-12-17 15:45:53 +0800

    Fixed dep to BasicService

--- layers/Engine/packages/field-query/src/Component.php
+++ layers/Engine/packages/field-query/src/Component.php
@@ -19,7 +19,6 @@ class Component extends AbstractComponent
     public static function getDependedComponentClasses(): array
     {
         return [
-            \PoP\BasicService\Component::class,
             \PoP\QueryParsing\Component::class,
         ];
     }
