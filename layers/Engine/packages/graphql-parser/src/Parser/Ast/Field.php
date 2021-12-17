54d15bd4d11e22f7e72bc5df9bb19a5312124098
Leonardo Losoviz <leo@getpop.org>
2021-12-17 10:21:00 +0800

    Extended Field

---
+++ layers/Engine/packages/graphql-parser/src/Parser/Ast/Field.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\GraphQLParser\Parser\Ast;
+
+use PoPBackbone\GraphQLParser\Parser\Ast\Field as UpstreamField;
+
+class Field extends UpstreamField
+{
+}
