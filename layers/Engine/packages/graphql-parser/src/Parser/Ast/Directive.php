4f2c4095cd2b4da433333ecf837a3870c825d418
Leonardo Losoviz <leo@getpop.org>
2021-12-17 11:16:21 +0800

    Made Location nullable, under Extensions

--- layers/Engine/packages/graphql-parser/src/Parser/Ast/Directive.php
+++ layers/Engine/packages/graphql-parser/src/Parser/Ast/Directive.php
@@ -5,7 +5,21 @@ declare(strict_types=1);
 namespace PoP\GraphQLParser\Parser\Ast;
 
 use PoPBackbone\GraphQLParser\Parser\Ast\Directive as UpstreamDirective;
+use PoPBackbone\GraphQLParser\Parser\Location;
 
 class Directive extends UpstreamDirective
 {
+    /**
+     * For the SiteBuilder, the generated GraphQL query will have no Location
+     *
+     * @param Argument[] $arguments
+     */
+    public function __construct(
+        private string $name,
+        array $arguments,
+        ?Location $location = null,
+    ) {
+        $location ??= new Location(0, 0);
+        parent::__construct($name, $arguments, $location);
+    }
 }
--- layers/Engine/packages/graphql-parser/src/Parser/Ast/Field.php
+++ layers/Engine/packages/graphql-parser/src/Parser/Ast/Field.php
@@ -4,8 +4,27 @@ declare(strict_types=1);
 
 namespace PoP\GraphQLParser\Parser\Ast;
 
+use PoPBackbone\GraphQLParser\Parser\Ast\Argument;
+use PoPBackbone\GraphQLParser\Parser\Ast\Directive;
 use PoPBackbone\GraphQLParser\Parser\Ast\Field as UpstreamField;
+use PoPBackbone\GraphQLParser\Parser\Location;
 
 class Field extends UpstreamField
 {
+    /**
+     * For the SiteBuilder, the generated GraphQL query will have no Location
+     *
+     * @param Argument[] $arguments
+     * @param Directive[] $directives
+     */
+    public function __construct(
+        private string $name,
+        private ?string $alias,
+        array $arguments,
+        array $directives,
+        ?Location $location = null,
+    ) {
+        $location ??= new Location(0, 0);
+        parent::__construct($name, $alias, $arguments, $directives, $location);
+    }
 }
