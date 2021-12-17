2e2313370784d3e41e955a2d3d79d3c486dfee1c
Leonardo Losoviz <leo@getpop.org>
2021-12-17 17:52:01 +0800

    Override error message

--- layers/Backbone/packages/graphql-parser/src/Validator/RequestValidator/RequestValidator.php
+++ layers/Backbone/packages/graphql-parser/src/Validator/RequestValidator/RequestValidator.php
@@ -25,35 +25,67 @@ class RequestValidator implements RequestValidatorInterface
 
         foreach ($request->getFragments() as $fragment) {
             if (!$fragment->isUsed()) {
-                throw new InvalidRequestException(sprintf('Fragment \'%s\' not used', $fragment->getName()), $fragment->getLocation());
+                throw new InvalidRequestException(
+                    $this->getFragmentNotUsedErrorMessage($fragment->getName()),
+                    $fragment->getLocation()
+                );
             }
         }
+    }
+
+    protected function getFragmentNotUsedErrorMessage(string $fragmentName): string
+    {
+        return sprintf('Fragment \'%s\' not used', $fragmentName);
     }
 
     private function assertFragmentReferencesValid(RequestInterface $request)
     {
         foreach ($request->getFragmentReferences() as $fragmentReference) {
             if (!$request->getFragment($fragmentReference->getName())) {
-                throw new InvalidRequestException(sprintf('Fragment \'%s\' not defined in query', $fragmentReference->getName()), $fragmentReference->getLocation());
+                throw new InvalidRequestException(
+                    $this->getFragmentNotDefinedInQueryErrorMessage($fragmentReference->getName()),
+                    $fragmentReference->getLocation()
+                );
             }
         }
+    }
+
+    protected function getFragmentNotDefinedInQueryErrorMessage(string $fragmentName): string
+    {
+        return sprintf('Fragment \'%s\' not defined in query', $fragmentName);
     }
 
     private function assertAllVariablesExists(RequestInterface $request)
     {
         foreach ($request->getVariableReferences() as $variableReference) {
             if (!$variableReference->getVariable()) {
-                throw new InvalidRequestException(sprintf('Variable \'%s\' not exists', $variableReference->getName()), $variableReference->getLocation());
+                throw new InvalidRequestException(
+                    $this->getVariableDoesNotExistErrorMessage($variableReference->getName()),
+                    $variableReference->getLocation()
+                );
             }
         }
+    }
+
+    protected function getVariableDoesNotExistErrorMessage(string $variableName): string
+    {
+        return sprintf('Variable \'%s\' does not exist', $variableName);
     }
 
     private function assertAllVariablesUsed(RequestInterface $request)
     {
         foreach ($request->getQueryVariables() as $queryVariable) {
             if (!$queryVariable->isUsed()) {
-                throw new InvalidRequestException(sprintf('Variable \'%s\' not used', $queryVariable->getName()), $queryVariable->getLocation());
+                throw new InvalidRequestException(
+                    $this->getVariableNotUsedErrorMessage($queryVariable->getName()),
+                    $queryVariable->getLocation()
+                );
             }
         }
+    }
+
+    protected function getVariableNotUsedErrorMessage(string $variableName): string
+    {
+        return sprintf('Variable \'%s\' not used', $variableName);
     }
 }
---
+++ layers/Engine/packages/graphql-parser/src/Validator/RequestValidator/RequestValidator.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\GraphQLParser\Validator\RequestValidator;
+
+use PoP\BasicService\BasicServiceTrait;
+use PoPBackbone\GraphQLParser\Validator\RequestValidator\RequestValidator as UpstreamRequestValidator;
+
+class RequestValidator extends UpstreamRequestValidator
+{
+    use BasicServiceTrait;
+    
+    protected function getFragmentNotUsedErrorMessage(string $fragmentName): string
+    {
+        return \sprintf(
+            $this->getTranslationAPI()->__('Fragment \'%s\' not used', 'graphql-parser'),
+            $fragmentName
+        );
+    }
+
+    protected function getFragmentNotDefinedInQueryErrorMessage(string $fragmentName): string
+    {
+        return \sprintf(
+            $this->getTranslationAPI()->__('Fragment \'%s\' not defined in query', 'graphql-parser'),
+            $fragmentName
+        );
+    }
+
+    protected function getVariableDoesNotExistErrorMessage(string $variableName): string
+    {
+        return \sprintf(
+            $this->getTranslationAPI()->__('Variable \'%s\' does not exist', 'graphql-parser'),
+            $variableName
+        );
+    }
+
+    protected function getVariableNotUsedErrorMessage(string $variableName): string
+    {
+        return \sprintf(
+            $this->getTranslationAPI()->__('Variable \'%s\' not used', 'graphql-parser'),
+            $variableName
+        );
+    }
+}
