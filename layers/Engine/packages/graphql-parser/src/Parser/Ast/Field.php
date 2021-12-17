576c458be919fa236a825f211762351476be4698
Leonardo Losoviz <leo@getpop.org>
2021-12-17 11:02:27 +0800

    Added `skipOutputIfNull`

--- layers/Engine/packages/graphql-parser/src/Parser/Ast/Field.php
+++ layers/Engine/packages/graphql-parser/src/Parser/Ast/Field.php
@@ -8,4 +8,11 @@ use PoPBackbone\GraphQLParser\Parser\Ast\Field as UpstreamField;
 
 class Field extends UpstreamField
 {
+    /**
+     * Custom extension, needed for PoP's SiteBuilder
+     */
+    public function skipOutputIfNull(): bool
+    {
+        return false;
+    }
 }
