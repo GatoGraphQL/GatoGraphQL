aeec7c4c6f998c290a68a3a214ce94bf4b42e984
Leonardo Losoviz <leo@getpop.org>
2021-12-22 09:52:11 +0800

    Add favicon to Lando webserver [skip ci]

--- webservers/graphql-by-pop/setup/configure.sh
+++ webservers/graphql-by-pop/setup/configure.sh
@@ -3,3 +3,4 @@ wp config set WP_DEBUG true --raw --path=/app/wordpress
 wp rewrite structure '/%postname%/' --hard --path=/app/wordpress
 cp /app/assets/.htaccess /app/wordpress
 cp /app/assets/phpinfo.php /app/wordpress
+cp /app/assets/favicon.ico /app/wordpress
