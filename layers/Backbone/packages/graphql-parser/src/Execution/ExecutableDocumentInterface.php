2a2b84932956355247486b81bcd9ed11da7f0808
Leonardo Losoviz <leo@getpop.org>
2021-12-23 17:13:17 +0800

    Added phpdoc

--- layers/Backbone/packages/graphql-parser/src/Execution/ExecutableDocumentInterface.php
+++ layers/Backbone/packages/graphql-parser/src/Execution/ExecutableDocumentInterface.php
@@ -9,6 +9,10 @@ use PoPBackbone\GraphQLParser\Exception\Parser\InvalidRequestException;
 interface ExecutableDocumentInterface
 {
     /**
+     * Integrate the variableValues into the included Operations in the Document.
+     * If some variable is missing, or other, throw an exception to signify
+     * that the validation fail.
+     *
      * @throws InvalidRequestException
      */
     public function validateAndMerge(): void;
