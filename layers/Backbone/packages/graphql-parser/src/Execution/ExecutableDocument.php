e1c6e411d9ce9b3da1ec61e84d2fc7d2bacaf393
Leonardo Losoviz <leo@getpop.org>
2021-12-23 16:57:26 +0800

    Created ExecutableDocument

---
+++ layers/Backbone/packages/graphql-parser/src/Execution/ExecutableDocument.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPBackbone\GraphQLParser\Execution;
+
+use PoPBackbone\GraphQLParser\Exception\Parser\InvalidRequestException;
+use PoPBackbone\GraphQLParser\Parser\Ast\Document;
+
+class ExecutableDocument implements ExecutableDocumentInterface
+{
+    public function __construct(
+        private Document $document,
+        private array $variableValues = [],
+    ) {
+    }
+
+    /**
+     * @throws InvalidRequestException
+     */
+    public function validate(): void
+    {
+
+    }
+}
---
+++ layers/Backbone/packages/graphql-parser/src/Execution/ExecutableDocumentInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPBackbone\GraphQLParser\Execution;
+
+use PoPBackbone\GraphQLParser\Exception\Parser\InvalidRequestException;
+
+interface ExecutableDocumentInterface
+{
+    /**
+     * @throws InvalidRequestException
+     */
+    public function validate(): void;
+}
