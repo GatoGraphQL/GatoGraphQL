19ae6515ffe370e0e9bf0150e56f31546eff8ecf
Leonardo Losoviz <leo@getpop.org>
2021-12-27 20:53:28 +0800

    Moved string overrides

--- layers/Engine/packages/graphql-parser/src/Execution/ExecutableDocument.php
+++ layers/Engine/packages/graphql-parser/src/Execution/ExecutableDocument.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace PoP\GraphQLParser\Execution;
 
+use PoP\BasicService\BasicServiceTrait;
 use PoP\GraphQLParser\Facades\Query\QueryAugmenterServiceFacade;
 use PoPBackbone\GraphQLParser\Execution\ExecutableDocument as UpstreamExecutableDocument;
 
 class ExecutableDocument extends UpstreamExecutableDocument
 {
+    use BasicServiceTrait;
+    
     /**
      * Override to support the "multiple query execution" feature:
      * If passing operation name `__ALL`, then execute all operations (hack)
@@ -23,5 +26,37 @@ class ExecutableDocument extends UpstreamExecutableDocument
         }
         
         return parent::getSelectedOperationsToExecute();
+    }
+
+    protected function getFragmentNotUsedErrorMessage(string $fragmentName): string
+    {
+        return \sprintf(
+            $this->getTranslationAPI()->__('Fragment \'%s\' not used', 'graphql-parser'),
+            $fragmentName
+        );
+    }
+
+    protected function getFragmentNotDefinedInQueryErrorMessage(string $fragmentName): string
+    {
+        return \sprintf(
+            $this->getTranslationAPI()->__('Fragment \'%s\' not defined in query', 'graphql-parser'),
+            $fragmentName
+        );
+    }
+
+    protected function getVariableDoesNotExistErrorMessage(string $variableName): string
+    {
+        return \sprintf(
+            $this->getTranslationAPI()->__('Variable \'%s\' does not exist', 'graphql-parser'),
+            $variableName
+        );
+    }
+
+    protected function getVariableNotUsedErrorMessage(string $variableName): string
+    {
+        return \sprintf(
+            $this->getTranslationAPI()->__('Variable \'%s\' not used', 'graphql-parser'),
+            $variableName
+        );
     }
 }
