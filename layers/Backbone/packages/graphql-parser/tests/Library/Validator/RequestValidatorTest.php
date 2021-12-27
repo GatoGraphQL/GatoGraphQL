9657fdaefe1e40d5b235995fe17c4ede1945e22c
Leonardo Losoviz <leo@getpop.org>
2021-12-27 20:48:23 +0800

    Fixed test

--- layers/Backbone/packages/graphql-parser/tests/Library/Validator/RequestValidatorTest.php
+++ layers/Backbone/packages/graphql-parser/tests/Library/Validator/RequestValidatorTest.php
@@ -8,13 +8,14 @@ use PHPUnit\Framework\TestCase;
 use PoPBackbone\GraphQLParser\Exception\Parser\InvalidRequestException;
 use PoPBackbone\GraphQLParser\Execution\Context;
 use PoPBackbone\GraphQLParser\Execution\ExecutableDocument;
-use PoPBackbone\GraphQLParser\Execution\Request;
 use PoPBackbone\GraphQLParser\Parser\Ast\Argument;
 use PoPBackbone\GraphQLParser\Parser\Ast\ArgumentValue\Variable;
 use PoPBackbone\GraphQLParser\Parser\Ast\ArgumentValue\VariableReference;
+use PoPBackbone\GraphQLParser\Parser\Ast\Document;
 use PoPBackbone\GraphQLParser\Parser\Ast\Field;
 use PoPBackbone\GraphQLParser\Parser\Ast\Fragment;
 use PoPBackbone\GraphQLParser\Parser\Ast\FragmentReference;
+use PoPBackbone\GraphQLParser\Parser\Ast\QueryOperation;
 use PoPBackbone\GraphQLParser\Parser\Ast\RelationalField;
 use PoPBackbone\GraphQLParser\Parser\Location;
 
@@ -31,7 +32,11 @@ class RequestValidatorTest extends TestCase
 
     public function invalidRequestProvider()
     {
-        $context = new Context();
+        $context = new Context(null, [
+            'test' => 'ponga',
+            'test2' => 'chonga',
+            'test3' => 'conga',
+        ]);
         $variable1 = new Variable('test', 'Int', false, false, true, new Location(1, 1));
         $variable1->setContext($context);
         $variable2 = new Variable('test2', 'Int', false, false, true, new Location(1, 1));
@@ -41,91 +46,84 @@ class RequestValidatorTest extends TestCase
 
         return [
             [
-                (new Request())->process([
-                    'queries'            => [
-                        new Field('test', null, [], [
-                            new FragmentReference('reference', new Location(1, 1))
-                        ], new Location(1, 1))
-                    ],
-                    'fragmentReferences' => [
-                        new FragmentReference('reference', new Location(1, 1))
-                    ]
-                ])
-            ],
-            [
-                (new Request())->process([
-                    'queries'            => [
-                        new Field('test', null, [], [
-                            new FragmentReference('reference', new Location(1, 1)),
-                            new FragmentReference('reference2', new Location(1, 1)),
-                        ], new Location(1, 1))
-                    ],
-                    'fragments'          => [
-                        new Fragment('reference', 'TestType', [], [], new Location(1, 1))
-                    ],
-                    'fragmentReferences' => [
-                        new FragmentReference('reference', new Location(1, 1)),
-                        new FragmentReference('reference2', new Location(1, 1))
-                    ]
-                ])
-            ],
-            [
-                (new Request())->process([
-                    'queries'            => [
-                        new Field('test', null, [], [
-                            new FragmentReference('reference', new Location(1, 1)),
-                        ], new Location(1, 1))
-                    ],
-                    'fragments'          => [
-                        new Fragment('reference', 'TestType', [], [], new Location(1, 1)),
-                        new Fragment('reference2', 'TestType', [], [], new Location(1, 1))
-                    ],
-                    'fragmentReferences' => [
-                        new FragmentReference('reference', new Location(1, 1))
-                    ]
-                ])
-            ],
-            [
-                (new Request())->process([
-                    'queries'            => [
-                        new RelationalField(
-                            'test',
-                            null,
+                (new ExecutableDocument(
+                    new Document([
+                        new QueryOperation('saranga', [], [], 
                             [
-                                new Argument('test', new VariableReference('test', null, new Location(1, 1)), new Location(1, 1))
-                            ],
-                            [
-                                new Field('test', null, [], [], new Location(1, 1))
-                            ],
-                            [],
-                            new Location(1, 1)
-                        )
-                    ],
-                    'variableReferences' => [
-                        new VariableReference('test', null, new Location(1, 1))
-                    ]
-                ], ['test' => 1])
+                                new RelationalField('test', null, [], [
+                                    new FragmentReference('reference', new Location(1, 1))
+                                ], [], new Location(1, 1)),
+                                new FragmentReference('reference', new Location(1, 1))
+                            ], new Location(1, 1)
+                        )], [])
+                    ))
             ],
             [
-                (new Request())->process([
-                    'queries'            => [
-                        new RelationalField('test', null, [
-                            new Argument('test', new VariableReference('test', $variable1, new Location(1, 1)), new Location(1, 1)),
-                            new Argument('test2', new VariableReference('test2', $variable2, new Location(1, 1)), new Location(1, 1)),
+                (new ExecutableDocument(
+                    new Document([
+                        new QueryOperation('saranga', [], [], 
+                            [
+                                new RelationalField('test', null, [], [
+                                    new FragmentReference('reference', new Location(1, 1)),
+                                    new FragmentReference('reference2', new Location(1, 1)),
+                                ], [], new Location(1, 1))
+                            ], new Location(1, 1))
                         ], [
-                            new Field('test', null, [], [], new Location(1, 1))
-                        ], [], new Location(1, 1))
-                    ],
-                    'variables'          => [
-                        $variable1,
-                        $variable2,
-                        $variable3
-                    ],
-                    'variableReferences' => [
-                        new VariableReference('test', $variable1, new Location(1, 1)),
-                        new VariableReference('test2', $variable2, new Location(1, 1))
-                    ]
-                ], ['test' => 1, 'test2' => 2])
+                        new Fragment('reference', 'TestType', [], [], new Location(1, 1))
+                    ]))
+                )
+            ],
+            [
+                (new ExecutableDocument(
+                    new Document([
+                        new QueryOperation('saranga', [], [], 
+                            [
+                                new RelationalField('test', null, [], [
+                                    new FragmentReference('reference', new Location(1, 1)),
+                                ], [], new Location(1, 1))
+                            ], new Location(1, 1))
+                        ], [
+                            new Fragment('reference', 'TestType', [], [], new Location(1, 1)),
+                            new Fragment('reference2', 'TestType', [], [], new Location(1, 1))
+                        ]
+                    ))
+                )
+            ],
+            [
+                (new ExecutableDocument(
+                    new Document([
+                        new QueryOperation('saranga', [], [], 
+                            [
+                                new RelationalField(
+                                    'test',
+                                    null,
+                                    [
+                                        new Argument('test', new VariableReference('test', null, new Location(1, 1)), new Location(1, 1))
+                                    ],
+                                    [
+                                        new Field('test', null, [], [], new Location(1, 1))
+                                    ],
+                                    [],
+                                    new Location(1, 1)
+                                )
+                            ], new Location(1, 1))
+                        ], []))
+                )
+            ],
+            [
+                (new ExecutableDocument(
+                    new Document([
+                        new QueryOperation('saranga', [], [], 
+                            [
+                                new RelationalField('test', null, [
+                                    new Argument('test', new VariableReference('test', $variable1, new Location(1, 1)), new Location(1, 1)),
+                                    new Argument('test2', new VariableReference('test2', $variable2, new Location(1, 1)), new Location(1, 1)),
+                                ], [
+                                    new Field('test', null, [], [], new Location(1, 1))
+                                ], [], new Location(1, 1))
+                            ], new Location(1, 1))
+                        ], []))
+                )
             ]
         ];
     }
