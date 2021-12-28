3f78777fc0099bcc5d1376b796cc44f006c63c5f
Leonardo Losoviz <leo@getpop.org>
2021-12-28 14:00:56 +0800

    Added test for fragment references

--- layers/Backbone/packages/graphql-parser/tests/Parser/DocumentTest.php
+++ layers/Backbone/packages/graphql-parser/tests/Parser/DocumentTest.php
@@ -9,6 +9,46 @@ use PoPBackbone\GraphQLParser\Exception\Parser\InvalidRequestException;
 
 class DocumentTest extends TestCase
 {
+    public function testValidationWorks()
+    {
+        $parser = new Parser();
+        
+        // Validate that there are no errors <= no Exception is thrown
+        $document = $parser->parse('
+            query StarWarsAppHomeRoute($names_0:[String!]!, $query: String) {
+              factions(names:$names_0, test: $query) {
+                id,
+                ...F2
+              }
+            }
+            fragment F1 on Faction {
+              id,
+              factionId
+            }
+            fragment F2 on Faction {
+              id,
+              factionId,
+              name,
+              _shipsDRnzJ:ships(first:10) {
+                edges {
+                  node {
+                    id,
+                    ...F0
+                  },
+                  cursor
+                },
+                pageInfo {
+                  hasNextPage,
+                  hasPreviousPage
+                }
+              },
+              ...F1
+            }
+        ');
+        $document->validate();
+        $this->assertTrue(true);
+    }
+
     public function testFragmentNotUsed()
     {
         $this->expectException(InvalidRequestException::class);
