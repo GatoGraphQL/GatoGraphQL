a354152744a5535f2c989284506cb8a30dcac3fc
Leonardo Losoviz <leo@getpop.org>
2021-12-28 15:14:50 +0800

    Added tests

--- layers/Backbone/packages/graphql-parser/tests/Execution/ExecutableDocumentTest.php
+++ layers/Backbone/packages/graphql-parser/tests/Execution/ExecutableDocumentTest.php
@@ -6,6 +6,12 @@ namespace PoPBackbone\GraphQLParser\Execution;
 
 use PHPUnit\Framework\TestCase;
 use PoPBackbone\GraphQLParser\Exception\Parser\InvalidRequestException;
+use PoPBackbone\GraphQLParser\Parser\Ast\Argument;
+use PoPBackbone\GraphQLParser\Parser\Ast\ArgumentValue\Literal;
+use PoPBackbone\GraphQLParser\Parser\Ast\LeafField;
+use PoPBackbone\GraphQLParser\Parser\Ast\QueryOperation;
+use PoPBackbone\GraphQLParser\Parser\Ast\RelationalField;
+use PoPBackbone\GraphQLParser\Parser\Location;
 use PoPBackbone\GraphQLParser\Parser\Parser;
 
 class ExecutableDocumentTest extends TestCase
@@ -13,7 +19,7 @@ class ExecutableDocumentTest extends TestCase
     public function testGetVariableFromContext()
     {
         $parser = new Parser();
-        
+
         // Validate that there are no errors <= no Exception is thrown
         $document = $parser->parse('
             query SomeQuery($includeUsers: Boolean) {
@@ -24,7 +30,7 @@ class ExecutableDocumentTest extends TestCase
             }
         ');
         $context = new Context(null, [
-          'includeUsers' => true,
+            'includeUsers' => true,
         ]);
         $executableDocument = new ExecutableDocument($document, $context);
         $executableDocument->validateAndInitialize();
@@ -33,7 +39,7 @@ class ExecutableDocumentTest extends TestCase
 
     public function testGetVariableDefaultValue()
     {
-        $parser = new Parser();        
+        $parser = new Parser();
         $document = $parser->parse('
             query SomeQuery($includeUsers: Boolean = true) {
               users {
@@ -50,7 +56,7 @@ class ExecutableDocumentTest extends TestCase
 
     public function testRequestDefinedOperation()
     {
-        $parser = new Parser();        
+        $parser = new Parser();
         $document = $parser->parse('
             query SomeQuery {
               users {
@@ -67,7 +73,7 @@ class ExecutableDocumentTest extends TestCase
 
     public function testRequestOneOfDefinedOperation()
     {
-        $parser = new Parser();        
+        $parser = new Parser();
         $document = $parser->parse('
             query SomeQuery {
               users {
@@ -92,7 +98,7 @@ class ExecutableDocumentTest extends TestCase
     public function testNonUniqueOperation()
     {
         $this->expectException(InvalidRequestException::class);
-        $parser = new Parser();        
+        $parser = new Parser();
         $document = $parser->parse('
             query SomeQuery {
               users {
@@ -117,7 +123,7 @@ class ExecutableDocumentTest extends TestCase
     public function testMissingVariableValue()
     {
         $this->expectException(InvalidRequestException::class);
-        $parser = new Parser();        
+        $parser = new Parser();
         $document = $parser->parse('
             query SomeQuery($format: String) {
               users {
@@ -163,5 +169,37 @@ class ExecutableDocumentTest extends TestCase
         $context = new Context('AnotherOp');
         $executableDocument = new ExecutableDocument($document, $context);
         $executableDocument->validateAndInitialize();
+    }
+
+    public function testNonInitializedRequest()
+    {
+        $this->expectException(InvalidRequestException::class);
+        $parser = new Parser();
+        $document = $parser->parse('{ id }');
+        $context = new Context();
+        $executableDocument = new ExecutableDocument($document, $context);
+        $executableDocument->getRequestedOperations();
+    }
+
+    public function testRequestedOperationsMatchOperations()
+    {
+        $parser = new Parser();
+        $document = $parser->parse('{ film(id: 1 filmID: 2) { title } }');
+        $context = new Context();
+        $executableDocument = new ExecutableDocument($document, $context);
+        $executableDocument->validateAndInitialize();
+        $this->assertEquals(
+            $executableDocument->getRequestedOperations(),
+            [
+                new QueryOperation('', [], [], [
+                    new RelationalField('film', null, [
+                        new Argument('id', new Literal(1, new Location(1, 12)), new Location(1, 8)),
+                        new Argument('filmID', new Literal(2, new Location(1, 22)), new Location(1, 14)),
+                    ], [
+                        new LeafField('title', null, [], [], new Location(1, 27)),
+                    ], [], new Location(1, 3))
+                ], new Location(1, 1)),
+            ]
+        );
     }
 }
