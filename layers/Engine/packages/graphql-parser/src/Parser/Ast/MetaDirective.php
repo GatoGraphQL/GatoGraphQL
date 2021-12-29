ffed06e6bc264a7be5f35dc0ebb5f3a2bf86cb2c
Leonardo Losoviz <leo@getpop.org>
2021-12-29 21:52:06 +0800

    Added MetaDirective

---
+++ layers/Engine/packages/graphql-parser/src/Parser/Ast/MetaDirective.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\GraphQLParser\Parser\Ast;
+
+use PoPBackbone\GraphQLParser\Parser\Ast\Argument;
+use PoPBackbone\GraphQLParser\Parser\Ast\WithDirectivesInterface;
+use PoPBackbone\GraphQLParser\Parser\Ast\WithDirectivesTrait;
+use PoPBackbone\GraphQLParser\Parser\Location;
+
+class MetaDirective extends Directive implements WithDirectivesInterface
+{
+    use WithDirectivesTrait;
+
+    /**
+     * @param Argument[] $arguments
+     */
+    public function __construct(
+        string $name,
+        array $arguments,
+        array $nestedDirectives,
+        ?Location $location = null,
+    ) {
+        parent::__construct(
+            $name,
+            $arguments,
+            $location,
+        );
+        $this->setDirectives($nestedDirectives);
+    }
+}
