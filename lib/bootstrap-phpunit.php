ee802f4a94f9cf8ae47a1cfc753b70df362cd375
Leonardo Losoviz <leo@getpop.org>
2022-01-03 15:33:30 +0800

    Don't initialize container here

--- lib/bootstrap-phpunit.php
+++ lib/bootstrap-phpunit.php
@@ -6,8 +6,4 @@ declare(strict_types=1);
  * @todo Load container services, otherwise can't use services in tests (eg: in GeneralUtilsTest.php)
  */
 
-use PoP\Root\Container\ContainerBuilderFactory;
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
-ContainerBuilderFactory::init();
