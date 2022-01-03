218e898a7a1fb84427ea779bbd9e356474ff4602
Leonardo Losoviz <leo@getpop.org>
2022-01-03 16:28:42 +0800

    Removed commented code

--- layers/Engine/packages/root/src-testing/PHPUnit/KernelTestCase.php
+++ layers/Engine/packages/root/src-testing/PHPUnit/KernelTestCase.php
@@ -22,11 +22,6 @@ abstract class KernelTestCase extends TestCase
 {
     protected static $class;
 
-    // /**
-    //  * @var KernelInterface
-    //  */
-    // protected static $kernel;
-
     protected static $booted = false;
 
     private static ?ContainerInterface $kernelContainer = null;
@@ -34,42 +29,9 @@ abstract class KernelTestCase extends TestCase
     protected function tearDown(): void
     {
         static::ensureKernelShutdown();
-        // static::$kernel = null;
         static::$booted = false;
     }
 
-    // /**
-    //  * @throws \RuntimeException
-    //  * @throws \LogicException
-    //  */
-    // protected static function getKernelClass(): string
-    // {
-    //     if (!isset($_SERVER['KERNEL_CLASS']) && !isset($_ENV['KERNEL_CLASS'])) {
-    //         throw new \LogicException(sprintf('You must set the KERNEL_CLASS environment variable to the fully-qualified class name of your Kernel in phpunit.xml / phpunit.xml.dist or override the "%1$s::createKernel()" or "%1$s::getKernelClass()" method.', static::class));
-    //     }
-
-    //     if (!class_exists($class = $_ENV['KERNEL_CLASS'] ?? $_SERVER['KERNEL_CLASS'])) {
-    //         throw new \RuntimeException(sprintf('Class "%s" doesn\'t exist or cannot be autoloaded. Check that the KERNEL_CLASS value in phpunit.xml matches the fully-qualified class name of your Kernel or override the "%s::createKernel()" method.', $class, static::class));
-    //     }
-
-    //     return $class;
-    // }
-
-    // /**
-    //  * Boots the Kernel for this test.
-    //  */
-    // protected static function bootKernel(array $options = []): KernelInterface
-    // {
-    //     static::ensureKernelShutdown();
-
-    //     static::$kernel = static::createKernel($options);
-    //     static::$kernel->boot();
-    //     static::$booted = true;
-
-    //     self::$kernelContainer = static::$kernel->getContainer();
-
-    //     return static::$kernel;
-    // }
     /**
      * Boots the Kernel for this test.
      */
@@ -77,20 +39,16 @@ abstract class KernelTestCase extends TestCase
     {
         static::ensureKernelShutdown();
 
-        // static::$kernel = static::createKernel($options);
-        // static::$kernel->boot();
-        // ContainerBuilderFactory::init();
         $componentClasses = [
             \PoP\GraphQLParser\Component::class,
         ];
         AppLoader::addComponentClassesToInitialize($componentClasses);
         AppLoader::bootSystem(false, null, null, true);
         AppLoader::bootApplication(false, null, null, true);
+
         static::$booted = true;
 
         self::$kernelContainer = ContainerBuilderFactory::getInstance();
-
-        // return static::$kernel;
     }
 
     /**
@@ -114,52 +72,12 @@ abstract class KernelTestCase extends TestCase
         }
     }
 
-    // /**
-    //  * Creates a Kernel.
-    //  *
-    //  * Available options:
-    //  *
-    //  *  * environment
-    //  *  * debug
-    //  */
-    // protected static function createKernel(array $options = []): KernelInterface
-    // {
-    //     if (null === static::$class) {
-    //         static::$class = static::getKernelClass();
-    //     }
-
-    //     if (isset($options['environment'])) {
-    //         $env = $options['environment'];
-    //     } elseif (isset($_ENV['APP_ENV'])) {
-    //         $env = $_ENV['APP_ENV'];
-    //     } elseif (isset($_SERVER['APP_ENV'])) {
-    //         $env = $_SERVER['APP_ENV'];
-    //     } else {
-    //         $env = 'test';
-    //     }
-
-    //     if (isset($options['debug'])) {
-    //         $debug = $options['debug'];
-    //     } elseif (isset($_ENV['APP_DEBUG'])) {
-    //         $debug = $_ENV['APP_DEBUG'];
-    //     } elseif (isset($_SERVER['APP_DEBUG'])) {
-    //         $debug = $_SERVER['APP_DEBUG'];
-    //     } else {
-    //         $debug = true;
-    //     }
-
-    //     return new static::$class($env, $debug);
-    // }
-
     /**
      * Shuts the kernel down if it was used in the test - called by the tearDown method by default.
      */
     protected static function ensureKernelShutdown()
     {
-        // if (null !== static::$kernel) {
-            // static::$kernel->shutdown();
-            static::$booted = false;
-        // }
+        static::$booted = false;
 
         if (self::$kernelContainer instanceof ResetInterface) {
             self::$kernelContainer->reset();
