886b09bcabd4fba169c47cf0da1ca27c0d26d37e
Leonardo Losoviz <leo@getpop.org>
2022-01-05 00:20:17 +0800

    Simplified bootstrap-phpunit.php

--- lib/bootstrap-phpunit.php
+++ lib/bootstrap-phpunit.php
@@ -2,8 +2,4 @@
 
 declare(strict_types=1);
 
-/**
- * @todo Load container services, otherwise can't use services in tests (eg: in GeneralUtilsTest.php)
- */
-
 require_once __DIR__ . '/../vendor/autoload.php';
