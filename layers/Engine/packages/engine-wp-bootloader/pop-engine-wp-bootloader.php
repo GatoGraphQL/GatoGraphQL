eb03c0ee5b2e6394573b6b5eaff8f185a2d690ec
Leonardo Losoviz <leo@getpop.org>
2022-01-05 14:34:37 +0800

    Inject new AppLoader()

--- layers/Engine/packages/engine-wp-bootloader/pop-engine-wp-bootloader.php
+++ layers/Engine/packages/engine-wp-bootloader/pop-engine-wp-bootloader.php
@@ -8,6 +8,7 @@ Author: Leonardo Losoviz
 */
 
 use PoP\Engine\App;
+use PoP\Engine\AppLoader;
 
 if (!class_exists(App::class)) {
     return;
@@ -20,7 +21,7 @@ if (!class_exists(App::class)) {
  * or not based on their required plugins being active.
  */
 \add_action('plugins_loaded', function(): void {
-    App::initialize();
+    App::initialize(new AppLoader());
     App::getAppLoader()->initializeComponents();
     App::getAppLoader()->bootSystem();
     App::getAppLoader()->bootApplication();
