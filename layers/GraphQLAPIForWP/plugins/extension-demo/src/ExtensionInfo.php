d273fc70de1afd293d29e8add1067be41c81792b
Leonardo Losoviz <leo@getpop.org>
2022-01-05 19:45:59 +0800

    Added ExtensionInfo

---
+++ layers/GraphQLAPIForWP/plugins/extension-demo/src/ExtensionInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\ExtensionDemo;
+
+use GraphQLAPI\GraphQLAPI\PluginSkeleton\AbstractExtensionInfo;
+
+class ExtensionInfo extends AbstractExtensionInfo
+{
+}
