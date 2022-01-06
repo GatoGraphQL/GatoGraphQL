bf98a2d88205e97a1047536d4df996f03f2f6666
Leonardo Losoviz <leo@getpop.org>
2022-01-07 07:46:03 +0800

    Improved code

--- layers/Engine/packages/engine/src/Misc/OperatorHelpers.php
+++ layers/Engine/packages/engine/src/Misc/OperatorHelpers.php
@@ -28,20 +28,23 @@ class OperatorHelpers
             if (!$dataPointer) {
                 // If we reached the end of the array and can't keep going down any level more, then it's an error
                 self::throwNoArrayItemUnderPathException($data, $path);
-            } elseif (array_key_exists($pathLevel, $dataPointer)) {
+            }
+            if (array_key_exists($pathLevel, $dataPointer)) {
                 // Retrieve the property under the pathLevel
                 $dataPointer = &$dataPointer[$pathLevel];
-            } elseif (is_array($dataPointer) && isset($dataPointer[0]) && is_array($dataPointer[0]) && isset($dataPointer[0][$pathLevel])) {
+                continue;
+            }
+            if (is_array($dataPointer) && isset($dataPointer[0]) && is_array($dataPointer[0]) && isset($dataPointer[0][$pathLevel])) {
                 // If it is an array, then retrieve that property from each element of the array
                 $dataPointerArray = array_map(function ($item) use ($pathLevel) {
                     return $item[$pathLevel];
                 }, $dataPointer);
                 $dataPointer = &$dataPointerArray;
-            } else {
-                // We are accessing a level that doesn't exist
-                // If we reached the end of the array and can't keep going down any level more, then it's an error
-                self::throwNoArrayItemUnderPathException($data, $path);
+                continue;
             }
+            // We are accessing a level that doesn't exist
+            // If we reached the end of the array and can't keep going down any level more, then it's an error
+            self::throwNoArrayItemUnderPathException($data, $path);
         }
         return $dataPointer;
     }
