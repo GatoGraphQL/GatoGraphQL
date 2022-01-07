1ef78d785bf562a32cb1c18a49f8ec7b410a9352
Leonardo Losoviz <leo@getpop.org>
2022-01-07 15:38:06 +0800

    Fixed signature

--- layers/Engine/packages/component-model/src/ComponentAppState.php
+++ layers/Engine/packages/component-model/src/ComponentAppState.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace PoP\Root\Component;
 
-interface ComponentAppStateInterface
+class AbstractComponentAppState extends AbstractComponentAppState
 {
     /**
      * Have the Component set its own state, accessible for all Components in the App
      *
      * @param array<string,mixed> $state
      */
-    public function initialize(array &$state): void;
+    public function initialize(array &$state): void
+    {
+        
+    }
 }
--- layers/Engine/packages/root/src/Component/AbstractComponentAppState.php
+++ layers/Engine/packages/root/src/Component/AbstractComponentAppState.php
@@ -6,8 +6,6 @@ namespace PoP\Root\Component;
 
 abstract class AbstractComponentAppState implements ComponentAppStateInterface
 {
-    protected array $values = [];
-
     public function __construct(
         protected ComponentInterface $component
     ) {
