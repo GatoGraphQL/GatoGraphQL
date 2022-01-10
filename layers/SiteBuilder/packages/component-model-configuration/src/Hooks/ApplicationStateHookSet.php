99e399e08a52364541ccc2c708bf986a76347988
Leonardo Losoviz <leo@getpop.org>
2022-01-10 18:49:48 +0800

    Migrated AppStateProvider

--- layers/SiteBuilder/packages/component-model-configuration/src/Hooks/ApplicationStateHookSet.php
+++ layers/SiteBuilder/packages/component-model-configuration/src/Hooks/ApplicationStateHookSet.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace PoP\ConfigurationComponentModel\Hooks;
 
 use PoP\BasicService\AbstractHookSet;
-use PoP\ConfigurationComponentModel\Constants\Values;
 use PoP\ComponentModel\ModelInstance\ModelInstance;
 use PoP\ComponentModel\State\ApplicationState;
-use PoP\ConfigurationComponentModel\Constants\Params;
-use PoP\ConfigurationComponentModel\Constants\Targets;
 
 class ApplicationStateHookSet extends AbstractHookSet
 {
@@ -19,41 +16,6 @@ class ApplicationStateHookSet extends AbstractHookSet
             ModelInstance::HOOK_COMPONENTSFROMVARS_RESULT,
             [$this, 'maybeAddComponent']
         );
-        $this->getHooksAPI()->addAction(
-            'ApplicationState:addVars',
-            [$this, 'addVars'],
-            10,
-            1
-        );
-    }
-    /**
-     * @param array<array> $vars_in_array
-     */
-    public function addVars(array $vars_in_array): void
-    {
-        [&$vars] = $vars_in_array;
-
-        // If not target, or invalid, reset it to "main"
-        // We allow an empty target if none provided, so that we can generate the settings cache when no target is provided
-        // (ie initial load) and when target is provided (ie loading pageSection)
-        $target = strtolower($_REQUEST[Params::TARGET] ?? '');
-        $targets = (array) $this->getHooksAPI()->applyFilters(
-            'ApplicationState:targets',
-            [
-                Targets::MAIN,
-            ]
-        );
-        if (!in_array($target, $targets)) {
-            $target = Targets::MAIN;
-        }
-
-        // If there is not format, then set it to 'default'
-        // This is needed so that the /generate/ generated configurations under a $model_instance_id (based on the value of $vars)
-        // can match the same $model_instance_id when visiting that page
-        $format = isset($_REQUEST[Params::FORMAT]) ? strtolower($_REQUEST[Params::FORMAT]) : Values::DEFAULT;
-
-        $vars['target'] = $target;
-        $vars['format'] = $format;
     }
 
     public function maybeAddComponent(array $components): array
--- layers/SiteBuilder/packages/component-model-configuration/src/State/AppStateProvider.php
+++ layers/SiteBuilder/packages/component-model-configuration/src/State/AppStateProvider.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace PoP\ConfigurationComponentModel\State;
 
 use PoP\ConfigurationComponentModel\Configuration\Request;
+use PoP\ConfigurationComponentModel\Constants\Params;
+use PoP\ConfigurationComponentModel\Constants\Targets;
+use PoP\ConfigurationComponentModel\Constants\Values;
 use PoP\Root\State\AbstractAppStateProvider;
 
 class AppStateProvider extends AbstractAppStateProvider
@@ -13,5 +16,27 @@ class AppStateProvider extends AbstractAppStateProvider
     {
         // Override the settings from ComponentModel
         $state['dataoutputitems'] = Request::getDataOutputItems();
+
+        // If not target, or invalid, reset it to "main"
+        // We allow an empty target if none provided, so that we can generate the settings cache when no target is provided
+        // (ie initial load) and when target is provided (ie loading pageSection)
+        $target = strtolower($_REQUEST[Params::TARGET] ?? '');
+        $targets = (array) $this->getHooksAPI()->applyFilters(
+            'ApplicationState:targets',
+            [
+                Targets::MAIN,
+            ]
+        );
+        if (!in_array($target, $targets)) {
+            $target = Targets::MAIN;
+        }
+
+        // If there is not format, then set it to 'default'
+        // This is needed so that the /generate/ generated configurations under a $model_instance_id (based on the value of $state)
+        // can match the same $model_instance_id when visiting that page
+        $format = isset($_REQUEST[Params::FORMAT]) ? strtolower($_REQUEST[Params::FORMAT]) : Values::DEFAULT;
+
+        $state['target'] = $target;
+        $state['format'] = $format;
     }
 }
