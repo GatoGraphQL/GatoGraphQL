51698ee47af68a787b8baf1cd5887aef7a66bcab
Leonardo Losoviz <leo@getpop.org>
2022-01-10 18:25:59 +0800

    Migrated AppState

--- layers/GraphQLByPoP/packages/graphql-server/src/Component.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/Component.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace GraphQLByPoP\GraphQLServer;
 
 use PoP\Root\App;
-use GraphQLByPoP\GraphQLRequest\Component as GraphQLRequestComponent;
 use GraphQLByPoP\GraphQLServer\Configuration\MutationSchemes;
 use GraphQLByPoP\GraphQLServer\Configuration\Request;
 use PoP\AccessControl\Component as AccessControlComponent;
--- layers/GraphQLByPoP/packages/graphql-server/src/Hooks/VarsHookSet.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/Hooks/VarsHookSet.php
@@ -4,81 +4,18 @@ declare(strict_types=1);
 
 namespace GraphQLByPoP\GraphQLServer\Hooks;
 
-use PoP\Root\App;
-use GraphQLByPoP\GraphQLServer\Component;
-use GraphQLByPoP\GraphQLServer\ComponentConfiguration;
-use GraphQLByPoP\GraphQLServer\Configuration\Request;
-use PoP\API\Response\Schemes as APISchemes;
 use PoP\ComponentModel\ModelInstance\ModelInstance;
 use PoP\ComponentModel\State\ApplicationState;
-use PoP\GraphQLAPI\DataStructureFormatters\GraphQLDataStructureFormatter;
 use PoP\BasicService\AbstractHookSet;
 
 class VarsHookSet extends AbstractHookSet
 {
-    private ?GraphQLDataStructureFormatter $graphQLDataStructureFormatter = null;
-
-    final public function setGraphQLDataStructureFormatter(GraphQLDataStructureFormatter $graphQLDataStructureFormatter): void
-    {
-        $this->graphQLDataStructureFormatter = $graphQLDataStructureFormatter;
-    }
-    final protected function getGraphQLDataStructureFormatter(): GraphQLDataStructureFormatter
-    {
-        return $this->graphQLDataStructureFormatter ??= $this->instanceManager->getInstance(GraphQLDataStructureFormatter::class);
-    }
-
     protected function init(): void
     {
-        $this->getHooksAPI()->addAction(
-            'ApplicationState:addVars',
-            array($this, 'addVars'),
-            10,
-            1
-        );
-        $this->getHooksAPI()->addAction(
-            'augmentVarsProperties',
-            [$this, 'augmentVarsProperties'],
-            10,
-            1
-        );
         $this->getHooksAPI()->addFilter(
             ModelInstance::HOOK_COMPONENTS_RESULT,
             array($this, 'getModelInstanceComponentsFromVars')
         );
-    }
-
-    /**
-     * Check if to use nested mutations from the GraphQL server config
-     * @param array<array> $vars_in_array
-     */
-    public function augmentVarsProperties(array $vars_in_array): void
-    {
-        /** @var ComponentConfiguration */
-        $componentConfiguration = App::getComponent(Component::class)->getConfiguration();
-
-        // The PQL always has nested mutations enabled. Only the for the standard GraphQL server
-        [&$vars] = $vars_in_array;
-        $vars['nested-mutations-enabled'] = $vars['standard-graphql'] ?
-            $componentConfiguration->enableNestedMutations()
-            : true;
-        // Check if the value has been defined by configuration. If so, use it.
-        // Otherwise, use the defaults:
-        // By default, Standard GraphQL has introspection enabled, and PQL is not
-        $enableGraphQLIntrospection = $componentConfiguration->enableGraphQLIntrospection();
-        $vars['graphql-introspection-enabled'] = $enableGraphQLIntrospection !== null ?
-            $enableGraphQLIntrospection
-            : $vars['standard-graphql'];
-    }
-
-    /**
-     * @param array<array> $vars_in_array
-     */
-    public function addVars(array $vars_in_array): void
-    {
-        [&$vars] = $vars_in_array;
-        if ($vars['scheme'] == APISchemes::API && $vars['datastructure'] == $this->getGraphQLDataStructureFormatter()->getName()) {
-            $vars['edit-schema'] = Request::editSchema();
-        }
     }
 
     public function getModelInstanceComponentsFromVars($components)
