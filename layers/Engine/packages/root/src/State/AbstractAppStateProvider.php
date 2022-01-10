7dc36a2328c435388ab5f0a2e1d81d4f772b105e
Leonardo Losoviz <leo@getpop.org>
2022-01-10 17:10:21 +0800

    Don't inject Component

--- layers/Engine/packages/root/src/State/AbstractAppStateProvider.php
+++ layers/Engine/packages/root/src/State/AbstractAppStateProvider.php
@@ -6,9 +6,8 @@ namespace PoP\Root\State;
 
 abstract class AbstractAppStateProvider implements AppStateProviderInterface
 {
-    public function __construct(
-        protected ComponentInterface $component
-    ) {
+    public function __construct()
+    {
     }
 
     public function augment(array &$state): void
