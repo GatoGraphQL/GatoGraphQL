c1a102949329f2cf20c26d0b444af4ece793899f
Leonardo Losoviz <leo@getpop.org>
2022-01-10 18:55:13 +0800

    Migrated AppStateProvider

--- layers/SiteBuilder/packages/site-builder-api/src/Hooks/ApplicationStateHookSet.php
+++ layers/SiteBuilder/packages/site-builder-api/src/Hooks/ApplicationStateHookSet.php
@@ -6,7 +6,6 @@ namespace PoP\SiteBuilderAPI\Hooks;
 
 use PoP\ComponentModel\ModelInstance\ModelInstance;
 use PoP\ComponentModel\State\ApplicationState;
-use PoP\ComponentModel\StratumManagerFactory;
 use PoP\BasicService\AbstractHookSet;
 
 class ApplicationStateHookSet extends AbstractHookSet
@@ -17,28 +16,6 @@ class ApplicationStateHookSet extends AbstractHookSet
             ModelInstance::HOOK_COMPONENTSFROMVARS_RESULT,
             [$this, 'maybeAddComponent']
         );
-        $this->getHooksAPI()->addAction(
-            'ApplicationState:addVars',
-            [$this, 'addVars'],
-            10,
-            1
-        );
-    }
-    /**
-     * @param array<array> $vars_in_array
-     */
-    public function addVars(array $vars_in_array): void
-    {
-        [&$vars] = $vars_in_array;
-
-        $platformmanager = StratumManagerFactory::getInstance();
-        $stratum = $platformmanager->getStratum();
-        $strata = $platformmanager->getStrata($stratum);
-        $stratum_isdefault = $platformmanager->isDefaultStratum();
-
-        $vars['stratum'] = $stratum;
-        $vars['strata'] = $strata;
-        $vars['stratum-isdefault'] = $stratum_isdefault;
     }
 
     public function maybeAddComponent(array $components): array
