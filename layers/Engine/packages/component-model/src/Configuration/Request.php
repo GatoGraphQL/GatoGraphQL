14e4a3546f929a76b25b5d888689f126090eed20
Leonardo Losoviz <leo@getpop.org>
2022-01-10 15:27:15 +0800

    Migrated entries to Request

--- layers/Engine/packages/component-model/src/Configuration/Request.php
+++ layers/Engine/packages/component-model/src/Configuration/Request.php
@@ -4,10 +4,50 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Configuration;
 
+use PoP\ComponentModel\Constants\Outputs;
 use PoP\ComponentModel\Constants\Params;
+use PoP\Definitions\Configuration\Request as DefinitionsRequest;
 
 class Request
 {
+    public static function getOutput(): string
+    {
+        $output = strtolower($_REQUEST[Params::OUTPUT] ?? '');
+        $outputs = [
+            Outputs::HTML,
+            Outputs::JSON,
+        ];
+        if (!in_array($output, $outputs)) {
+            return Outputs::HTML;
+        }
+        return $output;
+    }
+
+    public static function getDataStructure(): string
+    {
+        return strtolower($_REQUEST[Params::DATASTRUCTURE] ?? '');
+    }
+
+    public static function getMangledValue(): string
+    {
+        return DefinitionsRequest::isMangled() ? '' : DefinitionsRequest::URLPARAMVALUE_MANGLED_NONE;
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getActions(): array
+    {
+        return isset($_REQUEST[Params::ACTIONS]) ? array_map('strtolower', $_REQUEST[Params::ACTIONS]) : [];
+    }
+
+    public static function getScheme(): string
+    {
+        return strtolower($_REQUEST[Params::SCHEME] ?? '');
+    }
+    
+    
+    
     /**
      * Indicates the version constraint for all fields/directives in the query
      */
