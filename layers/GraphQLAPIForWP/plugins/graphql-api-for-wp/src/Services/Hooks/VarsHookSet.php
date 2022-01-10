b8733b4fcd9d131fd9e0c54f2086b2ac1d59772a
Leonardo Losoviz <leo@getpop.org>
2022-01-10 23:50:57 +0800

    Reverted change

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Hooks/VarsHookSet.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Hooks/VarsHookSet.php
@@ -59,7 +59,7 @@ class VarsHookSet extends AbstractHookSet
     public function maybeRemoveVars(array $vars_in_array): void
     {
         [&$vars] = $vars_in_array;
-        if (isset(\PoP\Root\App::getState('scheme')) && \PoP\Root\App::getState('scheme') == APISchemes::API) {
+        if (isset($vars['scheme']) && $vars['scheme'] == APISchemes::API) {
             // By setting explicit allowed datastructures, we avoid the empty one
             // being processed /?scheme=api <= native API
             // If ever need to support REST or another format, add a hook here
@@ -70,10 +70,10 @@ class VarsHookSet extends AbstractHookSet
                 // If single endpoint not enabled
                 !$this->getModuleRegistry()->isModuleEnabled(EndpointFunctionalityModuleResolver::SINGLE_ENDPOINT)
                 // If datastructure is not GraphQL (or another allowed one)
-                || !in_array(\PoP\Root\App::getState('datastructure'), $allowedDataStructures)
+                || !in_array($vars['datastructure'], $allowedDataStructures)
             ) {
-                unset(\PoP\Root\App::getState('scheme'));
-                unset(\PoP\Root\App::getState('datastructure'));
+                unset($vars['scheme']);
+                unset($vars['datastructure']);
             }
         }
     }
