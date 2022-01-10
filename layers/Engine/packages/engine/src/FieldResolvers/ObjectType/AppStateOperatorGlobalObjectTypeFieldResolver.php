c185cd953c212a7e005e2ebdeec004f8e4382a3f
Leonardo Losoviz <leo@getpop.org>
2022-01-11 07:00:00 +0800

    Moved 'var' and 'context' to new FieldResolver

---
+++ layers/Engine/packages/engine/src/FieldResolvers/ObjectType/AppStateOperatorGlobalObjectTypeFieldResolver.php
@@ -0,0 +1,169 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Engine\FieldResolvers\ObjectType;
+
+use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractGlobalObjectTypeFieldResolver;
+use PoP\ComponentModel\Schema\FieldQueryUtils;
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\State\ApplicationState;
+use PoP\ComponentModel\TypeResolvers\ConcreteTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
+use PoPSchema\SchemaCommons\TypeResolvers\ScalarType\JSONObjectScalarTypeResolver;
+
+class AppStateOperatorGlobalObjectTypeFieldResolver extends AbstractGlobalObjectTypeFieldResolver
+{
+    public const HOOK_SAFEVARS = __CLASS__ . ':safeVars';
+
+    /**
+     * @var array<string, mixed>
+     */
+    protected ?array $safeVars = null;
+
+    private ?JSONObjectScalarTypeResolver $jsonObjectScalarTypeResolver = null;
+    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
+
+    final public function setJSONObjectScalarTypeResolver(JSONObjectScalarTypeResolver $jsonObjectScalarTypeResolver): void
+    {
+        $this->jsonObjectScalarTypeResolver = $jsonObjectScalarTypeResolver;
+    }
+    final protected function getJSONObjectScalarTypeResolver(): JSONObjectScalarTypeResolver
+    {
+        return $this->jsonObjectScalarTypeResolver ??= $this->instanceManager->getInstance(JSONObjectScalarTypeResolver::class);
+    }
+    final public function setStringScalarTypeResolver(StringScalarTypeResolver $stringScalarTypeResolver): void
+    {
+        $this->stringScalarTypeResolver = $stringScalarTypeResolver;
+    }
+    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
+    {
+        return $this->stringScalarTypeResolver ??= $this->instanceManager->getInstance(StringScalarTypeResolver::class);
+    }
+
+    public function getFieldNamesToResolve(): array
+    {
+        return [
+            'var',
+            'context',
+        ];
+    }
+
+    public function getFieldTypeResolver(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ConcreteTypeResolverInterface
+    {
+        return match ($fieldName) {
+            'var' => $this->getDangerouslyDynamicScalarTypeResolver(),
+            'context' => $this->getJSONObjectScalarTypeResolver(),
+        };
+    }
+
+    public function getFieldTypeModifiers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): int
+    {
+        return match ($fieldName) {
+            'context' => SchemaTypeModifiers::NON_NULLABLE,
+            default => parent::getFieldTypeModifiers($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
+    {
+        return match ($fieldName) {
+            'var' => $this->__('Retrieve the value of a certain property from the application state', 'component-model'),
+            'context' => $this->__('Retrieve the application state', 'component-model'),
+            default => parent::getFieldDescription($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldArgNameTypeResolvers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): array
+    {
+        return match ($fieldName) {
+            'var' => [
+                'name' => $this->getStringScalarTypeResolver(),
+            ],
+            default => parent::getFieldArgNameTypeResolvers($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldArgDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName, string $fieldArgName): ?string
+    {
+        return match ([$fieldName => $fieldArgName]) {
+            ['var' => 'name'] => $this->__('The name of the variable to retrieve from the application state', 'component-model'),
+            default => parent::getFieldArgDescription($objectTypeResolver, $fieldName, $fieldArgName),
+        };
+    }
+
+    public function getFieldArgTypeModifiers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName, string $fieldArgName): int
+    {
+        return match ([$fieldName => $fieldArgName]) {
+            ['var' => 'name'] => SchemaTypeModifiers::MANDATORY,
+            default => parent::getFieldArgTypeModifiers($objectTypeResolver, $fieldName, $fieldArgName),
+        };
+    }
+
+    protected function doResolveSchemaValidationErrorDescriptions(
+        ObjectTypeResolverInterface $objectTypeResolver,
+        string $fieldName,
+        array $fieldArgs
+    ): array {
+        // Important: The validations below can only be done if no fieldArg contains a field!
+        // That is because this is a schema error, so we still don't have the $object against which to resolve the field
+        // For instance, this doesn't work: /?query=arrayItem(posts(),3)
+        // In that case, the validation will be done inside ->resolveValue(), and will be treated as a $dbError, not a $schemaError
+        if (!FieldQueryUtils::isAnyFieldArgumentValueAField($fieldArgs)) {
+            switch ($fieldName) {
+                case 'var':
+                    $safeVars = $this->getSafeVars();
+                    if (!isset($safeVars[$fieldArgs['name']])) {
+                        return [
+                            sprintf(
+                                $this->__('There is no property \'%s\' in the application state', 'component-model'),
+                                $fieldArgs['name']
+                            )
+                        ];
+                    };
+                    break;
+            }
+        }
+
+        return parent::doResolveSchemaValidationErrorDescriptions($objectTypeResolver, $fieldName, $fieldArgs);
+    }
+
+    protected function getSafeVars()
+    {
+        if (is_null($this->safeVars)) {
+            $this->safeVars = ApplicationState::getVars();
+            $this->getHooksAPI()->doAction(
+                self::HOOK_SAFEVARS,
+                array(&$this->safeVars)
+            );
+        }
+        return $this->safeVars;
+    }
+
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @param array<string, mixed>|null $variables
+     * @param array<string, mixed>|null $expressions
+     * @param array<string, mixed> $options
+     */
+    public function resolveValue(
+        ObjectTypeResolverInterface $objectTypeResolver,
+        object $object,
+        string $fieldName,
+        array $fieldArgs,
+        ?array $variables = null,
+        ?array $expressions = null,
+        array $options = []
+    ): mixed {
+        switch ($fieldName) {
+            case 'var':
+                $safeVars = $this->getSafeVars();
+                return $safeVars[$fieldArgs['name']];
+            case 'context':
+                return $this->getSafeVars();
+        }
+
+        return parent::resolveValue($objectTypeResolver, $object, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+}
