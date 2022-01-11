1c9fbbf9dd18e0f782c2a9755d1f221b906de06c
Leonardo Losoviz <leo@getpop.org>
2022-01-11 11:51:10 +0800

    Added temp code to convert GraphQL query to PoP

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/State/AbstractGraphQLQueryResolutionEndpointExecuterAppStateProvider.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/State/AbstractGraphQLQueryResolutionEndpointExecuterAppStateProvider.php
@@ -6,6 +6,8 @@ namespace GraphQLAPI\GraphQLAPI\State;
 
 use GraphQLAPI\GraphQLAPI\Services\EndpointExecuters\EndpointExecuterInterface;
 use GraphQLAPI\GraphQLAPI\Services\EndpointExecuters\GraphQLQueryResolutionEndpointExecuterInterface;
+use GraphQLByPoP\GraphQLQuery\Schema\GraphQLQueryConvertorInterface;
+use GraphQLByPoP\GraphQLQuery\Schema\OperationTypes;
 use PoP\API\Response\Schemes as APISchemes;
 use PoP\GraphQLAPI\DataStructureFormatters\GraphQLDataStructureFormatter;
 use PoP\Root\App;
@@ -13,6 +15,7 @@ use PoP\Root\App;
 abstract class AbstractGraphQLQueryResolutionEndpointExecuterAppStateProvider extends AbstractEndpointExecuterAppStateProvider
 {
     private ?GraphQLDataStructureFormatter $graphQLDataStructureFormatter = null;
+    private ?GraphQLQueryConvertorInterface $graphQLQueryConvertor = null;
 
     final public function setGraphQLDataStructureFormatter(GraphQLDataStructureFormatter $graphQLDataStructureFormatter): void
     {
@@ -21,6 +24,14 @@ abstract class AbstractGraphQLQueryResolutionEndpointExecuterAppStateProvider ex
     final protected function getGraphQLDataStructureFormatter(): GraphQLDataStructureFormatter
     {
         return $this->graphQLDataStructureFormatter ??= $this->instanceManager->getInstance(GraphQLDataStructureFormatter::class);
+    }
+    final public function setGraphQLQueryConvertor(GraphQLQueryConvertorInterface $graphQLQueryConvertor): void
+    {
+        $this->graphQLQueryConvertor = $graphQLQueryConvertor;
+    }
+    final protected function getGraphQLQueryConvertor(): GraphQLQueryConvertorInterface
+    {
+        return $this->graphQLQueryConvertor ??= $this->instanceManager->getInstance(GraphQLQueryConvertorInterface::class);
     }
 
     protected function getEndpointExecuter(): EndpointExecuterInterface
@@ -34,7 +45,10 @@ abstract class AbstractGraphQLQueryResolutionEndpointExecuterAppStateProvider ex
     {
         $state['scheme'] = APISchemes::API;
         $state['datastructure'] = $this->getGraphQLDataStructureFormatter()->getName();
+    }
 
+    public function consolidate(array &$state): void
+    {
         /**
          * Get the query and variables from the implementing class
          */
@@ -48,6 +62,31 @@ abstract class AbstractGraphQLQueryResolutionEndpointExecuterAppStateProvider ex
         }
 
         $state['query'] = $graphQLQuery;
+
+        // @todo Remove this code, to temporarily convert back from GraphQL to PoP query
+        // ---------------------------------------------
+        if ($state['query'] !== null) {
+            list(
+                $operationType,
+                $fieldQuery
+            ) = $this->getGraphQLQueryConvertor()->convertFromGraphQLToFieldQuery(
+                $state['query'],
+                $state['variables'],
+                null,
+            );
+            $state['query'] = $fieldQuery;
+
+            // Set the operation type and, based on it, if mutations are supported
+            $state['graphql-operation-type'] = $operationType;
+            $state['are-mutations-enabled'] = $operationType === OperationTypes::MUTATION;
+    
+            // If there was an error when parsing the query, the operationType will be null,
+            // then there's no need to execute the query
+            if ($operationType === null) {
+                $state['does-api-query-have-errors'] = true;
+            }
+        }
+        // ---------------------------------------------
 
         /**
          * Merge the variables into $state?
