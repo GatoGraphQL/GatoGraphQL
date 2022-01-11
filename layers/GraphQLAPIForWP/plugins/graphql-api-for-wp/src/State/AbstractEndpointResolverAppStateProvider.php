5f6976fd044886fb2160d29e60fe1370c9327a70
Leonardo Losoviz <leo@getpop.org>
2022-01-11 15:16:21 +0800

    Reuse code via trait

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/State/AbstractEndpointResolverAppStateProvider.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/State/AbstractEndpointResolverAppStateProvider.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\State;
 
+use GraphQLAPI\GraphQLAPI\Services\EndpointExecuters\GraphQLEndpointResolverInterface;
 use GraphQLAPI\GraphQLAPI\Services\EndpointResolvers\EndpointResolverInterface;
 use GraphQLByPoP\GraphQLQuery\Schema\GraphQLQueryConvertorInterface;
-use GraphQLByPoP\GraphQLQuery\Schema\OperationTypes;
-use PoP\API\Response\Schemes as APISchemes;
 use PoP\GraphQLAPI\DataStructureFormatters\GraphQLDataStructureFormatter;
-use PoP\Root\App;
 use PoP\Root\State\AbstractAppStateProvider;
-use PoP\Routing\RouteNatures;
 
 abstract class AbstractEndpointResolverAppStateProvider extends AbstractAppStateProvider
 {
+    use EndpointResolverAppStateProviderTrait;
+
     private ?GraphQLDataStructureFormatter $graphQLDataStructureFormatter = null;
     private ?GraphQLQueryConvertorInterface $graphQLQueryConvertor = null;
 
@@ -37,6 +36,11 @@ abstract class AbstractEndpointResolverAppStateProvider extends AbstractAppState
 
     abstract protected function getEndpointResolver(): EndpointResolverInterface;
 
+    protected function getGraphQLEndpointResolver(): GraphQLEndpointResolverInterface
+    {
+        return $this->getEndpointResolver();
+    }
+
     public function isServiceEnabled(): bool
     {
         return $this->getEndpointResolver()->isGraphQLQueryExecution();
@@ -44,72 +48,11 @@ abstract class AbstractEndpointResolverAppStateProvider extends AbstractAppState
 
     public function initialize(array &$state): void
     {
-        $state['scheme'] = APISchemes::API;
-        $state['datastructure'] = $this->getGraphQLDataStructureFormatter()->getName();
-
-        // Assign the single endpoint by setting it as the Home nature
-        $state['nature'] = RouteNatures::HOME;
+        $this->initializeState($state);
     }
 
     public function consolidate(array &$state): void
     {
-        /**
-         * Get the query and variables from the implementing class
-         */
-        list(
-            $graphQLQuery,
-            $graphQLVariables
-        ) = $this->getEndpointResolver()->getGraphQLQueryAndVariables(App::getState(['routing', 'queried-object']));
-        if (!$graphQLQuery) {
-            // If there is no query, nothing to do!
-            return;
-        }
-
-        $state['query'] = $graphQLQuery;
-
-        /**
-         * Merge the variables into $state?
-         *
-         * Normally, GraphQL variables must not override the variables from the request
-         * But this behavior can be overriden for the persisted query,
-         * by setting "Accept Variables as URL Params" => false
-         * When editing in the editor, 'queried-object' will be null, and that's OK
-         */
-        $graphQLVariables ??= [];
-        $state['variables'] = $this->getEndpointResolver()->doURLParamsOverrideGraphQLVariables(App::getState(['routing', 'queried-object'])) ?
-            array_merge(
-                $graphQLVariables,
-                $state['variables'] ?? []
-            ) :
-            array_merge(
-                $state['variables'] ?? [],
-                $graphQLVariables
-            );
-
-        // @todo Remove this code, to temporarily convert back from GraphQL to PoP query
-        // ---------------------------------------------
-        if ($state['query'] !== null) {
-            list(
-                $operationType,
-                $fieldQuery
-            ) = $this->getGraphQLQueryConvertor()->convertFromGraphQLToFieldQuery(
-                $state['query'],
-                $state['variables'],
-                null,
-            );
-            $state['query'] = $fieldQuery;
-
-            // Set the operation type and, based on it, if mutations are supported
-            $state['graphql-operation-type'] = $operationType;
-            $state['are-mutations-enabled'] = $operationType === OperationTypes::MUTATION;
-    
-            // If there was an error when parsing the query, the operationType will be null,
-            // then there's no need to execute the query
-            if ($operationType === null) {
-                $state['does-api-query-have-errors'] = true;
-            }
-        }
-        $state['standard-graphql'] = true;
-        // ---------------------------------------------
+        $this->consolidateState($state);
     }
 }
