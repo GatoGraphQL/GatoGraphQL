1ec0a07d00a201324e245f9796aa0abb0876f055
Leonardo Losoviz <leo@getpop.org>
2022-01-12 00:02:59 +0800

    When executing PHPUnit tests there'll be no URI

--- layers/Engine/packages/routing/src/RoutingUtils.php
+++ layers/Engine/packages/routing/src/RoutingUtils.php
@@ -13,7 +13,7 @@ class RoutingUtils
         // Allow to remove the language information from qTranslate (https://domain.com/en/...)
         $route = HooksAPIFacade::getInstance()->applyFilters(
             '\PoP\Routing:uri-route',
-            $_SERVER['REQUEST_URI']
+            $_SERVER['REQUEST_URI'] ?? '' // When executing PHPUnit tests there'll be no URI
         );
         $params_pos = strpos($route, '?');
         if ($params_pos !== false) {
