ab01f3af6e57f990da7736235c41aee9e0be2ad6
Leonardo Losoviz <leo@getpop.org>
2022-01-11 09:01:18 +0800

    Fixed registry

--- layers/Engine/packages/root/src/Container/CompilerPasses/RegisterAppStateProviderCompilerPass.php
+++ layers/Engine/packages/root/src/Container/CompilerPasses/RegisterAppStateProviderCompilerPass.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace PoP\Root\Container\CompilerPasses;
 
-use GraphQLAPI\GraphQLAPI\Registries\AccessControlRuleBlockRegistryInterface;
+use PoP\Root\Container\CompilerPasses\AbstractInjectServiceIntoRegistryCompilerPass;
+use PoP\Root\Registries\AppStateProviderRegistryInterface;
 use PoP\Root\State\AppStateProviderInterface;
 
 class RegisterAppStateProviderCompilerPass extends AbstractInjectServiceIntoRegistryCompilerPass
 {
     protected function getRegistryServiceDefinition(): string
     {
-        return AccessControlRuleBlockRegistryInterface::class;
+        return AppStateProviderRegistryInterface::class;
     }
     protected function getServiceClass(): string
     {
