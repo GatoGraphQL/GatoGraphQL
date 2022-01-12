13a119e7834f116022831524e41d983fd1f5bfe9
Leonardo Losoviz <leo@getpop.org>
2022-01-12 19:04:10 +0800

    Removed PersistedQueryEndpointExecuterRegistry

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Registries/PersistedQueryEndpointExecuterRegistry.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace GraphQLAPI\GraphQLAPI\Registries;
-
-class PersistedQueryEndpointExecuterRegistry extends AbstractEndpointExecuterRegistry implements PersistedQueryEndpointExecuterRegistryInterface
-{
-}
