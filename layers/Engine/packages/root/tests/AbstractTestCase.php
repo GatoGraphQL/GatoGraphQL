7661850358c6c3e65197d79264925929de0b0c7e
Leonardo Losoviz <leo@getpop.org>
2022-01-13 16:16:29 +0800

    Injectable HookManager in tests

--- layers/Engine/packages/root/tests/AbstractTestCase.php
+++ layers/Engine/packages/root/tests/AbstractTestCase.php
@@ -6,6 +6,8 @@ namespace PoP\Root;
 
 use PHPUnit\Framework\TestCase;
 use PoP\Root\Helpers\ClassHelpers;
+use PoP\Root\StateManagers\HookManager;
+use PoP\Root\StateManagers\HookManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 abstract class AbstractTestCase extends TestCase
@@ -26,7 +28,10 @@ abstract class AbstractTestCase extends TestCase
         ?string $containerDirectory = null,
         bool $isDev = false
     ): void {
-        App::initialize(static::getAppLoader());
+        App::initialize(
+            static::getAppLoader(),
+            static::getHookManager(),
+        );
         App::getAppLoader()->addComponentClassesToInitialize(static::getComponentClassesToInitialize());
         App::getAppLoader()->initializeComponents($isDev);
         App::getAppLoader()->bootSystem($cacheContainerConfiguration, $containerNamespace, $containerDirectory);
@@ -43,6 +48,11 @@ abstract class AbstractTestCase extends TestCase
     protected static function getAppLoader(): AppLoader
     {
         return new AppLoader();
+    }
+
+    protected static function getHookManager(): HookManagerInterface
+    {
+        return new HookManager();
     }
 
     /**
