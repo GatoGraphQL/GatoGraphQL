c2985f0eebf22bb76ff5ac1e2e4cd262390c38a2
Leonardo Losoviz <leo@getpop.org>
2022-01-13 14:58:38 +0800

    Removed stale import

--- layers/Engine/packages/hooks-wp/tests/ComponentTest.php
+++ layers/Engine/packages/hooks-wp/tests/ComponentTest.php
@@ -2,7 +2,6 @@
 
 namespace PoP\HooksWP;
 
-use PHPUnit\Framework\TestCase;
 use PoP\Root\AbstractComponentTest;
 
 /**
--- layers/Engine/packages/routing-wp/tests/ComponentTest.php
+++ layers/Engine/packages/routing-wp/tests/ComponentTest.php
@@ -2,7 +2,6 @@
 
 namespace PoP\RoutingWP;
 
-use PHPUnit\Framework\TestCase;
 use PoP\Root\AbstractComponentTest;
 
 /**
