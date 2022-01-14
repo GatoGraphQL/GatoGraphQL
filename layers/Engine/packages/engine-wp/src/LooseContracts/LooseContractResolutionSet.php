1b575415e582d74d20c7a548f896f6a5bd072d51
Leonardo Losoviz <leo@getpop.org>
2022-01-14 21:37:03 +0800

    Removed stale import

--- layers/Engine/packages/engine-wp/src/LooseContracts/LooseContractResolutionSet.php
+++ layers/Engine/packages/engine-wp/src/LooseContracts/LooseContractResolutionSet.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PoP\EngineWP\LooseContracts;
 
-use PoP\Root\App;
 use PoP\LooseContracts\AbstractLooseContractResolutionSet;
 
 class LooseContractResolutionSet extends AbstractLooseContractResolutionSet
