e6e416ca0a8d6caa6781fdd9910826fa1564fb2b
Leonardo Losoviz <leo@getpop.org>
2022-01-14 21:36:55 +0800

    Removed loose contracts for Actions

--- layers/Engine/packages/engine-wp/src/LooseContracts/LooseContractResolutionSet.php
+++ layers/Engine/packages/engine-wp/src/LooseContracts/LooseContractResolutionSet.php
@@ -11,22 +11,6 @@ class LooseContractResolutionSet extends AbstractLooseContractResolutionSet
 {
     protected function resolveContracts(): void
     {
-        // Actions
-        App::addAction('shutdown', function (): void {
-            App::doAction('popcms:shutdown');
-        });
-        App::addAction('activate_plugin', function (): void {
-            App::doAction('popcms:componentInstalledOrUninstalled');
-        });
-        App::addAction('deactivate_plugin', function (): void {
-            App::doAction('popcms:componentInstalledOrUninstalled');
-        });
-
-        $this->getLooseContractManager()->implementHooks([
-            'popcms:shutdown',
-            'popcms:componentInstalledOrUninstalled',
-        ]);
-
         $this->getNameResolver()->implementNames([
             'popcms:option:dateFormat' => 'date_format',
             'popcms:option:charset' => 'blog_charset',
