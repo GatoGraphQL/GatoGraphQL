010489e7ceff73aea1e5126dffd713d12eede06b
Leonardo Losoviz <leo@getpop.org>
2022-01-14 13:35:58 +0800

    Replaced RoutingUtils

--- layers/Engine/packages/root/src/Routing/AbstractRoutingManager.php
+++ layers/Engine/packages/root/src/Routing/AbstractRoutingManager.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 
 namespace PoP\Root\Routing;
 
-use PoP\Root\App;
 use PoP\Root\Constants\Params;
 use PoP\Root\Services\BasicServiceTrait;
 
 abstract class AbstractRoutingManager implements RoutingManagerInterface
 {
     use BasicServiceTrait;
+
+    private ?RoutingHelperServiceInterface $routingHelperService = null;
+
+    final public function setRoutingHelperService(RoutingHelperServiceInterface $routingHelperService): void
+    {
+        $this->routingHelperService = $routingHelperService;
+    }
+    final protected function getRoutingHelperService(): RoutingHelperServiceInterface
+    {
+        return $this->routingHelperService ??= $this->instanceManager->getInstance(RoutingHelperServiceInterface::class);
+    }
 
     /**
      * By default, everything is a generic route
@@ -26,7 +36,7 @@ abstract class AbstractRoutingManager implements RoutingManagerInterface
 
         // If it is a ROUTE, then the URL path is already the route
         if ($nature === RouteNatures::GENERIC) {
-            return RoutingUtils::getRequestURIPath();
+            return $this->getRoutingHelperService()->getRequestURIPath();
         }
 
         // If having set URL param "route", then use it
