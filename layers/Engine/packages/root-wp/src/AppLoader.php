2e53fb7d7ef4864f306d3a9ed14bb1ed280f28d1
Leonardo Losoviz <leo@getpop.org>
2022-01-14 20:51:49 +0800

    Fixed initializing App in REST

--- layers/Engine/packages/root-wp/src/AppLoader.php
+++ layers/Engine/packages/root-wp/src/AppLoader.php
@@ -19,37 +19,52 @@ class AppLoader extends UpstreamAppLoader
         App::getComponentManager()->beforeBoot();
 
         /**
-         * Find the right action, depending if we are in wp-admin or in frontend.
+         * Find the right action hook to initialize the application,
+         * depending if we are in wp-admin, in the WP REST API, or in the frontend.
          *
          * Boot once it has parsed the WP_Query, so that the requested post/user/etc
-         * is already processed and available. Only hook available is "wp".
+         * is already processed and available. For the front-end, the only hook
+         * available is "wp".
          *
-         * Watch out: "wp" doesn't trigger in the admin()!
-         * Hence, in that case, use "wp_loaded" instead
+         * Watch out:
+         * 
+         * - "wp" doesn't trigger in the admin() => use "wp_loaded" instead.
+         * - "wp" doesn't trigger in REST => use "rest_api_init" instead.
+         * 
+         * (Eg for the latter: when editing an ACL in the WordPress editor
+         * and clicking on Update, it uses a REST call.)
+         * 
+         * Because we don't know yet if the current request is a REST call or not
+         * (must wait until hook "parse_request" for that), simply
+         * load the hook for both 'rest_api_init' and 'wp', knowing
+         * that only one of them will be called anyway.
+         *
+         * @see https://stackoverflow.com/questions/41101294/check-whether-request-is-wp-rest-api-request
          */
-        $actionHook = \is_admin() ? 'wp_loaded' : 'wp';
-
-        // Override when the functionality is executed
-        App::addAction(
-            $actionHook,
-            fn () => App::getAppStateManager()->initializeAppState(),
-            0
-        );
-        App::addAction(
-            $actionHook,
-            fn () => App::getComponentManager()->boot(),
-            4
-        );
-        App::addAction(
-            $actionHook,
-            fn () => App::getComponentManager()->afterBoot(),
-            8
-        );
-        // Allow to inject functionality
-        App::addAction(
-            $actionHook,
-            fn () => App::doAction(HookNames::AFTER_BOOT_APPLICATION),
-            10
-        );
+        $actionHooks = \is_admin() ? ['wp_loaded'] : ['rest_api_init', 'wp'];
+        foreach ($actionHooks as $actionHook) {
+            // Override when the functionality is executed
+            App::addAction(
+                $actionHook,
+                fn () => App::getAppStateManager()->initializeAppState(),
+                0
+            );
+            App::addAction(
+                $actionHook,
+                fn () => App::getComponentManager()->boot(),
+                4
+            );
+            App::addAction(
+                $actionHook,
+                fn () => App::getComponentManager()->afterBoot(),
+                8
+            );
+            // Allow to inject functionality
+            App::addAction(
+                $actionHook,
+                fn () => App::doAction(HookNames::AFTER_BOOT_APPLICATION),
+                10
+            );
+        }
     }
 }
