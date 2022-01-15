1bd4c13a30264bdb4334737c025fc4914a019a11
Leonardo Losoviz <leo@getpop.org>
2022-01-15 13:40:51 +0800

    Created packages in Legacy

---
+++ layers/Legacy/Schema/packages/schema-commons-wp/src/LooseContracts/LooseContractResolutionSet.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\SchemaCommonsWP\LooseContracts;
+
+use PoP\LooseContracts\AbstractLooseContractResolutionSet;
+
+class LooseContractResolutionSet extends AbstractLooseContractResolutionSet
+{
+    protected function resolveContracts(): void
+    {
+        $this->getNameResolver()->implementNames([
+            'popcms:option:dateFormat' => 'date_format',
+            'popcms:option:charset' => 'blog_charset',
+            'popcms:option:gmtOffset' => 'gmt_offset',
+            'popcms:option:timezone' => 'timezone_string',
+        ]);
+    }
+}
---
+++ layers/Legacy/Schema/packages/schema-commons/src/LooseContracts/LooseContractSet.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\SchemaCommons\LooseContracts;
+
+use PoP\LooseContracts\AbstractLooseContractSet;
+
+class LooseContractSet extends AbstractLooseContractSet
+{
+    /**
+     * @return string[]
+     */
+    public function getRequiredNames(): array
+    {
+        return [
+            // Options
+            'popcms:option:dateFormat',
+            'popcms:option:charset',
+            'popcms:option:gmtOffset',
+            'popcms:option:timezone',
+        ];
+    }
+}
