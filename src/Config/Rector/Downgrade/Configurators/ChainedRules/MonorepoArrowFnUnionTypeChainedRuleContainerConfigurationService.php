4ea2af006b6aca1d6778c67a04d63d93603ce2c3
Leonardo Losoviz <leo@getpop.org>
2022-01-17 22:37:28 +0800

    Fixed path to files

--- src/Config/Rector/Downgrade/Configurators/ChainedRules/MonorepoArrowFnUnionTypeChainedRuleContainerConfigurationService.php
+++ src/Config/Rector/Downgrade/Configurators/ChainedRules/MonorepoArrowFnUnionTypeChainedRuleContainerConfigurationService.php
@@ -12,9 +12,9 @@ class MonorepoArrowFnUnionTypeChainedRuleContainerConfigurationService extends A
             $this->rootDirectory . '/layers/Engine/packages/component-model/src/TypeResolvers/AbstractRelationalTypeResolver.php',
             $this->rootDirectory . '/layers/Engine/packages/component-model/src/TypeResolvers/ObjectType/AbstractObjectTypeResolver.php',
             $this->rootDirectory . '/layers/Engine/packages/engine/src/DirectiveResolvers/FilterIDsSatisfyingConditionDirectiveResolverTrait.php',
-            $this->rootDirectory . '/layers/Schema/packages/menus/src/RelationalTypeDataLoaders/ObjectType/MenuItemTypeDataLoader.php',
-            $this->rootDirectory . '/layers/Schema/packages/menus/src/RelationalTypeDataLoaders/ObjectType/MenuTypeDataLoader.php',
-            $this->rootDirectory . '/layers/Schema/packages/user-avatars/src/RelationalTypeDataLoaders/ObjectType/UserAvatarTypeDataLoader.php',
+            $this->rootDirectory . '/layers/CMSSchema/packages/menus/src/RelationalTypeDataLoaders/ObjectType/MenuItemTypeDataLoader.php',
+            $this->rootDirectory . '/layers/CMSSchema/packages/menus/src/RelationalTypeDataLoaders/ObjectType/MenuTypeDataLoader.php',
+            $this->rootDirectory . '/layers/CMSSchema/packages/user-avatars/src/RelationalTypeDataLoaders/ObjectType/UserAvatarTypeDataLoader.php',
             $this->rootDirectory . '/layers/WPSchema/packages/menus/src/FieldResolvers/ObjectType/MenuObjectTypeFieldResolver.php',
         ];
     }
