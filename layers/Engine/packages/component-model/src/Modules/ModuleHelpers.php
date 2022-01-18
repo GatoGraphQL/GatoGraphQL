437058056c704dd8d45b86ba41a4a079b59bda57
Leonardo Losoviz <leo@getpop.org>
2022-01-18 15:16:37 +0800

    Added service

---
+++ layers/Engine/packages/component-model/src/Modules/ModuleHelpers.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\Modules;
+
+use PoP\Root\Services\BasicServiceTrait;
+
+class ModuleHelpers implements ModuleHelpersInterface
+{
+    use BasicServiceTrait;
+
+    
+}
