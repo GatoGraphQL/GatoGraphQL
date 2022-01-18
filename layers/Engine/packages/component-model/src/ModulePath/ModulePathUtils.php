4b66c2cedacf3e5368098fd4d358a012dc5e2571
Leonardo Losoviz <leo@getpop.org>
2022-01-18 11:38:33 +0800

    Read env var

--- layers/Engine/packages/component-model/src/ModulePath/ModulePathUtils.php
+++ layers/Engine/packages/component-model/src/ModulePath/ModulePathUtils.php
@@ -7,39 +7,51 @@ namespace PoP\ComponentModel\ModulePath;
 use PoP\ComponentModel\Constants\Params;
 use PoP\ComponentModel\Facades\ModulePath\ModulePathHelpersFacade;
 use PoP\ComponentModel\Tokens\ModulePath;
+use PoP\Root\App;
+use PoP\Root\Component as RootComponent;
+use PoP\Root\ComponentConfiguration as RootComponentConfiguration;
 
 class ModulePathUtils
 {
     public static function getModulePaths(): array
     {
-        $ret = array();
-        if ($paths = $_REQUEST[Params::MODULEPATHS] ?? null) {
-            if (!is_array($paths)) {
-                $paths = array($paths);
-            }
-
-            // If any path is a substring from another one, then it is its root, and only this one will be taken into account, so remove its substrings
-            // Eg: toplevel.pagesection-top is substring of toplevel, so if passing these 2 modulepaths, keep only toplevel
-            // Check that the last character is ".", to avoid toplevel1 to be removed
-            $paths = array_filter(
-                $paths,
-                function ($item) use ($paths) {
-                    foreach ($paths as $path) {
-                        if (strlen($item) > strlen($path) && str_starts_with($item, $path) && $item[strlen($path)] == ModulePath::MODULE_SEPARATOR) {
-                            return false;
-                        }
-                    }
-
-                    return true;
-                }
-            );
-
-            foreach ($paths as $path) {
-                // Each path must be converted to an array of the modules
-                $ret[] = ModulePathHelpersFacade::getInstance()->recastModulePath($path);
-            }
+        /** @var RootComponentConfiguration */
+        $rootComponentConfiguration = App::getComponent(RootComponent::class)->getConfiguration();
+        if (!$rootComponentConfiguration->enablePassingStateViaRequest()) {
+            return [];
         }
 
-        return $ret;
+        $paths = $_REQUEST[Params::MODULEPATHS];
+        if ($paths === null) {
+            return [];
+        }
+        
+        $modulePaths = [];
+        if (!is_array($paths)) {
+            $paths = array($paths);
+        }
+
+        // If any path is a substring from another one, then it is its root, and only this one will be taken into account, so remove its substrings
+        // Eg: toplevel.pagesection-top is substring of toplevel, so if passing these 2 modulepaths, keep only toplevel
+        // Check that the last character is ".", to avoid toplevel1 to be removed
+        $paths = array_filter(
+            $paths,
+            function ($item) use ($paths) {
+                foreach ($paths as $path) {
+                    if (strlen($item) > strlen($path) && str_starts_with($item, $path) && $item[strlen($path)] == ModulePath::MODULE_SEPARATOR) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        );
+
+        foreach ($paths as $path) {
+            // Each path must be converted to an array of the modules
+            $modulePaths[] = ModulePathHelpersFacade::getInstance()->recastModulePath($path);
+        }
+
+        return $modulePaths;
     }
 }
