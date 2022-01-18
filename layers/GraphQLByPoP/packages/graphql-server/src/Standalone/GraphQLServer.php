465197d39fc922181138e7d69b4380abdea08a07
Leonardo Losoviz <leo@getpop.org>
2022-01-18 20:48:57 +0800

    Don't initializeAppState again

--- layers/GraphQLByPoP/packages/graphql-server/src/Standalone/GraphQLServer.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/Standalone/GraphQLServer.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace GraphQLByPoP\GraphQLServer\Standalone;
 
+use GraphQLByPoP\GraphQLQuery\Facades\GraphQLQueryConvertorFacade;
+use GraphQLByPoP\GraphQLQuery\Schema\OperationTypes;
 use PoP\Engine\Facades\Engine\EngineFacade;
 use PoP\Root\App;
+use PoPAPI\API\Facades\FieldQueryConvertorFacade;
 use PoPAPI\API\Response\Schemes;
 use PoPAPI\API\Routing\RequestNature;
 
@@ -48,6 +51,8 @@ class GraphQLServer
             $this->containerNamespace,
             $this->containerDirectory
         );
+
+        $appLoader->bootApplicationComponents();
     }
 
     /**
@@ -57,7 +62,7 @@ class GraphQLServer
     {
         return [
             'scheme' => Schemes::API,
-            'datastructure' => 'graphql',
+            'datastructure' => 'graphql', // Replace here with output from service
             'nature' => RequestNature::QUERY_ROOT,
             'only-fieldname-as-outputkey' => true,
             'standard-graphql' => true,
@@ -66,11 +71,23 @@ class GraphQLServer
 
     public function execute(string $query, array $variables = []): void
     {
-        $appLoader = App::getAppLoader();
-        $appLoader->bootApplicationComponents([
-            'query' => $query,
-            'variables' => $variables,
-        ]);
+        $appStateManager = App::getAppStateManager();
+        $appStateManager->override('query', $query);
+        $appStateManager->override('variables', $variables);
+
+        [$operationType, $fieldQuery] = GraphQLQueryConvertorFacade::getInstance()->convertFromGraphQLToFieldQuery(
+            $query,
+            $variables,
+        );
+        $appStateManager->override('graphql-operation-type', $operationType);
+        $appStateManager->override('are-mutations-enabled', $operationType === OperationTypes::MUTATION);
+
+        $fieldQueryConvertor = FieldQueryConvertorFacade::getInstance();
+        $fieldQuerySet = $fieldQueryConvertor->convertAPIQuery($fieldQuery);
+        $appStateManager->override('executable-query', $fieldQuerySet->getExecutableFieldQuery());
+        if ($fieldQuerySet->areRequestedAndExecutableFieldQueriesDifferent()) {
+            $appStateManager->override('requested-query', $fieldQuerySet->getRequestedFieldQuery());
+        }
         
         $engine = EngineFacade::getInstance();
         $engine->outputResponse();
