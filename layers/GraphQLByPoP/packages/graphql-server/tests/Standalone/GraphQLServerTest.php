da8f9a8ba4e1573691b76a37b3f50f66af3e083a
Leonardo Losoviz <leo@getpop.org>
2022-01-18 19:43:00 +0800

    Added queryProvider

--- layers/GraphQLByPoP/packages/graphql-server/tests/Standalone/GraphQLServerTest.php
+++ layers/GraphQLByPoP/packages/graphql-server/tests/Standalone/GraphQLServerTest.php
@@ -6,15 +6,46 @@ namespace GraphQLByPoP\GraphQLServer\Standalone;
 
 class GraphQLServerTest extends AbstractGraphQLServerTestCase
 {
-    public function testHasDependedComponentClasses(): void
+    /**
+     * @dataProvider queryProvider
+     */
+    public function testGraphQLServerExecution(string $query, array $response): void
     {
-        $query = '{ id }';
-        $response = [
-            'data' => [
-                'id' => 'root',
-            ]
-        ];
         $this->expectOutputString(json_encode($response));
         self::getGraphQLServer()->execute($query);
+    }
+
+    public function queryProvider(): array
+    {
+        return [
+            'id' => [
+                <<<GRAPHQL
+                query {
+                    id
+                }
+                GRAPHQL,
+                [
+                    'data' => [
+                        'id' => 'root',
+                    ]
+                ]
+            ],
+            'self' => [
+                <<<GRAPHQL
+                query {
+                    self {
+                        id
+                    }
+                }
+                GRAPHQL,
+                [
+                    'data' => [
+                        'self' => [
+                            'id' => 'root',
+                        ]
+                    ]
+                ]
+            ],
+        ];
     }
 }
