f592405ccb66c3a6a502833fbed8c6f233224fc6
Leonardo Losoviz <leo@getpop.org>
2022-01-18 19:50:05 +0800

    Removed stale import

--- layers/GraphQLByPoP/packages/graphql-server/src/Standalone/GraphQLServer.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/Standalone/GraphQLServer.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace GraphQLByPoP\GraphQLServer\Standalone;
 
-use PoP\Engine\Engine\EngineInterface;
 use PoP\Engine\Facades\Engine\EngineFacade;
 use PoP\Root\App;
 use PoPAPI\API\Response\Schemes;
