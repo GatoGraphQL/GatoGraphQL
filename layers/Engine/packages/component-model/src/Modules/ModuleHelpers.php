f34ae98e9948de122b5723b4172321cab52440f7
Leonardo Losoviz <leo@getpop.org>
2022-01-18 15:17:32 +0800

    Added functions

--- layers/Engine/packages/component-model/src/Modules/ModuleHelpers.php
+++ layers/Engine/packages/component-model/src/Modules/ModuleHelpers.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Modules;
 
+use PoP\ComponentModel\ItemProcessors\ProcessorItemUtils;
 use PoP\Root\Services\BasicServiceTrait;
 
 class ModuleHelpers implements ModuleHelpersInterface
 {
     use BasicServiceTrait;
 
-    
+    public function getModuleFullName(array $module): string
+    {
+        return ProcessorItemUtils::getItemFullName($module);
+    }
+    public function getModuleFromFullName(string $moduleFullName): ?array
+    {
+        return ProcessorItemUtils::getItemFromFullName($moduleFullName);
+    }
+    public function getModuleOutputName(array $module): string
+    {
+        return ProcessorItemUtils::getItemOutputName($module, DefinitionGroups::MODULES);
+    }
+    public function getModuleFromOutputName(string $moduleOutputName): ?array
+    {
+        return ProcessorItemUtils::getItemFromOutputName($moduleOutputName, DefinitionGroups::MODULES);
+    }
 }
--- layers/Engine/packages/component-model/src/Modules/ModuleHelpersInterface.php
+++ layers/Engine/packages/component-model/src/Modules/ModuleHelpersInterface.php
@@ -6,5 +6,8 @@ namespace PoP\ComponentModel\Modules;
 
 interface ModuleHelpersInterface
 {
-    
+    public function getModuleFullName(array $module): string;
+    public function getModuleFromFullName(string $moduleFullName): ?array;
+    public function getModuleOutputName(array $module): string;
+    public function getModuleFromOutputName(string $moduleOutputName): ?array;
 }
