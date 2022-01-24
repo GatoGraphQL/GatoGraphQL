b61300f43a66e7e5b7c7a25da1de6516da6d339f
Leonardo Losoviz <leo@getpop.org>
2022-01-24 20:26:37 +0800

    phpcs:disable Generic.PHP.DisallowRequestSuperglobal

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ConditionalOnContext/Admin/SystemServices/TableActions/AbstractListTableAction.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ConditionalOnContext/Admin/SystemServices/TableActions/AbstractListTableAction.php
@@ -19,6 +19,7 @@ abstract class AbstractListTableAction extends AbstractAutomaticallyInstantiated
      * @since 3.1.0
      *
      * @return string|false The action name or False if no action was selected
+     * phpcs:disable Generic.PHP.DisallowRequestSuperglobal
      */
     public function currentAction(): string|false
     {
