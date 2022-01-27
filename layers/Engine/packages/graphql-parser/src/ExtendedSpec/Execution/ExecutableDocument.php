d4969c4f366c52d52aa996c412b1c4a7b3a0391b
Leonardo Losoviz <leo@getpop.org>
2022-01-27 15:32:18 +0800

    Remove: use StandaloneServiceTrait;

--- layers/Engine/packages/graphql-parser/src/ExtendedSpec/Execution/ExecutableDocument.php
+++ layers/Engine/packages/graphql-parser/src/ExtendedSpec/Execution/ExecutableDocument.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace PoP\GraphQLParser\ExtendedSpec\Execution;
 
-use PoP\Root\Services\StandaloneServiceTrait;
 use PoP\GraphQLParser\Facades\Query\QueryAugmenterServiceFacade;
 use PoP\GraphQLParser\Spec\Execution\ExecutableDocument as UpstreamExecutableDocument;
 use PoP\GraphQLParser\Spec\Parser\Ast\OperationInterface;
 
 class ExecutableDocument extends UpstreamExecutableDocument
 {
-    use StandaloneServiceTrait;
-
     /**
      * Override to support the "multiple query execution" feature:
      * If passing operation name `__ALL`, then execute all operations (hack)
