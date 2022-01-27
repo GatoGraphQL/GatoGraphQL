493451da43c7b16350ed999fa1bfc13598a51770
Leonardo Losoviz <leo@getpop.org>
2022-01-27 17:35:05 +0800

    Added service

---
+++ layers/Engine/packages/graphql-parser/src/Error/GraphQLErrorMessageProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\GraphQLParser\Error;
+
+use PoP\Root\Services\BasicServiceTrait;
+
+class GraphQLErrorMessageProvider implements GraphQLErrorMessageProviderInterface
+{
+    use BasicServiceTrait;
+}
