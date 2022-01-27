b2605a20e009f1e47ec84c1a070fb79e3efae120
Leonardo Losoviz <leo@getpop.org>
2022-01-27 11:58:09 +0800

    Reordered

--- layers/Engine/packages/engine/tests/Parser/EnabledMetaDirectiveTest.php
+++ layers/Engine/packages/engine/tests/Parser/EnabledMetaDirectiveTest.php
@@ -194,6 +194,51 @@ class EnabledMetaDirectiveTest extends AbstractMetaDirectiveTest
             [
                 <<<GRAPHQL
                     query {
+                        groupCapabilities @forEach @advancePointerInArrayOrObject(path: "group") @upperCase @lowerCase
+                    }
+                GRAPHQL,
+                new Document(
+                    [
+                        new QueryOperation(
+                            '',
+                            [],
+                            [],
+                            [
+                                new LeafField(
+                                    'groupCapabilities',
+                                    null,
+                                    [],
+                                    [
+                                        new MetaDirective(
+                                            'forEach',
+                                            [],
+                                            [
+                                                new MetaDirective(
+                                                    'advancePointerInArrayOrObject',
+                                                    [
+                                                        new Argument('path', new Literal('group', new Location(2, 74)), new Location(2, 67)),
+                                                    ],
+                                                    [
+                                                        new Directive('upperCase', [], new Location(2, 83)),
+                                                    ],
+                                                    new Location(2, 37)
+                                                )
+                                            ],
+                                            new Location(2, 28)
+                                        ),
+                                        new Directive('lowerCase', [], new Location(2, 94)),
+                                    ],
+                                    new Location(2, 9)
+                                ),
+                            ],
+                            new Location(1, 11)
+                        )
+                    ]
+                ),
+            ],
+            [
+                <<<GRAPHQL
+                    query {
                         groupCapabilities @forEach @advancePointerInArrayOrObject(path: "group", affectDirectivesUnderPos: [1,2]) @upperCase @lowerCase
                     }
                 GRAPHQL,
@@ -228,51 +273,6 @@ class EnabledMetaDirectiveTest extends AbstractMetaDirectiveTest
                                             ],
                                             new Location(2, 28)
                                         ),
-                                    ],
-                                    new Location(2, 9)
-                                ),
-                            ],
-                            new Location(1, 11)
-                        )
-                    ]
-                ),
-            ],
-            [
-                <<<GRAPHQL
-                    query {
-                        groupCapabilities @forEach @advancePointerInArrayOrObject(path: "group") @upperCase @lowerCase
-                    }
-                GRAPHQL,
-                new Document(
-                    [
-                        new QueryOperation(
-                            '',
-                            [],
-                            [],
-                            [
-                                new LeafField(
-                                    'groupCapabilities',
-                                    null,
-                                    [],
-                                    [
-                                        new MetaDirective(
-                                            'forEach',
-                                            [],
-                                            [
-                                                new MetaDirective(
-                                                    'advancePointerInArrayOrObject',
-                                                    [
-                                                        new Argument('path', new Literal('group', new Location(2, 74)), new Location(2, 67)),
-                                                    ],
-                                                    [
-                                                        new Directive('upperCase', [], new Location(2, 83)),
-                                                    ],
-                                                    new Location(2, 37)
-                                                )
-                                            ],
-                                            new Location(2, 28)
-                                        ),
-                                        new Directive('lowerCase', [], new Location(2, 94)),
                                     ],
                                     new Location(2, 9)
                                 ),
