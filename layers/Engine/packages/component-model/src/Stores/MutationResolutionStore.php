4295e5d08b57b00d47788d1d74ac510d57c6f45e
Leonardo Losoviz <leo@getpop.org>
2022-01-28 22:20:11 +0800

    MutationResolutionStoreInterface

--- layers/Engine/packages/component-model/src/Stores/MutationResolutionStore.php
+++ layers/Engine/packages/component-model/src/Stores/MutationResolutionStore.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Stores;
 
-class MutationResolutionStore
+class MutationResolutionStore implements MutationResolutionStoreInterface
 {
     /**
      * @var array<string, mixed>
