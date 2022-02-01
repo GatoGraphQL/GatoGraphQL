66d37d3adbadbd8d24d658bab41818a06b553927
Leonardo Losoviz <leo@getpop.org>
2022-02-01 17:05:29 +0800

    Imported class

--- layers/Engine/packages/root/src/Registries/FeedbackMessageRegistry.php
+++ layers/Engine/packages/root/src/Registries/FeedbackMessageRegistry.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\Root\Registries;
 
+use PoP\Root\FeedbackMessage\FeedbackMessageEntryKeys;
 use PoP\Root\FeedbackMessage\FeedbackMessageProviderInterface;
 
 class FeedbackMessageRegistry implements FeedbackMessageRegistryInterface
