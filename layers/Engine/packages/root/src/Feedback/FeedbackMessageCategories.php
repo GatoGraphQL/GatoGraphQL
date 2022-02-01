54f86d8fee093ac62db973777798c74855e5d141
Leonardo Losoviz <leo@getpop.org>
2022-02-01 10:07:11 +0800

    FeedbackMessageCategories

---
+++ layers/Engine/packages/root/src/Feedback/FeedbackMessageCategories.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Root\Registries;
+
+class FeedbackMessageCategories
+{
+    public const ERROR = 'error';
+    public const WARNING = 'warning';
+    public const SUGGESTION = 'suggestion';
+    public const NOTICE = 'notice';
+    public const DEPRECATION = 'deprecation';
+}
