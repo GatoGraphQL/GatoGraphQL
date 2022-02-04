a34eaf2f74df3acffbc1ee5656c0291fd0d66dd9
Leonardo Losoviz <leo@getpop.org>
2022-02-04 11:56:07 +0800

    Added GeneralFeedback

--- layers/Engine/packages/component-model/src/Feedback/FeedbackStore.php
+++ layers/Engine/packages/component-model/src/Feedback/FeedbackStore.php
@@ -6,13 +6,20 @@ namespace PoP\ComponentModel\Feedback;
 
 class FeedbackStore
 {
+    public GeneralFeedbackStore $generalFeedbackStore;
     public DocumentFeedbackStore $documentFeedbackStore;
     public SchemaFeedbackStore $schemaFeedbackStore;
     public ObjectFeedbackStore $objectFeedbackStore;
 
     public function __construct()
     {
+        $this->regenerateGeneralFeedbackStore();
         $this->regenerateDocumentFeedbackStore();
+    }
+
+    public function regenerateGeneralFeedbackStore(): void
+    {
+        $this->generalFeedbackStore = new GeneralFeedbackStore();
     }
 
     public function regenerateDocumentFeedbackStore(): void
