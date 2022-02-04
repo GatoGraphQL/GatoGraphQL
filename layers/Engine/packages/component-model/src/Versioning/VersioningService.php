e3204922b13a131bca917eacacb4084da79e9e49
Leonardo Losoviz <leo@getpop.org>
2022-02-04 12:27:46 +0800

    Use FeedbackMessageProviders

--- layers/Engine/packages/component-model/src/Versioning/VersioningService.php
+++ layers/Engine/packages/component-model/src/Versioning/VersioningService.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace PoP\ComponentModel\Versioning;
 
 use PoP\ComponentModel\App;
+use PoP\ComponentModel\Constants\Constants;
 use PoP\ComponentModel\Feedback\GeneralFeedback;
+use PoP\ComponentModel\FeedbackMessageProviders\FeedbackMessageProvider;
 use PoP\ComponentModel\Schema\FeedbackMessageStoreInterface;
 use PoP\Root\Services\BasicServiceTrait;
 
@@ -13,15 +15,11 @@ class VersioningService implements VersioningServiceInterface
 {
     use BasicServiceTrait;
 
-    /**
-     * Token used to separate the type from the field for setting version constraints
-     */
-    private const TYPE_FIELD_SEPARATOR = '.';
-
     private ?array $versionConstraintsForFields = null;
     private ?array $versionConstraintsForDirectives = null;
 
     private ?FeedbackMessageStoreInterface $feedbackMessageStore = null;
+    private ?FeedbackMessageProvider $feedbackMessageProvider = null;
 
     final public function setFeedbackMessageStore(FeedbackMessageStoreInterface $feedbackMessageStore): void
     {
@@ -31,41 +29,39 @@ class VersioningService implements VersioningServiceInterface
     {
         return $this->feedbackMessageStore ??= $this->instanceManager->getInstance(FeedbackMessageStoreInterface::class);
     }
+    final public function setFeedbackMessageProvider(FeedbackMessageProvider $feedbackMessageProvider): void
+    {
+        $this->feedbackMessageProvider = $feedbackMessageProvider;
+    }
+    final protected function getFeedbackMessageProvider(): FeedbackMessageProvider
+    {
+        return $this->feedbackMessageProvider ??= $this->instanceManager->getInstance(FeedbackMessageProvider::class);
+    }
 
     /**
      * Initialize the dictionary with the version constraints for specific fields in the schema
      */
     protected function initializeVersionConstraintsForFields(): void
     {
+        $generalFeedbackStore = App::getFeedbackStore()->generalFeedbackStore;
+        
         // Iterate through entries in `fieldVersionConstraints` and set them into a dictionary
         $this->versionConstraintsForFields = [];
-        $generalWarningMessages = [];
         foreach ((App::getState('field-version-constraints') ?? []) as $typeField => $versionConstraint) {
             // All fields are defined as "$type.$fieldName". If not, it's an error
-            $entry = explode(self::TYPE_FIELD_SEPARATOR, $typeField);
-            if (count($entry) != 2) {
-                $generalWarningMessages[] = sprintf(
-                    $this->__('URL param \'fieldVersionConstraints\' expects the type and field name separated by \'%s\' (eg: \'%s\'), so the following value has been ignored: \'%s\'', 'component-model'),
-                    self::TYPE_FIELD_SEPARATOR,
-                    '?fieldVersionConstraints[Post.title]=^0.1',
-                    $typeField
+            $entry = explode(Constants::TYPE_FIELD_SEPARATOR, $typeField);
+            if (count($entry) !== 2) {
+                $generalFeedbackStore->addGeneralWarning(
+                    new GeneralFeedback(
+                        $this->getFeedbackMessageProvider()->getMessage(FeedbackMessageProvider::W1, $typeField),
+                        $this->getFeedbackMessageProvider()->getNamespacedCode(FeedbackMessageProvider::W1)
+                    )
                 );
                 continue;
             }
             $maybeNamespacedTypeName = $entry[0];
             $fieldName = $entry[1];
             $this->versionConstraintsForFields[$maybeNamespacedTypeName][$fieldName] = $versionConstraint;
-        }
-        if ($generalWarningMessages) {
-            $generalFeedbackStore = App::getFeedbackStore()->generalFeedbackStore;
-            foreach ($generalWarningMessages as $warningMessage) {
-                $generalFeedbackStore->addGeneralWarning(
-                    new GeneralFeedback(
-                        $warningMessage,
-                        null
-                    )
-                );
-            }
         }
     }
 
