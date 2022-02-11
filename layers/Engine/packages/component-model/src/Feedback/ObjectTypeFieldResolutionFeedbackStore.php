c2086173fa855acff7a1158283b7d499848224fa
Leonardo Losoviz <leo@getpop.org>
2022-02-11 17:28:39 +0800

    Added ObjectTypeFieldResolutionFeedbackStore

---
+++ layers/Engine/packages/component-model/src/Feedback/ObjectTypeFieldResolutionFeedbackStore.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\Feedback;
+
+class ObjectTypeFieldResolutionFeedbackStore
+{
+    // /** @var FieldResolutionFeedbackInterface[] */
+    // private array $fieldResolutionErrors = [];
+    // /** @var FieldResolutionFeedbackInterface[] */
+    // private array $fieldResolutionWarnings = [];
+    // /** @var FieldResolutionFeedbackInterface[] */
+    // private array $fieldResolutionDeprecations = [];
+    // /** @var FieldResolutionFeedbackInterface[] */
+    // private array $fieldResolutionNotices = [];
+    // /** @var FieldResolutionFeedbackInterface[] */
+    // private array $fieldResolutionLogs = [];
+    // /** @var FieldResolutionFeedbackInterface[] */
+    // private array $fieldResolutionTraces = [];
+
+    // /**
+    //  * @return FieldResolutionFeedbackInterface[]
+    //  */
+    // public function getFieldResolutionErrors(): array
+    // {
+    //     return $this->fieldResolutionErrors;
+    // }
+
+    // public function addFieldResolutionError(FieldResolutionFeedbackInterface $fieldResolutionError): void
+    // {
+    //     $this->fieldResolutionErrors[] = $fieldResolutionError;
+    // }
+
+    // /**
+    //  * @return FieldResolutionFeedbackInterface[]
+    //  */
+    // public function getFieldResolutionWarnings(): array
+    // {
+    //     return $this->fieldResolutionWarnings;
+    // }
+
+    // public function addFieldResolutionWarning(FieldResolutionFeedbackInterface $fieldResolutionWarning): void
+    // {
+    //     $this->fieldResolutionWarnings[] = $fieldResolutionWarning;
+    // }
+
+    // /**
+    //  * @return FieldResolutionFeedbackInterface[]
+    //  */
+    // public function getFieldResolutionDeprecations(): array
+    // {
+    //     return $this->fieldResolutionDeprecations;
+    // }
+
+    // public function addFieldResolutionDeprecation(FieldResolutionFeedbackInterface $fieldResolutionDeprecation): void
+    // {
+    //     $this->fieldResolutionDeprecations[] = $fieldResolutionDeprecation;
+    // }
+
+    // /**
+    //  * @return FieldResolutionFeedbackInterface[]
+    //  */
+    // public function getFieldResolutionNotices(): array
+    // {
+    //     return $this->fieldResolutionNotices;
+    // }
+
+    // public function addFieldResolutionNotice(FieldResolutionFeedbackInterface $fieldResolutionNotice): void
+    // {
+    //     $this->fieldResolutionNotices[] = $fieldResolutionNotice;
+    // }
+
+    // /**
+    //  * @return FieldResolutionFeedbackInterface[]
+    //  */
+    // public function getFieldResolutionLogs(): array
+    // {
+    //     return $this->fieldResolutionLogs;
+    // }
+
+    // public function addFieldResolutionLog(FieldResolutionFeedbackInterface $fieldResolutionLog): void
+    // {
+    //     $this->fieldResolutionLogs[] = $fieldResolutionLog;
+    // }
+
+    // /**
+    //  * @return FieldResolutionFeedbackInterface[]
+    //  */
+    // public function getFieldResolutionTraces(): array
+    // {
+    //     return $this->fieldResolutionTraces;
+    // }
+
+    // public function addFieldResolutionTrace(FieldResolutionFeedbackInterface $fieldResolutionTrace): void
+    // {
+    //     $this->fieldResolutionTraces[] = $fieldResolutionTrace;
+    // }
+}
