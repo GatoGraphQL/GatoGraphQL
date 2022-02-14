d6d0948470d466598927321aec8f08a5a0f3928c
Leonardo Losoviz <leo@getpop.org>
2022-02-15 07:38:32 +0800

    Split FeedbackCategories

--- layers/Engine/packages/root/src/Feedback/FeedbackCategories.php
+++ layers/Engine/packages/root/src/Feedback/FeedbackCategories.php
@@ -7,9 +7,6 @@ namespace PoP\Root\Feedback;
 class FeedbackCategories
 {
     public const ERROR = 'error';
-    public const WARNING = 'warning';
-    public const SUGGESTION = 'suggestion';
-    public const NOTICE = 'notice';
     public const DEPRECATION = 'deprecation';
     public const LOG = 'log';
 }
