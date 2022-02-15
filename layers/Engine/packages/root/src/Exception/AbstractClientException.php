aeaf6485df4baa077af1eed89e1949dfb5cc0b7d
Leonardo Losoviz <leo@getpop.org>
2022-02-15 16:51:02 +0800

    phpdoc

--- layers/Engine/packages/root/src/Exception/AbstractClientException.php
+++ layers/Engine/packages/root/src/Exception/AbstractClientException.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace PoP\Root\Exception;
 
+/**
+ * Exceptions that are safe to be shown to the user
+ * of the application, such as GraphQL query validations.
+ */
 abstract class AbstractClientException extends AbstractException
 {
 }
--- layers/Engine/packages/root/src/Exception/AbstractSystemException.php
+++ layers/Engine/packages/root/src/Exception/AbstractSystemException.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace PoP\Root\Exception;
 
+/**
+ * Exceptions that may contain sensitive information,
+ * so they should be accessed by the admin only.
+ */
 abstract class AbstractSystemException extends AbstractException
 {
 }
