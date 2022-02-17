a4a2fc5b94bc08b38a208c39e73d63787a615cb5
Leonardo Losoviz <leo@getpop.org>
2022-02-17 16:06:45 +0800

    Migrated to CheckpointError

--- layers/Engine/packages/engine/src/CheckpointProcessors/RequestCheckpointProcessor.php
+++ layers/Engine/packages/engine/src/CheckpointProcessors/RequestCheckpointProcessor.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 namespace PoP\Engine\CheckpointProcessors;
 
 use PoP\ComponentModel\Checkpoint\CheckpointError;
-use PoP\Root\App;
 use PoP\ComponentModel\CheckpointProcessors\AbstractCheckpointProcessor;
 use PoP\ComponentModel\Error\Error;
+use PoP\Engine\FeedbackMessageProviders\CheckpointErrorMessageProvider;
+use PoP\Root\App;
 
 class RequestCheckpointProcessor extends AbstractCheckpointProcessor
 {
     public const DOING_POST = 'doing-post';
+
+    private ?CheckpointErrorMessageProvider $checkpointErrorMessageProvider = null;
+
+    final public function setCheckpointErrorMessageProvider(CheckpointErrorMessageProvider $checkpointErrorMessageProvider): void
+    {
+        $this->checkpointErrorMessageProvider = $checkpointErrorMessageProvider;
+    }
+    final protected function getCheckpointErrorMessageProvider(): CheckpointErrorMessageProvider
+    {
+        return $this->checkpointErrorMessageProvider ??= $this->instanceManager->getInstance(CheckpointErrorMessageProvider::class);
+    }
 
     public function getCheckpointsToProcess(): array
     {
@@ -25,6 +37,10 @@ class RequestCheckpointProcessor extends AbstractCheckpointProcessor
         switch ($checkpoint[1]) {
             case self::DOING_POST:
                 if ('POST' !== App::server('REQUEST_METHOD')) {
+                    return new CheckpointError(
+                        $this->getCheckpointErrorMessageProvider()->getMessage(CheckpointErrorMessageProvider::E1),
+                        $this->getCheckpointErrorMessageProvider()->getNamespacedCode(CheckpointErrorMessageProvider::E1),
+                    );
                     return new Error('notdoingpost');
                 }
                 break;
