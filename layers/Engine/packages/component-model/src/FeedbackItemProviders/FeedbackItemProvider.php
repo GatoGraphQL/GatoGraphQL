b33676b897179a0d58640be145208de90c4a50bb
Leonardo Losoviz <leo@getpop.org>
2022-02-18 11:56:08 +0800

    Added error

--- layers/Engine/packages/component-model/src/FeedbackItemProviders/FeedbackItemProvider.php
+++ layers/Engine/packages/component-model/src/FeedbackItemProviders/FeedbackItemProvider.php
@@ -14,6 +14,8 @@ class FeedbackItemProvider extends AbstractFeedbackItemProvider
     public const W1 = 'w1';
     public const E1 = 'e1';
     public const E2 = 'e2';
+    public const E3 = 'e3';
+    public const E4 = 'e4';
 
     /**
      * @return string[]
@@ -24,6 +26,8 @@ class FeedbackItemProvider extends AbstractFeedbackItemProvider
             self::W1,
             self::E1,
             self::E2,
+            self::E3,
+            self::E4,
         ];
     }
 
@@ -31,8 +35,10 @@ class FeedbackItemProvider extends AbstractFeedbackItemProvider
     {
         return match ($code) {
             self::W1 => $this->__('URL param \'' . Params::VERSION_CONSTRAINT_FOR_FIELDS . '\' expects the type and field name separated by \'' . Constants::TYPE_FIELD_SEPARATOR . '\' (eg: \'?' . Params::VERSION_CONSTRAINT_FOR_FIELDS . '[Post' . Constants::TYPE_FIELD_SEPARATOR . 'title]=^0.1\'), so the following value has been ignored: \'%s\'', 'component-model'),
-            self::E1 => $this->__('Field \'%s\' is not a connection', 'pop-component-model'),
-            self::E2 => $this->__('Field \'%s\' could not be processed due to nested error(s)', 'pop-component-model'),
+            self::E1 => $this->__('Field \'%s\' is not a connection', 'component-model'),
+            self::E2 => $this->__('Field \'%s\' could not be processed due to nested error(s)', 'component-model'),
+            self::E3 => $this->__('Resolving field \'%s\' triggered exception: \'%s\'', 'component-model'),
+            self::E4 => $this->__('Resolving field \'%s\' triggered an exception, please contact the admin', 'component-model'),
             default => parent::getMessagePlaceholder($code),
         };
     }
@@ -43,7 +49,9 @@ class FeedbackItemProvider extends AbstractFeedbackItemProvider
             self::W1
                 => FeedbackCategories::WARNING,
             self::E1,
-            self::E2
+            self::E2,
+            self::E3,
+            self::E4
                 => FeedbackCategories::ERROR,
             default => parent::getCategory($code),
         };
