631b033e24cf5c2b68af442887e8718d120f7880
Leonardo Losoviz <leo@getpop.org>
2022-02-18 11:49:14 +0800

    Added error

--- layers/Engine/packages/component-model/src/FeedbackItemProviders/FeedbackItemProvider.php
+++ layers/Engine/packages/component-model/src/FeedbackItemProviders/FeedbackItemProvider.php
@@ -13,6 +13,7 @@ class FeedbackItemProvider extends AbstractFeedbackItemProvider
 {
     public const W1 = 'w1';
     public const E1 = 'e1';
+    public const E2 = 'e2';
 
     /**
      * @return string[]
@@ -22,6 +23,7 @@ class FeedbackItemProvider extends AbstractFeedbackItemProvider
         return [
             self::W1,
             self::E1,
+            self::E2,
         ];
     }
 
@@ -30,6 +32,7 @@ class FeedbackItemProvider extends AbstractFeedbackItemProvider
         return match ($code) {
             self::W1 => $this->__('URL param \'' . Params::VERSION_CONSTRAINT_FOR_FIELDS . '\' expects the type and field name separated by \'' . Constants::TYPE_FIELD_SEPARATOR . '\' (eg: \'?' . Params::VERSION_CONSTRAINT_FOR_FIELDS . '[Post' . Constants::TYPE_FIELD_SEPARATOR . 'title]=^0.1\'), so the following value has been ignored: \'%s\'', 'component-model'),
             self::E1 => $this->__('Field \'%s\' is not a connection', 'pop-component-model'),
+            self::E2 => $this->__('Field \'%s\' could not be processed due to nested error(s)', 'pop-component-model'),
             default => parent::getMessagePlaceholder($code),
         };
     }
@@ -37,8 +40,11 @@ class FeedbackItemProvider extends AbstractFeedbackItemProvider
     public function getCategory(string $code): string
     {
         return match ($code) {
-            self::W1 => FeedbackCategories::WARNING,
-            self::E1 => FeedbackCategories::ERROR,
+            self::W1
+                => FeedbackCategories::WARNING,
+            self::E1,
+            self::E2
+                => FeedbackCategories::ERROR,
             default => parent::getCategory($code),
         };
     }
