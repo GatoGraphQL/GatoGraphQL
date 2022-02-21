0c3936ce4ecfa5eadabf902440a215fdf40819d7
Leonardo Losoviz <leo@getpop.org>
2022-02-21 15:11:05 +0800

    Added SchemaInputValidationFeedback

---
+++ layers/Engine/packages/component-model/src/Feedback/SchemaInputValidationFeedback.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\Feedback;
+
+use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
+use PoP\GraphQLParser\Spec\Parser\Location;
+
+class SchemaInputValidationFeedback extends AbstractQueryFeedback implements SchemaInputValidationFeedbackInterface
+{
+    public function __construct(
+        FeedbackItemResolution $feedbackItemResolution,
+        Location $location,
+        protected InputTypeResolverInterface $inputTypeResolver,
+        /** @var array<string, mixed> */
+        array $extensions = [],
+        /** @var array<string, mixed> */
+        array $data = [],
+        /** @var SchemaInputValidationFeedbackInterface[] */
+        protected array $nested = [],
+    ) {
+        parent::__construct(
+            $feedbackItemResolution,
+            $location,
+            $extensions,
+            $data,
+        );
+    }
+
+    /**
+     * @return SchemaInputValidationFeedbackInterface[]
+     */
+    public function getNested(): array
+    {
+        return $this->nested;
+    }
+}
