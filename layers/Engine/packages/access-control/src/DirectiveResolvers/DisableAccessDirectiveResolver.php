98b32b5673c82797f3bd403e4fc913e920ed2dd4
Leonardo Losoviz <leo@getpop.org>
2022-02-23 11:46:36 +0800

    Migrated getValidationFailedFeedbackItemResolution

--- layers/Engine/packages/access-control/src/DirectiveResolvers/DisableAccessDirectiveResolver.php
+++ layers/Engine/packages/access-control/src/DirectiveResolvers/DisableAccessDirectiveResolver.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\AccessControl\DirectiveResolvers;
 
+use PoP\AccessControl\FeedbackItemProviders\FeedbackItemProvider;
 use PoP\ComponentModel\DirectiveResolvers\AbstractValidateConditionDirectiveResolver;
 use PoP\ComponentModel\Feedback\FeedbackItemResolution;
 use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
@@ -22,15 +23,15 @@ class DisableAccessDirectiveResolver extends AbstractValidateConditionDirectiveR
 
     protected function getValidationFailedFeedbackItemResolution(RelationalTypeResolverInterface $relationalTypeResolver, array $failedDataFields): FeedbackItemResolution
     {
-        $errorMessage = $this->isValidatingDirective() ?
-            $this->__('Access to directives in field(s) \'%s\' has been disabled', 'access-control') :
-            $this->__('Access to field(s) \'%s\' has been disabled', 'access-control');
-        return sprintf(
-            $errorMessage,
-            implode(
-                $this->__('\', \''),
-                $failedDataFields
-            )
+        return new FeedbackItemResolution(
+            FeedbackItemProvider::class,
+            $this->isValidatingDirective() ? FeedbackItemProvider::E1 : FeedbackItemProvider::E2,
+            [
+                implode(
+                    $this->__('\', \''),
+                    $failedDataFields
+                ),
+            ]
         );
     }
 
