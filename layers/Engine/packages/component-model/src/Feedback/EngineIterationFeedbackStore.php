76094514ec0293e6543af28a6d1ac17c6a43333e
Leonardo Losoviz <leo@getpop.org>
2022-02-23 09:46:40 +0800

    Stale import

--- layers/Engine/packages/component-model/src/Feedback/EngineIterationFeedbackStore.php
+++ layers/Engine/packages/component-model/src/Feedback/EngineIterationFeedbackStore.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Feedback;
 
-use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
-
 class EngineIterationFeedbackStore
 {
     public SchemaFeedbackStore $schemaFeedbackStore;
