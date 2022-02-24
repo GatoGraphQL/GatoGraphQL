a7aa69cce3948dd69ab820f054bf31d656b8dd62
Leonardo Losoviz <leo@getpop.org>
2022-02-24 11:18:41 +0800

    Improved comment

--- layers/Engine/packages/component-model/src/TypeResolvers/InputObjectType/AbstractInputObjectTypeResolver.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/InputObjectType/AbstractInputObjectTypeResolver.php
@@ -351,9 +351,23 @@ abstract class AbstractInputObjectTypeResolver extends AbstractTypeResolver impl
             $schemaInputValidationFeedbackStore->incorporate($separateSchemaInputValidationFeedbackStore);
             /**
              * Assign the input field to the resulting InputObject,
-             * whether it succeeded or has errors, because the coerced
-             * value could be a List with `null` on the failing positions,
-             * but not on the whole field value.
+             * whether it succeeded or has errors.
+             *
+             * It works with errors too, because the coerced value
+             * with errors could be a List with `null` on the failing
+             * positions, but not on the whole field value.
+             *
+             * Eg: casting to [String]:
+             *
+             *     [1, "a", 3] => [1, null, 3]
+             *
+             * Right below it will check, in case there are errors,
+             * if the input field is a non-nullable List and,
+             * then, set the whole value to null.
+             *
+             * Eg: casting to [String!]:
+             *
+             *     [1, "a", 3] => null
              *
              * From the GraphQL spec:
              * 
@@ -366,12 +380,6 @@ abstract class AbstractInputObjectTypeResolver extends AbstractTypeResolver impl
                 /**
                  * If the input field is a non-nullable List and it has errors,
                  * then it must be set to null.
-                 *
-                 * From the GraphQL spec:
-                 * 
-                 *     If a list’s item type is nullable, then errors occurring during preparation or coercion of an individual item in the list must result in a the value null at that position in the list along with a field error added to the response. If a list’s item type is non-null, a field error occurring at an individual item in the list must result in a field error for the entire list.
-                 *
-                 * @see https://spec.graphql.org/draft/#sec-List.Result-Coercion
                  */
                 if (
                     ($inputFieldIsArrayType && $inputFieldIsNonNullArrayItemsType)
