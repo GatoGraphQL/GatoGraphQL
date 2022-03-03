28f5ec6630bf043e9d85c7dcbf4c6383028f9de5
Leonardo Losoviz <leo@getpop.org>
2022-03-03 11:17:09 +0800

    Removed getTranslationAPI

--- layers/CMSSchema/packages/user-state-mutations/src/MutationResolvers/ValidateUserLoggedInMutationResolverTrait.php
+++ layers/CMSSchema/packages/user-state-mutations/src/MutationResolvers/ValidateUserLoggedInMutationResolverTrait.php
@@ -6,13 +6,10 @@ namespace PoPCMSSchema\UserStateMutations\MutationResolvers;
 
 use PoP\ComponentModel\Feedback\FeedbackItemResolution;
 use PoP\Root\App;
-use PoP\Root\Translation\TranslationAPIInterface;
 use PoPCMSSchema\SchemaCommons\FeedbackItemProviders\MutationErrorFeedbackItemProvider;
 
 trait ValidateUserLoggedInMutationResolverTrait
 {
-    abstract protected function getTranslationAPI(): TranslationAPIInterface;
-
     /**
      * Check that the user is logged-in
      */
