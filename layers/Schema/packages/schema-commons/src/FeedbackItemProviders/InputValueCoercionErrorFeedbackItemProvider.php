0475714e3504868285060f01649785e9982170ed
Leonardo Losoviz <leo@getpop.org>
2022-03-04 16:55:56 +0800

    Moved FeedbackItems

--- layers/Schema/packages/schema-commons/src/FeedbackItemProviders/InputValueCoercionErrorFeedbackItemProvider.php
+++ layers/Schema/packages/schema-commons/src/FeedbackItemProviders/InputValueCoercionErrorFeedbackItemProvider.php
@@ -11,8 +11,6 @@ class InputValueCoercionErrorFeedbackItemProvider extends AbstractFeedbackItemPr
 {
     public const E1 = 'e1';
     public const E2 = 'e2';
-    public const E3 = 'e3';
-    public const E4 = 'e4';
 
     /**
      * @return string[]
@@ -22,18 +20,14 @@ class InputValueCoercionErrorFeedbackItemProvider extends AbstractFeedbackItemPr
         return [
             self::E1,
             self::E2,
-            self::E3,
-            self::E4,
         ];
     }
 
     public function getMessagePlaceholder(string $code): string
     {
         return match ($code) {
-            self::E1 => $this->__('Type \'%s\' must be provided with format \'%s\'', 'component-model'),
-            self::E2 => $this->__('Value \'%1$s\' for type \'%2$s\' is not valid (the only valid values are: \'%3$s\')', 'component-model'),
-            self::E3 => $this->__('The format for type \'%s\' is not right: it must be satisfied via regex /(\+{1}[0-9]{1,3}[0-9]{8,9})/', 'component-model'),
-            self::E4 => $this->__('The format for type \'%s\' is not right: it must be satisfied via regex /^{?[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}}?$/', 'component-model'),
+            self::E1 => $this->__('Type \'%s\' must be provided with format \'%s\'', 'schema-commons'),
+            self::E2 => $this->__('Value \'%1$s\' for type \'%2$s\' is not valid (the only valid values are: \'%3$s\')', 'schema-commons'),
             default => parent::getMessagePlaceholder($code),
         };
     }
