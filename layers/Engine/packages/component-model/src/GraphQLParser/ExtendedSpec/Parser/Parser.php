056a30a5091611164cf3f0ab4744588bf6bc9859
Leonardo Losoviz <leo@getpop.org>
2022-03-17 12:47:46 +0800

    Return instance of DynamicVariableReference

--- layers/Engine/packages/component-model/src/GraphQLParser/ExtendedSpec/Parser/Parser.php
+++ layers/Engine/packages/component-model/src/GraphQLParser/ExtendedSpec/Parser/Parser.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\GraphQLParser\ExtendedSpec\Parser;
 
+use PoP\ComponentModel\App;
 use PoP\ComponentModel\DirectiveResolvers\MetaDirectiveResolverInterface;
 use PoP\ComponentModel\Registries\MetaDirectiveRegistryInterface;
+use PoP\GraphQLParser\Component;
+use PoP\GraphQLParser\ComponentConfiguration;
 use PoP\GraphQLParser\ExtendedSpec\Parser\AbstractParser;
+use PoP\GraphQLParser\ExtendedSpec\Parser\Ast\ArgumentValue\DynamicVariableReference;
 use PoP\GraphQLParser\Spec\Parser\Ast\Argument;
+use PoP\GraphQLParser\Spec\Parser\Ast\ArgumentValue\Variable;
+use PoP\GraphQLParser\Spec\Parser\Ast\ArgumentValue\VariableReference;
 use PoP\GraphQLParser\Spec\Parser\Ast\Directive;
+use PoP\GraphQLParser\Spec\Parser\Location;
 
 class Parser extends AbstractParser
 {
@@ -55,5 +62,32 @@ class Parser extends AbstractParser
         /** @var MetaDirectiveResolverInterface */
         $metaDirectiveResolver = $this->getMetaDirectiveResolver($directive->getName());
         return $metaDirectiveResolver->getAffectDirectivesUnderPosArgumentDefaultValue();
+    }
+
+    final protected function isDynamicVariableReference(
+        string $name,
+        ?Variable $variable,
+    ): bool {
+        return $variable === null && \str_starts_with($this->name, '_');
+    }
+
+    protected function createVariableReference(
+        string $name,
+        ?Variable $variable,
+        Location $location,
+    ): VariableReference {
+        /** @var ComponentConfiguration */
+        $componentConfiguration = App::getComponent(Component::class)->getConfiguration();
+        if ($componentConfiguration->enableDynamicVariables()
+            && $this->isDynamicVariableReference($name, $variable)
+        ) {
+            return new DynamicVariableReference($name, $variable, $location);
+        }
+
+        return parent::createVariableReference(
+            $name,
+            $variable,
+            $location,
+        );
     }
 }
