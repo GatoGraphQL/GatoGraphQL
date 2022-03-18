5a3754b22dab6f480969a2f6e1329019fd8f9cab
Leonardo Losoviz <leo@getpop.org>
2022-03-18 08:37:22 +0800

    Added VariableReferenceInterface

---
+++ layers/Engine/packages/graphql-parser/src/Spec/Parser/Ast/ArgumentValue/VariableReferenceInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\GraphQLParser\Spec\Parser\Ast\ArgumentValue;
+
+use PoP\GraphQLParser\Spec\Parser\Ast\WithValueInterface;
+
+interface VariableReferenceInterface extends WithValueInterface
+{
+}
