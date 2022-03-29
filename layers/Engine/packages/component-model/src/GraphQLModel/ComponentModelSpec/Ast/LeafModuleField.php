c709a5e3964b45966881c95d6e19adc3ebcd2ac3
Leonardo Losoviz <leo@getpop.org>
2022-03-29 14:09:23 +0800

    Pass the query field to the constructor

--- layers/Engine/packages/component-model/src/GraphQLModel/ComponentModelSpec/Ast/LeafModuleField.php
+++ layers/Engine/packages/component-model/src/GraphQLModel/ComponentModelSpec/Ast/LeafModuleField.php
@@ -8,20 +8,36 @@ use PoP\ComponentModel\GraphQLModel\ExtendedSpec\Parser\Ast\LeafField;
 use PoP\GraphQLParser\Spec\Parser\Ast\Argument;
 use PoP\GraphQLParser\Spec\Parser\Ast\Directive;
 use PoP\GraphQLParser\StaticHelpers\LocationHelper;
+use PoP\GraphQLParser\Spec\Parser\Ast\LeafField as QueryLeafField;
 
 class LeafModuleField extends LeafField
 {
     /**
+     * If passing QueryLeafField, then retrieve all the properties from there.
+     * Otherwise, use the ones passed to the constructor.
+     *
      * @param Argument[] $arguments
      * @param Directive[] $directives
      */
     public function __construct(
         string $name,
+        ?QueryLeafField $leafField = null,
         ?string $alias = null,
         array $arguments = [],
         array $directives = [],
         bool $skipOutputIfNull = false,
     ) {
+        if ($leafField !== null) {
+            parent::__construct(
+                $leafField->getName(),
+                $leafField->getAlias(),
+                $leafField->getArguments(),
+                $leafField->getDirectives(),
+                $leafField->getLocation(),
+                false,
+            );
+            return;
+        }
         parent::__construct(
             $name,
             $alias,
--- layers/Engine/packages/component-model/src/GraphQLModel/ComponentModelSpec/Ast/RelationalModuleField.php
+++ layers/Engine/packages/component-model/src/GraphQLModel/ComponentModelSpec/Ast/RelationalModuleField.php
@@ -8,21 +8,38 @@ use PoP\ComponentModel\GraphQLModel\ExtendedSpec\Parser\Ast\LeafField;
 use PoP\GraphQLParser\Spec\Parser\Ast\Argument;
 use PoP\GraphQLParser\Spec\Parser\Ast\Directive;
 use PoP\GraphQLParser\StaticHelpers\LocationHelper;
+use PoP\GraphQLParser\Spec\Parser\Ast\RelationalField as QueryRelationalField;
 
 class RelationalModuleField extends LeafField
 {
     /**
+     * If passing QueryRelationalField, then retrieve all the properties from there.
+     * Otherwise, use the ones passed to the constructor.
+     *
      * @param Argument[] $arguments
      * @param Directive[] $directives
      */
     public function __construct(
         string $name,
         protected array $nestedModules,
+        ?QueryRelationalField $relationalField = null,
         ?string $alias = null,
         array $arguments = [],
         array $directives = [],
         bool $skipOutputIfNull = false,
     ) {
+        if ($relationalField !== null) {
+            parent::__construct(
+                $relationalField->getName(),
+                $relationalField->getAlias(),
+                $relationalField->getArguments(),
+                $relationalField->getDirectives(),
+                $relationalField->getLocation(),
+                false,
+            );
+            return;
+        }
+
         parent::__construct(
             $name,
             $alias,
