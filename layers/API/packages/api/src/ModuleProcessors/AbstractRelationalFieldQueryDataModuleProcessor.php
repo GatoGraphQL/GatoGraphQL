81cc65edbdf8734bce7b36ab50c82ec244fa26ec
Leonardo Losoviz <leo@getpop.org>
2022-03-30 10:33:52 +0800

    parse query

--- layers/API/packages/api/src/ModuleProcessors/AbstractRelationalFieldQueryDataModuleProcessor.php
+++ layers/API/packages/api/src/ModuleProcessors/AbstractRelationalFieldQueryDataModuleProcessor.php
@@ -8,6 +8,11 @@ use PoP\ComponentModel\App;
 use PoP\ComponentModel\GraphQLModel\ComponentModelSpec\Ast\LeafModuleField;
 use PoP\ComponentModel\GraphQLModel\ComponentModelSpec\Ast\RelationalModuleField;
 use PoP\ComponentModel\ModuleProcessors\AbstractQueryDataModuleProcessor;
+use PoP\GraphQLParser\Exception\Parser\InvalidRequestException;
+use PoP\GraphQLParser\Exception\Parser\SyntaxErrorException;
+use PoP\GraphQLParser\ExtendedSpec\Execution\ExecutableDocument;
+use PoP\GraphQLParser\ExtendedSpec\Parser\ParserInterface;
+use PoP\GraphQLParser\Spec\Execution\Context;
 use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
 use PoP\GraphQLParser\Spec\Parser\Ast\Fragment;
 use PoP\GraphQLParser\Spec\Parser\Ast\FragmentReference;
@@ -17,19 +22,62 @@ use PoP\GraphQLParser\Spec\Parser\Ast\RelationalField;
 
 abstract class AbstractRelationalFieldQueryDataModuleProcessor extends AbstractQueryDataModuleProcessor
 {
+    private ?ParserInterface $parser = null;
+    
+    final public function setParser(ParserInterface $parser): void
+    {
+        $this->parser = $parser;
+    }
+    final protected function getParser(): ParserInterface
+    {
+        return $this->parser ??= $this->instanceManager->getInstance(ParserInterface::class);
+    }
+
     /**
      * @return FieldInterface[]
      */
     protected function getFields(array $module, ?array $moduleAtts): array
     {
-        // If it is a virtual module, the fields are coded inside the virtual module atts
+        /**
+         * If it is a virtual module, the fields are coded
+         * inside the virtual module atts
+         */
         if ($moduleAtts !== null) {
             return $moduleAtts['fields'];
         }
 
         /**
+         * It is a normal module when calling the first time
+         * (i.e. for the fields at the root level).
+         *
+         * Parse the requested GraphQL query, and extract
+         * the root fields.
+         */
+        $query = App::getState('query');
+        if ($query === null || trim($query) === '') {
+            return [];
+        }
+
+        $variableValues = App::getState('variables');
+        $operationName = App::getState('graphql-operation-name');
+
+        try {
+            $document = $this->getParser()->parse($query);
+            $executableDocument = (
+                new ExecutableDocument(
+                    $document,
+                    new Context($operationName, $variableValues)
+                )
+            )->validateAndInitialize();
+        } catch (SyntaxErrorException | InvalidRequestException $e) {
+            // @todo Show GraphQL error in client
+            return [];
+        }
+
+        /**
          * If it is a normal module, it is the first added,
-         * then simply get the fields from the application state.
+         * then parse the requested GraphQL query, and fill
+         * its fields within the ensuing component model
          */
         return App::getState('executable-query') ?? [];
     }
