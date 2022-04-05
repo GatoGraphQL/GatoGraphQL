e38f2988e72d2a123849635cb352e5f9649e96a0
Leonardo Losoviz <leo@getpop.org>
2022-04-05 13:55:52 +0800

    Added IntrospectionQuery fixtures

---
+++ layers/GraphQLByPoP/packages/graphql-server/tests/Standalone/FixtureQueryExecutionGraphQLServerTestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLByPoP\GraphQLServer\Standalone;
+
+class FixtureQueryExecutionGraphQLServerTestCase extends AbstractFixtureQueryExecutionGraphQLServerTestCase
+{
+    
+}
