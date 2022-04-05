b54b5230341636ac4f209f3765d65f2b0fe40acb
Leonardo Losoviz <leo@getpop.org>
2022-04-05 13:51:53 +0800

    Re-did AbstractFixtureQueryExecutionGraphQLServerTestCase

--- layers/GraphQLByPoP/packages/graphql-server/tests/Standalone/AbstractFixtureQueryExecutionGraphQLServerTestCase.php
+++ layers/GraphQLByPoP/packages/graphql-server/tests/Standalone/AbstractFixtureQueryExecutionGraphQLServerTestCase.php
@@ -7,9 +7,17 @@ namespace GraphQLByPoP\GraphQLServer\Standalone;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
 
-abstract class AbstractFixtureQueryExecutionGraphQLServerTestCase extends AbstractQueryExecutionGraphQLServerTestCase
+abstract class AbstractFixtureQueryExecutionGraphQLServerTestCase extends AbstractGraphQLServerTestCase
 {
-    public function graphQLServerExecutionProvider(): array
+    /**
+     * @dataProvider fixtureGraphQLServerExecutionProvider
+     */
+    public function testFixtureGraphQLServerExecution(string $queryFile, string $expectedResponseFile, ?string $variablesFile = null, ?string $operationName = null): void
+    {
+        $this->assertFixtureGraphQLQueryExecution($queryFile, $expectedResponseFile, $variablesFile, $operationName);
+    }
+
+    public function fixtureGraphQLServerExecutionProvider(): array
     {
         $directory = $this->getFixtureFolder();
 
@@ -17,16 +25,20 @@ abstract class AbstractFixtureQueryExecutionGraphQLServerTestCase extends Abstra
          * Retrieve all files:
          *
          * - GraphQL queries: ending in ".gql" or ".graphql"
-         * - GraphQL responses: ending in ".json", but not ".var.json"
-         * - GraphQL variables: ending in ".var.json"
+         *
+         * Each of these files will need to have corresponding file(s)
+         * in the same folder, all of them sharing the same file name:
+         *
+         * - GraphQL response: ending in "${fileName}.json"
+         * - GraphQL variables: ending in "${fileName}.var.json"
          */
         $graphQLQueryFileNameFileInfos = $this->findFilesInDirectory($directory, ['*.gql', '*.graphql']);
-        $graphQLResponseFileNameFileInfos = $this->placeFileInfosUnderFileName(
-            $this->findFilesInDirectory($directory, ['*.json'], ['*.var.json'])
-        );
-        $graphQLVariableFileNameFileInfos = $this->placeFileInfosUnderFileName(
-            $this->findFilesInDirectory($directory, ['*.var.json'])
-        );
+        // $graphQLResponseFileNameFileInfos = $this->placeFileInfosUnderFileName(
+        //     $this->findFilesInDirectory($directory, ['*.json'], ['*.var.json'])
+        // );
+        // $graphQLVariableFileNameFileInfos = $this->placeFileInfosUnderFileName(
+        //     $this->findFilesInDirectory($directory, ['*.var.json'])
+        // );
 
         /**
          * Pair up these files among themselves. They must all share the
@@ -34,31 +46,39 @@ abstract class AbstractFixtureQueryExecutionGraphQLServerTestCase extends Abstra
          */
         $providerItems = [];
         foreach ($graphQLQueryFileNameFileInfos as $graphQLQueryFileInfo) {
-            $fileName = $graphQLQueryFileInfo->getRelativePathname();
-            /**
-             * The GraphQL response file is mandatory, but the variables one is optional
-             */
-            $graphQLResponseFileInfo = $graphQLResponseFileNameFileInfos[$fileName] ?? null;
-            if ($graphQLResponseFileInfo === null) {
-                // Stop here and mark this test as incomplete.
-                $this->markTestIncomplete(
-                    sprintf(
-                        'File "%s.json" (with the expected GraphQL response) is missing.',
-                        $fileName
-                    )
-                );
-                continue;
+            $fileName = $graphQLQueryFileInfo->getFilenameWithoutExtension();
+            $filePath = $graphQLQueryFileInfo->getPathname();
+            $graphQLQueryFile = $graphQLQueryFileInfo->getRealPath();
+            $graphQLResponseFile = $filePath . '/' . $fileName . '.json';
+            $graphQLVariablesFile = $filePath . '/' . $fileName . '.var.json';
+            if (\file_exists($graphQLVariablesFile)) {
+                $graphQLVariablesFile = null;
             }
-            $graphQLVariableFileInfo = $graphQLVariableFileNameFileInfos[$fileName] ?? null;
+            $providerItems[] = [$graphQLQueryFile, $graphQLResponseFile, $graphQLVariablesFile];
+            // /**
+            //  * The GraphQL response file is mandatory, but the variables one is optional
+            //  */
+            // $graphQLResponseFileInfo = $graphQLResponseFileNameFileInfos[$fileName] ?? null;
+            // if ($graphQLResponseFileInfo === null) {
+            //     // Stop here and mark this test as incomplete.
+            //     $this->markTestIncomplete(
+            //         sprintf(
+            //             'File "%s.json" (with the expected GraphQL response) is missing.',
+            //             $fileName
+            //         )
+            //     );
+            //     continue;
+            // }
+            // $graphQLVariableFileInfo = $graphQLVariableFileNameFileInfos[$fileName] ?? null;
 
-            /**
-             * We have identified all the files. Extract their content, and pass it upwards
-             * via the provider.
-             */
-            $graphQLQuery = file_get_contents($graphQLQueryFileInfo->getRealPath());
-            $graphQLResponse = file_get_contents($graphQLResponseFileInfo->getRealPath());
-            $graphQLVariables = $graphQLVariableFileInfo !== null ? file_get_contents($graphQLVariableFileInfo->getRealPath()) : [];
-            $providerItems[] = [$graphQLQuery, $graphQLResponse, $graphQLVariables];
+            // /**
+            //  * We have identified all the files. Extract their content, and pass it upwards
+            //  * via the provider.
+            //  */
+            // $graphQLQuery = file_get_contents($graphQLQueryFileInfo->getRealPath());
+            // $graphQLResponse = file_get_contents($graphQLResponseFileInfo->getRealPath());
+            // $graphQLVariables = $graphQLVariableFileInfo !== null ? file_get_contents($graphQLVariableFileInfo->getRealPath()) : [];
+            // $providerItems[] = [$graphQLQuery, $graphQLResponse, $graphQLVariables];
         }
         return $providerItems;
     }
@@ -109,17 +129,17 @@ abstract class AbstractFixtureQueryExecutionGraphQLServerTestCase extends Abstra
         return __DIR__ . '/Fixture';
     }
 
-    /**
-     * @param SplFileInfo[] $fileInfos
-     * @return array<string,SplFileInfo>
-     */
-    protected function placeFileInfosUnderFileName(array $fileInfos): array
-    {
-        $fileNameFileInfos = [];
-        foreach ($fileInfos as $fileInfo) {
-            $fileNameWithExtension = $fileInfo->getRelativePathname();
-            $fileNameFileInfos[$fileNameWithExtension] = $fileInfo;
-        }
-        return $fileNameFileInfos;
-    }
+    // /**
+    //  * @param SplFileInfo[] $fileInfos
+    //  * @return array<string,SplFileInfo>
+    //  */
+    // protected function placeFileInfosUnderFileName(array $fileInfos): array
+    // {
+    //     $fileNameFileInfos = [];
+    //     foreach ($fileInfos as $fileInfo) {
+    //         $fileNameWithExtension = $fileInfo->getRelativePathname();
+    //         $fileNameFileInfos[$fileNameWithExtension] = $fileInfo;
+    //     }
+    //     return $fileNameFileInfos;
+    // }
 }
