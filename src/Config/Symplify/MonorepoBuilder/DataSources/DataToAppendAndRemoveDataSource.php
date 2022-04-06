121a75e509e24edd6fbb0d8640b76e0212ce7751
Leonardo Losoviz <leo@getpop.org>
2022-04-06 20:02:34 +0800

    Moved require-dev from monorepo to package

--- src/Config/Symplify/MonorepoBuilder/DataSources/DataToAppendAndRemoveDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/DataToAppendAndRemoveDataSource.php
@@ -17,13 +17,6 @@ class DataToAppendAndRemoveDataSource
                 'symplify/monorepo-builder' => '^10.0',
                 'friendsofphp/php-cs-fixer' => '^3.5',
                 'slevomat/coding-standard' => '^7.0',
-                /**
-                 * BrainFaker has no releases yet, so point to dev-master
-                 * (under "leoloso/BrainFaker" repository, so it's under control)
-                 *
-                 * @see https://github.com/leoloso/BrainFaker
-                 */
-                'brain/faker' => 'dev-master',
             ],
             'autoload' => [
                 'psr-4' => [
@@ -34,16 +27,6 @@ class DataToAppendAndRemoveDataSource
                 [
                     'type' => 'vcs',
                     'url' => 'https://github.com/leoloso/monorepo-builder.git',
-                ],
-                [
-                    'type' => 'vcs',
-                    /**
-                     * Override `Brain-WP/BrainFaker` to use dependency "fakerphp/faker",
-                     * which works with PHP 8.
-                     *
-                     * @see https://github.com/leoloso/BrainFaker/commit/7747c4d684c44e94d94ea951372c2b62b7a7755d
-                     */
-                    'url' => 'https://github.com/leoloso/BrainFaker.git',
                 ],
             ],
             // 'extra' => [
