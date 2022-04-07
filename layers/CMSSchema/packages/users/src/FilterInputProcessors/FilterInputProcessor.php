b5ddbcef59ff912a7fc5e70a1e31dca8cbafc0e2
Leonardo Losoviz <leo@getpop.org>
2022-04-07 17:32:25 +0800

    UserBy: provide single "login" and "email" value (not array)

--- layers/CMSSchema/packages/users/src/FilterInputProcessors/FilterInputProcessor.php
+++ layers/CMSSchema/packages/users/src/FilterInputProcessors/FilterInputProcessor.php
@@ -9,14 +9,18 @@ use PoP\ComponentModel\FilterInputProcessors\AbstractFilterInputProcessor;
 class FilterInputProcessor extends AbstractFilterInputProcessor
 {
     public final const FILTERINPUT_NAME = 'filterinput-name';
+    public final const FILTERINPUT_USERNAME = 'filterinput-username';
     public final const FILTERINPUT_USERNAME_OR_USERNAMES = 'filterinput-username-or-usernames';
+    public final const FILTERINPUT_EMAIL = 'filterinput-email';
     public final const FILTERINPUT_EMAIL_OR_EMAILS = 'filterinput-email-or-emails';
 
     public function getFilterInputsToProcess(): array
     {
         return array(
             [self::class, self::FILTERINPUT_NAME],
+            [self::class, self::FILTERINPUT_USERNAME],
             [self::class, self::FILTERINPUT_USERNAME_OR_USERNAMES],
+            [self::class, self::FILTERINPUT_EMAIL],
             [self::class, self::FILTERINPUT_EMAIL_OR_EMAILS],
         );
     }
@@ -27,8 +31,14 @@ class FilterInputProcessor extends AbstractFilterInputProcessor
             case self::FILTERINPUT_NAME:
                 $query['name'] = $value;
                 break;
+            case self::FILTERINPUT_USERNAME:
+                $query['username'] = $value;
+                break;
             case self::FILTERINPUT_USERNAME_OR_USERNAMES:
                 $query['username'] = is_array($value) ? $value : [$value];
+                break;
+            case self::FILTERINPUT_EMAIL:
+                $query['emails'] = $value;
                 break;
             case self::FILTERINPUT_EMAIL_OR_EMAILS:
                 $query['emails'] = is_array($value) ? $value : [$value];
