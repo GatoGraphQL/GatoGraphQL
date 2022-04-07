05ce9ce92aadd06394d50155c2c0cf5fddbfa62a
Leonardo Losoviz <leo@getpop.org>
2022-04-07 12:53:47 +0800

    Added (empty) test

---
+++ layers/Backbone/packages/wp-data-parser/tests/ParserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPBackbone\WPDataParser;
+
+use PHPUnit\Framework\TestCase;
+
+class Parser extends TestCase
+{
+    public function testParser(): void
+    {
+
+    }
+}
