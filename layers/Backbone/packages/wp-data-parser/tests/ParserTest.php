b6015f908202553a373cdbf49bb4d73499bdaeed
Leonardo Losoviz <leo@getpop.org>
2022-04-07 12:58:26 +0800

    Added resources to test

--- layers/Backbone/packages/wp-data-parser/tests/ParserTest.php
+++ layers/Backbone/packages/wp-data-parser/tests/ParserTest.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 namespace PoPBackbone\WPDataParser;
 
 use PHPUnit\Framework\TestCase;
+use PoPBackbone\WPDataParser\Exception\ParserException;
 
 class Parser extends TestCase
 {
     public function testParser(): void
     {
+        $wpDataXMLExportFile = __DIR__ . '/sample-data.wordpress.xml';
+        $wpDataParser = new WPDataParser();
+        $parsedData = $wpDataParser->parse($wpDataXMLExportFile);
+        $this->assertEquals(
+            [
+                
+            ],
+            $parsedData
+        );
+    }
 
+    public function testInvalidFile(): void
+    {
+        $this->expectException(ParserException::class);
+        $wpDataXMLExportFile = __DIR__ . '/Resources/invalid.wordpress.xml';
+        $wpDataParser = new WPDataParser();
+        $parsedData = $wpDataParser->parse($wpDataXMLExportFile);
     }
 }
