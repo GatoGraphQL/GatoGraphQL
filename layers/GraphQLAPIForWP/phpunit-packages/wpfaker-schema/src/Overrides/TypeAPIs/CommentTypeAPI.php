81fd41b5ac6009c33da471d2e7af8cfc74bcb9c0
Leonardo Losoviz <leo@getpop.org>
2022-04-08 12:17:30 +0800

    Added services

--- layers/GraphQLAPIForWP/phpunit-packages/wpfaker-schema/src/Overrides/TypeAPIs/CommentTypeAPI.php
+++ layers/GraphQLAPIForWP/phpunit-packages/wpfaker-schema/src/Overrides/TypeAPIs/CommentTypeAPI.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\WPFakerSchema\Overrides\TypeAPIs;
 
+use GraphQLAPI\WPFakerSchema\DataProvider\DataProviderInterface;
 use PoPCMSSchema\CommentsWP\TypeAPIs\CommentTypeAPI as UpstreamCommentTypeAPI;
 
 class CommentTypeAPI extends UpstreamCommentTypeAPI
 {
+    use TypeAPITrait;
+
+    private ?DataProviderInterface $dataProvider = null;
+
+    final public function setDataProvider(DataProviderInterface $dataProvider): void
+    {
+        $this->dataProvider = $dataProvider;
+    }
+    final protected function getDataProvider(): DataProviderInterface
+    {
+        return $this->dataProvider ??= $this->instanceManager->getInstance(DataProviderInterface::class);
+    }
 }
