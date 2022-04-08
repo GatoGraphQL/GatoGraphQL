a2813a3624e478a45279e3412b9d2a09b2548304
Leonardo Losoviz <leo@getpop.org>
2022-04-08 08:31:19 +0800

    Added (empty) service

---
+++ layers/GraphQLAPIForWP/phpunit-packages/wpfaker-schema/src/Overrides/TypeAPIs/PostTypeAPI.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\WPFakerSchema\Overrides\TypeAPIs;
+
+use PoPCMSSchema\PostsWP\TypeAPIs\PostTypeAPI as UpstreamPostTypeAPI;
+
+class PostTypeAPI extends UpstreamPostTypeAPI
+{
+    
+}
