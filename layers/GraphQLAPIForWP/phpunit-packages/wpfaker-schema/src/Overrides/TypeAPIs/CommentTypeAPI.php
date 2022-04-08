ba6fbf14b60cee01a3685b7f0cfec49111f6fd1d
Leonardo Losoviz <leo@getpop.org>
2022-04-08 12:17:07 +0800

    Created (empty) service

---
+++ layers/GraphQLAPIForWP/phpunit-packages/wpfaker-schema/src/Overrides/TypeAPIs/CommentTypeAPI.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\WPFakerSchema\Overrides\TypeAPIs;
+
+use PoPCMSSchema\CommentsWP\TypeAPIs\CommentTypeAPI as UpstreamCommentTypeAPI;
+
+class CommentTypeAPI extends UpstreamCommentTypeAPI
+{
+}
