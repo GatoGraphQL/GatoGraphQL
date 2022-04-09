2e3dddf886e261b3c24b9e3c4882a5ee2b95c1dc
Leonardo Losoviz <leo@getpop.org>
2022-04-09 16:45:48 +0800

    Completed MockDataStore

--- layers/GraphQLAPIForWP/phpunit-packages/wpfaker-schema/src/State/MockDataStore.php
+++ layers/GraphQLAPIForWP/phpunit-packages/wpfaker-schema/src/State/MockDataStore.php
@@ -28,63 +28,64 @@ class MockDataStore
         $this->faker = faker();
         // @phpstan-ignore-next-line
         $this->wpFaker = $this->faker->wp();
-        $this->initializeFakePostDataEntries($files);
+        foreach ($files as $file) {
+            $this->mergeDataFromFile($file);
+        }
         $this->seedFakeData($options);
     }
 
-    protected function seedFakeData(array $options): void
+    /**
+     * Read the file, and extract its data.
+     *
+     * The file can be either:
+     *
+     * - a PHP file with the array containing the data
+     * - an XML WordPress data export file     
+     */
+    protected function mergeDataFromFile(string $file): void
     {
-        foreach (($this->data['posts'] ?? []) as $fakePostDataEntry) {
-            $this->wpFaker->post(['id' => $fakePostDataEntry['post_id'], ...$fakePostDataEntry]);
+        $isXML = str_ends_with($file, '.xml');
+        /**
+         * Validate all files are either XML or PHP,
+         * or throw an Exception otherwise
+         */
+        if (!($isXML || str_ends_with($file, '.php'))) {
+            throw new DatasetFileException(
+                sprintf(
+                    // $this->__(
+                        'The fixed dataset must be either a PHP or XML file, but file "%s" was provided',
+                    //     'wpfaker-schema'
+                    // ),
+                    $file
+                )
+            );
         }
+        /**
+         * Retrieve the WordPress data from the source file
+         */
+        $fileData = $isXML ? (new WPDataParser())->parse($file) : require $file;
+        if (!is_array($fileData)) {
+            throw new DatasetFileException(
+                sprintf(
+                    // $this->__(
+                        'File "%s" does not contain a valid dataset',
+                    //     'wpfaker-schema'
+                    // ),
+                    $file
+                )
+            );
+        }
+        $this->mergeData($fileData);
     }
 
     /**
-     * @param string[] $files
+     * Merge the datasets from different files.
+     *
+     * @param array<string,mixed> $data
      */
-    protected function initializeFakePostDataEntries(array $files): void
-    {
-        foreach ($files as $file) {
-            $isXML = str_ends_with($file, '.xml');
-            /**
-             * Validate all files are either XML or PHP,
-             * or throw an Exception otherwise
-             */
-            if (!($isXML || str_ends_with($file, '.php'))) {
-                throw new DatasetFileException(
-                    sprintf(
-                        // $this->__(
-                            'The fixed dataset must be either a PHP or XML file, but file "%s" was provided',
-                        //     'wpfaker-schema'
-                        // ),
-                        $file
-                    )
-                );
-            }
-            /**
-             * Retrieve the WordPress data from the source file
-             */
-            $fileData = $isXML ? (new WPDataParser())->parse($file) : require $file;
-            if (!is_array($fileData)) {
-                throw new DatasetFileException(
-                    sprintf(
-                        // $this->__(
-                            'File "%s" does not contain a valid dataset',
-                        //     'wpfaker-schema'
-                        // ),
-                        $file
-                    )
-                );
-            }
-            $this->mergeDataset($fileData);
-        }
-    }
-
-    protected function mergeDataset(array $data): void
+    protected function mergeData(array $data): void
     {
         /**
-         * Merge the datasets together.
-         *
          * Use `array_merge` instead of `array_merge_recursive`
          * as to have downstream datasets add more data, not
          * replace the one from upstream sources.
@@ -109,6 +110,28 @@ class MockDataStore
                 continue;
             }
             $this->data[$entityType] = $entityData;
+        }
+    }
+
+    /**
+     * Inject the dataset into BrainFaker
+     *
+     * @param array<string,mixed> $options
+     * @see https://github.com/Brain-WP/BrainFaker#what-is-mocked
+     */
+    protected function seedFakeData(array $options): void
+    {
+        $postDataEntries = ($this->data['posts'] ?? []);
+        if ($limitPosts = $options['limit-posts'] ?? 0) {
+            array_splice($postDataEntries, 0, $limitPosts);
+        }
+        foreach ($postDataEntries as $postDataEntry) {
+            $this->wpFaker->post(
+                [
+                    'id' => $postDataEntry['post_id'],
+                    ...$postDataEntry
+                ]
+            );
         }
     }
 }
