6378461e627d1b21cd9995402774e2118f162244
Leonardo Losoviz <leo@getpop.org>
2022-04-11 17:04:22 +0800

    Completed test

--- layers/Engine/packages/component-model/tests/ExtendedSpec/Execution/ExecutableDocumentTest.php
+++ layers/Engine/packages/component-model/tests/ExtendedSpec/Execution/ExecutableDocumentTest.php
@@ -6,9 +6,12 @@ namespace PoP\ComponentModel\ExtendedSpec\Execution;
 
 use PoP\ComponentModel\ExtendedSpec\Execution\ExecutableDocument;
 use PoP\ComponentModel\Upstream\GraphQLParser\ExtendedSpec\Execution\ExecutableDocumentTest as UpstreamExecutableDocumentTest;
+use PoP\GraphQLParser\Exception\Parser\InvalidRequestException;
+use PoP\GraphQLParser\FeedbackItemProviders\GraphQLSpecErrorFeedbackItemProvider;
 use PoP\GraphQLParser\Spec\Execution\Context;
 use PoP\GraphQLParser\Spec\Execution\ExecutableDocumentInterface;
 use PoP\GraphQLParser\Spec\Parser\Ast\Document;
+use PoP\Root\Feedback\FeedbackItemResolution;
 
 class ExecutableDocumentTest extends UpstreamExecutableDocumentTest
 {
@@ -17,5 +20,78 @@ class ExecutableDocumentTest extends UpstreamExecutableDocumentTest
       Context $context,
     ): ExecutableDocumentInterface {
         return new ExecutableDocument($document, $context);
+    }
+
+    public function testExistingTypeFragmentSpread()
+    {
+        $parser = $this->getParser();
+        $document = $parser->parse(
+            <<<GRAPHQL
+            {
+                customPosts {
+                    __typename
+                    ...PostData
+                }
+            }
+            
+            fragment PostData on Post {
+                id
+                title
+            }
+            GRAPHQL
+        );
+        $context = new Context();
+        $executableDocument = $this->createExecutableDocument($document, $context);
+        $executableDocument->validateAndInitialize();
+        $this->assertTrue(true);
+    }
+
+    public function testExistingInterfaceFragmentSpread()
+    {
+        $parser = $this->getParser();
+        $document = $parser->parse(
+            <<<GRAPHQL
+            {
+                customPosts {
+                    __typename
+                    ...PostData
+                }
+            }
+            
+            fragment PostData on IsCustomPost {
+                id
+                title
+            }
+            GRAPHQL
+        );
+        $context = new Context();
+        $executableDocument = $this->createExecutableDocument($document, $context);
+        $executableDocument->validateAndInitialize();
+        $this->assertTrue(true);
+    }
+
+    public function testNonExistingTypeOrInterfaceFragmentSpread()
+    {
+        $this->expectException(InvalidRequestException::class);
+        $this->expectExceptionMessage((new FeedbackItemResolution(GraphQLSpecErrorFeedbackItemProvider::class, GraphQLSpecErrorFeedbackItemProvider::E_5_5_1_2))->getMessage());
+        $parser = $this->getParser();
+        $document = $parser->parse(
+            <<<GRAPHQL
+            {
+                customPosts {
+                    __typename
+                    ...PostData
+                }
+            }
+            
+            fragment PostData on ThisTypeDoesNotExist {
+                id
+                title
+            }
+            GRAPHQL
+        );
+        $context = new Context();
+        $executableDocument = $this->createExecutableDocument($document, $context);
+        $executableDocument->validateAndInitialize();
     }
 }
