585a3c88a0021b2441e9d9f56ac500f565adab2e
Leonardo Losoviz <leo@getpop.org>
2022-04-12 15:39:14 +0800

    use function

--- layers/CMSSchema/packages/user-state-wp/src/TypeAPIs/UserStateTypeAPI.php
+++ layers/CMSSchema/packages/user-state-wp/src/TypeAPIs/UserStateTypeAPI.php
@@ -6,18 +6,22 @@ namespace PoPCMSSchema\UserStateWP\TypeAPIs;
 
 use PoPCMSSchema\UserState\TypeAPIs\UserStateTypeAPIInterface;
 
+use function get_current_user_id;
+use function is_user_logged_in;
+use function wp_get_current_user;
+
 class UserStateTypeAPI implements UserStateTypeAPIInterface
 {
     public function isUserLoggedIn(): bool
     {
-        return \is_user_logged_in();
+        return is_user_logged_in();
     }
     public function getCurrentUser(): ?object
     {
-        return \wp_get_current_user();
+        return wp_get_current_user();
     }
     public function getCurrentUserID(): string|int|null
     {
-        return \get_current_user_id();
+        return get_current_user_id();
     }
 }
