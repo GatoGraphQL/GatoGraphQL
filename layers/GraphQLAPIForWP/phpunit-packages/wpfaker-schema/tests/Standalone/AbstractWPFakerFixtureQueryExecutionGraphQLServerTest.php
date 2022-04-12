7cb6c4fc30482ea83bb3e27aa08d0e64a027a36d
Leonardo Losoviz <leo@getpop.org>
2022-04-12 14:58:42 +0800

    Load UserStateMutationsWP

--- layers/GraphQLAPIForWP/phpunit-packages/wpfaker-schema/tests/Standalone/AbstractWPFakerFixtureQueryExecutionGraphQLServerTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/wpfaker-schema/tests/Standalone/AbstractWPFakerFixtureQueryExecutionGraphQLServerTest.php
@@ -22,10 +22,9 @@ abstract class AbstractWPFakerFixtureQueryExecutionGraphQLServerTest extends Abs
                 \GraphQLAPI\WPFakerSchema\Component::class,
                 \PoPCMSSchema\PostCategoriesWP\Component::class,
                 \PoPCMSSchema\PostTagsWP\Component::class,
-                \PoPWPSchema\Users\Component::class,
-                \PoPWPSchema\Posts\Component::class,
                 \PoPWPSchema\Pages\Component::class,
                 \PoPCMSSchema\CommentMutationsWP\Component::class,
+                \PoPCMSSchema\UserStateMutationsWP\Component::class,
             ]
         ];
     }
