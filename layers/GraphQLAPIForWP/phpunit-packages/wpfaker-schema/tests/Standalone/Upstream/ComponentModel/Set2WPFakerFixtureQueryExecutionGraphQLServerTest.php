71a8e690ada6972542479839389ff906394bb8c9
Leonardo Losoviz <leo@getpop.org>
2022-04-12 14:34:49 +0800

    Test nested mutations

---
+++ layers/GraphQLAPIForWP/phpunit-packages/wpfaker-schema/tests/Standalone/Upstream/ComponentModel/Set2WPFakerFixtureQueryExecutionGraphQLServerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\WPFakerSchema\Standalone\Upstream\ComponentModel;
+
+use GraphQLAPI\WPFakerSchema\Standalone\AbstractWPFakerFixtureQueryExecutionGraphQLServerTest;
+
+class Set2WPFakerFixtureQueryExecutionGraphQLServerTest extends AbstractWPFakerFixtureQueryExecutionGraphQLServerTest
+{
+    /**
+     * Directory under the fixture files are placed
+     */
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/FixtureSet2';
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    protected static function getGraphQLServerComponentClassConfiguration(): array
+    {
+        return [
+            ...parent::getGraphQLServerComponentClassConfiguration(),
+            ...[
+                \PoP\ComponentModel\Component::class => [
+                    \PoP\ComponentModel\Environment::CONVERT_INPUT_VALUE_FROM_SINGLE_TO_LIST => true,
+                ],
+                \PoPCMSSchema\CommentMutations\Component::class => [
+                    \PoPCMSSchema\CommentMutations\Environment::MUST_USER_BE_LOGGED_IN_TO_ADD_COMMENT => false,
+                ],
+                \GraphQLByPoP\GraphQLServer\Component::class => [
+                    \GraphQLByPoP\GraphQLServer\Environment::ENABLE_NESTED_MUTATIONS => true,
+                ],
+            ]
+        ];
+    }
+}
