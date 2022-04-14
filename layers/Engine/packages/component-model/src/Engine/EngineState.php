d9e9496443dd3f1d68a6cfe9b19804e22bcf7817
Leonardo Losoviz <leo@getpop.org>
2022-04-14 17:12:58 +0800

    Moved resolvedValuesCache to EngineState

--- layers/Engine/packages/component-model/src/Engine/EngineState.php
+++ layers/Engine/packages/component-model/src/Engine/EngineState.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Engine;
 
+use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
+
 class EngineState
 {
     public function __construct(
@@ -54,6 +56,48 @@ class EngineState
          * @var array<string,array<string,mixed>>
          */
         public array $relationalTypeOutputDBKeyIDsDataFields = [],
+
+        /**
+         * After executing `resolveValue` on the Object/UnionTypeResolver,
+         * store the results to re-use for subsequent calls for same object/field.
+         *
+         * This is mandatory due to the "Resolved Field Variable References",
+         * which re-create the same field in the AST.
+         *
+         * For instance, in this query, the `id` field is created twice in the AST:
+         *
+         * ```graphql
+         * {
+         *   id
+         *   echo(value: $_id)
+         * }
+         * ```
+         *
+         * But the 2nd AST must not be recalculated.
+         *
+         * @todo Incorporate with AST to compare against the Field->getLocation(), to make sure 2 fields are indeed the same
+         * @todo Check if caching by $objectID and $field is enough; $variables? $options? $feedbackStore?
+         * @todo Check how this plays out for mutations; should they be executed more than once? If so, when/how?
+         *
+         * @var array<string|id,array<string,mixed>> Multidimensional array of [$relationalTypeResolverNamespacedName][$objectID][$field] => $value
+         */
+        protected array $relationalTypeResolvedValuesCache = [],
     ) {
+    }
+
+    public function hasRelationalTypeResolvedValue(
+        RelationalTypeResolverInterface $relationalTypeResolver,
+        string|int $objectID,
+        string $field
+    ): bool {
+        return array_key_exists($field, $this->relationalTypeResolvedValuesCache[$relationalTypeResolver->getNamespacedTypeName()][$objectID] ?? []);
+    }
+
+    public function getRelationalTypeResolvedValue(
+        RelationalTypeResolverInterface $relationalTypeResolver,
+        string|int $objectID,
+        string $field
+    ): mixed {
+        return $this->relationalTypeResolvedValuesCache[$relationalTypeResolver->getNamespacedTypeName()][$objectID][$field];
     }
 }
