8c9bf36d04c8e3dbff755fc0dc0d17f19544679d
Leonardo Losoviz <leo@getpop.org>
2022-04-15 17:07:36 +0800

    LocalhostWordPressAuthenticatedUserWebserverRequestTest

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/WebserverRequests/LocalhostWordPressAuthenticatedUserWebserverRequestTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\WebserverRequests;
+
+use PHPUnitForGraphQLAPI\WebserverRequests\AbstractWebserverRequestTestCase;
+use PHPUnitForGraphQLAPI\WebserverRequests\WordPressAuthenticatedUserWebserverRequestTestCaseTrait;
+
+class LocalhostWordPressAuthenticatedUserWebserverRequestTest extends AbstractWebserverRequestTestCase
+{
+    use LocalhostWebserverRequestTestTrait;
+    use WordPressAuthenticatedUserWebserverRequestTestCaseTrait;
+
+    /**
+     * @return array<string,array<mixed>>
+     */
+    protected function provideEndpointEntries(): array
+    {
+        return [
+            'admin-client' => [
+                <<<JSON
+                {
+                    "data": {
+                        "id": "root"
+                    }
+                }
+                JSON,
+                'wp-admin/edit.php?page=graphql_api&action=execute_query',
+                <<<GRAPHQL
+                {
+                    id
+                }
+                GRAPHQL,
+            ],
+        ];
+    }
+}
