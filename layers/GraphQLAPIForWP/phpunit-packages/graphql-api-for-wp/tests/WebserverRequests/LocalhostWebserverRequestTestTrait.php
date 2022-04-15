a74351161231231924512dd1a6bbc428e0a977e3
Leonardo Losoviz <leo@getpop.org>
2022-04-15 15:29:12 +0800

    Converted class to trait

--- layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/WebserverRequests/LocalhostWebserverRequestTestTrait.php
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/WebserverRequests/LocalhostWebserverRequestTestTrait.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace PHPUnitForGraphQLAPI\GraphQLAPI\WebserverRequests;
 
-use PHPUnitForGraphQLAPI\WebserverRequests\AbstractWebserverRequestTestCase;
-
-abstract class AbstractLocalhostWebserverRequestTest extends AbstractWebserverRequestTestCase
+trait LocalhostWebserverRequestTestTrait
 {
     protected static function getWebserverDomain(): string
     {
