fad7f3c755dca80ab94af6972b2a06070f11d39e
Leonardo Losoviz <leo@getpop.org>
2022-04-17 13:44:33 +0800

    Added env vars

---
+++ layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/src/ComponentConfiguration.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\WebserverRequests;
+
+use PoP\Root\Component\AbstractComponentConfiguration;
+
+class ComponentConfiguration extends AbstractComponentConfiguration
+{
+    public function getIntegrationTestsWebserverDomain(): ?string
+    {
+        $envVariable = Environment::INTEGRATION_TESTS_WEBSERVER_DOMAIN;
+        $defaultValue = null;
+
+        return $this->retrieveConfigurationValueOrUseDefault(
+            $envVariable,
+            $defaultValue,
+        );
+    }
+
+    public function getIntegrationTestsWebserverAuthenticatedUserUsername(): array
+    {
+        $envVariable = Environment::INTEGRATION_TESTS_AUTHENTICATED_USER_USERNAME;
+        $defaultValue = null;
+
+        return $this->retrieveConfigurationValueOrUseDefault(
+            $envVariable,
+            $defaultValue,
+        );
+    }
+
+    public function getIntegrationTestsWebserverAuthenticatedUserPassword(): array
+    {
+        $envVariable = Environment::INTEGRATION_TESTS_AUTHENTICATED_USER_PASSWORD;
+        $defaultValue = null;
+
+        return $this->retrieveConfigurationValueOrUseDefault(
+            $envVariable,
+            $defaultValue,
+        );
+    }
+}
