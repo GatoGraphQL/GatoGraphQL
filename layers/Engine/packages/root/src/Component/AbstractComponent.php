4e0b225f9286671d3a8e564628482e63546961ee
Leonardo Losoviz <leo@getpop.org>
2022-04-18 16:11:32 +0800

    Added `setEnabled`

--- layers/Engine/packages/root/src/Component/AbstractComponent.php
+++ layers/Engine/packages/root/src/Component/AbstractComponent.php
@@ -175,19 +175,24 @@ abstract class AbstractComponent implements ComponentInterface
     public function isEnabled(): bool
     {
         if ($this->enabled === null) {
-            // If any dependency is disabled, then disable this component too
-            if ($this->onlyEnableIfAllDependenciesAreEnabled()) {
-                foreach ($this->getDependedComponentClasses() as $dependedComponentClass) {
-                    $dependedComponent = App::getComponent($dependedComponentClass);
-                    if (!$dependedComponent->isEnabled()) {
-                        $this->enabled = false;
-                        return $this->enabled;
-                    }
-                }
-            }
-            $this->enabled = $this->resolveEnabled();
+            $this->setEnabled();
         }
         return $this->enabled;
+    }
+
+    private function setEnabled(): void
+    {
+        // If any dependency is disabled, then disable this component too
+        if ($this->onlyEnableIfAllDependenciesAreEnabled()) {
+            foreach ($this->getDependedComponentClasses() as $dependedComponentClass) {
+                $dependedComponent = App::getComponent($dependedComponentClass);
+                if (!$dependedComponent->isEnabled()) {
+                    $this->enabled = false;
+                    return;
+                }
+            }
+        }
+        $this->enabled = $this->resolveEnabled();
     }
 
     public function onlyEnableIfAllDependenciesAreEnabled(): bool
