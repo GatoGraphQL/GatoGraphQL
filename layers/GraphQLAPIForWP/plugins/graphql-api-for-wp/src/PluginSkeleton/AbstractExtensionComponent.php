d85317e56d515feb06f351b75b1e91eafca68a29
Leonardo Losoviz <leo@getpop.org>
2022-04-18 16:08:02 +0800

    do not onlyEnableIfAllDependenciesAreEnabled for extensions

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginSkeleton/AbstractExtensionComponent.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginSkeleton/AbstractExtensionComponent.php
@@ -6,4 +6,12 @@ namespace GraphQLAPI\GraphQLAPI\PluginSkeleton;
 
 abstract class AbstractExtensionComponent extends AbstractPluginComponent
 {
+    /**
+     * Do not disable the plugin if one of its required
+     * 3rd parties is not installed/activated.
+     */
+    public function onlyEnableIfAllDependenciesAreEnabled(): bool
+    {
+        return false;
+    }
 }
