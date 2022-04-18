a5980643d2fd62edc62057f2cd13b46389dc0920
Leonardo Losoviz <leo@getpop.org>
2022-04-18 16:38:09 +0800

    Added property

--- layers/Engine/packages/root/src/Component/AbstractComponent.php
+++ layers/Engine/packages/root/src/Component/AbstractComponent.php
@@ -11,6 +11,15 @@ abstract class AbstractComponent implements ComponentInterface
 {
     use InitializeContainerServicesInComponentTrait;
 
+    /**
+     * Indicate if there is some other component that satisfies
+     * the contracts by this component.
+     *
+     * For instance, the packages under CMSSchema have generic contracts
+     * for any CMS, that require to be satisfied for some specific CMS
+     * (such as WordPress).
+     */
+    private bool $hasSatisfyingComponent = false;
     private ?bool $enabled = null;
     protected ?ComponentConfigurationInterface $componentConfiguration = null;
     protected ?ComponentInfoInterface $componentInfo = null;
@@ -68,7 +77,7 @@ abstract class AbstractComponent implements ComponentInterface
     }
 
     /**
-     * Indicate if this components requires some other component
+     * Indicate if this component requires some other component
      * to satisfy its contracts.
      *
      * For instance, the packages under CMSSchema have generic contracts
