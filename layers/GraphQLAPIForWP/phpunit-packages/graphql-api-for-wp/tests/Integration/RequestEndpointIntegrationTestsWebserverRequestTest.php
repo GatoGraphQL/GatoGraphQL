eb9f48e9b7e3b610d10d6069ee58b3fae3a1d78e
Leonardo Losoviz <leo@getpop.org>
2022-04-19 16:28:25 +0800

    RequestEndpointIntegrationTestsWebserverRequestTest

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/RequestEndpointIntegrationTestsWebserverRequestTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use PHPUnitForGraphQLAPI\WebserverRequests\AbstractWebserverRequestTestCase;
+
+class RequestEndpointIntegrationTestsWebserverRequestTest extends AbstractWebserverRequestTestCase
+{
+    /**
+     * Execute a GraphQL query against each of the endpoints
+     * @return array<string,array<mixed>>
+     */
+    protected function provideEndpointEntries(): array
+    {
+        $query = $this->getGraphQLQuery();
+        $expectedResponseBody = $this->getGraphQLExpectedResponse();
+        $endpoints = [
+            'custom-endpoint' => 'graphql/mobile-app/',
+        ];
+        $entries = [];
+        foreach ($endpoints as $dataName => $endpoint) {
+            $entries[$dataName] = [
+                'application/json',
+                $expectedResponseBody,
+                $endpoint,
+                [],
+                $query,
+            ];
+        }
+        return $entries;
+    }
+
+    protected function getGraphQLQuery(): string
+    {
+        return <<<GRAPHQL
+            query MyQuery(\$limit: Int = 3) {
+                posts(pagination: {
+                    limit: \$limit
+                }) {
+                    date
+                    excerpt
+                    url
+                    title
+                    tags {
+                        name
+                    }
+                    author {
+                        displayName
+                        url
+                        id
+                    }
+                }
+            }        
+        GRAPHQL;
+    }
+
+    protected function getGraphQLExpectedResponse(): string
+    {
+        return <<<JSON
+            {
+                "data": {
+                    "posts": [
+                        {
+                            "date": "2022-04-17T13:06:58+00:00",
+                            "excerpt": "Welcome to WordPress. This is your first post. Edit or delete it, then start writing!",
+                            "url": "http://graphql-api.lndo.site/hello-world/",
+                            "title": "Hello world!",
+                            "tags": [],
+                            "author": {
+                                "displayName": "admin",
+                                "url": "http://graphql-api.lndo.site/author/admin/",
+                                "id": 1
+                            }
+                        },
+                        {
+                            "date": "2020-12-12T04:08:47+00:00",
+                            "excerpt": "Categories Block Latest Posts Block",
+                            "url": "http://graphql-api.lndo.site/http-caching-improves-performance/",
+                            "title": "HTTP caching improves performance",
+                            "tags": [],
+                            "author": {
+                                "displayName": "admin",
+                                "url": "http://graphql-api.lndo.site/author/admin/",
+                                "id": 1
+                            }
+                        },
+                        {
+                            "date": "2020-12-12T04:06:52+00:00",
+                            "excerpt": "Verse Block Write poetry and other literary expressions honoring all spaces and line-breaks. Table Block Row 1 Column 1 Row 1 Column 2 Row 2 Column 1 Row 2 Column 2 Row 3 Column 1 Row 3 Column 2 Separator Block Spacer Block",
+                            "url": "http://graphql-api.lndo.site/public-or-private-api-mode-for-extra-security/",
+                            "title": "Public or Private API mode, for extra security",
+                            "tags": [
+                                {
+                                    "name": "security"
+                                },
+                                {
+                                    "name": "features"
+                                }
+                            ],
+                            "author": {
+                                "displayName": "admin",
+                                "url": "http://graphql-api.lndo.site/author/admin/",
+                                "id": 1
+                            }
+                        }
+                    ]
+                }
+            }
+        JSON;
+    }
+}
