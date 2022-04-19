0961e70761f55b4ca9767d75e1a30734c41c4cdc
Leonardo Losoviz <leo@getpop.org>
2022-04-19 14:55:20 +0800

    Added tests against disabled endpoints

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/DisabledEndpointIntegrationTestsWebserverRequestTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use PHPUnitForGraphQLAPI\WebserverRequests\AbstractWebserverRequestTestCase;
+
+class DisabledEndpointIntegrationTestsWebserverRequestTest extends AbstractWebserverRequestTestCase
+{
+    /**
+     * Assert that the endpoints are disabled,
+     * hence they return HTML, not JSON
+     *
+     * @return array<string,array<mixed>>
+     */
+    protected function provideEndpointEntries(): array
+    {
+        $query = <<<GRAPHQL
+            query {
+                id
+            }        
+        GRAPHQL;
+        return [
+            'persisted-query' => [
+                'text/html; charset=UTF-8',
+                null,
+                'graphql-query/website/',
+                [],
+                $query,
+            ],
+        ];
+    }
+}
