383cb5162720013bf4c7046dea2b71a8419e9e8d
Leonardo Losoviz <leo@getpop.org>
2022-04-20 12:57:46 +0800

    Created hierarchy

---
+++ layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/tests/AbstractCodeThirdPartyPluginDependencyWordPressAuthenticatedUserWebserverRequestTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\WebserverRequests;
+
+use PoP\Root\Exception\ShouldNotHappenException;
+
+/**
+ * Test that enabling/disabling a required 3rd-party plugin works well.
+ */
+abstract class AbstractCodeThirdPartyPluginDependencyWordPressAuthenticatedUserWebserverRequestTest extends AbstractThirdPartyPluginDependencyWordPressAuthenticatedUserWebserverRequestTest
+{
+    /**
+     * @return array<string,array<string>> An array of [$pluginName => ['query' => "...", 'response-enabled' => "...", 'response-disabled' => "..."]]
+     */
+    protected function getPluginNameEntries(): array
+    {
+        $pluginEntries = [];
+        foreach ($this->getPluginNames() as $pluginName) {
+            $pluginEntries[$pluginName] = [
+                'query' => $this->getPluginGraphQLQuery($pluginName),
+                'response-enabled' => $this->getPluginEnabledExpectedGraphQLResponse($pluginName),
+                'response-disabled' => $this->getPluginDisabledExpectedGraphQLResponse($pluginName),
+            ];
+        }
+        return $pluginEntries;
+    }
+
+    /**
+     * @return string[]
+     */
+    abstract protected function getPluginNames(): array;
+
+    protected function getPluginGraphQLQuery(string $pluginName): string
+    {
+        $this->throwUnsupportedPluginName($pluginName);
+    }
+
+    protected function getPluginEnabledExpectedGraphQLResponse(string $pluginName): string
+    {
+        $this->throwUnsupportedPluginName($pluginName);
+    }
+
+    protected function getPluginDisabledExpectedGraphQLResponse(string $pluginName): string
+    {
+        $this->throwUnsupportedPluginName($pluginName);
+    }    
+
+    protected function throwUnsupportedPluginName(string $pluginName): never
+    {
+        throw new ShouldNotHappenException(
+            sprintf(
+                'Configuration for plugin "%s" is not complete',
+                $pluginName
+            )
+        );
+    }
+}
--- layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/tests/AbstractThirdPartyPluginDependencyWordPressAuthenticatedUserWebserverRequestTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/tests/AbstractThirdPartyPluginDependencyWordPressAuthenticatedUserWebserverRequestTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace PHPUnitForGraphQLAPI\WebserverRequests;
 
-use PoP\Root\Exception\ShouldNotHappenException;
-
 /**
  * Test that enabling/disabling a required 3rd-party plugin works well.
  *
@@ -46,46 +44,5 @@ abstract class AbstractThirdPartyPluginDependencyWordPressAuthenticatedUserWebse
     /**
      * @return array<string,array<string>> An array of [$pluginName => ['query' => "...", 'response-enabled' => "...", 'response-disabled' => "..."]]
      */
-    protected function getPluginNameEntries(): array
-    {
-        $pluginEntries = [];
-        foreach ($this->getPluginNames() as $pluginName) {
-            $pluginEntries[$pluginName] = [
-                'query' => $this->getPluginGraphQLQuery($pluginName),
-                'response-enabled' => $this->getPluginEnabledExpectedGraphQLResponse($pluginName),
-                'response-disabled' => $this->getPluginDisabledExpectedGraphQLResponse($pluginName),
-            ];
-        }
-        return $pluginEntries;
-    }
-
-    /**
-     * @return string[]
-     */
-    abstract protected function getPluginNames(): array;
-
-    protected function getPluginGraphQLQuery(string $pluginName): string
-    {
-        $this->throwUnsupportedPluginName($pluginName);
-    }
-
-    protected function getPluginEnabledExpectedGraphQLResponse(string $pluginName): string
-    {
-        $this->throwUnsupportedPluginName($pluginName);
-    }
-
-    protected function getPluginDisabledExpectedGraphQLResponse(string $pluginName): string
-    {
-        $this->throwUnsupportedPluginName($pluginName);
-    }    
-
-    protected function throwUnsupportedPluginName(string $pluginName): never
-    {
-        throw new ShouldNotHappenException(
-            sprintf(
-                'Configuration for plugin "%s" is not complete',
-                $pluginName
-            )
-        );
-    }
+    abstract protected function getPluginNameEntries(): array;
 }
