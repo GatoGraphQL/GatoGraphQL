0b41e5f154728e81dd3c26cc2fd74015c27c0c0e
Leonardo Losoviz <leo@getpop.org>
2022-04-22 15:55:47 +0800

    Added `getRouteFromNamespacedRoute`

--- layers/GraphQLAPIForWP/phpunit-plugins/graphql-api-for-wp-testing/src/RESTAPI/Controllers/AbstractRESTController.php
+++ layers/GraphQLAPIForWP/phpunit-plugins/graphql-api-for-wp-testing/src/RESTAPI/Controllers/AbstractRESTController.php
@@ -24,34 +24,45 @@ abstract class AbstractRESTController extends WP_REST_Controller
 			return;
 		}
 
+		$namespace = $this->getNamespace();
 		foreach ($routeOptions as $route => $routeOptions) {
-			$namespace = sprintf(
-				'%s/%s',
-				$this->getPluginNamespace(),
-				$this->getVersion(),
-			);
-			$controllerNamespace = $this->getControllerNamespace();
-			if ($controllerNamespace !== '') {
-				$namespace = sprintf(
-					'%s/%s',
-					$namespace,
-					$controllerNamespace
-				);
-			}
-			$routeBase = $this->getRouteBase();
-			if ($routeBase !== '') {
-				$route = sprintf(
-					'%s/%s',
-					$routeBase,
-					$route
-				);
-			}
 			register_rest_route(
 				$namespace,
-				$route,
+				$this->getRoute($route),
 				$routeOptions
 			);
 		}
+	}
+
+	final protected function getNamespace(): string
+	{
+		$namespace = sprintf(
+			'%s/%s',
+			$this->getPluginNamespace(),
+			$this->getVersion(),
+		);
+		$controllerNamespace = $this->getControllerNamespace();
+		if ($controllerNamespace !== '') {
+			$namespace = sprintf(
+				'%s/%s',
+				$namespace,
+				$controllerNamespace
+			);
+		}
+		return $namespace;
+	}
+
+	final protected function getRoute(string $route): string
+	{
+		$routeBase = $this->getRouteBase();
+		if ($routeBase !== '') {
+			$route = sprintf(
+				'%s/%s',
+				$routeBase,
+				$route
+			);
+		}
+		return $route;
 	}
 
 	/**
@@ -94,5 +105,26 @@ abstract class AbstractRESTController extends WP_REST_Controller
 	{
 		echo wp_json_encode( $result->get_data() );
 		die;
+	}
+
+	protected function getRouteFromNamespacedRoute(string $namespacedRoute): string
+	{
+		return $this->getRouteFromCompleteRoute(
+			$this->getCompleteRouteFromNamespacedRoute($namespacedRoute)
+		);
+	}
+
+	protected function getCompleteRouteFromNamespacedRoute(string $namespacedRoute): string
+	{
+		return substr($namespacedRoute, strlen('/' . $this->getNamespace() . '/'));
+	}
+
+	protected function getRouteFromCompleteRoute(string $completeRoute): string
+	{
+		$routeBase = $this->getRouteBase();
+		if ($routeBase === '') {
+			return $completeRoute;
+		}
+		return substr($completeRoute, strlen($routeBase . '/'));
 	}
 }
