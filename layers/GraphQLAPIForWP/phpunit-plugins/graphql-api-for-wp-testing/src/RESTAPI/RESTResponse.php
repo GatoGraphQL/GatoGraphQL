df6e81d6403f5e7746d3e51d712dc45b1dd9b506
Leonardo Losoviz <leo@getpop.org>
2022-04-22 12:46:56 +0800

    property promotion

--- layers/GraphQLAPIForWP/phpunit-plugins/graphql-api-for-wp-testing/src/RESTAPI/RESTResponse.php
+++ layers/GraphQLAPIForWP/phpunit-plugins/graphql-api-for-wp-testing/src/RESTAPI/RESTResponse.php
@@ -4,19 +4,17 @@ declare(strict_types=1);
 
 namespace PHPUnitForGraphQLAPI\GraphQLAPITesting\RESTAPI;
 
-use PHPUnitForGraphQLAPI\GraphQLAPITesting\RESTAPI\Constants\ResponseStatus;
 use stdClass;
 
 class RESTResponse
 {
-	public string $status = '';
-	public string $message = '';
-	/**
-	 * Extra data
-	 */
-	public stdClass $data;
-
-	public function __construct() {
-		$this->data = new stdClass();
+	public function __construct(
+		public string $status = '',
+		public string $message = '',
+		/**
+		 * Extra data
+		 */
+		public stdClass $data = new stdClass(),
+	) {
 	}
 }
