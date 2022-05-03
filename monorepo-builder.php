ed7a0d5d9131972e3e49d71a61f2ee2c157e99c3
Leonardo Losoviz <leo@getpop.org>
2022-05-03 15:12:10 +0800

    Undid changes

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,11 +3,11 @@
 declare(strict_types=1);
 
 use PoP\PoP\Config\Symplify\MonorepoBuilder\Configurators\ContainerConfigurationService;
-use Rector\Config\RectorConfig;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (RectorConfig $rectorConfig): void {
+return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurationService = new ContainerConfigurationService(
-        $rectorConfig,
+        $containerConfigurator,
         __DIR__
     );
     $containerConfigurationService->configureContainer();
