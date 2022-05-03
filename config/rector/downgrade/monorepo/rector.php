7e6dcf0afccc42ea7aa07fc0fa99d8c4036d6225
Leonardo Losoviz <leo@getpop.org>
2022-05-03 10:53:57 +0800

    Renamed class

--- config/rector/downgrade/monorepo/rector.php
+++ config/rector/downgrade/monorepo/rector.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-use PoP\PoP\Config\Rector\Downgrade\Configurators\MonorepoContainerConfigurationService;
+use PoP\PoP\Config\Rector\Downgrade\Configurators\MonorepoDowngradeContainerConfigurationService;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurationService = new MonorepoContainerConfigurationService(
+    $containerConfigurationService = new MonorepoDowngradeContainerConfigurationService(
         $containerConfigurator,
         dirname(__DIR__, 4)
     );
--- src/Config/Rector/Downgrade/Configurators/MonorepoDowngradeContainerConfigurationService.php
+++ src/Config/Rector/Downgrade/Configurators/MonorepoDowngradeContainerConfigurationService.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\PoP\Config\Rector\Downgrade\Configurators;
 
-class MonorepoContainerConfigurationService extends AbstractDowngradeContainerConfigurationService
+class MonorepoDowngradeContainerConfigurationService extends AbstractDowngradeContainerConfigurationService
 {
     /**
      * @return string[]
