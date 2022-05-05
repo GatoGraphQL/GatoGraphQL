00dd2b6dc2c07cbd16d9fe2566896e59a1d0cd5e
Leonardo Losoviz <leo@getpop.org>
2022-05-05 16:08:56 +0800

    Added test

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/DefaultPublicPrivateSchemaModeModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\Constants\ModuleSettingOptions;
+use GraphQLAPI\GraphQLAPI\ModuleResolvers\SchemaConfigurationFunctionalityModuleResolver;
+use PoP\AccessControl\Schema\SchemaModes;
+
+class DefaultPublicPrivateSchemaModeModifyPluginSettingsFixtureEndpointWebserverRequestTest extends AbstractModifyPluginSettingsFixtureEndpointWebserverRequestTestCase
+{
+    protected function getEndpoint(): string
+    {
+        // This endpoint has "Visibility of the schema metadata" as "Default"
+        return 'graphql/power-users/';
+    }
+
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-default-public-private-schema-mode';
+    }
+
+    protected function getSettingsKey(): string
+    {
+        return SchemaConfigurationFunctionalityModuleResolver::OPTION_MODE;
+    }
+
+    protected function getModuleID(string $dataName): string
+    {
+        return 'graphqlapi_graphqlapi_public-private-schema';
+    }
+
+    protected function getPluginSettingsNewValue(): mixed
+    {
+        return SchemaModes::PRIVATE_SCHEMA_MODE;
+    }
+}
