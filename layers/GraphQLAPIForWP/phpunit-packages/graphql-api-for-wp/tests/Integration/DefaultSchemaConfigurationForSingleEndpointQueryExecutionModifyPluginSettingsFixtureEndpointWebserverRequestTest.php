32b19ce3c30b0c441a794f1d3192cbff30f71aa2
Leonardo Losoviz <leo@getpop.org>
2022-05-05 15:09:02 +0800

    Removed commented code

--- layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/DefaultSchemaConfigurationForSingleEndpointQueryExecutionModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/DefaultSchemaConfigurationForSingleEndpointQueryExecutionModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
 
 use GraphQLAPI\GraphQLAPI\Constants\ModuleSettingOptions;
-use GraphQLAPI\GraphQLAPI\Constants\ModuleSettingOptionValues;
 
 class DefaultSchemaConfigurationForSingleEndpointQueryExecutionModifyPluginSettingsFixtureEndpointWebserverRequestTest extends AbstractModifyPluginSettingsFixtureEndpointWebserverRequestTestCase
 {
@@ -32,15 +31,6 @@ class DefaultSchemaConfigurationForSingleEndpointQueryExecutionModifyPluginSetti
     protected function getModuleID(string $dataName): string
     {
         return 'graphqlapi_graphqlapi_schema-configuration';
-        // return match ($dataName) {
-        //     'default-schema-configuration-for-single-endpoint' => 'graphqlapi_graphqlapi_schema-configuration',
-        //     default => throw new ShouldNotHappenException(
-        //         sprintf(
-        //             'There is no moduleID configured for $dataName \'%s\'',
-        //             $dataName
-        //         )
-        //     )
-        // };
     }
 
     protected function getPluginSettingsNewValue(): mixed
