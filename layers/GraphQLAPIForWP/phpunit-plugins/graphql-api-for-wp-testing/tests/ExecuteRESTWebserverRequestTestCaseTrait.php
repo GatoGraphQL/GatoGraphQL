9005d96ad92d50f3fcb6fa9ebce15205566e0c44
Leonardo Losoviz <leo@getpop.org>
2022-05-05 15:08:10 +0800

    assertRESTPostCallIsSuccessful

--- layers/GraphQLAPIForWP/phpunit-plugins/graphql-api-for-wp-testing/tests/ExecuteRESTWebserverRequestTestCaseTrait.php
+++ layers/GraphQLAPIForWP/phpunit-plugins/graphql-api-for-wp-testing/tests/ExecuteRESTWebserverRequestTestCaseTrait.php
@@ -13,11 +13,20 @@ trait ExecuteRESTWebserverRequestTestCaseTrait
     /**
      * Assert the REST API call is successful, or already fail the test
      */
-    protected function assertRESTCallIsSuccessful(
+    protected function assertRESTGetCallIsSuccessful(
         ResponseInterface $response
     ): void {
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertStringStartsWith('application/json', $response->getHeaderLine('content-type'));
+    }
+
+    /**
+     * Assert the REST API call is successful, or already fail the test
+     */
+    protected function assertRESTPostCallIsSuccessful(
+        ResponseInterface $response
+    ): void {
+        $this->assertRESTGetCallIsSuccessful($response);
         $restResponse = RESTResponse::fromClientResponse($response);
         $this->assertEquals(ResponseStatus::SUCCESS, $restResponse->status);
     }
