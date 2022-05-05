be6a505e4028afae64f3ea6ecff8adba7b5e6831
Leonardo Losoviz <leo@getpop.org>
2022-05-05 11:53:52 +0800

    Split class into 2

--- layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/tests/AbstractClientPathSettingsWebserverRequestTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/tests/AbstractClientPathSettingsWebserverRequestTest.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace PHPUnitForGraphQLAPI\WebserverRequests;
 
 use GraphQLAPI\GraphQLAPI\Constants\ModuleSettingOptions;
-use PHPUnitForGraphQLAPI\GraphQLAPI\Constants\RESTAPIEndpoints;
-use PHPUnitForGraphQLAPI\GraphQLAPITesting\ExecuteRESTWebserverRequestTestCaseTrait;
-use PHPUnitForGraphQLAPI\GraphQLAPITesting\RESTAPI\Constants\Params;
 
 /**
  * Test that enabling/disabling clients (GraphiQL/Voyager)
  * in Custom Endpoints works well
  */
-abstract class AbstractClientPathSettingsWebserverRequestTest extends AbstractWebserverRequestTestCase
+abstract class AbstractClientPathSettingsWebserverRequestTest extends AbstractModifyPluginSettingsWebserverRequestTest
 {
-    use RequestRESTAPIWordPressAuthenticatedUserWebserverRequestTestTrait;
-    use ExecuteRESTWebserverRequestTestCaseTrait;
     use ClientWebserverRequestTestCaseTrait;
+
+    protected function getSettingsKey(): string
+    {
+        return ModuleSettingOptions::PATH;
+    }
 
     /**
      * Test that:
@@ -39,59 +39,4 @@ abstract class AbstractClientPathSettingsWebserverRequestTest extends AbstractWe
      * @return array<string,string[]> Array of 2 elements: [ ${newClientPath}, ${previousClientPath} ]
      */
     abstract protected function provideClientPathEntries(): array;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        /**
-         * Execute a REST API to update the client path before the test
-         */
-        $dataName = $this->dataName();
-        $data = $this->getProvidedData();
-        $newClientPath = $data[0];
-        $this->executeRESTEndpointToUpdateClientPath($dataName, $newClientPath);
-    }
-
-    protected function tearDown(): void
-    {
-        /**
-         * Execute a REST API to restore the client path after the test
-         */
-        $dataName = $this->dataName();
-        $data = $this->getProvidedData();
-        $previousClientPath = $data[1];
-        $this->executeRESTEndpointToUpdateClientPath($dataName, $previousClientPath);
-
-        parent::tearDown();
-    }
-
-    protected function executeRESTEndpointToUpdateClientPath(
-        string $dataName,
-        string $path
-    ): void {
-        $client = static::getClient();
-        $endpointURLPlaceholder = static::getWebserverHomeURL() . '/' . RESTAPIEndpoints::MODULE_SETTINGS;
-        $endpointURL = sprintf(
-            $endpointURLPlaceholder,
-            $this->getModuleID($dataName),
-        );
-        $options = static::getRESTEndpointRequestOptions();
-        $options['query'][Params::OPTION_VALUES] = [
-            ModuleSettingOptions::PATH => $path,
-        ];
-        $response = $client->post(
-            $endpointURL,
-            $options,
-        );
-        // Assert the REST API call is successful, or already fail the test
-        $this->assertRESTCallIsSuccessful($response);
-    }
-
-    /**
-     * To visualize the list of all the modules, and find the "moduleID":
-     *
-     * @see http://graphql-api.lndo.site/wp-json/graphql-api/v1/admin/modules
-     */
-    abstract protected function getModuleID(string $dataName): string;
 }
