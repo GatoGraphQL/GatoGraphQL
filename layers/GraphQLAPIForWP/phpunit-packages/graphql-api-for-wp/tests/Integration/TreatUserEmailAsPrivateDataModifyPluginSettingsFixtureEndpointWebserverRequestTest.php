af771fb8042ff69b5dec70d8dec9600a64813a99
Leonardo Losoviz <leo@getpop.org>
2022-05-06 16:02:43 +0800

    Added test

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/TreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\ModuleResolvers\SchemaTypeModuleResolver;
+
+class TreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest extends AbstractModifyPluginSettingsFixtureEndpointWebserverRequestTestCase
+{
+    protected function getEndpoint(): string
+    {
+        return 'graphql/';
+    }
+
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-user-email-as-private-data';
+    }
+
+    protected function getSettingsKey(): string
+    {
+        return SchemaTypeModuleResolver::OPTION_TREAT_USER_EMAIL_AS_ADMIN_DATA;
+    }
+
+    protected function getModuleID(string $dataName): string
+    {
+        return 'graphqlapi_graphqlapi_schema-users';
+    }
+
+    protected function getPluginSettingsNewValue(): mixed
+    {
+        return false;
+    }
+}
