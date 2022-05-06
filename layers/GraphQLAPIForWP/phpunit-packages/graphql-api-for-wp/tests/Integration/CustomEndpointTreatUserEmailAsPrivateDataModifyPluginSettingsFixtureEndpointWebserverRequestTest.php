0f580d414bf544ddb1bcdf4c3efde69664486907
Leonardo Losoviz <leo@getpop.org>
2022-05-06 17:50:11 +0800

    Updated phpdoc

--- layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/CustomEndpointTreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/CustomEndpointTreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
@@ -6,8 +6,10 @@ namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
 
 /**
  * Use a custom endpoint that has option "Expose admin elements in the schema?"
- * with value "disabled". Then, those fields treated as "admin" won't be
- * added to the schema, and the query will produce an error.
+ * with value "Do not expose admin elements".
+ *
+ * Then, those fields treated as "admin" won't be added to the schema,
+ * and the query will produce an error.
  */
 class CustomEndpointTreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest extends AbstractTreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest
 {
