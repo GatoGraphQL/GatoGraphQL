442de934d1979f1621177b02dc0d6903e50c607b
Leonardo Losoviz <leo@getpop.org>
2022-05-06 17:47:55 +0800

    Updated phpdoc

--- layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/CustomEndpointTreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/CustomEndpointTreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
@@ -6,7 +6,8 @@ namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
 
 /**
  * Use a custom endpoint that has option "Expose admin elements in the schema?"
- * with value "default"
+ * with value "disabled". Then, those fields treated as "admin" won't be
+ * added to the schema, and the query will produce an error.
  */
 class CustomEndpointTreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest extends AbstractTreatUserEmailAsPrivateDataModifyPluginSettingsFixtureEndpointWebserverRequestTest
 {
