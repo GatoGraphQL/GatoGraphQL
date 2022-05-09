a6a6029edd7a183aed5669fa6064f81258baca58
Leonardo Losoviz <leo@getpop.org>
2022-05-09 22:13:00 +0800

    import-data => delete-data

---
+++ webservers/graphql-api-for-wp/setup/delete-data.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# First remove the first "Hello world!" post (and "Sample Page" and "Privacy Policy" pages), to avoid duplication (it's in the dataset)
+wp post delete $(wp post list --post_type='post,page' --format=ids --path=/app/wordpress) --force --path=/app/wordpress
--- webservers/graphql-api-for-wp/setup/import-data.sh
+++ webservers/graphql-api-for-wp/setup/import-data.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-# First remove the first "Hello world!" post (and "Sample Page" and "Privacy Policy" pages), to avoid duplication (it's in the dataset)
-wp post delete $(wp post list --post_type='post,page' --format=ids --path=/app/wordpress) --force --path=/app/wordpress
 wp import /app/assets/graphql-api-data.xml --authors=create --path=/app/wordpress
