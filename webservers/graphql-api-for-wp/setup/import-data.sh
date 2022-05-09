e4c4217389b8e96f7bc7edcef75055c2b0ae672b
Leonardo Losoviz <leo@getpop.org>
2022-05-09 22:09:37 +0800

    Reverted: Delete initial comments

--- webservers/graphql-api-for-wp/setup/import-data.sh
+++ webservers/graphql-api-for-wp/setup/import-data.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# First remove the first "Hello world!" post (and "Sample Page" and "Privacy Policy" pages, and comments), to avoid duplication (it's in the dataset)
+# First remove the first "Hello world!" post (and "Sample Page" and "Privacy Policy" pages), to avoid duplication (it's in the dataset)
 wp post delete $(wp post list --post_type='post,page' --format=ids --path=/app/wordpress) --force --path=/app/wordpress
-wp comment delete $(wp comment list --field=ID --path=/app/wordpress) --force --path=/app/wordpress
 wp import /app/assets/graphql-api-data.xml --authors=create --path=/app/wordpress
