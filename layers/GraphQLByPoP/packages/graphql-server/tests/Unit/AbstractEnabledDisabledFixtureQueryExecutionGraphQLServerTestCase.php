4c242f23b13231fa2697742ac228c539adbbe6e3
Leonardo Losoviz <leo@getpop.org>
2022-05-09 16:53:11 +0800

    Moved logic from class to trait for reuse

--- layers/GraphQLByPoP/packages/graphql-server/tests/Unit/AbstractEnabledDisabledFixtureQueryExecutionGraphQLServerTestCase.php
+++ layers/GraphQLByPoP/packages/graphql-server/tests/Unit/AbstractEnabledDisabledFixtureQueryExecutionGraphQLServerTestCase.php
@@ -6,22 +6,5 @@ namespace GraphQLByPoP\GraphQLServer\Unit;
 
 abstract class AbstractEnabledDisabledFixtureQueryExecutionGraphQLServerTestCase extends AbstractFixtureQueryExecutionGraphQLServerTestCase
 {
-    abstract protected static function isEnabled(): bool;
-
-    protected function getGraphQLResponseFile(string $filePath, string $fileName): string
-    {
-        $state = $this->getFileState();
-        return $filePath . \DIRECTORY_SEPARATOR . $fileName . '@' . $state . '.json';
-    }
-
-    protected function getGraphQLVariablesFile(string $filePath, string $fileName): string
-    {
-        $state = $this->getFileState();
-        return $filePath . \DIRECTORY_SEPARATOR . $fileName . '@' . $state . '.var.json';
-    }
-
-    protected function getFileState(): string
-    {
-        return $this->isEnabled() ? 'enabled' : 'disabled';
-    }
+    use EnabledDisabledFixtureQueryExecutionGraphQLServerTestCaseTrait;
 }
