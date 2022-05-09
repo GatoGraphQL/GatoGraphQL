a0ee466417c1593feec7061e5f8e6a65d68e4d32
Leonardo Losoviz <leo@getpop.org>
2022-05-09 17:42:13 +0800

    Use const `CACHE_CONTROL_MAX_AGE`

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Constants/SettingsDefaultValues.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\Constants;
+
+class SettingsDefaultValues
+{
+    public final const CACHE_CONTROL_MAX_AGE = 86400; // 1 day;
+}
