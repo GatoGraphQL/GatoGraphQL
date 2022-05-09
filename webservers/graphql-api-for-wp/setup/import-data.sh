ce1026b4166788af9480ad3425d6cc97e548aac5
Leonardo Losoviz <leo@getpop.org>
2022-05-09 21:19:42 +0800

    wp post delete 1

--- webservers/graphql-api-for-wp/setup/import-data.sh
+++ webservers/graphql-api-for-wp/setup/import-data.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# First remove the first "Hello world!" post, to avoid duplication (it's in the dataset)
+wp post delete 1 --force --path=/app/wordpress
 wp import /app/assets/graphql-api-data.xml --authors=create --path=/app/wordpress
