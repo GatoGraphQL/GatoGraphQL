b5c38f0dcfa5d4e24678d85eef2c4edf4913cd30
Leonardo Losoviz <leo@getpop.org>
2022-05-10 14:46:06 +0800

    Test ConvertInputValueFromSingleToList

---
+++ layers/GraphQLByPoP/packages/graphql-server/tests/Unit/AbstractConvertInputValueFromSingleToListFixtureQueryExecutionGraphQLServerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLByPoP\GraphQLServer\Unit;
+
+abstract class AbstractConvertInputValueFromSingleToListFixtureQueryExecutionGraphQLServerTest extends AbstractEnabledDisabledFixtureQueryExecutionGraphQLServerTestCase
+{
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-convert-input-value-from-single-to-list';
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    protected static function getGraphQLServerComponentClassConfiguration(): array
+    {
+        return [
+            ...parent::getGraphQLServerComponentClassConfiguration(),
+            ...[
+                \PoP\ComponentModel\Component::class => [
+                    \PoP\ComponentModel\Environment::CONVERT_INPUT_VALUE_FROM_SINGLE_TO_LIST => static::isEnabled(),
+                ],
+            ]
+        ];
+    }
+}
