1eedb810a888a57607ab7a11410558eefca7cb80
Leonardo Losoviz <leo@getpop.org>
2022-05-18 14:24:37 +0800

    module => component

--- layers/Engine/packages/componentrouting/src/AbstractComponentRoutingProcessorManager.php
+++ layers/Engine/packages/componentrouting/src/AbstractComponentRoutingProcessorManager.php
@@ -47,24 +47,24 @@ abstract class AbstractComponentRoutingProcessorManager implements ComponentRout
         $appState = App::getAppStateManager()->all();
 
         $processors = $this->getProcessors($group);
-        $most_matching_module = false;
+        $most_matching_component = false;
         $most_matching_properties_count = -1; // Start with -1, since 0 matches is possible
 
         foreach ($processors as $processor) {
-            $nature_route_vars_properties = $processor->getStatePropertiesToSelectComponentByNatureAndRoute();
+            $nature_route_state_properties = $processor->getStatePropertiesToSelectComponentByNatureAndRoute();
 
-            // Check if this processor implements modules for this nature and route
-            if ($route_vars_properties = $nature_route_vars_properties[$nature] ?? null) {
-                if ($vars_properties = $route_vars_properties[$route] ?? null) {
-                    foreach ($vars_properties as $vars_properties_set) {
-                        // Check if the all the $vars_properties_set are satisfied <= if all those key/values are also present in the application state
-                        $conditions = $vars_properties_set['conditions'] ?? [];
+            // Check if this processor implements components for this nature and route
+            if ($route_state_properties = $nature_route_state_properties[$nature] ?? null) {
+                if ($state_properties = $route_state_properties[$route] ?? null) {
+                    foreach ($state_properties as $state_properties_set) {
+                        // Check if the all the $state_properties_set are satisfied <= if all those key/values are also present in the application state
+                        $conditions = $state_properties_set['conditions'] ?? [];
                         if (Methods::arrayIsSubset($conditions, $appState)) {
-                            // Check how many matches there are, and if it's the most, this is the most matching module
+                            // Check how many matches there are, and if it's the most, this is the most matching component
                             // Check that it is >= instead of >. This is done so that later processors can override the behavior from previous processors,
                             // which makes sense since plugins are loaded in a specific order
                             if (($matching_properties_count = count($conditions, COUNT_RECURSIVE)) >= $most_matching_properties_count) {
-                                $most_matching_module = $vars_properties_set['component'];
+                                $most_matching_component = $state_properties_set['component'];
                                 $most_matching_properties_count = $matching_properties_count;
                             }
                         }
@@ -73,24 +73,24 @@ abstract class AbstractComponentRoutingProcessorManager implements ComponentRout
             }
         }
 
-        // If there was a satisfying module, then return it
-        // We can override the default module, for a specific route, by setting it to module null! Hence, here ask if the chosen module is not false,
+        // If there was a satisfying component, then return it
+        // We can override the default component, for a specific route, by setting it to component null! Hence, here ask if the chosen component is not false,
         // and if so already return it, allowing for null values too (eg: POPTHEME_WASSUP_ROUTE_LOADERS_INITIALFRAMES in poptheme-wassup/library/componentroutingprocessors/pagesection-maincontent.php)
-        if ($most_matching_module !== false) {
-            return $most_matching_module;
+        if ($most_matching_component !== false) {
+            return $most_matching_component;
         }
 
         // Otherwise, repeat the procedure checking for one level lower: with only the nature
         foreach ($processors as $processor) {
-            $nature_vars_properties = $processor->getStatePropertiesToSelectComponentByNature();
-            if ($vars_properties = $nature_vars_properties[$nature] ?? null) {
-                foreach ($vars_properties as $vars_properties_set) {
-                    // Check if the all the $vars_properties are satisfied <= if all those key/values are also present in the application state
-                    $conditions = $vars_properties_set['conditions'] ?? [];
+            $nature_state_properties = $processor->getStatePropertiesToSelectComponentByNature();
+            if ($state_properties = $nature_state_properties[$nature] ?? null) {
+                foreach ($state_properties as $state_properties_set) {
+                    // Check if the all the $state_properties are satisfied <= if all those key/values are also present in the application state
+                    $conditions = $state_properties_set['conditions'] ?? [];
                     if (Methods::arrayIsSubset($conditions, $appState)) {
-                        // Check how many matches there are, and if it's the most, this is the most matching module
+                        // Check how many matches there are, and if it's the most, this is the most matching component
                         if (($matching_properties_count = count($conditions, COUNT_RECURSIVE)) >= $most_matching_properties_count) {
-                            $most_matching_module = $vars_properties_set['component'];
+                            $most_matching_component = $state_properties_set['component'];
                             $most_matching_properties_count = $matching_properties_count;
                         }
                     }
@@ -98,20 +98,20 @@ abstract class AbstractComponentRoutingProcessorManager implements ComponentRout
             }
         }
 
-        if ($most_matching_module !== false) {
-            return $most_matching_module;
+        if ($most_matching_component !== false) {
+            return $most_matching_component;
         }
 
         // Finally, check without nature or route
         foreach ($processors as $processor) {
-            if ($vars_properties = $processor->getStatePropertiesToSelectComponent()) {
-                foreach ($vars_properties as $vars_properties_set) {
-                    // Check if the all the $vars_properties are satisfied <= if all those key/values are also present in the application state
-                    $conditions = $vars_properties_set['conditions'] ?? [];
+            if ($state_properties = $processor->getStatePropertiesToSelectComponent()) {
+                foreach ($state_properties as $state_properties_set) {
+                    // Check if the all the $state_properties are satisfied <= if all those key/values are also present in the application state
+                    $conditions = $state_properties_set['conditions'] ?? [];
                     if (Methods::arrayIsSubset($conditions, $appState)) {
-                        // Check how many matches there are, and if it's the most, this is the most matching module
+                        // Check how many matches there are, and if it's the most, this is the most matching component
                         if (($matching_properties_count = count($conditions, COUNT_RECURSIVE)) >= $most_matching_properties_count) {
-                            $most_matching_module = $vars_properties_set['component'];
+                            $most_matching_component = $state_properties_set['component'];
                             $most_matching_properties_count = $matching_properties_count;
                         }
                     }
@@ -120,6 +120,6 @@ abstract class AbstractComponentRoutingProcessorManager implements ComponentRout
         }
 
         // If it is false, then return null
-        return $most_matching_module ? (array)$most_matching_module : null;
+        return $most_matching_component ? (array)$most_matching_component : null;
     }
 }
