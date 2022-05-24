c2e61bd49d8e01faf34f9f00dd7660e3c056b0c2
Leonardo Losoviz <leo@getpop.org>
2022-05-24 18:02:12 +0800

    If there are no sticky posts, the result must be empty

--- layers/WPSchema/packages/posts/src/Hooks/QueryHookSet.php
+++ layers/WPSchema/packages/posts/src/Hooks/QueryHookSet.php
@@ -10,6 +10,8 @@ use PoPCMSSchema\PostsWP\TypeAPIs\PostTypeAPI;
 
 class QueryHookSet extends AbstractHookSet
 {
+    public final const NON_EXISTING_CUSTOM_POST_ID = 'non-existing-id';
+
     protected function init(): void
     {
         App::addFilter(
@@ -24,8 +26,13 @@ class QueryHookSet extends AbstractHookSet
     {
         if (isset($query['is-sticky'])) {
             $stickyPosts = \get_option('sticky_posts', []);
+
             // Add the sticky posts to whichever posts were already set
             if ($query['is-sticky']) {
+                // If there are no sticky posts, then the result must be empty
+                if ($stickyPosts === []) {
+                    $stickyPosts = [self::NON_EXISTING_CUSTOM_POST_ID];
+                }
                 $query['post__in'] = array_merge(
                     $query['post__in'] ?? [],
                     $stickyPosts
