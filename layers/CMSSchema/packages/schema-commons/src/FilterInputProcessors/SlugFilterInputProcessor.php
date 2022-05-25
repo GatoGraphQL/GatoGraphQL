98246bd2ade0e4784ce834e76bde7c7477318095
Leonardo Losoviz <leo@getpop.org>
2022-05-25 17:18:10 +0800

    Created classes from SchemaCommons

---
+++ layers/CMSSchema/packages/schema-commons/src/FilterInputProcessors/SlugFilterInputProcessor.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\SchemaCommons\FilterInputProcessors;
+
+use PoP\ComponentModel\FilterInputProcessors\AbstractValueToQueryFilterInputProcessor;
+
+class SlugFilterInputProcessor extends AbstractValueToQueryFilterInputProcessor
+{
+    protected function getQueryArgKey(array $filterInput): string
+    {
+        return 'slug';
+    }
+}
