b7024d21d902267c72719a009aed09e3dad80762
Leonardo Losoviz <leo@getpop.org>
2022-05-25 18:20:27 +0800

    Migrated classes from Menus

---
+++ layers/WPSchema/packages/menus/src/FilterInputProcessors/LocationsFilterInputProcessor.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Menus\FilterInputProcessors;
+
+use PoP\ComponentModel\FilterInputProcessors\AbstractValueToQueryFilterInputProcessor;
+
+class LocationsFilterInputProcessor extends AbstractValueToQueryFilterInputProcessor
+{
+    protected function getQueryArgKey(array $filterInput): string
+    {
+        return 'locations';
+    }
+}
--- layers/WPSchema/packages/menus/src/Overrides/TypeResolvers/InputObjectType/RootMenusFilterInputObjectTypeResolver.php
+++ layers/WPSchema/packages/menus/src/Overrides/TypeResolvers/InputObjectType/RootMenusFilterInputObjectTypeResolver.php
@@ -7,12 +7,13 @@ namespace PoPWPSchema\Menus\Overrides\TypeResolvers\InputObjectType;
 use PoP\ComponentModel\FilterInputProcessors\FilterInputProcessorInterface;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoPCMSSchema\Menus\TypeResolvers\InputObjectType\RootMenusFilterInputObjectTypeResolver as UpstreamRootMenusFilterInputObjectTypeResolver;
-use PoPWPSchema\Menus\FilterInputProcessors\FilterInputProcessor;
+use PoPWPSchema\Menus\FilterInputProcessors\LocationsFilterInputProcessor;
 use PoPWPSchema\Menus\TypeResolvers\ScalarType\MenuLocationSelectableStringTypeResolver;
 
 class RootMenusFilterInputObjectTypeResolver extends UpstreamRootMenusFilterInputObjectTypeResolver
 {
     private ?MenuLocationSelectableStringTypeResolver $menuLocationEnumTypeResolver = null;
+    private ?LocationsFilterInputProcessor $locationsFilterInputProcessor = null;
 
     final public function setMenuLocationSelectableStringTypeResolver(MenuLocationSelectableStringTypeResolver $menuLocationEnumTypeResolver): void
     {
@@ -21,6 +22,14 @@ class RootMenusFilterInputObjectTypeResolver extends UpstreamRootMenusFilterInpu
     final protected function getMenuLocationSelectableStringTypeResolver(): MenuLocationSelectableStringTypeResolver
     {
         return $this->menuLocationEnumTypeResolver ??= $this->instanceManager->getInstance(MenuLocationSelectableStringTypeResolver::class);
+    }
+    final public function setLocationsFilterInputProcessor(LocationsFilterInputProcessor $locationsFilterInputProcessor): void
+    {
+        $this->locationsFilterInputProcessor = $locationsFilterInputProcessor;
+    }
+    final protected function getLocationsFilterInputProcessor(): LocationsFilterInputProcessor
+    {
+        return $this->locationsFilterInputProcessor ??= $this->instanceManager->getInstance(LocationsFilterInputProcessor::class);
     }
 
     public function getInputFieldNameTypeResolvers(): array
@@ -52,7 +61,7 @@ class RootMenusFilterInputObjectTypeResolver extends UpstreamRootMenusFilterInpu
     public function getInputFieldFilterInput(string $inputFieldName): ?FilterInputProcessorInterface
     {
         return match ($inputFieldName) {
-            'locations' => [FilterInputProcessor::class, FilterInputProcessor::FILTERINPUT_LOCATIONS],
+            'locations' => $this->getLocationsFilterInputProcessor(),
             default => parent::getInputFieldFilterInput($inputFieldName),
         };
     }
