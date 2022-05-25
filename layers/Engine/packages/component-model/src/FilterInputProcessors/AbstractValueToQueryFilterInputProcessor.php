057977745cfd2d340ededf7704ca03e7632bb4ec
Leonardo Losoviz <leo@getpop.org>
2022-05-25 16:29:32 +0800

    Expanded filterDataloadQueryArgs

--- layers/Engine/packages/component-model/src/FilterInputProcessors/AbstractValueToQueryFilterInputProcessor.php
+++ layers/Engine/packages/component-model/src/FilterInputProcessors/AbstractValueToQueryFilterInputProcessor.php
@@ -8,8 +8,22 @@ abstract class AbstractValueToQueryFilterInputProcessor extends AbstractFilterIn
 {
     final public function filterDataloadQueryArgs(array $filterInput, array &$query, mixed $value): void
     {
+        $value = $this->getValue($value);
+        if ($this->avoidSettingValueIfEmpty() && empty($value)) {
+            return;
+        }
         $query[$this->getQueryArgKey($filterInput)] = $value;
     }
 
     abstract protected function getQueryArgKey(array $filterInput): string;
+    
+    protected function getValue(mixed $value): mixed
+    {
+        return $value;
+    }
+
+    protected function avoidSettingValueIfEmpty(): bool
+    {
+        return false;
+    }
 }
