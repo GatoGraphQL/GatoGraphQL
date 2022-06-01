9c03619751a6e2ffe5e28d1a3e58dca68a9a8733
Leonardo Losoviz <leo@getpop.org>
2022-06-01 14:46:54 +0800

    typed fn

--- layers/CMSSchema/packages/pages/src/Hooks/VarsHookSet.php
+++ layers/CMSSchema/packages/pages/src/Hooks/VarsHookSet.php
@@ -20,7 +20,7 @@ class VarsHookSet extends AbstractHookSet
         );
     }
 
-    public function getModelInstanceComponentsFromAppState($components)
+    public function getModelInstanceComponentsFromAppState(array $components): array
     {
         switch (App::getState('nature')) {
             case RequestNature::PAGE:
