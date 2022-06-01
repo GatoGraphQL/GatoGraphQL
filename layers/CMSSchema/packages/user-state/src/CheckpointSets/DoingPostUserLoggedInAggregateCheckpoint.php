416bcb040e3208470b72c11342eb71b07b3a7adc
Leonardo Losoviz <leo@getpop.org>
2022-06-01 13:18:51 +0800

    DoingPostUserLoggedInAggregateCheckpoint

---
+++ layers/CMSSchema/packages/user-state/src/CheckpointSets/DoingPostUserLoggedInAggregateCheckpoint.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\UserState\CheckpointSets;
+
+use PoP\ComponentModel\Checkpoints\AbstractAggregateCheckpoint;
+use PoP\Engine\Checkpoints\DoingPostCheckpoint;
+use PoPCMSSchema\UserState\Checkpoints\UserLoggedInCheckpoint;
+
+class DoingPostUserLoggedInAggregateCheckpoint extends AbstractAggregateCheckpoint
+{
+    private ?UserLoggedInCheckpoint $userLoggedInCheckpoint = null;
+    private ?DoingPostCheckpoint $doingPostCheckpoint = null;
+
+    final public function setUserLoggedInCheckpoint(UserLoggedInCheckpoint $userLoggedInCheckpoint): void
+    {
+        $this->userLoggedInCheckpoint = $userLoggedInCheckpoint;
+    }
+    final protected function getUserLoggedInCheckpoint(): UserLoggedInCheckpoint
+    {
+        return $this->userLoggedInCheckpoint ??= $this->instanceManager->getInstance(UserLoggedInCheckpoint::class);
+    }
+    final public function setDoingPostCheckpoint(DoingPostCheckpoint $doingPostCheckpoint): void
+    {
+        $this->doingPostCheckpoint = $doingPostCheckpoint;
+    }
+    final protected function getDoingPostCheckpoint(): DoingPostCheckpoint
+    {
+        return $this->doingPostCheckpoint ??= $this->instanceManager->getInstance(DoingPostCheckpoint::class);
+    }
+
+    /**
+     * @return CheckpointInterface[]
+     */
+    protected function getCheckpoints(): array
+    {
+        return [
+            $this->getDoingPostCheckpoint(),
+            $this->getUserLoggedInCheckpoint(),
+        ];
+    }
+}
