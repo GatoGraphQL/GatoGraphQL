68968bf3cf2720fcf305a0b826cdd59664e4159b
Leonardo Losoviz <leo@getpop.org>
2022-06-01 15:13:55 +0800

    getSchemaCacheKeyElements

--- layers/GraphQLByPoP/packages/graphql-server/src/Hooks/SchemaCacheHookSet.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/Hooks/SchemaCacheHookSet.php
@@ -13,12 +13,12 @@ class SchemaCacheHookSet extends AbstractHookSet
     protected function init(): void
     {
         App::addFilter(
-            CacheUtils::HOOK_SCHEMA_CACHE_KEY_COMPONENTS,
-            $this->getSchemaCacheKeyComponents(...)
+            CacheUtils::HOOK_SCHEMA_CACHE_KEY_ELEMENTS,
+            $this->getSchemaCacheKeyElements(...)
         );
     }
 
-    public function getSchemaCacheKeyComponents(array $elements): array
+    public function getSchemaCacheKeyElements(array $elements): array
     {
         if ($graphQLOperationType = App::getState('graphql-operation-type')) {
             $elements['graphql-operation-type'] = $graphQLOperationType;
