6666cc77c3b23970cb145dae2180897282f55639
Leonardo Losoviz <leo@getpop.org>
2022-06-01 14:25:13 +0800

    removed stale var

--- layers/Legacy/Schema/packages/migrate-everythingelse/migrate/plugins/pop-automatedemails/library/automatedemails/processorautomatedemails-base.php
+++ layers/Legacy/Schema/packages/migrate-everythingelse/migrate/plugins/pop-automatedemails/library/automatedemails/processorautomatedemails-base.php
@@ -1,5 +1,4 @@
 <?php
-use PoP\ComponentModel\Facades\ComponentProcessors\ComponentProcessorManagerFacade;
 use PoP\ComponentRouting\Facades\ComponentRoutingProcessorManagerFacade;
 
 class PoP_ProcessorAutomatedEmailsBase extends PoP_AutomatedEmailsBase
@@ -30,7 +29,6 @@ class PoP_ProcessorAutomatedEmailsBase extends PoP_AutomatedEmailsBase
     
     protected function hasResults()
     {
-        $componentprocessor_manager = ComponentProcessorManagerFacade::getInstance();
         $pagesection_settings_id = $this->getPagesectionSettingsid();
         $block_component = $this->getBlockComponent();
         $block_settings_id = \PoP\ComponentModel\Facades\Modules\ComponentHelpersFacade::getInstance()->getComponentOutputName($block_component);
