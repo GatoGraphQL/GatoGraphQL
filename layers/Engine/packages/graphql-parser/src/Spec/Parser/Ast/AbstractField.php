3c41bdec94c3c6758c0ac397ca9fdc83ba51f59c
Leonardo Losoviz <leo@getpop.org>
2022-06-13 17:43:17 +0800

    Added AbstractField

---
+++ layers/Engine/packages/graphql-parser/src/Spec/Parser/Ast/AbstractField.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\GraphQLParser\Spec\Parser\Ast;
+
+use PoP\GraphQLParser\Spec\Parser\Ast\Directive;
+use PoP\GraphQLParser\Spec\Parser\Location;
+
+abstract class AbstractField extends AbstractAst implements FieldInterface
+{
+    use WithArgumentsTrait;
+    use WithDirectivesTrait;
+
+    protected RelationalField|Fragment|InlineFragment|OperationInterface $parent;
+
+    public function asFieldOutputQueryString(): string
+    {
+        // Generate the string for arguments
+        $strFieldArguments = '';
+        if ($this->getArguments() !== []) {
+            $strArguments = [];
+            foreach ($this->getArguments() as $argument) {
+                $strArguments[] = $argument->asQueryString();
+            }
+            $strFieldArguments = sprintf(
+                '(%s)',
+                implode(', ', $strArguments)
+            );
+        }
+
+        return sprintf(
+            '%s%s%s',
+            $this->getAlias() !== null ? sprintf('%s: ', $this->getAlias()) : '',
+            $this->getName(),
+            $strFieldArguments,
+        );
+    }
+
+    /**
+     * Print the field as string, and attach the location as a GraphQL comment
+     */
+    public function getUniqueID(): string
+    {
+        $location = $this->getLocation();
+        $locationComment = ' # Location: ' . $location->getLine() . 'x' . $location->getColumn();
+        return $this->asFieldOutputQueryString() . $locationComment;
+    }
+
+    protected function doAsQueryString(): string
+    {
+        // Generate the string for directives
+        $strFieldDirectives = '';
+        if ($this->getDirectives() !== []) {
+            $strDirectives = [];
+            foreach ($this->getDirectives() as $directive) {
+                $strDirectives[] = $directive->asQueryString();
+            }
+            $strFieldDirectives = sprintf(
+                ' %s',
+                implode(' ', $strDirectives)
+            );
+        }
+
+        return sprintf(
+            '%s%s',
+            $this->asFieldOutputQueryString(),
+            $strFieldDirectives,
+        );
+    }
+
+    abstract public function getName(): string;
+
+    abstract public function getAlias(): ?string;
+
+    abstract public function getLocation(): Location;
+
+    /**
+     * @return Directive[]
+     */
+    abstract public function getDirectives(): array;
+
+    /**
+     * @return Argument[]
+     */
+    abstract public function getArguments(): array;
+
+    /**
+     * Take the Location into consideration to indicate
+     * that 2 Fields are the same
+     */
+    public function __toString(): string
+    {
+        return $this->getUniqueID();
+    }
+
+    public function getOutputKey(): string
+    {
+        return $this->getAlias() ?? $this->getName();
+    }
+}
