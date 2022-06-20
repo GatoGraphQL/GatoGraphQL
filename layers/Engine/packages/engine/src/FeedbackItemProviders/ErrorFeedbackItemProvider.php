1ab0a934a73e7f22abb5b4a8f575b16c7d8f120c
Leonardo Losoviz <leo@getpop.org>
2022-06-20 22:08:01 +0800

    Removed E3 error

--- layers/Engine/packages/engine/src/FeedbackItemProviders/ErrorFeedbackItemProvider.php
+++ layers/Engine/packages/engine/src/FeedbackItemProviders/ErrorFeedbackItemProvider.php
@@ -10,7 +10,6 @@ use PoP\ComponentModel\Feedback\FeedbackCategories;
 class ErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
 {
     public final const E2 = 'e2';
-    public final const E3 = 'e3';
     public final const E4 = 'e4';
     public final const E5 = 'e5';
     public final const E6 = 'e6';
@@ -23,7 +22,6 @@ class ErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     {
         return [
             self::E2,
-            self::E3,
             self::E4,
             self::E5,
             self::E6,
@@ -35,7 +33,6 @@ class ErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     {
         return match ($code) {
             self::E2 => $this->__('Field \'%s\' hadn\'t been set for object with ID \'%s\', so it can\'t be transformed', 'engine'),
-            self::E3 => $this->__('The value for field \'%s\' (under property \'%s\') is not an array, so execution of this directive can\'t continue', 'engine'),
             self::E4 => $this->__('The value for field \'%s\' is not an array, so execution of this directive can\'t continue', 'engine'),
             self::E5 => $this->__('No composed directives were provided to \'%s\'', 'engine'),
             self::E6 => $this->__('There is no property \'%s\' in the application state', 'engine'),
