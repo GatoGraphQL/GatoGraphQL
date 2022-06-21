46053da9c5dcea0a70712aceb533a37d6258b53c
Leonardo Losoviz <leo@getpop.org>
2022-06-21 11:55:42 +0800

    Imported classes

--- layers/Engine/packages/component-model/src/DirectiveResolvers/RemoveIDFieldSetDirectiveResolverTrait.php
+++ layers/Engine/packages/component-model/src/DirectiveResolvers/RemoveIDFieldSetDirectiveResolverTrait.php
@@ -6,6 +6,8 @@ namespace PoP\ComponentModel\DirectiveResolvers;
 
 use PoP\ComponentModel\Engine\EngineIterationFieldSet;
 use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
+use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
+use SplObjectStorage;
 
 trait RemoveIDFieldSetDirectiveResolverTrait
 {
