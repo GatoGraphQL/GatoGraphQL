89b8a7510f981f78d522191b6124cea3bafe2842
Leonardo Losoviz <leo@getpop.org>
2022-06-22 16:10:02 +0800

    Removed convertFromGraphQLToFieldQuery

--- layers/GraphQLByPoP/packages/graphql-query/src/Schema/GraphQLQueryConvertorInterface.php
+++ layers/GraphQLByPoP/packages/graphql-query/src/Schema/GraphQLQueryConvertorInterface.php
@@ -7,17 +7,6 @@ namespace GraphQLByPoP\GraphQLQuery\Schema;
 interface GraphQLQueryConvertorInterface
 {
     /**
-     * Convert the GraphQL Query to PoP query in its requested form.
-     *
-     * @return array 2 items: [operationType or null (if the query has errors), fieldQuery]
-     */
-    public function convertFromGraphQLToFieldQuery(
-        string $graphQLQuery,
-        ?array $variables = [],
-        ?string $operationName = null
-    ): array;
-
-    /**
      * Indicates if the variable must be dealt with as an expression: if its name starts with "_"
      */
     public function treatVariableAsExpression(string $variableName): bool;
