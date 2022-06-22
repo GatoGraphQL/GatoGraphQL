cbc3c33e2c6cb7bd54cab2361cf03536b12df071
Leonardo Losoviz <leo@getpop.org>
2022-06-22 19:34:30 +0800

    Override method

--- layers/GraphQLByPoP/packages/graphql-server/tests/Unit/OrderedFieldsFixtureQueryExecutionGraphQLServerTest.php
+++ layers/GraphQLByPoP/packages/graphql-server/tests/Unit/OrderedFieldsFixtureQueryExecutionGraphQLServerTest.php
@@ -13,4 +13,15 @@ class OrderedFieldsFixtureQueryExecutionGraphQLServerTest extends AbstractFixtur
     {
         return __DIR__ . '/fixture-ordered-fields';
     }
+
+    /**
+     * Override, to also test the order of the fields
+     */
+    protected function doAssertFixtureGraphQLQueryExecution(string $expectedResponseFile, string $actualResponseContent): void
+    {
+        $this->assertSame(
+            json_encode(json_decode(file_get_contents($expectedResponseFile)), JSON_PRETTY_PRINT),
+            json_encode(json_decode($actualResponseContent), JSON_PRETTY_PRINT)
+        );
+    }
 }
