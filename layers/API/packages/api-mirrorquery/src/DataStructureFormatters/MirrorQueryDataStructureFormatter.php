65324c036ae161efbaba9e8c73a7724ce5e7d8ef
Leonardo Losoviz <leo@getpop.org>
2022-06-22 12:28:34 +0800

    Renamed vars

--- layers/API/packages/api-mirrorquery/src/DataStructureFormatters/MirrorQueryDataStructureFormatter.php
+++ layers/API/packages/api-mirrorquery/src/DataStructureFormatters/MirrorQueryDataStructureFormatter.php
@@ -63,14 +63,14 @@ class MirrorQueryDataStructureFormatter extends AbstractJSONDataStructureFormatt
     protected function addData(?array &$ret, array $fields, array &$databases, array &$unionTypeOutputKeyIDs, array|string|int $objectIDorIDs, string $objectKeyPath, array &$typeOutputKeyPaths, bool $concatenateField = true): void
     {
         // Property fields have numeric key only. From them, obtain the fields to print for the object
-        $propertyFields = array_filter(
+        $leafFields = array_filter(
             $fields,
             fn (string|int $key) =>  is_numeric($key),
             ARRAY_FILTER_USE_KEY
         );
 
         // All other fields must be nested, to keep fetching data for the object relationships
-        $nestedFields = array_filter(
+        $relationalFields = array_filter(
             $fields,
             fn (string|int $key) => !is_numeric($key),
             ARRAY_FILTER_USE_KEY
@@ -82,20 +82,20 @@ class MirrorQueryDataStructureFormatter extends AbstractJSONDataStructureFormatt
                 // Add a new array for this DB object, where to return all its properties
                 $ret[] = [];
                 $resolvedObjectRet = &$ret[count($ret) - 1];
-                $this->addObjectData($resolvedObjectRet, $propertyFields, $nestedFields, $databases, $unionTypeOutputKeyIDs, $objectID, $objectKeyPath, $typeOutputKeyPaths, $concatenateField);
+                $this->addObjectData($resolvedObjectRet, $leafFields, $relationalFields, $databases, $unionTypeOutputKeyIDs, $objectID, $objectKeyPath, $typeOutputKeyPaths, $concatenateField);
             }
             return;
         }
         $objectID = $objectIDorIDs;
-        $this->addObjectData($ret, $propertyFields, $nestedFields, $databases, $unionTypeOutputKeyIDs, $objectID, $objectKeyPath, $typeOutputKeyPaths, $concatenateField);
+        $this->addObjectData($ret, $leafFields, $relationalFields, $databases, $unionTypeOutputKeyIDs, $objectID, $objectKeyPath, $typeOutputKeyPaths, $concatenateField);
     }
 
     /**
      * Undocumented function
      *
      * @param array<string,mixed>|null $resolvedObjectRet
-     * @param array<string> $propertyFields
-     * @param array<string,array<string>> $nestedFields
+     * @param array<string> $leafFields
+     * @param array<string,array<string>> $relationalFields
      * @param array<string,array<string|int,SplObjectStorage<FieldInterface,mixed>>> $databases
      * @param array<string,array<string|int,array<string,array<string|int>|string|int|null>>> $unionTypeOutputKeyIDs
      * @param string|integer $objectID
@@ -103,13 +103,13 @@ class MirrorQueryDataStructureFormatter extends AbstractJSONDataStructureFormatt
      * @param array<string> $typeOutputKeyPaths
      * @param boolean $concatenateField
      */
-    protected function addObjectData(?array &$resolvedObjectRet, array $propertyFields, array $nestedFields, array &$databases, array &$unionTypeOutputKeyIDs, string|int $objectID, string $objectKeyPath, array &$typeOutputKeyPaths, bool $concatenateField): void
+    protected function addObjectData(?array &$resolvedObjectRet, array $leafFields, array $relationalFields, array &$databases, array &$unionTypeOutputKeyIDs, string|int $objectID, string $objectKeyPath, array &$typeOutputKeyPaths, bool $concatenateField): void
     {
-        // If there are no property fields and no nestedFields, then do nothing.
+        // If there are no property fields and no relationalFields, then do nothing.
         // Otherwise, it could throw an error on `extractObjectTypeAndID`
         // because it only has the ID and not the name of the type
         // Eg: When a validation on the last field fails, such as: /?query=posts.id(
-        if (!$propertyFields && !$nestedFields) {
+        if (!$leafFields && !$relationalFields) {
             return;
         }
         // Execute for all fields other than the first one, "root", for both UnionTypeResolvers and non-union ones
@@ -136,9 +136,9 @@ class MirrorQueryDataStructureFormatter extends AbstractJSONDataStructureFormatt
 
         /** @var SplObjectStorage<FieldInterface,mixed> */
         $resolvedObject = $databases[$typeOutputKey][$objectID] ?? new SplObjectStorage();
-        foreach ($propertyFields as $propertyField) {
+        foreach ($leafFields as $leafField) {
             // Only if the property has been set (in case of dbError it is not set)
-            $propertyFieldOutputKey = $this->getFieldQueryInterpreter()->getFieldOutputKey($propertyField);
+            $propertyFieldOutputKey = $this->getFieldQueryInterpreter()->getFieldOutputKey($leafField);
 
             // @todo Re-do this logic, by passing the FieldInterface directly
             /** @var FieldInterface|null */
@@ -158,22 +158,22 @@ class MirrorQueryDataStructureFormatter extends AbstractJSONDataStructureFormatt
         }
 
         // Add the nested levels
-        foreach ($nestedFields as $nestedField => $nestedPropertyFields) {
-            $nestedFieldOutputKey = $this->getFieldQueryInterpreter()->getFieldOutputKey($nestedField);
+        foreach ($relationalFields as $relationalField => $relationalNestedFields) {
+            $relationalFieldOutputKey = $this->getFieldQueryInterpreter()->getFieldOutputKey($relationalField);
         
 
             // @todo Re-do this logic, by passing the FieldInterface directly
             /** @var FieldInterface|null */
-            $nestedFieldInstance = null;
+            $relationalFieldInstance = null;
             /** @var FieldInterface[] */
             $resolvedObjectFields = iterator_to_array($resolvedObject);
             foreach ($resolvedObjectFields as $resolvedObjectField) {
-                if ($resolvedObjectField->getOutputKey() === $nestedFieldOutputKey) {
-                    $nestedFieldInstance = $resolvedObjectField;
+                if ($resolvedObjectField->getOutputKey() === $relationalFieldOutputKey) {
+                    $relationalFieldInstance = $resolvedObjectField;
                     break;
                 }
             }
-            if ($nestedFieldInstance === null) {
+            if ($relationalFieldInstance === null) {
                 continue;
             }
 
@@ -184,13 +184,13 @@ class MirrorQueryDataStructureFormatter extends AbstractJSONDataStructureFormatt
              * if it is to be skipped, there will be no value
              * (which is different than a null)
              */
-            if (!$resolvedObject->contains($nestedFieldInstance)) {
+            if (!$resolvedObject->contains($relationalFieldInstance)) {
                 continue;
             }
 
             // If it's null, directly assign the null to the result
-            if ($resolvedObject[$nestedFieldInstance] === null) {
-                $resolvedObjectRet[$nestedFieldOutputKey] = null;
+            if ($resolvedObject[$relationalFieldInstance] === null) {
+                $resolvedObjectRet[$relationalFieldOutputKey] = null;
                 continue;
             }
 
@@ -202,17 +202,17 @@ class MirrorQueryDataStructureFormatter extends AbstractJSONDataStructureFormatt
              * under the objectID for all fields other than the "root", and this one
              * has its field hardcoded as "id".
              */
-            $nextField = ($concatenateField ? $objectKeyPath . Constants::RELATIONAL_FIELD_PATH_SEPARATOR : '') . $nestedFieldOutputKey;
+            $nextField = ($concatenateField ? $objectKeyPath . Constants::RELATIONAL_FIELD_PATH_SEPARATOR : '') . $relationalFieldOutputKey;
 
             // The type with ID may be stored under $unionTypeOutputKeyIDs
-            $unionTypeOutputKeyID = $unionTypeOutputKeyIDs[$typeOutputKey][$objectID][$nestedFieldInstance] ?? null;
+            $unionTypeOutputKeyID = $unionTypeOutputKeyIDs[$typeOutputKey][$objectID][$relationalFieldInstance] ?? null;
 
             // Add a new subarray for the nested property
-            $resolvedObjectNestedPropertyRet = &$resolvedObjectRet[$nestedFieldOutputKey];
+            $resolvedObjectNestedPropertyRet = &$resolvedObjectRet[$relationalFieldOutputKey];
 
             // If it is an empty array, then directly add an empty array as the result
-            if (is_array($resolvedObject[$nestedFieldInstance]) && empty($resolvedObject[$nestedFieldInstance])) {
-                $resolvedObjectRet[$nestedFieldOutputKey] = [];
+            if (is_array($resolvedObject[$relationalFieldInstance]) && empty($resolvedObject[$relationalFieldInstance])) {
+                $resolvedObjectRet[$relationalFieldOutputKey] = [];
                 continue;
             }
 
@@ -221,19 +221,19 @@ class MirrorQueryDataStructureFormatter extends AbstractJSONDataStructureFormatt
                 // Eg: /posts/api/graphql/?query=id|author,author.name will first return "author => 1" and on the "1" element add property "name"
                 // Then, if this situation happens, simply override the ID (which is a scalar value, such as an int or string) with an object with the 'id' property
                 if (!is_array($resolvedObjectNestedPropertyRet)) {
-                    $resolvedObjectRet[$nestedFieldOutputKey] = [
-                        FieldOutputKeys::ID => $resolvedObjectRet[$nestedFieldOutputKey],
+                    $resolvedObjectRet[$relationalFieldOutputKey] = [
+                        FieldOutputKeys::ID => $resolvedObjectRet[$relationalFieldOutputKey],
                     ];
                 } else {
                     // 2. If the previous iteration loaded an array of IDs, then override this value with an empty array and initialize the ID again to this object, through adding property 'id' on the next iteration
                     // Eg: /api/graphql/?query=tags,tags.name
-                    $resolvedObjectRet[$nestedFieldOutputKey] = [];
-                    if (!in_array(FieldOutputKeys::ID, $nestedPropertyFields)) {
-                        array_unshift($nestedPropertyFields, FieldOutputKeys::ID);
+                    $resolvedObjectRet[$relationalFieldOutputKey] = [];
+                    if (!in_array(FieldOutputKeys::ID, $relationalNestedFields)) {
+                        array_unshift($relationalNestedFields, FieldOutputKeys::ID);
                     }
                 }
             }
-            $this->addData($resolvedObjectNestedPropertyRet, $nestedPropertyFields, $databases, $unionTypeOutputKeyIDs, $unionTypeOutputKeyID ?? $resolvedObject[$nestedFieldInstance], $nextField, $typeOutputKeyPaths);
+            $this->addData($resolvedObjectNestedPropertyRet, $relationalNestedFields, $databases, $unionTypeOutputKeyIDs, $unionTypeOutputKeyID ?? $resolvedObject[$relationalFieldInstance], $nextField, $typeOutputKeyPaths);
         }
     }
 }
