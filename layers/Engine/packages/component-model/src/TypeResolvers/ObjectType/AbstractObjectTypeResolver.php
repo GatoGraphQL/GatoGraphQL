39340e07477654c4d214cd7e104005ec7731f238
Leonardo Losoviz <leo@getpop.org>
2022-06-23 14:34:39 +0800

    format

--- layers/Engine/packages/component-model/src/TypeResolvers/ObjectType/AbstractObjectTypeResolver.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/ObjectType/AbstractObjectTypeResolver.php
@@ -139,7 +139,10 @@ abstract class AbstractObjectTypeResolver extends AbstractRelationalTypeResolver
         $mandatoryDirectivesForFields = [];
         $typeResolverDecorators = $this->getAllRelationalTypeResolverDecorators();
         foreach ($typeResolverDecorators as $typeResolverDecorator) {
-            // array_merge_recursive so that if 2 different decorators add a directive for the same field, the results are merged together, not override each other
+            /**
+             * `array_merge_recursive` so that if 2 different decorators add a directive
+             * for the same field, the results are merged together, not override each other.
+             */
             if ($typeResolverDecorator->enabled($this)) {
                 $mandatoryDirectivesForFields = array_merge_recursive(
                     $mandatoryDirectivesForFields,
