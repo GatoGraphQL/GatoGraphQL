115c2c43068c7fbd99b602d0985335e2ab2e7534
Leonardo Losoviz <leo@getpop.org>
2022-06-23 22:08:42 +0800

    Fixed PHPStan

--- layers/CMSSchema/packages/user-state-access-control/src/ConditionalOnModule/CacheControl/RelationalTypeResolverDecorators/NoCacheConfigurableAccessControlRelationalTypeResolverDecoratorTrait.php
+++ layers/CMSSchema/packages/user-state-access-control/src/ConditionalOnModule/CacheControl/RelationalTypeResolverDecorators/NoCacheConfigurableAccessControlRelationalTypeResolverDecoratorTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PoPCMSSchema\UserStateAccessControl\ConditionalOnModule\CacheControl\RelationalTypeResolverDecorators;
 
 use PoP\CacheControl\Helpers\CacheControlHelper;
+use PoP\GraphQLParser\Spec\Parser\Ast\Directive;
 
 trait NoCacheConfigurableAccessControlRelationalTypeResolverDecoratorTrait
 {
