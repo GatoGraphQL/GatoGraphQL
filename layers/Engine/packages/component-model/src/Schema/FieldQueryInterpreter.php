1356e8ad57a8ce648436db5649b7035d12b7c2b5
Leonardo Losoviz <leo@getpop.org>
2022-06-25 12:10:30 +0800

    Removed extractStaticDirectiveArguments

--- layers/Engine/packages/component-model/src/Schema/FieldQueryInterpreter.php
+++ layers/Engine/packages/component-model/src/Schema/FieldQueryInterpreter.php
@@ -105,20 +105,6 @@ class FieldQueryInterpreter extends UpstreamFieldQueryInterpreter implements Fie
         return $this->objectSerializationManager ??= $this->instanceManager->getInstance(ObjectSerializationManagerInterface::class);
     }
 
-    /**
-     * Extract field args without using the schema.
-     * It is needed to find out which fieldResolver will process a field,
-     * where we can't depend on the schema since this one needs to know
-     * who the fieldResolver is, creating an infitine loop.
-     *
-     * Directive arguments have the same syntax as field arguments,
-     * so simply re-utilize the corresponding function for field arguments.
-     */
-    public function extractStaticDirectiveArguments(string $directive, ?array $variables = null): array
-    {
-        return $this->extractStaticFieldArguments($directive, $variables);
-    }
-
     protected function getVariablesHash(?array $variables): string
     {
         return (string)hash('crc32', json_encode($variables ?? []));
