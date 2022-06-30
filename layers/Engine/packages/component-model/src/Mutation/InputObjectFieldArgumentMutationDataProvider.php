5a462561296961dd3315a31c0a3ad4188989d0a1
Leonardo Losoviz <leo@getpop.org>
2022-07-01 05:23:25 +0800

    Inject argumentName

--- layers/Engine/packages/component-model/src/Mutation/InputObjectFieldArgumentMutationDataProvider.php
+++ layers/Engine/packages/component-model/src/Mutation/InputObjectFieldArgumentMutationDataProvider.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Mutation;
 
+use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
+
 class InputObjectFieldArgumentMutationDataProvider extends AbstractInputObjectFieldArgumentMutationDataProvider
 {
+    public function __construct(
+        FieldInterface $field,
+        protected string $fieldInputArgumentName
+    ) {
+        parent::__construct($field);
+    }
+
     protected function getArgumentName(): string
     {
-        return 'input';
+        return $this->fieldInputArgumentName;
     }
 }
