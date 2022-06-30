5d0dd1a68cd8cd2f9e21113bf30bd345c7d3fea2
Leonardo Losoviz <leo@getpop.org>
2022-07-01 07:11:24 +0800

    FieldArgumentMutationDataProviderInterface

--- layers/Engine/packages/component-model/src/Mutation/AbstractFieldArgumentMutationDataProvider.php
+++ layers/Engine/packages/component-model/src/Mutation/AbstractFieldArgumentMutationDataProvider.php
@@ -6,10 +6,15 @@ namespace PoP\ComponentModel\Mutation;
 
 use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
 
-abstract class AbstractFieldArgumentMutationDataProvider implements MutationDataProviderInterface
+abstract class AbstractFieldArgumentMutationDataProvider implements FieldArgumentMutationDataProviderInterface
 {
     public function __construct(
         protected FieldInterface $field
     ) {
+    }
+
+    public function getField(): FieldInterface
+    {
+        return $this->field;
     }
 }
