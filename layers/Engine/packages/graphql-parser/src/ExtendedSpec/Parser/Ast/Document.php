07ba7b3887e165dd1f584243243886ebe57be8bf
Leonardo Losoviz <leo@getpop.org>
2022-07-01 14:17:35 +0800

    Handle MetaDirective

--- layers/Engine/packages/graphql-parser/src/ExtendedSpec/Parser/Ast/Document.php
+++ layers/Engine/packages/graphql-parser/src/ExtendedSpec/Parser/Ast/Document.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace PoP\GraphQLParser\ExtendedSpec\Parser\Ast;
 
+use PoP\GraphQLParser\ExtendedSpec\Parser\Ast\ArgumentValue\DynamicVariableReferenceInterface;
 use PoP\GraphQLParser\Module;
 use PoP\GraphQLParser\ModuleConfiguration;
-use PoP\GraphQLParser\ExtendedSpec\Parser\Ast\ArgumentValue\DynamicVariableReferenceInterface;
 use PoP\GraphQLParser\Spec\Parser\Ast\ArgumentValue\VariableReference;
+use PoP\GraphQLParser\Spec\Parser\Ast\Directive;
 use PoP\GraphQLParser\Spec\Parser\Ast\Document as UpstreamDocument;
 use PoP\Root\App;
 
@@ -26,5 +27,26 @@ class Document extends UpstreamDocument
             return true;
         }
         return parent::isVariableDefined($variableReference);
+    }
+
+    /**
+     * @param Directive[] $directives
+     * @return VariableReference[]
+     */
+    protected function getVariableReferencesInDirectives(array $directives): array
+    {
+        $variableReferences = parent::getVariableReferencesInDirectives($directives);
+        /** @var MetaDirective[] */
+        $metaDirectives = array_filter(
+            $directives,
+            fn (Directive $directive) => $directive instanceof MetaDirective
+        );
+        foreach ($metaDirectives as $metaDirective) {
+            $variableReferences = array_merge(
+                $variableReferences,
+                $this->getVariableReferencesInDirectives($metaDirective->getNestedDirectives())
+            );
+        }
+        return $variableReferences;
     }
 }
