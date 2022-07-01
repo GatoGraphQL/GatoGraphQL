59c060fc23b372371c7e0e491b7ee8663833c6ff
Leonardo Losoviz <leo@getpop.org>
2022-07-01 10:46:57 +0800

    Abstract => non-abstract

--- layers/Engine/packages/component-model/src/Mutation/InputObjectFieldArgumentMutationDataProvider.php
+++ layers/Engine/packages/component-model/src/Mutation/InputObjectFieldArgumentMutationDataProvider.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Mutation;
 
+use PoP\GraphQLParser\Spec\Parser\Ast\ArgumentValue\InputObject;
 use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
+use stdClass;
 
-class InputObjectFieldArgumentMutationDataProvider extends AbstractInputObjectFieldArgumentMutationDataProvider
+class InputObjectFieldArgumentMutationDataProvider extends FieldArgumentMutationDataProvider implements InputObjectFieldArgumentMutationDataProviderInterface
 {
     public function __construct(
         FieldInterface $field,
@@ -19,5 +21,46 @@ class InputObjectFieldArgumentMutationDataProvider extends AbstractInputObjectFi
     public function getArgumentName(): string
     {
         return $this->fieldInputArgumentName;
+    }
+    
+    /**
+     * @return string[]
+     */
+    public function getPropertyNames(): array
+    {
+        $inputObjectValue = $this->getInputObjectValue();
+        return array_unique(array_merge(
+            parent::getPropertyNames(),
+            array_keys((array) $inputObjectValue)
+        ));
+    }
+
+    public function hasProperty(string $propertyName): bool
+    {
+        $inputObjectValue = $this->getInputObjectValue();
+        if (property_exists($inputObjectValue, $propertyName)) {
+            return true;
+        }
+        return parent::hasProperty($propertyName);
+    }
+
+    public function getValue(string $propertyName): mixed
+    {
+        $inputObjectValue = $this->getInputObjectValue();
+        if (property_exists($inputObjectValue, $propertyName)) {
+            return $inputObjectValue->$propertyName;
+        }
+        return parent::getValue($propertyName);
+    }
+
+    protected function getInputObjectValue(): stdClass
+    {
+        return $this->getInputObject()->getValue();
+    }
+
+    final protected function getInputObject(): InputObject
+    {
+        $argument = $this->field->getArgument($this->getArgumentName());
+        return $argument->getValueAST();
     }
 }
--- layers/Engine/packages/component-model/src/Mutation/SubpropertyInputObjectFieldArgumentMutationDataProvider.php
+++ layers/Engine/packages/component-model/src/Mutation/SubpropertyInputObjectFieldArgumentMutationDataProvider.php
@@ -5,25 +5,28 @@ declare(strict_types=1);
 namespace PoP\ComponentModel\Mutation;
 
 use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
+use stdClass;
 
-class SubpropertyInputObjectFieldArgumentMutationDataProvider extends AbstractSubpropertyInputObjectFieldArgumentMutationDataProvider
+class SubpropertyInputObjectFieldArgumentMutationDataProvider extends InputObjectFieldArgumentMutationDataProvider
 {
     public function __construct(
         FieldInterface $field,
-        protected string $fieldInputArgumentName,
+        string $fieldInputArgumentName,
         protected string $subpropertyName,
         array $propertyValues = [],
     ) {
-        parent::__construct($field, $propertyValues);
-    }
-
-    public function getArgumentName(): string
-    {
-        return $this->fieldInputArgumentName;
+        parent::__construct($field, $fieldInputArgumentName, $propertyValues);
     }
 
     protected function getSubpropertyName(): string
     {
         return $this->subpropertyName;
+    }
+
+    protected function getInputObjectValue(): stdClass
+    {
+        $inputObjectValue = parent::getInputObjectValue();
+        $subpropertyName = $this->getSubpropertyName();
+        return $inputObjectValue->$subpropertyName;
     }
 }
