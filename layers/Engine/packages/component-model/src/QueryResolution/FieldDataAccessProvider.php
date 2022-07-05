4b84df384b149cf12e6becf3c95ec94159bb6419
Leonardo Losoviz <leo@getpop.org>
2022-07-05 13:19:14 +0800

    Added phpdoc

--- layers/Engine/packages/component-model/src/QueryResolution/FieldDataAccessProvider.php
+++ layers/Engine/packages/component-model/src/QueryResolution/FieldDataAccessProvider.php
@@ -10,6 +10,47 @@ use PoP\Root\Exception\ShouldNotHappenException;
 use PoP\Root\Services\StandaloneServiceTrait;
 use SplObjectStorage;
 
+/**
+ * Class to retrieve the values contained in Field Arguments.
+ *
+ * It transforms the FieldArgs passed in the GraphQL query, into the "normalized"
+ * values that will be used when resolving the query. These include:
+ *
+ * - The default values that were not provided in the GraphQL query
+ * - Coercing values according to the schema definition
+ * - Potential custom additions of values into the FieldArgs, such as the
+ *   logged-in user's name and email when adding comments
+ *
+ * There are 3 possibilities for hosting Field Argument values:
+ *
+ * 1. Data from a Field in an ObjectTypeResolver: a single instance of the
+ *    FieldArgs will satisfy all queried objects, since the same schema applies
+ *    to all of them.
+ *
+ * 2. Data from a Field in an UnionTypeResolver: the union field does not have
+ *    the schema information, but only the corresponding ObjectTypeResolver
+ *    that will resolve the entity does.
+ *    For instance, when querying 'customPosts { dateStr }', field `dateStr`
+ *    could be evaluated against a Post or Page types, and they could have
+ *    different definitions of the `dateStr` field, such as making argument
+ *    `$format` mandatory or not. Then, there will be a different FieldArgs
+ *    produced for each targetObjectTypeResolver in the UnionTypeResolver
+ *
+ * 3. Data for a specific object: When executing nested mutations, the FieldArgs
+ *    for each object will be different, as it will contain implicit information
+ *    belonging to the object.
+ *    For instance, when querying `mutation { posts { update(title: "New title") { id } } }`,
+ *    the value for the `$postID` is injected into the FieldArgs for each object,
+ *    and the validation of the FieldArgs must also be executed for each object.
+ *
+ * To satisfy all 3 cases with a single implementation, all data is stored
+ * in a chained SplObjectStorage that organizes data by:
+ *
+ * 1. The Field
+ * 2. The ObjectTypeResolver: directly the same ObjectTypeResolver when coming from there,
+ *    or each of the targetObjectTypeResolvers when coming from an UnionTypeResolver
+ * 3. The object, or a special "wildcard object" to signify "all objects"
+ */
 class FieldDataAccessProvider implements FieldDataAccessProviderInterface
 {
     use StandaloneServiceTrait;
