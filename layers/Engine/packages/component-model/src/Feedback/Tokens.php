31d82347560fdc60a963fd1abf5d1e20e3fee63b
Leonardo Losoviz <leo@getpop.org>
2022-07-12 16:30:53 +0800

    const FIELD

--- layers/Engine/packages/component-model/src/Feedback/Tokens.php
+++ layers/Engine/packages/component-model/src/Feedback/Tokens.php
@@ -6,8 +6,9 @@ namespace PoP\ComponentModel\Feedback;
 
 class Tokens
 {
-    public const PATH = 'path';
     public const MESSAGE = 'message';
     public const LOCATIONS = 'locations';
     public const EXTENSIONS = 'extensions';
+    public const PATH = 'path';
+    public const FIELD = 'field';
 }
