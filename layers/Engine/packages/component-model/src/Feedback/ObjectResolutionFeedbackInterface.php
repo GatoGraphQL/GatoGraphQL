e105ed5feb6bc2328339a97784e7abe2cc29f0b8
Leonardo Losoviz <leo@getpop.org>
2022-07-12 14:33:04 +0800

    Fixed PHPStan

--- layers/Engine/packages/component-model/src/Feedback/ObjectResolutionFeedbackInterface.php
+++ layers/Engine/packages/component-model/src/Feedback/ObjectResolutionFeedbackInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Feedback;
 
+use PoP\ComponentModel\Engine\EngineIterationFieldSet;
 use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
 use PoP\GraphQLParser\Spec\Parser\Ast\Directive;
 
