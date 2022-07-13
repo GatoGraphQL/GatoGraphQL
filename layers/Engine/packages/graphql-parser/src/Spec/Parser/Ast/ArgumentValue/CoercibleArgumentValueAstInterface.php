3f713138565258132cb2e28c336d9dbc54a65a8a
Leonardo Losoviz <leo@getpop.org>
2022-07-13 15:18:13 +0800

    Removed interface

--- layers/Engine/packages/graphql-parser/src/Spec/Parser/Ast/ArgumentValue/CoercibleArgumentValueAstInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace PoP\GraphQLParser\Spec\Parser\Ast\ArgumentValue;
-
-interface CoercibleArgumentValueAstInterface extends ArgumentValueAstInterface
-{
-}
