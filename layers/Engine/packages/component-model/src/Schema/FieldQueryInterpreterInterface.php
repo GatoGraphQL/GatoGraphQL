d76fde6c6799c4f907be982890511d70227ae028
Leonardo Losoviz <leo@getpop.org>
2022-07-13 11:58:54 +0800

    Removed all "maybeConvert" functions

--- layers/Engine/packages/component-model/src/Schema/FieldQueryInterpreterInterface.php
+++ layers/Engine/packages/component-model/src/Schema/FieldQueryInterpreterInterface.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\Schema;
 
-use PoP\ComponentModel\DirectiveResolvers\DirectiveResolverInterface;
-use PoP\ComponentModel\Feedback\EngineIterationFeedbackStore;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
 use PoP\FieldQuery\FieldQueryInterpreterInterface as UpstreamFieldQueryInterpreterInterface;
-use PoP\GraphQLParser\Spec\Parser\Ast\Directive;
-use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
-use SplObjectStorage;
 
 interface FieldQueryInterpreterInterface extends UpstreamFieldQueryInterpreterInterface
 {
-    public function maybeConvertFieldArgumentValue(mixed $fieldArgValue, ?array $variables = null): mixed;
-    public function maybeConvertFieldArgumentArrayOrObjectValue(mixed $fieldArgValue, ?array $variables = null): mixed;
     public function resolveExpression(
         RelationalTypeResolverInterface $relationalTypeResolver,
         mixed $fieldArgValue,
