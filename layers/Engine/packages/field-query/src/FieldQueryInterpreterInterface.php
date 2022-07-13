3b6e9bcc845899d1e79e93f5e6faa34dd3c9239f
Leonardo Losoviz <leo@getpop.org>
2022-07-13 12:05:20 +0800

    Removed getFieldAliasPositionSpanInField

--- layers/Engine/packages/field-query/src/FieldQueryInterpreterInterface.php
+++ layers/Engine/packages/field-query/src/FieldQueryInterpreterInterface.php
@@ -14,11 +14,4 @@ interface FieldQueryInterpreterInterface
     public function isFieldArgumentValueAVariable(mixed $fieldArgValue): bool;
     public function isFieldArgumentValueAnExpression(mixed $fieldArgValue): bool;
     public function isFieldArgumentValueDynamic(mixed $fieldArgValue): bool;
-    /**
-     * Return an array with the position where the alias starts (including the "@") and its length
-     * Return null if the field has no alias
-     *
-     * @return array<string, int>|null
-     */
-    public function getFieldAliasPositionSpanInField(string $field): ?array;
 }
