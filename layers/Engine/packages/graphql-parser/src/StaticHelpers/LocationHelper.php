c1dc2ec5489d510078a3520b5ad27ef7728c458f
Leonardo Losoviz <leo@getpop.org>
2022-07-14 15:13:06 +0800

    Improved doc

--- layers/Engine/packages/graphql-parser/src/StaticHelpers/LocationHelper.php
+++ layers/Engine/packages/graphql-parser/src/StaticHelpers/LocationHelper.php
@@ -6,13 +6,23 @@ namespace PoP\GraphQLParser\StaticHelpers;
 
 use PoP\GraphQLParser\Spec\Parser\Location;
 
-/**
- * Temporary class until fully migrating the Engine to GraphQL
- */
 class LocationHelper
 {
+    /**
+     * Use a non-existing location to indicate that the
+     * AST node was created on runtime, and is not to be
+     * found on the GraphQL query.
+     *
+     * Eg of runtime AST nodes:
+     *
+     * - @resolveValueAndMerge
+     * - @serializeLeafOutputTypeValues
+     * - Directives added via `getMandatoryDirectivesForFields`
+     *
+     * This Location will not be printed on the GraphQL response
+     */
     public static function getNonSpecificLocation(): Location
     {
-        return new Location(1, 1);
+        return new Location(-1, -1);
     }
 }
