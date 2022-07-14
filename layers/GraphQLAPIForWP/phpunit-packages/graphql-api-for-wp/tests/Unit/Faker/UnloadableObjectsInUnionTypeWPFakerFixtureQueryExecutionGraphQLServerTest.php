8fd722ec76e72ff1d72384a5ec229470ea5f6fed
Leonardo Losoviz <leo@getpop.org>
2022-07-14 14:49:58 +0800

    Test "unloadable-objects-in-union"

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Unit/Faker/UnloadableObjectsInUnionTypeWPFakerFixtureQueryExecutionGraphQLServerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Unit\Faker;
+
+use PHPUnitForGraphQLAPI\WPFakerSchema\Unit\AbstractWPFakerFixtureQueryExecutionGraphQLServerTest;
+
+class UnloadableObjectsInUnionTypeWPFakerFixtureQueryExecutionGraphQLServerTest extends AbstractWPFakerFixtureQueryExecutionGraphQLServerTest
+{
+    /**
+     * Directory under the fixture files are placed
+     */
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-unloadable-objects-in-union';
+    }
+
+    /**
+     * @return string[]
+     */
+    protected static function getGraphQLServerModuleClasses(): array
+    {
+        return [
+            ...parent::getGraphQLServerModuleClasses(),
+            ...[
+                \PoPWPSchema\Posts\Module::class,
+                \PoPCMSSchema\CustomPostMutationsWP\Module::class,
+            ]
+        ];
+    }
+}
