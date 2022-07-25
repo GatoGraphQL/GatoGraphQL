49d9723cb4a6334990c320f3594c9ed2a6e725e3
Leonardo Losoviz <leo@getpop.org>
2022-07-25 13:46:07 +0800

    Removed constructor

--- layers/Engine/packages/root/src/Container/Loader/ServiceYamlFileLoader.php
+++ layers/Engine/packages/root/src/Container/Loader/ServiceYamlFileLoader.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace PoP\Root\Container\Loader;
 
-use Symfony\Component\Config\FileLocatorInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
@@ -19,14 +17,6 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 class ServiceYamlFileLoader extends YamlFileLoader
 {
     use ServiceYamlFileLoaderTrait;
-
-    public function __construct(
-        ContainerBuilder $container,
-        FileLocatorInterface $locator,
-        protected bool $autoconfigure = true
-    ) {
-        parent::__construct($container, $locator);
-    }
 
     protected function loadFile(string $file): ?array
     {
