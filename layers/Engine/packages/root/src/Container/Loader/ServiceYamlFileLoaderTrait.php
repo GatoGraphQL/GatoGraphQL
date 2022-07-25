27d565f49f941264857c87c8a7052975d101f5f2
Leonardo Losoviz <leo@getpop.org>
2022-07-25 13:39:08 +0800

    ServiceYamlFileLoaderTrait

---
+++ layers/Engine/packages/root/src/Container/Loader/ServiceYamlFileLoaderTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Root\Container\Loader;
+
+use Symfony\Component\Config\FileLocatorInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Trait to help override the Symfony class, to:
+ *
+ * - add the required tag "container.ignore_attributes"
+ *   to avoid PHP 8's attributes.
+ *   Otherwise, an exception is produced when the plugin
+ *   is downgraded to PHP 7.*
+ *
+ * @see https://github.com/symfony/symfony/blob/6fef5b44b0ed982c6c930da2b435179662f8fd25/src/Symfony/Component/DependencyInjection/Compiler/RegisterAutoconfigureAttributesPass.php#L19-L25
+ */
+trait ServiceYamlFileLoaderTrait
+{
+    public function __construct(
+        ContainerBuilder $container,
+        FileLocatorInterface $locator,
+        protected bool $autoconfigure = true
+    ) {
+        parent::__construct($container, $locator);
+    }
+
+    protected function getFileDefinition(string $file): ?array
+    {
+        $content = parent::loadFile($file);
+        if ($content === null) {
+            return null;
+        }
+        if ($content['services']['_defaults']['autoconfigure'] ?? null) {
+            $content['services']['_defaults']['tags'] ??= [];
+            $content['services']['_defaults']['tags'][] = 'container.ignore_attributes';
+        }
+        return $content;
+    }
+}
