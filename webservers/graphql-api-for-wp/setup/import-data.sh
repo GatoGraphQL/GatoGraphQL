9ef9d425b3a937c9a653a337230f0f45fa04a342
Leonardo Losoviz <leo@getpop.org>
2022-07-26 18:09:14 +0800

    reset-db: Delete the uploads folder

--- webservers/graphql-api-for-wp/setup/import-data.sh
+++ webservers/graphql-api-for-wp/setup/import-data.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+# Delete the uploads folder, to avoid duplicated images appending "-1" on the filename
+UPLOADS_DIR=/app/wordpress/wp-content/uploads
+if [ -d "$UPLOADS_DIR" ]; then
+    echo "Deleting the Uploads folder"
+    rm -rf $UPLOADS_DIR
+fi
+
 wp import /app/assets/graphql-api-data.xml --authors=create --path=/app/wordpress
