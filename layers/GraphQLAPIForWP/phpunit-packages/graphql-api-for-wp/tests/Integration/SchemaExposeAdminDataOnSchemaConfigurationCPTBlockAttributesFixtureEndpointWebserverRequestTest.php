9438470965f2f9a057b960bd3a145306866ad611
Leonardo Losoviz <leo@getpop.org>
2022-07-26 15:32:22 +0800

    Test "expose admin data" in CPT

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/SchemaExposeAdminDataOnSchemaConfigurationCPTBlockAttributesFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\Constants\BlockAttributeNames;
+use GraphQLAPI\GraphQLAPI\Constants\BlockAttributeValues;
+
+class SchemaExposeAdminDataOnSchemaConfigurationCPTBlockAttributesFixtureEndpointWebserverRequestTest extends AbstractModifyCPTBlockAttributesFixtureEndpointWebserverRequestTestCase
+{
+    public const WEBSITE_SCHEMA_CONFIGURATION_ID = 191;
+
+    protected function getEndpoint(): string
+    {
+        /**
+         * This endpoint:
+         *
+         * - Has "Expose admin elements in the schema?" as "Do not expose"
+         * - Has the Schema Configuration "Website" (with ID 191)
+         */
+        return 'graphql/website/';
+    }
+
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-schema-expose-admin-data-in-cpt';
+    }
+
+    protected function getCPTBlockAttributesNewValue(): array
+    {
+        return [
+            BlockAttributeNames::ENABLED_CONST => BlockAttributeValues::ENABLED,
+        ];
+    }
+
+    protected function getCustomPostID(string $dataName): int
+    {
+        return self::WEBSITE_SCHEMA_CONFIGURATION_ID;
+    }
+
+    protected function getBlockNamespacedID(string $dataName): string
+    {
+        return 'graphql-api/schema-config-expose-admin-data';
+    }
+}
