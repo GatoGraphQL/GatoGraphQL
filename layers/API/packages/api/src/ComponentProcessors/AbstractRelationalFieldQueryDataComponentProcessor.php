9cecfd0317dc8e9f65e5d59350a385674bc882b2
Leonardo Losoviz <leo@getpop.org>
2022-07-27 15:43:30 +0800

    phpdoc

--- layers/API/packages/api/src/ComponentProcessors/AbstractRelationalFieldQueryDataComponentProcessor.php
+++ layers/API/packages/api/src/ComponentProcessors/AbstractRelationalFieldQueryDataComponentProcessor.php
@@ -133,10 +133,58 @@ abstract class AbstractRelationalFieldQueryDataComponentProcessor extends Abstra
         $fragments = $executableDocument->getDocument()->getFragments();
         $fieldFragmentModelsTuples = [];
         $requestedOperations = $executableDocument->getRequestedOperations();
+
         /**
          * Multiple Query Execution: In order to have the fields
-         * of the operations be resolved in the same order as the
-         * operations, wrap them on a "self" field.
+         * of the subsequent operations be resolved in the same
+         * order as the operations (which is necessary for `@export`
+         * to work), then wrap them on a "self" field.
+         *
+         * For instance, in the following GraphQL query:
+         *
+         *     query One {
+         *       user(by: {id: 1}) {
+         *         name @export(as: "_name")
+         *       }
+         *     }
+         *
+         *     query Two {
+         *       firstEcho: echo(value: $_name) @upperCase @export(as: "_ucName")
+         *     }
+         *
+         *     query Three {
+         *       secondEcho: echo(value: $_ucName)
+         *     }
+         * 
+         * `firstEcho` is normally resolved on the first iteration for `Root`,
+         * that is before `name @export(as: "_name")` is resolved on the
+         * second iteration on `User`.
+         *
+         * For that reason, the fields are wrapped in `self`, and the query
+         * above is converted to:
+         *
+         *     query One {
+         *       user(by: {id: 1}) {
+         *         name @export(as: "_name")
+         *       }
+         *     }
+         *
+         *     query Two {
+         *       self {
+         *         firstEcho: echo(value: $_name) @upperCase @export(as: "_ucName")
+         *       }
+         *     }
+         *
+         *     query Three {
+         *       self {
+         *         self {
+         *           secondEcho: echo(value: $_ucName)
+         *         }
+         *       }
+         *     }
+         *
+         * Now, `firstEcho` is resolved on the third iteration (second on `Root`),
+         * which is after `name @export(as: "_name")`.
          */
         $requestedOperationsCount = count($requestedOperations);
         for ($operationOrder = 0; $operationOrder < $requestedOperationsCount; $operationOrder++) {
