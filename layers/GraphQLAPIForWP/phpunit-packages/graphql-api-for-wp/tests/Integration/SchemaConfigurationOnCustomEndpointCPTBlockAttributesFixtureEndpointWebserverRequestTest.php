1bbacb6324d880b6fb345bc25d34fb59a900538b
Leonardo Losoviz <leo@getpop.org>
2022-07-27 09:11:10 +0800

    Test "Schema Configuration" on Custom Endpoint in CPT

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/SchemaConfigurationOnCustomEndpointCPTBlockAttributesFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\Services\Blocks\EndpointSchemaConfigurationBlock;
+
+class SchemaConfigurationOnCustomEndpointCPTBlockAttributesFixtureEndpointWebserverRequestTest extends AbstractModifyCPTBlockAttributesFixtureEndpointWebserverRequestTestCase
+{
+    public const WEBSITE_CUSTOM_ENDPOINT_ID = 196;
+    public const POWER_USERS_SCHEMA_CONFIGURATION_ID = 261;
+
+    protected function getEndpoint(): string
+    {
+        /**
+         * This endpoint:
+         *
+         * - Originally has the Schema Configuration "Website" (with ID 191)
+         * - Then changed to "Power users" (with ID 261)
+         */
+        return 'graphql/website/';
+    }
+
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-schema-config-on-custom-endpoint-in-cpt';
+    }
+
+    protected function getCPTBlockAttributesNewValue(): array
+    {
+        return [
+            EndpointSchemaConfigurationBlock::ATTRIBUTE_NAME_SCHEMA_CONFIGURATION => self::POWER_USERS_SCHEMA_CONFIGURATION_ID,
+        ];
+    }
+
+    protected function getCustomPostID(string $dataName): int
+    {
+        return self::WEBSITE_CUSTOM_ENDPOINT_ID;
+    }
+
+    protected function getBlockNamespacedID(string $dataName): string
+    {
+        return 'graphql-api/schema-configuration';
+    }
+}
