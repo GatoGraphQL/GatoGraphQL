30bbf5f60a86be99cc5c43412d4b24cbed8795cd
Leonardo Losoviz <leo@getpop.org>
2022-07-27 10:35:02 +0800

    Test CustomEndpointOptions

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/CustomEndpointOptionsOnCustomEndpointCPTBlockAttributesFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\Constants\BlockAttributeNames;
+
+class CustomEndpointOptionsOnCustomEndpointCPTBlockAttributesFixtureEndpointWebserverRequestTest extends AbstractModifyCPTBlockAttributesFixtureEndpointWebserverRequestTestCase
+{
+    public const WEBSITE_CUSTOM_ENDPOINT_ID = 196;
+
+    protected function getEndpoint(): string
+    {
+        return 'graphql/website/';
+    }
+
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-custom-endpoint-options-on-custom-endpoint-in-cpt';
+    }
+
+    protected function getCPTBlockAttributesNewValue(): array
+    {
+        return [
+            BlockAttributeNames::IS_ENABLED => false,
+        ];
+    }
+
+    protected function getCustomPostID(string $dataName): int
+    {
+        return self::WEBSITE_CUSTOM_ENDPOINT_ID;
+    }
+
+    protected function getBlockNamespacedID(string $dataName): string
+    {
+        return 'graphql-api/custom-endpoint-options';
+    }
+
+    /**
+     * This test disables the endpoint, then update the providerItem
+     * via code.
+     *
+     * @param array<string,mixed> $providerItems
+     * @return array<string,mixed>
+     */
+    protected function customizeProviderEndpointEntries(array $providerItems): array
+    {
+        // expectedContentType
+        $providerItems['query'][0] = 'text/html';
+        // expectedResponseBody. null => no exection of test
+        $providerItems['query'][1] = null;
+        return $providerItems;
+    }
+}
