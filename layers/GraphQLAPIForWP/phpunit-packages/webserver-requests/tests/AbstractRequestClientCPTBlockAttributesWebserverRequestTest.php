292bd5a9889567a145f21678bf04709ad0481cef
Leonardo Losoviz <leo@getpop.org>
2022-07-27 11:53:30 +0800

    Added "original" providerItem

--- layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/tests/AbstractRequestClientCPTBlockAttributesWebserverRequestTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/tests/AbstractRequestClientCPTBlockAttributesWebserverRequestTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PHPUnitForGraphQLAPI\WebserverRequests;
 
 use GraphQLAPI\GraphQLAPI\Constants\ModuleSettingOptions;
+use GraphQLByPoP\GraphQLClientsForWP\Constants\CustomHeaders;
 
 /**
  * Test that accepting a URL produces the expected status code
@@ -13,24 +14,48 @@ abstract class AbstractRequestClientCPTBlockAttributesWebserverRequestTest exten
 {
     use RequestURLWebserverRequestTestCaseTrait;
 
+    protected function executeCPTBlockAttributesSetUpTearDown(string $dataName): bool
+    {
+        if ($dataName === 'original') {
+            return false;
+        }
+        return true;
+    }
+
     /**
      * Test that:
      *
      * 1. The enabled client returns a 200
      * 2. The disabled client returns a 404
+     * 
+     * @dataProvider provideClientEnabledDisabledEntries
      */
-    public function testClientEnabledDisabled(): void
+    public function testClientEnabledDisabled(bool $enabled): void
     {
         $this->doTestClientEnabledDisabled(
             $this->getClientURL(),
+            $enabled,
         );
+    }
+
+    protected function provideClientEnabledDisabledEntries(): array
+    {
+        return [
+            'original' => [true],
+            'updated' => [false],
+        ];
     }
 
     protected function doTestClientEnabledDisabled(
         string $clientURL,
+        bool $enabled,
     ): void {
-        // $this->doTestEnabledOrDisabledPath($clientURL, 200, 'application/json', true);
-        $this->doTestEnabledOrDisabledPath($clientURL, 404, null, false);
+        $this->doTestEnabledOrDisabledPath($clientURL, 200, null, $enabled);
+    }
+
+    protected function getCustomHeader(): ?string
+    {
+        return CustomHeaders::CLIENT_ENDPOINT;
     }
 
     abstract protected function getClientURL(): string;
