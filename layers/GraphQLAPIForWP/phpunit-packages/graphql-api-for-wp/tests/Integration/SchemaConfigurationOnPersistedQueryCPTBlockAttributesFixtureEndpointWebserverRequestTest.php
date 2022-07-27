73dffe403f377fba7b5866b686c5015da24f9b76
Leonardo Losoviz <leo@getpop.org>
2022-07-27 09:23:39 +0800

    Test SchemaConfiguration on Persisted Query in CPT

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/SchemaConfigurationOnPersistedQueryCPTBlockAttributesFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\Services\Blocks\EndpointSchemaConfigurationBlock;
+
+class SchemaConfigurationOnPersistedQueryCPTBlockAttributesFixtureEndpointWebserverRequestTest extends AbstractModifyCPTBlockAttributesFixtureEndpointWebserverRequestTestCase
+{
+    public const LATEST_POSTS_FOR_MOBILE_APP_PERSISTED_QUERY_ID = 65;
+    public const POWER_USERS_SCHEMA_CONFIGURATION_ID = 261;
+
+    protected function getEndpoint(): string
+    {
+        /**
+         * This endpoint:
+         *
+         * - Originally has the Schema Configuration "Mobile App"
+         * - Then changed to "Power users"
+         */
+        return 'graphql-query/latest-posts-for-mobile-app/';
+    }
+
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-schema-config-on-persisted-query-in-cpt';
+    }
+
+    protected function getCPTBlockAttributesNewValue(): array
+    {
+        return [
+            EndpointSchemaConfigurationBlock::ATTRIBUTE_NAME_SCHEMA_CONFIGURATION => self::POWER_USERS_SCHEMA_CONFIGURATION_ID,
+        ];
+    }
+
+    protected function getCustomPostID(string $dataName): int
+    {
+        return self::LATEST_POSTS_FOR_MOBILE_APP_PERSISTED_QUERY_ID;
+    }
+
+    protected function getBlockNamespacedID(string $dataName): string
+    {
+        return 'graphql-api/schema-configuration';
+    }
+}
