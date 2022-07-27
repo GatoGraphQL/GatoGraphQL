ae157f7abd029dcd9ea04f34cbc92c95fade932a
Leonardo Losoviz <leo@getpop.org>
2022-07-27 12:09:34 +0800

    Test Voyager client

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/ExposeVoyagerClientOnCustomEndpointCPTBlockAttributesFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\Constants\RequestParams;
+
+class ExposeVoyagerClientOnCustomEndpointCPTBlockAttributesFixtureEndpointWebserverRequestTest extends AbstractExposeClientOnCustomEndpointCPTBlockAttributesFixtureEndpointWebserverRequestTest
+{
+    public const WEBSITE_CUSTOM_ENDPOINT_ID = 196;
+
+    protected function getEndpoint(): string
+    {
+        return 'graphql/website/';
+    }
+
+    protected function getClientName(): string
+    {
+        return RequestParams::VIEW_SCHEMA;
+    }
+
+    protected function getCustomPostID(string $dataName): int
+    {
+        return self::WEBSITE_CUSTOM_ENDPOINT_ID;
+    }
+
+    protected function getBlockNamespacedID(string $dataName): string
+    {
+        return 'graphql-api/endpoint-voyager';
+    }
+}
