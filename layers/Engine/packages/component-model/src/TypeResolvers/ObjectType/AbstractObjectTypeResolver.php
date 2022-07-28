841b9785bffd0a1125d31ff7ab78f75ca6c3c4fb
Leonardo Losoviz <leo@getpop.org>
2022-07-28 14:23:41 +0800

    Removed from finally

--- layers/Engine/packages/component-model/src/TypeResolvers/ObjectType/AbstractObjectTypeResolver.php
+++ layers/Engine/packages/component-model/src/TypeResolvers/ObjectType/AbstractObjectTypeResolver.php
@@ -512,20 +512,19 @@ abstract class AbstractObjectTypeResolver extends AbstractRelationalTypeResolver
                     $field,
                 )
             );
-            return null;
-        } finally {
-            /**
-             * Set the resolved value (null or otherwise) to the AppState
-             * to resolve the FieldValuePromises
-             *
-             * @var SplObjectStorage<FieldInterface,mixed>
-             */
-            $resolvedFieldValues = App::getState('resolved-field-values');
-            $resolvedFieldValues[$field] = $value;
-
-            $appStateManager = App::getAppStateManager();
-            $appStateManager->override('resolved-field-values', $resolvedFieldValues);
         }
+
+        /**
+         * Set the resolved value (null or otherwise) to the AppState
+         * to resolve the FieldValuePromises
+         *
+         * @var SplObjectStorage<FieldInterface,mixed>
+         */
+        $resolvedFieldValues = App::getState('resolved-field-values');
+        $resolvedFieldValues[$field] = $value;
+
+        $appStateManager = App::getAppStateManager();
+        $appStateManager->override('resolved-field-values', $resolvedFieldValues);
 
         /**
          * If there were errors, return already
