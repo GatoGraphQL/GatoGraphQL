7d9eebc42d51b3f0239b15aab8841ac5b7196649
Leonardo Losoviz <leo@getpop.org>
2022-07-29 10:19:01 +0800

    Execute service

--- layers/API/packages/api/src/ComponentProcessors/AbstractRelationalFieldQueryDataComponentProcessor.php
+++ layers/API/packages/api/src/ComponentProcessors/AbstractRelationalFieldQueryDataComponentProcessor.php
@@ -143,88 +143,17 @@ abstract class AbstractRelationalFieldQueryDataComponentProcessor extends Abstra
         $fragments = $executableDocument->getDocument()->getFragments();
         $fieldFragmentModelsTuples = [];
         $requestedOperations = $executableDocument->getRequestedOperations();
-
+        
         /**
          * Multiple Query Execution: In order to have the fields
          * of the subsequent operations be resolved in the same
          * order as the operations (which is necessary for `@export`
          * to work), then wrap them on a "self" field.
-         *
-         * For instance, in the following GraphQL query:
-         *
-         *     query One {
-         *       user(by: {id: 1}) {
-         *         name @export(as: "_name")
-         *       }
-         *     }
-         *
-         *     query Two {
-         *       firstEcho: echo(value: $_name) @upperCase @export(as: "_ucName")
-         *     }
-         *
-         *     query Three {
-         *       secondEcho: echo(value: $_ucName)
-         *     }
-         *
-         * `firstEcho` is normally resolved on the first iteration for `Root`,
-         * that is before `name @export(as: "_name")` is resolved on the
-         * second iteration on `User`.
-         *
-         * For that reason, the fields are wrapped in `self`, and the query
-         * above is converted to:
-         *
-         *     query One {
-         *       user(by: {id: 1}) {
-         *         name @export(as: "_name")
-         *       }
-         *     }
-         *
-         *     query Two {
-         *       self {
-         *         firstEcho: echo(value: $_name) @upperCase @export(as: "_ucName")
-         *       }
-         *     }
-         *
-         *     query Three {
-         *       self {
-         *         self {
-         *           secondEcho: echo(value: $_ucName)
-         *         }
-         *       }
-         *     }
-         *
-         * Now, `firstEcho` is resolved on the third iteration (second on `Root`),
-         * which is after `name @export(as: "_name")`.
          */
-        $requestedOperationsCount = count($requestedOperations);
-        for ($operationOrder = 0; $operationOrder < $requestedOperationsCount; $operationOrder++) {
-            $operation = $requestedOperations[$operationOrder];
-            $fieldOrFragmentBonds = $operation->getFieldsOrFragmentBonds();
-            for ($i = 0; $i < $operationOrder; $i++) {
-                /**
-                 * Use an alias to both help visualize which is the field (optional),
-                 * and get its cached instance (mandatory!)
-                 */
-                $alias = sprintf(
-                    '_%s_op%s_level%s_',
-                    'dynamicSelf',
-                    $operationOrder,
-                    $i
-                );
-                if (!isset($this->fieldInstanceContainer[$alias])) {
-                    $this->fieldInstanceContainer[$alias] = new RelationalField(
-                        'self',
-                        $alias,
-                        [],
-                        $fieldOrFragmentBonds,
-                        [],
-                        LocationHelper::getNonSpecificLocation()
-                    );
-                }
-                $fieldOrFragmentBonds = [
-                    $this->fieldInstanceContainer[$alias],
-                ];
-            }
+        $operationFieldOrFragmentBonds = $this->getQueryASTTransformationService()->prepareOperationFieldAndFragmentBondsForMultipleQueryExecution($requestedOperations);
+        /** @var OperationInterface $operation */
+        foreach ($operationFieldOrFragmentBonds as $operation) {
+            $fieldOrFragmentBonds = $operationFieldOrFragmentBonds[$operation];
             $fieldFragmentModelsTuples = array_merge(
                 $fieldFragmentModelsTuples,
                 $this->getAllFieldFragmentModelsTuplesFromFieldsOrFragmentBonds(
