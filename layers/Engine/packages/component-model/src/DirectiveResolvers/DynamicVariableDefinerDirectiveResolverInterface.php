127f34670ca9e26e9037e5db5bb5e05524e755bb
Leonardo Losoviz <leo@getpop.org>
2022-08-01 09:55:29 +0800

    phpdoc

--- layers/Engine/packages/component-model/src/DirectiveResolvers/DynamicVariableDefinerDirectiveResolverInterface.php
+++ layers/Engine/packages/component-model/src/DirectiveResolvers/DynamicVariableDefinerDirectiveResolverInterface.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\DirectiveResolvers;
 
+/**
+ * Interface that indicates what directives will export values
+ * to dynamic variables.
+ *
+ * By default, this is expected to be `@export(as: "variableName")`,
+ * but the GraphQL spec does not define it. Then, through this
+ * interface, we can provide different variations, or even
+ * different directives, to provide this functionality.
+ *
+ * @see https://github.com/graphql/graphql-spec/issues/583
+ */
 interface DynamicVariableDefinerDirectiveResolverInterface extends DirectiveResolverInterface
 {
     /**
