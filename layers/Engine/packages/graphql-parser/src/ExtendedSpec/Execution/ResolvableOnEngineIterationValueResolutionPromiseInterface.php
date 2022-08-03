bb14cc925b70e7a3615b40cd923631087a1b58b9
Leonardo Losoviz <leo@getpop.org>
2022-08-03 17:30:36 +0800

    Added interfaces

---
+++ layers/Engine/packages/graphql-parser/src/ExtendedSpec/Execution/ResolvableOnEngineIterationValueResolutionPromiseInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\GraphQLParser\ExtendedSpec\Execution;
+
+interface ResolvableOnEngineIterationValueResolutionPromiseInterface extends ValueResolutionPromiseInterface
+{
+}
