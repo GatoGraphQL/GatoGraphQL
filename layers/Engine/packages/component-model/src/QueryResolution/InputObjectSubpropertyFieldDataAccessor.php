f341f989cfc6d15cebf0d32b627898e8759b6461
Leonardo Losoviz <leo@getpop.org>
2022-08-03 09:44:24 +0800

    Simplified getInputObjectValue

--- layers/Engine/packages/component-model/src/QueryResolution/InputObjectSubpropertyFieldDataAccessor.php
+++ layers/Engine/packages/component-model/src/QueryResolution/InputObjectSubpropertyFieldDataAccessor.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace PoP\ComponentModel\QueryResolution;
 
 use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
-use PoP\Root\Exception\ShouldNotHappenException;
 use PoP\Root\Services\StandaloneServiceTrait;
 use stdClass;
 
@@ -34,22 +33,8 @@ class InputObjectSubpropertyFieldDataAccessor extends FieldDataAccessor implemen
         return (array) $this->getInputObjectValue();
     }
 
-    /**
-     * @throws ShouldNotHappenException If the argument value under the provided inputName is not an InputObject
-     */
     protected function getInputObjectValue(): stdClass
     {
-        $inputObjectValue = $this->getResolvedFieldArgs()[$this->getInputObjectSubpropertyName()];
-        if (!($inputObjectValue instanceof stdClass)) {
-            throw new ShouldNotHappenException(
-                sprintf(
-                    $this->__(
-                        'Input value under argument \'%s\' is not an InputObject type'
-                    ),
-                    $this->getInputObjectSubpropertyName()
-                )
-            );
-        }
-        return $inputObjectValue;
+        return $this->getResolvedFieldArgs()[$this->getInputObjectSubpropertyName()];
     }
 }
