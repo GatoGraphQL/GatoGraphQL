8ddda03bb636d7d4d80d29babce4e947933d4a85
Leonardo Losoviz <leo@getpop.org>
2022-08-04 17:09:09 +0800

    Added service

---
+++ layers/Engine/packages/component-model/src/TypeSerialization/LeafOutputTypeSerializationService.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\TypeSerialization;
+
+class LeafOutputTypeSerializationService implements LeafOutputTypeSerializationServiceInterface
+{
+}
