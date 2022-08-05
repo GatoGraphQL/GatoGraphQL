fac601d44085a200b962141092b7fadc52c2cbfa
Leonardo Losoviz <leo@getpop.org>
2022-08-05 10:38:21 +0800

    GraphQLQueryParsingPayload

---
+++ layers/API/packages/api/src/ObjectModels/GraphQLQueryParsingPayload.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPAPI\ObjectModels\GraphQLQueryParsingPayload;
+
+use PoP\ComponentModel\ExtendedSpec\Execution\ExecutableDocument;
+
+class GraphQLQueryParsingPayload
+{
+    public function __construct(
+        public ExecutableDocument $executableDocument,
+    ) {
+    }
+}
