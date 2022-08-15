8ba47438b8abfa00d3b8d72f7d0f2a4665429735
Leonardo Losoviz <leo@getpop.org>
2022-08-15 14:32:19 +0800

    Removed addSpacingToExpressions

--- layers/API/packages/api/src/PersistedQueries/PersistedQueryUtils.php
+++ layers/API/packages/api/src/PersistedQueries/PersistedQueryUtils.php
@@ -13,14 +13,4 @@ class PersistedQueryUtils
     {
         return preg_replace('/[ ]{2,}|[\t]|[\n]/', '', trim($fragmentResolution));
     }
-
-    /**
-     * Symfony's DependencyInjection component uses format "%parameter%", and PoP API uses format "%expression%",
-     * so when passing an expression like "%{self}%" it throws an exception, expecting this to be a parameter (which doesn't exist!)
-     * To fix it, we add a space in all expressions like this: "% expression %", which works for the PoP API since the expression name is trimmed
-     */
-    public static function addSpacingToExpressions(string $fragmentResolution): string
-    {
-        return preg_replace('/%([\s\S]+?)%/', '% $1 %', $fragmentResolution);
-    }
 }
