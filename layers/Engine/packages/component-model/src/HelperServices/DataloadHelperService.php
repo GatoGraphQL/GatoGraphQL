7b0d0b5cf33c4ca0cfffea7f593c99ade70eebec
Leonardo Losoviz <leo@getpop.org>
2022-08-16 13:58:00 +0800

    phpdoc

--- layers/Engine/packages/component-model/src/HelperServices/DataloadHelperService.php
+++ layers/Engine/packages/component-model/src/HelperServices/DataloadHelperService.php
@@ -30,14 +30,6 @@ class DataloadHelperService implements DataloadHelperServiceInterface
      * Accept RelationalTypeResolverInterface as param, instead of the more natural
      * ObjectTypeResolverInterface, to make it easy within the application to check
      * for this result without checking in advance what's the typeResolver.
-     *
-     * If the FeedbackStore is provided, report errors in the GraphQL query,
-     * such as nested fields requested on leaf fields:
-     *
-     *   `{ id { id } }`
-     *
-     * This is optional as this method is called in multiple places,
-     * but the error needs to be added only once.
      */
     public function getTypeResolverFromSubcomponentField(
         RelationalTypeResolverInterface $relationalTypeResolver,
