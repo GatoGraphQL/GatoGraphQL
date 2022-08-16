99ccc2ef2c7ad05f76c83bdf1e08f6ac47f8417c
Leonardo Losoviz <leo@getpop.org>
2022-08-16 16:36:01 +0800

    Validate on schema

--- layers/Engine/packages/component-model/src/DirectiveResolvers/ValidateDirectiveResolver.php
+++ layers/Engine/packages/component-model/src/DirectiveResolvers/ValidateDirectiveResolver.php
@@ -15,6 +15,7 @@ use PoP\ComponentModel\StaticHelpers\MethodHelpers;
 use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\PipelinePositions;
 use PoP\ComponentModel\TypeResolvers\RelationalTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\UnionType\UnionTypeResolverInterface;
 use PoP\ComponentModel\TypeSerialization\TypeSerializationServiceInterface;
 use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
 use PoP\GraphQLParser\Spec\Parser\Ast\RelationalField;
@@ -56,6 +57,10 @@ final class ValidateDirectiveResolver extends AbstractGlobalDirectiveResolver im
     }
 
     /**
+     * Validate at the schema level first that Fields
+     * exist, and RelationalFields are indeed relational
+     * in the resolver.
+     *
      * @param array<string|int,EngineIterationFieldSet> $idFieldSet
      * @param array<array<string|int,EngineIterationFieldSet>> $succeedingPipelineIDFieldSet
      * @param array<FieldDataAccessProviderInterface> $succeedingPipelineFieldDataAccessProviders
@@ -76,76 +81,125 @@ final class ValidateDirectiveResolver extends AbstractGlobalDirectiveResolver im
         array &$messages,
         EngineIterationFeedbackStore $engineIterationFeedbackStore,
     ): void {
-        /**
-         * Validate at the schema level first that Fields
-         * exist, and RelationalFields are indeed relational
-         * in the resolver.
-         */
-        if ($relationalTypeResolver instanceof ObjectTypeResolverInterface) {
-            /** @var ObjectTypeResolverInterface */
-            $objectTypeResolver = $relationalTypeResolver;
-            $fields = MethodHelpers::getFieldsFromIDFieldSet($idFieldSet);
-            foreach ($fields as $field) {
-                $objectTypeFieldResolver = $objectTypeResolver->getExecutableObjectTypeFieldResolverForField($field);
-                if ($objectTypeFieldResolver === null) {
-                    $this->processFailure(
-                        $relationalTypeResolver,
-                        new FeedbackItemResolution(
-                            FieldResolutionErrorFeedbackItemProvider::class,
-                            FieldResolutionErrorFeedbackItemProvider::E1,
-                            [
-                                $field->getName(),
-                                $objectTypeResolver->getMaybeNamespacedTypeName(),
-                            ]
-                        ),
-                        [$field],
+        if ($relationalTypeResolver instanceof UnionTypeResolverInterface) {
+            /** @var UnionTypeResolverInterface */
+            $unionTypeResolver = $relationalTypeResolver;
+            /** @var SplObjectStorage<ObjectTypeResolverInterface,FieldInterface> */
+            $processedObjectTypeResolverFields = new SplObjectStorage();
+            foreach ($idFieldSet as $id => $fieldSet) {
+                $object = $idObjects[$id] ?? null;
+                if ($object === null) {
+                    continue;
+                }
+                $targetObjectTypeResolver = $unionTypeResolver->getTargetObjectTypeResolver($object);
+                if ($targetObjectTypeResolver === null) {
+                    continue;
+                }
+                $processedFields = $processedObjectTypeResolverFields[$targetObjectTypeResolver] ?? [];
+                foreach ($fieldSet->fields as $field) {
+                    if (in_array($field, $processedFields)) {
+                        continue;
+                    }
+                    $processedFields[] = $field;
+                    $this->executeValidationForField(
+                        $targetObjectTypeResolver,
+                        $field,
                         $idFieldSet,
                         $succeedingPipelineIDFieldSet,
-                        $this->directive,
                         $resolvedIDFieldValues,
                         $engineIterationFeedbackStore,
                     );
-                    continue;
                 }
-
-                /**
-                 * Validate that a RelationalField in the AST is not actually
-                 * a leaf field in the resolver.
-                 *
-                 * Eg: field "id" is built as RelationalField in the AST, but it is
-                 * not a connection:
-                 *
-                 *   ```
-                 *   {
-                 *     id {
-                 *       __typename
-                 *     }
-                 *   }
-                 *   ```
-                 */
-                if ($field instanceof RelationalField
-                    && !($objectTypeFieldResolver->getFieldTypeResolver($objectTypeResolver, $field->getName()) instanceof RelationalTypeResolverInterface)
-                ) {
-                    $this->processFailure(
-                        $relationalTypeResolver,
-                        new FeedbackItemResolution(
-                            ErrorFeedbackItemProvider::class,
-                            ErrorFeedbackItemProvider::E1,
-                            [
-                                $field->getOutputKey(),
-                                $objectTypeResolver->getMaybeNamespacedTypeName(),
-                            ]
-                        ),
-                        [$field],
-                        $idFieldSet,
-                        $succeedingPipelineIDFieldSet,
-                        $this->directive,
-                        $resolvedIDFieldValues,
-                        $engineIterationFeedbackStore,
-                    );
-                    continue;
-                }
+                $processedObjectTypeResolverFields[$targetObjectTypeResolver] = $processedFields;
             }
+            return;
+        }
+
+        /** @var ObjectTypeResolverInterface */
+        $objectTypeResolver = $relationalTypeResolver;
+        $fields = MethodHelpers::getFieldsFromIDFieldSet($idFieldSet);
+        foreach ($fields as $field) {
+            $this->executeValidationForField(
+                $objectTypeResolver,
+                $field,
+                $idFieldSet,
+                $succeedingPipelineIDFieldSet,
+                $resolvedIDFieldValues,
+                $engineIterationFeedbackStore,
+            );
+        }
+    }
+
+    /**
+     * @param array<string|int,EngineIterationFieldSet> $idFieldSet
+     * @param array<array<string|int,EngineIterationFieldSet>> $succeedingPipelineIDFieldSet
+     * @param array<string|int,SplObjectStorage<FieldInterface,mixed>> $resolvedIDFieldValues
+     */
+    protected function executeValidationForField(
+        ObjectTypeResolverInterface $objectTypeResolver,
+        FieldInterface $field,
+        array $idFieldSet,
+        array &$succeedingPipelineIDFieldSet,
+        array &$resolvedIDFieldValues,
+        EngineIterationFeedbackStore $engineIterationFeedbackStore,
+    ): void {
+        $objectTypeFieldResolver = $objectTypeResolver->getExecutableObjectTypeFieldResolverForField($field);
+        if ($objectTypeFieldResolver === null) {
+            $this->processFailure(
+                $objectTypeResolver,
+                new FeedbackItemResolution(
+                    FieldResolutionErrorFeedbackItemProvider::class,
+                    FieldResolutionErrorFeedbackItemProvider::E1,
+                    [
+                        $field->getName(),
+                        $objectTypeResolver->getMaybeNamespacedTypeName(),
+                    ]
+                ),
+                [$field],
+                $idFieldSet,
+                $succeedingPipelineIDFieldSet,
+                $this->directive,
+                $resolvedIDFieldValues,
+                $engineIterationFeedbackStore,
+            );
+            return;
+        }
+
+        /**
+         * Validate that a RelationalField in the AST is not actually
+         * a leaf field in the resolver.
+         *
+         * Eg: field "id" is built as RelationalField in the AST, but it is
+         * not a connection:
+         *
+         *   ```
+         *   {
+         *     id {
+         *       __typename
+         *     }
+         *   }
+         *   ```
+         */
+        if ($field instanceof RelationalField
+            && !($objectTypeFieldResolver->getFieldTypeResolver($objectTypeResolver, $field->getName()) instanceof RelationalTypeResolverInterface)
+        ) {
+            $this->processFailure(
+                $objectTypeResolver,
+                new FeedbackItemResolution(
+                    ErrorFeedbackItemProvider::class,
+                    ErrorFeedbackItemProvider::E1,
+                    [
+                        $field->getOutputKey(),
+                        $objectTypeResolver->getMaybeNamespacedTypeName(),
+                    ]
+                ),
+                [$field],
+                $idFieldSet,
+                $succeedingPipelineIDFieldSet,
+                $this->directive,
+                $resolvedIDFieldValues,
+                $engineIterationFeedbackStore,
+            );
         }
     }
 
