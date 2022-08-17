bc2893e31ef4fb64cea572a88006a1843d2cf790
Leonardo Losoviz <leo@getpop.org>
2022-08-17 17:45:03 +0800

    Removed commented code

--- layers/Engine/packages/component-model/src/MutationResolvers/AbstractOneofMutationResolver.php
+++ layers/Engine/packages/component-model/src/MutationResolvers/AbstractOneofMutationResolver.php
@@ -86,28 +86,6 @@ abstract class AbstractOneofMutationResolver extends AbstractMutationResolver
     }
 
     /**
-     * @todo Review this commenting works for different oneof mutations
-     * eg: http://graphql-by-pop-pro.lndo.site/graphiql/?query=mutation%20LoginUser%20%7B%0A%20%20loginUser(by%3A%20%7Bcredentials%3A%20%7BusernameOrEmail%3A%20%22admin%22%2C%20password%3A%20%22admin%22%7D%7D)%20%7B%0A%20%20%20%20id%0A%20%20%20%20name%0A%20%20%7D%0A%7D&operationName=LoginUser&variables=%7B%0A%20%20%22authorID%22%3A%203%0A%7D
-     */
-    // /**
-    //  * @return array<string,mixed>|stdClass
-    //  * @throws QueryResolutionException If the form data for the input field is not present in the array
-    //  */
-    // protected function getInputFieldFormData(string $inputFieldName, stdClass $oneofInputObjectFormData): array|stdClass
-    // {
-    //     $inputFieldFormData = $oneofInputObjectFormData->$inputFieldName ?? null;
-    //     if ($inputFieldFormData === null) {
-    //         throw new QueryResolutionException(
-    //             sprintf(
-    //                 $this->__('There is not form data for input field with name \'%s\'', 'component-model'),
-    //                 $inputFieldName
-    //             )
-    //         );
-    //     }
-    //     return $inputFieldFormData;
-    // }
-
-    /**
      * Assume there's only one argument in the field,
      * for this OneofMutationResolver.
      * If that's not the case, this function must be overriden,
@@ -194,12 +172,6 @@ abstract class AbstractOneofMutationResolver extends AbstractMutationResolver
             $inputObjectFieldArgumentFieldDataAccessor,
             $oneOfPropertyName
         );
-        /**
-         * @todo Review this commenting works for different oneof mutations
-         * eg: http://graphql-by-pop-pro.lndo.site/graphiql/?query=mutation%20LoginUser%20%7B%0A%20%20loginUser(by%3A%20%7Bcredentials%3A%20%7BusernameOrEmail%3A%20%22admin%22%2C%20password%3A%20%22admin%22%7D%7D)%20%7B%0A%20%20%20%20id%0A%20%20%20%20name%0A%20%20%7D%0A%7D&operationName=LoginUser&variables=%7B%0A%20%20%22authorID%22%3A%203%0A%7D
-         */
-        // $inputFieldFormData = $this->getInputFieldFormData($inputFieldName, $oneofInputObjectFormData);
-        // return [$inputFieldMutationResolver, $inputFieldFormData];
         return [$inputFieldMutationResolver, $oneOfFieldDataAccessor];
     }
 
