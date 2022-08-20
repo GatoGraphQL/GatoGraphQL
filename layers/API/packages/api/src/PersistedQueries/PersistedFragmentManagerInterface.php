a0a84933044afe7888fe4de6704942715d2c3dd6
Leonardo Losoviz <leo@getpop.org>
2022-08-20 15:08:57 +0800

    Fixed PHPStan

--- layers/API/packages/api/src/PersistedQueries/PersistedFragmentManagerInterface.php
+++ layers/API/packages/api/src/PersistedQueries/PersistedFragmentManagerInterface.php
@@ -7,11 +7,11 @@ namespace PoPAPI\API\PersistedQueries;
 interface PersistedFragmentManagerInterface
 {
     /**
-     * @return string[]
+     * @return array<string,string>
      */
     public function getPersistedFragments(): array;
     /**
-     * @return string[]
+     * @return array<string,array<string,string>>
      */
     public function getPersistedFragmentsForSchema(): array;
     public function addPersistedFragment(string $fragmentName, string $fragmentResolution, ?string $description = null): void;
