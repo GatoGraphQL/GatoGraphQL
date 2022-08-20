ac7db63da5f6abe8ce068bfab026f1c61295df72
Leonardo Losoviz <leo@getpop.org>
2022-08-20 17:09:51 +0800

    typed

--- layers/Engine/packages/component-model/src/Schema/SchemaNamespacingServiceInterface.php
+++ layers/Engine/packages/component-model/src/Schema/SchemaNamespacingServiceInterface.php
@@ -6,7 +6,7 @@ namespace PoP\ComponentModel\Schema;
 
 interface SchemaNamespacingServiceInterface
 {
-    public function addSchemaNamespaceForClassOwnerAndProjectNamespace(string $classOwnerAndProjectNamespace, $schemaNamespace): void;
+    public function addSchemaNamespaceForClassOwnerAndProjectNamespace(string $classOwnerAndProjectNamespace, string $schemaNamespace): void;
     public function getSchemaNamespace(string $class): string;
     public function getSchemaNamespacedName(string $schemaNamespace, string $name): string;
 }
