fe68a55d94236c5558e3662e086b6421ea4ab828
Leonardo Losoviz <leo@getpop.org>
2022-08-23 14:45:44 +0800

    Improved test

--- layers/Engine/packages/graphql-parser/tests/Ast/FieldEqualsToTest.php
+++ layers/Engine/packages/graphql-parser/tests/Ast/FieldEqualsToTest.php
@@ -140,6 +140,14 @@ class FieldEqualsToTest extends AbstractTestCase
      */
     protected function getDoesNotEqualToLeafFields(): array
     {
+        $inputObject1 = new stdClass();
+        $inputObject1->someLiteral = new Literal('someValue', new Location(1, 1));
+        $inputObject2 = new stdClass();
+        $inputObject2->anotherLiteral = new Literal('someValue', new Location(2, 2));
+        $inputObject3 = new stdClass();
+        $inputObject3->literal = new Literal('someValue', new Location(1, 1));
+        $inputObject4 = new stdClass();
+        $inputObject4->literal = new Literal('anotherValue', new Location(2, 2));
         return [
             'name' => [
                 new LeafField('someField', null, [], [], new Location(1, 1)),
@@ -169,13 +177,25 @@ class FieldEqualsToTest extends AbstractTestCase
                 new LeafField('someField', null, [new Argument('someArg', new Literal('someValue', new Location(1, 1)), new Location(1, 1))], [], new Location(1, 1)),
                 new LeafField('someField', null, [new Argument('someArg', new Literal('someValue', new Location(1, 1)), new Location(1, 1)), new Argument('someArg', new Literal('someValue', new Location(1, 1)), new Location(1, 1))], [], new Location(2, 2)),
             ],
-            'args-with-different-values' => [
+            'args-with-different-literal-values' => [
                 new LeafField('someField', null, [new Argument('someArg', new Literal('someValue', new Location(1, 1)), new Location(1, 1))], [], new Location(1, 1)),
                 new LeafField('someField', null, [new Argument('someArg', new Literal('anotherValue', new Location(1, 1)), new Location(1, 1))], [], new Location(2, 2)),
             ],
-            'args-with-different-values-2' => [
+            'args-with-different-enum-values' => [
+                new LeafField('someField', null, [new Argument('someArg', new Enum('someValue', new Location(1, 1)), new Location(1, 1))], [], new Location(1, 1)),
+                new LeafField('someField', null, [new Argument('someArg', new Enum('anotherValue', new Location(1, 1)), new Location(1, 1))], [], new Location(2, 2)),
+            ],
+            'args-with-different-variable-reference-values' => [
                 new LeafField('someField', null, [new Argument('someArg', new VariableReference('someVariable', null, new Location(1, 1)), new Location(1, 1))], [], new Location(1, 1)),
                 new LeafField('someField', null, [new Argument('someArg', new VariableReference('anotherVariable', null, new Location(1, 1)), new Location(1, 1))], [], new Location(2, 2)),
+            ],
+            'args-with-different-input-object-values' => [
+                new LeafField('someField', null, [new Argument('someArg', new InputObject($inputObject1, new Location(1, 1)), new Location(1, 1))], [], new Location(1, 1)),
+                new LeafField('someField', null, [new Argument('someArg', new InputObject($inputObject2, new Location(2, 2)), new Location(2, 2))], [], new Location(2, 2)),
+            ],
+            'args-with-different-input-object-values-2' => [
+                new LeafField('someField', null, [new Argument('someArg', new InputObject($inputObject3, new Location(1, 1)), new Location(1, 1))], [], new Location(1, 1)),
+                new LeafField('someField', null, [new Argument('someArg', new InputObject($inputObject4, new Location(2, 2)), new Location(2, 2))], [], new Location(2, 2)),
             ],
             'unordered-directives' => [
                 new LeafField('someField', null, [], [new Directive('someDirective', [], new Location(1, 1)), new Directive('anotherDirective', [], new Location(1, 1))], new Location(1, 1)),
