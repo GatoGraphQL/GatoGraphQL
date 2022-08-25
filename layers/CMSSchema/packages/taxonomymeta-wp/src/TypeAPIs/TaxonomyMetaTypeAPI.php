7d35f14afe19be232ba929089a7e443d320db0b0
Leonardo Losoviz <leo@getpop.org>
2022-08-25 14:46:44 +0800

    getTaxonomyTermMeta

--- layers/CMSSchema/packages/taxonomymeta-wp/src/TypeAPIs/TaxonomyMetaTypeAPI.php
+++ layers/CMSSchema/packages/taxonomymeta-wp/src/TypeAPIs/TaxonomyMetaTypeAPI.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PoPCMSSchema\TaxonomyMetaWP\TypeAPIs;
 
 use PoPCMSSchema\TaxonomyMeta\TypeAPIs\AbstractTaxonomyMetaTypeAPI;
+use WP_Term;
 
 class TaxonomyMetaTypeAPI extends AbstractTaxonomyMetaTypeAPI
 {
@@ -12,8 +13,16 @@ class TaxonomyMetaTypeAPI extends AbstractTaxonomyMetaTypeAPI
      * If the key is non-existent, return `null`.
      * Otherwise, return the value.
      */
-    protected function doGetTaxonomyMeta(string|int $termID, string $key, bool $single = false): mixed
+    protected function doGetTaxonomyMeta(string|int|object $termObjectOrID, string $key, bool $single = false): mixed
     {
+        if (is_object($termObjectOrID)) {
+            /** @var WP_Term */
+            $term = $termObjectOrID;
+            $termID = $term->term_id;
+        } else {
+            $termID = $termObjectOrID;
+        }
+
         // This function does not differentiate between a stored empty value,
         // and a non-existing key! So if empty, treat it as non-existant and return null
         $value = \get_term_meta((int)$termID, $key, $single);
--- layers/CMSSchema/packages/taxonomymeta/src/TypeAPIs/AbstractTaxonomyMetaTypeAPI.php
+++ layers/CMSSchema/packages/taxonomymeta/src/TypeAPIs/AbstractTaxonomyMetaTypeAPI.php
@@ -21,12 +21,12 @@ abstract class AbstractTaxonomyMetaTypeAPI extends AbstractMetaTypeAPI implement
      * @param array<string,mixed> $options
      * @throws MetaKeyNotAllowedException
      */
-    final public function getTaxonomyTermMeta(string|int $termID, string $key, bool $single = false, array $options = []): mixed
+    final public function getTaxonomyTermMeta(string|int|object $termObjectOrID, string $key, bool $single = false, array $options = []): mixed
     {
         if ($options['assert-is-meta-key-allowed'] ?? null) {
             $this->assertIsMetaKeyAllowed($key);
         }
-        return $this->doGetTaxonomyMeta($termID, $key, $single);
+        return $this->doGetTaxonomyMeta($termObjectOrID, $key, $single);
     }
 
     /**
@@ -49,5 +49,5 @@ abstract class AbstractTaxonomyMetaTypeAPI extends AbstractMetaTypeAPI implement
      * If the key is non-existent, return `null`.
      * Otherwise, return the value.
      */
-    abstract protected function doGetTaxonomyMeta(string|int $termID, string $key, bool $single = false): mixed;
+    abstract protected function doGetTaxonomyMeta(string|int|object $termObjectOrID, string $key, bool $single = false): mixed;
 }
