bbed1deba61a4c118607a93ab1ebe337f846fbb0
Leonardo Losoviz <leo@getpop.org>
2022-09-01 18:04:25 +0800

    addLocalLanguage to block

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/SchemaConfigNamespacingBlock.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/SchemaConfigNamespacingBlock.php
@@ -34,4 +34,21 @@ class SchemaConfigNamespacingBlock extends AbstractDefaultEnableDisableFunctiona
     {
         return \__('Namespacing', 'graphql-api');
     }
+
+    /**
+     * Add the locale language to the localized data?
+     */
+    protected function addLocalLanguage(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Default language for the script/component's documentation
+     */
+    protected function getDefaultLanguage(): ?string
+    {
+        // English
+        return 'en';
+    }
 }
