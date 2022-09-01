cd3d50a149c1341f2da3b5f0c14431d74cf99a3e
Leonardo Losoviz <leo@getpop.org>
2022-09-01 12:43:02 +0800

    Accept string or int in ArrayTraversionHelperService

--- layers/Engine/packages/engine/src/HelperServices/ArrayTraversionHelperService.php
+++ layers/Engine/packages/engine/src/HelperServices/ArrayTraversionHelperService.php
@@ -28,9 +28,9 @@ class ArrayTraversionHelperService implements ArrayTraversionHelperServiceInterf
     /**
      * @throws RuntimeOperationException If the path cannot be reached under the array, or if its value is not an array
      * @return mixed[]
-     * @param array<string,mixed> $data
+     * @param array<string|int,mixed> $data
      */
-    public function &getPointerToArrayItemUnderPath(array &$data, string $path): array
+    public function &getPointerToArrayItemUnderPath(array &$data, int|string $path): array
     {
         $dataPointer = $this->getPointerToElementItemUnderPath($data, $path);
 
@@ -43,34 +43,42 @@ class ArrayTraversionHelperService implements ArrayTraversionHelperServiceInterf
 
     /**
      * @throws RuntimeOperationException If the path cannot be reached under the array
-     * @param array<string,mixed> $data
+     * @param array<string|int,mixed> $data
      */
-    public function &getPointerToElementItemUnderPath(array &$data, string $path): mixed
+    public function &getPointerToElementItemUnderPath(array &$data, int|string $path): mixed
     {
         $dataPointer = &$data;
 
-        // Iterate the data array to the provided path.
-        foreach (explode(OperationSymbols::ARRAY_PATH_DELIMITER, $path) as $pathLevel) {
-            if (!$dataPointer) {
+        if (is_integer($path)) {
+            if (array_key_exists($path, $dataPointer)) {
+                $dataPointer = &$dataPointer[$path];
+            } else {
+                $this->throwNoArrayItemUnderPathException($data, $path);
+            }
+        } else {
+            // Iterate the data array to the provided path.
+            foreach (explode(OperationSymbols::ARRAY_PATH_DELIMITER, $path) as $pathLevel) {
+                if (!$dataPointer) {
+                    // If we reached the end of the array and can't keep going down any level more, then it's an error
+                    $this->throwNoArrayItemUnderPathException($data, $path);
+                }
+                if (array_key_exists($pathLevel, $dataPointer)) {
+                    // Retrieve the property under the pathLevel
+                    $dataPointer = &$dataPointer[$pathLevel];
+                    continue;
+                }
+                if (is_array($dataPointer) && isset($dataPointer[0]) && is_array($dataPointer[0]) && isset($dataPointer[0][$pathLevel])) {
+                    // If it is an array, then retrieve that property from each element of the array
+                    $dataPointerArray = array_map(function ($item) use ($pathLevel) {
+                        return $item[$pathLevel];
+                    }, $dataPointer);
+                    $dataPointer = &$dataPointerArray;
+                    continue;
+                }
+                // We are accessing a level that doesn't exist
                 // If we reached the end of the array and can't keep going down any level more, then it's an error
                 $this->throwNoArrayItemUnderPathException($data, $path);
             }
-            if (array_key_exists($pathLevel, $dataPointer)) {
-                // Retrieve the property under the pathLevel
-                $dataPointer = &$dataPointer[$pathLevel];
-                continue;
-            }
-            if (is_array($dataPointer) && isset($dataPointer[0]) && is_array($dataPointer[0]) && isset($dataPointer[0][$pathLevel])) {
-                // If it is an array, then retrieve that property from each element of the array
-                $dataPointerArray = array_map(function ($item) use ($pathLevel) {
-                    return $item[$pathLevel];
-                }, $dataPointer);
-                $dataPointer = &$dataPointerArray;
-                continue;
-            }
-            // We are accessing a level that doesn't exist
-            // If we reached the end of the array and can't keep going down any level more, then it's an error
-            $this->throwNoArrayItemUnderPathException($data, $path);
         }
 
         return $dataPointer;
@@ -78,48 +86,68 @@ class ArrayTraversionHelperService implements ArrayTraversionHelperServiceInterf
 
     /**
      * @throws RuntimeOperationException
-     * @param array<string,mixed> $data
+     * @param array<string|int,mixed> $data
      */
-    protected function throwNoArrayItemUnderPathException(array $data, string $path): void
+    protected function throwNoArrayItemUnderPathException(array $data, int|string $path): void
     {
         throw new RuntimeOperationException(
-            sprintf(
-                $this->__('Path \'%s\' is not reachable for object: %s', 'component-model'),
-                $path,
-                $this->getOutputService()->jsonEncodeArrayOrStdClassValue($data)
-            )
+            is_integer($path)
+                ? sprintf(
+                    $this->__('Index \'%s\' is not set for object: %s', 'component-model'),
+                    $path,
+                    $this->getOutputService()->jsonEncodeArrayOrStdClassValue($data)
+                )
+                : sprintf(
+                    $this->__('Path \'%s\' is not reachable for object: %s', 'component-model'),
+                    $path,
+                    $this->getOutputService()->jsonEncodeArrayOrStdClassValue($data)
+                )
         );
     }
 
     /**
      * @throws RuntimeOperationException
      */
-    protected function throwItemUnderPathIsNotArrayException(mixed $dataPointer, string $path): void
+    protected function throwItemUnderPathIsNotArrayException(mixed $dataPointer, int|string $path): void
     {
         throw new RuntimeOperationException(
-            sprintf(
-                $this->__('The item under path \'%s\' (with value \'%s\') is not an array', 'component-model'),
-                $path,
-                $dataPointer
-            )
+            is_integer($path)
+                ? sprintf(
+                    $this->__('The item under index \'%s\' (with value \'%s\') is not an array', 'component-model'),
+                    $path,
+                    $dataPointer
+                )
+                : sprintf(
+                    $this->__('The item under path \'%s\' (with value \'%s\') is not an array', 'component-model'),
+                    $path,
+                    $dataPointer
+                )
         );
     }
 
     /**
      * @throws RuntimeOperationException If the path cannot be reached under the array
-     * @param array<string,mixed> $data
+     * @param array<string|int,mixed> $data
      */
-    public function setValueToArrayItemUnderPath(array &$data, string $path, mixed $value): void
+    public function setValueToArrayItemUnderPath(array &$data, int|string $path, mixed $value): void
     {
         $dataPointer = &$data;
 
-        // Iterate the data array to the provided path.
-        foreach (explode(OperationSymbols::ARRAY_PATH_DELIMITER, $path) as $pathLevel) {
-            if (!array_key_exists($pathLevel, $dataPointer)) {
-                // If we reached the end of the array and can't keep going down any level more, then it's an error
+        if (is_integer($path)) {
+            if (array_key_exists($path, $dataPointer)) {
+                $dataPointer = &$dataPointer[$path];
+            } else {
                 $this->throwNoArrayItemUnderPathException($data, $path);
             }
-            $dataPointer = &$dataPointer[$pathLevel];
+        } else {
+            // Iterate the data array to the provided path.
+            foreach (explode(OperationSymbols::ARRAY_PATH_DELIMITER, $path) as $pathLevel) {
+                if (!array_key_exists($pathLevel, $dataPointer)) {
+                    // If we reached the end of the array and can't keep going down any level more, then it's an error
+                    $this->throwNoArrayItemUnderPathException($data, $path);
+                }
+                $dataPointer = &$dataPointer[$pathLevel];
+            }
         }
 
         // We reached the end. Set the value
