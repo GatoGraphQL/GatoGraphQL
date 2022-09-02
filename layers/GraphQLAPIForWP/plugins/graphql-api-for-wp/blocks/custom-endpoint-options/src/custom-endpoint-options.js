fb89ce1949be12be5b92841f5a7e275577eb5321
Leonardo Losoviz <leo@getpop.org>
2022-09-02 21:31:45 +0800

    Doc link for Custom Endpoint Options

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/custom-endpoint-options/src/custom-endpoint-options.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/custom-endpoint-options/src/custom-endpoint-options.js
@@ -8,6 +8,7 @@ import { ToggleControl } from '@wordpress/components';
 /**
  * Internal dependencies
  */
+import { getModuleDocMarkdownContentOrUseDefault } from './module-doc-markdown-loader';
 import {
 	withCard,
 	withEditableOnFocus,
@@ -48,7 +49,8 @@ const CustomEndpointOptions = ( props ) => {
 
 export default compose( [
 	withState( {
-		header: __('Options', 'graphql-api'),
+		header: __('Custom Endpoint Options', 'graphql-api'),
+		getMarkdownContentCallback: getModuleDocMarkdownContentOrUseDefault
 	} ),
 	withEditableOnFocus(),
 	withCard(),
