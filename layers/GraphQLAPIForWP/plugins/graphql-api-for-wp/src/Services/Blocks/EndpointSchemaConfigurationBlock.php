154597d223de35853a1fdf0111538d635044e1f5
Leonardo Losoviz <leo@getpop.org>
2022-09-02 21:07:10 +0800

    Doc link in Schema Config

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/EndpointSchemaConfigurationBlock.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/EndpointSchemaConfigurationBlock.php
@@ -144,4 +144,21 @@ EOF;
             $schemaConfigurationContent
         );
     }
+
+    /**
+     * Add the locale language to the localized data?
+     */
+    protected function addLocalLanguage(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Default language for the script/component's documentation
+     */
+    protected function getDefaultLanguage(): ?string
+    {
+        // English
+        return 'en';
+    }
 }
