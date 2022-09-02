97b3e513bb91ee2ab357aa7dfd36817fb04e91b0
Leonardo Losoviz <leo@getpop.org>
2022-09-02 18:26:25 +0800

    Added doc link for Expose Admin Data

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-expose-admin-data/src/edit.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-expose-admin-data/src/edit.js
@@ -2,6 +2,7 @@
  * Application imports
  */
 import SchemaConfigExposeAdminDataCard from './schema-config-expose-admin-data-card';
+import { getModuleDocMarkdownContentOrUseDefault } from './module-doc-markdown-loader';
 
 const EditBlock = ( props ) => {
 	const { className } = props;
@@ -9,6 +10,7 @@ const EditBlock = ( props ) => {
 		<div class={ className }>
 			<SchemaConfigExposeAdminDataCard
 				{ ...props }
+				getMarkdownContentCallback={ getModuleDocMarkdownContentOrUseDefault }
 			/>
 		</div>
 	)
