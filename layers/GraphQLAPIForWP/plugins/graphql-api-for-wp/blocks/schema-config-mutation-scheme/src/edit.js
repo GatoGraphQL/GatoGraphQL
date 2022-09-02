7258416661f53234e05d3979f4c5b36875c8a321
Leonardo Losoviz <leo@getpop.org>
2022-09-02 18:34:50 +0800

    Added doc link for Mutation Scheme

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-mutation-scheme/src/edit.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-mutation-scheme/src/edit.js
@@ -2,6 +2,7 @@
  * Application imports
  */
 import SchemaConfigMutationSchemeCard from './schema-config-mutation-scheme-card';
+import { getModuleDocMarkdownContentOrUseDefault } from './module-doc-markdown-loader';
 
 const EditBlock = ( props ) => {
 	const { className } = props;
@@ -9,6 +10,7 @@ const EditBlock = ( props ) => {
 		<div class={ className }>
 			<SchemaConfigMutationSchemeCard
 				{ ...props }
+				getMarkdownContentCallback={ getModuleDocMarkdownContentOrUseDefault }
 			/>
 		</div>
 	)
--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-mutation-scheme/src/schema-config-mutation-scheme-card.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-mutation-scheme/src/schema-config-mutation-scheme-card.js
@@ -2,15 +2,17 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Card, CardHeader, CardBody, RadioControl } from '@wordpress/components';
+import { RadioControl } from '@wordpress/components';
+import { compose, withState } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
 import {
 	InfoTooltip,
-	getEditableOnFocusComponentClass,
 	SETTINGS_VALUE_LABEL,
+	withCard,
+	withEditableOnFocus,
 } from '@graphqlapi/components';
 import {
 	ATTRIBUTE_VALUE_MUTATION_SCHEME_DEFAULT,
@@ -22,13 +24,11 @@ import {
 const SchemaConfigMutationSchemeCard = ( props ) => {
 	const {
 		isSelected,
-		className,
 		setAttributes,
 		attributes: {
 			mutationScheme,
 		},
 	} = props;
-	const componentClassName = `${ className } ${ getEditableOnFocusComponentClass(isSelected) }`;
 	const options = [
 		{
 			label: SETTINGS_VALUE_LABEL,
@@ -49,52 +49,50 @@ const SchemaConfigMutationSchemeCard = ( props ) => {
 	];
 	const optionValues = options.map( option => option.value );
 	return (
-		<div className={ componentClassName }>
-			<Card { ...props }>
-				<CardHeader isShady>
-					{ __('Mutation Scheme', 'graphql-api') }
-				</CardHeader>
-				<CardBody>
-					<div className={ `${ className }__nestedmutations` }>
-						<em>{ __('Support nested mutations?', 'graphql-api') }</em>
-						<InfoTooltip
-							{ ...props }
-							text={ __('Add mutation fields on entities other than the root type?', 'graphql-api') }
-						/>
-						{ !isSelected && (
-							<>
-								<br />
-								{ ( mutationScheme == ATTRIBUTE_VALUE_MUTATION_SCHEME_DEFAULT || !optionValues.includes(mutationScheme) ) &&
-									<span>🟡 { __('Default', 'graphql-api') }</span>
-								}
-								{ mutationScheme == ATTRIBUTE_VALUE_MUTATION_SCHEME_STANDARD &&
-									<span>❌ { __('Do not enable nested mutations', 'graphql-api') }</span>
-								}
-								{ mutationScheme == ATTRIBUTE_VALUE_MUTATION_SCHEME_NESTED_WITH_REDUNDANT_ROOT_FIELDS &&
-									<span>✅ { __('Enable nested mutations, keeping all mutation fields in the root', 'graphql-api') }</span>
-								}
-								{ mutationScheme == ATTRIBUTE_VALUE_MUTATION_SCHEME_NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS &&
-									<span>✳️ { __('Enable nested mutations, removing the redundant mutation fields from the root', 'graphql-api') }</span>
-								}
-							</>
-						) }
-						{ isSelected &&
-							<RadioControl
-								{ ...props }
-								options={ options }
-								selected={ mutationScheme }
-								onChange={ newValue => (
-									setAttributes( {
-										mutationScheme: newValue
-									} )
-								)}
-							/>
-						}
-					</div>
-				</CardBody>
-			</Card>
-		</div>
+		<>
+			<em>{ __('Support nested mutations?', 'graphql-api') }</em>
+			<InfoTooltip
+				{ ...props }
+				text={ __('Add mutation fields on entities other than the root type?', 'graphql-api') }
+			/>
+			{ !isSelected && (
+				<>
+					<br />
+					{ ( mutationScheme == ATTRIBUTE_VALUE_MUTATION_SCHEME_DEFAULT || !optionValues.includes(mutationScheme) ) &&
+						<span>🟡 { __('Default', 'graphql-api') }</span>
+					}
+					{ mutationScheme == ATTRIBUTE_VALUE_MUTATION_SCHEME_STANDARD &&
+						<span>❌ { __('Do not enable nested mutations', 'graphql-api') }</span>
+					}
+					{ mutationScheme == ATTRIBUTE_VALUE_MUTATION_SCHEME_NESTED_WITH_REDUNDANT_ROOT_FIELDS &&
+						<span>✅ { __('Enable nested mutations, keeping all mutation fields in the root', 'graphql-api') }</span>
+					}
+					{ mutationScheme == ATTRIBUTE_VALUE_MUTATION_SCHEME_NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS &&
+						<span>✳️ { __('Enable nested mutations, removing the redundant mutation fields from the root', 'graphql-api') }</span>
+					}
+				</>
+			) }
+			{ isSelected &&
+				<RadioControl
+					{ ...props }
+					options={ options }
+					selected={ mutationScheme }
+					onChange={ newValue => (
+						setAttributes( {
+							mutationScheme: newValue
+						} )
+					)}
+				/>
+			}
+		</>
 	);
 }
 
-export default SchemaConfigMutationSchemeCard;
+export default compose( [
+	withState( {
+		header: __('Mutation Scheme', 'graphql-api'),
+		className: 'graphql-api-mutation-scheme',
+	} ),
+	withEditableOnFocus(),
+	withCard(),
+] )( SchemaConfigMutationSchemeCard );
