02e1de0308030afad7f7d9095470579667a779aa
Leonardo Losoviz <leo@getpop.org>
2022-09-14 09:57:02 +0800

    phpdoc

--- layers/Engine/packages/mandatory-directives-by-configuration/src/Constants/WildcardConfigurationValues.php
+++ layers/Engine/packages/mandatory-directives-by-configuration/src/Constants/WildcardConfigurationValues.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace PoP\MandatoryDirectivesByConfiguration\Constants;
 
+/**
+ * When storing the ACL configuration for fields,
+ * we can use value "*" to represent "any type or interface"
+ * and also "any field (within a certain type)"
+ */
 class WildcardConfigurationValues
 {
     final public const TYPE_OR_INTERFACE_RESOLVER_CLASS = '*';
