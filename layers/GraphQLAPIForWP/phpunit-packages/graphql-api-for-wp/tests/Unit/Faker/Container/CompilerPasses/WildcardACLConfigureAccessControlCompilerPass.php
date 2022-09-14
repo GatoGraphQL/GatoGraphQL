63fc8d06b1dad3085af3598db3610ba479104660
Leonardo Losoviz <leo@getpop.org>
2022-09-14 11:24:54 +0800

    Added test

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Unit/Faker/Container/CompilerPasses/WildcardACLConfigureAccessControlCompilerPass.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Unit\Faker\Container\CompilerPasses;
+
+use PoPCMSSchema\Comments\TypeResolvers\ObjectType\CommentObjectTypeResolver;
+use PoPCMSSchema\CustomPosts\TypeResolvers\InterfaceType\IsCustomPostInterfaceTypeResolver;
+use PoPCMSSchema\UserStateAccessControl\ConfigurationEntries\UserStates;
+use PoPCMSSchema\UserStateAccessControl\Services\AccessControlGroups as UserStateAccessControlGroups;
+use PoP\AccessControl\Services\AccessControlManagerInterface;
+use PoP\Root\Container\CompilerPasses\AbstractCompilerPass;
+use PoP\Root\Container\ContainerBuilderWrapperInterface;
+
+class WildcardACLConfigureAccessControlCompilerPass extends AbstractCompilerPass
+{
+    protected function doProcess(ContainerBuilderWrapperInterface $containerBuilderWrapper): void
+    {
+        $accessControlManagerDefinition = $containerBuilderWrapper->getDefinition(AccessControlManagerInterface::class);
+        $accessControlManagerDefinition->addMethodCall(
+            'addEntriesForFields',
+            [
+                UserStateAccessControlGroups::STATE,
+                [
+                    // Wildcard type or interface
+                    ['*', 'users', UserStates::IN],
+                    // Wildcard field on type
+                    [CommentObjectTypeResolver::class, '*', UserStates::IN],
+                    // Wildcard field on interface
+                    [IsCustomPostInterfaceTypeResolver::class, '*', UserStates::IN],
+                ]
+            ]
+        );
+    }
+}
---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Unit/Faker/WildcardACLWPFakerFixtureQueryExecutionGraphQLServerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Unit\Faker;
+
+use PHPUnitForGraphQLAPI\WPFakerSchema\Unit\AbstractWPFakerFixtureQueryExecutionGraphQLServerTest;
+use PoP\Root\Module\ModuleInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+class WildcardACLWPFakerFixtureQueryExecutionGraphQLServerTest extends AbstractWPFakerFixtureQueryExecutionGraphQLServerTest
+{
+    /**
+     * Directory under the fixture files are placed
+     */
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-wildcard-acl';
+    }
+
+    /**
+     * @return array<class-string<ModuleInterface>>
+     */
+    protected static function getGraphQLServerModuleClasses(): array
+    {
+        return [
+            ...parent::getGraphQLServerModuleClasses(),
+            ...[
+                \PoPWPSchema\Posts\Module::class,
+                \PoPWPSchema\Comments\Module::class,
+                \PoPWPSchema\Users\Module::class,
+                \PoPCMSSchema\UserStateAccessControl\Module::class,
+                \PoPCMSSchema\UserStateMutationsWP\Module::class,
+            ]
+        ];
+    }
+
+    /**
+     * @return array<class-string<CompilerPassInterface>>
+     */
+    protected static function getGraphQLServerApplicationContainerCompilerPassClasses(): array
+    {
+        return [
+            ...parent::getGraphQLServerApplicationContainerCompilerPassClasses(),
+            ...[
+                \PHPUnitForGraphQLAPI\GraphQLAPI\Unit\Faker\Container\CompilerPasses\WildcardACLConfigureAccessControlCompilerPass::class,
+            ]
+        ];
+    }
+}
