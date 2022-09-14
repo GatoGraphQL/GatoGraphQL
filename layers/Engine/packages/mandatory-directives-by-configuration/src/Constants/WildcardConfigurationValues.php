24cf7f9bdbade49c362cd7b4c20fa2cdd30e9e3e
Leonardo Losoviz <leo@getpop.org>
2022-09-14 09:50:18 +0800

    WildcardConfigurationValues

---
+++ layers/Engine/packages/mandatory-directives-by-configuration/src/Constants/WildcardConfigurationValues.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\MandatoryDirectivesByConfiguration\Constants;
+
+class WildcardConfigurationValues
+{
+    final public const TYPENAME = '*';
+    final public const FIELDNAME = '*';
+}
