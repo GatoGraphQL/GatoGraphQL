b771a9b96926339c4f780f654ca553025ff0e865
Leonardo Losoviz <leo@getpop.org>
2022-09-14 14:17:51 +0800

    Abstract class

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Unit/Faker/AbstractWildcardACLWPFakerFixtureQueryExecutionGraphQLServerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Unit\Faker;
+
+use PHPUnitForGraphQLAPI\WPFakerSchema\Unit\AbstractWPFakerFixtureQueryExecutionGraphQLServerTest;
+use PoP\Root\Module\ModuleInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+abstract class AbstractWildcardACLWPFakerFixtureQueryExecutionGraphQLServerTest extends AbstractWPFakerFixtureQueryExecutionGraphQLServerTest
+{
+    /**
+     * Directory under the fixture files are placed
+     */
+    protected function getFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-wildcard-acl';
+    }
+
+    /**
+     * @return array<class-string<ModuleInterface>>
+     */
+    protected static function getGraphQLServerModuleClasses(): array
+    {
+        return [
+            ...parent::getGraphQLServerModuleClasses(),
+            ...[
+                \PoPWPSchema\Posts\Module::class,
+                \PoPWPSchema\Comments\Module::class,
+                \PoPWPSchema\Users\Module::class,
+                \PoPCMSSchema\UserStateAccessControl\Module::class,
+                \PoPCMSSchema\UserStateWP\Module::class,
+            ]
+        ];
+    }
+
+    /**
+     * @return array<class-string<CompilerPassInterface>>
+     */
+    protected static function getGraphQLServerApplicationContainerCompilerPassClasses(): array
+    {
+        $graphQLServerApplicationContainerCompilerPassClasses = parent::getGraphQLServerApplicationContainerCompilerPassClasses();
+        $wildcardCompilerPassClass = static::getWildcardCompilerPassClass();
+        if ($wildcardCompilerPassClass === null) {
+            return $graphQLServerApplicationContainerCompilerPassClasses;
+        }
+        return [
+            ...$graphQLServerApplicationContainerCompilerPassClasses,
+            ...[
+                $wildcardCompilerPassClass,
+            ]
+        ];
+    }
+
+    /**
+     * @return class-string<CompilerPassInterface>|null
+     */
+    abstract protected static function getWildcardCompilerPassClass(): ?string;
+}
--- layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Unit/Faker/WildcardPublicACLWPFakerFixtureQueryExecutionGraphQLServerTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Unit/Faker/WildcardPublicACLWPFakerFixtureQueryExecutionGraphQLServerTest.php
@@ -4,47 +4,15 @@ declare(strict_types=1);
 
 namespace PHPUnitForGraphQLAPI\GraphQLAPI\Unit\Faker;
 
-use PHPUnitForGraphQLAPI\WPFakerSchema\Unit\AbstractWPFakerFixtureQueryExecutionGraphQLServerTest;
-use PoP\Root\Module\ModuleInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 
-class WildcardPublicACLWPFakerFixtureQueryExecutionGraphQLServerTest extends AbstractWPFakerFixtureQueryExecutionGraphQLServerTest
+class WildcardPublicACLWPFakerFixtureQueryExecutionGraphQLServerTest extends AbstractWildcardACLWPFakerFixtureQueryExecutionGraphQLServerTest
 {
     /**
-     * Directory under the fixture files are placed
+     * @return class-string<CompilerPassInterface>|null
      */
-    protected function getFixtureFolder(): string
+    protected static function getWildcardCompilerPassClass(): ?string
     {
-        return __DIR__ . '/fixture-wildcard-acl';
-    }
-
-    /**
-     * @return array<class-string<ModuleInterface>>
-     */
-    protected static function getGraphQLServerModuleClasses(): array
-    {
-        return [
-            ...parent::getGraphQLServerModuleClasses(),
-            ...[
-                \PoPWPSchema\Posts\Module::class,
-                \PoPWPSchema\Comments\Module::class,
-                \PoPWPSchema\Users\Module::class,
-                \PoPCMSSchema\UserStateAccessControl\Module::class,
-                \PoPCMSSchema\UserStateWP\Module::class,
-            ]
-        ];
-    }
-
-    /**
-     * @return array<class-string<CompilerPassInterface>>
-     */
-    protected static function getGraphQLServerApplicationContainerCompilerPassClasses(): array
-    {
-        return [
-            ...parent::getGraphQLServerApplicationContainerCompilerPassClasses(),
-            ...[
-                \PHPUnitForGraphQLAPI\GraphQLAPI\Unit\Faker\Container\CompilerPasses\WildcardPublicACLConfigureAccessControlCompilerPass::class,
-            ]
-        ];
+        return \PHPUnitForGraphQLAPI\GraphQLAPI\Unit\Faker\Container\CompilerPasses\WildcardPublicACLConfigureAccessControlCompilerPass::class;
     }
 }
