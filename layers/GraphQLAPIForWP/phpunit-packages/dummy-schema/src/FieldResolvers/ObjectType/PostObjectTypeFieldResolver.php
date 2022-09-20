a3e0c33b9d016685ec52dfb89c2d388331aab14f
Leonardo Losoviz <leo@getpop.org>
2022-09-20 08:58:13 +0800

    Added dummy fields for [Date] and [[Date]]

--- layers/GraphQLAPIForWP/phpunit-packages/dummy-schema/src/FieldResolvers/ObjectType/PostObjectTypeFieldResolver.php
+++ layers/GraphQLAPIForWP/phpunit-packages/dummy-schema/src/FieldResolvers/ObjectType/PostObjectTypeFieldResolver.php
@@ -4,39 +4,29 @@ declare(strict_types=1);
 
 namespace PHPUnitForGraphQLAPI\DummySchema\FieldResolvers\ObjectType;
 
+use DateTime;
 use PoPCMSSchema\Posts\TypeResolvers\ObjectType\PostObjectTypeResolver;
+use PoPSchema\SchemaCommons\TypeResolvers\ScalarType\DateTimeScalarTypeResolver;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractQueryableObjectTypeFieldResolver;
 use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoP\ComponentModel\TypeResolvers\ConcreteTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
-use PoP\ComponentModel\TypeResolvers\ScalarType\IntScalarTypeResolver;
-use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
 use WP_Post;
 
 class PostObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolver
 {
-    private ?IntScalarTypeResolver $intScalarTypeResolver = null;
-    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
+    private ?DateTimeScalarTypeResolver $dateTimeScalarTypeResolver = null;
 
-    final public function setIntScalarTypeResolver(IntScalarTypeResolver $intScalarTypeResolver): void
+    final public function setDateTimeScalarTypeResolver(DateTimeScalarTypeResolver $dateTimeScalarTypeResolver): void
     {
-        $this->intScalarTypeResolver = $intScalarTypeResolver;
+        $this->dateTimeScalarTypeResolver = $dateTimeScalarTypeResolver;
     }
-    final protected function getIntScalarTypeResolver(): IntScalarTypeResolver
+    final protected function getDateTimeScalarTypeResolver(): DateTimeScalarTypeResolver
     {
-        /** @var IntScalarTypeResolver */
-        return $this->intScalarTypeResolver ??= $this->instanceManager->getInstance(IntScalarTypeResolver::class);
-    }
-    final public function setStringScalarTypeResolver(StringScalarTypeResolver $stringScalarTypeResolver): void
-    {
-        $this->stringScalarTypeResolver = $stringScalarTypeResolver;
-    }
-    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
-    {
-        /** @var StringScalarTypeResolver */
-        return $this->stringScalarTypeResolver ??= $this->instanceManager->getInstance(StringScalarTypeResolver::class);
+        /** @var DateTimeScalarTypeResolver */
+        return $this->dateTimeScalarTypeResolver ??= $this->instanceManager->getInstance(DateTimeScalarTypeResolver::class);
     }
 
     /**
@@ -55,16 +45,16 @@ class PostObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolv
     public function getFieldNamesToResolve(): array
     {
         return [
-            // 'karma',
-            // 'authorIP',
+            'listOfDates',
+            'listOfListsOfDates',
         ];
     }
 
     public function getFieldDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
     {
         return match ($fieldName) {
-            // 'karma' => $this->__('Comment karma', 'dummy-schema'),
-            // 'authorIP' => $this->__('The author IP', 'dummy-schema'),
+            'listOfDates' => $this->__('Dummy field that returns a list of dates: [Date]', 'dummy-schema'),
+            'listOfListsOfDates' => $this->__('Dummy field that returns a list of lists of dates: [[Date]]', 'dummy-schema'),
             default => parent::getFieldDescription($objectTypeResolver, $fieldName),
         };
     }
@@ -72,17 +62,22 @@ class PostObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolv
     public function getFieldTypeResolver(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ConcreteTypeResolverInterface
     {
         return match ($fieldName) {
-            // 'karma' => $this->getIntScalarTypeResolver(),
-            // 'authorIP' => $this->getStringScalarTypeResolver(),
-            default => parent::getFieldTypeResolver($objectTypeResolver, $fieldName),
+            'listOfDates',
+            'listOfListsOfDates'
+                => $this->getDateTimeScalarTypeResolver(),
+            default
+                => parent::getFieldTypeResolver($objectTypeResolver, $fieldName),
         };
     }
 
     public function getFieldTypeModifiers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): int
     {
         return match ($fieldName) {
-            // 'karma' => SchemaTypeModifiers::NON_NULLABLE,
-            default => parent::getFieldTypeModifiers($objectTypeResolver, $fieldName),
+            'listOfDates',
+            'listOfListsOfDates'
+                => SchemaTypeModifiers::NON_NULLABLE,
+            default
+                => parent::getFieldTypeModifiers($objectTypeResolver, $fieldName),
         };
     }
 
@@ -94,12 +89,29 @@ class PostObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolv
     ): mixed {
         /** @var WP_Post */
         $post = $object;
-        // switch ($fieldDataAccessor->getFieldName()) {
-        //     case 'karma':
-        //         return (int)$post->comment_karma;
-        //     case 'authorIP':
-        //         return $post->comment_author_IP;
-        // }
+        switch ($fieldDataAccessor->getFieldName()) {
+            case 'listOfDates':
+                return [
+                    new DateTime($post->post_date),
+                    new DateTime(date('Y-m-d H:i:s', strtotime($post->post_date . ' +1 day'))),
+                    new DateTime(date('Y-m-d H:i:s', strtotime($post->post_date . ' +7 day'))),
+                ];
+            case 'listOfListsOfDates':
+                return [
+                    [
+                        new DateTime($post->post_date),
+                        new DateTime(date('Y-m-d H:i:s', strtotime($post->post_date . ' +1 day'))),
+                        new DateTime(date('Y-m-d H:i:s', strtotime($post->post_date . ' +7 day'))),
+                    ],
+                    [
+                        new DateTime(date('Y-m-d H:i:s', strtotime($post->post_date . ' +9 day'))),
+                        new DateTime(date('Y-m-d H:i:s', strtotime($post->post_date . ' +28 day'))),
+                    ],
+                    [
+                        new DateTime(date('Y-m-d H:i:s', strtotime($post->post_date . ' +66 day'))),
+                    ],
+                ];
+        }
 
         return parent::resolveValue($objectTypeResolver, $object, $fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
     }
