ea0b8a3d5a87b10ec8aa97059d34432f91f5acc9
Leonardo Losoviz <leo@getpop.org>
2022-09-21 15:10:56 +0800

    Skip $enableModifyingEngineBehaviorViaRequest For ?query and ?operationName

--- layers/API/packages/api/src/Configuration/EngineRequest.php
+++ layers/API/packages/api/src/Configuration/EngineRequest.php
@@ -15,20 +15,12 @@ class EngineRequest
     public static function getQuery(bool $enableModifyingEngineBehaviorViaRequest): ?string
     {
         $default = null;
-        if (!$enableModifyingEngineBehaviorViaRequest) {
-            return $default;
-        }
-
         return App::request(QueryInputs::QUERY) ?? App::query(QueryInputs::QUERY, $default);
     }
 
     public static function getOperationName(bool $enableModifyingEngineBehaviorViaRequest): ?string
     {
         $default = null;
-        if (!$enableModifyingEngineBehaviorViaRequest) {
-            return $default;
-        }
-
         return App::request(QueryInputs::OPERATION_NAME) ?? App::query(QueryInputs::OPERATION_NAME, $default);
     }
 }
