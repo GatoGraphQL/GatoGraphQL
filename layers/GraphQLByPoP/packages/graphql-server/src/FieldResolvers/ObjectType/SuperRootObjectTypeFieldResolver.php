acd35e0ab4372955474a971d79e10f4f5796bf9e
Leonardo Losoviz <leo@getpop.org>
2022-09-22 10:19:58 +0800

    resolve fields

--- layers/GraphQLByPoP/packages/graphql-server/src/FieldResolvers/ObjectType/SuperRootObjectTypeFieldResolver.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/FieldResolvers/ObjectType/SuperRootObjectTypeFieldResolver.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace GraphQLByPoP\GraphQLServer\FieldResolvers\ObjectType;
 
+use GraphQLByPoP\GraphQLServer\ObjectModels\MutationRoot;
+use GraphQLByPoP\GraphQLServer\ObjectModels\QueryRoot;
 use GraphQLByPoP\GraphQLServer\TypeResolvers\ObjectType\MutationRootObjectTypeResolver;
 use GraphQLByPoP\GraphQLServer\TypeResolvers\ObjectType\QueryRootObjectTypeResolver;
 use GraphQLByPoP\GraphQLServer\TypeResolvers\ObjectType\SuperRootObjectTypeResolver;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractObjectTypeFieldResolver;
+use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
 use PoP\ComponentModel\TypeResolvers\ConcreteTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
+use PoP\Engine\ObjectModels\Root;
 use PoP\Engine\TypeResolvers\ObjectType\RootObjectTypeResolver;
 
 class SuperRootObjectTypeFieldResolver extends AbstractObjectTypeFieldResolver
@@ -82,6 +87,22 @@ class SuperRootObjectTypeFieldResolver extends AbstractObjectTypeFieldResolver
             'queryRoot' => $this->getQueryRootObjectTypeResolver(),
             'mutationRoot' => $this->getMutationRootObjectTypeResolver(),
             default => parent::getFieldTypeResolver($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function resolveValue(
+        ObjectTypeResolverInterface $objectTypeResolver,
+        object $object,
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): mixed {
+        /** @var SuperRoot */
+        $superRoot = $object;
+        return match ($fieldDataAccessor->getFieldName()) {
+            'root' => Root::ID,
+            'queryRoot' => QueryRoot::ID,
+            'mutationRoot' => MutationRoot::ID,
+            default => parent::resolveValue($objectTypeResolver, $object, $fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore),
         };
     }
 }
