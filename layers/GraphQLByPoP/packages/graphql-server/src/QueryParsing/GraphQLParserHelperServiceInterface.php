4e1876f3b670d4098eccda2ef5d6500925016674
Leonardo Losoviz <leo@getpop.org>
2022-09-22 09:23:18 +0800

    phpdoc

--- layers/GraphQLByPoP/packages/graphql-server/src/QueryParsing/GraphQLParserHelperServiceInterface.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/QueryParsing/GraphQLParserHelperServiceInterface.php
@@ -9,5 +9,13 @@ use PoP\GraphQLParser\Spec\Parser\Ast\Document;
 
 interface GraphQLParserHelperServiceInterface extends UpstreamGraphQLParserHelperServiceInterface
 {
+    /**
+     * Convert the operations (query, mutation, subscription) in the
+     * GraphQL Documents, to the corresponding field in the SuperRoot
+     * type ("queryRoot", "mutationRoot", "subscriptionRoot"), which is
+     * the type from which the GraphQL query is resolved.
+     *
+     * @see layers/GraphQLByPoP/packages/graphql-server/src/ComponentRoutingProcessors/EntryComponentRoutingProcessor.php
+     */
     public function convertOperationsToSuperRootFieldsInAST(Document $document): Document;
 }
