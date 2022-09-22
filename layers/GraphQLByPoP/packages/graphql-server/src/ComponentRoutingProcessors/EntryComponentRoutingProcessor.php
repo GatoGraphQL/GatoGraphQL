38fd04798948a0bef61d8019ffb9184ec92bc768
Leonardo Losoviz <leo@getpop.org>
2022-09-22 08:48:10 +0800

    Added ComponentRouting

--- layers/GraphQLByPoP/packages/graphql-server/src/ComponentRoutingProcessors/EntryComponentRoutingProcessor.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/ComponentRoutingProcessors/EntryComponentRoutingProcessor.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace GraphQLByPoP\GraphQLServer\ComponentRoutingProcessors;
 
-use GraphQLByPoP\GraphQLServer\Constants\OperationTypes;
 use GraphQLByPoP\GraphQLServer\ComponentProcessors\RootRelationalFieldDataloadComponentProcessor;
-use PoP\ComponentModel\Component\Component;
-use PoP\ComponentRouting\AbstractEntryComponentRoutingProcessor;
+use GraphQLByPoP\GraphQLServer\Constants\OperationTypes;
+use PoPAPI\API\ComponentProcessors\SuperRootRelationalFieldDataloadComponentProcessor;
 use PoPAPI\API\Response\Schemes as APISchemes;
 use PoPAPI\API\Routing\RequestNature;
 use PoPAPI\GraphQLAPI\DataStructureFormatters\GraphQLDataStructureFormatter;
+use PoP\ComponentModel\Component\Component;
+use PoP\ComponentRouting\AbstractEntryComponentRoutingProcessor;
 
 class EntryComponentRoutingProcessor extends AbstractEntryComponentRoutingProcessor
 {
@@ -35,26 +36,37 @@ class EntryComponentRoutingProcessor extends AbstractEntryComponentRoutingProces
 
         $ret[RequestNature::QUERY_ROOT][] = [
             'component' => new Component(
-                RootRelationalFieldDataloadComponentProcessor::class,
-                RootRelationalFieldDataloadComponentProcessor::COMPONENT_DATALOAD_RELATIONALFIELDS_QUERYROOT
+                SuperRootRelationalFieldDataloadComponentProcessor::class,
+                SuperRootRelationalFieldDataloadComponentProcessor::COMPONENT_DATALOAD_RELATIONALFIELDS_SUPERROOT
             ),
             'conditions' => [
                 'scheme' => APISchemes::API,
                 'datastructure' => $this->getGraphQLDataStructureFormatter()->getName(),
-                'graphql-operation-type' => OperationTypes::QUERY,
             ],
         ];
-        $ret[RequestNature::QUERY_ROOT][] = [
-            'component' => new Component(
-                RootRelationalFieldDataloadComponentProcessor::class,
-                RootRelationalFieldDataloadComponentProcessor::COMPONENT_DATALOAD_RELATIONALFIELDS_MUTATIONROOT
-            ),
-            'conditions' => [
-                'scheme' => APISchemes::API,
-                'datastructure' => $this->getGraphQLDataStructureFormatter()->getName(),
-                'graphql-operation-type' => OperationTypes::MUTATION,
-            ],
-        ];
+        // @todo Remove this commented code
+        // $ret[RequestNature::QUERY_ROOT][] = [
+        //     'component' => new Component(
+        //         RootRelationalFieldDataloadComponentProcessor::class,
+        //         RootRelationalFieldDataloadComponentProcessor::COMPONENT_DATALOAD_RELATIONALFIELDS_QUERYROOT
+        //     ),
+        //     'conditions' => [
+        //         'scheme' => APISchemes::API,
+        //         'datastructure' => $this->getGraphQLDataStructureFormatter()->getName(),
+        //         'graphql-operation-type' => OperationTypes::QUERY,
+        //     ],
+        // ];
+        // $ret[RequestNature::QUERY_ROOT][] = [
+        //     'component' => new Component(
+        //         RootRelationalFieldDataloadComponentProcessor::class,
+        //         RootRelationalFieldDataloadComponentProcessor::COMPONENT_DATALOAD_RELATIONALFIELDS_MUTATIONROOT
+        //     ),
+        //     'conditions' => [
+        //         'scheme' => APISchemes::API,
+        //         'datastructure' => $this->getGraphQLDataStructureFormatter()->getName(),
+        //         'graphql-operation-type' => OperationTypes::MUTATION,
+        //     ],
+        // ];
 
         return $ret;
     }
