f4fe11691d4b109bcb4b430e579e2857a826e2fd
Leonardo Losoviz <leo@getpop.org>
2022-09-23 14:30:50 +0800

    phpdoc

--- layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/EnumType/DirectiveLocationEnumTypeResolver.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/TypeResolvers/EnumType/DirectiveLocationEnumTypeResolver.php
@@ -17,6 +17,14 @@ class DirectiveLocationEnumTypeResolver extends AbstractIntrospectionEnumTypeRes
     }
 
     /**
+     * Because this GraphQL server is code-first, the resolver
+     * definitions are provided via PHP code, and not via the
+     * GraphQL schema (as with SDL-first servers).
+     *
+     * Then, "Schema Type" directive locations are never used,
+     * and exposing this information in the schema is most
+     * likely meaningless. So it's disabled (by default).
+     *
      * @return string[]
      */
     public function getEnumValues(): array
