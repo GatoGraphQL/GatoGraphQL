cdfe9f0d7aa85aca18895d9ce0bdcd4d7c10b70f
Leonardo Losoviz <leo@getpop.org>
2022-09-28 10:30:06 +0800

    Duplicated AbstractFieldDirectiveResolver in Engine

--- layers/Engine/packages/component-model/src/DirectiveResolvers/AbstractFieldDirectiveResolver.php
+++ layers/Engine/packages/component-model/src/DirectiveResolvers/AbstractFieldDirectiveResolver.php
@@ -691,50 +691,18 @@ abstract class AbstractFieldDirectiveResolver extends AbstractDirectiveResolver 
     }
 
     /**
-     * For a FieldDirectiveResolver to only behave as a
-     * OperationDirectiveResolver, it must only support
-     * the SuperRoot type.
-     *
      * @return array<class-string<ConcreteTypeResolverInterface>>|null
      */
     protected function getSupportedFieldTypeResolverClasses(): ?array
     {
-        $fieldDirectiveBehavior = $this->getFieldDirectiveBehavior();
-        if ($fieldDirectiveBehavior === FieldDirectiveBehaviors::OPERATION) {
-            return [
-                SuperRootObjectTypeResolver::class,
-            ];
-        }
-
         return null;
     }
 
     /**
-     * For a FieldDirectiveResolver to not also behave as a
-     * OperationDirectiveResolver, it must be excluded from
-     * the SuperRoot type.
-     *
-     * Watch out: System directives (like @resolveValueAndMerge)
-     * must always be allowed, it's only the "query-type" and
-     * "schema-type" directives that must be excluded.
-     *
      * @return array<class-string<ConcreteTypeResolverInterface>>|null
      */
     protected function getExcludedFieldTypeResolverClasses(): ?array
     {
-        $fieldDirectiveBehavior = $this->getFieldDirectiveBehavior();
-        if (
-            $fieldDirectiveBehavior === FieldDirectiveBehaviors::FIELD
-            && (
-                $this->isQueryTypeDirective()
-                || $this->getDirectiveKind() === DirectiveKinds::SCHEMA
-            )
-        ) {
-            return [
-                SuperRootObjectTypeResolver::class,
-            ];
-        }
-
         return null;
     }
 
