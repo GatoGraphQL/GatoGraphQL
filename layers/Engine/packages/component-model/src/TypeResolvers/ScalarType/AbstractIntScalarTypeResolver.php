736e6259c8ed3540dd90980c135753a1e5cb9cac
Leonardo Losoviz <leo@getpop.org>
2022-10-03 21:20:09 +0800

    Replaced `NonCanonicalTypeNameTypeResolverTrait` ...with abstract classes for `Int` and `Float`

---
+++ layers/Engine/packages/component-model/src/TypeResolvers/ScalarType/AbstractIntScalarTypeResolver.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\TypeResolvers\ScalarType;
+
+use CastToType;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
+use PoP\GraphQLParser\Spec\Parser\Ast\AstInterface;
+use stdClass;
+
+/**
+ * GraphQL Built-in Scalar
+ *
+ * @see https://spec.graphql.org/draft/#sec-Scalars.Built-in-Scalars
+ */
+abstract class AbstractIntScalarTypeResolver extends AbstractScalarTypeResolver
+{
+    public function coerceValue(
+        string|int|float|bool|stdClass $inputValue,
+        AstInterface $astNode,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): string|int|float|bool|object|null {
+        $errorCount = $objectTypeFieldResolutionFeedbackStore->getErrors();
+        $this->validateIsNotStdClass($inputValue, $astNode, $objectTypeFieldResolutionFeedbackStore);
+        if ($objectTypeFieldResolutionFeedbackStore->getErrors() > $errorCount) {
+            return null;
+        }
+        /** @var string|int|float|bool $inputValue */
+
+        $castInputValue = CastToType::_int($inputValue);
+        if ($castInputValue === null) {
+            $this->addDefaultError($inputValue, $astNode, $objectTypeFieldResolutionFeedbackStore);
+            return null;
+        }
+        return (int) $castInputValue;
+    }
+}
