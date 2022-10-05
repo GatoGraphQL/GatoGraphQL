6bb7ec406fd863765dbfd5a6a2e7114ea3cd50d0
Leonardo Losoviz <leo@getpop.org>
2022-10-05 14:38:01 +0800

    Update siteurl option with https

--- webservers/graphql-api-for-wp/setup/install.sh
+++ webservers/graphql-api-for-wp/setup/install.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 wp core install --url="graphql-api.lndo.site" --title="GraphQL API" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --path=/app/wordpress 
+wp option update siteurl "https://graphql-api.lndo.site" --path=/app/wordpress 
+wp option update home "https://graphql-api.lndo.site" --path=/app/wordpress 
