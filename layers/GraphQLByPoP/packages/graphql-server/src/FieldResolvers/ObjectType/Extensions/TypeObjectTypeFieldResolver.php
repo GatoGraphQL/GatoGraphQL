a84c2002d13d58d4d567abc5c02fd80980e82f50
Leonardo Losoviz <leo@getpop.org>
2022-10-07 10:58:24 +0800

    Override 'fields'

--- layers/GraphQLByPoP/packages/graphql-server/src/FieldResolvers/ObjectType/Extensions/TypeObjectTypeFieldResolver.php
+++ layers/GraphQLByPoP/packages/graphql-server/src/FieldResolvers/ObjectType/Extensions/TypeObjectTypeFieldResolver.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace GraphQLByPoP\GraphQLServer\FieldResolvers\ObjectType\Extensions;
 
-use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
-use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
-use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
-use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
-use GraphQLByPoP\GraphQLServer\ObjectModels\NamedTypeInterface;
-use PoP\ComponentModel\Schema\SchemaTypeModifiers;
-use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
 use GraphQLByPoP\GraphQLServer\FieldResolvers\ObjectType\TypeObjectTypeFieldResolver as UpstreamTypeObjectTypeFieldResolver;
+use GraphQLByPoP\GraphQLServer\ObjectModels\HasFieldsTypeInterface;
+use GraphQLByPoP\GraphQLServer\ObjectModels\NamedTypeInterface;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
+use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
+use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
 
 class TypeObjectTypeFieldResolver extends UpstreamTypeObjectTypeFieldResolver
 {
@@ -28,6 +29,7 @@ class TypeObjectTypeFieldResolver extends UpstreamTypeObjectTypeFieldResolver
     {
         return [
             'name',
+            'fields',
         ];
     }
 
@@ -40,6 +42,7 @@ class TypeObjectTypeFieldResolver extends UpstreamTypeObjectTypeFieldResolver
     ): bool {
         return match ($field->getName()) {
             'name' => $field->hasArgument('namespaced'),
+            'fields' => $field->hasArgument('includeGlobal'),
             default => parent::resolveCanProcess($objectTypeResolver, $field),
         };
     }
@@ -53,6 +56,10 @@ class TypeObjectTypeFieldResolver extends UpstreamTypeObjectTypeFieldResolver
             'name' => [
                 'namespaced' => $this->getBooleanScalarTypeResolver(),
             ],
+            'fields' => [
+                ...parent::getFieldArgNameTypeResolvers($objectTypeResolver, $fieldName),
+                'includeGlobal' => $this->getBooleanScalarTypeResolver(),
+            ],
             default => parent::getFieldArgNameTypeResolvers($objectTypeResolver, $fieldName),
         };
     }
@@ -61,6 +68,7 @@ class TypeObjectTypeFieldResolver extends UpstreamTypeObjectTypeFieldResolver
     {
         return match ([$fieldName => $fieldArgName]) {
             ['name' => 'namespaced'] => $this->__('Namespace type name?', 'graphql-server'),
+            ['fields' => 'includeGlobal'] => $this->__('Include global fields?', 'graphql-server'),
             default => parent::getFieldArgDescription($objectTypeResolver, $fieldName, $fieldArgName),
         };
     }
@@ -69,6 +77,7 @@ class TypeObjectTypeFieldResolver extends UpstreamTypeObjectTypeFieldResolver
     {
         return match ([$fieldName => $fieldArgName]) {
             ['name' => 'namespaced'] => SchemaTypeModifiers::MANDATORY,
+            ['fields' => 'includeGlobal'] => SchemaTypeModifiers::MANDATORY,
             default => parent::getFieldArgTypeModifiers($objectTypeResolver, $fieldName, $fieldArgName),
         };
     }
@@ -87,6 +96,16 @@ class TypeObjectTypeFieldResolver extends UpstreamTypeObjectTypeFieldResolver
                     return $type->getNamespacedName();
                 }
                 return $type->getElementName();
+            case 'fields':
+                // From GraphQL spec (https://graphql.github.io/graphql-spec/draft/#sel-FAJbLAC1BJC3BAn6e):
+                // "should be non-null for OBJECT and INTERFACE only, must be null for the others"
+                if ($type instanceof HasFieldsTypeInterface) {
+                    return $type->getFieldIDs(
+                        $fieldDataAccessor->getValue('includeDeprecated'),
+                        $fieldDataAccessor->getValue('includeGlobal'),
+                    );
+                }
+                return null;
         }
 
         return parent::resolveValue($objectTypeResolver, $object, $fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
