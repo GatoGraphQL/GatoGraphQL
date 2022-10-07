729ddadc291086f0de150d01b6c0dd85e876466d
Leonardo Losoviz <leo@getpop.org>
2022-10-07 22:26:52 +0800

    add non-disable tabs only

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/packages/components/src/components/field-directive-multi-select-control/field-directive-tab-panel.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/packages/components/src/components/field-directive-multi-select-control/field-directive-tab-panel.js
@@ -5,28 +5,37 @@ import GlobalFieldMultiSelectControl from './global-field-multi-select-control';
 import DirectiveMultiSelectControl from './directive-multi-select-control';
 
 const FieldDirectiveTabPanel = ( props ) => {
-	const { className, typeFields, globalFields, directives } = props;
+	const {
+		className,
+		typeFields,
+		globalFields,
+		directives,
+		disableTypeFields,
+		disableGlobalFields,
+		disableDirectives,
+	} = props;
+	const tabs = [
+		...( disableTypeFields ? [] : [ {
+			name: 'tabTypeFields',
+			title: __('Fields', 'graphql-api'),
+			className: 'tab tab-fields tab-standard-fields',
+		} ] ),
+		...( disableGlobalFields ? [] : [ {
+			name: 'tabGlobalFields',
+			title: __('Global Fields', 'graphql-api'),
+			className: 'tab tab-fields tab-global-fields',
+		} ] ),
+		...( disableDirectives ? [] : [ {
+			name: 'tabDirectives',
+			title: __('Directives', 'graphql-api'),
+			className: 'tab tab-directives',
+		} ] ),
+	]
 	return (
 		<TabPanel
 			className={ className + '__tab_panel' }
 			activeClass="active-tab"
-			tabs={ [
-				{
-					name: 'tabTypeFields',
-					title: __('Fields', 'graphql-api'),
-					className: 'tab tab-fields tab-standard-fields',
-				},
-				{
-					name: 'tabGlobalFields',
-					title: __('Global Fields', 'graphql-api'),
-					className: 'tab tab-fields tab-global-fields',
-				},
-				{
-					name: 'tabDirectives',
-					title: __('Directives', 'graphql-api'),
-					className: 'tab tab-directives',
-				},
-			] }
+			tabs={ tabs }
 		>
 			{
 				( tab ) => tab.name == 'tabTypeFields' ?
