ddd389053ac46c6e6185e7eab221a74ca486d4f6
Leonardo Losoviz <leo@getpop.org>
2022-10-10 16:12:00 +0800

    FeatureNotSupportedException extends AbstractASTNodeException

--- layers/Engine/packages/graphql-parser/src/Exception/FeatureNotSupportedException.php
+++ layers/Engine/packages/graphql-parser/src/Exception/FeatureNotSupportedException.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace PoP\GraphQLParser\Exception;
 
-final class FeatureNotSupportedException extends AbstractASTNodeParserException
+final class FeatureNotSupportedException extends AbstractASTNodeException
 {
 }
