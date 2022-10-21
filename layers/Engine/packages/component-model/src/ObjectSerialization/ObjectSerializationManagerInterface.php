66495796ec1a9bfc94ae8b5364e0811fdd6afc15
Leonardo Losoviz <leo@getpop.org>
2022-10-21 13:57:45 +0800

    serialize can return stdClass

--- layers/Engine/packages/component-model/src/ObjectSerialization/ObjectSerializationManagerInterface.php
+++ layers/Engine/packages/component-model/src/ObjectSerialization/ObjectSerializationManagerInterface.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\ObjectSerialization;
 
+use stdClass;
+
 interface ObjectSerializationManagerInterface
 {
     public function addObjectSerializer(ObjectSerializerInterface $objectSerializer): void;
-    public function serialize(object $object): string;
+    public function serialize(object $object): string|int|float|bool|array|stdClass;
 }
