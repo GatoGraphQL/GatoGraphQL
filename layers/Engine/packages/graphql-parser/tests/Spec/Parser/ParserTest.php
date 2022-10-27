ef95b8de1b174652aba1192bc57154dd5986148f
Leonardo Losoviz <leo@getpop.org>
2022-10-27 15:12:23 +0800

    renamed vars

--- layers/Engine/packages/graphql-parser/tests/Spec/Parser/ParserTest.php
+++ layers/Engine/packages/graphql-parser/tests/Spec/Parser/ParserTest.php
@@ -675,26 +675,26 @@ GRAPHQL;
     public function queryWithVariablesProvider(): array
     {
         $arrayVariable = new Variable('arrayVariable', 'String', false, true, false, false, false, [], new Location(2, 9));
-        $arrayMandatoryVariable = new Variable('arrayMandatoryVariable', 'String', false, true, true, false, false, [], new Location(3, 9));
+        $arrayElemRequiredVariable = new Variable('arrayElemRequiredVariable', 'String', false, true, true, false, false, [], new Location(3, 9));
         $arrayOfArraysVariable = new Variable('arrayOfArraysVariable', 'Int', false, true, true, true, false, [], new Location(4, 9));
-        $arrayOfArraysMandatoryVariable = new Variable('arrayOfArraysMandatoryVariable', 'Int', false, true, true, true, true, [], new Location(5, 9));
-        $mandatoryArrayOfArraysMandatoryVariable = new Variable('mandatoryArrayOfArraysMandatoryVariable', 'Boolean', true, true, true, true, true, [], new Location(6, 9));
+        $arrayOfArraysElemRequiredVariable = new Variable('arrayOfArraysElemRequiredVariable', 'Int', false, true, true, true, true, [], new Location(5, 9));
+        $arrayOfArraysElemRequiredRequiredVariable = new Variable('arrayOfArraysElemRequiredRequiredVariable', 'Boolean', true, true, true, true, true, [], new Location(6, 9));
         return [
             [
                 <<<GRAPHQL
                     query SomeQuery(
                         \$arrayVariable: [String],
-                        \$arrayMandatoryVariable: [String!],
+                        \$arrayElemRequiredVariable: [String!],
                         \$arrayOfArraysVariable: [[Int]],
-                        \$arrayOfArraysMandatoryVariable: [[Int!]!],
-                        \$mandatoryArrayOfArraysMandatoryVariable: [[Boolean!]!]!,
+                        \$arrayOfArraysElemRequiredVariable: [[Int!]!],
+                        \$arrayOfArraysElemRequiredRequiredVariable: [[Boolean!]!]!,
                     ) {
                         items(
                             findBy1: \$arrayVariable,
-                            findBy2: \$arrayMandatoryVariable,
+                            findBy2: \$arrayElemRequiredVariable,
                             findBy3: \$arrayOfArraysVariable,
-                            findBy4: \$arrayOfArraysMandatoryVariable,
-                            findBy5: \$mandatoryArrayOfArraysMandatoryVariable,
+                            findBy4: \$arrayOfArraysElemRequiredVariable,
+                            findBy5: \$arrayOfArraysElemRequiredRequiredVariable,
                         ) {
                             name
                         }
@@ -706,10 +706,10 @@ GRAPHQL;
                             'SomeQuery',
                             [
                                 $arrayVariable,
-                                $arrayMandatoryVariable,
+                                $arrayElemRequiredVariable,
                                 $arrayOfArraysVariable,
-                                $arrayOfArraysMandatoryVariable,
-                                $mandatoryArrayOfArraysMandatoryVariable,
+                                $arrayOfArraysElemRequiredVariable,
+                                $arrayOfArraysElemRequiredRequiredVariable,
                             ],
                             [],
                             [
@@ -718,10 +718,10 @@ GRAPHQL;
                                     null,
                                     [
                                         new Argument('findBy1', new VariableReference('arrayVariable', $arrayVariable, new Location(1, 39)), new Location(1, 33)),
-                                        new Argument('findBy2', new VariableReference('arrayMandatoryVariable', $arrayMandatoryVariable, new Location(21, 39)), new Location(21, 33)),
+                                        new Argument('findBy2', new VariableReference('arrayElemRequiredVariable', $arrayElemRequiredVariable, new Location(21, 39)), new Location(21, 33)),
                                         new Argument('findBy3', new VariableReference('arrayOfArraysVariable', $arrayOfArraysVariable, new Location(31, 39)), new Location(31, 33)),
-                                        new Argument('findBy4', new VariableReference('arrayOfArraysMandatoryVariable', $arrayOfArraysMandatoryVariable, new Location(41, 39)), new Location(41, 33)),
-                                        new Argument('findBy5', new VariableReference('mandatoryArrayOfArraysMandatoryVariable', $mandatoryArrayOfArraysMandatoryVariable, new Location(51, 39)), new Location(51, 33)),
+                                        new Argument('findBy4', new VariableReference('arrayOfArraysElemRequiredVariable', $arrayOfArraysElemRequiredVariable, new Location(41, 39)), new Location(41, 33)),
+                                        new Argument('findBy5', new VariableReference('arrayOfArraysElemRequiredRequiredVariable', $arrayOfArraysElemRequiredRequiredVariable, new Location(51, 39)), new Location(51, 33)),
                                     ],
                                     [
                                         new LeafField('name', null, [], [], new Location(1, 60)),
@@ -734,7 +734,7 @@ GRAPHQL;
                         )
                     ]
                 ),
-                'query SomeQuery ($arrayVariable: [String], $arrayMandatoryVariable: [String!], $arrayOfArraysVariable: [[Int]!], $arrayOfArraysMandatoryVariable: [[Int!]!], $mandatoryArrayOfArraysMandatoryVariable: [[Int!]!]!) { query(items(findBy1: $arrayVariable, findBy2: $arrayMandatoryVariable, findBy3: $arrayOfArraysVariable, findBy4: $arrayOfArraysMandatoryVariable, findBy5: $mandatoryArrayOfArraysMandatoryVariable) { name } }',
+                'query SomeQuery ($arrayVariable: [String], $arrayElemRequiredVariable: [String!], $arrayOfArraysVariable: [[Int]!], $arrayOfArraysElemRequiredVariable: [[Int!]!], $arrayOfArraysElemRequiredRequiredVariable: [[Int!]!]!) { query(items(findBy1: $arrayVariable, findBy2: $arrayElemRequiredVariable, findBy3: $arrayOfArraysVariable, findBy4: $arrayOfArraysElemRequiredVariable, findBy5: $arrayOfArraysElemRequiredRequiredVariable) { name } }',
             ],
         ];
     }
