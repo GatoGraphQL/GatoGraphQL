dd014ef970fab9f873f90f5b9aa8fdc439ceed5b
Leonardo Losoviz <leo@getpop.org>
2022-11-03 17:11:22 +0800

    Added (commented) option for InstaWP

--- webservers/graphql-api-for-wp/setup/install.sh
+++ webservers/graphql-api-for-wp/setup/install.sh
@@ -2,3 +2,5 @@
 wp core install --url="graphql-api.lndo.site" --title="GraphQL API" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --path=/app/wordpress 
 wp option update siteurl "https://graphql-api.lndo.site" --path=/app/wordpress 
 wp option update home "https://graphql-api.lndo.site" --path=/app/wordpress 
+# This change is needed on InstaWP
+# wp option update admin_email "admin@example.com" --path=/app/wordpress
