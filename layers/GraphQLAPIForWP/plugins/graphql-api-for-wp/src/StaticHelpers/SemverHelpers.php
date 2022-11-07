6b60751c4aabb7a10fbf6325502c8a1b0b726dc5
Leonardo Losoviz <leo@getpop.org>
2022-11-07 08:52:17 +0800

    phpdoc

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/StaticHelpers/SemverHelpers.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/StaticHelpers/SemverHelpers.php
@@ -9,7 +9,14 @@ class SemverHelpers
     /**
      * Remove the "-dev..." section from the version, required during development
      * and testing of a generated plugin as to regenerate the container,
-     * but not needed for version constaints (and not supported by Composer Semver)
+     * but not needed for version constaints.
+     *
+     * Indeed, it must be removed as it is not supported by Composer Semver:
+     * "-dev" cannot be followed by the commit hash:
+     *
+     *   > private static $modifierRegex = '[._-]?(?:(stable|beta|b|RC|alpha|a|patch|pl|p)((?:[.-]?\d+)*+)?)?([.-]?dev)?';
+     *
+     * @see https://github.com/composer/semver/blob/9b2d75f/src/VersionParser.php#L39
      */
     public static function removeDevMetadataFromPluginVersion(string $pluginVersion): string
     {
