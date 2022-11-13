9dd35bf4ca55acc424dc08451e95c47a8a350036
Leonardo Losoviz <leo@getpop.org>
2022-11-13 16:58:05 +0800

    INSTAWP_INSTANCE_SLEEPING_TIME

--- src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
@@ -8,6 +8,7 @@ class EnvironmentVariablesDataSource
 {
     public final const GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE = 'GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE';
     public final const RETENTION_DAYS_FOR_GENERATED_PLUGINS = 'RETENTION_DAYS_FOR_GENERATED_PLUGINS';
+    public final const INSTAWP_INSTANCE_SLEEPING_TIME = 'INSTAWP_INSTANCE_SLEEPING_TIME';
 
     /**
      * @return array<string,string>
@@ -17,6 +18,7 @@ class EnvironmentVariablesDataSource
         return [
             self::GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE => false,
             self::RETENTION_DAYS_FOR_GENERATED_PLUGINS => 30,
+            self::INSTAWP_INSTANCE_SLEEPING_TIME => 120,
         ];
     }
 }
