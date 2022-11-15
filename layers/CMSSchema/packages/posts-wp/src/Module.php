6d5e9d4e7256039a58a25711789d9daf685b1b98
Leonardo Losoviz <leo@getpop.org>
2022-11-15 13:01:23 +0800

    Fixed Overrides

--- layers/CMSSchema/packages/posts-wp/src/Module.php
+++ layers/CMSSchema/packages/posts-wp/src/Module.php
@@ -40,5 +40,6 @@ class Module extends AbstractModule
         array $skipSchemaModuleClasses,
     ): void {
         $this->initServices(dirname(__DIR__));
+        $this->initSchemaServices(dirname(__DIR__), $skipSchema, '/Overrides');
     }
 }
