f4d3cde06b21694cd7a1eead8e3d2890a7f59b79
Leonardo Losoviz <leo@getpop.org>
2022-11-18 16:42:57 +0800

    Fields for Transient object

---
+++ layers/Schema/packages/schema-commons/src/FieldResolvers/ObjectType/TransientEntityPayloadObjectTypeFieldResolver.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\SchemaCommons\FieldResolvers\ObjectType;
+
+use PoPSchema\SchemaCommons\ObjectModels\AbstractTransientEntityPayload;
+use PoPSchema\SchemaCommons\TypeResolvers\ObjectType\ErrorPayloadObjectTypeResolver;
+use PoPSchema\SchemaCommons\TypeResolvers\ObjectType\AbstractTransientEntityPayloadObjectTypeResolver;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
+use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractObjectTypeFieldResolver;
+use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\TypeResolvers\ConcreteTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ScalarType\IDScalarTypeResolver;
+use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
+use PoPSchema\SchemaCommons\ObjectModels\ErrorPayload;
+
+class TransientEntityPayloadObjectTypeFieldResolver extends AbstractObjectTypeFieldResolver
+{
+    private ?IDScalarTypeResolver $idScalarTypeResolver = null;
+    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
+    private ?ErrorPayloadObjectTypeResolver $errorPayloadObjectTypeResolver = null;
+
+    final public function setIDScalarTypeResolver(IDScalarTypeResolver $idScalarTypeResolver): void
+    {
+        $this->idScalarTypeResolver = $idScalarTypeResolver;
+    }
+    final protected function getIDScalarTypeResolver(): IDScalarTypeResolver
+    {
+        /** @var IDScalarTypeResolver */
+        return $this->idScalarTypeResolver ??= $this->instanceManager->getInstance(IDScalarTypeResolver::class);
+    }
+    final public function setStringScalarTypeResolver(StringScalarTypeResolver $stringScalarTypeResolver): void
+    {
+        $this->stringScalarTypeResolver = $stringScalarTypeResolver;
+    }
+    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
+    {
+        /** @var StringScalarTypeResolver */
+        return $this->stringScalarTypeResolver ??= $this->instanceManager->getInstance(StringScalarTypeResolver::class);
+    }
+    final public function setErrorPayloadObjectTypeResolver(ErrorPayloadObjectTypeResolver $errorPayloadObjectTypeResolver): void
+    {
+        $this->errorPayloadObjectTypeResolver = $errorPayloadObjectTypeResolver;
+    }
+    final protected function getErrorPayloadObjectTypeResolver(): ErrorPayloadObjectTypeResolver
+    {
+        /** @var ErrorPayloadObjectTypeResolver */
+        return $this->errorPayloadObjectTypeResolver ??= $this->instanceManager->getInstance(ErrorPayloadObjectTypeResolver::class);
+    }
+
+    /**
+     * @return array<class-string<ObjectTypeResolverInterface>>
+     */
+    public function getObjectTypeResolverClassesToAttachTo(): array
+    {
+        return [
+            AbstractTransientEntityPayloadObjectTypeResolver::class,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getFieldNamesToResolve(): array
+    {
+        return [
+            'status',
+            'objectID',
+            'errors',
+        ];
+    }
+
+    public function getFieldTypeResolver(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ConcreteTypeResolverInterface
+    {
+        return match ($fieldName) {
+            'status' => $this->getStringScalarTypeResolver(),
+            'objectID' => $this->getIDScalarTypeResolver(),
+            'errors' => $this->getErrorPayloadObjectTypeResolver(),
+            default
+                => parent::getFieldTypeResolver($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldTypeModifiers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): int
+    {
+        return match ($fieldName) {
+            'status' => SchemaTypeModifiers::NON_NULLABLE,
+            'errors' => SchemaTypeModifiers::IS_ARRAY | SchemaTypeModifiers::IS_NON_NULLABLE_ITEMS_IN_ARRAY,
+            default => parent::getFieldTypeModifiers($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
+    {
+        return match ($fieldName) {
+            'status' => $this->__('Status of the operation', 'schema-commons'),
+            'objectID' => $this->__('ID of the entity, if the operation was successful', 'schema-commons'),
+            'errors' => $this->__('List of error data, if the operation failed', 'schema-commons'),
+            default => parent::getFieldDescription($objectTypeResolver, $fieldName),
+        };
+    }
+
+    /**
+     * The parent already resolves all fields
+     */
+    public function resolveValue(
+        ObjectTypeResolverInterface $objectTypeResolver,
+        object $object,
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): mixed {
+        /** @var AbstractTransientEntityPayload */
+        $transientEntityPayload = $object;
+        $fieldName = $fieldDataAccessor->getFieldName();
+        switch ($fieldName) {
+            case 'errors':
+                if ($transientEntityPayload->errors === null) {
+                    return null;
+                }
+                return array_map(
+                    fn (ErrorPayload $errorPayload) => $errorPayload->getID(),
+                    $transientEntityPayload->errors,
+                );
+        }
+        return parent::resolveValue($objectTypeResolver, $object, $fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
+    }
+}
