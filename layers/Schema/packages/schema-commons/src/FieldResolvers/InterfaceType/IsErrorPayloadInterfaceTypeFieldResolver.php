abb72a6860c8d5819de6bbb5c50877def96cbfc6
Leonardo Losoviz <leo@getpop.org>
2022-11-23 15:05:13 +0800

    Removed fields from IsErrorPayload interface

--- layers/Schema/packages/schema-commons/src/FieldResolvers/InterfaceType/IsErrorPayloadInterfaceTypeFieldResolver.php
+++ layers/Schema/packages/schema-commons/src/FieldResolvers/InterfaceType/IsErrorPayloadInterfaceTypeFieldResolver.php
@@ -5,29 +5,16 @@ declare(strict_types=1);
 namespace PoPSchema\SchemaCommons\FieldResolvers\InterfaceType;
 
 use PoPSchema\SchemaCommons\TypeResolvers\InterfaceType\IsErrorPayloadInterfaceTypeResolver;
-use PoPSchema\SchemaCommons\TypeResolvers\ScalarType\StringOrIntScalarTypeResolver;
 use PoP\ComponentModel\FieldResolvers\InterfaceType\AbstractInterfaceTypeFieldResolver;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoP\ComponentModel\TypeResolvers\ConcreteTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\InterfaceType\InterfaceTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
-use PoP\Engine\TypeResolvers\ScalarType\JSONObjectScalarTypeResolver;
 
 class IsErrorPayloadInterfaceTypeFieldResolver extends AbstractInterfaceTypeFieldResolver
 {
-    private ?StringOrIntScalarTypeResolver $stringOrIntScalarTypeResolver = null;
     private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
-    private ?JSONObjectScalarTypeResolver $jsonObjectScalarTypeResolver = null;
 
-    final public function setStringOrIntScalarTypeResolver(StringOrIntScalarTypeResolver $stringOrIntScalarTypeResolver): void
-    {
-        $this->stringOrIntScalarTypeResolver = $stringOrIntScalarTypeResolver;
-    }
-    final protected function getStringOrIntScalarTypeResolver(): StringOrIntScalarTypeResolver
-    {
-        /** @var StringOrIntScalarTypeResolver */
-        return $this->stringOrIntScalarTypeResolver ??= $this->instanceManager->getInstance(StringOrIntScalarTypeResolver::class);
-    }
     final public function setStringScalarTypeResolver(StringScalarTypeResolver $stringScalarTypeResolver): void
     {
         $this->stringScalarTypeResolver = $stringScalarTypeResolver;
@@ -36,15 +23,6 @@ class IsErrorPayloadInterfaceTypeFieldResolver extends AbstractInterfaceTypeFiel
     {
         /** @var StringScalarTypeResolver */
         return $this->stringScalarTypeResolver ??= $this->instanceManager->getInstance(StringScalarTypeResolver::class);
-    }
-    final public function setJSONObjectScalarTypeResolver(JSONObjectScalarTypeResolver $jsonObjectScalarTypeResolver): void
-    {
-        $this->jsonObjectScalarTypeResolver = $jsonObjectScalarTypeResolver;
-    }
-    final protected function getJSONObjectScalarTypeResolver(): JSONObjectScalarTypeResolver
-    {
-        /** @var JSONObjectScalarTypeResolver */
-        return $this->jsonObjectScalarTypeResolver ??= $this->instanceManager->getInstance(JSONObjectScalarTypeResolver::class);
     }
     
     /**
@@ -64,8 +42,6 @@ class IsErrorPayloadInterfaceTypeFieldResolver extends AbstractInterfaceTypeFiel
     {
         return [
             'message',
-            'code',
-            'data',
         ];
     }
 
@@ -73,10 +49,7 @@ class IsErrorPayloadInterfaceTypeFieldResolver extends AbstractInterfaceTypeFiel
     {
         return match ($fieldName) {
             'message' => $this->getStringScalarTypeResolver(),
-            'code' => $this->getStringOrIntScalarTypeResolver(),
-            'data' => $this->getJSONObjectScalarTypeResolver(),
-            default
-                => parent::getFieldTypeResolver($fieldName),
+            default => parent::getFieldTypeResolver($fieldName),
         };
     }
 
@@ -92,8 +65,6 @@ class IsErrorPayloadInterfaceTypeFieldResolver extends AbstractInterfaceTypeFiel
     {
         return match ($fieldName) {
             'message' => $this->__('Error message', 'schema-commons'),
-            'code' => $this->__('Error code', 'schema-commons'),
-            'data' => $this->__('Error data', 'schema-commons'),
             default => parent::getFieldDescription($fieldName),
         };
     }
