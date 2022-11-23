316cf5d02f84cf22b132f83d6f1ed1b41a9c7a92
Leonardo Losoviz <leo@getpop.org>
2022-11-23 14:08:14 +0800

    Moved code to PayloadableUpdateCustomPostMutationResolverTrait

--- layers/CMSSchema/packages/custompost-mutations/src/MutationResolvers/PayloadableUpdateCustomPostMutationResolverTrait.php
+++ layers/CMSSchema/packages/custompost-mutations/src/MutationResolvers/PayloadableUpdateCustomPostMutationResolverTrait.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CustomPostMutations\MutationResolvers;
 
+use PoPCMSSchema\CustomPostMutations\Exception\CustomPostCRUDMutationException;
 use PoPCMSSchema\CustomPostMutations\FeedbackItemProviders\MutationErrorFeedbackItemProvider;
 use PoPCMSSchema\CustomPostMutations\ObjectModels\CustomPostDoesNotExistErrorPayload;
 use PoPCMSSchema\CustomPostMutations\ObjectModels\LoggedInUserHasNoPermissionToEditCustomPostErrorPayload;
+use PoPSchema\SchemaCommons\MutationResolvers\PayloadableMutationResolverTrait;
 use PoPSchema\SchemaCommons\ObjectModels\ErrorPayloadInterface;
 use PoPSchema\SchemaCommons\ObjectModels\GenericErrorPayload;
+use PoP\ComponentModel\Container\ObjectDictionaryInterface;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackInterface;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
@@ -16,8 +19,21 @@ use PoP\Root\Exception\AbstractException;
 
 trait PayloadableUpdateCustomPostMutationResolverTrait
 {
+    use PayloadableMutationResolverTrait;    
     use UpdateCustomPostMutationResolverTrait {
         UpdateCustomPostMutationResolverTrait::executeMutation as upstreamExecuteMutation;
+    }
+
+    private ?ObjectDictionaryInterface $objectDictionary = null;
+
+    final public function setObjectDictionary(ObjectDictionaryInterface $objectDictionary): void
+    {
+        $this->objectDictionary = $objectDictionary;
+    }
+    final protected function getObjectDictionary(): ObjectDictionaryInterface
+    {
+        /** @var ObjectDictionaryInterface */
+        return $this->objectDictionary ??= $this->instanceManager->getInstance(ObjectDictionaryInterface::class);
     }
     
     /**
@@ -34,14 +50,24 @@ trait PayloadableUpdateCustomPostMutationResolverTrait
         if ($separateObjectTypeFieldResolutionFeedbackStore->getErrors() !== []) {
             return $this->createAndStoreErrorPayloadsFromObjectTypeFieldResolutionFeedbacks($separateObjectTypeFieldResolutionFeedbackStore->getErrors());
         }
-        $result = $this->upstreamExecuteMutation(
-            $fieldDataAccessor,
-            $separateObjectTypeFieldResolutionFeedbackStore,
-        );
+
+        $customPostID = null;
+        try {
+            /** @var string|int */
+            $customPostID = $this->upstreamExecuteMutation(
+                $fieldDataAccessor,
+                $separateObjectTypeFieldResolutionFeedbackStore,
+            );
+        } catch (CustomPostCRUDMutationException $customPostCRUDMutationException) {
+            return $this->createFailurePayloadMutation($customPostCRUDMutationException);
+        }
+
         if ($separateObjectTypeFieldResolutionFeedbackStore->getErrors() !== []) {
             return $this->createAndStoreErrorPayloadsFromObjectTypeFieldResolutionFeedbacks($separateObjectTypeFieldResolutionFeedbackStore->getErrors());
-        }    
-        return $result;
+        }
+
+        /** @var string|int $customPostID */
+        return $this->createSuccessPayloadMutation($customPostID);
     }
 
     /**
--- layers/CMSSchema/packages/post-mutations/src/MutationResolvers/PayloadableUpdatePostMutationResolver.php
+++ layers/CMSSchema/packages/post-mutations/src/MutationResolvers/PayloadableUpdatePostMutationResolver.php
@@ -4,51 +4,9 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\PostMutations\MutationResolvers;
 
-use PoPCMSSchema\CustomPostMutations\Exception\CustomPostCRUDMutationException;
 use PoPCMSSchema\CustomPostMutations\MutationResolvers\PayloadableUpdateCustomPostMutationResolverTrait;
-use PoPSchema\SchemaCommons\MutationResolvers\PayloadableMutationResolverTrait;
-use PoP\ComponentModel\Container\ObjectDictionaryInterface;
-use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
-use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
-use PoP\Root\Exception\AbstractException;
 
-class PayloadableUpdatePostMutationResolver extends UpdatePostMutationResolver
+class PayloadableUpdatePostMutationResolver extends AbstractCreateUpdatePostMutationResolver
 {
     use PayloadableUpdateCustomPostMutationResolverTrait;
-    use PayloadableMutationResolverTrait;
-    
-    private ?ObjectDictionaryInterface $objectDictionary = null;
-
-    final public function setObjectDictionary(ObjectDictionaryInterface $objectDictionary): void
-    {
-        $this->objectDictionary = $objectDictionary;
-    }
-    final protected function getObjectDictionary(): ObjectDictionaryInterface
-    {
-        /** @var ObjectDictionaryInterface */
-        return $this->objectDictionary ??= $this->instanceManager->getInstance(ObjectDictionaryInterface::class);
-    }
-
-    /**
-     * Catch the CRUD exception, and return it in the Mutation Payload
-     * 
-     * @throws AbstractException In case of error
-     */
-    public function executeMutation(
-        FieldDataAccessorInterface $fieldDataAccessor,
-        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
-    ): mixed {
-        $customPostID = null;
-        try {
-            /** @var string|int */
-            $customPostID = parent::executeMutation(
-                $fieldDataAccessor,
-                $objectTypeFieldResolutionFeedbackStore,
-            );
-        } catch (CustomPostCRUDMutationException $customPostCRUDMutationException) {
-            return $this->createFailurePayloadMutation($customPostCRUDMutationException);
-        }
-        /** @var string|int $customPostID */
-        return $this->createSuccessPayloadMutation($customPostID);
-    }
 }
