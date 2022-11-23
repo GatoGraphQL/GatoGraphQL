c35f06ec87915e0bbb0677836b8609a71858e36b
Leonardo Losoviz <leo@getpop.org>
2022-11-23 14:45:52 +0800

    Re-added MutationPayload

--- layers/Schema/packages/schema-commons/src/ObjectModels/ObjectMutationPayload.php
+++ layers/Schema/packages/schema-commons/src/ObjectModels/ObjectMutationPayload.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace PoPSchema\SchemaCommons\ObjectModels;
 
+/**
+ * Mutations executed on an object, such as `Post.update`
+ */
 final class ObjectMutationPayload extends AbstractObjectMutationTransientOperationPayload
 {
 }
