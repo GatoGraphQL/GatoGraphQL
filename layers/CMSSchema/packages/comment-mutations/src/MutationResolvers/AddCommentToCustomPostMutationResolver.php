fb565bbbf1eaac5762b5d31cbdf672e19947f3de
Leonardo Losoviz <leo@getpop.org>
2022-11-25 16:05:43 +0800

    if ($customPostID =

--- layers/CMSSchema/packages/comment-mutations/src/MutationResolvers/AddCommentToCustomPostMutationResolver.php
+++ layers/CMSSchema/packages/comment-mutations/src/MutationResolvers/AddCommentToCustomPostMutationResolver.php
@@ -148,20 +148,21 @@ class AddCommentToCustomPostMutationResolver extends AbstractMutationResolver
             }
         }
         // Make sure the custom post exists
-        $customPostID = $fieldDataAccessor->getValue(MutationInputProperties::CUSTOMPOST_ID);
-        if (!$this->getCustomPostTypeAPI()->customPostExists($customPostID)) {
-            $objectTypeFieldResolutionFeedbackStore->addError(
-                new ObjectTypeFieldResolutionFeedback(
-                    new FeedbackItemResolution(
-                        MutationErrorFeedbackItemProvider::class,
-                        MutationErrorFeedbackItemProvider::E7,
-                        [
-                            $customPostID,
-                        ]
-                    ),
-                    $field,
-                )
-            );
+        if ($customPostID = $fieldDataAccessor->getValue(MutationInputProperties::CUSTOMPOST_ID)) {
+            if (!$this->getCustomPostTypeAPI()->customPostExists($customPostID)) {
+                $objectTypeFieldResolutionFeedbackStore->addError(
+                    new ObjectTypeFieldResolutionFeedback(
+                        new FeedbackItemResolution(
+                            MutationErrorFeedbackItemProvider::class,
+                            MutationErrorFeedbackItemProvider::E7,
+                            [
+                                $customPostID,
+                            ]
+                        ),
+                        $field,
+                    )
+                );
+            }
         }
         if (!$fieldDataAccessor->getValue(MutationInputProperties::COMMENT)) {
             $objectTypeFieldResolutionFeedbackStore->addError(
