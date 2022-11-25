17a74e68cafd4acfda92613e84dffd5d6094db34
Leonardo Losoviz <leo@getpop.org>
2022-11-25 14:54:37 +0800

    Validate that the custom post exists

--- layers/CMSSchema/packages/comment-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
+++ layers/CMSSchema/packages/comment-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
@@ -15,6 +15,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     public final const E4 = 'e4';
     public final const E5 = 'e5';
     public final const E6 = 'e6';
+    public final const E7 = 'e7';
 
     /**
      * @return string[]
@@ -28,6 +29,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
             self::E4,
             self::E5,
             self::E6,
+            self::E7,
         ];
     }
 
@@ -40,6 +42,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
             self::E4 => $this->__('The custom post ID is missing.', 'comment-mutations'),
             self::E5 => $this->__('The comment is empty.', 'comment-mutations'),
             self::E6 => $this->__('There is no (parent) comment with ID \'%s\'.', 'comment-mutations'),
+            self::E7 => $this->__('There is no custom post with ID \'%s\'.', 'comment-mutations'),
             default => parent::getMessagePlaceholder($code),
         };
     }
