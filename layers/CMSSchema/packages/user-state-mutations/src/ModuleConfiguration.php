0f954af661ff0a5183f6d8fdc5636cb7a8ea33d2
Leonardo Losoviz <leo@getpop.org>
2022-11-28 13:59:30 +0800

    Added env var USE_PAYLOADABLE_USERSTATE_MUTATIONS

---
+++ layers/CMSSchema/packages/user-state-mutations/src/ModuleConfiguration.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\UserStateMutations;
+
+use PoP\Root\Module\AbstractModuleConfiguration;
+use PoP\Root\Module\EnvironmentValueHelpers;
+
+class ModuleConfiguration extends AbstractModuleConfiguration
+{
+    /**
+     * Indicate if to return the errors in an ObjectMutationPayload
+     * object in the response, or if to use the top-level errors.
+     */
+    public function usePayloadableUserStateMutations(): bool
+    {
+        $envVariable = Environment::USE_PAYLOADABLE_USERSTATE_MUTATIONS;
+        $defaultValue = true;
+        $callback = EnvironmentValueHelpers::toBool(...);
+
+        return $this->retrieveConfigurationValueOrUseDefault(
+            $envVariable,
+            $defaultValue,
+            $callback,
+        );
+    }
+}
