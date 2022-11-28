e6730e24004aba2975ec2068f05895ff16009f5c
Leonardo Losoviz <leo@getpop.org>
2022-11-29 07:55:46 +0800

    env var

---
+++ layers/CMSSchema/packages/custompostmedia-mutations/src/Environment.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\CustomPostMediaMutations;
+
+class Environment
+{
+    public final const USE_PAYLOADABLE_CUSTOMPOSTMEDIA_MUTATIONS = 'USE_PAYLOADABLE_CUSTOMPOSTMEDIA_MUTATIONS';
+}
