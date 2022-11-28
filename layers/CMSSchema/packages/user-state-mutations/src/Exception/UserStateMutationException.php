6ac142666f8313015d89824fce4099677ef55123
Leonardo Losoviz <leo@getpop.org>
2022-11-28 14:26:46 +0800

    extend AbstractPayloadClientException

--- layers/CMSSchema/packages/user-state-mutations/src/Exception/UserStateMutationException.php
+++ layers/CMSSchema/packages/user-state-mutations/src/Exception/UserStateMutationException.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\UserStateMutations\Exception;
 
-use PoP\Root\Exception\AbstractClientException;
+use PoPSchema\SchemaCommons\Exception\AbstractPayloadClientException;
 
-final class UserStateMutationException extends AbstractClientException
+final class UserStateMutationException extends AbstractPayloadClientException
 {
 }
