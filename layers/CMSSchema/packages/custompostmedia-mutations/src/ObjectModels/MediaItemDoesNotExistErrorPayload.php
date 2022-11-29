ced5af8faaaf8404a14cd0150d649f1c7124759c
Leonardo Losoviz <leo@getpop.org>
2022-11-29 08:50:32 +0800

    Media Item does not exist error

---
+++ layers/CMSSchema/packages/custompostmedia-mutations/src/ObjectModels/MediaItemDoesNotExistErrorPayload.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\CustomPostMediaMutations\ObjectModels;
+
+use PoPSchema\SchemaCommons\ObjectModels\AbstractErrorPayload;
+
+final class MediaItemDoesNotExistErrorPayload extends AbstractErrorPayload
+{
+}
