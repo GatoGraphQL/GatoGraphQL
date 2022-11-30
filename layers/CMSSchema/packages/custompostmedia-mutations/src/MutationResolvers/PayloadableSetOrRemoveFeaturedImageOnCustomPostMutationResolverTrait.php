8252c8bd8d18a2e20137611c8240ebc4270c9fef
Leonardo Losoviz <leo@getpop.org>
2022-11-30 15:51:46 +0800

    PayloadableCustomPostMutationResolverTrait

--- layers/CMSSchema/packages/custompostmedia-mutations/src/MutationResolvers/PayloadableSetOrRemoveFeaturedImageOnCustomPostMutationResolverTrait.php
+++ layers/CMSSchema/packages/custompostmedia-mutations/src/MutationResolvers/PayloadableSetOrRemoveFeaturedImageOnCustomPostMutationResolverTrait.php
@@ -6,18 +6,17 @@ namespace PoPCMSSchema\CustomPostMediaMutations\MutationResolvers;
 
 use PoPCMSSchema\CustomPostMediaMutations\FeedbackItemProviders\MutationErrorFeedbackItemProvider;
 use PoPCMSSchema\CustomPostMediaMutations\ObjectModels\MediaItemDoesNotExistErrorPayload;
-use PoPCMSSchema\CustomPostMutations\FeedbackItemProviders\MutationErrorFeedbackItemProvider as CustomPostsMutationErrorFeedbackItemProvider;
-use PoPCMSSchema\CustomPostMutations\ObjectModels\CustomPostDoesNotExistErrorPayload;
-use PoPCMSSchema\CustomPostMutations\ObjectModels\LoggedInUserHasNoEditingCustomPostCapabilityErrorPayload;
-use PoPCMSSchema\CustomPostMutations\ObjectModels\LoggedInUserHasNoPermissionToEditCustomPostErrorPayload;
-use PoPCMSSchema\CustomPostMutations\ObjectModels\LoggedInUserHasNoPublishingCustomPostCapabilityErrorPayload;
+use PoPCMSSchema\CustomPostMutations\MutationResolvers\PayloadableCustomPostMutationResolverTrait;
 use PoPCMSSchema\UserStateMutations\ObjectModels\UserIsNotLoggedInErrorPayload;
 use PoPSchema\SchemaCommons\ObjectModels\ErrorPayloadInterface;
-use PoPSchema\SchemaCommons\ObjectModels\GenericErrorPayload;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackInterface;
 
 trait PayloadableSetOrRemoveFeaturedImageOnCustomPostMutationResolverTrait
 {
+    use PayloadableCustomPostMutationResolverTrait {
+        PayloadableCustomPostMutationResolverTrait::createErrorPayloadFromObjectTypeFieldResolutionFeedback as upstreamCreateErrorPayloadFromObjectTypeFieldResolutionFeedback;
+    }
+    
     protected function createErrorPayloadFromObjectTypeFieldResolutionFeedback(
         ObjectTypeFieldResolutionFeedbackInterface $objectTypeFieldResolutionFeedback
     ): ErrorPayloadInterface {
@@ -30,38 +29,13 @@ trait PayloadableSetOrRemoveFeaturedImageOnCustomPostMutationResolverTrait
                 $errorFeedbackItemResolution->getMessage(),
             ),
             [
-                CustomPostsMutationErrorFeedbackItemProvider::class,
-                CustomPostsMutationErrorFeedbackItemProvider::E2,
-            ] => new LoggedInUserHasNoEditingCustomPostCapabilityErrorPayload(
-                $errorFeedbackItemResolution->getMessage(),
-            ),
-            [
-                CustomPostsMutationErrorFeedbackItemProvider::class,
-                CustomPostsMutationErrorFeedbackItemProvider::E3,
-            ] => new LoggedInUserHasNoPublishingCustomPostCapabilityErrorPayload(
-                $errorFeedbackItemResolution->getMessage(),
-            ),
-            [
-                CustomPostsMutationErrorFeedbackItemProvider::class,
-                CustomPostsMutationErrorFeedbackItemProvider::E8,
-            ] => new LoggedInUserHasNoPermissionToEditCustomPostErrorPayload(
-                $errorFeedbackItemResolution->getMessage(),
-            ),
-            [
-                CustomPostsMutationErrorFeedbackItemProvider::class,
-                CustomPostsMutationErrorFeedbackItemProvider::E7,
-            ] => new CustomPostDoesNotExistErrorPayload(
-                $errorFeedbackItemResolution->getMessage(),
-            ),
-            [
                 MutationErrorFeedbackItemProvider::class,
                 MutationErrorFeedbackItemProvider::E2,
             ] => new MediaItemDoesNotExistErrorPayload(
                 $errorFeedbackItemResolution->getMessage(),
             ),
-            default => new GenericErrorPayload(
-                $errorFeedbackItemResolution->getMessage(),
-                $errorFeedbackItemResolution->getNamespacedCode(),
+            default => $this->upstreamCreateErrorPayloadFromObjectTypeFieldResolutionFeedback(
+                $objectTypeFieldResolutionFeedback
             ),
         };
     }
