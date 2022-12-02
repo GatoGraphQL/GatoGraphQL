772f90f982245b4b3bdca97b1f8f830172f1b4a0
Leonardo Losoviz <leo@getpop.org>
2022-12-02 13:12:52 +0800

    maybeValidateCategories

--- layers/CMSSchema/packages/custompost-category-mutations/src/Hooks/AbstractMutationResolverHookSet.php
+++ layers/CMSSchema/packages/custompost-category-mutations/src/Hooks/AbstractMutationResolverHookSet.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CustomPostCategoryMutations\Hooks;
 
+use PoPCMSSchema\CustomPostCategoryMutations\MutationResolvers\MutationInputProperties;
+use PoPCMSSchema\CustomPostCategoryMutations\MutationResolvers\SetCategoriesOnCustomPostMutationResolverTrait;
+use PoPCMSSchema\CustomPostCategoryMutations\TypeAPIs\CustomPostCategoryTypeMutationAPIInterface;
+use PoPCMSSchema\CustomPostMutations\MutationResolvers\AbstractCreateUpdateCustomPostMutationResolver;
+use PoPCMSSchema\CustomPostMutations\MutationResolvers\MutationInputProperties as CustomPostMutationsMutationInputProperties;
+use PoPCMSSchema\CustomPosts\TypeAPIs\CustomPostTypeAPIInterface;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
 use PoP\Root\App;
 use PoP\Root\Hooks\AbstractHookSet;
-use PoPCMSSchema\CustomPostCategoryMutations\MutationResolvers\MutationInputProperties;
-use PoPCMSSchema\CustomPostCategoryMutations\TypeAPIs\CustomPostCategoryTypeMutationAPIInterface;
-use PoPCMSSchema\CustomPostMutations\MutationResolvers\AbstractCreateUpdateCustomPostMutationResolver;
-use PoPCMSSchema\CustomPosts\TypeAPIs\CustomPostTypeAPIInterface;
 
 abstract class AbstractMutationResolverHookSet extends AbstractHookSet
 {
+    use SetCategoriesOnCustomPostMutationResolverTrait;
+ 
     private ?CustomPostTypeAPIInterface $customPostTypeAPI = null;
 
     final public function setCustomPostTypeAPI(CustomPostTypeAPIInterface $customPostTypeAPI): void
@@ -29,6 +34,12 @@ abstract class AbstractMutationResolverHookSet extends AbstractHookSet
     protected function init(): void
     {
         App::addAction(
+            AbstractCreateUpdateCustomPostMutationResolver::HOOK_VALIDATE_CREATE_OR_UPDATE,
+            $this->maybeValidateCategories(...),
+            10,
+            2
+        );
+        App::addAction(
             AbstractCreateUpdateCustomPostMutationResolver::HOOK_EXECUTE_CREATE_OR_UPDATE,
             $this->maybeSetCategories(...),
             10,
@@ -36,13 +47,33 @@ abstract class AbstractMutationResolverHookSet extends AbstractHookSet
         );
     }
 
-    public function maybeSetCategories(int|string $customPostID, FieldDataAccessorInterface $fieldDataAccessor): void
-    {
+    public function maybeValidateCategories(
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): void {
+        if (!$fieldDataAccessor->hasValue(MutationInputProperties::CATEGORY_IDS)) {
+            return;
+        }
         // Only for that specific CPT
+        $customPostID = $fieldDataAccessor->getValue(CustomPostMutationsMutationInputProperties::ID);
         if ($this->getCustomPostTypeAPI()->getCustomPostType($customPostID) !== $this->getCustomPostType()) {
             return;
         }
+        $customPostCategoryIDs = $fieldDataAccessor->getValue(MutationInputProperties::CATEGORY_IDS);
+        $this->validateCategoriesExist(
+            $customPostCategoryIDs,
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+    }
+
+    public function maybeSetCategories(int|string $customPostID, FieldDataAccessorInterface $fieldDataAccessor): void
+    {
         if (!$fieldDataAccessor->hasValue(MutationInputProperties::CATEGORY_IDS)) {
+            return;
+        }
+        // Only for that specific CPT
+        if ($this->getCustomPostTypeAPI()->getCustomPostType($customPostID) !== $this->getCustomPostType()) {
             return;
         }
         $customPostCategoryIDs = $fieldDataAccessor->getValue(MutationInputProperties::CATEGORY_IDS);
