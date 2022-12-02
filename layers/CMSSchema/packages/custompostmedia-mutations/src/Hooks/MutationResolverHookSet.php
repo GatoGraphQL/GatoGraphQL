6f105d28959fe66678be21d58595005c8b78c2e4
Leonardo Losoviz <leo@getpop.org>
2022-12-02 13:28:17 +0800

    Validate featuredImage exists

--- layers/CMSSchema/packages/custompostmedia-mutations/src/Hooks/MutationResolverHookSet.php
+++ layers/CMSSchema/packages/custompostmedia-mutations/src/Hooks/MutationResolverHookSet.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CustomPostMediaMutations\Hooks;
 
+use PoPCMSSchema\CustomPostMediaMutations\MutationResolvers\MutationInputProperties;
+use PoPCMSSchema\CustomPostMediaMutations\MutationResolvers\SetFeaturedImageOnCustomPostMutationResolverTrait;
+use PoPCMSSchema\CustomPostMediaMutations\TypeAPIs\CustomPostMediaTypeMutationAPIInterface;
+use PoPCMSSchema\CustomPostMutations\MutationResolvers\AbstractCreateUpdateCustomPostMutationResolver;
+use PoPCMSSchema\Media\TypeAPIs\MediaTypeAPIInterface;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
 use PoP\Root\App;
 use PoP\Root\Hooks\AbstractHookSet;
-use PoPCMSSchema\CustomPostMediaMutations\MutationResolvers\MutationInputProperties;
-use PoPCMSSchema\CustomPostMediaMutations\TypeAPIs\CustomPostMediaTypeMutationAPIInterface;
-use PoPCMSSchema\CustomPostMutations\MutationResolvers\AbstractCreateUpdateCustomPostMutationResolver;
 
 class MutationResolverHookSet extends AbstractHookSet
 {
+    use SetFeaturedImageOnCustomPostMutationResolverTrait;
+
     private ?CustomPostMediaTypeMutationAPIInterface $customPostMediaTypeMutationAPI = null;
+    private ?MediaTypeAPIInterface $mediaTypeAPI = null;
 
     final public function setCustomPostMediaTypeMutationAPI(CustomPostMediaTypeMutationAPIInterface $customPostMediaTypeMutationAPI): void
     {
@@ -24,9 +30,24 @@ class MutationResolverHookSet extends AbstractHookSet
         /** @var CustomPostMediaTypeMutationAPIInterface */
         return $this->customPostMediaTypeMutationAPI ??= $this->instanceManager->getInstance(CustomPostMediaTypeMutationAPIInterface::class);
     }
+    final public function setMediaTypeAPI(MediaTypeAPIInterface $mediaTypeAPI): void
+    {
+        $this->mediaTypeAPI = $mediaTypeAPI;
+    }
+    final protected function getMediaTypeAPI(): MediaTypeAPIInterface
+    {
+        /** @var MediaTypeAPIInterface */
+        return $this->mediaTypeAPI ??= $this->instanceManager->getInstance(MediaTypeAPIInterface::class);
+    }
 
     protected function init(): void
     {
+        App::addAction(
+            AbstractCreateUpdateCustomPostMutationResolver::HOOK_VALIDATE_CREATE_OR_UPDATE,
+            $this->maybeValidateMediaItem(...),
+            10,
+            2
+        );
         App::addAction(
             AbstractCreateUpdateCustomPostMutationResolver::HOOK_EXECUTE_CREATE_OR_UPDATE,
             $this->setOrRemoveFeaturedImage(...),
@@ -35,18 +56,50 @@ class MutationResolverHookSet extends AbstractHookSet
         );
     }
 
+    public function maybeValidateMediaItem(
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): void {
+        if (!$this->canExecuteMutation($fieldDataAccessor)) {
+            return;
+        }
+        $mediaItemID = $fieldDataAccessor->getValue(MutationInputProperties::MEDIA_ITEM_ID);
+        $this->validateMediaItemExists(
+            $mediaItemID,
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+    }
+
+    /**
+     * Entry "featuredImageID" must either have an ID or `null` to execute
+     * the mutation. Only if not provided, then nothing to do.
+     */
+    protected function canExecuteMutation(
+        FieldDataAccessorInterface $fieldDataAccessor,
+    ): bool {
+        return $fieldDataAccessor->hasValue(MutationInputProperties::FEATUREDIMAGE_ID);
+    }
+
     /**
      * If entry "featuredImageID" has an ID, set it. If it is null, remove it
      */
     public function setOrRemoveFeaturedImage(int|string $customPostID, FieldDataAccessorInterface $fieldDataAccessor): void
     {
-        if (!$fieldDataAccessor->hasValue(MutationInputProperties::FEATUREDIMAGE_ID)) {
+        if (!$this->canExecuteMutation($fieldDataAccessor)) {
             return;
         }
-        if ($featuredImageID = $fieldDataAccessor->getValue(MutationInputProperties::FEATUREDIMAGE_ID)) {
+        /**
+         * If it has an ID, set the featured image
+         */
+        $featuredImageID = $fieldDataAccessor->getValue(MutationInputProperties::FEATUREDIMAGE_ID);
+        if ($featuredImageID !== null) {
             $this->getCustomPostMediaTypeMutationAPI()->setFeaturedImage($customPostID, $featuredImageID);
-        } else {
-            $this->getCustomPostMediaTypeMutationAPI()->removeFeaturedImage($customPostID);
+            return;
         }
+        /**
+         * If is `null` => remove the featured image
+         */
+        $this->getCustomPostMediaTypeMutationAPI()->removeFeaturedImage($customPostID);
     }
 }
