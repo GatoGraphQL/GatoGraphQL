110357d031fe6df660bc21e2683eed7c22eeb40f
Leonardo Losoviz <leo@getpop.org>
2022-12-05 15:52:42 +0800

    SplObjectStorage $fragmentReferenceFragments

--- layers/Engine/packages/graphql-parser/src/AST/ASTNodeDuplicatorService.php
+++ layers/Engine/packages/graphql-parser/src/AST/ASTNodeDuplicatorService.php
@@ -6,18 +6,30 @@ namespace PoP\GraphQLParser\AST;
 
 use PoP\GraphQLParser\Spec\Parser\Ast\Fragment;
 use PoP\GraphQLParser\Spec\Parser\Ast\FragmentReference;
+use SplObjectStorage;
 
 class ASTNodeDuplicatorService implements ASTNodeDuplicatorServiceInterface
 {
     /**
+     * @var SplObjectStorage<FragmentReference,Fragment>
+     */
+    protected SplObjectStorage $fragmentReferenceFragments;
+
+    /**
+     * Retrieve a specific and dynamic Fragment for that FragmentReference
+     *
      * @param Fragment[] $fragments
      */
     public function getFragment(
         FragmentReference $fragmentReference,
         array $fragments,
     ): ?Fragment {
+        if ($this->fragmentReferenceFragments->contains($fragmentReference)) {
+            return $this->fragmentReferenceFragments[$fragmentReference];
+        }
         foreach ($fragments as $fragment) {
             if ($fragment->getName() === $fragmentReference->getName()) {
+                $this->fragmentReferenceFragments[$fragmentReference] = $fragment;
                 return $fragment;
             }
         }
