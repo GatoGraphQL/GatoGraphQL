175e75cfb5eae678613443e724db5ecbd7dc896d
Leonardo Losoviz <leo@getpop.org>
2022-12-06 10:53:53 +0800

    RuntimeLocation

---
+++ layers/Engine/packages/graphql-parser/src/ExtendedSpec/Parser/RuntimeLocation.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\GraphQLParser\ExtendedSpec\Parser;
+
+use PoP\GraphQLParser\Spec\Parser\Ast\AstInterface;
+use PoP\GraphQLParser\Spec\Parser\Location;
+
+class RuntimeLocation extends Location
+{
+    public function __construct(
+        int $line,
+        int $column,
+        protected AstInterface $astNode,
+    ) {
+        parent::__construct(
+            $line,
+            $column
+        );
+    }
+
+    public function getASTNode(): AstInterface
+    {
+        return $this->astNode;
+    }
+}
