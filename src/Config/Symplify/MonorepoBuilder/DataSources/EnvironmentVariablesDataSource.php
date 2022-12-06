1b169f0fdde4f09dcb9157d290e17d48e1747ef7
Leonardo Losoviz <leo@getpop.org>
2022-12-06 18:34:00 +0800

    InstaWP sleeping time: 180s

--- src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
@@ -18,7 +18,7 @@ class EnvironmentVariablesDataSource
         return [
             self::GENERATE_ARTIFACT_WITH_DOWNGRADED_CODE => false,
             self::RETENTION_DAYS_FOR_GENERATED_PLUGINS => 30,
-            self::INSTAWP_INSTANCE_SLEEPING_TIME => 120,
+            self::INSTAWP_INSTANCE_SLEEPING_TIME => 180,
         ];
     }
 }
