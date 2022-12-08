61426fe68be78dd1feeadc901135b96a4e5c8093
Leonardo Losoviz <leo@getpop.org>
2022-12-08 13:09:02 +0800

    DowngradingPhpDumper

---
+++ layers/Engine/packages/root/src/Container/Dumper/DowngradingPhpDumper.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Root\Container\Dumper;
+
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+
+class DowngradingPhpDumper extends PhpDumper
+{
+}
