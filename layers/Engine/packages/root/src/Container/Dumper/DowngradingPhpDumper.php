fd07479e4d9420ff2849d59d2413394b31cfae59
Leonardo Losoviz <leo@getpop.org>
2022-12-08 13:11:21 +0800

    public function dump(

--- layers/Engine/packages/root/src/Container/Dumper/DowngradingPhpDumper.php
+++ layers/Engine/packages/root/src/Container/Dumper/DowngradingPhpDumper.php
@@ -5,7 +5,34 @@ declare(strict_types=1);
 namespace PoP\Root\Container\Dumper;
 
 use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Exception\EnvParameterException;
 
+/**
+ * Starting from Symfony v2.6, PhpDumper generates code
+ * that contains `??=`, so it's compatible with PHP 7.4+.
+ *
+ * Replace the code to make it compatible with PHP 7.1+
+ */
 class DowngradingPhpDumper extends PhpDumper
 {
+    /**
+     * Dumps the service container as a PHP class.
+     *
+     * Available options:
+     *
+     *  * class:      The class name
+     *  * base_class: The base class name
+     *  * namespace:  The class namespace
+     *  * as_files:   To split the container in several files
+     *
+     * @return string|array A PHP class representing the service container or an array of PHP files if the "as_files" option is set
+     *
+     * @throws EnvParameterException When an env var exists but has not been dumped
+     */
+    public function dump(array $options = []): string|array
+    {
+        $dump = parent::dump($options);
+
+        return $dump;
+    }
 }
