a45c5213d4c4f3f66ccde9d10eeec4311ee59f1f
Leonardo Losoviz <leo@getpop.org>
2022-12-09 15:52:57 +0800

    Pass "any" instead of list of all post types

--- layers/WPSchema/packages/comments/src/Overrides/RelationalTypeDataLoaders/ObjectType/CommentTypeDataLoader.php
+++ layers/WPSchema/packages/comments/src/Overrides/RelationalTypeDataLoaders/ObjectType/CommentTypeDataLoader.php
@@ -5,20 +5,29 @@ declare(strict_types=1);
 namespace PoPWPSchema\Comments\Overrides\RelationalTypeDataLoaders\ObjectType;
 
 use PoPCMSSchema\Comments\RelationalTypeDataLoaders\ObjectType\CommentTypeDataLoader as UpstreamCommentTypeDataLoader;
-use PoPWPSchema\CustomPosts\Enums\CustomPostStatus;
 
 class CommentTypeDataLoader extends UpstreamCommentTypeDataLoader
 {
     /**
+     * From the WordPress docs:
+     *
+     *   > Post status or array of post statuses to retrieve affiliated comments for.
+     *   > Pass 'any' to match any value.
+     *
+     * @see https://developer.wordpress.org/reference/classes/wp_comment_query/__construct/
+     *
      * @return string[]
      */
     protected function getAllCustomPostStatuses(): array
     {
-        return array_merge(
-            parent::getAllCustomPostStatuses(),
-            [
-                CustomPostStatus::INHERIT,
-            ]
-        );
+        return [
+            'any',
+        ];
+        // return array_merge(
+        //     parent::getAllCustomPostStatuses(),
+        //     [
+        //         CustomPostStatus::INHERIT,
+        //     ]
+        // );
     }
 }
