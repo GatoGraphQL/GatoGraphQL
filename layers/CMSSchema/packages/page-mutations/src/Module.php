303bd5969e25648cb345d8b692a60c8fcb72fc05
Leonardo Losoviz <leo@getpop.org>
2022-12-09 14:27:32 +0800

    fixed services

--- layers/CMSSchema/packages/page-mutations/src/Module.php
+++ layers/CMSSchema/packages/page-mutations/src/Module.php
@@ -6,10 +6,8 @@ namespace PoPCMSSchema\PageMutations;
 
 use PoP\Root\Module\ModuleInterface;
 use PoP\Root\App;
-use PoPAPI\API\Module as APIModule;
 use PoP\Root\Module\AbstractModule;
 use PoPCMSSchema\CommentMutations\Module as CommentMutationsModule;
-use PoPCMSSchema\Users\Module as UsersModule;
 
 class Module extends AbstractModule
 {
@@ -30,9 +28,7 @@ class Module extends AbstractModule
     public function getDependedConditionalModuleClasses(): array
     {
         return [
-            \PoPAPI\API\Module::class,
             \PoPCMSSchema\CommentMutations\Module::class,
-            \PoPCMSSchema\Users\Module::class,
         ];
     }
 
@@ -45,18 +41,6 @@ class Module extends AbstractModule
         bool $skipSchema,
         array $skipSchemaModuleClasses,
     ): void {
-        $this->initServices(dirname(__DIR__));
-        $this->initSchemaServices(dirname(__DIR__), $skipSchema);
-        if (class_exists(APIModule::class) && App::getModule(APIModule::class)->isEnabled()) {
-            $this->initServices(dirname(__DIR__), '/ConditionalOnModule/API');
-        }
-        if (class_exists(UsersModule::class) && App::getModule(UsersModule::class)->isEnabled()) {
-            $this->initSchemaServices(
-                dirname(__DIR__),
-                $skipSchema || in_array(UsersModule::class, $skipSchemaModuleClasses),
-                '/ConditionalOnModule/Users'
-            );
-        }
         if (class_exists(CommentMutationsModule::class) && App::getModule(CommentMutationsModule::class)->isEnabled()) {
             $this->initSchemaServices(
                 dirname(__DIR__),
