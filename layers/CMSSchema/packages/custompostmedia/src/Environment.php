3832fc6491197a7ae29efaf4545f2b4c46117833
Leonardo Losoviz <leo@getpop.org>
2022-12-12 08:59:52 +0800

    Abstracted class, impl for Generic

---
+++ layers/CMSSchema/packages/custompostmedia/src/Environment.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\CustomPostMedia;
+
+class Environment
+{
+    public final const ENABLE_FEATURED_IMAGE_FOR_GENERIC_CUSTOMPOSTS = 'ENABLE_FEATURED_IMAGE_FOR_GENERIC_CUSTOMPOSTS';
+}
