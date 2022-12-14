86b3c3601f1c5c40e0c6aa0c7f3ca508cc790fb4
Leonardo Losoviz <leo@getpop.org>
2022-12-14 16:27:05 +0800

    PHPStan

--- layers/CMSSchema/packages/taxonomies-wp/src/TypeAPIs/TaxonomyTermTypeAPI.php
+++ layers/CMSSchema/packages/taxonomies-wp/src/TypeAPIs/TaxonomyTermTypeAPI.php
@@ -17,6 +17,7 @@ class TaxonomyTermTypeAPI implements TaxonomyTermTypeAPIInterface
      */
     public function getTermTaxonomyName(object $taxonomyTerm): string
     {
+        /** @var WP_Term */
         return $taxonomyTerm->taxonomy;
     }
 }
