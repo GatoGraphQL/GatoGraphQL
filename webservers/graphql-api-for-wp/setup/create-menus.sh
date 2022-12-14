00f072879f95c01fcf9e08468c7216b99c3027ae
Leonardo Losoviz <leo@getpop.org>
2022-12-14 18:05:05 +0800

    added extra levels to menus for testing

--- webservers/graphql-api-for-wp/setup/create-menus.sh
+++ webservers/graphql-api-for-wp/setup/create-menus.sh
@@ -11,6 +11,7 @@ wp menu create "Bottom Menu" --path=/app/wordpress
 wp menu item add-post bottom-menu 2 --path=/app/wordpress
 wp menu item add-custom bottom-menu Google https://google.com --path=/app/wordpress
 wp menu item add-custom bottom-menu Amazon https://amazon.com --parent-id=$(wp menu item add-post bottom-menu 1 --title="Ancestor post in Menu for testing" --porcelain --path=/app/wordpress) --path=/app/wordpress
+wp menu item add-custom bottom-menu BBC https://bbc.com --parent-id=$(wp menu item add-post bottom-menu 1 --title="Parent post in Menu for testing" --parent-id=$(wp menu item add-post bottom-menu 1 --title="Most ancestor post in Menu for testing" --porcelain --path=/app/wordpress) --porcelain --path=/app/wordpress) --path=/app/wordpress
 wp menu location assign bottom-menu footer --path=/app/wordpress
 
 wp menu create "Unassigned Menu" --path=/app/wordpress
