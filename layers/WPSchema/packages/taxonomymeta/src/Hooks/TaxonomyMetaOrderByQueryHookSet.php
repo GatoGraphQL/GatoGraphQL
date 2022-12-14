e83aae72e6ec59245f3ef3a3e783c1a63d3b8c08
Leonardo Losoviz <leo@getpop.org>
2022-12-14 14:48:31 +0800

    AbstractTaxonomyTypeAPI::

--- layers/WPSchema/packages/taxonomymeta/src/Hooks/TaxonomyMetaOrderByQueryHookSet.php
+++ layers/WPSchema/packages/taxonomymeta/src/Hooks/TaxonomyMetaOrderByQueryHookSet.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace PoPWPSchema\TaxonomyMeta\Hooks;
 
-use PoPCMSSchema\TaxonomiesWP\TypeAPIs\TaxonomyTermTypeAPI;
+use PoPCMSSchema\TaxonomiesWP\TypeAPIs\AbstractTaxonomyTypeAPI;
 use PoPWPSchema\Meta\Hooks\AbstractMetaOrderByQueryHookSet;
 
 class TaxonomyMetaOrderByQueryHookSet extends AbstractMetaOrderByQueryHookSet
 {
     protected function getHookName(): string
     {
-        return TaxonomyTermTypeAPI::HOOK_ORDERBY_QUERY_ARG_VALUE;
+        return AbstractTaxonomyTypeAPI::HOOK_ORDERBY_QUERY_ARG_VALUE;
     }
 }
