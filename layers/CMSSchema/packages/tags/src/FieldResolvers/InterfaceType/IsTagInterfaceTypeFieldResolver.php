c5743aaa1a4c1b0ca90271517230f62ac5f66759
Leonardo Losoviz <leo@getpop.org>
2022-12-19 16:32:38 +0800

    AbstractIsTaxonomyInterfaceTypeFieldResolver

--- layers/CMSSchema/packages/tags/src/FieldResolvers/InterfaceType/IsTagInterfaceTypeFieldResolver.php
+++ layers/CMSSchema/packages/tags/src/FieldResolvers/InterfaceType/IsTagInterfaceTypeFieldResolver.php
@@ -4,48 +4,12 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\Tags\FieldResolvers\InterfaceType;
 
-use PoP\ComponentModel\TypeResolvers\InterfaceType\InterfaceTypeResolverInterface;
-use PoP\ComponentModel\FieldResolvers\InterfaceType\AbstractQueryableSchemaInterfaceTypeFieldResolver;
-use PoP\ComponentModel\Schema\SchemaTypeModifiers;
-use PoP\ComponentModel\TypeResolvers\ConcreteTypeResolverInterface;
-use PoP\ComponentModel\TypeResolvers\ScalarType\IntScalarTypeResolver;
-use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
 use PoPCMSSchema\Tags\TypeResolvers\InterfaceType\IsTagInterfaceTypeResolver;
-use PoPCMSSchema\QueriedObject\FieldResolvers\InterfaceType\QueryableInterfaceTypeFieldResolver;
+use PoPCMSSchema\Taxonomies\FieldResolvers\InterfaceType\AbstractIsTaxonomyInterfaceTypeFieldResolver;
+use PoP\ComponentModel\TypeResolvers\InterfaceType\InterfaceTypeResolverInterface;
 
-class IsTagInterfaceTypeFieldResolver extends AbstractQueryableSchemaInterfaceTypeFieldResolver
+class IsTagInterfaceTypeFieldResolver extends AbstractIsTaxonomyInterfaceTypeFieldResolver
 {
-    private ?IntScalarTypeResolver $intScalarTypeResolver = null;
-    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
-
-    final public function setIntScalarTypeResolver(IntScalarTypeResolver $intScalarTypeResolver): void
-    {
-        $this->intScalarTypeResolver = $intScalarTypeResolver;
-    }
-    final protected function getIntScalarTypeResolver(): IntScalarTypeResolver
-    {
-        /** @var IntScalarTypeResolver */
-        return $this->intScalarTypeResolver ??= $this->instanceManager->getInstance(IntScalarTypeResolver::class);
-    }
-    final public function setStringScalarTypeResolver(StringScalarTypeResolver $stringScalarTypeResolver): void
-    {
-        $this->stringScalarTypeResolver = $stringScalarTypeResolver;
-    }
-    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
-    {
-        /** @var StringScalarTypeResolver */
-        return $this->stringScalarTypeResolver ??= $this->instanceManager->getInstance(StringScalarTypeResolver::class);
-    }
-    final public function setQueryableInterfaceTypeFieldResolver(QueryableInterfaceTypeFieldResolver $queryableInterfaceTypeFieldResolver): void
-    {
-        $this->queryableInterfaceTypeFieldResolver = $queryableInterfaceTypeFieldResolver;
-    }
-    final protected function getQueryableInterfaceTypeFieldResolver(): QueryableInterfaceTypeFieldResolver
-    {
-        /** @var QueryableInterfaceTypeFieldResolver */
-        return $this->queryableInterfaceTypeFieldResolver ??= $this->instanceManager->getInstance(QueryableInterfaceTypeFieldResolver::class);
-    }
-
     /**
      * @return array<class-string<InterfaceTypeResolverInterface>>
      */
@@ -56,45 +20,12 @@ class IsTagInterfaceTypeFieldResolver extends AbstractQueryableSchemaInterfaceTy
         ];
     }
 
-    /**
-     * @return string[]
-     */
-    public function getFieldNamesToImplement(): array
-    {
-        return [
-            'name',
-            'count',
-            'description',
-        ];
-    }
-
-    public function getFieldTypeResolver(string $fieldName): ConcreteTypeResolverInterface
-    {
-        return match ($fieldName) {
-            'name' => $this->getStringScalarTypeResolver(),
-            'description' => $this->getStringScalarTypeResolver(),
-            'count' => $this->getIntScalarTypeResolver(),
-            default => parent::getFieldTypeResolver($fieldName),
-        };
-    }
-
-    public function getFieldTypeModifiers(string $fieldName): int
-    {
-        return match ($fieldName) {
-            'name',
-            'count'
-                => SchemaTypeModifiers::NON_NULLABLE,
-            default
-                => parent::getFieldTypeModifiers($fieldName),
-        };
-    }
-
     public function getFieldDescription(string $fieldName): ?string
     {
         return match ($fieldName) {
-            'name' => $this->__('Tag', 'pop-tags'),
-            'description' => $this->__('Tag description', 'pop-tags'),
-            'count' => $this->__('Number of custom posts containing this tag', 'pop-tags'),
+            'name' => $this->__('Tag', 'tags'),
+            'description' => $this->__('Tag description', 'tags'),
+            'count' => $this->__('Number of custom posts containing this tag', 'tags'),
             default => parent::getFieldDescription($fieldName),
         };
     }
---
+++ layers/CMSSchema/packages/taxonomies/src/FieldResolvers/InterfaceType/AbstractIsTaxonomyInterfaceTypeFieldResolver.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\Taxonomies\FieldResolvers\InterfaceType;
+
+use PoP\ComponentModel\FieldResolvers\InterfaceType\AbstractQueryableSchemaInterfaceTypeFieldResolver;
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\TypeResolvers\ConcreteTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ScalarType\IntScalarTypeResolver;
+use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
+
+abstract class AbstractIsTaxonomyInterfaceTypeFieldResolver extends AbstractQueryableSchemaInterfaceTypeFieldResolver
+{
+    private ?IntScalarTypeResolver $intScalarTypeResolver = null;
+    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
+
+    final public function setIntScalarTypeResolver(IntScalarTypeResolver $intScalarTypeResolver): void
+    {
+        $this->intScalarTypeResolver = $intScalarTypeResolver;
+    }
+    final protected function getIntScalarTypeResolver(): IntScalarTypeResolver
+    {
+        /** @var IntScalarTypeResolver */
+        return $this->intScalarTypeResolver ??= $this->instanceManager->getInstance(IntScalarTypeResolver::class);
+    }
+    final public function setStringScalarTypeResolver(StringScalarTypeResolver $stringScalarTypeResolver): void
+    {
+        $this->stringScalarTypeResolver = $stringScalarTypeResolver;
+    }
+    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
+    {
+        /** @var StringScalarTypeResolver */
+        return $this->stringScalarTypeResolver ??= $this->instanceManager->getInstance(StringScalarTypeResolver::class);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getFieldNamesToImplement(): array
+    {
+        return [
+            'name',
+            'count',
+            'description',
+        ];
+    }
+
+    public function getFieldTypeResolver(string $fieldName): ConcreteTypeResolverInterface
+    {
+        return match ($fieldName) {
+            'name' => $this->getStringScalarTypeResolver(),
+            'description' => $this->getStringScalarTypeResolver(),
+            'count' => $this->getIntScalarTypeResolver(),
+            default => parent::getFieldTypeResolver($fieldName),
+        };
+    }
+
+    public function getFieldTypeModifiers(string $fieldName): int
+    {
+        return match ($fieldName) {
+            'name',
+            'count'
+                => SchemaTypeModifiers::NON_NULLABLE,
+            default
+                => parent::getFieldTypeModifiers($fieldName),
+        };
+    }
+
+    public function getFieldDescription(string $fieldName): ?string
+    {
+        return match ($fieldName) {
+            'name' => $this->__('Taxonomy', 'taxonomies'),
+            'description' => $this->__('Taxonomy description', 'taxonomies'),
+            'count' => $this->__('Number of custom posts containing this taxonomy', 'taxonomies'),
+            default => parent::getFieldDescription($fieldName),
+        };
+    }
+}
