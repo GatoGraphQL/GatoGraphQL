dc7a60b6bac23193ae42503b1156104ad58bcee5
Leonardo Losoviz <leo@getpop.org>
2022-12-19 14:56:17 +0800

    QUERYABLE_CATEGORY_TAXONOMIES

--- layers/CMSSchema/packages/tags/src/Environment.php
+++ layers/CMSSchema/packages/tags/src/Environment.php
@@ -8,4 +8,5 @@ class Environment
 {
     public final const TAG_LIST_DEFAULT_LIMIT = 'TAG_LIST_DEFAULT_LIMIT';
     public final const TAG_LIST_MAX_LIMIT = 'TAG_LIST_MAX_LIMIT';
+    public final const QUERYABLE_TAG_TAXONOMIES = 'QUERYABLE_TAG_TAXONOMIES';
 }
