c889b07f7fba899917bcf3c5513f498900446d3e
Leonardo Losoviz <leo@getpop.org>
2022-12-20 14:02:18 +0800

    removed duplicate fn

--- layers/Legacy/Schema/packages/taxonomies/src/TypeAPIs/TaxonomyTypeAPIInterface.php
+++ layers/Legacy/Schema/packages/taxonomies/src/TypeAPIs/TaxonomyTypeAPIInterface.php
@@ -12,7 +12,6 @@ interface TaxonomyTermTypeAPIInterface
     public function getCustomPostTypeTaxonomies($post_type);
     public function isTaxonomyHierarchical($taxonomy);
     public function getTaxonomyTerms($taxonomy, $options = []);
-    public function getCustomPostTaxonomyTerms($post_id, $taxonomy, $options = []);
     public function setPostTerms($post_id, $tags, $taxonomy, $append = false);
     public function hasTerm($term_id, $taxonomy, $post_id);
     public function getTermLink($term_id);
