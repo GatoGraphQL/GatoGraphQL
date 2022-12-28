905603663e63edd6f368a91917eb0a0ca462adbb
Leonardo Losoviz <leo@getpop.org>
2022-12-28 16:33:42 +0800

    Added 'categoryTaxonomy' filter

--- layers/CMSSchema/packages/categories/src/ConditionalOnModule/Users/SchemaHooks/UserCustomPostsAddCategoryFilterInputObjectTypeHookSet.php
+++ layers/CMSSchema/packages/categories/src/ConditionalOnModule/Users/SchemaHooks/UserCustomPostsAddCategoryFilterInputObjectTypeHookSet.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\Categories\ConditionalOnModule\Users\SchemaHooks;
 
-use PoPCMSSchema\Categories\SchemaHooks\AbstractAddCategoryFilterInputObjectTypeHookSet;
+use PoPCMSSchema\Categories\SchemaHooks\AbstractCustomPostAddCategoryFilterInputObjectTypeHookSet;
 use PoPCMSSchema\Users\ConditionalOnModule\CustomPosts\TypeResolvers\InputObjectType\UserCustomPostsFilterInputObjectTypeResolver;
 
-class UserCustomPostsAddCategoryFilterInputObjectTypeHookSet extends AbstractAddCategoryFilterInputObjectTypeHookSet
+class UserCustomPostsAddCategoryFilterInputObjectTypeHookSet extends AbstractCustomPostAddCategoryFilterInputObjectTypeHookSet
 {
     protected function getInputObjectTypeResolverClass(): string
     {
