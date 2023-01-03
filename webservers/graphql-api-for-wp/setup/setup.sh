cbb09667e450bcd305e0137c9e58774ca3de2223
Leonardo Losoviz <leo@getpop.org>
2023-01-03 11:48:50 +0800

    Add post_formats via graphql-api-data.xml

--- webservers/graphql-api-for-wp/setup/setup.sh
+++ webservers/graphql-api-for-wp/setup/setup.sh
@@ -17,4 +17,4 @@ echo "Installing WordPress..."
 /bin/sh /app/setup/configure-plugin-settings.sh
 /bin/sh /app/setup/import-data.sh
 /bin/sh /app/setup/create-menus.sh
-/bin/sh /app/setup/create-taxonomy-terms.sh
+# /bin/sh /app/setup/create-taxonomy-terms.sh
