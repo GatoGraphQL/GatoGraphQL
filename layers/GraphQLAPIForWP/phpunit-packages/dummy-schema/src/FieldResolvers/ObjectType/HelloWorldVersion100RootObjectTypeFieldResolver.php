d2a02e04ba1f346ad04edd799dbf16bc672a1ebc
Leonardo Losoviz <leo@getpop.org>
2023-01-06 15:07:35 +0800

    Added dummy field "helloWorld"

---
+++ layers/GraphQLAPIForWP/phpunit-packages/dummy-schema/src/FieldResolvers/ObjectType/HelloWorldVersion100RootObjectTypeFieldResolver.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\DummySchema\FieldResolvers\ObjectType;
+
+use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
+
+class HelloWorldVersion100RootObjectTypeFieldResolver extends AbstractHelloWorldRootObjectTypeFieldResolver
+{
+    public function getFieldVersion(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
+    {
+        return '1.0.0';
+    }
+
+    public function getPriorityToAttachToClasses(): int
+    {
+        return 20;
+    }
+}
