0b31fd7a4074a397fa5d7da7882aeb7e4fc05452
Leonardo Losoviz <leo@getpop.org>
2023-01-09 20:12:21 +0800

    Depend on Engine

--- layers/GraphQLAPIForWP/phpunit-packages/dummy-schema/src/Module.php
+++ layers/GraphQLAPIForWP/phpunit-packages/dummy-schema/src/Module.php
@@ -15,7 +15,7 @@ class Module extends AbstractModule
     public function getDependedModuleClasses(): array
     {
         return [
-            \PoPCMSSchema\SchemaCommons\Module::class,
+            \PoP\Engine\Module::class,
         ];
     }
 
