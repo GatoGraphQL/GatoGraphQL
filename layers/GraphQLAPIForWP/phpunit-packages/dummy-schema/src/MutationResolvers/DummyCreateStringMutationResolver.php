0198f635a17e3fa3447427055858cb4a394de286
Leonardo Losoviz <leo@getpop.org>
2023-01-09 19:42:32 +0800

    Added dummy mutation

---
+++ layers/GraphQLAPIForWP/phpunit-packages/dummy-schema/src/MutationResolvers/DummyCreateStringMutationResolver.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\DummySchema\MutationResolvers;
+
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
+use PoP\ComponentModel\MutationResolvers\AbstractMutationResolver;
+use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
+
+class DummyCreateStringMutationResolver extends AbstractMutationResolver
+{
+    protected int $counter = 0;
+
+    public function executeMutation(
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): mixed {
+        $this->counter++;
+        return sprintf(
+            $this->__('Hello world #%s', 'dummy-schema'),
+            $this->counter
+        );
+    }
+}
