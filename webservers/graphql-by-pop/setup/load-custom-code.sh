842a88cdc8dd86764e384f6d208f1d04c39bde80
Leonardo Losoviz <leo@getpop.org>
2023-01-11 17:51:50 +0800

    renamed initialize-pop-components.php

--- webservers/graphql-by-pop/setup/load-custom-code.sh
+++ webservers/graphql-by-pop/setup/load-custom-code.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed "s#require_once ABSPATH . 'wp-settings.php';#require_once(__DIR__ . '/../vendor/autoload.php'); require_once(__DIR__ . '/../code-src/initialize-pop-components.php'); require_once  ABSPATH  .  'wp-settings.php' ;#g" /app/wordpress/wp-config.php > /app/wp-config.php.tmp
+sed "s#require_once ABSPATH . 'wp-settings.php';#require_once(__DIR__ . '/../vendor/autoload.php'); require_once(__DIR__ . '/../code-src/initialize-pop-modules.php'); require_once  ABSPATH  .  'wp-settings.php' ;#g" /app/wordpress/wp-config.php > /app/wp-config.php.tmp
 mv /app/wp-config.php.tmp /app/wordpress/wp-config.php
