6847af1860c89e408a3373787e18a78ced4c1d48
Leonardo Losoviz <leo@getpop.org>
2023-01-25 16:49:54 +0800

    Canonical: use single source of truth

--- layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/CanonicalNamespacingModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/CanonicalNamespacingModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
@@ -16,4 +16,18 @@ class CanonicalNamespacingModifyPluginSettingsFixtureEndpointWebserverRequestTes
     {
         return __DIR__ . '/fixture-canonical-namespacing';
     }
+
+    /**
+     * Canonical types must always be the same, then use
+     * the contents of one response.
+     *
+     * @param array<string,mixed> $providerItems
+     * @return array<string,mixed>
+     */
+    protected function customizeProviderEndpointEntries(array $providerItems): array
+    {
+        $providerItems = parent::customizeProviderEndpointEntries($providerItems);
+        $providerItems['introspection-types:0'] = $providerItems['introspection-types'];
+        return $providerItems;
+    }
 }
