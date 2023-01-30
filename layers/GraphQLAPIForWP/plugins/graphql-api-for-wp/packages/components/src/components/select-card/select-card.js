342ad3ff417e3074fa6d2b96877d8a6cc4c00851
Leonardo Losoviz <leo@getpop.org>
2023-01-30 14:08:36 +0800

    Use WithCard

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/packages/components/src/components/select-card/select-card.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/packages/components/src/components/select-card/select-card.js
@@ -2,50 +2,13 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Card, CardHeader, CardBody } from '@wordpress/components';
-import { getEditableOnFocusComponentClass } from '../base-styles'
-import { InfoTooltip } from '../info-tooltip';
-import { MarkdownInfoModalButton } from '../markdown-modal';
+import { withCard } from '../card';
 import { GraphQLAPISelect } from '../select';
 
-const SelectCard = ( props ) => {
-	const {
-		label,
-		isSelected,
-		tooltip,
-		getMarkdownContentCallback,
-	} = props;
-	const componentClassName = 'graphql-api-select-card';
-	const componentClass = `${ componentClassName } ${ getEditableOnFocusComponentClass(isSelected) }`;
-	const documentationTitle = __(`Documentation for: "${ label }"`, 'graphql-api');
-	return (
-		<div className={ componentClass }>
-			<Card { ...props }>
-				<CardHeader isShady>
-					<div>
-						{ label }
-						{ !! tooltip &&
-							<InfoTooltip
-								{ ...props }
-								text={ tooltip }
-							/>
-						}
-						{ !! getMarkdownContentCallback && (
-							<MarkdownInfoModalButton
-								title={ documentationTitle }
-								getMarkdownContentCallback={ getMarkdownContentCallback }
-							/>
-						) }
-					</div>
-				</CardHeader>
-				<CardBody>
-					<GraphQLAPISelect
-						{ ...props }
-					/>
-				</CardBody>
-			</Card>
-		</div>
-	);
-}
-
-export default SelectCard;
+export default compose( [
+	withState( {
+		className: 'graphql-api-select-card',
+	} ),
+	withEditableOnFocus(),
+	withCard(),
+] )( GraphQLAPISelect );
