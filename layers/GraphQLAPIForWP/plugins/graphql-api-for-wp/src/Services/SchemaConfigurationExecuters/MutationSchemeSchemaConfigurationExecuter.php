554adbc6c9c067cb3faf9872f177b824ec95816a
Leonardo Losoviz <leo@getpop.org>
2023-01-31 14:16:15 +0800

    early return

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/SchemaConfigurationExecuters/MutationSchemeSchemaConfigurationExecuter.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/SchemaConfigurationExecuters/MutationSchemeSchemaConfigurationExecuter.php
@@ -36,48 +36,49 @@ class MutationSchemeSchemaConfigurationExecuter extends AbstractSchemaConfigurat
     public function executeSchemaConfiguration(int $schemaConfigurationID): void
     {
         $schemaConfigBlockDataItem = $this->getSchemaConfigBlockDataItem($schemaConfigurationID);
-        if ($schemaConfigBlockDataItem !== null) {
-            /**
-             * Default value (if not defined in DB): `default`. Then do nothing
-             */
-            $mutationScheme = $schemaConfigBlockDataItem['attrs'][SchemaConfigMutationSchemeBlock::ATTRIBUTE_NAME_MUTATION_SCHEME] ?? null;
-            /**
-             * Only execute if it has any selectable value (no null, no invented).
-             * If "default", then the general settings will already take effect,
-             * so do nothing.
-             */
-            if (
-                !in_array($mutationScheme, [
-                    MutationSchemes::STANDARD,
-                    MutationSchemes::NESTED_WITH_REDUNDANT_ROOT_FIELDS,
-                    MutationSchemes::NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS,
-                ])
-            ) {
-                return;
-            }
-            /**
-             * Define the settings value through a hook.
-             * Execute last so it overrides the default settings
-             */
-            $hookName = ModuleConfigurationHelpers::getHookName(
-                GraphQLServerModule::class,
-                GraphQLServerEnvironment::ENABLE_NESTED_MUTATIONS
-            );
-            \add_filter(
-                $hookName,
-                fn () => $mutationScheme !== MutationSchemes::STANDARD,
-                PHP_INT_MAX
-            );
-            $hookName = ModuleConfigurationHelpers::getHookName(
-                EngineModule::class,
-                EngineEnvironment::DISABLE_REDUNDANT_ROOT_TYPE_MUTATION_FIELDS
-            );
-            \add_filter(
-                $hookName,
-                fn () => $mutationScheme === MutationSchemes::NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS,
-                PHP_INT_MAX
-            );
+        if ($schemaConfigBlockDataItem === null) {
+            return;
         }
+        /**
+         * Default value (if not defined in DB): `default`. Then do nothing
+         */
+        $mutationScheme = $schemaConfigBlockDataItem['attrs'][SchemaConfigMutationSchemeBlock::ATTRIBUTE_NAME_MUTATION_SCHEME] ?? null;
+        /**
+         * Only execute if it has any selectable value (no null, no invented).
+         * If "default", then the general settings will already take effect,
+         * so do nothing.
+         */
+        if (
+            !in_array($mutationScheme, [
+                MutationSchemes::STANDARD,
+                MutationSchemes::NESTED_WITH_REDUNDANT_ROOT_FIELDS,
+                MutationSchemes::NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS,
+            ])
+        ) {
+            return;
+        }
+        /**
+         * Define the settings value through a hook.
+         * Execute last so it overrides the default settings
+         */
+        $hookName = ModuleConfigurationHelpers::getHookName(
+            GraphQLServerModule::class,
+            GraphQLServerEnvironment::ENABLE_NESTED_MUTATIONS
+        );
+        \add_filter(
+            $hookName,
+            fn () => $mutationScheme !== MutationSchemes::STANDARD,
+            PHP_INT_MAX
+        );
+        $hookName = ModuleConfigurationHelpers::getHookName(
+            EngineModule::class,
+            EngineEnvironment::DISABLE_REDUNDANT_ROOT_TYPE_MUTATION_FIELDS
+        );
+        \add_filter(
+            $hookName,
+            fn () => $mutationScheme === MutationSchemes::NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS,
+            PHP_INT_MAX
+        );
     }
 
     protected function getBlock(): BlockInterface
