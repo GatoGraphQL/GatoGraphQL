4c95553b0a8e4f138a8eab9990e917f28baad84e
Leonardo Losoviz <leo@getpop.org>
2023-02-03 11:21:49 +0800

    Test for Categories

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/QueryableCategoryTaxonomiesOnSchemaConfigurationCPTBlockAttributesFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\Services\Blocks\AbstractSchemaConfigCustomizableConfigurationBlock;
+use GraphQLAPI\GraphQLAPI\Services\Blocks\SchemaConfigSchemaCategoriesBlock;
+use PHPUnitForGraphQLAPI\GraphQLAPI\Integration\AbstractModifyCPTBlockAttributesFixtureEndpointWebserverRequestTestCase;
+
+class QueryableCategoryTaxonomiesOnSchemaConfigurationCPTBlockAttributesFixtureEndpointWebserverRequestTest extends AbstractModifyCPTBlockAttributesFixtureEndpointWebserverRequestTestCase
+{
+    use QueryableCategoryTaxonomiesFixtureEndpointWebserverRequestTestTrait;
+
+    public const MOBILE_APP_SCHEMA_CONFIGURATION_ID = 193;
+
+    protected function getEndpoint(): string
+    {
+        /**
+         * This endpoint:
+         *
+         * - Has "Customize configuration, or use default from Settings?" as Default (i.e. false)
+         */
+        return 'graphql/mobile-app/';
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    protected function getCPTBlockAttributesNewValue(): array
+    {
+        return [
+            AbstractSchemaConfigCustomizableConfigurationBlock::ATTRIBUTE_NAME_CUSTOMIZE_CONFIGURATION => 'true',
+            SchemaConfigSchemaCategoriesBlock::ATTRIBUTE_NAME_INCLUDED_CATEGORY_TAXONOMIES => $this->getIncludedCategoryTaxonomiesNewValue(),
+        ];
+    }
+
+    protected function getCustomPostID(string $dataName): int
+    {
+        return self::MOBILE_APP_SCHEMA_CONFIGURATION_ID;
+    }
+
+    protected function getBlockNamespacedID(string $dataName): string
+    {
+        return 'graphql-api/schema-config-schema-categories';
+    }
+}
