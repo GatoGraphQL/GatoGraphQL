1d4e6a32fa47e8ce37b36cf53afd16fabb622d37
Leonardo Losoviz <leo@getpop.org>
2023-02-14 17:23:55 +0700

    ATTRIBUTE_NAME_ENTRIES

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-schema-custompost-meta/src/index.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-schema-custompost-meta/src/index.js
@@ -67,7 +67,7 @@ registerBlockType( 'graphql-api/schema-config-schema-custompost-meta', {
 		},
 		/**
 		 * Same attribute name as defined in
-		 * GraphQLAPI\GraphQLAPI\Services\Blocks\SchemaConfigSchemaCustomPostMetaBlock::ATTRIBUTE_NAME_META_KEYS
+		 * GraphQLAPI\GraphQLAPI\Services\Blocks\SchemaConfigSchemaCustomPostMetaBlock::ATTRIBUTE_NAME_ENTRIES
 		 */
 		entries: {
 			type: 'array',
