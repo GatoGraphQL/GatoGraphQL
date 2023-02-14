ec769e72172edac240a3a0d9fe7c460c20ec6dc6
Leonardo Losoviz <leo@getpop.org>
2023-02-14 16:53:55 +0700

    component in block

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-schema-custompost-meta/src/schema-config-schema-custompost-meta-card.js
@@ -0,0 +1,68 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { compose, withState } from '@wordpress/compose';
+import { TextareaControl } from '@wordpress/components';
+// import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { getModuleDocMarkdownContentOrUseDefault } from './module-doc-markdown-loader';
+import {
+	InfoTooltip,
+	withCard,
+	withCustomizableConfiguration,
+	withEditableOnFocus,
+} from '@graphqlapi/components';
+
+const SchemaConfigCustomPostMetaCard = ( props ) => {
+	const {
+		setAttributes,
+		attributes: {
+			metaKeys,
+			behavior,
+		},
+	} = props;
+	// const [ /*text, */setText ] = useState( '' );
+	const helpText = __('List of all the meta keys, to either allow or deny access to, when querying fields `metaValue` and `metaValues` on %s', 'graphql-api')
+		.replace('%s', __('custom posts', 'graphql-api'));
+	const attributeName = 'metaKeys';
+	return (
+		<>
+			<div>
+				<span>
+					<em>{ __('Meta keys:', 'graphql-api') }</em>
+					<InfoTooltip
+						{ ...props }
+						text={ helpText }
+					/>
+				</span>
+				<TextareaControl
+					// label={ __('Meta keys', 'graphql-api') }
+					help={ __('<strong>Heads up:</strong> Entries surrounded with ... @TODO complete!!!', 'graphql-api') }
+					// value={ text }
+					value={ metaKeys.join('\n') }
+					// onChange={ ( value ) => setText( value ) }
+					onChange={ value =>
+						setAttributes( {
+							[ attributeName ]: value.split('\n')
+						} )
+					}
+				/>
+			</div>
+		</>
+	);
+}
+
+export default compose( [
+	withEditableOnFocus(),
+	withState( {
+		header: __('Custom Post Meta', 'graphql-api'),
+		className: 'graphql-api-custompost-meta',
+		getMarkdownContentCallback: getModuleDocMarkdownContentOrUseDefault
+	} ),
+	withCard(),
+	withCustomizableConfiguration(),
+] )( SchemaConfigCustomPostMetaCard );
