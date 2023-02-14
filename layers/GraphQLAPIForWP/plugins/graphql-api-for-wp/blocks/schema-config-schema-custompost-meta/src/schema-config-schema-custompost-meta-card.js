fac294de2d21926a2108978175135a4775e63c57
Leonardo Losoviz <leo@getpop.org>
2023-02-14 18:32:47 +0700

    Added Behavior control

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-schema-custompost-meta/src/schema-config-schema-custompost-meta-card.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-schema-custompost-meta/src/schema-config-schema-custompost-meta-card.js
@@ -3,6 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import { compose, withState } from '@wordpress/compose';
+import { RadioControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -15,9 +16,15 @@ import {
 	withEditableOnFocus,
 	EditableArrayTextareaControl,
 } from '@graphqlapi/components';
+import {
+	ATTRIBUTE_VALUE_BEHAVIOR_ALLOW,
+	ATTRIBUTE_VALUE_BEHAVIOR_DENY,
+} from './behavior-meta-values';
 
 const SchemaConfigCustomPostMetaCard = ( props ) => {
 	const {
+		isSelected,
+		setAttributes,
 		attributes: {
 			entries,
 			behavior,
@@ -25,6 +32,16 @@ const SchemaConfigCustomPostMetaCard = ( props ) => {
 	} = props;
 	const helpText = __('List of all the meta keys, to either allow or deny access to, when querying fields `metaValue` and `metaValues` on %s', 'graphql-api')
 		.replace('%s', __('custom posts', 'graphql-api'));
+	const options = [
+		{
+			label: __('Allow access', 'graphql-api'),
+			value: ATTRIBUTE_VALUE_BEHAVIOR_ALLOW,
+		},
+		{
+			label: __('Deny access', 'graphql-api'),
+			value: ATTRIBUTE_VALUE_BEHAVIOR_DENY,
+		},
+	];
 	return (
 		<>
 			<div>
@@ -42,6 +59,36 @@ const SchemaConfigCustomPostMetaCard = ( props ) => {
 					help={ __('<strong>Heads up:</strong> Entries surrounded with ... @TODO complete!!!', 'graphql-api') }
 					rows='10'
 				/>
+			</div>
+			<div>
+				<em>{ __('Behavior:', 'graphql-api') }</em>
+				<InfoTooltip
+					{ ...props }
+					text={ __('Are the entries being allowed or denied?', 'graphql-api') }
+				/>
+				{ !isSelected && (
+					<>
+						<br />
+						{ behavior == ATTRIBUTE_VALUE_BEHAVIOR_ALLOW &&
+							<span>✅ { __('Allow access', 'graphql-api') }</span>
+						}
+						{ behavior == ATTRIBUTE_VALUE_BEHAVIOR_DENY &&
+							<span>❌ { __('Deny access', 'graphql-api') }</span>
+						}
+					</>
+				) }
+				{ isSelected &&
+					<RadioControl
+						{ ...props }
+						options={ options }
+						selected={ behavior }
+						onChange={ newValue => (
+							setAttributes( {
+								behavior: newValue
+							} )
+						)}
+					/>
+				}
 			</div>
 		</>
 	);
