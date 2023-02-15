f9f22adf7667683987db5cb108cabc090747e1be
Leonardo Losoviz <leo@getpop.org>
2023-02-15 13:11:38 +0700

    PHP block and executer

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/SchemaConfigSchemaSettingsBlock.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\Services\Blocks;
+
+use GraphQLAPI\GraphQLAPI\ModuleResolvers\SchemaTypeModuleResolver;
+
+class SchemaConfigSchemaSettingsBlock extends AbstractSchemaConfigSchemaAllowAccessToEntriesBlock
+{
+    use MainPluginBlockTrait;
+    use OptionsBlockTrait;
+
+    protected function getBlockName(): string
+    {
+        return 'schema-config-schema-settings';
+    }
+
+    public function getBlockPriority(): int
+    {
+        return 10065;
+    }
+
+    public function getEnablingModule(): ?string
+    {
+        return SchemaTypeModuleResolver::SCHEMA_SETTINGS;
+    }
+
+    protected function getBlockTitle(): string
+    {
+        return \__('Settings', 'graphql-api');
+    }
+
+    protected function getRenderBlockLabel(): string
+    {
+        return $this->__('Settings entries', 'graphql-api');
+    }
+
+    /**
+     * Register style-index.css
+     */
+    protected function registerCommonStyleCSS(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Add the locale language to the localized data?
+     */
+    protected function addLocalLanguage(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Default language for the script/component's documentation
+     */
+    protected function getDefaultLanguage(): ?string
+    {
+        // English
+        return 'en';
+    }
+}
