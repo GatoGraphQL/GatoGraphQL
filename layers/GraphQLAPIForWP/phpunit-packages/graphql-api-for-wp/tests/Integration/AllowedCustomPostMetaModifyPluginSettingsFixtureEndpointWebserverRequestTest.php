d7c3afa18e89179cdc68f7bfb038c582e8d42344
Leonardo Losoviz <leo@getpop.org>
2023-02-15 08:07:42 +0700

    Added test

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/AllowedCustomPostMetaModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\Constants\ModuleSettingOptions;
+
+class AllowedCustomPostMetaModifyPluginSettingsFixtureEndpointWebserverRequestTest extends AbstractModifyPluginSettingsFixtureEndpointWebserverRequestTestCase
+{
+    use AllowedCustomPostMetaFixtureEndpointWebserverRequestTestTrait;
+
+    protected function getEndpoint(): string
+    {
+        return 'graphql/';
+    }
+
+    protected function getSettingsKey(): string
+    {
+        return ModuleSettingOptions::ENTRIES;
+    }
+
+    protected function getModuleID(string $dataName): string
+    {
+        return 'graphqlapi_graphqlapi_schema-custompost-meta';
+    }
+
+    protected function getPluginSettingsNewValue(): mixed
+    {
+        return $this->getAllowedCustomPostMetaKeyEntriesNewValue();
+    }
+}
---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/AllowedCustomPostMetaOnSchemaConfigurationCPTBlockAttributesFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+use GraphQLAPI\GraphQLAPI\Services\Blocks\AbstractSchemaConfigCustomizableConfigurationBlock;
+use GraphQLAPI\GraphQLAPI\Services\Blocks\SchemaConfigSchemaCustomPostMetaBlock;
+use PHPUnitForGraphQLAPI\GraphQLAPI\Integration\AbstractModifyCPTBlockAttributesFixtureEndpointWebserverRequestTestCase;
+
+class AllowedCustomPostMetaOnSchemaConfigurationCPTBlockAttributesFixtureEndpointWebserverRequestTest extends AbstractModifyCPTBlockAttributesFixtureEndpointWebserverRequestTestCase
+{
+    use AllowedCustomPostMetaFixtureEndpointWebserverRequestTestTrait;
+
+    public const MOBILE_APP_SCHEMA_CONFIGURATION_ID = 193;
+
+    protected function getEndpoint(): string
+    {
+        /**
+         * This endpoint:
+         *
+         * - Has "Customize configuration? (Or use default from Settings?)" as Default (i.e. false)
+         */
+        return 'graphql/mobile-app/';
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    protected function getCPTBlockAttributesNewValue(): array
+    {
+        return [
+            AbstractSchemaConfigCustomizableConfigurationBlock::ATTRIBUTE_NAME_CUSTOMIZE_CONFIGURATION => 'true',
+            SchemaConfigSchemaCustomPostMetaBlock::ATTRIBUTE_NAME_ENTRIES => $this->getAllowedCustomPostMetaKeyEntriesNewValue(),
+        ];
+    }
+
+    protected function getCustomPostID(string $dataName): int
+    {
+        return self::MOBILE_APP_SCHEMA_CONFIGURATION_ID;
+    }
+
+    protected function getBlockNamespacedID(string $dataName): string
+    {
+        return 'graphql-api/schema-config-schema-custompost-meta';
+    }
+}
