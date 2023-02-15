fa7d31dfb06079ef6402457bdad478c46a01f51a
Leonardo Losoviz <leo@getpop.org>
2023-02-15 10:29:09 +0700

    format

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/packages/components/src/components/schema-configuration-meta-card/schema-configuration-meta-card.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/packages/components/src/components/schema-configuration-meta-card/schema-configuration-meta-card.js
@@ -17,12 +17,11 @@ const SchemaConfigMetaCard = ( props ) => {
 	const metaKeyDesc = __('List of all the meta keys, to either allow or deny access to, when querying fields `metaValue` and `metaValues` on %s (one entry per line).', 'graphql-api')
 		.replace('%s', labelEntity);
 	const headsUpDesc = __('Entries surrounded with "/" or "#" are evaluated as regex (regular expressions).', 'graphql-api');
-	const examples = labelExampleEntries.join('", "');
 	const entryDesc = __('For example, meta key "%1$s" is matched by any of the following entries: %2$s.', 'graphql-api')
 		.replace('%1$s', labelExampleMetaKey)
 		.replace(
 			'%2$s',
-			`"${ examples }"`
+			`"${ labelExampleEntries.join('", "') }"`
 		);
 	const entriesLabel = `${ metaKeyDesc } ${ headsUpDesc } ${ entryDesc }`;
 	return (
