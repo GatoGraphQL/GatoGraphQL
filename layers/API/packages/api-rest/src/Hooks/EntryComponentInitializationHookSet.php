3169cccaa518287a74eb5bcd7cc8e8b52cd5271e
Leonardo Losoviz <leo@getpop.org>
2023-02-21 18:52:54 +0800

    ImpossibleToHappenException

--- layers/API/packages/api-rest/src/Hooks/EntryComponentInitializationHookSet.php
+++ layers/API/packages/api-rest/src/Hooks/EntryComponentInitializationHookSet.php
@@ -9,11 +9,13 @@ use PoPAPI\RESTAPI\DataStructureFormatters\RESTDataStructureFormatter;
 use PoP\ComponentModel\Component\Component;
 use PoP\ComponentModel\Engine\Engine;
 use PoP\Root\App;
+use PoP\Root\Exception\ImpossibleToHappenException;
 use PoP\Root\Hooks\AbstractHookSet;
 
 class EntryComponentInitializationHookSet extends AbstractHookSet
 {
     private ?RESTDataStructureFormatter $restDataStructureFormatter = null;
+    private ?ApplicationStateFillerServiceInterface $applicationStateFillerService = null;
 
     final public function setRESTDataStructureFormatter(RESTDataStructureFormatter $restDataStructureFormatter): void
     {
@@ -23,6 +25,15 @@ class EntryComponentInitializationHookSet extends AbstractHookSet
     {
         /** @var RESTDataStructureFormatter */
         return $this->restDataStructureFormatter ??= $this->instanceManager->getInstance(RESTDataStructureFormatter::class);
+    }
+    final public function setApplicationStateFillerService(ApplicationStateFillerServiceInterface $applicationStateFillerService): void
+    {
+        $this->applicationStateFillerService = $applicationStateFillerService;
+    }
+    final protected function getApplicationStateFillerService(): ApplicationStateFillerServiceInterface
+    {
+        /** @var ApplicationStateFillerServiceInterface */
+        return $this->applicationStateFillerService ??= $this->instanceManager->getInstance(ApplicationStateFillerServiceInterface::class);
     }
 
     protected function init(): void
@@ -39,6 +50,18 @@ class EntryComponentInitializationHookSet extends AbstractHookSet
             || App::getState('datastructure') !== $this->getRestDataStructureFormatter()->getName()
         ) {
             return;
+        }
+
+        /**
+         * If no query was requested, and the entry component defines
+         * a query (eg: REST), then parse it and set as the app query
+         * to resolve
+         */
+        $executableDocument = App::getState('executable-document-ast');
+        if ($executableDocument !== null || !isset($entryComponent->atts['query'])) {
+            throw new ImpossibleToHappenException(
+                $this->__('When requesting a REST resource, the GraphQL AST must be built from the entry component\'s "query" string', 'component-model')
+            );
         }
     }
 }
