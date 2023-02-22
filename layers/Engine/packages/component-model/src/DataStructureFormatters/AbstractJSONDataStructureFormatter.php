872fb1bd833452efcb159d63ba0d59507bdaf243
Leonardo Losoviz <leo@getpop.org>
2023-02-22 10:03:51 +0800

    getDatabasesOutput

--- layers/Engine/packages/component-model/src/DataStructureFormatters/AbstractJSONDataStructureFormatter.php
+++ layers/Engine/packages/component-model/src/DataStructureFormatters/AbstractJSONDataStructureFormatter.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\DataStructureFormatters;
 
+use PoP\ComponentModel\App;
+use PoP\ComponentModel\Constants\DataOutputItems;
+use PoP\ComponentModel\Constants\DatabasesOutputModes;
+use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
+use SplObjectStorage;
+
 abstract class AbstractJSONDataStructureFormatter extends AbstractDataStructureFormatter
 {
     public function getContentType(): string
@@ -17,5 +23,74 @@ abstract class AbstractJSONDataStructureFormatter extends AbstractDataStructureF
     public function getOutputContent(array &$data): string
     {
         return (string)json_encode($data);
+    }
+
+    /**
+     * @return array<string,mixed>
+     * @param array<string,mixed> $data
+     */
+    public function getFormattedData(array $data): array
+    {
+        $dataoutputitems = App::getState('dataoutputitems');
+        if (!in_array(DataOutputItems::DATABASES, $dataoutputitems)) {
+            return parent::getFormattedData($data);
+        }
+
+        $databases = $data['databases'];
+        unset($data['databases']);
+        return [
+            ...parent::getFormattedData($data),
+            [
+                'databases' => $this->getDatabasesOutput($databases),
+            ]
+        ];
+    }
+
+    /**
+     * @return array<string,mixed>
+     * @param array<string,array<string|int,SplObjectStorage<FieldInterface,mixed>>>|array<string,array<string,array<string|int,SplObjectStorage<FieldInterface,mixed>>>> $databases
+     */
+    protected function getDatabasesOutput(array $databases): array
+    {
+        if (empty($databases)) {
+            return [];
+        }
+
+        $outputDatabase = [];
+        $dboutputmode = App::getState('dboutputmode');
+        if ($dboutputmode === DatabasesOutputModes::SPLITBYDATABASES) {
+            /**
+             * Notifications can appear under "database" and
+             * "userstatedatabase", showing different fields on each.
+             */
+            foreach ($databases as $databaseName => $database) {
+                $this->addDatabaseOutput($database, $outputDatabase);
+            }
+        } elseif ($dboutputmode === DatabasesOutputModes::COMBINED) {
+            $this->addDatabaseOutput($databases, $outputDatabase);
+        }
+        
+        return $outputDatabase;
+    }
+
+    /**
+     * The "databases" contains entries of type SplObjectStorage,
+     * so these must be handled separately.
+     *
+     * @param array<string,array<string|int,SplObjectStorage<FieldInterface,mixed>>> $database
+     * @param array<string,array<string|int,SplObjectStorage<FieldInterface,mixed>>> $outputDatabase
+     */
+    protected function addDatabaseOutput(array &$database, array &$outputDatabase): void
+    {
+        foreach ($database as $dbKey => $dbObjectIDStorage) {
+            foreach ($dbObjectIDStorage as $dbObjectID => $dbObjectStorage) {
+                /** @var FieldInterface $field */
+                foreach ($dbObjectStorage as $field) {
+                    /** @var mixed $field */
+                    $fieldValue = $dbObjectStorage[$field];
+                    $outputDatabase[$dbKey][$dbObjectID][$field->getOutputKey()] = $fieldValue;
+                }
+            }
+        }
     }
 }
