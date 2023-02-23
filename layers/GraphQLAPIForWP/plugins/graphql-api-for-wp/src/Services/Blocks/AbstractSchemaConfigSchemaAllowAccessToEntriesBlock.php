ed3c30398c94d0f284bb1471ddad7d118118f705
Leonardo Losoviz <leo@getpop.org>
2023-02-23 11:20:29 +0800

    AllowAccessToEntriesBlockTrait

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/AbstractSchemaConfigSchemaAllowAccessToEntriesBlock.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/AbstractSchemaConfigSchemaAllowAccessToEntriesBlock.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Services\Blocks;
 
-use GraphQLAPI\GraphQLAPI\Constants\BlockAttributeNames;
-use GraphQLAPI\GraphQLAPI\PluginEnvironment;
-use PoPSchema\SchemaCommons\Constants\Behaviors;
-
 abstract class AbstractSchemaConfigSchemaAllowAccessToEntriesBlock extends AbstractSchemaConfigCustomizableConfigurationBlock
 {
+    use AllowAccessToEntriesBlockTrait;
+
     /**
      * Pass localized data to the block
      *
@@ -19,17 +17,8 @@ abstract class AbstractSchemaConfigSchemaAllowAccessToEntriesBlock extends Abstr
     {
         return array_merge(
             parent::getLocalizedData(),
-            [
-                'defaultBehavior' => $this->getDefaultBehavior(),
-            ]
+            $this->getDefaultBehaviorLocalizedData()
         );
-    }
-
-    protected function getDefaultBehavior(): string
-    {
-        return PluginEnvironment::areUnsafeDefaultsEnabled()
-            ? Behaviors::DENY
-            : Behaviors::ALLOW;
     }
 
     /**
@@ -37,31 +26,6 @@ abstract class AbstractSchemaConfigSchemaAllowAccessToEntriesBlock extends Abstr
      */
     protected function doRenderBlock(array $attributes, string $content): string
     {
-        $placeholder = '<p><strong>%s</strong></p>%s';
-        $entries = $attributes[BlockAttributeNames::ENTRIES] ?? [];
-        $behavior = $attributes[BlockAttributeNames::BEHAVIOR] ?? $this->getDefaultBehavior();
-        return sprintf(
-            $placeholder,
-            $this->getRenderBlockLabel(),
-            $entries ?
-                sprintf(
-                    '<ul><li><code>%s</code></li></ul>',
-                    implode('</code></li><li><code>', $entries)
-                ) :
-                sprintf(
-                    '<p><em>%s</em></p>',
-                    \__('(not set)', 'graphql-api')
-                )
-        ) . sprintf(
-            $placeholder,
-            $this->__('Behavior', 'graphql-api'),
-            match ($behavior) {
-                Behaviors::ALLOW => sprintf('✅ %s', $this->__('Allow access', 'graphql-api')),
-                Behaviors::DENY => sprintf('❌ %s', $this->__('Deny access', 'graphql-api')),
-                default => $behavior,
-            }
-        );
+        return $this->renderAllowAccessToEntriesBlock($attributes);
     }
-
-    abstract protected function getRenderBlockLabel(): string;
 }
---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/AllowAccessToEntriesBlockTrait.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\Services\Blocks;
+
+use GraphQLAPI\GraphQLAPI\Constants\BlockAttributeNames;
+use GraphQLAPI\GraphQLAPI\PluginEnvironment;
+use PoPSchema\SchemaCommons\Constants\Behaviors;
+
+trait AllowAccessToEntriesBlockTrait
+{
+    /**
+     * Pass localized data to the block
+     *
+     * @return array<string,mixed>
+     */
+    protected function getDefaultBehaviorLocalizedData(): array
+    {
+        return [
+            'defaultBehavior' => $this->getDefaultBehavior(),
+        ];
+    }
+
+    protected function getDefaultBehavior(): string
+    {
+        return PluginEnvironment::areUnsafeDefaultsEnabled()
+            ? Behaviors::DENY
+            : Behaviors::ALLOW;
+    }
+
+    /**
+     * @param array<string,mixed> $attributes
+     */
+    protected function renderAllowAccessToEntriesBlock(array $attributes): string
+    {
+        $placeholder = '<p><strong>%s</strong></p>%s';
+        $entries = $attributes[BlockAttributeNames::ENTRIES] ?? [];
+        $behavior = $attributes[BlockAttributeNames::BEHAVIOR] ?? $this->getDefaultBehavior();
+        return sprintf(
+            $placeholder,
+            $this->getRenderBlockLabel(),
+            $entries ?
+                sprintf(
+                    '<ul><li><code>%s</code></li></ul>',
+                    implode('</code></li><li><code>', $entries)
+                ) :
+                sprintf(
+                    '<p><em>%s</em></p>',
+                    \__('(not set)', 'graphql-api')
+                )
+        ) . sprintf(
+            $placeholder,
+            $this->__('Behavior', 'graphql-api'),
+            match ($behavior) {
+                Behaviors::ALLOW => sprintf('✅ %s', $this->__('Allow access', 'graphql-api')),
+                Behaviors::DENY => sprintf('❌ %s', $this->__('Deny access', 'graphql-api')),
+                default => $behavior,
+            }
+        );
+    }
+
+    abstract protected function getRenderBlockLabel(): string;
+}
