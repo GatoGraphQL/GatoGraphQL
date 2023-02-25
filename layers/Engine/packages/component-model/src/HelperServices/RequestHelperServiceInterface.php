4f1bdb8186e2817d9db2c1ccfc7d8ec358aecaa4
Leonardo Losoviz <leo@getpop.org>
2023-02-25 17:39:12 +0800

    Reordered methods

--- layers/Engine/packages/component-model/src/HelperServices/RequestHelperServiceInterface.php
+++ layers/Engine/packages/component-model/src/HelperServices/RequestHelperServiceInterface.php
@@ -6,14 +6,14 @@ namespace PoP\ComponentModel\HelperServices;
 
 interface RequestHelperServiceInterface
 {
-    public function getComponentModelCurrentURL(): ?string;
-
     /**
      * Return the requested full URL
      *
      * @param boolean $useHostRequestedByClient If true, get the host from user-provided HTTP_HOST, otherwise from the server-defined SERVER_NAME
      */
     public function getRequestedFullURL(bool $useHostRequestedByClient = false): ?string;
+
+    public function getComponentModelCurrentURL(): ?string;
 
     /**
      * Retrieve the visitor's IP address. If the property name
