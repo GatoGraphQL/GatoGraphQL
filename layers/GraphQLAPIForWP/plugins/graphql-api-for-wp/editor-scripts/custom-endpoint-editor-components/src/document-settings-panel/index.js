52e5ce07da7b0f5080d9b55db9ae6d8aecc05abb
Leonardo Losoviz <leo@getpop.org>
2023-02-28 15:22:45 +0800

    custom-endpoint

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/editor-scripts/custom-endpoint-editor-components/src/document-settings-panel/index.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/editor-scripts/custom-endpoint-editor-components/src/document-settings-panel/index.js
@@ -7,35 +7,31 @@ import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 /**
  * Internal dependencies
  */
-import { MarkdownInfoModalButton } from '@graphqlapi/components';
+import { MarkdownGuideButton } from '@graphqlapi/components';
 import { getMarkdownContentOrUseDefault } from '../markdown-loader';
 
 /**
  * Constants to customize
  */
 const DOCUMENT_SETTINGS_PANEL_NAME = 'custom-endpoint-document-settings-panel';
+const guideName = __('Creating Custom Endpoints', 'graphql-api');
+const pageFilenames = [
+    'welcome-guide',
+    'schema-config-options',
+]
 /**
  * Component
  */
 const DocumentSettingsPanel = () => (
     <PluginDocumentSettingPanel
         name={ DOCUMENT_SETTINGS_PANEL_NAME }
-        title={ __('Additional Documentation', 'graphql-api') }
+        title={ __('Welcome Guide', 'graphql-api') }
     >
-        <MarkdownInfoModalButton
-            title={ __('AnyBuiltInScalar', 'graphql-api') }
-            pageFilename="any-built-in-scalar"
+        <MarkdownGuideButton
             getMarkdownContentCallback={ getMarkdownContentOrUseDefault }
-        />
-        <MarkdownInfoModalButton
-            title={ __('OneOf Input Object', 'graphql-api') }
-            pageFilename="oneof-input-object"
-            getMarkdownContentCallback={ getMarkdownContentOrUseDefault }
-        />
-        <MarkdownInfoModalButton
-            title={ __('Query Schema Extensions via Introspection', 'graphql-api') }
-            pageFilename="query-schema-extensions-via-introspection"
-            getMarkdownContentCallback={ getMarkdownContentOrUseDefault }
+            contentLabel={ guideName }
+            guideName={ guideName }
+            pageFilenames={ pageFilenames }
         />
     </PluginDocumentSettingPanel>
 );
