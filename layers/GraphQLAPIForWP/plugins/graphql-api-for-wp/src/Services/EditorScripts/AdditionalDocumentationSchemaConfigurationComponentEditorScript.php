951c2e5a23be826238eabbc09ad2fd72f1e6a80a
Leonardo Losoviz <leo@getpop.org>
2023-03-01 08:53:59 +0800

    renamed SCHEMA_CONFIGURATION_ADDITIONAL_DOCUMENTATION_SIDEBAR_COMPONENT

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/EditorScripts/AdditionalDocumentationSchemaConfigurationComponentEditorScript.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/EditorScripts/AdditionalDocumentationSchemaConfigurationComponentEditorScript.php
@@ -31,7 +31,7 @@ class AdditionalDocumentationSchemaConfigurationComponentEditorScript extends Ab
 
     public function getEnablingModule(): ?string
     {
-        return UserInterfaceFunctionalityModuleResolver::SCHEMA_CONFIGURATION_SIDEBAR_DOCUMENTATION;
+        return UserInterfaceFunctionalityModuleResolver::SCHEMA_CONFIGURATION_ADDITIONAL_DOCUMENTATION_SIDEBAR_COMPONENT;
     }
 
     /**
