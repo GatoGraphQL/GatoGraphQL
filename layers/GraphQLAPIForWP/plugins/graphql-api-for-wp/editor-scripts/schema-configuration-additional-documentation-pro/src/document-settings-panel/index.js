9993f1e54b8af7d9cdfc7cee7610281b6bafa4de
Leonardo Losoviz <leo@getpop.org>
2023-03-01 09:44:21 +0800

    desc

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/editor-scripts/schema-configuration-additional-documentation-pro/src/document-settings-panel/index.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/editor-scripts/schema-configuration-additional-documentation-pro/src/document-settings-panel/index.js
@@ -40,7 +40,7 @@ const DocumentSettingsPanel = () => (
         name={ DOCUMENT_SETTINGS_PANEL_NAME }
         title={ __('[PRO] Additional Documentation', 'graphql-api') }
     >
-        <p>{ __('Docs for additional features in the GraphQL API PRO:', 'graphql-api') }</p>
+        <p>{ __('Docs for additional features unlocked by the GraphQL API PRO:', 'graphql-api') }</p>
         <p>
             {
                 implicitFeaturesDocEntries.map( ( entry ) =>
