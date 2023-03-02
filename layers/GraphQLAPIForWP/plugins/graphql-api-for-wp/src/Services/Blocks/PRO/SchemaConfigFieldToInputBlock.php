f4fdd404fc1be4b9c9c784c68bf6d5adfdba518a
Leonardo Losoviz <leo@getpop.org>
2023-03-02 13:31:30 +0800

    removed stale imports

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/PRO/SchemaConfigFieldToInputBlock.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Blocks/PRO/SchemaConfigFieldToInputBlock.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Services\Blocks\PRO;
 
-use GraphQLAPI\GraphQLAPI\Services\Blocks\AbstractDefaultEnableDisableFunctionalitySchemaConfigBlock;
 use GraphQLAPI\GraphQLAPIPRO\ModuleResolvers\SchemaConfigurationFunctionalityModuleResolver;
 
 class SchemaConfigFieldToInputBlock extends AbstractSchemaConfigPlaceholderPROBlock
