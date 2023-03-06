6d67bb54930bbf11b4ddd2b17ba3c8f3d1211f26
Leonardo Losoviz <leo@getpop.org>
2023-03-06 17:54:15 +0800

    Added tabs with Markdown Content

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/MenuPages/RecipesMenuPage.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/MenuPages/RecipesMenuPage.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Services\MenuPages;
 
+use GraphQLAPI\GraphQLAPI\App;
+use GraphQLAPI\GraphQLAPI\Constants\RequestParams;
 use GraphQLAPI\GraphQLAPI\ContentProcessors\ContentParserOptions;
 use GraphQLAPI\GraphQLAPI\ContentProcessors\PluginMarkdownContentRetrieverTrait;
 use GraphQLAPI\GraphQLAPI\Services\MenuPages\AbstractDocsMenuPage;
@@ -26,20 +28,71 @@ class RecipesMenuPage extends AbstractDocsMenuPage
 
     protected function getContentToPrint(): string
     {
-        $markdownContent = '';
         $recipeEntries = [
             [
                 'sample-recipe-1',
-                'Any built-in scalar',
+                'Sample Recipe 1',
             ],
             [
                 'sample-recipe-2',
-                'OneOf Input Object',
+                'Sample Recipe 2',
             ],
         ];
+        // By default, focus on the first recipe
+        $activeRecipeName = $recipeEntries[0][0];
+        // If passing a tab, focus on that one, if the module exists
+        $tab = App::query(RequestParams::TAB);
+        if ($tab !== null) {
+            $recipeNames = array_map(
+                fn (array $recipeEntry) => $recipeEntry[0],
+                $recipeEntries
+            );
+            if (in_array($tab, $recipeNames)) {
+                $activeRecipeName = $tab;
+            }
+        }
+        $class = 'wrap vertical-tabs graphql-api-tabpanel';
+        
+        $markdownContent = sprintf(
+            <<<HTML
+            <div id="%s" class="%s">
+                <h1>%s</h1>
+                <div class="nav-tab-container">
+                    <!-- Tabs -->
+                    <h2 class="nav-tab-wrapper">
+            HTML,
+            'graphql-api-recipes',
+            $class,
+            \__('GraphQL API — Recipes', 'graphql-api')
+        );
+
         foreach ($recipeEntries as $recipeEntry) {
-            $markdownContent .= $this->getMarkdownContent(
-                'recipes/' . $recipeEntry[0],
+            $recipeEntryName = $recipeEntry[0];
+            $recipeEntryTitle = $recipeEntry[1];
+            $markdownContent .= sprintf(
+                '<a href="#%s" class="nav-tab %s">%s</a>',
+                $recipeEntryName,
+                $recipeEntryName === $activeRecipeName ? 'nav-tab-active' : '',
+                $recipeEntryTitle
+            );
+        }
+
+        $markdownContent .= sprintf(
+            <<<HTML
+                            </h2>
+                            <div class="nav-tab-content">
+            HTML,
+            'graphql-api-recipes',
+            $class,
+            \__('GraphQL API — Recipes', 'graphql-api')
+        );
+
+        foreach ($recipeEntries as $recipeEntry) {
+            $recipeEntryName = $recipeEntry[0];
+            $recipeEntryTitle = $recipeEntry[1];
+
+            $recipeContent = $this->getMarkdownContent(
+                'recipes/' . $recipeEntryName,
                 '',
                 [
                     ContentParserOptions::TAB_CONTENT => false,
@@ -48,10 +101,33 @@ class RecipesMenuPage extends AbstractDocsMenuPage
                 '<p>%s</p>',
                 sprintf(
                     \__('Oops, there was a problem loading recipe "%s"', 'graphql-api'),
-                    $recipeEntry[1]
+                    $recipeEntryTitle
                 )
             );
+
+            $markdownContent .= sprintf(
+                <<<HTML
+                    <div id="%s" class="%s" style="%s">
+                        <!--<h2>%s</h2><hr/>-->
+                        %s
+                    </div>
+                HTML,
+                $recipeEntryName,
+                'tab-content',
+                sprintf(
+                    'display: %s;',
+                    $recipeEntryName === $activeRecipeName ? 'block' : 'none'
+                ),
+                $recipeEntryTitle,
+                $recipeContent
+            );
         }
+
+        $markdownContent .= <<<HTML
+                </div> <!-- class="nav-tab-content" -->
+            </div> <!-- class="nav-tab-container" -->
+        </div>
+        HTML;
         return $markdownContent;
     }
 }
