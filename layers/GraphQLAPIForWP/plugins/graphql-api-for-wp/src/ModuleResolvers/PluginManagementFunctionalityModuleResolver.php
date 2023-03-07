92112a8e9b9445abd32eadee9701c85656525e3f
Leonardo Losoviz <leo@getpop.org>
2023-03-07 17:09:11 +0800

    Added module

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/ModuleResolvers/PluginManagementFunctionalityModuleResolver.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\ModuleResolvers;
+
+use GraphQLAPI\GraphQLAPI\Constants\ResetDBOptions;
+use GraphQLAPI\GraphQLAPI\ContentProcessors\MarkdownContentParserInterface;
+use GraphQLAPI\GraphQLAPI\ModuleSettings\Properties;
+use GraphQLAPI\GraphQLAPI\Plugin;
+
+class PluginManagementFunctionalityModuleResolver extends AbstractFunctionalityModuleResolver
+{
+    use ModuleResolverTrait;
+    use PluginManagementFunctionalityModuleResolverTrait;
+
+    public final const PLUGIN_MANAGEMENT = Plugin::NAMESPACE . '\plugin-management';
+
+    /**
+     * Setting options
+     */
+    public final const OPTION_RESET_DB = 'reset-db';
+
+    private ?MarkdownContentParserInterface $markdownContentParser = null;
+
+    final public function setMarkdownContentParser(MarkdownContentParserInterface $markdownContentParser): void
+    {
+        $this->markdownContentParser = $markdownContentParser;
+    }
+    final protected function getMarkdownContentParser(): MarkdownContentParserInterface
+    {
+        /** @var MarkdownContentParserInterface */
+        return $this->markdownContentParser ??= $this->instanceManager->getInstance(MarkdownContentParserInterface::class);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getModulesToResolve(): array
+    {
+        return [
+            self::PLUGIN_MANAGEMENT,
+        ];
+    }
+
+    public function isPredefinedEnabledOrDisabled(string $module): ?bool
+    {
+        return match ($module) {
+            self::PLUGIN_MANAGEMENT => true,
+            default => parent::isPredefinedEnabledOrDisabled($module),
+        };
+    }
+
+    public function isHidden(string $module): bool
+    {
+        return match ($module) {
+            self::PLUGIN_MANAGEMENT => true,
+            default => parent::isHidden($module),
+        };
+    }
+
+    public function getName(string $module): string
+    {
+        return match ($module) {
+            self::PLUGIN_MANAGEMENT => \__('Plugin Management', 'graphql-api'),
+            default => $module,
+        };
+    }
+
+    public function getDescription(string $module): string
+    {
+        return match ($module) {
+            self::PLUGIN_MANAGEMENT => \__('Admin tools to manage the GraphQL API plugin', 'graphql-api'),
+            default => parent::getDescription($module),
+        };
+    }
+
+    /**
+     * Default value for an option set by the module
+     */
+    public function getSettingsDefaultValue(string $module, string $option): mixed
+    {
+        $defaultValues = [
+            self::PLUGIN_MANAGEMENT => [
+                self::OPTION_RESET_DB => '',
+            ],
+        ];
+        return $defaultValues[$module][$option] ?? null;
+    }
+
+    /**
+     * Array with the inputs to show as settings for the module
+     *
+     * @return array<array<string,mixed>> List of settings for the module, each entry is an array with property => value
+     */
+    public function getSettings(string $module): array
+    {
+        $moduleSettings = parent::getSettings($module);
+        if ($module === self::PLUGIN_MANAGEMENT) {
+            $option = self::OPTION_RESET_DB;
+            $moduleSettings[] = [
+                Properties::INPUT => $option,
+                Properties::NAME => $this->getSettingOptionName(
+                    $module,
+                    $option
+                ),
+                Properties::TITLE => \__('Reset the Settings?', 'graphql-api'),
+                Properties::DESCRIPTION => \__('Reset all values stored in the Settings, and define the default behavior of the GraphQL API (before the new settings are stored): safe or unsafe.', 'graphql-api'),
+                Properties::TYPE => Properties::TYPE_STRING,
+                Properties::POSSIBLE_VALUES => [
+                    '' => \__('Select', 'graphql-api'),
+                    ResetDBOptions::SAFE => \__('Reset settings, using "safe" default values', 'graphql-api'),
+                    ResetDBOptions::UNSAFE => \__('Reset settings, using "unsafe" default values', 'graphql-api'),
+                ],
+            ];
+        }
+        return $moduleSettings;
+    }
+}
