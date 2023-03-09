52d8e81e70c8df3fcece0c5a4f83f3b4f5897cfc
Leonardo Losoviz <leo@getpop.org>
2023-03-09 09:56:57 +0800

    Made tabpanel JS more specific

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/assets/js/tabpanel.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/assets/js/tabpanel.js
@@ -1,10 +1,10 @@
 jQuery( document ).ready( function($){
-    $('.graphql-api-tabpanel .nav-tab').on('click', function(e){
+    $('.graphql-api-tabpanel > .nav-tab-container > .nav-tab-wrapper > a.nav-tab').on('click', function(e){
         e.preventDefault();
         tab = $(this).attr('href');
-        $('.graphql-api-tabpanel .tab-content').hide();
+        $('.graphql-api-tabpanel > .nav-tab-container > .nav-tab-content .tab-content').hide();
         $(tab).show();
-        $('.graphql-api-tabpanel .nav-tab').removeClass('nav-tab-active');
-        $('.graphql-api-tabpanel a[href="'+tab+'"].nav-tab').addClass('nav-tab-active');
+        $('.graphql-api-tabpanel > .nav-tab-container > .nav-tab-wrapper > a.nav-tab').removeClass('nav-tab-active');
+        $('.graphql-api-tabpanel > .nav-tab-container > .nav-tab-wrapper > a[href="'+tab+'"].nav-tab').addClass('nav-tab-active');
     });
 });
