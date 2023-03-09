3e54d4ce3a07577fe62a91a493ba902431648d31
Leonardo Losoviz <leo@getpop.org>
2023-03-09 10:17:32 +0800

    pass tab-content-target in tab-panel

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/assets/js/tabpanel.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/assets/js/tabpanel.js
@@ -2,20 +2,21 @@ jQuery( document ).ready( function($){
     $('.graphql-api-tabpanel > .nav-tab-container > .nav-tab-wrapper > a.nav-tab').on('click', function(e){
         e.preventDefault();
         tab = $(this).attr('href');
+        tabPanel = $(tab).closest('.graphql-api-tabpanel');
+        tabPanelSelector = '#' + tabPanel.attr('id');
         /**
          * Allow to specify which is the target to show/hide.
          * This allows to have a tabPanel wrapping another tabPanel,
          * and avoid toggling the inner ".tab-content" (in addition
          * to the outer ones) when clicking on the outer tabPanels's tab.
          */
-        tabContentTarget = $(this).data('tab-content-target');
-        if (tabContentTarget !== undefined) {
-            $(tabContentTarget).hide();
-        } else {
-            $('.graphql-api-tabpanel > .nav-tab-container > .nav-tab-content .tab-content').hide();
-        }        
+        tabContentTarget = tabPanel.data('tab-content-target');
+        if (tabContentTarget == undefined) {
+            tabContentTarget = tabPanelSelector +' > .nav-tab-container > .nav-tab-content .tab-content';
+        }
+        $(tabContentTarget).hide();
         $(tab).show();
-        $('.graphql-api-tabpanel > .nav-tab-container > .nav-tab-wrapper > a.nav-tab').removeClass('nav-tab-active');
-        $('.graphql-api-tabpanel > .nav-tab-container > .nav-tab-wrapper > a[href="'+tab+'"].nav-tab').addClass('nav-tab-active');
+        $(tabPanelSelector +' > .nav-tab-container > .nav-tab-wrapper > a.nav-tab').removeClass('nav-tab-active');
+        $(tabPanelSelector +' > .nav-tab-container > .nav-tab-wrapper > a[href="'+tab+'"].nav-tab').addClass('nav-tab-active');
     });
 });
