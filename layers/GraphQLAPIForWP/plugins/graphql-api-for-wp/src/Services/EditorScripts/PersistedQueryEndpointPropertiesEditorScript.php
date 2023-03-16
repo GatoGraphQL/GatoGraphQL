3355ac6886bbddae4e433d0cfa470e296b15465a
Leonardo Losoviz <leo@getpop.org>
2023-03-16 11:51:41 +0800

    PHP editor script

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/EditorScripts/PersistedQueryEndpointPropertiesEditorScript.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\Services\EditorScripts;
+
+use GraphQLAPI\GraphQLAPI\ModuleResolvers\UserInterfaceFunctionalityModuleResolver;
+use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\GraphQLPersistedQueryEndpointCustomPostType;
+use GraphQLAPI\GraphQLAPI\Services\Scripts\MainPluginScriptTrait;
+
+class PersistedQueryEndpointPropertiesEditorScript extends AbstractEditorScript
+{
+    use MainPluginScriptTrait;
+
+    private ?GraphQLPersistedQueryEndpointCustomPostType $graphQLPersistedQueryEndpointCustomPostType = null;
+
+    final public function setGraphQLPersistedQueryEndpointCustomPostType(GraphQLPersistedQueryEndpointCustomPostType $graphQLPersistedQueryEndpointCustomPostType): void
+    {
+        $this->graphQLPersistedQueryEndpointCustomPostType = $graphQLPersistedQueryEndpointCustomPostType;
+    }
+    final protected function getGraphQLPersistedQueryEndpointCustomPostType(): GraphQLPersistedQueryEndpointCustomPostType
+    {
+        /** @var GraphQLPersistedQueryEndpointCustomPostType */
+        return $this->graphQLPersistedQueryEndpointCustomPostType ??= $this->instanceManager->getInstance(GraphQLPersistedQueryEndpointCustomPostType::class);
+    }
+
+    protected function getScriptName(): string
+    {
+        return 'persisted-query-endpoint-properties';
+    }
+
+    public function getEnablingModule(): ?string
+    {
+        return UserInterfaceFunctionalityModuleResolver::PERSISTED_QUERY_ENDPOINT_PROPERTIES;
+    }
+
+    /**
+     * Add the locale language to the localized data?
+     */
+    protected function addLocalLanguage(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Default language for the script/component's documentation
+     */
+    protected function getDefaultLanguage(): ?string
+    {
+        // English
+        return 'en';
+    }
+
+    protected function getAllowedPostTypes(): array
+    {
+        return array_merge(
+            parent::getAllowedPostTypes(),
+            [
+                $this->getGraphQLPersistedQueryEndpointCustomPostType()->getCustomPostType(),
+            ]
+        );
+    }
+
+    protected function registerStyleIndexCSS(): bool
+    {
+        return true;
+    }
+}
