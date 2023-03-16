ac002a7cebe987878280d005c39de52e750f55d0
Leonardo Losoviz <leo@getpop.org>
2023-03-16 09:56:29 +0800

    removed code

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/editor-scripts/custom-endpoint-properties/src/document-settings-panel/custom-endpoint-properties.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/editor-scripts/custom-endpoint-properties/src/document-settings-panel/custom-endpoint-properties.js
@@ -1,12 +1,10 @@
 /**
  * WordPress dependencies
  */
-import { useSelect, useDispatch } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
 import { safeDecodeURIComponent, cleanForSlug } from '@wordpress/url';
-import { useState } from '@wordpress/element';
-import { __experimentalInspectorPopoverHeader as InspectorPopoverHeader } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
-import { TextControl, ExternalLink } from '@wordpress/components';
+import { ExternalLink } from '@wordpress/components';
 import { store as coreStore } from '@wordpress/core-data';
 import { store as editorStore } from '@wordpress/editor';
 
@@ -42,60 +40,10 @@ export default function CustomEndpointProperties( { onClose } ) {
 		};
 	}, [] );
 
-	const { editPost } = useDispatch( editorStore );
-
-	const [ forceEmptyField, setForceEmptyField ] = useState( false );
-
 	return (
 		<div className="editor-post-url">
 			<p><strong>{ __('URLs:', 'graphql-api') }</strong></p>
 			<p><strong>{ __('Clients:', 'graphql-api') }</strong></p>
-			{/* <InspectorPopoverHeader title={ __( 'URL' ) } onClose={ onClose } />
-			{ isEditable && (
-				<TextControl
-					__nextHasNoMarginBottom
-					label={ __( 'Permalink' ) }
-					value={ forceEmptyField ? '' : postSlug }
-					autoComplete="off"
-					spellCheck="false"
-					help={
-						<>
-							{ __( 'The last part of the URL.' ) }{ ' ' }
-							<ExternalLink
-								href={ __(
-									'https://wordpress.org/support/article/settings-sidebar/#permalink'
-								) }
-							>
-								{ __( 'Learn more.' ) }
-							</ExternalLink>
-						</>
-					}
-					onChange={ ( newValue ) => {
-						editPost( { slug: newValue } );
-						// When we delete the field the permalink gets
-						// reverted to the original value.
-						// The forceEmptyField logic allows the user to have
-						// the field temporarily empty while typing.
-						if ( ! newValue ) {
-							if ( ! forceEmptyField ) {
-								setForceEmptyField( true );
-							}
-							return;
-						}
-						if ( forceEmptyField ) {
-							setForceEmptyField( false );
-						}
-					} }
-					onBlur={ ( event ) => {
-						editPost( {
-							slug: cleanForSlug( event.target.value ),
-						} );
-						if ( forceEmptyField ) {
-							setForceEmptyField( false );
-						}
-					} }
-				/>
-			) }
 			{ isEditable && (
 				<h3 className="editor-post-url__link-label">
 					{ viewPostLabel ?? __( 'View post' ) }
@@ -123,7 +71,7 @@ export default function CustomEndpointProperties( { onClose } ) {
 						postLink
 					) }
 				</ExternalLink>
-			</p> */}
+			</p>
 		</div>
 	);
 }
