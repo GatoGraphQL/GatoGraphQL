044990c951734fb9375cf948402ac1e64cf4619b
Leonardo Losoviz <leo@getpop.org>
2023-03-22 16:53:31 +0800

    Initialize based on USE_PAYLOADABLE_MUTATIONS

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginInitializationConfiguration.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/PluginInitializationConfiguration.php
@@ -468,6 +468,43 @@ class PluginInitializationConfiguration extends AbstractMainPluginInitialization
                 'module' => SchemaTypeModuleResolver::SCHEMA_USER_ROLES,
                 'option' => SchemaTypeModuleResolver::OPTION_TREAT_USER_CAPABILITY_AS_SENSITIVE_DATA,
             ],
+            // Do not use the Payloadable types for mutations
+            [
+                'class' => \PoPCMSSchema\CommentMutations\Module::class,
+                'envVariable' => \PoPCMSSchema\CommentMutations\Environment::USE_PAYLOADABLE_COMMENT_MUTATIONS,
+                'module' => MutationSchemaTypeModuleResolver::SCHEMA_MUTATIONS,
+                'option' => MutationSchemaTypeModuleResolver::USE_PAYLOADABLE_MUTATIONS,
+            ],
+            [
+                'class' => \PoPCMSSchema\CustomPostCategoryMutations\Module::class,
+                'envVariable' => \PoPCMSSchema\CustomPostCategoryMutations\Environment::USE_PAYLOADABLE_CUSTOMPOSTCATEGORY_MUTATIONS,
+                'module' => MutationSchemaTypeModuleResolver::SCHEMA_MUTATIONS,
+                'option' => MutationSchemaTypeModuleResolver::USE_PAYLOADABLE_MUTATIONS,
+            ],
+            [
+                'class' => \PoPCMSSchema\CustomPostMutations\Module::class,
+                'envVariable' => \PoPCMSSchema\CustomPostMutations\Environment::USE_PAYLOADABLE_CUSTOMPOST_MUTATIONS,
+                'module' => MutationSchemaTypeModuleResolver::SCHEMA_MUTATIONS,
+                'option' => MutationSchemaTypeModuleResolver::USE_PAYLOADABLE_MUTATIONS,
+            ],
+            [
+                'class' => \PoPCMSSchema\CustomPostTagMutations\Module::class,
+                'envVariable' => \PoPCMSSchema\CustomPostTagMutations\Environment::USE_PAYLOADABLE_CUSTOMPOSTTAG_MUTATIONS,
+                'module' => MutationSchemaTypeModuleResolver::SCHEMA_MUTATIONS,
+                'option' => MutationSchemaTypeModuleResolver::USE_PAYLOADABLE_MUTATIONS,
+            ],
+            [
+                'class' => \PoPCMSSchema\CustomPostMediaMutations\Module::class,
+                'envVariable' => \PoPCMSSchema\CustomPostMediaMutations\Environment::USE_PAYLOADABLE_CUSTOMPOSTMEDIA_MUTATIONS,
+                'module' => MutationSchemaTypeModuleResolver::SCHEMA_MUTATIONS,
+                'option' => MutationSchemaTypeModuleResolver::USE_PAYLOADABLE_MUTATIONS,
+            ],
+            [
+                'class' => \PoPCMSSchema\UserStateMutations\Module::class,
+                'envVariable' => \PoPCMSSchema\UserStateMutations\Environment::USE_PAYLOADABLE_USERSTATE_MUTATIONS,
+                'module' => MutationSchemaTypeModuleResolver::SCHEMA_MUTATIONS,
+                'option' => MutationSchemaTypeModuleResolver::USE_PAYLOADABLE_MUTATIONS,
+            ],
         ];
     }
 
