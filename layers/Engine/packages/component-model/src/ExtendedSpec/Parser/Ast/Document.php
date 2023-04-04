f291d6962b4b5cae9f0c48ee57df585f3d80c5f1
Leonardo Losoviz <leo@getpop.org>
2023-04-04 11:04:26 +0800

    getAdditionalExportUnderVariableNameArguments

--- layers/Engine/packages/component-model/src/ExtendedSpec/Parser/Ast/Document.php
+++ layers/Engine/packages/component-model/src/ExtendedSpec/Parser/Ast/Document.php
@@ -57,6 +57,22 @@ class Document extends AbstractDocument
         return $directive->getArgument($exportUnderVariableNameArgumentName);
     }
 
+    /**
+     * @return Argument[]|null
+     */
+    protected function getAdditionalExportUnderVariableNameArguments(Directive $directive): ?array
+    {
+        $dynamicVariableDefinerFieldDirectiveResolver = $this->getDynamicVariableDefinerFieldDirectiveResolver($directive);
+        if ($dynamicVariableDefinerFieldDirectiveResolver === null) {
+            return null;
+        }
+        $additionalExportUnderVariableNameArgumentNames = $dynamicVariableDefinerFieldDirectiveResolver->getAdditionalExportUnderVariableNameArgumentNames();
+        return array_map(
+            fn (string $exportUnderVariableNameArgumentName) => $directive->getArgument($exportUnderVariableNameArgumentName),
+            $additionalExportUnderVariableNameArgumentNames
+        );
+    }
+
     protected function isOperationDependencyDefinerDirective(Directive $directive): bool
     {
         return $this->getOperationDependencyDefinerFieldDirectiveResolver($directive) !== null;
