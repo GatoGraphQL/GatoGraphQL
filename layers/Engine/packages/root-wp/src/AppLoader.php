56d3816c6eb21c528dfc196417581f83daeee2c3
Leonardo Losoviz <leo@getpop.org>
2023-04-06 15:13:07 +0800

    getBootApplicationHooks

--- layers/Engine/packages/root-wp/src/AppLoader.php
+++ layers/Engine/packages/root-wp/src/AppLoader.php
@@ -15,35 +15,39 @@ class AppLoader extends UpstreamAppLoader
      */
     public function bootApplicationModules(): void
     {
-        /**
-         * Find the right action hook to initialize the application,
-         * depending if we are in wp-admin, in the WP REST API, or in the frontend.
-         *
-         * Boot once it has parsed the WP_Query, so that the requested post/user/etc
-         * is already processed and available. For the front-end, the only hook
-         * available is "wp".
-         *
-         * Watch out:
-         *
-         * - "wp" doesn't trigger in the admin() => use "wp_loaded" instead.
-         * - "wp" doesn't trigger in REST => use "rest_api_init" instead.
-         *
-         * (Eg for the latter: when editing an ACL in the WordPress editor
-         * and clicking on Update, it uses a REST call.)
-         *
-         * Because we don't know yet if the current request is a REST call or not
-         * (must wait until hook "parse_request" for that), simply
-         * load the hook for both 'rest_api_init' and 'wp', knowing
-         * that only one of them will be called anyway.
-         *
-         * @see https://stackoverflow.com/questions/41101294/check-whether-request-is-wp-rest-api-request
-         */
-        $actionHooks = \is_admin() ? ['wp_loaded'] : ['rest_api_init', 'wp'];
-        foreach ($actionHooks as $actionHook) {
+        foreach ($this->getBootApplicationHooks() as $actionHook) {
             App::addAction(
                 $actionHook,
                 fn () => parent::bootApplicationModules()
             );
         }
+    }
+
+    /**
+     * Find the right action hook to initialize the application,
+     * depending if we are in wp-admin, in the WP REST API, or in the frontend.
+     *
+     * Boot once it has parsed the WP_Query, so that the requested post/user/etc
+     * is already processed and available. For the front-end, the only hook
+     * available is "wp".
+     *
+     * Watch out:
+     *
+     * - "wp" doesn't trigger in the admin() => use "wp_loaded" instead.
+     * - "wp" doesn't trigger in REST => use "rest_api_init" instead.
+     *
+     * (Eg for the latter: when editing an ACL in the WordPress editor
+     * and clicking on Update, it uses a REST call.)
+     *
+     * Because we don't know yet if the current request is a REST call or not
+     * (must wait until hook "parse_request" for that), simply
+     * load the hook for both 'rest_api_init' and 'wp', knowing
+     * that only one of them will be called anyway.
+     *
+     * @see https://stackoverflow.com/questions/41101294/check-whether-request-is-wp-rest-api-request
+     */
+    protected function getBootApplicationHooks(): array
+    {
+        return \is_admin() ? ['wp_loaded'] : ['rest_api_init', 'wp'];
     }
 }
