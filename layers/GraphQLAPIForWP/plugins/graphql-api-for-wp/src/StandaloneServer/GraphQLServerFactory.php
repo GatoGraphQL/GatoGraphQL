06dfb8fb5439c4521b5f21a4b3b7bb0779db4d53
Leonardo Losoviz <leo@getpop.org>
2023-04-06 13:08:36 +0800

    GraphQLServerFactory

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/StandaloneServer/GraphQLServerFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\StandaloneServer;
+
+use GraphQLByPoP\GraphQLServer\Standalone\GraphQLServer;
+use PoP\Root\Module\ModuleInterface;
+
+/**
+ * Obtain a single instance of the GraphQLServer object,
+ * initialized with the same configuration as the wp-admin
+ * endpoint.
+ */
+class GraphQLServerFactory
+{
+    private static ?GraphQLServer $graphQLServer = null;
+
+    public static function getInstance(): GraphQLServer
+    {
+        if (self::$graphQLServer === null) {
+            self::$graphQLServer = static::createInstance();
+        }
+        return self::$graphQLServer;
+    }
+    
+    private static function createInstance(): GraphQLServer
+    {
+        return new GraphQLServer(
+            static::getModuleClasses(),
+            static::getModuleClassConfiguration(),
+            [],
+            [],
+            false,
+        );
+    }
+
+    /**
+     * @return array<class-string<ModuleInterface>>
+     */
+    private static function getModuleClasses(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return array<class-string<ModuleInterface>,array<string,mixed>> [key]: Module class, [value]: Configuration
+     */
+    private static function getModuleClassConfiguration(): array
+    {
+        return [];
+    }
+}
