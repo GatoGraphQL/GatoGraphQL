1095d83ee74f1c5291c534a079e1f2155b64f228
Leonardo Losoviz <leo@getpop.org>
2023-04-06 13:46:58 +0800

    phpdoc

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Exception/GraphQLServerNotReadyException.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Exception/GraphQLServerNotReadyException.php
@@ -6,6 +6,10 @@ namespace GraphQLAPI\GraphQLAPI\Exception;
 
 use PoP\Root\Exception\AbstractClientException;
 
+/**
+ * Indicate that the standalone GraphQLServer instance
+ * has been requested before it can be initialized.
+ */
 final class GraphQLServerNotReadyException extends AbstractClientException
 {
 }
