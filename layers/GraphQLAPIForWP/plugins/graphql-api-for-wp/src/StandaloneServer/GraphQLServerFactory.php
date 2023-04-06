a0ab41e7b9fe2b14ad0db1abeee06fc58ccaf85e
Leonardo Losoviz <leo@getpop.org>
2023-04-06 15:26:27 +0800

    fixed message for GraphQLServerNotReadyException

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/StandaloneServer/GraphQLServerFactory.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/StandaloneServer/GraphQLServerFactory.php
@@ -34,6 +34,8 @@ class GraphQLServerFactory
      * Create a new instance of the GraphQLServer
      *
      * @throws GraphQLServerNotReadyException if the application is not ready yet
+     *
+     * @see layers/Engine/packages/root-wp/src/AppLoader.php
      */
     private static function createInstance(): GraphQLServer
     {
@@ -41,7 +43,7 @@ class GraphQLServerFactory
         if (!$appLoader->isReadyState()) {
             throw new GraphQLServerNotReadyException(
                 sprintf(
-                    \__('The initialization of the GraphQL server has not yet taken place. This takes place in these hooks: \'%s\' in the wp-admin, \'%s\' in the WP REST API, and \'%s\' otherwise (i.e. in the actual website). Invoke the GraphQL server only after these hooks have taken place. See file: layers/Engine/packages/root-wp/src/AppLoader.php', 'graphql-api'),
+                    \__('The GraphQL server is not ready yet. Its initialization takes place in hooks: \'%s\' in the wp-admin, \'%s\' in the WP REST API, and \'%s\' otherwise (i.e. in the actual website). Invoke the GraphQL server only after these hooks have been invoked. See file: layers/Engine/packages/root-wp/src/AppLoader.php', 'graphql-api'),
                     'wp_loaded',
                     'rest_api_init',
                     'wp'
