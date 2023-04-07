f45129e33c3f2fc7e395740ac671ebe160cc66ea
Leonardo Losoviz <leo@getpop.org>
2023-04-07 17:38:16 +0800

    Use WP AppLoader

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Standalone/GraphQLServer.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Standalone/GraphQLServer.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace GraphQLAPI\GraphQLAPI\Standalone;
 
 use GraphQLByPoP\GraphQLServer\Standalone\GraphQLServer as UpstreamGraphQLServer;
-use PoP\Root\StateManagers\HookManagerInterface;
+use PoP\RootWP\AppLoader;
 use PoP\RootWP\StateManagers\HookManager;
+use PoP\Root\AppLoaderInterface;
+use PoP\Root\StateManagers\HookManagerInterface;
 
 class GraphQLServer extends UpstreamGraphQLServer
 {
+    protected function getAppLoader(): AppLoaderInterface
+    {
+        return new AppLoader();
+    }
+
     protected function getHookManager(): HookManagerInterface
     {
         return new HookManager();
