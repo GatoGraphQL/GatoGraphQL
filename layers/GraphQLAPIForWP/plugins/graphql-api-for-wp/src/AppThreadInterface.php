0c095b7d605fd14f86e57e477e8598971b52b372
Leonardo Losoviz <leo@getpop.org>
2023-04-08 21:46:49 +0800

    Removed stale code from AppThread

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/AppThreadInterface.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/AppThreadInterface.php
@@ -4,29 +4,8 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI;
 
-use GraphQLAPI\GraphQLAPI\PluginManagement\ExtensionManager;
-use GraphQLAPI\GraphQLAPI\PluginManagement\MainPluginManager;
-use GraphQLAPI\GraphQLAPI\PluginSkeleton\ExtensionInterface;
-use GraphQLAPI\GraphQLAPI\PluginSkeleton\MainPluginInterface;
 use PoP\ComponentModel\AppThreadInterface as UpstreamAppThreadInterface;
 
 interface AppThreadInterface extends UpstreamAppThreadInterface
 {
-    public function initializePlugin(
-        ?MainPluginManager $mainPluginManager = null,
-        ?ExtensionManager $extensionManager = null,
-    ): void;
-
-    public function getMainPluginManager(): MainPluginManager;
-    public function getExtensionManager(): ExtensionManager;
-
-    /**
-     * Shortcut function.
-     */
-    public function getMainPlugin(): MainPluginInterface;
-
-    /**
-     * Shortcut function.
-     */
-    public function getExtension(string $extensionClass): ExtensionInterface;
 }
