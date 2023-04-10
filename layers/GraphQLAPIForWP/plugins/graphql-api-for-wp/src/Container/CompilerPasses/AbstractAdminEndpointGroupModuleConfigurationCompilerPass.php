d72bfbe2f99f3a521c82ec4d9400b8b7394fe3d0
Leonardo Losoviz <leo@getpop.org>
2023-04-10 14:16:14 +0800

    completed CompilerPass

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Container/CompilerPasses/AbstractAdminEndpointGroupModuleConfigurationCompilerPass.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Container/CompilerPasses/AbstractAdminEndpointGroupModuleConfigurationCompilerPass.php
@@ -4,43 +4,29 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Container\CompilerPasses;
 
-use PoP\ComponentModel\Schema\SchemaNamespacingServiceInterface;
+use GraphQLAPI\GraphQLAPI\EndpointConfiguration\AdminEndpointModuleConfigurationStoreInterface;
 use PoP\Root\Container\CompilerPasses\AbstractCompilerPass;
 use PoP\Root\Container\ContainerBuilderWrapperInterface;
-use PoP\Root\Exception\ShouldNotHappenException;
 use PoP\Root\Module\ModuleInterface;
 
 abstract class AbstractAdminEndpointGroupModuleConfigurationCompilerPass extends AbstractCompilerPass
 {
     protected function doProcess(ContainerBuilderWrapperInterface $containerBuilderWrapper): void
     {
-        $schemaNamespacingServiceDefinition = $containerBuilderWrapper->getDefinition(SchemaNamespacingServiceInterface::class);
-        $schemaNamespace = $this->getSchemaNamespace();
-        foreach ($this->getModuleClasses() as $moduleClass) {
-            $pos = strrpos($moduleClass, '\\');
-            if ($pos === false) {
-                throw new ShouldNotHappenException(
-                    sprintf(
-                        'Module class \'%s\' has no namespace!',
-                        $moduleClass
-                    )
-                );
-            }
-            $moduleClassNamespace = substr($moduleClass, 0, $pos);
-            $schemaNamespacingServiceDefinition->addMethodCall(
-                'addSchemaNamespaceForClassOwnerAndProjectNamespace',
-                [
-                    $moduleClassNamespace,
-                    $schemaNamespace
-                ]
-            );
-        }
+        $adminEndpointModuleConfigurationStore = $containerBuilderWrapper->getDefinition(AdminEndpointModuleConfigurationStoreInterface::class);
+        $adminEndpointModuleConfigurationStore->addMethodCall(
+            'addEndpointGroupModuleConfiguration',
+            [
+                $this->getEndpointGroup(),
+                $this->getModuleConfiguration()
+            ]
+        );
     }
 
     /**
-     * @return string[]
-     * @phpstan-return array<class-string<ModuleInterface>>
+     * @return array<string,array<string,mixed>>
+     * @phpstan-return array<class-string<ModuleInterface>,array<string,mixed>>
      */
-    abstract protected function getModuleClasses(): array;
-    abstract protected function getSchemaNamespace(): string;
+    abstract protected function getModuleConfiguration(): array;
+    abstract protected function getEndpointGroup(): string;
 }
