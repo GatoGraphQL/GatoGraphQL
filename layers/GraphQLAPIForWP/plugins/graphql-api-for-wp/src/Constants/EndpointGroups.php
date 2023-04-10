004e0c2f87487517cca0265894b0ddb251280a33
Leonardo Losoviz <leo@getpop.org>
2023-04-10 14:19:19 +0800

    PLUGIN_INTERNAL_WP_EDITOR

---
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Constants/EndpointGroups.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLAPI\GraphQLAPI\Constants;
+
+class EndpointGroups
+{
+    public final const PLUGIN_INTERNAL_WP_EDITOR = 'pluginInternalWPEditor';
+}
