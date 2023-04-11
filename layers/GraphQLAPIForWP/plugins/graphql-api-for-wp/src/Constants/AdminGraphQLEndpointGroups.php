457cc670963cd322778cb384c54605b992aec435
Leonardo Losoviz <leo@getpop.org>
2023-04-11 13:16:38 +0800

    Added const

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Constants/AdminGraphQLEndpointGroups.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Constants/AdminGraphQLEndpointGroups.php
@@ -6,5 +6,6 @@ namespace GraphQLAPI\GraphQLAPI\Constants;
 
 class AdminGraphQLEndpointGroups
 {
+    public final const PERSISTED_QUERY = 'persistedQuery';
     public final const PLUGIN_INTERNAL = 'pluginInternal';
 }
