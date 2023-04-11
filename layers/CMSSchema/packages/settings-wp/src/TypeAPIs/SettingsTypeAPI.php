13e6932e58dd3d257b6225839e587fed29cd86e1
Leonardo Losoviz <leo@getpop.org>
2023-04-11 19:48:32 +0800

    optionValue return null if option doesn't exist

--- layers/CMSSchema/packages/settings-wp/src/TypeAPIs/SettingsTypeAPI.php
+++ layers/CMSSchema/packages/settings-wp/src/TypeAPIs/SettingsTypeAPI.php
@@ -8,8 +8,12 @@ use PoPCMSSchema\Settings\TypeAPIs\AbstractSettingsTypeAPI;
 
 class SettingsTypeAPI extends AbstractSettingsTypeAPI
 {
+    /**
+     * If the name is non-existent, return `null`.
+     * Otherwise, return the value.
+     */
     protected function doGetOption(string $name): mixed
     {
-        return \get_option($name);
+        return \get_option($name, null);
     }
 }
