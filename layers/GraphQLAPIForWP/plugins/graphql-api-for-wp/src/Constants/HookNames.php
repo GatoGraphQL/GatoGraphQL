71ed5f1ef54d3ba01ce375a0cf90dfeb0b414886
Leonardo Losoviz <leo@getpop.org>
2023-04-12 14:20:38 +0800

    renamed hook

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Constants/HookNames.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Constants/HookNames.php
@@ -16,5 +16,5 @@ class HookNames
     public final const ADMIN_ENDPOINT_GROUP_MODULE_CONFIGURATION = __CLASS__ . ':admin-endpoint-group-module-configuration';
     public final const ADMIN_ENDPOINT_GROUP_MODULE_CLASSES_TO_SKIP = __CLASS__ . ':admin-endpoint-group-module-classes-to-skip';
 
-    public final const SUPPORTED_ADMIN_ENDPOINT_GROUPS = __CLASS__ . ':supported-endpoint-groups';
+    public final const SUPPORTED_ADMIN_ENDPOINT_GROUPS = __CLASS__ . ':supported-admin-endpoint-groups';
 }
