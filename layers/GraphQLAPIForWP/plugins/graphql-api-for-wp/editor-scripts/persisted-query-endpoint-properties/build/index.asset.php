6c45f5c163f6a6775f6b3e3e608099e01559303a
Leonardo Losoviz <leo@getpop.org>
2023-04-13 17:23:43 +0800

    npm run build

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/editor-scripts/persisted-query-endpoint-properties/build/index.asset.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/editor-scripts/persisted-query-endpoint-properties/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'ff128208490756919bc795a2739b7631');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'f0df49eba76811f5c956415fe23e315c');
