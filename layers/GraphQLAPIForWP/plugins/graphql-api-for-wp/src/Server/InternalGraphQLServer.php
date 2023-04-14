dcfedbe5dd24556269379db70d78dd5451e5885f
Leonardo Losoviz <leo@getpop.org>
2023-04-14 14:18:52 +0800

    InternalGraphQLServer

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Server/InternalGraphQLServer.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Server/InternalGraphQLServer.php
@@ -4,12 +4,71 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Server;
 
+use GraphQLAPI\GraphQLAPI\AppThread;
 use GraphQLByPoP\GraphQLServer\Server\AbstractGraphQLServer;
-use PoP\RootWP\StateManagers\HookManager;
+use PoP\ComponentModel\App;
+use PoP\ComponentModel\AppThreadInterface;
+use PoP\ComponentModel\ExtendedSpec\Execution\ExecutableDocument;
+use PoP\Root\HttpFoundation\Response;
 use PoP\Root\StateManagers\HookManagerInterface;
+use PoP\RootWP\StateManagers\HookManager;
 
 class InternalGraphQLServer extends AbstractGraphQLServer
 {
+    private AppThreadInterface $appThread;
+
+    /**
+     * Initialize the App with a new AppThread
+     */
+    public function __construct()
+    {
+        $this->appThread = new AppThread();
+        
+        /**
+         * Keep the current AppThread, switch to the GraphQLServer's
+         * one, initialize the App, and then restore the current AppThread.
+         */
+        $currentAppThread = App::getAppThread();
+        App::setAppThread($this->appThread);
+
+        // Initialize the App
+        // ...
+
+        // Restore the original AppThread
+        App::setAppThread($currentAppThread);
+    }
+
+    /**
+     * The basic state for executing GraphQL queries is already set.
+     * In addition, inject the actual GraphQL query and variables,
+     * build the AST, and generate and print the data.
+     *
+     * @param array<string,mixed> $variables
+     */
+    public function execute(
+        string|ExecutableDocument $queryOrExecutableDocument,
+        array $variables = [],
+        ?string $operationName = null
+    ): Response {
+        /**
+         * Keep the current AppThread, switch to the GraphQLServer's
+         * one, resolve the query, and then restore the current AppThread.
+         */
+        $currentAppThread = App::getAppThread();
+        App::setAppThread($this->appThread);
+
+        $response = parent::execute(
+            $queryOrExecutableDocument,
+            $variables,
+            $operationName,
+        );
+
+        // Restore the original AppThread
+        App::setAppThread($currentAppThread);
+
+        return $response;
+    }
+
     protected function getHookManager(): HookManagerInterface
     {
         return new HookManager();
