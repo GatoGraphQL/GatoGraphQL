9fa928542b6c86b7faf1d699e8c276954a45f646
Leonardo Losoviz <leo@getpop.org>
2023-04-14 14:44:09 +0800

    do_action with PluginAppGraphQLServerNames

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Server/InternalGraphQLServer.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Server/InternalGraphQLServer.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace GraphQLAPI\GraphQLAPI\Server;
 
 use GraphQLAPI\GraphQLAPI\AppThread;
+use GraphQLAPI\GraphQLAPI\PluginAppGraphQLServerNames;
 use GraphQLAPI\GraphQLAPI\PluginAppHooks;
 use GraphQLByPoP\GraphQLServer\Server\AbstractAttachedGraphQLServer;
 use PoP\ComponentModel\App;
 use PoP\ComponentModel\AppThreadInterface;
 use PoP\RootWP\StateManagers\HookManager;
-
 use PoP\Root\StateManagers\HookManagerInterface;
+
 use function do_action;
 
 class InternalGraphQLServer extends AbstractAttachedGraphQLServer
@@ -28,7 +29,10 @@ class InternalGraphQLServer extends AbstractAttachedGraphQLServer
 
     protected function initializeApp(): AppThreadInterface
     {
-        do_action(PluginAppHooks::INITIALIZE_APP);
+        do_action(
+            PluginAppHooks::INITIALIZE_APP,
+            PluginAppGraphQLServerNames::INTERNAL,
+        );
         return App::getAppThread();
     }
 }
