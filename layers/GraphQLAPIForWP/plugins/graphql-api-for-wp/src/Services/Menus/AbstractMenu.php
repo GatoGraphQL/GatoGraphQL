ed6cd78c96b0ce100c5bf5691520a840fb10efb1
Leonardo Losoviz <leo@getpop.org>
2023-04-14 20:14:27 +0800

    Only initialize once, for the main AppThread

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Menus/AbstractMenu.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/Menus/AbstractMenu.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Services\Menus;
 
-use PoP\Root\Services\BasicServiceTrait;
+use GraphQLAPI\GraphQLAPI\App;
+use GraphQLAPI\GraphQLAPI\PluginAppGraphQLServerNames;
 use PoP\Root\Services\AbstractAutomaticallyInstantiatedService;
+use PoP\Root\Services\BasicServiceTrait;
 
 abstract class AbstractMenu extends AbstractAutomaticallyInstantiatedService implements MenuInterface
 {
     use BasicServiceTrait;
+
+    /**
+     * Only initialize once, for the main AppThread
+     */
+    public function isServiceEnabled(): bool
+    {
+        return App::getAppThread()->getName() === PluginAppGraphQLServerNames::STANDARD;
+    }
 
     /**
      * Initialize the endpoints
