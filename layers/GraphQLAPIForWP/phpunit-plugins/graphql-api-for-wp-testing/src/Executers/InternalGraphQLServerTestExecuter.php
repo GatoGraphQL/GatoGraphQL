f70844bea71ebae55e173e0059e11549b1ef785f
Leonardo Losoviz <leo@getpop.org>
2023-04-17 14:32:27 +0800

    InternalGraphQLServerTestExecuter

---
+++ layers/GraphQLAPIForWP/phpunit-plugins/graphql-api-for-wp-testing/src/Executers/InternalGraphQLServerTestExecuter.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPITesting\Executers;
+
+use PHPUnitForGraphQLAPI\GraphQLAPITesting\Constants\Actions;
+use PoP\Root\App;
+use PoP\Root\Constants\HookNames;
+
+use function add_action;
+
+class InternalGraphQLServerTestExecuter
+{
+    public function __construct()
+    {
+        add_action(
+            HookNames::APPLICATION_READY,
+            $this->maybeSetupInternalGraphQLServerTesting(...)
+        );
+    }
+
+    /**
+     * Check if the params are set in the request to test
+     * the `InternalGraphQLServer`
+     */
+    public function maybeSetupInternalGraphQLServerTesting(): void
+    {
+        /** @var string[] */
+        $actions = App::getState('actions');
+        if (!in_array(Actions::TEST_INTERNAL_GRAPHQL_SERVER, $actions)) {
+            return;
+        }
+
+        $this->setupInternalGraphQLServerTesting();
+    }
+
+    /**
+     * Set-up the GraphQL queries to execute to test
+     * the `InternalGraphQLServer`
+     */
+    protected function setupInternalGraphQLServerTesting(): void
+    {
+        // Do something
+    }
+}
