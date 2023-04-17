53d96394bd7d962b5ebdf75f10132232cf40be9b
Leonardo Losoviz <leo@getpop.org>
2023-04-17 14:27:58 +0800

    HookNames::APPLICATION_READY

--- layers/Engine/packages/root/src/Constants/HookNames.php
+++ layers/Engine/packages/root/src/Constants/HookNames.php
@@ -7,4 +7,5 @@ namespace PoP\Root\Constants;
 class HookNames
 {
     public final const AFTER_BOOT_APPLICATION = __CLASS__ . ':after-boot-application';
+    public final const APPLICATION_READY = __CLASS__ . ':application-ready';
 }
