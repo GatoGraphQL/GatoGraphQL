f09eef9f38c762616ee1211c7568e66ade2d92b3
Leonardo Losoviz <leo@getpop.org>
2023-04-17 15:03:27 +0800

    Inject the "internal-graphql-server-response" state

--- layers/GraphQLAPIForWP/phpunit-plugins/graphql-api-for-wp-testing/src/Executers/InternalGraphQLServerTestExecuter.php
+++ layers/GraphQLAPIForWP/phpunit-plugins/graphql-api-for-wp-testing/src/Executers/InternalGraphQLServerTestExecuter.php
@@ -5,17 +5,19 @@ declare(strict_types=1);
 namespace PHPUnitForGraphQLAPI\GraphQLAPITesting\Executers;
 
 use PHPUnitForGraphQLAPI\GraphQLAPITesting\Constants\Actions;
-use PoP\Root\App;
 use PoP\Root\Constants\HookNames;
-
-use function add_action;
 
 class InternalGraphQLServerTestExecuter
 {
+    /**
+     * Inject the "internal-graphql-server-response" state
+     * containing the response of the same requested query
+     * via the endpoint, but executed against `GraphQLServer`.
+     */
     public function __construct()
     {
-        add_action(
-            HookNames::APPLICATION_READY,
+        add_filter(
+            HookNames::APP_STATE_CONSOLIDATED,
             $this->maybeSetupInternalGraphQLServerTesting(...)
         );
     }
@@ -23,28 +25,54 @@ class InternalGraphQLServerTestExecuter
     /**
      * When resolving a GraphQL query, check if the params
      * are set in the request to test the `InternalGraphQLServer`
+     *
+     * @param array<string,mixed> $state
+     * @return array<string,mixed>
      */
-    public function maybeSetupInternalGraphQLServerTesting(): void
+    public function maybeSetupInternalGraphQLServerTesting(array $state): array
     {
-        if (!App::getState('executing-graphql')) {
-            return;
+        if (!$state['executing-graphql'] || $state['query'] === null) {
+            return $state;
         }
 
         /** @var string[] */
-        $actions = App::getState('actions');
+        $actions = $state['actions'];
         if (!in_array(Actions::TEST_INTERNAL_GRAPHQL_SERVER, $actions)) {
-            return;
+            return $state;
         }
 
-        $this->setupInternalGraphQLServerTesting();
+        return $this->setupInternalGraphQLServerTesting($state);
     }
 
     /**
      * Set-up the GraphQL queries to execute to test
      * the `InternalGraphQLServer`
      */
-    protected function setupInternalGraphQLServerTesting(): void
+    protected function setupInternalGraphQLServerTesting(array $state): array
     {
-        // Do something
+        $appStateKey = 'internal-graphql-server-response';
+        $state[$appStateKey] = null;
+
+        /**
+         * Modify the query with a simple hack:
+         * Append field "_appStateValue" at the end of the query
+         *
+         * @var string
+         */
+        $query = $state['query'];
+        $closingBracketPost = strrpos($query, '}');
+        $query = substr(
+            $query,
+            0,
+            $closingBracketPost
+        ) . sprintf(
+            ' internalGraphQLServerResponse: _appStateValue(name: "%s") ',
+            $appStateKey
+        ) . substr(
+            $query,
+            $closingBracketPost
+        );
+        $state['query'] = $query;
+        return $state;
     }
 }
