49e54bcc4fa9173d4006811ff71863e4b363bc46
Leonardo Losoviz <leo@getpop.org>
2023-04-17 14:11:47 +0800

    Enable "appState" fields on DEV

---
+++ layers/Engine/packages/engine/src/Constants/Actions.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Engine\Constants;
+
+class Actions
+{
+    public final const ENABLE_APP_STATE_FIELDS = 'enable-app-state-fields';
+}
--- layers/Engine/packages/engine/src/ModuleConfiguration.php
+++ layers/Engine/packages/engine/src/ModuleConfiguration.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace PoP\Engine;
 
+use PoP\Engine\Constants\Actions;
+use PoP\Root\App;
+use PoP\Root\Environment as RootEnvironment;
 use PoP\Root\Module\AbstractModuleConfiguration;
 use PoP\Root\Module\EnvironmentValueHelpers;
 
@@ -22,10 +25,19 @@ class ModuleConfiguration extends AbstractModuleConfiguration
         );
     }
 
+    /**
+     * Enable fields "_appState", "_appStateKeys" and "_appStateValue"
+     * 
+     * Enable only on DEV as to test the application,
+     * and only when passing ?actions[]=enable-app-state-fields
+     */
     public function enableQueryingAppStateFields(): bool
     {
+        /** @var string[] */
+        $actions = App::getState('actions') ?? [];
         $envVariable = Environment::ENABLE_QUERYING_APP_STATE_FIELDS;
-        $defaultValue = false;
+        $defaultValue = RootEnvironment::isApplicationEnvironmentDev()
+            && in_array(Actions::ENABLE_APP_STATE_FIELDS, $actions);
         $callback = EnvironmentValueHelpers::toBool(...);
 
         return $this->retrieveConfigurationValueOrUseDefault(
