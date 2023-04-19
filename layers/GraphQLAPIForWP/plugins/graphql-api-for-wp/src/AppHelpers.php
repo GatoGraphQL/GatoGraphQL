f33dc6ffd73c71ada452e3ce2607f43d08b43cbc
Leonardo Losoviz <leo@getpop.org>
2023-04-19 15:49:33 +0800

    phpdoc

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/AppHelpers.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/AppHelpers.php
@@ -6,6 +6,10 @@ namespace GraphQLAPI\GraphQLAPI;
 
 class AppHelpers
 {
+    /**
+     * Indicate if the current AppThread is the one used for
+     * the External GraphQL Server. This is the default state.
+     */
     public static function isExternalGraphQLServerAppThread(): bool
     {
         return App::getAppThread()->getName() === PluginAppGraphQLServerNames::EXTERNAL;
@@ -19,6 +23,11 @@ class AppHelpers
         return static::isExternalGraphQLServerAppThread();
     }
 
+    /**
+     * Indicate if the current AppThread is the one used for
+     * the Internal GraphQL Server. This happens within the
+     * invocation of `InternalGraphQLServer->execute`
+     */
     public static function isInternalGraphQLServerAppThread(): bool
     {
         return App::getAppThread()->getName() === PluginAppGraphQLServerNames::INTERNAL;
