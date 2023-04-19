4e6149b011cdd46b058c486f4550875501c6eea0
Leonardo Losoviz <leo@getpop.org>
2023-04-19 10:59:25 +0800

    format

--- layers/Engine/packages/engine-wp/templates/SendResponse.php
+++ layers/Engine/packages/engine-wp/templates/SendResponse.php
@@ -7,5 +7,6 @@ use PoP\Root\App;
 /**
  * Send the Response to the client
  */
-App::getResponse()->sendHeaders();
-echo App::getResponse()->getContent();
+$response = App::getResponse();
+$response->sendHeaders();
+echo $response->getContent();
