7158533064fd40fddc18acab283982afe5b32e47
Leonardo Losoviz <leo@getpop.org>
2023-04-20 10:04:25 +0800

    Renamed tests

--- layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/AbstractAdminPublicPersistedQueryQueryExecutionFixtureWebserverRequestTestCase.php
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/AbstractAdminPublicPersistedQueryQueryExecutionFixtureWebserverRequestTestCase.php
@@ -6,7 +6,7 @@ namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
 
 use PHPUnitForGraphQLAPI\WebserverRequests\WordPressAuthenticatedUserWebserverRequestTestCaseTrait;
 
-abstract class AbstractAdminPersistedQueryQueryExecutionFixtureWebserverRequestTestCase extends AbstractFixtureEndpointWebserverRequestTestCase
+abstract class AbstractAdminPublicPersistedQueryQueryExecutionFixtureWebserverRequestTestCase extends AbstractFixtureEndpointWebserverRequestTestCase
 {
     use WordPressAuthenticatedUserWebserverRequestTestCaseTrait;
 
@@ -15,6 +15,6 @@ abstract class AbstractAdminPersistedQueryQueryExecutionFixtureWebserverRequestT
      */
     protected function getEndpoint(): string
     {
-        return 'wp-admin/edit.php?page=graphql_api&action=execute_query&endpoint_group=persistedQuery&persisted_query_id=65';
+        return 'wp-admin/edit.php?page=graphql_api&action=execute_query&endpoint_group=publicPersistedQuery&persisted_query_id=65';
     }
 }
