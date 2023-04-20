4ce0a83afe7f62491754cb212a6fe5a5326469f7
Leonardo Losoviz <leo@getpop.org>
2023-04-20 09:34:23 +0800

    Split class into abstract

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/EndpointExecuters/PublicPersistedQueryEndpointGraphQLQueryResolutionEndpointExecuter.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Services/EndpointExecuters/PublicPersistedQueryEndpointGraphQLQueryResolutionEndpointExecuter.php
@@ -5,17 +5,13 @@ declare(strict_types=1);
 namespace GraphQLAPI\GraphQLAPI\Services\EndpointExecuters;
 
 use GraphQLAPI\GraphQLAPI\ModuleResolvers\EndpointFunctionalityModuleResolver;
-use GraphQLAPI\GraphQLAPI\Services\Blocks\PersistedQueryEndpointOptionsBlock;
 use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\GraphQLEndpointCustomPostTypeInterface;
 use GraphQLAPI\GraphQLAPI\Services\CustomPostTypes\GraphQLPublicPersistedQueryEndpointCustomPostType;
-use GraphQLAPI\GraphQLAPI\Services\Helpers\GraphQLQueryPostTypeHelpers;
-use WP_Post;
 
-class PublicPersistedQueryEndpointGraphQLQueryResolutionEndpointExecuter extends AbstractGraphQLQueryResolutionEndpointExecuter
+class PublicPersistedQueryEndpointGraphQLQueryResolutionEndpointExecuter extends AbstractPersistedQueryEndpointGraphQLQueryResolutionEndpointExecuter
 {
     private ?GraphQLPublicPersistedQueryEndpointCustomPostType $graphQLPublicPersistedQueryEndpointCustomPostType = null;
-    private ?GraphQLQueryPostTypeHelpers $graphQLQueryPostTypeHelpers = null;
-
+    
     final public function setGraphQLPublicPersistedQueryEndpointCustomPostType(GraphQLPublicPersistedQueryEndpointCustomPostType $graphQLPublicPersistedQueryEndpointCustomPostType): void
     {
         $this->graphQLPublicPersistedQueryEndpointCustomPostType = $graphQLPublicPersistedQueryEndpointCustomPostType;
@@ -24,15 +20,6 @@ class PublicPersistedQueryEndpointGraphQLQueryResolutionEndpointExecuter extends
     {
         /** @var GraphQLPublicPersistedQueryEndpointCustomPostType */
         return $this->graphQLPublicPersistedQueryEndpointCustomPostType ??= $this->instanceManager->getInstance(GraphQLPublicPersistedQueryEndpointCustomPostType::class);
-    }
-    final public function setGraphQLQueryPostTypeHelpers(GraphQLQueryPostTypeHelpers $graphQLQueryPostTypeHelpers): void
-    {
-        $this->graphQLQueryPostTypeHelpers = $graphQLQueryPostTypeHelpers;
-    }
-    final protected function getGraphQLQueryPostTypeHelpers(): GraphQLQueryPostTypeHelpers
-    {
-        /** @var GraphQLQueryPostTypeHelpers */
-        return $this->graphQLQueryPostTypeHelpers ??= $this->instanceManager->getInstance(GraphQLQueryPostTypeHelpers::class);
     }
 
     public function getEnablingModule(): ?string
@@ -43,36 +30,5 @@ class PublicPersistedQueryEndpointGraphQLQueryResolutionEndpointExecuter extends
     protected function getCustomPostType(): GraphQLEndpointCustomPostTypeInterface
     {
         return $this->getGraphQLPublicPersistedQueryEndpointCustomPostType();
-    }
-
-    /**
-     * Provide the query to execute and its variables
-     *
-     * @return array{0:?string,1:?array<string,mixed>} Array of 2 elements: [query, variables]
-     */
-    public function getGraphQLQueryAndVariables(?WP_Post $graphQLQueryPost): array
-    {
-        /**
-         * Extract the query from the post (or from its parents), and set it in the application state
-         */
-        return $this->getGraphQLQueryPostTypeHelpers()->getGraphQLQueryPostAttributes($graphQLQueryPost, true);
-    }
-
-    /**
-     * Indicate if the GraphQL variables must override the URL params
-     */
-    public function doURLParamsOverrideGraphQLVariables(?WP_Post $customPost): bool
-    {
-        if ($customPost === null) {
-            return parent::doURLParamsOverrideGraphQLVariables($customPost);
-        }
-        $default = true;
-        $optionsBlockDataItem = $this->getCustomPostType()->getOptionsBlockDataItem($customPost);
-        if ($optionsBlockDataItem === null) {
-            return $default;
-        }
-
-        // `true` is the default option in Gutenberg, so it's not saved to the DB!
-        return $optionsBlockDataItem['attrs'][PersistedQueryEndpointOptionsBlock::ATTRIBUTE_NAME_ACCEPT_VARIABLES_AS_URL_PARAMS] ?? $default;
     }
 }
