74b834b65da34cee97404b2008ed47ac321646ed
Leonardo Losoviz <leo@getpop.org>
2023-04-25 13:57:46 +0800

    Added tests

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/AccessPrivateCustomEndpointByAdminQueryExecutionFixtureWebserverRequestTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+class AccessPrivateCustomEndpointByAdminQueryExecutionFixtureWebserverRequestTest extends AbstractAccessPrivateCustomEndpointQueryExecutionFixtureWebserverRequestTest
+{
+    protected function getResponseFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-private-custom-endpoints-success';
+    }
+
+    protected function accessClient(): bool
+    {
+        return false;
+    }
+}
