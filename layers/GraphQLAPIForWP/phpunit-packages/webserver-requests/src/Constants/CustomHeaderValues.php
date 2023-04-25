0d75bf71a097eff1b220bf471397776a26fee9ae
Leonardo Losoviz <leo@getpop.org>
2023-04-25 14:27:06 +0800

    Added custom headers

---
+++ layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/src/Constants/CustomHeaderValues.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\WebserverRequests\Constants;
+
+class CustomHeaderValues
+{
+    public const REQUEST_SOURCE_VALUE = 'WebserverRequestTest';
+}
---
+++ layers/GraphQLAPIForWP/phpunit-packages/webserver-requests/src/Constants/CustomHeaders.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\WebserverRequests\Constants;
+
+class CustomHeaders
+{
+    public const REQUEST_SOURCE = 'X-Request-Source';
+}
