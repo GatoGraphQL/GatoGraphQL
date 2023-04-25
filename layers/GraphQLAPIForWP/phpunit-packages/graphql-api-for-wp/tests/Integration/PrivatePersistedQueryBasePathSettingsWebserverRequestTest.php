e7c412189e1dc82be0e43515c28cb245fe4ddbfe
Leonardo Losoviz <leo@getpop.org>
2023-04-25 08:32:45 +0800

    test to remove

--- layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/PrivatePersistedQueryBasePathSettingsWebserverRequestTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/PrivatePersistedQueryBasePathSettingsWebserverRequestTest.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
 
 /**
- * Test that updating the base path for the private persisted queries works well
+ * Test that updating the base path for the private persisted queries works well.
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Temporarily disabled by making the class abstract.
+ *
+ * @todo Remove when "Private Persisted Queries" are supported again.
  */
-class PrivatePersistedQueryBasePathSettingsWebserverRequestTest extends AbstractPersistedQueryBasePathSettingsWebserverRequestTest
+abstract class PrivatePersistedQueryBasePathSettingsWebserverRequestTest extends AbstractPersistedQueryBasePathSettingsWebserverRequestTest
 {
     protected function getModuleID(string $dataName): string
     {
