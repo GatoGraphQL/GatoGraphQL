0d76c841b7b3175fc6a4c2292984eaef30f58978
Leonardo Losoviz <leo@getpop.org>
2023-04-26 11:24:43 +0800

    build block

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-payload-types-for-mutations/build/index.asset.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks/schema-config-payload-types-for-mutations/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'fecf9ffdeba530ba14a294b4d1ae534b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1cc1618cd37df1dc73a5131b0730872c');
