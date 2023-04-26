2403bbbc01737ae579c45cb451ea9d33621e5e2d
Leonardo Losoviz <leo@getpop.org>
2023-04-26 11:20:08 +0800

    Moved module

--- layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/SchemaPayloadTypesForMutationsModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/SchemaPayloadTypesForMutationsModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
 
-use GraphQLAPI\GraphQLAPI\ModuleResolvers\MutationSchemaTypeModuleResolver;
+use GraphQLAPI\GraphQLAPI\ModuleResolvers\SchemaConfigurationFunctionalityModuleResolver;
 use PHPUnitForGraphQLAPI\GraphQLAPI\Integration\AbstractModifyPluginSettingsFixtureEndpointWebserverRequestTestCase;
 
 class SchemaPayloadTypesForMutationsModifyPluginSettingsFixtureEndpointWebserverRequestTest extends AbstractModifyPluginSettingsFixtureEndpointWebserverRequestTestCase
@@ -21,7 +21,7 @@ class SchemaPayloadTypesForMutationsModifyPluginSettingsFixtureEndpointWebserver
 
     protected function getSettingsKey(): string
     {
-        return MutationSchemaTypeModuleResolver::USE_PAYLOADABLE_MUTATIONS_DEFAULT_VALUE;
+        return SchemaConfigurationFunctionalityModuleResolver::USE_PAYLOADABLE_MUTATIONS_DEFAULT_VALUE;
     }
 
     protected function getModuleID(string $dataName): string
