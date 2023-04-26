b49b9f181d51d5d177cff66dce6711b0061ce348
Leonardo Losoviz <leo@getpop.org>
2023-04-26 14:37:36 +0800

    Added test

---
+++ layers/GraphQLAPIForWP/phpunit-packages/graphql-api-for-wp/tests/Integration/SchemaConfigurationForPrivateEndpointQueryExecutionModifyPluginSettingsFixtureEndpointWebserverRequestTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGraphQLAPI\GraphQLAPI\Integration;
+
+class SchemaConfigurationForPrivateEndpointQueryExecutionModifyPluginSettingsFixtureEndpointWebserverRequestTest extends AbstractDefaultSchemaConfigurationForEndpointQueryExecutionModifyPluginSettingsFixtureEndpointWebserverRequestTest
+{
+    /**
+     * Admin client endpoint
+     */
+    protected function getEndpoint(): string
+    {
+        return 'wp-admin/edit.php?page=graphql_api&action=execute_query';
+    }
+
+    protected function getResponseFixtureFolder(): string
+    {
+        return __DIR__ . '/fixture-default-schema-configuration-for-endpoints';
+    }
+
+    protected function getModuleID(string $dataName): string
+    {
+        return 'graphqlapi_graphqlapi_private-endpoint';
+    }
+}
