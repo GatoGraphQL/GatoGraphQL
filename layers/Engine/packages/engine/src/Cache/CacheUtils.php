34b7b33fda573fe52a679ca4a155107401f048a8
Leonardo Losoviz <leo@getpop.org>
2023-04-28 11:04:40 +0800

    modify getSchemaCacheKeyElements

--- layers/Engine/packages/engine/src/Cache/CacheUtils.php
+++ layers/Engine/packages/engine/src/Cache/CacheUtils.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace PoP\Engine\Cache;
 
 use PoP\Root\App;
-use PoP\Engine\Module as EngineModule;
-use PoP\Engine\ModuleConfiguration as EngineModuleConfiguration;
+use PoP\ComponentModel\Module as ComponentModelModule;
+use PoP\ComponentModel\ModuleConfiguration as ComponentModelModuleConfiguration;
+use PoP\Engine\Module;
+use PoP\Engine\ModuleConfiguration;
 
 class CacheUtils
 {
@@ -17,17 +19,25 @@ class CacheUtils
      */
     public static function getSchemaCacheKeyElements(): array
     {
-        /** @var EngineModuleConfiguration */
-        $moduleConfiguration = App::getModule(EngineModule::class)->getConfiguration();
-        return (array)App::applyFilters(
-            self::HOOK_SCHEMA_CACHE_KEY_ELEMENTS,
-            [
+        $schemaCacheKeyElements = [];
+
+        /** @var ComponentModelModuleConfiguration */
+        $componentModelModuleConfiguration = App::getModule(ComponentModelModule::class)->getConfiguration();
+        if ($componentModelModuleConfiguration->supportDefiningServicesInTheContainerBasedOnTheContext()) {
+            /** @var ModuleConfiguration */
+            $moduleConfiguration = App::getModule(Module::class)->getConfiguration();
+            $schemaCacheKeyElements = [
                 'namespaced' => App::getState('namespace-types-and-interfaces'),
                 'version-constraint' => App::getState('version-constraint'),
                 'field-version-constraints' => App::getState('field-version-constraints'),
                 'directive-version-constraints' => App::getState('directive-version-constraints'),
                 'redundant-root-fields-disabled' => $moduleConfiguration->disableRedundantRootTypeMutationFields(),
-            ]
+            ];
+        }
+
+        return (array)App::applyFilters(
+            self::HOOK_SCHEMA_CACHE_KEY_ELEMENTS,
+            $schemaCacheKeyElements
         );
     }
 }
