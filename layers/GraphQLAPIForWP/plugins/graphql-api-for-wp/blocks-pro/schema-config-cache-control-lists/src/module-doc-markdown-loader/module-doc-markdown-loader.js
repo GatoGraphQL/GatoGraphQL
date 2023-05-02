fc15680386276ea92697a4946fd8ecdb0fed124b
Leonardo Losoviz <leo@getpop.org>
2023-05-02 20:13:01 +0800

    Fixed block global var name

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks-pro/schema-config-cache-control-lists/src/module-doc-markdown-loader/module-doc-markdown-loader.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks-pro/schema-config-cache-control-lists/src/module-doc-markdown-loader/module-doc-markdown-loader.js
@@ -2,7 +2,7 @@
  * Path to load the lazy chunks on the fly
  * @see https://v4.webpack.js.org/guides/public-path/#on-the-fly
  */
-__webpack_public_path__ = window.graphqlApiProSchemaConfigCacheControlLists?.publicPath;
+__webpack_public_path__ = window.graphqlApiProSchemaConfigCacheControl?.publicPath;
 
 /**
  * Read the content from a Markdown file in a given language, and return it as HTML
@@ -22,8 +22,8 @@ const getModuleDocMarkdownContent = ( lang ) => {
  * @param {string|null} lang The language to translate to. If none provided, get it from the localized data
  */
 const getModuleDocMarkdownContentOrUseDefault = ( defaultLang, lang ) => {
-	lang = lang || window.graphqlApiProSchemaConfigCacheControlLists?.localeLang
-	defaultLang = defaultLang || window.graphqlApiProSchemaConfigCacheControlLists?.defaultLang
+	lang = lang || window.graphqlApiProSchemaConfigCacheControl?.localeLang
+	defaultLang = defaultLang || window.graphqlApiProSchemaConfigCacheControl?.defaultLang
 	return getModuleDocMarkdownContent( lang )
 		.catch(err => getModuleDocMarkdownContent( defaultLang ) )
 }
--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks-pro/schema-config-cache-control-lists/webpack.config.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks-pro/schema-config-cache-control-lists/webpack.config.js
@@ -63,7 +63,7 @@ config.module.rules.push(
  * @see https://webpack.js.org/configuration/output/#outputjsonpfunction
  */
 // ------------------------------------------------------
-config.output.jsonpFunction = 'webpackJsonpGraphQLAPIProSchemaConfigCacheControlLists';
+config.output.jsonpFunction = 'webpackJsonpGraphQLAPIProSchemaConfigCacheControl';
 // ------------------------------------------------------
 
 module.exports = config;
