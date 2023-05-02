d125ba10cb877874d086ab3043020f5e52d37f1a
Leonardo Losoviz <leo@getpop.org>
2023-05-03 07:52:57 +0800

    renamed SchemaConfigFieldDeprecationLists

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks-pro/schema-config-field-deprecation/webpack.config.js
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/blocks-pro/schema-config-field-deprecation/webpack.config.js
@@ -63,7 +63,7 @@ config.module.rules.push(
  * @see https://webpack.js.org/configuration/output/#outputjsonpfunction
  */
 // ------------------------------------------------------
-config.output.jsonpFunction = 'webpackJsonpGraphQLAPIProSchemaConfigFieldDeprecationLists';
+config.output.jsonpFunction = 'webpackJsonpGraphQLAPIProSchemaConfigFieldDeprecation';
 // ------------------------------------------------------
 
 module.exports = config;
