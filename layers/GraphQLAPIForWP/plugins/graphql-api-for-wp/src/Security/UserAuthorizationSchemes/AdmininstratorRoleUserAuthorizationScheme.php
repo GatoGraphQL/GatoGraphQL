242352b27e44fdeaf1c8c552a8d7d6779f5eda4f
Leonardo Losoviz <leo@getpop.org>
2023-05-04 22:21:21 +0800

    renamed class

--- layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Security/UserAuthorizationSchemes/AdmininstratorRoleUserAuthorizationScheme.php
+++ layers/GraphQLAPIForWP/plugins/graphql-api-for-wp/src/Security/UserAuthorizationSchemes/AdmininstratorRoleUserAuthorizationScheme.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace GraphQLAPI\GraphQLAPI\Security\UserAuthorizationSchemes;
 
-class AdminRoleUserAuthorizationScheme extends AbstractByRoleUserAuthorizationScheme
+class AdmininstratorRoleUserAuthorizationScheme extends AbstractByRoleUserAuthorizationScheme
 {
     /**
      * @return string[]
