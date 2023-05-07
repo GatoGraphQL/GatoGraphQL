d07c082d50f2c9c151cc216a29dd2646c3df8e55
Leonardo Losoviz <leo@getpop.org>
2023-05-08 07:55:47 +0800

    getAdminEndpoint

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/GatoGraphQL.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/GatoGraphQL.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL;
 
+use GatoGraphQL\GatoGraphQL\Services\Helpers\EndpointHelpers;
+use PoP\Root\Facades\Instances\InstanceManagerFacade;
+
 class GatoGraphQL
 {
+    public function getAdminEndpoint(?string $endpointGroup = null): string
+    {
+        $instanceManager = InstanceManagerFacade::getInstance();
+        /** @var EndpointHelpers */
+        $endpointHelpers = $instanceManager->getInstance(EndpointHelpers::class);
+        return $endpointHelpers->getAdminGraphQLEndpoint();
+    }
 }
