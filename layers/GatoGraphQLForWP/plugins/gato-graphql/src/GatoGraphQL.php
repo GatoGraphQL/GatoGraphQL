aaafdd7037a31702fcf37478717fad488080d96e
Leonardo Losoviz <leo@getpop.org>
2023-05-08 08:09:09 +0800

    getEndpointHelpers()

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/GatoGraphQL.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/GatoGraphQL.php
@@ -9,27 +9,30 @@ use PoP\Root\Facades\Instances\InstanceManagerFacade;
 
 final class GatoGraphQL
 {
+    private static ?EndpointHelpers $endpointHelpers = null;
+
+    final static private function getEndpointHelpers(): EndpointHelpers
+    {
+        if (self::$endpointHelpers === null) {
+            $instanceManager = InstanceManagerFacade::getInstance();
+            /** @var EndpointHelpers */
+            self::$endpointHelpers = $instanceManager->getInstance(EndpointHelpers::class);
+        }
+        return self::$endpointHelpers;
+    }
+
     public static function getAdminEndpoint(): string
     {
-        $instanceManager = InstanceManagerFacade::getInstance();
-        /** @var EndpointHelpers */
-        $endpointHelpers = $instanceManager->getInstance(EndpointHelpers::class);
-        return $endpointHelpers->getAdminGraphQLEndpoint();
+        return static::getEndpointHelpers()->getAdminGraphQLEndpoint();
     }
 
     public static function getAdminBlockEditorEndpoint(): string
     {
-        $instanceManager = InstanceManagerFacade::getInstance();
-        /** @var EndpointHelpers */
-        $endpointHelpers = $instanceManager->getInstance(EndpointHelpers::class);
-        return $endpointHelpers->getAdminBlockEditorGraphQLEndpoint();
+        return static::getEndpointHelpers()->getAdminBlockEditorGraphQLEndpoint();
     }
 
     public static function getAdminCustomEndpoint(string $endpointGroup): string
     {
-        $instanceManager = InstanceManagerFacade::getInstance();
-        /** @var EndpointHelpers */
-        $endpointHelpers = $instanceManager->getInstance(EndpointHelpers::class);
-        return $endpointHelpers->getAdminGraphQLEndpoint($endpointGroup);
+        return static::getEndpointHelpers()->getAdminGraphQLEndpoint($endpointGroup);
     }
 }
