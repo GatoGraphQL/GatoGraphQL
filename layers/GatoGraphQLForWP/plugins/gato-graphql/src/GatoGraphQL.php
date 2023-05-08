80e2b57297a34cc61507c8143642358f8f879c3d
Leonardo Losoviz <leo@getpop.org>
2023-05-08 08:06:19 +0800

    3 methods

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/GatoGraphQL.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/GatoGraphQL.php
@@ -9,7 +9,23 @@ use PoP\Root\Facades\Instances\InstanceManagerFacade;
 
 class GatoGraphQL
 {
-    public function getAdminEndpoint(?string $endpointGroup = null): string
+    public function getAdminEndpoint(): string
+    {
+        $instanceManager = InstanceManagerFacade::getInstance();
+        /** @var EndpointHelpers */
+        $endpointHelpers = $instanceManager->getInstance(EndpointHelpers::class);
+        return $endpointHelpers->getAdminGraphQLEndpoint();
+    }
+
+    public function getAdminBlockEditorEndpoint(): string
+    {
+        $instanceManager = InstanceManagerFacade::getInstance();
+        /** @var EndpointHelpers */
+        $endpointHelpers = $instanceManager->getInstance(EndpointHelpers::class);
+        return $endpointHelpers->getAdminBlockEditorGraphQLEndpoint();
+    }
+
+    public function getAdminCustomEndpoint(string $endpointGroup): string
     {
         $instanceManager = InstanceManagerFacade::getInstance();
         /** @var EndpointHelpers */
