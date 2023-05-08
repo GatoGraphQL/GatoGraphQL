99442c29c8f21e869858df1a8459d44c561c43ba
Leonardo Losoviz <leo@getpop.org>
2023-05-08 14:27:08 +0800

    TEST_GATO_GRAPHQL_EXECUTE_QUERY_IN_FILE_METHOD

--- layers/GatoGraphQLForWP/phpunit-plugins/gato-graphql-testing/src/Constants/Actions.php
+++ layers/GatoGraphQLForWP/phpunit-plugins/gato-graphql-testing/src/Constants/Actions.php
@@ -10,4 +10,9 @@ class Actions
     public const TEST_DEEP_NESTED_INTERNAL_GRAPHQL_SERVER = 'test-deep-nested-internal-graphql-server';
     public const TEST_INTERNAL_GRAPHQL_SERVER_NOT_READY = 'test-internal-graphql-server-not-ready';
     public const TEST_GATO_GRAPHQL_ADMIN_ENDPOINTS = 'test-gato-graphql-admin-endpoints';
+    /**
+     * This const is not being invoked in any test as it's the default state
+     */
+    public const TEST_GATO_GRAPHQL_EXECUTE_QUERY_METHOD = 'test-gato-graphql-execute-query-method';
+    public const TEST_GATO_GRAPHQL_EXECUTE_QUERY_IN_FILE_METHOD = 'test-gato-graphql-execute-query-in-file-method';
 }
