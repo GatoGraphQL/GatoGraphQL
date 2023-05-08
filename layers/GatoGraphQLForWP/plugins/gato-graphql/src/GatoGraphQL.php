149e1ff5ce586b987cc3c29979e6c0a36508cd90
Leonardo Losoviz <leo@getpop.org>
2023-05-08 08:22:51 +0800

    phpdoc

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/GatoGraphQL.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/GatoGraphQL.php
@@ -21,16 +21,48 @@ final class GatoGraphQL
         return self::$endpointHelpers;
     }
 
+    /**
+     * Obtain the URL of the private wp-admin endpoint
+     * (which powers the GraphiQL/Interactive Schema clients):
+     *
+     *   /wp-admin/edit.php?page=gato_graphql&action=execute_query
+     *
+     * This endpoint is affected by the configuration selected in the
+     * Settings page (for the selected Schema Configuration and/or
+     * default values).
+     */
     public static function getAdminEndpoint(): string
     {
         return static::getEndpointHelpers()->getAdminGraphQLEndpoint();
     }
 
+    /**
+     * Obtain the private wp-admin endpoint used by blocks in the
+     * WordPress block editor (allowing any application, theme
+     * or plugin to use GraphQL to fetch data for their blocks):
+     *
+     *   /wp-admin/edit.php?page=gato_graphql&action=execute_query&endpoint_group=blockEditor
+     *
+     * This endpoint is NOT affected by the configuration selected in the
+     * Settings page; it has a pre-defined non-restrictive configuration,
+     * allowing to fetch any piece of data.
+     */
     public static function getAdminBlockEditorEndpoint(): string
     {
         return static::getEndpointHelpers()->getAdminBlockEditorGraphQLEndpoint();
     }
 
+    /**
+     * Obtain a custom private wp-admin endpoint:
+     *
+     *   /wp-admin/edit.php?page=gato_graphql&action=execute_query&endpoint_group={customEndpointGroup}
+     *
+     * This custom endpoint must be defined and configured via PHP code,
+     * allowing the developer to expose a private endpoint that has a
+     * specific configuration, defined via code.
+     *
+     * See the Recipes section to learn how to define a custom private endpoint.
+     */
     public static function getAdminCustomEndpoint(string $endpointGroup): string
     {
         return static::getEndpointHelpers()->getAdminGraphQLEndpoint($endpointGroup);
