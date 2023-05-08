30b84c65cf8f491a166d1b41d5c935748300353c
Leonardo Losoviz <leo@getpop.org>
2023-05-08 20:07:59 +0800

    adapted test

--- layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Integration/InternalGraphQLServerNotReadyQueryExecutionFixtureWebserverRequestTest.php
+++ layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Integration/InternalGraphQLServerNotReadyQueryExecutionFixtureWebserverRequestTest.php
@@ -7,6 +7,8 @@ namespace PHPUnitForGatoGraphQL\GatoGraphQL\Integration;
 use PHPUnitForGatoGraphQL\WebserverRequests\AbstractEndpointWebserverRequestTestCase;
 
 /**
+ * @todo This test doesn't make sense anymore, remove!!!
+ *
  * Test the InternalGraphQLServer throws the GraphQLServerNotReady exception.
  *
  * @see layers/GatoGraphQLForWP/phpunit-plugins/gato-graphql-testing/src/Executers/InternalGraphQLServerTestExecuter.php
@@ -58,18 +60,10 @@ class InternalGraphQLServerNotReadyQueryExecutionFixtureWebserverRequestTest ext
     {
         return <<<JSON
         {
-            "code": "internal_server_error",
-            "message": "Uncaught GatoGraphQL\\\\GatoGraphQL\\\\Exception\\\\GraphQLServerNotReadyException: The GraphQL server is not ready yet. Its initialization takes place in WordPress action hooks: 'wp_loaded' in the wp-admin, 'rest_api_init' in the WP REST API, and 'wp' otherwise (i.e. in the actual website). Retrieve the instance of the GraphQL server only after these hooks have been invoked.",
             "data": {
-                "status": 500
-            },
-            "additional_errors": []
+                "id": "root"
+            }
         }
         JSON;
-    }
-
-    protected function getExpectedResponseStatusCode(): int
-    {
-        return 500;
     }
 }
