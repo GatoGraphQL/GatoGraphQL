24b742c1d936ce639cd9a67e1deb18f92c1fc954
Leonardo Losoviz <leo@getpop.org>
2023-05-09 08:34:16 +0800

    Enable module in .sh

--- webservers/gato-graphql-for-prod/setup/configure-plugin-settings.sh
+++ webservers/gato-graphql-for-prod/setup/configure-plugin-settings.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-echo "Configuring the Gato GraphQL plugin settings"
-echo "Selecting \"dummy\" CPT, categories and tags"
+echo -e 'Configuring the Gato GraphQL plugin settings\n'
+
+echo -e 'Enabling modules for DEV\n'
 
 ADMIN_USER_APP_PASSWORD=$(wp user meta get 1 app_password --path=/app/wordpress)
 SITE_DOMAIN=$(wp option get siteurl --path=/app/wordpress)
+
+curl -i --insecure \
+  --user "admin:$(echo $ADMIN_USER_APP_PASSWORD)" \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"state": "enabled"}' \
+  $(echo $SITE_DOMAIN)/wp-json/gato-graphql/v1/admin/modules/gatographql_gatographql_single-endpoint
+
+
+echo -e 'Selecting "dummy" CPT, categories and tags\n'
 
 curl -i --insecure \
   --user "admin:$(echo $ADMIN_USER_APP_PASSWORD)" \
