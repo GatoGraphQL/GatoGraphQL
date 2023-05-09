1efe419d4d77d3e7acffbd151794aa61eb309a66
Leonardo Losoviz <leo@getpop.org>
2023-05-09 14:30:34 +0800

    activate testing-schema plugin

--- webservers/gato-graphql/setup/activate-plugins.sh
+++ webservers/gato-graphql/setup/activate-plugins.sh
@@ -3,4 +3,5 @@
 wp plugin install wordpress-importer --activate --path=/app/wordpress
 # Activate own plugins
 wp plugin activate gato-graphql --path=/app/wordpress
+wp plugin activate gato-graphql-testing-schema --path=/app/wordpress
 wp plugin activate gato-graphql-testing --path=/app/wordpress
