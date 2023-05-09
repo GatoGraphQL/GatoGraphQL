b5a9242ab00c4c03d1daf4d8ad24d00908551f7c
Leonardo Losoviz <leo@getpop.org>
2023-05-09 14:34:50 +0800

    enableQueryingAppStateFields => false

--- layers/Engine/packages/engine/src/ModuleConfiguration.php
+++ layers/Engine/packages/engine/src/ModuleConfiguration.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace PoP\Engine;
 
-use PoP\Engine\Constants\Actions;
-use PoP\Root\App;
-use PoP\Root\Environment as RootEnvironment;
 use PoP\Root\Module\AbstractModuleConfiguration;
 use PoP\Root\Module\EnvironmentValueHelpers;
 
@@ -33,11 +30,8 @@ class ModuleConfiguration extends AbstractModuleConfiguration
      */
     public function enableQueryingAppStateFields(): bool
     {
-        /** @var string[] */
-        $actions = App::getState('actions') ?? [];
         $envVariable = Environment::ENABLE_QUERYING_APP_STATE_FIELDS;
-        $defaultValue = RootEnvironment::isApplicationEnvironmentDev()
-            && in_array(Actions::ENABLE_APP_STATE_FIELDS, $actions);
+        $defaultValue = false;
         $callback = EnvironmentValueHelpers::toBool(...);
 
         return $this->retrieveConfigurationValueOrUseDefault(
