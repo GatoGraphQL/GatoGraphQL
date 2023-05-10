e651760a6ee1c7440edb4284d4fcbdcfb7af1995
Leonardo Losoviz <leo@getpop.org>
2023-05-10 13:25:55 +0800

    stale import

--- layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Integration/WPCronFixtureWebserverRequestTest.php
+++ layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Integration/WPCronFixtureWebserverRequestTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PHPUnitForGatoGraphQL\GatoGraphQL\Integration;
 
-use GuzzleHttp\RequestOptions;
 use PHPUnitForGatoGraphQL\GatoGraphQLTesting\Constants\Placeholders;
 
 /**
