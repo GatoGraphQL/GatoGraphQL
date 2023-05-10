0152aa38d612dc5f0bedc7066c4b786927e682ca
Leonardo Losoviz <leo@getpop.org>
2023-05-10 11:31:08 +0800

    removed testing code

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/AbstractMainPlugin.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/AbstractMainPlugin.php
@@ -25,7 +25,6 @@ use PoP\Root\AppLoader as ImmediateAppLoader;
 use PoP\Root\Environment as RootEnvironment;
 use PoP\Root\Helpers\ClassHelpers;
 use PoP\Root\Module\ModuleInterface;
-use RuntimeException;
 
 use function __;
 use function add_action;
@@ -589,19 +588,10 @@ abstract class AbstractMainPlugin extends AbstractPlugin implements MainPluginIn
                 ?string $operationName = null
             ): void {
                 // No need to print the response
-                $response = GatoGraphQL::executeQuery(
+                GatoGraphQL::executeQuery(
                     $query,
                     $variables,
                     $operationName
-                );
-                throw new RuntimeException(
-                    json_encode([
-                        'user' => get_current_user_id(),
-                        'response' => $response->getContent(),
-                        'query' => $query,
-                        'variables' => $variables,
-                        'operationName' => $operationName,
-                    ])
                 );
             },
             10,
