7059a16ab9520845072bdacc1bc62c7ff200f20f
Leonardo Losoviz <leo@getpop.org>
2023-05-10 11:31:35 +0800

    do not LogUserIn

--- layers/GatoGraphQLForWP/phpunit-plugins/gato-graphql-testing/src/Executers/WPCronTestExecuter.php
+++ layers/GatoGraphQLForWP/phpunit-plugins/gato-graphql-testing/src/Executers/WPCronTestExecuter.php
@@ -85,29 +85,9 @@ class WPCronTestExecuter
             'gato_graphql__execute_query',
             [
                 <<<GRAPHQL
-                mutation LogUserIn {
-                    loginUser(by: {
-                        credentials: {
-                            usernameOrEmail: "admin",
-                            password: "admin"
-                        }
-                    }) {
-                        status
-                        errors {
-                            __typename
-                            ...on ErrorPayload {
-                                message
-                            }
-                        }
-                        user {
-                            id
-                            username
-                        }
-                    }
-                }
                 mutation CreateTrashedPostWithUniqueSlug(
                     \$postTitle: String!
-                ) @depends(on: "LogUserIn") {
+                ) {
                     createPost(input:{
                         title: \$postTitle
                         status: trash
