048c1268429e9f6f5d5475ae1476e6afb0dfa34e
Leonardo Losoviz <leo@getpop.org>
2023-05-10 08:04:56 +0800

    validateResolvedFieldType => true

--- layers/Engine/packages/component-model/src/FieldResolvers/ObjectType/AbstractObjectTypeFieldResolver.php
+++ layers/Engine/packages/component-model/src/FieldResolvers/ObjectType/AbstractObjectTypeFieldResolver.php
@@ -680,26 +680,28 @@ abstract class AbstractObjectTypeFieldResolver extends AbstractFieldResolver imp
     }
 
     /**
-     * This method could return `true`, which means:
+     * This method returning `true` means:
      *
-     *   "Do always automatically validate the type
-     *   of the result against the type declared in the schema".
+     *   "By default, do automatically validate the type of the
+     *   field's result against the type declared in the schema".
      *
-     * However, the great majority of fields already return
-     * the proper type as it is the response from the method they
-     * call, then there's no need to execute this validation.
+     * This is a conservative approach, needed as there are
+     * fields that cannot guarantee what they will return,
+     * because their called methods could return many types of
+     * values (eg: `App::request(...): mixed` or sending an HTTP request)
      *
-     * For this reason, keep the value in `false`, and have
-     * every field that is not sure what it will return
-     * (eg: calling App::request(...) or a Guzzle HTTP request)
-     * indicate that it must be validated, by defining
-     * `validateResolvedFieldType` => `true`
+     * However, most fields already return the proper type
+     * as it is the response from the method they call (eg: 
+     * 'title' => $post->post_title)
+     *
+     * For these, as there's no need to execute this validation,
+     * this method can return `false`, as to optimize the application.
      */
     public function validateResolvedFieldType(
         ObjectTypeResolverInterface $objectTypeResolver,
         FieldInterface $field,
     ): bool {
-        return false;
+        return true;
     }
 
     /**
