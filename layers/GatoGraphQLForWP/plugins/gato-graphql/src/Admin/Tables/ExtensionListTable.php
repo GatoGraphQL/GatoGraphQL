0b4a478e472aa6b00737d326258057b8fdd3adfe
Leonardo Losoviz <leo@getpop.org>
2023-05-11 15:27:29 +0800

    require_once

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Admin/Tables/ExtensionListTable.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Admin/Tables/ExtensionListTable.php
@@ -7,6 +7,12 @@ namespace GatoGraphQL\GatoGraphQL\Admin\Tables;
 use WP_Plugin_Install_List_Table;
 
 /**
+ * The file containing class WP_Plugin_Install_List_Table is not
+ * loaded by default in WordPress.
+ */
+require_once ABSPATH . 'wp-admin/includes/class-wp-plugin-install-list-table.php';
+
+/**
  * Extension Table
  */
 class ExtensionListTable extends WP_Plugin_Install_List_Table implements ItemListTableInterface
