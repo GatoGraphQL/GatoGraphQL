cf95af4d48b03e4aeb00a5a951006ba93b57969e
Leonardo Losoviz <leo@getpop.org>
2023-05-12 15:29:51 +0800

    TableInterface

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Admin/Tables/ItemListTableInterface.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Admin/Tables/ItemListTableInterface.php
@@ -4,28 +4,8 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\Admin\Tables;
 
-interface ItemListTableInterface
+interface ItemListTableInterface extends TableInterface
 {
-    /**
-     * Displays the list of views available on this table.
-     * @return void
-     */
-    public function views();
-
-    /**
-     * Prepares the list of items for displaying.
-     * @return void
-     * phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
-     */
-    public function prepare_items();
-
-    /**
-     * Displays the table.
-     * @return void
-     * phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
-     */
-    public function display();
-
     public function setItemsPerPageOptionName(string $itemsPerPageOptionName): void;
     public function setDefaultItemsPerPage(int $defaultItemsPerPage): void;
 }
