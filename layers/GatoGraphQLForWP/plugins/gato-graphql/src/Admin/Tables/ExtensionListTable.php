656b0483bd225f46cbeac121447c47937f0347cb
Leonardo Losoviz <leo@getpop.org>
2023-05-12 17:57:42 +0800

    Use "gato_extension_slug"

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Admin/Tables/ExtensionListTable.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Admin/Tables/ExtensionListTable.php
@@ -85,7 +85,7 @@ class ExtensionListTable extends WP_Plugin_Install_List_Table implements ItemLis
             $plugin['homepage'] ??= sprintf(
                 '%s/extensions/%s',
                 $moduleConfiguration->getGatoGraphQLWebsiteURL(),
-                $plugin['gato_extension_extension_slug']
+                $plugin['gato_extension_slug']
             );
             $plugin['icons'] ??= [
                 'svg' =>  $gatoGraphQLLogoFile,
@@ -237,7 +237,7 @@ class ExtensionListTable extends WP_Plugin_Install_List_Table implements ItemLis
                 '&amp;TB_iframe=true&amp;width=600&amp;height=550'
             );
             // Replace it with this other link
-            $extensionModule = 'GatoGraphQL\\GatoGraphQL\\extensions\\' . $plugin['gato_extension_extension_slug'];
+            $extensionModule = 'GatoGraphQL\\GatoGraphQL\\extensions\\' . $plugin['gato_extension_slug'];
             $adaptedDetailsLink = \admin_url(sprintf(
                 'admin.php?page=%s&%s=%s&%s=%s&TB_iframe=true&width=600&height=550',
                 App::request('page') ?? App::query('page', ''),
