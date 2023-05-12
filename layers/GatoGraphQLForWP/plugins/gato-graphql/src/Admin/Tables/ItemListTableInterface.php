2f2e671ca4826cecbbd2dc8a91da5821255237c4
Leonardo Losoviz <leo@getpop.org>
2023-05-12 13:16:35 +0800

    removed commented code

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Admin/Tables/ItemListTableInterface.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Admin/Tables/ItemListTableInterface.php
@@ -10,12 +10,14 @@ interface ItemListTableInterface
 	 * Displays the list of views available on this table.
 	 */
 	public function views();
+
     /**
 	 * Prepares the list of items for displaying.
      * @return void
      * phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
 	 */
 	public function prepare_items();
+
     /**
 	 * Displays the table.
      * @return void
@@ -23,31 +25,6 @@ interface ItemListTableInterface
 	 */
 	public function display();
 
-
     public function setItemsPerPageOptionName(string $itemsPerPageOptionName): void;
     public function setDefaultItemsPerPage(int $defaultItemsPerPage): void;
-
-
-    // public function getItemsPerPageOptionName(): string;
-    // public function getDefaultItemsPerPage(): int;
-
-    // // /**
-    // //  * Singular name of the listed records
-    // //  */
-    // // public function getItemSingularName(): string;
-
-    // // /**
-    // //  * Plural name of the listed records
-    // //  */
-    // // public function getItemPluralName(): string;
-
-    // /**
-    //  * Print custom styles, such as the width of the columns
-    //  */
-    // public function printStyles(): void;
-
-    // /**
-    //  * Enqueue the required assets
-    //  */
-    // public function enqueueAssets(): void;
 }
