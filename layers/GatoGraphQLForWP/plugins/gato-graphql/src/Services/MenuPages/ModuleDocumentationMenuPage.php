54adcdce2d5bee5b3df2d9984c928b85ca2e05c3
Leonardo Losoviz <leo@getpop.org>
2023-05-13 16:00:22 +0800

    fixed cs

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/MenuPages/ModuleDocumentationMenuPage.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/MenuPages/ModuleDocumentationMenuPage.php
@@ -7,7 +7,7 @@ namespace GatoGraphQL\GatoGraphQL\Services\MenuPages;
 class ModuleDocumentationMenuPage extends AbstractModuleDocsMenuPage
 {
     private ?ModulesMenuPage $modulesMenuPage = null;
-    
+
     final public function setModulesMenuPage(ModulesMenuPage $modulesMenuPage): void
     {
         $this->modulesMenuPage = $modulesMenuPage;
