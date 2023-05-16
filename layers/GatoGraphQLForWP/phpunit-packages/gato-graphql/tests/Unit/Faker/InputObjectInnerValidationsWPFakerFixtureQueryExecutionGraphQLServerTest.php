af61518a802a9e49f451032a3e9ea8bc7dff970a
Leonardo Losoviz <leo@getpop.org>
2023-05-16 14:55:03 +0800

    phpdoc

--- layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Unit/Faker/InputObjectInnerValidationsWPFakerFixtureQueryExecutionGraphQLServerTest.php
+++ layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Unit/Faker/InputObjectInnerValidationsWPFakerFixtureQueryExecutionGraphQLServerTest.php
@@ -7,6 +7,12 @@ namespace PHPUnitForGatoGraphQL\GatoGraphQL\Unit;
 use PHPUnitForGatoGraphQL\WPFakerSchema\Unit\AbstractWPFakerFixtureQueryExecutionGraphQLServerTestCase;
 use PoP\Root\Module\ModuleInterface;
 
+/**
+ * Test that calling method `validateInputFieldValue` on an inner
+ * Input Object (nested from some other Input Object) works.
+ *
+ * @see layers/GatoGraphQLForWP/phpunit-packages/dummy-schema/src/TypeResolvers/InputObjectType/FourthLayerInputObjectTypeResolver.php function `validateInputFieldValue`
+ */
 class InputObjectInnerValidationsWPFakerFixtureQueryExecutionGraphQLServerTest extends AbstractWPFakerFixtureQueryExecutionGraphQLServerTestCase
 {
     protected function getFixtureFolder(): string
