7d6579fdcf40f6cd7d0bd77312e5a2916c98428b
Leonardo Losoviz <leo@getpop.org>
2023-05-16 14:48:09 +0800

    validateInputFieldValue

--- layers/GatoGraphQLForWP/phpunit-packages/dummy-schema/src/TypeResolvers/InputObjectType/FourthLayerInputObjectTypeResolver.php
+++ layers/GatoGraphQLForWP/phpunit-packages/dummy-schema/src/TypeResolvers/InputObjectType/FourthLayerInputObjectTypeResolver.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace PHPUnitForGatoGraphQL\DummySchema\TypeResolvers\InputObjectType;
 
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedback;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoP\ComponentModel\TypeResolvers\InputObjectType\AbstractInputObjectTypeResolver;
 use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
+use PoP\GraphQLParser\Spec\Parser\Ast\AstInterface;
+use PoP\Root\FeedbackItemProviders\GenericFeedbackItemProvider;
+use PoP\Root\Feedback\FeedbackItemResolution;
 
 class FourthLayerInputObjectTypeResolver extends AbstractInputObjectTypeResolver
 {
@@ -57,5 +62,36 @@ class FourthLayerInputObjectTypeResolver extends AbstractInputObjectTypeResolver
             'name' => SchemaTypeModifiers::MANDATORY,
             default => parent::getInputFieldTypeModifiers($inputFieldName),
         };
+    }
+
+    /**
+     * Validate constraints on the input field's value
+     */
+    protected function validateInputFieldValue(
+        string $inputFieldName,
+        mixed $inputFieldValue,
+        AstInterface $astNode,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): void {
+        parent::validateInputFieldValue($inputFieldName, $inputFieldValue, $astNode, $objectTypeFieldResolutionFeedbackStore);
+
+        if ($inputFieldName === 'name' && $inputFieldValue === '') {
+            $objectTypeFieldResolutionFeedbackStore->addError(
+                new ObjectTypeFieldResolutionFeedback(
+                    new FeedbackItemResolution(
+                        GenericFeedbackItemProvider::class,
+                        GenericFeedbackItemProvider::E1,
+                        [
+                            sprintf(
+                                $this->__('Input \'%s\' cannot be empty', 'dummy-schema'),
+                                $inputFieldName,
+                            ),
+                        ]
+                    ),
+                    $astNode,
+                )
+            );
+            return;
+        }
     }
 }
