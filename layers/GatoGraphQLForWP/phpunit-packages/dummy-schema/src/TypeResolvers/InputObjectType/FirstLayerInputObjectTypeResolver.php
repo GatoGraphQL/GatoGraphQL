3e135d3900bd5f4a14704dff98faa7aa58fb35ad
Leonardo Losoviz <leo@getpop.org>
2023-05-16 13:44:13 +0800

    Added dummy schema types

---
+++ layers/GatoGraphQLForWP/phpunit-packages/dummy-schema/src/TypeResolvers/InputObjectType/FirstLayerInputObjectTypeResolver.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGatoGraphQL\DummySchema\TypeResolvers\InputObjectType;
+
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\TypeResolvers\InputObjectType\AbstractInputObjectTypeResolver;
+use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
+use stdClass;
+
+class FirstLayerInputObjectTypeResolver extends AbstractInputObjectTypeResolver
+{
+    private ?SecondLayerInputObjectTypeResolver $secondLayerFromInputObjectTypeResolver = null;
+
+    final public function setSecondLayerInputObjectTypeResolver(SecondLayerInputObjectTypeResolver $secondLayerFromInputObjectTypeResolver): void
+    {
+        $this->secondLayerFromInputObjectTypeResolver = $secondLayerFromInputObjectTypeResolver;
+    }
+    final protected function getSecondLayerInputObjectTypeResolver(): SecondLayerInputObjectTypeResolver
+    {
+        /** @var SecondLayerInputObjectTypeResolver */
+        return $this->secondLayerFromInputObjectTypeResolver ??= $this->instanceManager->getInstance(SecondLayerInputObjectTypeResolver::class);
+    }
+
+    public function getTypeName(): string
+    {
+        return 'FirstLayerInput';
+    }
+
+    public function getTypeDescription(): ?string
+    {
+        return $this->__('Input Object containing another Input Objects, to test their validation is performed', 'dummy-schema');
+    }
+
+    /**
+     * @return array<string,InputTypeResolverInterface>
+     */
+    public function getInputFieldNameTypeResolvers(): array
+    {
+        return [
+            'innerLayer' => $this->getSecondLayerInputObjectTypeResolver(),
+        ];
+    }
+
+    public function getInputFieldDescription(string $inputFieldName): ?string
+    {
+        return match ($inputFieldName) {
+            'innerLayer' => $this->__('Inner Input Object', 'dummy-schema'),
+            default => parent::getInputFieldDefaultValue($inputFieldName),
+        };
+    }
+
+    public function getInputFieldTypeModifiers(string $inputFieldName): int
+    {
+        return match ($inputFieldName) {
+            'innerLayer' => SchemaTypeModifiers::MANDATORY,
+            default => parent::getInputFieldTypeModifiers($inputFieldName),
+        };
+    }
+
+    public function getInputFieldDefaultValue(string $inputFieldName): mixed
+    {
+        return match ($inputFieldName) {
+            'innerLayer' => new stdClass(),
+            default => parent::getInputFieldDefaultValue($inputFieldName),
+        };
+    }
+}
---
+++ layers/GatoGraphQLForWP/phpunit-packages/dummy-schema/src/TypeResolvers/InputObjectType/FourthLayerInputObjectTypeResolver.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGatoGraphQL\DummySchema\TypeResolvers\InputObjectType;
+
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\TypeResolvers\InputObjectType\AbstractInputObjectTypeResolver;
+use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
+use stdClass;
+
+class FourthLayerInputObjectTypeResolver extends AbstractInputObjectTypeResolver
+{
+    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
+
+    final public function setStringScalarTypeResolver(StringScalarTypeResolver $stringScalarTypeResolver): void
+    {
+        $this->stringScalarTypeResolver = $stringScalarTypeResolver;
+    }
+    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
+    {
+        /** @var StringScalarTypeResolver */
+        return $this->stringScalarTypeResolver ??= $this->instanceManager->getInstance(StringScalarTypeResolver::class);
+    }
+
+    public function getTypeName(): string
+    {
+        return 'FourthLayerInput';
+    }
+
+    public function getTypeDescription(): ?string
+    {
+        return $this->__('Last level of the Input Object containing another Input Objects, to test their validation is performed', 'dummy-schema');
+    }
+
+    /**
+     * @return array<string,InputTypeResolverInterface>
+     */
+    public function getInputFieldNameTypeResolvers(): array
+    {
+        return [
+            'name' => $this->getStringScalarTypeResolver(),
+        ];
+    }
+
+    public function getInputFieldDescription(string $inputFieldName): ?string
+    {
+        return match ($inputFieldName) {
+            'name' => $this->__('Random name', 'dummy-schema'),
+            default => parent::getInputFieldDefaultValue($inputFieldName),
+        };
+    }
+
+    public function getInputFieldTypeModifiers(string $inputFieldName): int
+    {
+        return match ($inputFieldName) {
+            'name' => SchemaTypeModifiers::MANDATORY,
+            default => parent::getInputFieldTypeModifiers($inputFieldName),
+        };
+    }
+}
