7c44848d97d74b2f0452fc96ff8790181f786b61
Leonardo Losoviz <leo@getpop.org>
2023-05-17 14:23:34 +0800

    PostContentInput

---
+++ layers/CMSSchema/packages/custompost-mutations/src/TypeResolvers/InputObjectType/CustomPostContentAsOneofInputObjectTypeResolver.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\CustomPostMutations\TypeResolvers\InputObjectType;
+
+use PoPCMSSchema\CommentMutations\Constants\MutationInputProperties;
+use PoPSchema\SchemaCommons\TypeResolvers\ScalarType\HTMLScalarTypeResolver;
+use PoP\ComponentModel\TypeResolvers\InputObjectType\AbstractOneofInputObjectTypeResolver;
+use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
+
+class CustomPostContentAsOneofInputObjectTypeResolver extends AbstractOneofInputObjectTypeResolver
+{
+    private ?HTMLScalarTypeResolver $htmlScalarTypeResolver = null;
+
+    final public function setHTMLScalarTypeResolver(HTMLScalarTypeResolver $htmlScalarTypeResolver): void
+    {
+        $this->htmlScalarTypeResolver = $htmlScalarTypeResolver;
+    }
+    final protected function getHTMLScalarTypeResolver(): HTMLScalarTypeResolver
+    {
+        /** @var HTMLScalarTypeResolver */
+        return $this->htmlScalarTypeResolver ??= $this->instanceManager->getInstance(HTMLScalarTypeResolver::class);
+    }
+
+    public function getTypeName(): string
+    {
+        return 'CustomPostContentInput';
+    }
+
+    /**
+     * @return array<string,InputTypeResolverInterface>
+     */
+    public function getInputFieldNameTypeResolvers(): array
+    {
+        return [
+            MutationInputProperties::HTML => $this->getHTMLScalarTypeResolver(),
+        ];
+    }
+
+    public function getInputFieldDescription(string $inputFieldName): ?string
+    {
+        return match ($inputFieldName) {
+            MutationInputProperties::HTML => $this->__('Use HTML as content for the custom post', 'custompost-mutations'),
+            default => parent::getInputFieldDescription($inputFieldName),
+        };
+    }
+}
