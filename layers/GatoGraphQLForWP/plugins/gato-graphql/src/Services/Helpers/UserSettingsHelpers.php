b81c93e458b5d4e3e74b3cf7bd93dcd4f4a093b0
Leonardo Losoviz <leo@getpop.org>
2023-05-18 16:49:42 +0800

    Moved in AbstractSchemaEntityBlockSchemaConfigurationExecuter

---
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/Helpers/UserSettingsHelpers.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Services\Helpers;
+
+use GatoGraphQL\GatoGraphQL\Constants\ModuleSettingOptions;
+use GatoGraphQL\GatoGraphQL\Facades\UserSettingsManagerFacade;
+use GatoGraphQL\GatoGraphQL\Settings\UserSettingsManagerInterface;
+use PoP\Root\Services\BasicServiceTrait;
+
+class UserSettingsHelpers
+{
+    use BasicServiceTrait;
+
+    private ?UserSettingsManagerInterface $userSettingsManager = null;
+
+    public function setUserSettingsManager(UserSettingsManagerInterface $userSettingsManager): void
+    {
+        $this->userSettingsManager = $userSettingsManager;
+    }
+    protected function getUserSettingsManager(): UserSettingsManagerInterface
+    {
+        return $this->userSettingsManager ??= UserSettingsManagerFacade::getInstance();
+    }
+
+    /**
+     * Return the default Settings for custom post IDs
+     * for some module
+     *
+     * @return int[]
+     */
+    public function getUserDefaultSettingCustomPostValueIDs(string $module): array
+    {
+        /** @var int[] */
+        return $this->getUserSettingsManager()->getSetting(
+            $module,
+            ModuleSettingOptions::DEFAULT_VALUE
+        );
+    }
+}
---
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/SchemaConfigurationExecuters/AbstractSchemaEntityBlockSchemaConfigurationExecuter.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Services\SchemaConfigurationExecuters;
+
+use GatoGraphQL\GatoGraphQL\Services\Helpers\UserSettingsHelpers;
+use GatoGraphQL\GatoGraphQL\Services\SchemaConfigurationExecuters\AbstractCustomizableConfigurationBlockSchemaConfigurationExecuter;
+use GatoGraphQL\GatoGraphQL\Services\SchemaConfigurators\SchemaEntityConfiguratorInterface;
+
+abstract class AbstractSchemaEntityBlockSchemaConfigurationExecuter extends AbstractCustomizableConfigurationBlockSchemaConfigurationExecuter
+{
+    private ?UserSettingsHelpers $userSettingsHelpers = null;
+
+    public function setUserSettingsHelpers(UserSettingsHelpers $userSettingsHelpers): void
+    {
+        $this->userSettingsHelpers = $userSettingsHelpers;
+    }
+    protected function getUserSettingsHelpers(): UserSettingsHelpers
+    {
+        /** @var UserSettingsHelpers */
+        return $this->userSettingsHelpers ??= $this->instanceManager->getInstance(UserSettingsHelpers::class);
+    }
+
+    protected function mustAlsoExecuteWhenSchemaConfigurationModuleIsDisabled(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @param array<string,mixed> $schemaConfigBlockDataItem
+     */
+    protected function doExecuteBlockSchemaConfiguration(array $schemaConfigBlockDataItem): void
+    {
+        $customPostIDs = $schemaConfigBlockDataItem['attrs'][$this->getCustomPostListsAttributeName()] ?? [];
+
+        /**
+         * Cast to int[]
+         */
+        $customPostIDs = array_map(
+            fn (int|string $item) => (int) $item,
+            $customPostIDs
+        );
+
+        /** @var int[] $customPostIDs */
+        $this->executeCustomPostListsBlockSchemaConfiguration($customPostIDs);
+    }
+
+    abstract protected function getCustomPostListsAttributeName(): string;
+
+    /**
+     * @param int[] $customPostIDs
+     */
+    protected function executeCustomPostListsBlockSchemaConfiguration(array $customPostIDs): void
+    {
+        if ($customPostIDs === []) {
+            return;
+        }
+        $customPostSchemaEntityConfigurator = $this->getSchemaEntityConfigurator();
+        foreach ($customPostIDs as $customPostID) {
+            $customPostSchemaEntityConfigurator->executeSchemaEntityConfiguration($customPostID);
+        }
+    }
+
+    abstract protected function getSchemaEntityConfigurator(): SchemaEntityConfiguratorInterface;
+
+    protected function executeNotCustomizedSchemaConfiguration(): void
+    {
+        /** @var string */
+        $enablingModule = $this->getEnablingModule();
+        $customPostIDs = $this->getUserSettingsHelpers()->getUserDefaultSettingCustomPostValueIDs(
+            $enablingModule,
+        );
+        $this->executeCustomPostListsBlockSchemaConfiguration($customPostIDs);
+    }
+}
