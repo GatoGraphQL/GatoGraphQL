4a4056d6557fda143995e8210318bd7d0d3097a0
Leonardo Losoviz <leo@getpop.org>
2023-05-19 17:30:44 +0800

    Removed schema-config-global-fields from blocks-pro

--- layers/GatoGraphQLForWP/plugins/gato-graphql/blocks-pro/schema-config-global-fields/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '2dec86380f9bd9fffa6705a90fb2808f');
