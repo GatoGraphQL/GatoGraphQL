f0cd28163aaad7da77aac6dc460a9213e1afc7c1
Leonardo Losoviz <leo@getpop.org>
2023-05-21 13:08:04 +0800

    Obtain the list of all the Gato GraphQL Extensions

--- layers/GatoGraphQLForWP/phpunit-plugins/gato-graphql-testing/src/Executers/BulkPluginActivationDeactivationExecuter.php
+++ layers/GatoGraphQLForWP/phpunit-plugins/gato-graphql-testing/src/Executers/BulkPluginActivationDeactivationExecuter.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace PHPUnitForGatoGraphQL\GatoGraphQLTesting\Executers;
 
+use GatoGraphQL\GatoGraphQL\Facades\Registries\ModuleRegistryFacade;
+use GatoGraphQL\GatoGraphQL\ModuleResolvers\Extensions\ExtensionModuleResolver;
+use GatoGraphQL\GatoGraphQL\ModuleResolvers\Extensions\ExtensionModuleResolverInterface;
+use GatoGraphQL\GatoGraphQL\PluginAppHooks;
+use GatoGraphQL\GatoGraphQL\PluginSkeleton\PluginLifecyclePriorities;
 use PHPUnitForGatoGraphQL\GatoGraphQLTesting\Constants\Actions;
 use PHPUnitForGatoGraphQL\GatoGraphQLTesting\Constants\Params;
+use PoP\Root\Facades\Instances\InstanceManagerFacade;
 use RuntimeException;
 
 /**
@@ -23,11 +29,21 @@ class BulkPluginActivationDeactivationExecuter
 
     public function __construct()
     {
+        \add_action(
+            PluginAppHooks::INITIALIZE_APP,
+            $this->maybeExecute(...),
+            PluginLifecyclePriorities::INITIALIZE_APP
+        );
+    }
+
+    protected function maybeExecute(): void
+    {
         // phpcs:disable SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable.DisallowedSuperGlobalVariable
         $actions = $_GET['actions'] ?? null;
         if ($actions === null || !is_array($actions)) {
             return;
         }
+
         /** @var string[] $actions */
         $executeBulkPluginActivation = in_array(Actions::EXECUTE_BULK_PLUGIN_ACTIVATION, $actions);
         $executeBulkPluginDeactivation = in_array(Actions::EXECUTE_BULK_PLUGIN_DEACTIVATION, $actions);
@@ -35,11 +51,27 @@ class BulkPluginActivationDeactivationExecuter
             return;
         }
 
+        // Obtain the list of all the Gato GraphQL Extensions
+        $moduleRegistry = ModuleRegistryFacade::getInstance();
+        $modules = $moduleRegistry->getAllModules(true, false, false);
+        /** @var string[] */
+        $gatoGraphQLExtensions = [];
+        foreach ($modules as $module) {
+            $moduleResolver = $moduleRegistry->getModuleResolver($module);
+            if (!($moduleResolver instanceof ExtensionModuleResolverInterface)) {
+                continue;
+            }
+            $gatoGraphQLExtensions = [
+                ...$gatoGraphQLExtensions,
+                $moduleResolver->getModulesToResolve()
+            ];
+        }
+
         if ($executeBulkPluginDeactivation) {
-            $this->executeBulkPluginDeactivation();
+            $this->executeBulkPluginDeactivation($gatoGraphQLExtensions);
             $message = \__('Plugins deactivated successfully');
         } else {
-            $this->executeBulkPluginActivation();
+            $this->executeBulkPluginActivation($gatoGraphQLExtensions);
             $message = \__('Plugins activated successfully');
         }
         
