64c813e09e7d1c19f6d424d0f1be0cc6082bc877
Leonardo Losoviz <leo@getpop.org>
2023-05-22 15:55:16 +0800

    restored global fields block priority

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/Blocks/SchemaConfigGlobalFieldsBlock.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/Blocks/SchemaConfigGlobalFieldsBlock.php
@@ -26,7 +26,7 @@ class SchemaConfigGlobalFieldsBlock extends AbstractSchemaConfigBlock
 
     public function getBlockPriority(): int
     {
-        return 10115;
+        return 10090;
     }
 
     public function getEnablingModule(): ?string
