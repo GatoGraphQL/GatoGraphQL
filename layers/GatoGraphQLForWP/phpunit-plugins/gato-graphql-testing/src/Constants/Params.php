0f9938a08eab925615454d8ec8322968ee7ddaac
Leonardo Losoviz <leo@getpop.org>
2023-05-29 15:26:42 +0800

    Moved consts

--- layers/GatoGraphQLForWP/phpunit-plugins/gato-graphql-testing/src/Constants/Params.php
+++ layers/GatoGraphQLForWP/phpunit-plugins/gato-graphql-testing/src/Constants/Params.php
@@ -6,9 +6,5 @@ namespace PHPUnitForGatoGraphQL\GatoGraphQLTesting\Constants;
 
 class Params
 {
-    public const PERSISTED_QUERY_ID = 'persisted_query_id';
-    public const PERSISTED_QUERY_SLUG = 'persisted_query_slug';
-    public const TIMESTAMP = 'timestamp';
-
     public const SKIP_DEACTIVATING_PLUGIN_FILES = 'skip_deactivating_plugin_files';
 }
