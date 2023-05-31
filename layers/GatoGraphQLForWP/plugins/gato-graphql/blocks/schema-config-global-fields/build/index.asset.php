4c753fa843fd6d9f0f5eb226c24a5e397dfe94e4
Leonardo Losoviz <leo@getpop.org>
2023-05-31 10:17:08 +0800

    npm run build

--- layers/GatoGraphQLForWP/plugins/gato-graphql/blocks/schema-config-global-fields/build/index.asset.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/blocks/schema-config-global-fields/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '6a3d4c1db60ed3e40cf79fed237b893b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '074215c7644bd3c922a03e0802cc8e12');
