103d76e83474362cd9e4ebf42c8d6487aab74897
Leonardo Losoviz <leo@getpop.org>
2023-05-31 09:48:10 +0800

    removed Gato GraphQL PRO extension

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/ModuleResolvers/Extensions/ExtensionModuleResolver.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/ModuleResolvers/Extensions/ExtensionModuleResolver.php
@@ -11,7 +11,6 @@ use GatoGraphQL\GatoGraphQL\Plugin;
 
 class ExtensionModuleResolver extends AbstractExtensionModuleResolver
 {
-    public const GATO_GRAPHQL_PRO = Plugin::NAMESPACE . '\\extensions\\gato-graphql-pro';
     public const ACCESS_CONTROL = Plugin::NAMESPACE . '\\extensions\\access-control';
     public const ACCESS_CONTROL_VISITOR_IP = Plugin::NAMESPACE . '\\extensions\\access-control-visitor-ip';
     public const AUTOMATION = Plugin::NAMESPACE . '\\extensions\\automation';
@@ -46,7 +45,6 @@ class ExtensionModuleResolver extends AbstractExtensionModuleResolver
     public function getModulesToResolve(): array
     {
         return [
-            self::GATO_GRAPHQL_PRO,
             self::ACCESS_CONTROL,
             self::ACCESS_CONTROL_VISITOR_IP,
             self::AUTOMATION,
@@ -80,7 +78,6 @@ class ExtensionModuleResolver extends AbstractExtensionModuleResolver
     public function getName(string $module): string
     {
         return match ($module) {
-            self::GATO_GRAPHQL_PRO => \__('Gato GraphQL PRO', 'gato-graphql'),
             self::ACCESS_CONTROL => \__('Access Control', 'gato-graphql'),
             self::ACCESS_CONTROL_VISITOR_IP => \__('Access Control: Visitor IP', 'gato-graphql'),
             self::AUTOMATION => \__('Automation', 'gato-graphql'),
@@ -115,7 +112,6 @@ class ExtensionModuleResolver extends AbstractExtensionModuleResolver
     public function getDescription(string $module): string
     {
         return match ($module) {
-            self::GATO_GRAPHQL_PRO => \__('Superpower your app with PRO features.', 'gato-graphql'),
             self::ACCESS_CONTROL => \__('Grant user access to schema elements via Access Control Lists.', 'gato-graphql'),
             self::ACCESS_CONTROL_VISITOR_IP => \__('Grant access to schema elements based on the visitor\'s IP address (Access Control extension is rquired).', 'gato-graphql'),
             self::AUTOMATION => \__('Automate tasks by chaining GraphQL queries via hooks and triggering them via WP-Cron.', 'gato-graphql'),
@@ -147,35 +143,9 @@ class ExtensionModuleResolver extends AbstractExtensionModuleResolver
         };
     }
 
-    public function getGatoGraphQLExtensionSlug(string $module): string
-    {
-        return match ($module) {
-            self::GATO_GRAPHQL_PRO => $this->getSlug($module),
-            default => parent::getGatoGraphQLExtensionSlug($module),
-        };
-    }
-
-    public function getWebsiteURL(string $module): string
-    {
-        if ($module === self::GATO_GRAPHQL_PRO) {
-            /** @var ModuleConfiguration */
-            $moduleConfiguration = App::getModule(Module::class)->getConfiguration();
-            return sprintf(
-                '%s/pro',
-                $moduleConfiguration->getGatoGraphQLWebsiteURL()
-            );
-        }
-
-        return parent::getWebsiteURL($module);
-    }
-
     public function getLogoURL(string $module): string
     {
         $logoURL = parent::getLogoURL($module);
-        if ($module === self::GATO_GRAPHQL_PRO) {
-            return $logoURL;
-        }
-
         return str_replace(
             'GatoGraphQL-logo.png',
             'GatoGraphQL-logo2.png',
