f73e31b74733a47a10b6fe01063b0ace2d2eabc1
Leonardo Losoviz <leo@getpop.org>
2023-06-01 05:38:56 +0800

    getGatoGraphQLPluginCustomPostTypes

--- layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Integration/WPAdminPageWebserverRequestTestCase.php
+++ layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Integration/WPAdminPageWebserverRequestTestCase.php
@@ -37,6 +37,14 @@ class WPAdminPageWebserverRequestTestCase extends AbstractWPAdminPageWebserverRe
                 ),
             ];
         }
+        foreach ($this->getGatoGraphQLPluginCustomPostTypes() as $customPostType) {
+            $entries[$customPostType] = [
+                sprintf(
+                    'wp-admin/edit.php?post_type=%s',
+                    $customPostType
+                ),
+            ];
+        }
         return $entries;
     }
 
@@ -60,6 +68,24 @@ class WPAdminPageWebserverRequestTestCase extends AbstractWPAdminPageWebserverRe
             'recipes',
             'settings',
             'voyager',
+        ];
+    }
+
+    /**
+     * Provide all the Custom Post Types registered by Gato GraphQL.
+     * These are under function `getCustomPostType` from `CustomPostTypeInterface`,
+     * such as `GraphQLPersistedQueryEndpointCustomPostType`.
+     *
+     * @see layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/CustomPostTypes/GraphQLPersistedQueryEndpointCustomPostType.php
+     * 
+     * @return string[]
+     */
+    protected function getGatoGraphQLPluginCustomPostTypes(): array
+    {
+        return [
+            'graphql-endpoint',
+            'graphql-query',
+            'graphql-schemaconfig',
         ];
     }
 }
