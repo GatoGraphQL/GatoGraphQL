b9110758c7152ac4449b51365712b365c4d288bc
Leonardo Losoviz <leo@getpop.org>
2023-06-01 05:27:52 +0800

    getGatoGraphQLPluginMenuPageSlugs

--- layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Integration/WPAdminPageWebserverRequestTestCase.php
+++ layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Integration/WPAdminPageWebserverRequestTestCase.php
@@ -9,12 +9,36 @@ use PHPUnitForGatoGraphQL\WebserverRequests\AbstractWPAdminPageWebserverRequestT
 class WPAdminPageWebserverRequestTestCase extends AbstractWPAdminPageWebserverRequestTestCase
 {
     /**
+     * Test that all pages added by the plugin produce
+     * a 200 status code.
+     *
      * @return array<string,string[]>
      */
     protected function providePageEntries(): array
     {
-        return [
+        $entries = [];
+        foreach ($this->getGatoGraphQLPluginMenuPageSlugs() as $pageSlug) {
+            $entries[$pageSlug] = sprintf(
+                'wp-admin/edit.php?page=gato_graphql_%s',
+                $pageSlug
+            );
+        }
+        return $entries;
+    }
 
+    /**
+     * Provide all the MenuPageSlug registered by Gato GraphQL.
+     * These are under function `getMenuPageSlug` from `MenuPageInterface`,
+     * such as `AboutMenuPage`.
+     *
+     * @see layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/MenuPages/AboutMenuPage.php
+     * 
+     * @return string[]
+     */
+    protected function getGatoGraphQLPluginMenuPageSlugs(): array
+    {
+        return [
+            'about',
         ];
     }
 }
