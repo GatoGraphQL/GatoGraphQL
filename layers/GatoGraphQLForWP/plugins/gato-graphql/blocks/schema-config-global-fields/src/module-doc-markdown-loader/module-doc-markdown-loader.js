6ac74759cc76478e2111e5e19ee12f1895009266
Leonardo Losoviz <leo@getpop.org>
2023-05-31 10:18:12 +0800

    fixed bug

--- layers/GatoGraphQLForWP/plugins/gato-graphql/blocks/schema-config-global-fields/src/module-doc-markdown-loader/module-doc-markdown-loader.js
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/blocks/schema-config-global-fields/src/module-doc-markdown-loader/module-doc-markdown-loader.js
@@ -2,7 +2,7 @@
  * Path to load the lazy chunks on the fly
  * @see https://v4.webpack.js.org/guides/public-path/#on-the-fly
  */
-__webpack_public_path__ = window.gatoGraphQLSchemaConfigGlobalFields?.publicPath;
+__webpack_public_path__ = window.gatoGraphqlSchemaConfigGlobalFields?.publicPath;
 
 /**
  * Read the content from a Markdown file in a given language, and return it as HTML
@@ -22,8 +22,8 @@ const getModuleDocMarkdownContent = ( lang ) => {
  * @param {string|null} lang The language to translate to. If none provided, get it from the localized data
  */
 const getModuleDocMarkdownContentOrUseDefault = ( defaultLang, lang ) => {
-	lang = lang || window.gatoGraphQLSchemaConfigGlobalFields?.localeLang
-	defaultLang = defaultLang || window.gatoGraphQLSchemaConfigGlobalFields?.defaultLang
+	lang = lang || window.gatoGraphqlSchemaConfigGlobalFields?.localeLang
+	defaultLang = defaultLang || window.gatoGraphqlSchemaConfigGlobalFields?.defaultLang
 	return getModuleDocMarkdownContent( lang )
 		.catch(err => getModuleDocMarkdownContent( defaultLang ) )
 }
