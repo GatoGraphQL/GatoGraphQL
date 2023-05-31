d09bafd950a7591c052a8f317df43cda390422c6
Leonardo Losoviz <leo@getpop.org>
2023-06-01 05:21:11 +0800

    added class

---
+++ layers/GatoGraphQLForWP/phpunit-packages/gato-graphql/tests/Integration/WPAdminPageWebserverRequestTestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitForGatoGraphQL\GatoGraphQL\Integration;
+
+use PHPUnitForGatoGraphQL\WebserverRequests\AbstractWPAdminPageWebserverRequestTestCase;
+
+class WPAdminPageWebserverRequestTestCase extends AbstractWPAdminPageWebserverRequestTestCase
+{
+    /**
+     * @return array<string,string[]>
+     */
+    protected function providePageEntries(): array
+    {
+        return [
+
+        ];
+    }
+}
