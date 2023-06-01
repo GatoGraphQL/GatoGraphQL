9c8f6706659ee8d9a1b5adede7762533a6dc7031
Leonardo Losoviz <leo@getpop.org>
2023-06-01 10:18:54 +0800

    convert to slugs

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginStaticHelpers.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginStaticHelpers.php
@@ -11,7 +11,7 @@ class PluginStaticHelpers
     /**
      * @var string[]|null
      */
-    private static ?array $activeWordPressPluginFiles = null;
+    private static ?array $activeWordPressPluginSlugs = null;
 
     public static function getGitHubRepoDocsRootPathURL(): string
     {
@@ -22,11 +22,22 @@ class PluginStaticHelpers
         return 'https://raw.githubusercontent.com/leoloso/PoP/' . $tag . '/layers/GatoGraphQLForWP/plugins/gato-graphql/';
     }
 
-    public static function isWordPressPluginActive(string $pluginFile): bool
+    public static function isWordPressPluginActive(string $pluginSlug): bool
     {
-        if (self::$activeWordPressPluginFiles === null) {
-            self::$activeWordPressPluginFiles = apply_filters('active_plugins', get_option('active_plugins', []));
+        if (self::$activeWordPressPluginSlugs === null) {
+            $activeWordPressPluginFiles = apply_filters('active_plugins', get_option('active_plugins', []));
+            self::$activeWordPressPluginSlugs = array_map(
+                function (string $pluginFile): string
+                {
+                    $pos = strpos($pluginFile, '/');
+                    if ($pos === false) {
+                        return $pluginFile;
+                    }
+                    return substr($pluginFile, 0, $pos + 1);
+                },
+                $activeWordPressPluginFiles
+            );
         }
-        return in_array($pluginFile, self::$activeWordPressPluginFiles);
+        return in_array($pluginSlug, self::$activeWordPressPluginSlugs);
     }
 }
