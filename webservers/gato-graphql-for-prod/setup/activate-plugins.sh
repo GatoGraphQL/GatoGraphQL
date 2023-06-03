6dbb29a3c104635fce12647ab8ea36f3de19bc87
Leonardo Losoviz <leo@getpop.org>
2023-06-03 19:39:03 +0800

    Revert "Execute all "wp plugin activate" with 1 command"
    This reverts commit 7927279e904d89dac5f8dce3df3d8ae2f9855a2f.

--- webservers/gato-graphql-for-prod/setup/activate-plugins.sh
+++ webservers/gato-graphql-for-prod/setup/activate-plugins.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 # Download and activate external plugins
 wp plugin install wordpress-importer --activate --path=/app/wordpress
-# Install own plugins
-if ! wp plugin is-installed gato-graphql --path=/app/wordpress; then
+# Install/activate own plugins
+if wp plugin is-installed gato-graphql --path=/app/wordpress; then
+    wp plugin activate gato-graphql --path=/app/wordpress
+else
     wp plugin install https://github.com/leoloso/PoP/releases/latest/download/gato-graphql.zip --force --activate --path=/app/wordpress
 fi
-if ! wp plugin is-installed gato-graphql-testing-schema --path=/app/wordpress; then
+if wp plugin is-installed gato-graphql-testing-schema --path=/app/wordpress; then
+    wp plugin activate gato-graphql-testing-schema --path=/app/wordpress
+else
     wp plugin install https://github.com/leoloso/PoP/releases/latest/download/gato-graphql-testing-schema.zip --force --activate --path=/app/wordpress
 fi
-if ! wp plugin is-installed gato-graphql-testing --path=/app/wordpress; then
+if wp plugin is-installed gato-graphql-testing --path=/app/wordpress; then
+    wp plugin activate gato-graphql-testing --path=/app/wordpress
+else
     wp plugin install https://github.com/leoloso/PoP/releases/latest/download/gato-graphql-testing.zip --force --activate --path=/app/wordpress
 fi
-# Activate own plugins
-wp plugin activate gato-graphql gato-graphql-testing-schema gato-graphql-testing --path=/app/wordpress
--- webservers/gato-graphql/setup/activate-plugins.sh
+++ webservers/gato-graphql/setup/activate-plugins.sh
@@ -2,4 +2,6 @@
 # Download and activate external plugins
 wp plugin install wordpress-importer --activate --path=/app/wordpress
 # Activate own plugins
-wp plugin activate gato-graphql gato-graphql-testing-schema gato-graphql-testing --path=/app/wordpress
+wp plugin activate gato-graphql --path=/app/wordpress
+wp plugin activate gato-graphql-testing-schema --path=/app/wordpress
+wp plugin activate gato-graphql-testing --path=/app/wordpress
