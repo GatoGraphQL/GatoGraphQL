65ca1529cf45c822c0fc2836403b8f79d9e1e43a
Leonardo Losoviz <leo@getpop.org>
2023-06-05 17:38:48 +0800

    Added Block fields

---
+++ layers/WPSchema/packages/blocks/src/FieldResolvers/ObjectType/BlockObjectTypeFieldResolver.php
@@ -0,0 +1,139 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Blocks\FieldResolvers\ObjectType;
+
+use PoPWPSchema\Blocks\ObjectModels\BlockInterface;
+use PoPWPSchema\Blocks\TypeResolvers\ObjectType\BlockObjectTypeResolver;
+use PoPWPSchema\Blocks\TypeResolvers\UnionType\BlockUnionTypeResolver;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
+use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractObjectTypeFieldResolver;
+use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\TypeResolvers\ConcreteTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
+use PoP\Engine\TypeResolvers\ScalarType\JSONObjectScalarTypeResolver;
+use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
+
+class BlockObjectTypeFieldResolver extends AbstractObjectTypeFieldResolver
+{
+    private ?JSONObjectScalarTypeResolver $jsonObjectScalarTypeResolver = null;
+    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
+    private ?BlockUnionTypeResolver $blockUnionTypeResolver = null;
+
+    final public function setJSONObjectScalarTypeResolver(JSONObjectScalarTypeResolver $jsonObjectScalarTypeResolver): void
+    {
+        $this->jsonObjectScalarTypeResolver = $jsonObjectScalarTypeResolver;
+    }
+    final protected function getJSONObjectScalarTypeResolver(): JSONObjectScalarTypeResolver
+    {
+        /** @var JSONObjectScalarTypeResolver */
+        return $this->jsonObjectScalarTypeResolver ??= $this->instanceManager->getInstance(JSONObjectScalarTypeResolver::class);
+    }
+    final public function setStringScalarTypeResolver(StringScalarTypeResolver $stringScalarTypeResolver): void
+    {
+        $this->stringScalarTypeResolver = $stringScalarTypeResolver;
+    }
+    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
+    {
+        /** @var StringScalarTypeResolver */
+        return $this->stringScalarTypeResolver ??= $this->instanceManager->getInstance(StringScalarTypeResolver::class);
+    }
+    final public function setBlockUnionTypeResolver(BlockUnionTypeResolver $blockUnionTypeResolver): void
+    {
+        $this->blockUnionTypeResolver = $blockUnionTypeResolver;
+    }
+    final protected function getBlockUnionTypeResolver(): BlockUnionTypeResolver
+    {
+        /** @var BlockUnionTypeResolver */
+        return $this->blockUnionTypeResolver ??= $this->instanceManager->getInstance(BlockUnionTypeResolver::class);
+    }
+
+    /**
+     * @return array<class-string<ObjectTypeResolverInterface>>
+     */
+    public function getObjectTypeResolverClassesToAttachTo(): array
+    {
+        return [
+            BlockObjectTypeResolver::class,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getFieldNamesToResolve(): array
+    {
+        return [
+            'name',
+            'attributes',
+            'innerBlocks',
+        ];
+    }
+
+    public function getFieldTypeResolver(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ConcreteTypeResolverInterface
+    {
+        return match ($fieldName) {
+            'name' => $this->getStringScalarTypeResolver(),
+            'attributes' => $this->getJSONObjectScalarTypeResolver(),
+            'innerBlocks' => $this->getBlockUnionTypeResolver(),
+            default => parent::getFieldTypeResolver($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldTypeModifiers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): int
+    {
+        return match ($fieldName) {
+            'name' => SchemaTypeModifiers::NON_NULLABLE,
+            'innerBlocks' => SchemaTypeModifiers::IS_ARRAY | SchemaTypeModifiers::IS_NON_NULLABLE_ITEMS_IN_ARRAY,
+            default => parent::getFieldTypeModifiers($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
+    {
+        return match ($fieldName) {
+            'name' => $this->__('Block name', 'blocks'),
+            'attributes' => $this->__('Block attributes, parsed to the type declared in their block.json schema', 'blocks'),
+            'innerBlocks' => $this->__('Block\'s inner blocks (if suitable)', 'blocks'),
+            default => parent::getFieldDescription($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function resolveValue(
+        ObjectTypeResolverInterface $objectTypeResolver,
+        object $object,
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): mixed {
+        /** @var BlockInterface */
+        $block = $object;
+        $fieldName = $fieldDataAccessor->getFieldName();
+        switch ($fieldName) {
+            case 'innerBlocks':
+                $innerBlocks = $block->getInnerBlocks();
+                if ($innerBlocks === null) {
+                    return null;
+                }
+                return array_map(
+                    fn (BlockInterface $block) => $block->getID(),
+                    $innerBlocks
+                );
+        }
+
+        return parent::resolveValue($objectTypeResolver, $object, $fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
+    }
+
+    /**
+     * Since the return type is known for all the fields in this
+     * FieldResolver, there's no need to validate them
+     */
+    public function validateResolvedFieldType(
+        ObjectTypeResolverInterface $objectTypeResolver,
+        FieldInterface $field,
+    ): bool {
+        return false;
+    }
+}
