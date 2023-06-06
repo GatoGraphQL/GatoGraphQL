cf8948806dfe0cf426e9dbdce877935e541adef2
Leonardo Losoviz <leo@getpop.org>
2023-06-06 08:30:05 +0800

    Added `parse` to interface

--- layers/WPSchema/packages/block-content-parser/src/BlockContentParser.php
+++ layers/WPSchema/packages/block-content-parser/src/BlockContentParser.php
@@ -6,6 +6,7 @@ namespace PoPWPSchema\BlockContentParser;
 
 use PoP\DOMCrawler\CrawlerFactoryInterface;
 use PoP\Root\Services\BasicServiceTrait;
+use WP_Error;
 
 class BlockContentParser implements BlockContentParserInterface
 {
@@ -21,5 +22,22 @@ class BlockContentParser implements BlockContentParserInterface
     {
         /** @var CrawlerFactoryInterface */
         return $this->crawlerFactory ??= $this->instanceManager->getInstance(CrawlerFactoryInterface::class);
+    }
+
+    /**
+	 * @param string $customPostContent HTML content of a post.
+	 * @param int|null $customPostID ID of the post being parsed. Required for blocks containing meta-sourced attributes and some block filters.
+	 * @param array<string,mixed> $filterOptions An associative array of options for filtering blocks. Can contain keys:
+	 *              'exclude': An array of block names to block from the response.
+	 *              'include': An array of block names that are allowed in the response.
+	 *
+	 * @return array<string,mixed>|WP_Error
+	 */
+	public function parse(
+        string $customPostContent,
+        ?int $customPostID = null,
+        array $filterOptions = [],
+    ): array|WP_Error {
+        return [];
     }
 }
--- layers/WPSchema/packages/block-content-parser/src/BlockContentParserInterface.php
+++ layers/WPSchema/packages/block-content-parser/src/BlockContentParserInterface.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace PoPWPSchema\BlockContentParser;
 
+use WP_Error;
+
 interface BlockContentParserInterface
 {
+    /**
+	 * @param string $customPostContent HTML content of a post.
+	 * @param int|null $customPostID ID of the post being parsed. Required for blocks containing meta-sourced attributes and some block filters.
+	 * @param array<string,mixed> $filterOptions An associative array of options for filtering blocks. Can contain keys:
+	 *              'exclude': An array of block names to block from the response.
+	 *              'include': An array of block names that are allowed in the response.
+	 *
+	 * @return array<string,mixed>|WP_Error
+	 */
+	public function parse(
+        string $customPostContent,
+        ?int $customPostID = null,
+        array $filterOptions = [],
+    ): array|WP_Error;
 }
