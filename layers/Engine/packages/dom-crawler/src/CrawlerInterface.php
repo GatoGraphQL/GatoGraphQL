7f7dd8d34b917cb9110d9124430750edf9ee5757
Leonardo Losoviz <leo@getpop.org>
2023-06-06 08:00:25 +0800

    interface

--- layers/Engine/packages/dom-crawler/src/CrawlerInterface.php
+++ layers/Engine/packages/dom-crawler/src/CrawlerInterface.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 
 namespace PoP\DOMCrawler;
 
+use DOMNodeList;
+use DOMNode;
+use Symfony\Component\DomCrawler\Crawler;
+
 interface CrawlerInterface
 {
+    /**
+     * @param DOMNodeList|DOMNode|DOMNode[]|string|null $node A Node to use as the base for the crawling
+     */
+    public function createCrawler(
+        DOMNodeList|DOMNode|array|string $node = null,
+        string $uri = null,
+        string $baseHref = null,
+        bool $useHtml5Parser = true,
+    ): Crawler;
 }
