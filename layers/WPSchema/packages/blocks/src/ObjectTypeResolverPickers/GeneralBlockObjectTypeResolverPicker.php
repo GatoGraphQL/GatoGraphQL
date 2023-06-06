4ac6d8a35f3589bf67a08f9e892860b1b83da0ea
Leonardo Losoviz <leo@getpop.org>
2023-06-06 18:30:10 +0800

    fixed bug

--- layers/WPSchema/packages/blocks/src/ObjectTypeResolverPickers/GeneralBlockObjectTypeResolverPicker.php
+++ layers/WPSchema/packages/blocks/src/ObjectTypeResolverPickers/GeneralBlockObjectTypeResolverPicker.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoPWPSchema\Blocks\ObjectTypeResolverPickers;
 
+use PoPWPSchema\Blocks\ObjectModels\GeneralBlock;
 use PoPWPSchema\Blocks\TypeResolvers\ObjectType\GeneralBlockObjectTypeResolver;
 use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
 
