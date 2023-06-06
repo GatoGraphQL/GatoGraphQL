ca0ba95be7378b9c746bd653d6600c8620472893
Leonardo Losoviz <leo@getpop.org>
2023-06-06 20:33:18 +0800

    fixed type

--- layers/Engine/packages/dom-crawler/src/CrawlerFactory.php
+++ layers/Engine/packages/dom-crawler/src/CrawlerFactory.php
@@ -13,7 +13,7 @@ class CrawlerFactory implements CrawlerFactoryInterface
      * @param DOMNodeList|DOMNode|DOMNode[]|string|null $node A Node to use as the base for the crawling
      */
     public function createCrawler(
-        DOMNodeList|DOMNode|array|string $node = null,
+        null|DOMNodeList|DOMNode|array|string $node = null,
         string $uri = null,
         string $baseHref = null,
         bool $useHtml5Parser = true,
