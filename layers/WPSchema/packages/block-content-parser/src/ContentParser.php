f103c061a854ba98f4ef5b519a64e1957264350d
Leonardo Losoviz <leo@getpop.org>
2023-06-06 08:17:22 +0800

    moved service

--- layers/WPSchema/packages/block-content-parser/src/ContentParser.php
+++ layers/WPSchema/packages/block-content-parser/src/ContentParser.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace PoPWPSchema\BlockContentParser;
 
+use PoP\DOMCrawler\CrawlerFactoryInterface;
+use PoP\Root\Services\BasicServiceTrait;
+
 class ContentParser implements ContentParserInterface
 {
+    use BasicServiceTrait;
+    
+    private ?CrawlerFactoryInterface $crawlerFactory = null;
+    
+    final public function setCrawlerFactory(CrawlerFactoryInterface $crawlerFactory): void
+    {
+        $this->crawlerFactory = $crawlerFactory;
+    }
+    final protected function getCrawlerFactory(): CrawlerFactoryInterface
+    {
+        /** @var CrawlerFactoryInterface */
+        return $this->crawlerFactory ??= $this->instanceManager->getInstance(CrawlerFactoryInterface::class);
+    }
 }
