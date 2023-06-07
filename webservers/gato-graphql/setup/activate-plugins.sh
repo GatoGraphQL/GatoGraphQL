cfe0928c61febb5453cedb5cd7cb4430897c0795
Leonardo Losoviz <leo@getpop.org>
2023-06-07 13:01:14 +0800

    Install Classic Press

--- webservers/gato-graphql/setup/activate-plugins.sh
+++ webservers/gato-graphql/setup/activate-plugins.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-# Download and activate external plugins
+# Download and maybe activate external plugins
 wp plugin install wordpress-importer --activate --path=/app/wordpress
+wp plugin install classic-editor --path=/app/wordpress
+
 # Activate own plugins
 wp plugin activate gato-graphql --path=/app/wordpress
 wp plugin activate gato-graphql-testing-schema --path=/app/wordpress
