3e586bf054baf36e4547b1e8c9787a3ad24bea5f
Leonardo Losoviz <leo@getpop.org>
2023-06-07 11:07:38 +0800

    npm run build

--- layers/GatoGraphQLForWP/plugins/testing-schema/blocks/not-server-registered-schema-testing/build/index.asset.php
+++ layers/GatoGraphQLForWP/plugins/testing-schema/blocks/not-server-registered-schema-testing/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'bd427c48b3ef0ae50232acc93d8fa753');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ff3a40d67a464ccd2ca67a5fae3caa98');
