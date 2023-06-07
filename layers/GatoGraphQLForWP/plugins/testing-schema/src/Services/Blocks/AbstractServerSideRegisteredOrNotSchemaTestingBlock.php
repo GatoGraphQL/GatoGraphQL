6a6aebd27614d547c7be63068c5e48c33c7ebec8
Leonardo Losoviz <leo@getpop.org>
2023-06-07 11:49:13 +0800

    abstract class

---
+++ layers/GatoGraphQLForWP/plugins/testing-schema/src/Services/Blocks/AbstractServerSideRegisteredOrNotSchemaTestingBlock.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\TestingSchema\Services\Blocks;
+
+use GatoGraphQL\GatoGraphQL\Services\Blocks\AbstractBlock;
+
+abstract class AbstractServerSideRegisteredOrNotSchemaTestingBlock extends AbstractBlock
+{
+    use ExtensionBlockTrait;
+
+    protected function isDynamicBlock(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @param array<string,mixed> $attributes
+     */
+    public function renderBlock(array $attributes, string $content): string
+    {
+        // Append "-front" because this style must be used only on the client, not on the admin
+        $className = $this->getBlockClassName() . '-front';
+
+        $blockContentPlaceholder = '<p><strong>%s</strong></p><p>%s</p>';
+        $blockContent = sprintf(
+            $blockContentPlaceholder,
+            \__('This is a block for testing the schema', 'gato-graphql-testing-schema'),
+            \__('In particular, to test field <code>CustomPost.blocks</code>, to see that blocks not registered on the server-side display a warning when parsed.', 'gato-graphql-testing-schema'),
+        );
+
+        $blockContentPlaceholder = <<<EOT
+            <div class="%s">
+                <h3 class="%s">%s</h3>
+                %s
+            </div>
+        EOT;
+        return sprintf(
+            $blockContentPlaceholder,
+            $className . ' ' . $this->getAlignClassName(),
+            $className . '__title',
+            \__('Gato GraphQL: Block for testing the schema', 'gato-graphql-testing-schema'),
+            $blockContent
+        );
+    }
+}
--- layers/GatoGraphQLForWP/plugins/testing-schema/src/Services/Blocks/NotServerSideRegisteredSchemaTestingBlock.php
+++ layers/GatoGraphQLForWP/plugins/testing-schema/src/Services/Blocks/NotServerSideRegisteredSchemaTestingBlock.php
@@ -6,10 +6,8 @@ namespace GatoGraphQL\TestingSchema\Services\Blocks;
 
 use GatoGraphQL\GatoGraphQL\Services\Blocks\AbstractBlock;
 
-class NotServerSideRegisteredSchemaTestingBlock extends AbstractBlock
+class NotServerSideRegisteredSchemaTestingBlock extends AbstractServerSideRegisteredOrNotSchemaTestingBlock
 {
-    use ExtensionBlockTrait;
-
     protected function getBlockName(): string
     {
         return 'not-server-side-registered-schema-testing';
@@ -18,40 +16,5 @@ class NotServerSideRegisteredSchemaTestingBlock extends AbstractBlock
     protected function registerBlockServerSide(): bool
     {
         return false;
-    }
-
-    protected function isDynamicBlock(): bool
-    {
-        return true;
-    }
-
-    /**
-     * @param array<string,mixed> $attributes
-     */
-    public function renderBlock(array $attributes, string $content): string
-    {
-        // Append "-front" because this style must be used only on the client, not on the admin
-        $className = $this->getBlockClassName() . '-front';
-
-        $blockContentPlaceholder = '<p><strong>%s</strong></p><p>%s</p>';
-        $blockContent = sprintf(
-            $blockContentPlaceholder,
-            \__('This is a block for testing the schema', 'gato-graphql-testing-schema'),
-            \__('In particular, to test field <code>CustomPost.blocks</code>, to see that blocks not registered on the server-side display a warning when parsed.', 'gato-graphql-testing-schema'),
-        );
-
-        $blockContentPlaceholder = <<<EOT
-            <div class="%s">
-                <h3 class="%s">%s</h3>
-                %s
-            </div>
-        EOT;
-        return sprintf(
-            $blockContentPlaceholder,
-            $className . ' ' . $this->getAlignClassName(),
-            $className . '__title',
-            \__('Interactive Schema', 'gato-graphql-testing-schema'),
-            $blockContent
-        );
     }
 }
