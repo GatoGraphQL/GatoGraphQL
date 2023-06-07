03a3690113a2ceda098343dc18ad459451012e41
Leonardo Losoviz <leo@getpop.org>
2023-06-07 11:51:02 +0800

    duplicated block

---
+++ layers/GatoGraphQLForWP/plugins/testing-schema/blocks/server-side-registered-block-schema-testing/webpack.config.js
@@ -0,0 +1,14 @@
+const config = require( '@wordpress/scripts/config/webpack.config' );
+
+/**
+ * Because the block and the package have their own webpack configuration,
+ * they must provide a unique name for the global scope (which is used to lazy-load chunks),
+ * otherwise it throws a JS error when loading blocks compiled with `npm run build`
+ * @see https://github.com/WordPress/gutenberg/issues/23607
+ * @see https://webpack.js.org/configuration/output/#outputjsonpfunction
+ */
+// ------------------------------------------------------
+config.output.jsonpFunction = 'webpackJsonpGatoGraphQLTestingSchemaNotServerSideRegisteredBlock';
+// ------------------------------------------------------
+
+module.exports = config;
