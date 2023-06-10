e30afd944a0d1ba1568767aa6f861a2bda31c382
Leonardo Losoviz <leo@getpop.org>
2023-06-10 18:21:20 +0800

    Created FeedbackItemResolution in ComponentModel

---
+++ layers/Engine/packages/component-model/src/Feedback/FeedbackItemResolution.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\Feedback;
+
+use PoP\Root\Feedback\FeedbackItemResolution as UpstreamFeedbackItemResolution;
+
+class FeedbackItemResolution extends UpstreamFeedbackItemResolution
+{
+}
