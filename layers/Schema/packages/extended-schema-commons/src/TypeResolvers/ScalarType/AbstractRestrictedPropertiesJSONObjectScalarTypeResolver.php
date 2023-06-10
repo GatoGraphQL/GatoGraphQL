6a9a10c02686fdf5ddcaa37e2585751aa64960b2
Leonardo Losoviz <leo@getpop.org>
2023-06-10 11:22:45 +0800

    getRestrictedProperties

--- layers/Schema/packages/extended-schema-commons/src/TypeResolvers/ScalarType/AbstractRestrictedPropertiesJSONObjectScalarTypeResolver.php
+++ layers/Schema/packages/extended-schema-commons/src/TypeResolvers/ScalarType/AbstractRestrictedPropertiesJSONObjectScalarTypeResolver.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace PoPSchema\ExtendedSchemaCommons\TypeResolvers\ScalarType;
 
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\Engine\TypeResolvers\ScalarType\JSONObjectScalarTypeResolver;
+use PoP\GraphQLParser\Spec\Parser\Ast\AstInterface;
+use stdClass;
 
 abstract class AbstractRestrictedPropertiesJSONObjectScalarTypeResolver extends JSONObjectScalarTypeResolver
 {
@@ -14,10 +17,15 @@ abstract class AbstractRestrictedPropertiesJSONObjectScalarTypeResolver extends 
             $this->__('Custom scalar representing a JSON Object with restricted properties: \'%s\'', 'extended-schema-commons'),
             implode(
                 $this->__('\', \'', 'extended-schema-commons'),
-                []
+                $this->getRestrictedProperties()
             )
         );
     }
+
+    /**
+     * @return string[]
+     */
+    abstract protected function getRestrictedProperties(): array;
 
     public function getSpecifiedByURL(): ?string
     {
