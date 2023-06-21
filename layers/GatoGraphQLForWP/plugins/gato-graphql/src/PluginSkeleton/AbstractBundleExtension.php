09c5f7cd152582476566d7ed8950092be107ce9c
Leonardo Losoviz <leo@getpop.org>
2023-06-21 17:08:01 +0800

    getGatoGraphQLExtensionFilenames

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/AbstractBundleExtension.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/AbstractBundleExtension.php
@@ -6,4 +6,15 @@ namespace GatoGraphQL\GatoGraphQL\PluginSkeleton;
 
 abstract class AbstractBundleExtension extends AbstractExtension implements BundleExtensionInterface
 {
+    /**
+     * @param string[] $extensions
+     * @return string[]
+     */
+    protected static function getGatoGraphQLExtensionFilenames(array $extensions): array
+    {
+        return array_map(
+            fn (string $extension) => $extension . '/gato-graphql-' . $extension . '.php',
+            $extensions
+        );
+    }
 }
