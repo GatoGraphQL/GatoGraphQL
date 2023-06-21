ff8016b05ac9b2f4a107ee0563c9430028c5c2a2
Leonardo Losoviz <leo@getpop.org>
2023-06-21 17:39:22 +0800

    renamed getBundledExtensionSlugModuleClasses

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/AbstractBundleExtension.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/AbstractBundleExtension.php
@@ -6,4 +6,45 @@ namespace GatoGraphQL\GatoGraphQL\PluginSkeleton;
 
 abstract class AbstractBundleExtension extends AbstractExtension implements BundleExtensionInterface
 {
+    /**
+     * Provide the Extensions that are bundled by the Extension Bundle
+     *
+     * @return array<class-string<ExtensionInterface>>
+     */
+    public function getBundledExtensionClasses(): array
+    {
+        return array_values($this->getBundledExtensionSlugModuleClasses());
+    }
+
+    /**
+     * Convenience method, to return an array with the extension
+     * slug => extension Module class
+     *
+     * @return array<string,class-string<ModuleInterface>>
+     */
+    abstract public function getBundledExtensionSlugModuleClasses(): array;
+
+    /**
+     * Provide the Extension plugin filenames that are bundled
+     * by this Extension Bundle
+     *
+     * @return string[]
+     */
+    public function getBundledExtensionFilenames(): array
+    {
+        $extensions = array_keys($this->getBundledExtensionSlugModuleClasses());
+        return static::getExtensionFilenames($extensions);
+    }
+
+    /**
+     * @param string[] $extensions
+     * @return string[]
+     */
+    protected static function getExtensionFilenames(array $extensions): array
+    {
+        return array_map(
+            fn (string $extension) => $extension . '/' . $extension . '.php',
+            $extensions
+        );
+    }
 }
--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/AbstractGatoGraphQLBundleExtension.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/AbstractGatoGraphQLBundleExtension.php
@@ -4,45 +4,13 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\PluginSkeleton;
 
-use PoP\Root\Module\ModuleInterface;
-
 abstract class AbstractGatoGraphQLBundleExtension extends AbstractBundleExtension
 {
-    /**
-     * Provide the Extensions that are bundled by the Extension Bundle
-     *
-     * @return array<class-string<ExtensionInterface>>
-     */
-    final public function getBundledExtensionClasses(): array
-    {
-        return array_values($this->getGatoGraphQLBundledExtensionSlugModuleClasses());
-    }
-
-    /**
-     * Convenience method, to return an array with the extension
-     * slug => extension Module class
-     *
-     * @return array<string,class-string<ModuleInterface>>
-     */
-    abstract public function getGatoGraphQLBundledExtensionSlugModuleClasses(): array;
-
-    /**
-     * Provide the Extension plugin filenames that are bundled
-     * by this Extension Bundle
-     *
-     * @return string[]
-     */
-    final public function getBundledExtensionFilenames(): array
-    {
-        $extensions = array_keys($this->getGatoGraphQLBundledExtensionSlugModuleClasses());
-        return static::getGatoGraphQLExtensionFilenames($extensions);
-    }
-
     /**
      * @param string[] $extensions
      * @return string[]
      */
-    protected static function getGatoGraphQLExtensionFilenames(array $extensions): array
+    protected static function getExtensionFilenames(array $extensions): array
     {
         return array_map(
             fn (string $extension) => $extension . '/gato-graphql-' . $extension . '.php',
