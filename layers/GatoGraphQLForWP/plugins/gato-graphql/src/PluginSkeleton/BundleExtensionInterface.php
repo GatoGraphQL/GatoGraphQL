f90a71295acb7ee04ee8c9b93dba660f4f309a49
Leonardo Losoviz <leo@getpop.org>
2023-06-21 16:56:11 +0800

    getBundledExtensionClasses

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/BundleExtensionInterface.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginSkeleton/BundleExtensionInterface.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\PluginSkeleton;
 
+use GatoGraphQL\GatoGraphQL\PluginSkeleton\ExtensionInterface;
+
 interface BundleExtensionInterface extends ExtensionInterface
 {
+    /**
+     * Provide the Extensions that are bundled by the Extension Bundle
+     *
+     * @return array<class-string<ExtensionInterface>> $bundledExtensionClasses
+     */
+    public static function getBundledExtensionClasses(): array;
 }
