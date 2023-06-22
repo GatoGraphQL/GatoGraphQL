124fe0011b5950409d2b1c2116cab4abdbbb5a92
Leonardo Losoviz <leo@getpop.org>
2023-06-22 17:00:15 +0800

    DocMenuPageTrait

---
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/MenuPages/DocMenuPageTrait.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Services\MenuPages;
+
+use GatoGraphQL\GatoGraphQL\Constants\RequestParams;
+use GatoGraphQL\GatoGraphQL\ContentProcessors\MarkdownContentRetrieverTrait;
+use PoP\Root\App;
+
+/**
+ * Open a document provided via ?doc=...
+ */
+trait DocMenuPageTrait
+{
+    use MarkdownContentRetrieverTrait;
+    
+    protected function getRelativePathDir(): string
+    {
+        return '';
+    }
+
+    protected function getDocParamContent(): string
+    {
+        $filename = App::query(RequestParams::DOC, '');
+        $relativePathDir = $this->getRelativePathDir();
+
+        /**
+         * Move any potential "../" relative path from
+         * $filename to $relativePathDir.
+         *
+         * Eg: Links to release-notes .md files in wp-admin/admin.php?page=gato_graphql_about
+         */
+        while (str_starts_with($filename, '../')) {
+            $filename = substr($filename, 3);
+            $relativePathDir .=  '/..';
+        }
+
+        // Enable "/" in the filename
+        add_filter(
+            'sanitize_file_name_chars',
+            $this->enableSpecialCharsForSanitization(...)
+        );
+        $doc = \sanitize_file_name($filename);
+        remove_filter(
+            'sanitize_file_name_chars',
+            $this->enableSpecialCharsForSanitization(...)
+        );
+        return $this->getMarkdownContent(
+            $doc,
+            $relativePathDir
+        ) ?? sprintf(
+            \__('Page \'%s\' does not exist', 'gato-graphql'),
+            $doc
+        );
+    }
+
+    /**
+     * Enable "/" in the filename
+     *
+     * @param string[] $specialChars
+     * @return string[]
+     */
+    public function enableSpecialCharsForSanitization(array $specialChars): array
+    {
+        return array_diff(
+            $specialChars,
+            [
+                '/',
+            ]
+        );
+    }
+}
