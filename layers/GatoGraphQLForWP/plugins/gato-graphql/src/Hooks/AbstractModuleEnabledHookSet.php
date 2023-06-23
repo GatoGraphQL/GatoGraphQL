c51dd2f87e16823bdd46cbf5347153f052f3e839
Leonardo Losoviz <leo@getpop.org>
2023-06-23 15:12:07 +0800

    AbstractModuleEnabledHookSet

---
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Hooks/AbstractModuleEnabledHookSet.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Hooks;
+
+use GatoGraphQL\GatoGraphQL\Registries\ModuleRegistryInterface;
+use PoP\Root\Hooks\AbstractHookSet;
+
+abstract class AbstractModuleEnabledHookSet extends AbstractHookSet
+{
+    private ?ModuleRegistryInterface $moduleRegistry = null;
+
+    final public function setModuleRegistry(ModuleRegistryInterface $moduleRegistry): void
+    {
+        $this->moduleRegistry = $moduleRegistry;
+    }
+    final protected function getModuleRegistry(): ModuleRegistryInterface
+    {
+        /** @var ModuleRegistryInterface */
+        return $this->moduleRegistry ??= $this->instanceManager->getInstance(ModuleRegistryInterface::class);
+    }
+    
+    public function getEnablingModule(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * Only enable the service, if the corresponding module is also enabled
+     */
+    public function isServiceEnabled(): bool
+    {
+        $enablingModule = $this->getEnablingModule();
+        if ($enablingModule !== null) {
+            return $this->getModuleRegistry()->isModuleEnabled($enablingModule)
+                && parent::isServiceEnabled();
+        }
+        return parent::isServiceEnabled();
+    }
+}
