64efb2f9f18f5d79b645120ffc7b3fa781b431ab
Leonardo Losoviz <leo@getpop.org>
2023-07-06 15:20:09 +0800

    initialize settings

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginInitializationConfiguration.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/PluginInitializationConfiguration.php
@@ -545,6 +545,12 @@ class PluginInitializationConfiguration extends AbstractMainPluginInitialization
                 'module' => SchemaConfigurationFunctionalityModuleResolver::MUTATIONS,
                 'option' => SchemaConfigurationFunctionalityModuleResolver::USE_PAYLOADABLE_MUTATIONS_DEFAULT_VALUE,
             ],
+            [
+                'class' => \PoPCMSSchema\CustomPostUserMutations\Module::class,
+                'envVariable' => \PoPCMSSchema\CustomPostUserMutations\Environment::TREAT_AUTHOR_INPUT_IN_CUSTOMPOST_MUTATION_AS_SENSITIVE_DATA,
+                'module' => MutationSchemaTypeModuleResolver::SCHEMA_CUSTOMPOST_USER_MUTATIONS,
+                'option' => MutationSchemaTypeModuleResolver::OPTION_TREAT_AUTHOR_INPUT_IN_CUSTOMPOST_MUTATION_AS_SENSITIVE_DATA,
+            ],
         ];
     }
 
