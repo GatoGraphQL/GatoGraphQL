ddc4055633dae1d259bc137716a8c12a5352cb93
Leonardo Losoviz <leo@getpop.org>
2023-07-06 18:55:01 +0800

    implement service

--- layers/CMSSchema/packages/post-tag-mutations/src/MutationResolvers/SetTagsOnPostMutationResolver.php
+++ layers/CMSSchema/packages/post-tag-mutations/src/MutationResolvers/SetTagsOnPostMutationResolver.php
@@ -7,10 +7,13 @@ namespace PoPCMSSchema\PostTagMutations\MutationResolvers;
 use PoPCMSSchema\CustomPostTagMutations\MutationResolvers\AbstractSetTagsOnCustomPostMutationResolver;
 use PoPCMSSchema\CustomPostTagMutations\TypeAPIs\CustomPostTagTypeMutationAPIInterface;
 use PoPCMSSchema\PostTagMutations\TypeAPIs\PostTagTypeMutationAPIInterface;
+use PoPCMSSchema\PostTags\TypeAPIs\PostTagTypeAPIInterface;
+use PoPCMSSchema\Tags\TypeAPIs\TagTypeAPIInterface;
 
 class SetTagsOnPostMutationResolver extends AbstractSetTagsOnCustomPostMutationResolver
 {
     private ?PostTagTypeMutationAPIInterface $postCategoryTypeMutationAPIInterface = null;
+    private ?PostTagTypeAPIInterface $postTagTypeAPI = null;
 
     final public function setPostTagTypeMutationAPI(PostTagTypeMutationAPIInterface $postCategoryTypeMutationAPIInterface): void
     {
@@ -25,10 +28,28 @@ class SetTagsOnPostMutationResolver extends AbstractSetTagsOnCustomPostMutationR
         }
         return $this->postCategoryTypeMutationAPIInterface;
     }
+    final public function setPostTagTypeAPI(PostTagTypeAPIInterface $postTagTypeAPI): void
+    {
+        $this->postTagTypeAPI = $postTagTypeAPI;
+    }
+    final protected function getPostTagTypeAPI(): PostTagTypeAPIInterface
+    {
+        if ($this->postTagTypeAPI === null) {
+            /** @var PostTagTypeAPIInterface */
+            $postTagTypeAPI = $this->instanceManager->getInstance(PostTagTypeAPIInterface::class);
+            $this->postTagTypeAPI = $postTagTypeAPI;
+        }
+        return $this->postTagTypeAPI;
+    }
 
     protected function getCustomPostTagTypeMutationAPI(): CustomPostTagTypeMutationAPIInterface
     {
         return $this->getPostTagTypeMutationAPI();
+    }
+
+    protected function getTagTypeAPI(): TagTypeAPIInterface
+    {
+        return $this->getPostTagTypeAPI();
     }
 
     protected function getEntityName(): string
