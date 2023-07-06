28fab0b46a800e7be103089e1b2a006bd024304e
Leonardo Losoviz <leo@getpop.org>
2023-07-06 22:08:58 +0800

    cast attributes to object

--- layers/WPSchema/packages/block-content-parser/src/BlockContentParser.php
+++ layers/WPSchema/packages/block-content-parser/src/BlockContentParser.php
@@ -108,7 +108,7 @@ class BlockContentParser implements BlockContentParserInterface
                 if (isset($item->attributes)) {
                     /** @var array<string,mixed>|stdClass */
                     $blockAttributes = $item->attributes;
-                    $item->attributes = MethodHelpers::recursivelyConvertAssociativeArrayToStdClass((array) $blockAttributes);
+                    $item->attributes = (object) MethodHelpers::recursivelyConvertAssociativeArrayToStdClass((array) $blockAttributes);
                 }
 
                 // Recursively call for the block's inner blocks'
