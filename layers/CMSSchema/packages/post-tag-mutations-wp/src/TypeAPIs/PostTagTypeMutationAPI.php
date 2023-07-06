571f6c6b33a1dbeac80fd132501b77f5c8684926
Leonardo Losoviz <leo@getpop.org>
2023-07-06 16:03:06 +0800

    array<string|int> $tags

--- layers/CMSSchema/packages/post-tag-mutations-wp/src/TypeAPIs/PostTagTypeMutationAPI.php
+++ layers/CMSSchema/packages/post-tag-mutations-wp/src/TypeAPIs/PostTagTypeMutationAPI.php
@@ -15,7 +15,7 @@ class PostTagTypeMutationAPI implements PostTagTypeMutationAPIInterface
      * Watch out! $tags must be the array of tags as string, not their IDs
      * Passing the IDs will create a tag with that ID as the name!
      *
-     * @param string[] $tags
+     * @param array<string|int> $tags List of tags by ID, slug, or a combination of them
      */
     public function setTags(int|string $postID, array $tags, bool $append = false): void
     {
