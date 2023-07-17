344b62e30b41533c2eabfcd08840bf5d55cebc97
Leonardo Losoviz <leo@getpop.org>
2023-07-17 11:00:05 +0800

    block in PHP

---
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/Blocks/SchemaConfigResponseHeadersBlock.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Services\Blocks;
+
+use GatoGraphQL\GatoGraphQL\Constants\BlockAttributeNames;
+use GatoGraphQL\GatoGraphQL\Constants\ConfigurationDefaultValues;
+use GatoGraphQL\GatoGraphQL\ModuleResolvers\SchemaConfigurationFunctionalityModuleResolver;
+
+class SchemaConfigResponseHeadersBlock extends AbstractSchemaConfigCustomizableConfigurationBlock
+{
+    use MainPluginBlockTrait;
+    use OptionsBlockTrait;
+
+    protected function getBlockName(): string
+    {
+        return 'schema-config-response-headers';
+    }
+
+    public function getBlockPriority(): int
+    {
+        return 10120;
+    }
+
+    public function getEnablingModule(): ?string
+    {
+        return SchemaConfigurationFunctionalityModuleResolver::RESPONSE_HEADERS;
+    }
+
+    /**
+     * @param array<string,mixed> $attributes
+     */
+    protected function doRenderBlock(array $attributes, string $content): string
+    {
+        $values = $attributes[BlockAttributeNames::ENTRIES] ?? [];
+        return sprintf(
+            '<p><strong>%s</strong></p>%s',
+            $this->__('Custom headers to add to the API response', 'gato-graphql'),
+            $values ?
+                sprintf(
+                    '<ul><li><code>%s</code></li></ul>',
+                    implode('</code></li><li><code>', $values)
+                ) :
+                sprintf(
+                    '<p><em>%s</em></p>',
+                    \__('(not set)', 'gato-graphql')
+                )
+        );
+    }
+
+    protected function getBlockTitle(): string
+    {
+        return \__('Response Headers', 'gato-graphql');
+    }
+
+    /**
+     * Register style-index.css
+     */
+    protected function registerCommonStyleCSS(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Add the locale language to the localized data?
+     */
+    protected function addLocalLanguage(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Default language for the script/component's documentation
+     */
+    protected function getDefaultLanguage(): ?string
+    {
+        // English
+        return 'en';
+    }
+}
