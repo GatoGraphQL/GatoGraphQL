e1f4980423442dc3b1e1a7515c8132aef876b5e6
Leonardo Losoviz <leo@getpop.org>
2023-07-17 11:30:33 +0800

    use getResponseHeadersFromEntries

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/SchemaConfigurationExecuters/ResponseHeadersBlockSchemaConfigurationExecuter.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Services/SchemaConfigurationExecuters/ResponseHeadersBlockSchemaConfigurationExecuter.php
@@ -7,12 +7,10 @@ namespace GatoGraphQL\GatoGraphQL\Services\SchemaConfigurationExecuters;
 use GatoGraphQL\GatoGraphQL\App;
 use GatoGraphQL\GatoGraphQL\Constants\BlockAttributeNames;
 use GatoGraphQL\GatoGraphQL\ModuleResolvers\SchemaConfigurationFunctionalityModuleResolver;
+use GatoGraphQL\GatoGraphQL\PluginStaticHelpers;
 use GatoGraphQL\GatoGraphQL\Services\Blocks\BlockInterface;
 use GatoGraphQL\GatoGraphQL\Services\Blocks\SchemaConfigResponseHeadersBlock;
-use PoPCMSSchema\Categories\Environment as CategoriesEnvironment;
-use PoPCMSSchema\Categories\Module as CategoriesModule;
 use PoP\ComponentModel\Engine\EngineHookNames;
-use PoP\Root\Module\ModuleConfigurationHelpers;
 
 class ResponseHeadersBlockSchemaConfigurationExecuter extends AbstractCustomizableConfigurationBlockSchemaConfigurationExecuter implements PersistedQueryEndpointSchemaConfigurationExecuterServiceTagInterface, EndpointSchemaConfigurationExecuterServiceTagInterface
 {
@@ -47,28 +45,7 @@ class ResponseHeadersBlockSchemaConfigurationExecuter extends AbstractCustomizab
             return;
         }
 
-        /**
-         * Convert the entries from string[] into an array of string => string:
-         *   header name => header value
-         *
-         * @var array<string,string> Header name => value
-         */
-        $responseHeaders = [];
-        foreach ($entries as $entry) {
-            $entry = trim($entry);
-            $separatorPos = strpos($entry, ':');
-            if ($separatorPos === false) {
-                $headerName = $entry;
-                $headerValue = '';
-            } else {
-                $headerName = trim(substr($entry, 0, $separatorPos));
-                $headerValue = trim(substr($entry, $separatorPos + strlen(':')));
-            }
-            if ($headerName === '') {
-                continue;
-            }
-            $responseHeaders[$headerName] = $headerValue;
-        }
+        $responseHeaders = PluginStaticHelpers::getResponseHeadersFromEntries($entries);
         if ($responseHeaders === []) {
             return;
         }
