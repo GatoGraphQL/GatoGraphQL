cda0ff6e9925a25e4972f7414ddd7012930969a2
Leonardo Losoviz <leo@getpop.org>
2023-07-24 15:42:49 +0800

    Moved class to SchemaHooks

--- layers/CMSSchema/packages/custompost-category-mutations/src/SchemaHooks/AbstractCustomPostMutationResolverHookSet.php
+++ layers/CMSSchema/packages/custompost-category-mutations/src/SchemaHooks/AbstractCustomPostMutationResolverHookSet.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace PoPCMSSchema\CustomPostCategoryMutations\Hooks;
+namespace PoPCMSSchema\CustomPostCategoryMutations\SchemaHooks;
 
 use PoP\Root\App;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
