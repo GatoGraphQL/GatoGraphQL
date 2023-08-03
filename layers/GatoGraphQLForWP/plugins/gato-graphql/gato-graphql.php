835e2d2ac3cb67fc64f2bd33b5c5861e5a328715
Leonardo Losoviz <leo@getpop.org>
2023-08-03 16:01:37 +0800

    Temporarily remove "-dev" from plugin version

--- layers/GatoGraphQLForWP/plugins/gato-graphql/gato-graphql.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/gato-graphql.php
@@ -39,7 +39,7 @@ if (!defined('ABSPATH')) {
  *
  * @see https://getcomposer.org/doc/articles/versions.md#stabilities
  */
-$pluginVersion = '1.0.0-dev';
+$pluginVersion = '1.0.0';
 $pluginName = __('Gato GraphQL', 'gato-graphql');
 
 /**
