74f4e291a70a0a35a6841b2b25aa2fff3beedd5a
Leonardo Losoviz <leo@getpop.org>
2023-08-09 10:41:24 +0800

    No INSTANCE_NAME

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Constants/MarketplaceProductLicenseProperties.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Constants/MarketplaceProductLicenseProperties.php
@@ -12,9 +12,6 @@ class MarketplaceProductLicenseProperties
     /** After activating a license, what's its ID in the Marketplace Provider system */
     public final const INSTANCE_ID = 'instanceID';
 
-    /** How to identify the instance */
-    public final const INSTANCE_NAME = 'instanceName';
-
     /** After executing an operation against the Marketplace Provider's API, store the response */
     public final const API_RESPONSE_PAYLOAD = 'apiResponsePayload';
 }
