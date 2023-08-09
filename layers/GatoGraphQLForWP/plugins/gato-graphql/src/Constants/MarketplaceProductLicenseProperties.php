a5c024154895a025490b263089a80041550538ab
Leonardo Losoviz <leo@getpop.org>
2023-08-09 10:41:36 +0800

    order

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Constants/MarketplaceProductLicenseProperties.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Constants/MarketplaceProductLicenseProperties.php
@@ -6,12 +6,12 @@ namespace GatoGraphQL\GatoGraphQL\Constants;
 
 class MarketplaceProductLicenseProperties
 {
+    /** After executing an operation against the Marketplace Provider's API, store the response */
+    public final const API_RESPONSE_PAYLOAD = 'apiResponsePayload';
+
     /** The status of the license */
     public final const STATUS = 'status';
 
     /** After activating a license, what's its ID in the Marketplace Provider system */
     public final const INSTANCE_ID = 'instanceID';
-
-    /** After executing an operation against the Marketplace Provider's API, store the response */
-    public final const API_RESPONSE_PAYLOAD = 'apiResponsePayload';
 }
