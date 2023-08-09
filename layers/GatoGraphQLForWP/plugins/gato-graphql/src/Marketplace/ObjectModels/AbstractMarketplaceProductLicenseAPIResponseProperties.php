41e17c655a96d25c28ba89a368ecd9df71b5de30
Leonardo Losoviz <leo@getpop.org>
2023-08-09 10:48:12 +0800

    validations

--- layers/GatoGraphQLForWP/plugins/gato-graphql/src/Marketplace/ObjectModels/AbstractMarketplaceProductLicenseAPIResponseProperties.php
+++ layers/GatoGraphQLForWP/plugins/gato-graphql/src/Marketplace/ObjectModels/AbstractMarketplaceProductLicenseAPIResponseProperties.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\Marketplace\ObjectModels;
 
+use PoP\Root\Exception\ShouldNotHappenException;
+
 abstract class AbstractMarketplaceProductLicenseAPIResponseProperties
 {
     /**
@@ -15,6 +17,29 @@ abstract class AbstractMarketplaceProductLicenseAPIResponseProperties
         public readonly ?string $errorMessage,
         public readonly ?string $successMessage,
     ) {
+        if ($errorMessage === null) {
+            if ($successMessage === null) {
+                throw new ShouldNotHappenException(
+                    \__('The operation must either have an error, or be successful', 'gato-graphql')
+                );
+            }
+            if ($apiResponsePayload === null) {
+                throw new ShouldNotHappenException(
+                    \__('Must provide the API response payload for a successful operation', 'gato-graphql')
+                );
+            }
+            if ($status === null) {
+                throw new ShouldNotHappenException(
+                    \__('Must provide the status for a successful operation', 'gato-graphql')
+                );
+            }
+        } else {
+            if ($successMessage !== null) {
+                throw new ShouldNotHappenException(
+                    \__('The operation cannot have error and success at the same time', 'gato-graphql')
+                );
+            }
+        }
     }
 
     public function isSuccessful(): bool
