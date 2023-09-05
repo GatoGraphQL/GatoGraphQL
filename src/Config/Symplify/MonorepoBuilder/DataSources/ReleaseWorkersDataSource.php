02f4a49ecd9f420a8f1aff3c14bfed71386b2ec0
Leonardo Losoviz <leo@getpop.org>
2023-09-05 16:24:29 +0800

    added workers

--- src/Config/Symplify/MonorepoBuilder/DataSources/ReleaseWorkersDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/ReleaseWorkersDataSource.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace PoP\PoP\Config\Symplify\MonorepoBuilder\DataSources;
 
+use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\BumpVersionForDevInMonorepoMetadataFileReleaseWorker;
+use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\BumpVersionForDevInPluginMainFileReleaseWorker;
+use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\BumpVersionForDevInPluginNodeJSPackageJSONFilesReleaseWorker;
 use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\GuardOnDefaultBranchReleaseWorker;
+use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\ConvertVersionForProdInMonorepoMetadataFileReleaseWorker;
+use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\ConvertVersionForProdInPluginBlockCompiledMarkdownFilesReleaseWorker;
+use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\ConvertVersionForProdInPluginMainFileReleaseWorker;
+use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\ConvertVersionForProdInPluginNodeJSPackageJSONFilesReleaseWorker;
+use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\RestoreVersionForDevInPluginBlockCompiledMarkdownFilesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualConflictsReleaseWorker;
@@ -23,6 +31,14 @@ class ReleaseWorkersDataSource
     {
         return [
             GuardOnDefaultBranchReleaseWorker::class,
+
+            // Remove "-dev" from the version
+            ConvertVersionForProdInPluginMainFileReleaseWorker::class,
+            ConvertVersionForProdInPluginNodeJSPackageJSONFilesReleaseWorker::class,
+            ConvertVersionForProdInMonorepoMetadataFileReleaseWorker::class,
+            ConvertVersionForProdInPluginBlockCompiledMarkdownFilesReleaseWorker::class,
+            
+            // Default workers
             UpdateReplaceReleaseWorker::class,
             SetCurrentMutualConflictsReleaseWorker::class,
             SetCurrentMutualDependenciesReleaseWorker::class,
@@ -30,6 +46,14 @@ class ReleaseWorkersDataSource
             PushTagReleaseWorker::class,
             SetNextMutualDependenciesReleaseWorker::class,
             UpdateBranchAliasReleaseWorker::class,
+
+            // Add "-dev" again to the version
+            RestoreVersionForDevInPluginBlockCompiledMarkdownFilesReleaseWorker::class,
+            BumpVersionForDevInMonorepoMetadataFileReleaseWorker::class,
+            BumpVersionForDevInPluginNodeJSPackageJSONFilesReleaseWorker::class,
+            BumpVersionForDevInPluginMainFileReleaseWorker::class,
+            
+            // Default workers
             PushNextDevReleaseWorker::class,
         ];
     }
