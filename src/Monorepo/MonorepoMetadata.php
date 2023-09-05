c6cb78b21864723cea849dd912de6b30d813b087
Leonardo Losoviz <leo@getpop.org>
2023-09-05 17:08:33 +0800

    phpdoc

--- src/Monorepo/MonorepoMetadata.php
+++ src/Monorepo/MonorepoMetadata.php
@@ -7,7 +7,11 @@ namespace PoP\PoP\Monorepo;
 final class MonorepoMetadata
 {
     /**
-     * Modify this const when bumping the code to a new version
+     * Modify this const when bumping the code to a new version.
+     * 
+     * Important: Do not modify the formatting of this PHP code!
+     * A regex will search for this exact pattern, to update the
+     * version in the ReleaseWorker when deploying for PROD.
      */
     final public const VERSION = '1.0.0-dev';
 }
