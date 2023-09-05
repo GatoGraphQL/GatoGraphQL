67de6994b266c72c26d5d68b91107184ffe03573
Leonardo Losoviz <leo@getpop.org>
2023-09-05 14:36:57 +0800

    MonorepoMetadataVersionUtils

---
+++ src/Extensions/Symplify/MonorepoBuilder/Utils/MonorepoMetadataVersionUtils.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\PoP\Extensions\Symplify\MonorepoBuilder\Utils;
+
+use PoP\PoP\Monorepo\MonorepoMetadata;
+
+/**
+ * MonorepoMetadata::VERSION can have "-dev" (in the source code
+ * for development) or not (in the converted code for production).
+ *
+ * This helper helps abstract these, and always access the required
+ * DEV or PROD version.
+ */
+final class MonorepoMetadataVersionUtils
+{
+    public function getDevVersion(): string
+    {
+        if (str_ends_with(MonorepoMetadata::VERSION, '-dev')) {
+            return MonorepoMetadata::VERSION;
+        }
+        return MonorepoMetadata::VERSION . '-dev';
+    }
+    
+    public function getProdVersion(): string
+    {
+        if (str_ends_with(MonorepoMetadata::VERSION, '-dev')) {
+            return substr(MonorepoMetadata::VERSION, 0, strlen(MonorepoMetadata::VERSION) - strlen('-dev'));
+        }
+        return MonorepoMetadata::VERSION;
+    }
+}
