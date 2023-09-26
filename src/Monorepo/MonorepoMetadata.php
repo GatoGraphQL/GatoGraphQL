aa40d3c32a0a865d045337cd5f13d5806041c491
Leonardo Losoviz <leo@getpop.org>
2023-09-26 13:18:37 +0800

    improved phpdoc

--- src/Monorepo/MonorepoMetadata.php
+++ src/Monorepo/MonorepoMetadata.php
@@ -9,9 +9,10 @@ final class MonorepoMetadata
     /**
      * Modify this const when bumping the code to a new version.
      *
-     * Important: Do not modify the formatting of this PHP code!
-     * A regex will search for this exact pattern, to update the
-     * version in the ReleaseWorker when deploying for PROD.
+     * Important: This code is read-only! A ReleaseWorker
+     * will search for this pattern using a regex, to update the
+     * version when creating a new release
+     * (i.e. via `composer release-major|minor|patch`).
      */
     final public const VERSION = '1.1.0-dev';
 
