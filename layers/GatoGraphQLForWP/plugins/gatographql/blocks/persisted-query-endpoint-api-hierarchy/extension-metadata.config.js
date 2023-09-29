b8da2b1f6767b133688620c89b2ba9bae6ac901e
Leonardo Losoviz <leo@getpop.org>
2023-09-29 16:42:25 +0800

    Externalize config data from webpack.config.js

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/blocks/persisted-query-endpoint-api-hierarchy/extension-metadata.config.js
@@ -0,0 +1,3 @@
+module.exports.GIT_BASE_BRANCH = 'master'
+module.exports.GITHUB_REPO_OWNER = 'GatoGraphQL';
+module.exports.GITHUB_REPO_NAME = 'GatoGraphQL';
--- layers/GatoGraphQLForWP/plugins/gatographql/blocks/schema-config-multifield-directives/webpack.config.js
+++ layers/GatoGraphQLForWP/plugins/gatographql/blocks/schema-config-multifield-directives/webpack.config.js
@@ -4,9 +4,9 @@
 const MODULE = 'multifield-directives';
 
 const MODULE_DOCS_PATH = `docs/modules/${ MODULE }/`;
-const NPM_PACKAGE_VERSION = process.env.npm_package_version;
-const PACKAGE_TAG = NPM_PACKAGE_VERSION.endsWith('-dev') ? 'master' : NPM_PACKAGE_VERSION;
-const GITHUB_BASE_URL = `https://raw.githubusercontent.com/GatoGraphQL/GatoGraphQL/${ PACKAGE_TAG }/layers/GatoGraphQLForWP/plugins/gatographql`
+const ExtensionMetadataHelpers = require("./extension-metadata.helpers")
+const PACKAGE_TAG = ExtensionMetadataHelpers.getStablePackageTagForCurrentVersion();
+const GITHUB_BASE_URL = `${ ExtensionMetadataHelpers.getGitHubRepoDocsRootURL() }/${ PACKAGE_TAG }/layers/GatoGraphQLForWP/plugins/gatographql`
 const BASE_URL = process.env.NODE_ENV === 'production'
 	? `${ GITHUB_BASE_URL }/${ MODULE_DOCS_PATH }`
 	: null;
