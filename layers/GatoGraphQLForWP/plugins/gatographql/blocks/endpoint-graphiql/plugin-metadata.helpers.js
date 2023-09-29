b8de6032781a6c0c6be0731723dd1a167482eb48
Leonardo Losoviz <leo@getpop.org>
2023-09-29 16:59:02 +0800

    renamed var

--- layers/GatoGraphQLForWP/plugins/gatographql/blocks/endpoint-graphiql/plugin-metadata.helpers.js
+++ layers/GatoGraphQLForWP/plugins/gatographql/blocks/endpoint-graphiql/plugin-metadata.helpers.js
@@ -1,10 +1,10 @@
-const extensionMetadataConfig = require("./plugin-metadata.config")
+const PluginMetadataConfig = require("./plugin-metadata.config")
 
 module.exports.getGitHubRepoDocsRootURL = function () {
-    return `https://raw.githubusercontent.com/${ extensionMetadataConfig.GITHUB_REPO_OWNER }/${ extensionMetadataConfig.GITHUB_REPO_NAME }`;
+    return `https://raw.githubusercontent.com/${ PluginMetadataConfig.GITHUB_REPO_OWNER }/${ PluginMetadataConfig.GITHUB_REPO_NAME }`;
 }
 
 module.exports.getStablePackageTagForCurrentVersion = function () {
     const NPM_PACKAGE_VERSION = process.env.npm_package_version;
-    return NPM_PACKAGE_VERSION.endsWith('-dev') ? extensionMetadataConfig.GIT_BASE_BRANCH : NPM_PACKAGE_VERSION;
+    return NPM_PACKAGE_VERSION.endsWith('-dev') ? PluginMetadataConfig.GIT_BASE_BRANCH : NPM_PACKAGE_VERSION;
 }
