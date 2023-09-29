665d264fd821d4662ac42eaaf35d865e413d60b4
Leonardo Losoviz <leo@getpop.org>
2023-09-29 17:15:50 +0800

    Metadata classes

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/PluginMetadata.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL;
+
+final class PluginMetadata
+{
+    final public const GIT_BASE_BRANCH = 'master';
+    final public const GITHUB_REPO_OWNER = 'GatoGraphQL';
+    final public const GITHUB_REPO_NAME = 'GatoGraphQL';
+}
---
+++ layers/GatoGraphQLForWP/plugins/testing-schema/src/ExtensionMetadata.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\TestingSchema;
+
+final class ExtensionMetadata
+{
+    final public const GIT_BASE_BRANCH = 'master';
+    final public const GITHUB_REPO_OWNER = 'GatoGraphQL';
+    final public const GITHUB_REPO_NAME = 'GatoGraphQL';
+}
