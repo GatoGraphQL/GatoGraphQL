ab3ea2f9776a8656ab6f6dc89fbf0c8a628f3eca
Leonardo Losoviz <leo@getpop.org>
2023-10-02 14:49:55 +0800

    Copied files to block-config/helpers

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/block-config/plugin-metadata.config.js
@@ -0,0 +1,3 @@
+module.exports.DOCS_GIT_BASE_BRANCH = 'master'
+module.exports.DOCS_GITHUB_REPO_OWNER = 'GatoGraphQL';
+module.exports.DOCS_GITHUB_REPO_NAME = 'GatoGraphQL';
---
+++ layers/GatoGraphQLForWP/plugins/gatographql/block-helpers/plugin-metadata.helpers.js
@@ -0,0 +1,10 @@
+const PluginMetadataConfig = require("../block-config/plugin-metadata.config")
+
+module.exports.getGitHubRepoDocsRootURL = function () {
+    return `https://raw.githubusercontent.com/${ PluginMetadataConfig.DOCS_GITHUB_REPO_OWNER }/${ PluginMetadataConfig.DOCS_GITHUB_REPO_NAME }`;
+}
+
+module.exports.getGitHubRepoDocsBranchOrTag = function () {
+    const NPM_PACKAGE_VERSION = process.env.npm_package_version;
+    return NPM_PACKAGE_VERSION.endsWith('-dev') ? PluginMetadataConfig.DOCS_GIT_BASE_BRANCH : NPM_PACKAGE_VERSION;
+}
