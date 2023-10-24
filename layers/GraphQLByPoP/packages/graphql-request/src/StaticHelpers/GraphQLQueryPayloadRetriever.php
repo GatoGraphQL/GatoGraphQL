57ae10d4b849754865526e30a49742e30e3e66ce
leoloso <leo@getpop.org>
2023-10-24 16:29:21 +0800

    elseif (isset(App::query('operationName')))

--- layers/GraphQLByPoP/packages/graphql-request/src/StaticHelpers/GraphQLQueryPayloadRetriever.php
+++ layers/GraphQLByPoP/packages/graphql-request/src/StaticHelpers/GraphQLQueryPayloadRetriever.php
@@ -40,6 +40,12 @@ class GraphQLQueryPayloadRetriever
             }
             if (isset($json->operationName)) {
                 $payload['operationName'] = $json->operationName;
+            } elseif (isset(App::query('operationName'))) {
+                /**
+                 * ?operationName=... can be passed as a GET param in the URL,
+                 * eg: to execute a Persisted Query while still passing a body via POST
+                 */
+                $payload['operationName'] = App::query('operationName');
             }
             return $payload;
         }
