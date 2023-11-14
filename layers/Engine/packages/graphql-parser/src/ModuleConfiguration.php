ca6679468e24ed60d746be1b2be2ebf80df1dd54
leoloso <leo@getpop.org>
2023-11-14 13:17:12 +0800

    default is false

--- layers/Engine/packages/graphql-parser/src/ModuleConfiguration.php
+++ layers/Engine/packages/graphql-parser/src/ModuleConfiguration.php
@@ -77,7 +77,7 @@ class ModuleConfiguration extends AbstractModuleConfiguration
     public function useLastOperationInDocumentForMultipleQueryExecutionWhenOperationNameNotProvided(): bool
     {
         $envVariable = Environment::USE_LAST_OPERATION_IN_DOCUMENT_FOR_MULTIPLE_QUERY_EXECUTION_WHEN_OPERATION_NAME_NOT_PROVIDED;
-        $defaultValue = true;
+        $defaultValue = false;
         $callback = EnvironmentValueHelpers::toBool(...);
 
         return $this->retrieveConfigurationValueOrUseDefault(
