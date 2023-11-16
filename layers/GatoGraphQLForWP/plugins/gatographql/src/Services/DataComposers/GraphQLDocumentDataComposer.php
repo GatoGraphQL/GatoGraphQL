31c3ec5edc6dba5762dd227fef47774480fab5ab
leoloso <leo@getpop.org>
2023-11-16 13:55:32 +0800

    format

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Services/DataComposers/GraphQLDocumentDataComposer.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Services/DataComposers/GraphQLDocumentDataComposer.php
@@ -76,6 +76,7 @@ class GraphQLDocumentDataComposer
                 )
             );
         }
+        
         $headerRequirementsSectionItems = [
             '*********************************************************************',
             '',
