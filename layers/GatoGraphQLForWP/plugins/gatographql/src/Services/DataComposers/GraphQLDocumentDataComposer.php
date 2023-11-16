79365cb115d527a4540886585d61d742b4ecb929
leoloso <leo@getpop.org>
2023-11-16 13:53:56 +0800

    Print extensions

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Services/DataComposers/GraphQLDocumentDataComposer.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Services/DataComposers/GraphQLDocumentDataComposer.php
@@ -4,11 +4,31 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\Services\DataComposers;
 
+use GatoGraphQL\GatoGraphQL\Services\DataProviders\RecipeDataProvider;
+use PoP\Root\Services\BasicServiceTrait;
 use RuntimeException;
 
 class GraphQLDocumentDataComposer
 {
+    use BasicServiceTrait;
+    
     public const GRAPHQL_DOCUMENT_HEADER_SEPARATOR = '########################################################################';
+
+    private ?RecipeDataProvider $recipeDataProvider = null;
+
+    final public function setRecipeDataProvider(RecipeDataProvider $recipeDataProvider): void
+    {
+        $this->recipeDataProvider = $recipeDataProvider;
+    }
+    final protected function getRecipeDataProvider(): RecipeDataProvider
+    {
+        if ($this->recipeDataProvider === null) {
+            /** @var RecipeDataProvider */
+            $recipeDataProvider = $this->instanceManager->getInstance(RecipeDataProvider::class);
+            $this->recipeDataProvider = $recipeDataProvider;
+        }
+        return $this->recipeDataProvider;
+    }
 
     /**
      * Append the required extensions and bundles to the header
@@ -18,6 +38,25 @@ class GraphQLDocumentDataComposer
         string $graphQLDocument,
         string $recipeSlug,
     ): string {
+        /**
+         * Check if there are required extensions for the recipe
+         */
+        $recipeSlugDataItems = $this->getRecipeDataProvider()->getRecipeSlugDataItems();
+        $recipeDataItem = $recipeSlugDataItems[$recipeSlug] ?? null;
+        if ($recipeDataItem === null) {
+            throw new RuntimeException(
+                sprintf(
+                    \__('There is no recipe with slug "%s"', 'gatographql'),
+                    $recipeSlug
+                )
+            );
+        }
+        $requiredExtensions = $recipeDataItem[1] ?? [];
+        if ($requiredExtensions === []) {
+            return $graphQLDocument;
+        }
+        $requiredBundles = $recipeDataItem[2] ?? [];
+
         /**
          * Find the last instance of the header separator
          * (there will normally be two instances)
@@ -40,8 +79,28 @@ class GraphQLDocumentDataComposer
         $headerRequirementsSectionItems = [
             '*********************************************************************',
             '',
+            \__('Required Extensions:', 'gatographql'),
         ];
+
+        foreach ($requiredExtensions as $extension) {
+            $headerRequirementsSectionItems[] = sprintf(
+                \__('  - %s', 'gatographql'),
+                $extension
+            );
+        }
+        
         $headerRequirementsSectionItems[] = '';
+        $headerRequirementsSectionItems[] = 'These Extensions are all included in any of these Bundles:';
+        
+        foreach ($requiredBundles as $bundle) {
+            $headerRequirementsSectionItems[] = sprintf(
+                \__('  - %s', 'gatographql'),
+                $bundle
+            );
+        }
+        
+        $headerRequirementsSectionItems[] = '';
+
         return substr(
             $graphQLDocument,
             0,
