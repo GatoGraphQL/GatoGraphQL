fd230244f671b0c40d8c702e701c7fac8c3d7012
leoloso <leo@getpop.org>
2023-11-21 14:50:54 +0800

    reverted container sets

--- src/Config/Rector/Downgrade/Configurators/AbstractDowngradeContainerConfigurationService.php
+++ src/Config/Rector/Downgrade/Configurators/AbstractDowngradeContainerConfigurationService.php
@@ -11,13 +11,15 @@ namespace PoP\PoP\Config\Rector\Downgrade\Configurators;
 use PoP\PoP\Config\Rector\Configurators\AbstractContainerConfigurationService;
 use PoP\PoP\Extensions\Rector\Set\ValueObject\CustomDowngradeSetList;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 abstract class AbstractDowngradeContainerConfigurationService extends AbstractContainerConfigurationService
 {
     public function configureContainer(): void
     {
         $this->rectorConfig->sets([
-            CustomDowngradeSetList::DOWN_TO_PHP_72,
+            CustomDowngradeSetList::BEFORE_DOWNGRADE,
+            DowngradeLevelSetList::DOWN_TO_PHP_72,
         ]);
 
         /**
