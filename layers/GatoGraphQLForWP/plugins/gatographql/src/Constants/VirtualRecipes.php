a237cc865106b220b45c00eacbaab24227d5e4aa
leoloso <leo@getpop.org>
2023-11-26 12:53:25 +0800

    Virtual Recipes

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Constants/VirtualRecipes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Constants;
+
+/**
+ * Recipes which have not been implemented, but are still needed to add predefined persisted queries
+ */
+class VirtualRecipes
+{
+    public const IMPORTING_A_POST_FROM_WORDPRESS_RSS_FEED = 'importing-a-post-from-wordpress-rss-feed';
+}
