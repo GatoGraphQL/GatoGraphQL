37eecb7ca178c3ce7a62ecf6e362f9aef09cca6b
leoloso <leo@getpop.org>
2023-11-30 10:22:46 +0800

    BundleExtensionAggregator

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Services/DataAggregators/BundleExtensionAggregator.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Services\DataAggregators;
+
+use GatoGraphQL\GatoGraphQL\ModuleResolvers\Extensions\BundleExtensionModuleResolverInterface;
+use GatoGraphQL\GatoGraphQL\Registries\ModuleRegistryInterface;
+use PoP\Root\Services\BasicServiceTrait;
+
+class BundleExtensionAggregator
+{
+    use BasicServiceTrait;
+
+    private ?ModuleRegistryInterface $moduleRegistry = null;
+
+    final public function setModuleRegistry(ModuleRegistryInterface $moduleRegistry): void
+    {
+        $this->moduleRegistry = $moduleRegistry;
+    }
+    final protected function getModuleRegistry(): ModuleRegistryInterface
+    {
+        if ($this->moduleRegistry === null) {
+            /** @var ModuleRegistryInterface */
+            $moduleRegistry = $this->instanceManager->getInstance(ModuleRegistryInterface::class);
+            $this->moduleRegistry = $moduleRegistry;
+        }
+        return $this->moduleRegistry;
+    }
+
+    /**
+     * Given a bunch of extensions, retrieve all bundles that
+     * comprise them all
+     *
+     * @param string[] $extensionModules
+     * @return string[]
+     */
+    public function getBundleModulesComprisingAllExtensionModules(
+        array $extensionModules
+    ): array {
+        // Obtain the list of all the Gato GraphQL Bundles
+        $modules = $this->getModuleRegistry()->getAllModules();
+        $bundleModules = [];
+        foreach ($modules as $module) {
+            $moduleResolver = $this->getModuleRegistry()->getModuleResolver($module);
+            if (!($moduleResolver instanceof BundleExtensionModuleResolverInterface)) {
+                continue;
+            }
+            $bundleModules = [
+                ...$bundleModules,
+                ...$moduleResolver->getModulesToResolve(),
+            ];
+        }
+
+        $extensionBundleModules = [];
+
+        foreach ($bundleModules as $bundleModule) {
+            /** @var BundleExtensionModuleResolverInterface */
+            $bundleModuleResolver = $this->getModuleRegistry()->getModuleResolver($bundleModule);
+            $bundledExtensionModules = $bundleModuleResolver->getBundledExtensionModules($bundleModule);
+            if (array_diff($extensionModules, $bundledExtensionModules) !== []) {
+                continue;
+            }
+            $extensionBundleModules[] = $bundleModule;
+        }
+
+        return $extensionBundleModules;
+    }
+}
