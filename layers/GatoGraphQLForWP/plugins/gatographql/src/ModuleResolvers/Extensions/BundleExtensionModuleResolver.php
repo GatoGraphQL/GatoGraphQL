f81e879404b123a1bc3b32bd4597372bc4d266b0
leoloso <leo@getpop.org>
2023-12-21 17:07:39 +0800

    bundle extension modules

--- layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/Extensions/BundleExtensionModuleResolver.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/Extensions/BundleExtensionModuleResolver.php
@@ -13,6 +13,13 @@ class BundleExtensionModuleResolver extends AbstractBundleExtensionModuleResolve
     public const WORDPRESS_AUTOMATOR = Plugin::NAMESPACE . '\\bundle-extensions\\wordpress-automator';
     public const WORDPRESS_CONTENT_TRANSLATION = Plugin::NAMESPACE . '\\bundle-extensions\\wordpress-content-translation';
     public const WORDPRESS_PUBLIC_API = Plugin::NAMESPACE . '\\bundle-extensions\\wordpress-public-api';
+    public const AUTOMATED_CONTENT_TRANSLATION_AND_SYNC_FOR_WORDPRESS_MULTISITE = Plugin::NAMESPACE . '\\bundle-extensions\\automated-content-translation-and-sync-for-wordpress-multisite';
+    public const PRIVATE_GRAPHQL_SERVER_FOR_WORDPRESS = Plugin::NAMESPACE . '\\bundle-extensions\\private-graphql-server-for-wordpress';
+    public const WORDPRESS_AS_API_GATEWAY = Plugin::NAMESPACE . '\\bundle-extensions\\wordpress-as-api-gateway';
+    public const WORDPRESS_CONTENT_IMPORT_EXPORT_AND_SYNC = Plugin::NAMESPACE . '\\bundle-extensions\\wordpress-content-import-export-and-sync';
+    public const WORDPRESS_CONTENT_TRANSFORMER = Plugin::NAMESPACE . '\\bundle-extensions\\wordpress-content-transformer';
+    public const WORDPRESS_EMAIL_NOTIFICATIONS = Plugin::NAMESPACE . '\\bundle-extensions\\wordpress-email-notifications';
+    public const WORDPRESS_REMOTE_REQUEST_AND_PROCESS = Plugin::NAMESPACE . '\\bundle-extensions\\wordpress-remote-request-and-process';
 
     /**
      * @return string[]
@@ -24,6 +31,13 @@ class BundleExtensionModuleResolver extends AbstractBundleExtensionModuleResolve
             self::WORDPRESS_AUTOMATOR,
             self::WORDPRESS_CONTENT_TRANSLATION,
             self::WORDPRESS_PUBLIC_API,
+            self::AUTOMATED_CONTENT_TRANSLATION_AND_SYNC_FOR_WORDPRESS_MULTISITE,
+            self::PRIVATE_GRAPHQL_SERVER_FOR_WORDPRESS,
+            self::WORDPRESS_AS_API_GATEWAY,
+            self::WORDPRESS_CONTENT_IMPORT_EXPORT_AND_SYNC,
+            self::WORDPRESS_CONTENT_TRANSFORMER,
+            self::WORDPRESS_EMAIL_NOTIFICATIONS,
+            self::WORDPRESS_REMOTE_REQUEST_AND_PROCESS,
         ];
     }
 
@@ -35,6 +49,13 @@ class BundleExtensionModuleResolver extends AbstractBundleExtensionModuleResolve
             self::WORDPRESS_AUTOMATOR => sprintf($placeholder, \__('WordPress Automator', 'gatographql')),
             self::WORDPRESS_CONTENT_TRANSLATION => sprintf($placeholder, \__('WordPress Content Translation', 'gatographql')),
             self::WORDPRESS_PUBLIC_API => sprintf($placeholder, \__('WordPress Public API', 'gatographql')),
+            self::AUTOMATED_CONTENT_TRANSLATION_AND_SYNC_FOR_WORDPRESS_MULTISITE => sprintf($placeholder, \__('Automated Content Translation & Sync for WordPress Multisite', 'gatographql')),
+            self::PRIVATE_GRAPHQL_SERVER_FOR_WORDPRESS => sprintf($placeholder, \__('Private GraphQL Server for WordPress', 'gatographql')),
+            self::WORDPRESS_AS_API_GATEWAY => sprintf($placeholder, \__('WordPress as API Gateway', 'gatographql')),
+            self::WORDPRESS_CONTENT_IMPORT_EXPORT_AND_SYNC => sprintf($placeholder, \__('WordPress Content Import, Export & Sync', 'gatographql')),
+            self::WORDPRESS_CONTENT_TRANSFORMER => sprintf($placeholder, \__('WordPress Content Transformer', 'gatographql')),
+            self::WORDPRESS_EMAIL_NOTIFICATIONS => sprintf($placeholder, \__('WordPress Email Notifications', 'gatographql')),
+            self::WORDPRESS_REMOTE_REQUEST_AND_PROCESS => sprintf($placeholder, \__('WordPress Remote Request & Process', 'gatographql')),
             default => $module,
         };
     }
@@ -46,6 +67,13 @@ class BundleExtensionModuleResolver extends AbstractBundleExtensionModuleResolve
             self::WORDPRESS_AUTOMATOR => \__('Keep content in sync, help migrate websites, send notifications, interact with 3rd-party services and APIs, create automation workflows, and more.', 'gatographql'),
             self::WORDPRESS_CONTENT_TRANSLATION => \__('Translate content via the Google Translate API, even within the deep structure of (Gutenberg) blocks.', 'gatographql'),
             self::WORDPRESS_PUBLIC_API => \__('Expose your public APIs in a secure manner, make them faster through caching, leverage tools to access data, and evolve the schema.', 'gatographql'),
+            self::AUTOMATED_CONTENT_TRANSLATION_AND_SYNC_FOR_WORDPRESS_MULTISITE => \__('@todo New bundle', 'gatographql'),
+            self::PRIVATE_GRAPHQL_SERVER_FOR_WORDPRESS => \__('@todo New bundle', 'gatographql'),
+            self::WORDPRESS_AS_API_GATEWAY => \__('@todo New bundle', 'gatographql'),
+            self::WORDPRESS_CONTENT_IMPORT_EXPORT_AND_SYNC => \__('@todo New bundle', 'gatographql'),
+            self::WORDPRESS_CONTENT_TRANSFORMER => \__('@todo New bundle', 'gatographql'),
+            self::WORDPRESS_EMAIL_NOTIFICATIONS => \__('@todo New bundle', 'gatographql'),
+            self::WORDPRESS_REMOTE_REQUEST_AND_PROCESS => \__('@todo New bundle', 'gatographql'),
             default => parent::getDescription($module),
         };
     }
@@ -140,6 +168,109 @@ class BundleExtensionModuleResolver extends AbstractBundleExtensionModuleResolve
                 ExtensionModuleResolver::MULTIPLE_QUERY_EXECUTION,
                 ExtensionModuleResolver::RESPONSE_ERROR_TRIGGER,
                 ExtensionModuleResolver::SCHEMA_EDITING_ACCESS,
+            ],
+            self::AUTOMATED_CONTENT_TRANSLATION_AND_SYNC_FOR_WORDPRESS_MULTISITE => [
+                ExtensionModuleResolver::AUTOMATION,
+                ExtensionModuleResolver::CONDITIONAL_FIELD_MANIPULATION,
+                ExtensionModuleResolver::FIELD_ON_FIELD,
+                ExtensionModuleResolver::FIELD_RESPONSE_REMOVAL,
+                ExtensionModuleResolver::FIELD_TO_INPUT,
+                ExtensionModuleResolver::FIELD_VALUE_ITERATION_AND_MANIPULATION,
+                ExtensionModuleResolver::GOOGLE_TRANSLATE,
+                ExtensionModuleResolver::HTTP_CLIENT,
+                ExtensionModuleResolver::INTERNAL_GRAPHQL_SERVER,
+                ExtensionModuleResolver::MULTIPLE_QUERY_EXECUTION,
+                ExtensionModuleResolver::PHP_CONSTANTS_AND_ENVIRONMENT_VARIABLES_VIA_SCHEMA,
+                ExtensionModuleResolver::PHP_FUNCTIONS_VIA_SCHEMA,
+                ExtensionModuleResolver::RESPONSE_ERROR_TRIGGER,
+            ],
+            self::PRIVATE_GRAPHQL_SERVER_FOR_WORDPRESS => [
+                ExtensionModuleResolver::CONDITIONAL_FIELD_MANIPULATION,
+                ExtensionModuleResolver::EMAIL_SENDER,
+                ExtensionModuleResolver::FIELD_DEFAULT_VALUE,
+                ExtensionModuleResolver::FIELD_ON_FIELD,
+                ExtensionModuleResolver::FIELD_RESOLUTION_CACHING,
+                ExtensionModuleResolver::FIELD_RESPONSE_REMOVAL,
+                ExtensionModuleResolver::FIELD_TO_INPUT,
+                ExtensionModuleResolver::FIELD_VALUE_ITERATION_AND_MANIPULATION,
+                ExtensionModuleResolver::HELPER_FUNCTION_COLLECTION,
+                ExtensionModuleResolver::HTTP_CLIENT,
+                ExtensionModuleResolver::HTTP_REQUEST_VIA_SCHEMA,
+                ExtensionModuleResolver::INTERNAL_GRAPHQL_SERVER,
+                ExtensionModuleResolver::MULTIPLE_QUERY_EXECUTION,
+                ExtensionModuleResolver::PHP_CONSTANTS_AND_ENVIRONMENT_VARIABLES_VIA_SCHEMA,
+                ExtensionModuleResolver::PHP_FUNCTIONS_VIA_SCHEMA,
+                ExtensionModuleResolver::RESPONSE_ERROR_TRIGGER,
+            ],
+            self::WORDPRESS_AS_API_GATEWAY => [
+                ExtensionModuleResolver::ACCESS_CONTROL,
+                ExtensionModuleResolver::ACCESS_CONTROL_VISITOR_IP,
+                ExtensionModuleResolver::CONDITIONAL_FIELD_MANIPULATION,
+                ExtensionModuleResolver::EMAIL_SENDER,
+                ExtensionModuleResolver::FIELD_DEFAULT_VALUE,
+                ExtensionModuleResolver::FIELD_ON_FIELD,
+                ExtensionModuleResolver::FIELD_RESPONSE_REMOVAL,
+                ExtensionModuleResolver::FIELD_TO_INPUT,
+                ExtensionModuleResolver::FIELD_VALUE_ITERATION_AND_MANIPULATION,
+                ExtensionModuleResolver::HELPER_FUNCTION_COLLECTION,
+                ExtensionModuleResolver::HTTP_CLIENT,
+                ExtensionModuleResolver::HTTP_REQUEST_VIA_SCHEMA,
+                ExtensionModuleResolver::MULTIPLE_QUERY_EXECUTION,
+                ExtensionModuleResolver::PHP_CONSTANTS_AND_ENVIRONMENT_VARIABLES_VIA_SCHEMA,
+                ExtensionModuleResolver::PHP_FUNCTIONS_VIA_SCHEMA,
+                ExtensionModuleResolver::RESPONSE_ERROR_TRIGGER,
+            ],
+            self::WORDPRESS_CONTENT_IMPORT_EXPORT_AND_SYNC => [
+                ExtensionModuleResolver::CONDITIONAL_FIELD_MANIPULATION,
+                ExtensionModuleResolver::FIELD_DEFAULT_VALUE,
+                ExtensionModuleResolver::FIELD_ON_FIELD,
+                ExtensionModuleResolver::FIELD_RESOLUTION_CACHING,
+                ExtensionModuleResolver::FIELD_RESPONSE_REMOVAL,
+                ExtensionModuleResolver::FIELD_TO_INPUT,
+                ExtensionModuleResolver::FIELD_VALUE_ITERATION_AND_MANIPULATION,
+                ExtensionModuleResolver::HELPER_FUNCTION_COLLECTION,
+                ExtensionModuleResolver::HTTP_CLIENT,
+                ExtensionModuleResolver::HTTP_REQUEST_VIA_SCHEMA,
+                ExtensionModuleResolver::MULTIPLE_QUERY_EXECUTION,
+                ExtensionModuleResolver::PHP_CONSTANTS_AND_ENVIRONMENT_VARIABLES_VIA_SCHEMA,
+                ExtensionModuleResolver::PHP_FUNCTIONS_VIA_SCHEMA,
+                ExtensionModuleResolver::RESPONSE_ERROR_TRIGGER,
+            ],
+            self::WORDPRESS_CONTENT_TRANSFORMER => [
+                ExtensionModuleResolver::CONDITIONAL_FIELD_MANIPULATION,
+                ExtensionModuleResolver::FIELD_DEFAULT_VALUE,
+                ExtensionModuleResolver::FIELD_ON_FIELD,
+                ExtensionModuleResolver::FIELD_TO_INPUT,
+                ExtensionModuleResolver::FIELD_VALUE_ITERATION_AND_MANIPULATION,
+                ExtensionModuleResolver::HELPER_FUNCTION_COLLECTION,
+                ExtensionModuleResolver::MULTIPLE_QUERY_EXECUTION,
+                ExtensionModuleResolver::PHP_FUNCTIONS_VIA_SCHEMA,
+            ],
+            self::WORDPRESS_EMAIL_NOTIFICATIONS => [
+                ExtensionModuleResolver::EMAIL_SENDER,
+                ExtensionModuleResolver::FIELD_ON_FIELD,
+                ExtensionModuleResolver::FIELD_TO_INPUT,
+                ExtensionModuleResolver::FIELD_VALUE_ITERATION_AND_MANIPULATION,
+                ExtensionModuleResolver::HELPER_FUNCTION_COLLECTION,
+                ExtensionModuleResolver::MULTIPLE_QUERY_EXECUTION,
+                ExtensionModuleResolver::PHP_CONSTANTS_AND_ENVIRONMENT_VARIABLES_VIA_SCHEMA,
+                ExtensionModuleResolver::PHP_FUNCTIONS_VIA_SCHEMA,
+            ],
+            self::WORDPRESS_REMOTE_REQUEST_AND_PROCESS => [
+                ExtensionModuleResolver::CONDITIONAL_FIELD_MANIPULATION,
+                ExtensionModuleResolver::FIELD_DEFAULT_VALUE,
+                ExtensionModuleResolver::FIELD_ON_FIELD,
+                ExtensionModuleResolver::FIELD_RESOLUTION_CACHING,
+                ExtensionModuleResolver::FIELD_RESPONSE_REMOVAL,
+                ExtensionModuleResolver::FIELD_TO_INPUT,
+                ExtensionModuleResolver::FIELD_VALUE_ITERATION_AND_MANIPULATION,
+                ExtensionModuleResolver::HELPER_FUNCTION_COLLECTION,
+                ExtensionModuleResolver::HTTP_CLIENT,
+                ExtensionModuleResolver::HTTP_REQUEST_VIA_SCHEMA,
+                ExtensionModuleResolver::MULTIPLE_QUERY_EXECUTION,
+                ExtensionModuleResolver::PHP_CONSTANTS_AND_ENVIRONMENT_VARIABLES_VIA_SCHEMA,
+                ExtensionModuleResolver::PHP_FUNCTIONS_VIA_SCHEMA,
+                ExtensionModuleResolver::RESPONSE_ERROR_TRIGGER,
             ],
             default => [],
         };
