9f9dbfda97653f8b45e1f24d01698abb8dae3992
leoloso <leo@getpop.org>
2024-01-18 16:59:32 +0800

    delta MutationInputProperties

--- layers/CMSSchema/packages/media-mutations/src/Constants/MutationInputProperties.php
+++ layers/CMSSchema/packages/media-mutations/src/Constants/MutationInputProperties.php
@@ -8,7 +8,6 @@ class MutationInputProperties
 {
     public final const INPUT = 'input';
     public final const FROM = 'from';
-    public final const CUSTOMPOST_PARENT_ID = 'customPostParentID';
     public final const AUTHOR_ID = 'authorID';
     public final const TITLE = 'title';
     public final const SLUG = 'slug';
