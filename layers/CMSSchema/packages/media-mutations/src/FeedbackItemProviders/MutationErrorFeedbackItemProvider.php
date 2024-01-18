cf0101e32a768bea5efc449591967e386b6937dd
leoloso <leo@getpop.org>
2024-01-18 15:48:06 +0800

    organize errors

--- layers/CMSSchema/packages/media-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
+++ layers/CMSSchema/packages/media-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
@@ -13,11 +13,6 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     public final const E2 = 'e2';
     public final const E3 = 'e3';
     public final const E4 = 'e4';
-    public final const E5 = 'e5';
-    public final const E6 = 'e6';
-    public final const E7 = 'e7';
-    public final const E8 = 'e8';
-    public final const E9 = 'e9';
 
     /**
      * @return string[]
@@ -29,11 +24,6 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
             self::E2,
             self::E3,
             self::E4,
-            self::E5,
-            self::E6,
-            self::E7,
-            self::E8,
-            self::E9,
         ];
     }
 
@@ -41,14 +31,9 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     {
         return match ($code) {
             self::E1 => $this->__('You are not logged in', 'user-state-mutations'),
-            self::E2 => $this->__('The comment author\'s name is missing', 'media-mutations'),
-            self::E3 => $this->__('The comment author\'s email is missing', 'media-mutations'),
-            self::E4 => $this->__('The custom post ID is missing', 'media-mutations'),
-            self::E5 => $this->__('The comment is empty', 'media-mutations'),
-            self::E6 => $this->__('There is no (parent) comment with ID \'%s\'', 'media-mutations'),
-            self::E7 => $this->__('There is no custom post with ID \'%s\'', 'media-mutations'),
-            self::E8 => $this->__('Comments are not supported for custom post type \'%s\'', 'media-mutations'),
-            self::E9 => $this->__('Comments are not open for custom post with ID \'%s\'', 'media-mutations'),
+            self::E2 => $this->__('You don\'t have permission to upload files', 'media-mutations'),
+            self::E3 => $this->__('There is no custom post with ID \'%s\'', 'media-mutations'),
+            self::E4 => $this->__('The attachment source (URL/contents) is empty', 'media-mutations'),
             default => parent::getMessagePlaceholder($code),
         };
     }
