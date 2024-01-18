26a4465e385354c6beed1957cd858b45d726e8ad
leoloso <leo@getpop.org>
2024-01-18 15:17:13 +0800

    MutationInputProperties

--- layers/CMSSchema/packages/media-mutations/src/Constants/MutationInputProperties.php
+++ layers/CMSSchema/packages/media-mutations/src/Constants/MutationInputProperties.php
@@ -7,12 +7,15 @@ namespace PoPCMSSchema\MediaMutations\Constants;
 class MutationInputProperties
 {
     public final const INPUT = 'input';
-    public final const CUSTOMPOST_ID = 'customPostID';
-    public final const COMMENT_AS = 'commentAs';
-    public final const PARENT_COMMENT_ID = 'parentCommentID';
-    public final const AUTHOR_NAME = 'authorName';
-    public final const AUTHOR_EMAIL = 'authorEmail';
-    public final const AUTHOR_URL = 'authorURL';
+    public final const FROM = 'from';
+    public final const CUSTOMPOST_PARENT_ID = 'customPostParentID';
+    public final const AUTHOR_ID = 'authorID';
+    public final const TITLE = 'title';
+    public final const SLUG = 'slug';
+    public final const CAPTION = 'caption';
+    public final const DESCRIPTION = 'description';
+    public final const MIME_TYPE = 'mimeType';
 
-    public final const HTML = 'html';
+    public final const URL = 'url';
+    public final const CONTENTS = 'contents';
 }
