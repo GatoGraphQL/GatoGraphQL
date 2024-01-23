cf55b38230385242a25d19601cdaf4e889f0ca10
leoloso <leo@getpop.org>
2024-01-23 13:26:19 +0800

    log user in in query

--- layers/GatoGraphQLForWP/phpunit-packages/gatographql/tests/Integration/SchemaMutationsFixtureEndpointWebserverRequestTest.php
+++ layers/GatoGraphQLForWP/phpunit-packages/gatographql/tests/Integration/SchemaMutationsFixtureEndpointWebserverRequestTest.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace PHPUnitForGatoGraphQL\GatoGraphQL\Integration;
 
-class SchemaMutationsFixtureEndpointWebserverRequestTest extends AbstractAdminClientQueryExecutionFixtureWebserverRequestTestCase
+class SchemaMutationsFixtureEndpointWebserverRequestTest extends AbstractFixtureEndpointWebserverRequestTestCase
 {
     protected static function getFixtureFolder(): string
     {
         return __DIR__ . '/fixture-schema-mutations';
+    }
+
+    protected static function getEndpoint(): string
+    {
+        return 'graphql/';
     }
 
     /**
