cd7cd24134d2787da75596f084f7362b73cd051d
leoloso <leo@getpop.org>
2024-01-24 09:49:44 +0800

    abstract class

---
+++ layers/Schema/packages/schema-commons/src/TypeResolvers/ScalarType/AbstractURIScalarTypeResolver.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\SchemaCommons\TypeResolvers\ScalarType;
+
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
+use PoP\ComponentModel\TypeResolvers\ScalarType\AbstractScalarTypeResolver;
+use PoP\GraphQLParser\Spec\Parser\Ast\AstInterface;
+use stdClass;
+
+/**
+ * GraphQL Custom Scalar
+ *
+ * @see https://spec.graphql.org/draft/#sec-Scalars.Custom-Scalars
+ */
+abstract class AbstractURIScalarTypeResolver extends AbstractScalarTypeResolver
+{
+    public function coerceValue(
+        string|int|float|bool|stdClass $inputValue,
+        AstInterface $astNode,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): string|int|float|bool|object|null {
+        $errorCount = $objectTypeFieldResolutionFeedbackStore->getErrorCount();
+        $this->validateIsString($inputValue, $astNode, $objectTypeFieldResolutionFeedbackStore);
+        if ($objectTypeFieldResolutionFeedbackStore->getErrorCount() > $errorCount) {
+            return null;
+        }
+        /** @var string $inputValue */
+
+        $this->validateFilterVar($inputValue, $astNode, $objectTypeFieldResolutionFeedbackStore, \FILTER_VALIDATE_URL);
+        if ($objectTypeFieldResolutionFeedbackStore->getErrorCount() > $errorCount) {
+            return null;
+        }
+
+        return $inputValue;
+    }
+}
--- layers/Schema/packages/schema-commons/src/TypeResolvers/ScalarType/URLScalarTypeResolver.php
+++ layers/Schema/packages/schema-commons/src/TypeResolvers/ScalarType/URLScalarTypeResolver.php
@@ -4,17 +4,12 @@ declare(strict_types=1);
 
 namespace PoPSchema\SchemaCommons\TypeResolvers\ScalarType;
 
-use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
-use PoP\ComponentModel\TypeResolvers\ScalarType\AbstractScalarTypeResolver;
-use PoP\GraphQLParser\Spec\Parser\Ast\AstInterface;
-use stdClass;
-
 /**
  * GraphQL Custom Scalar
  *
  * @see https://spec.graphql.org/draft/#sec-Scalars.Custom-Scalars
  */
-class URLScalarTypeResolver extends AbstractScalarTypeResolver
+class URLScalarTypeResolver extends AbstractURIScalarTypeResolver
 {
     public function getTypeName(): string
     {
@@ -29,25 +24,5 @@ class URLScalarTypeResolver extends AbstractScalarTypeResolver
     public function getSpecifiedByURL(): ?string
     {
         return 'https://url.spec.whatwg.org/#url-representation';
-    }
-
-    public function coerceValue(
-        string|int|float|bool|stdClass $inputValue,
-        AstInterface $astNode,
-        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
-    ): string|int|float|bool|object|null {
-        $errorCount = $objectTypeFieldResolutionFeedbackStore->getErrorCount();
-        $this->validateIsString($inputValue, $astNode, $objectTypeFieldResolutionFeedbackStore);
-        if ($objectTypeFieldResolutionFeedbackStore->getErrorCount() > $errorCount) {
-            return null;
-        }
-        /** @var string $inputValue */
-
-        $this->validateFilterVar($inputValue, $astNode, $objectTypeFieldResolutionFeedbackStore, \FILTER_VALIDATE_URL);
-        if ($objectTypeFieldResolutionFeedbackStore->getErrorCount() > $errorCount) {
-            return null;
-        }
-
-        return $inputValue;
     }
 }
