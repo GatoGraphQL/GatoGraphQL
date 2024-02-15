74d864df98552090ffff9a19bfe8c35e36e431b5
leoloso <leo@getpop.org>
2024-02-15 20:37:15 +0800

    prepare release

--- layers/GatoGraphQLForWP/plugins/gatographql/gatographql.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/gatographql.php
@@ -4,7 +4,7 @@ Plugin Name: Gato GraphQL
 Plugin URI: https://gatographql.com
 GitHub Plugin URI: https://github.com/GatoGraphQL/GatoGraphQL
 Description: Interact with all your data in WordPress.
-Version: 2.2.0-dev
+Version: 2.1.2
 Requires at least: 5.4
 Requires PHP: 8.1
 Author: Gato GraphQL
@@ -48,7 +48,7 @@ if (!defined('ABSPATH')) {
  *
  * @gatographql-readonly-code
  */
-$pluginVersion = '2.2.0-dev';
+$pluginVersion = '2.1.2';
 $pluginName = __('Gato GraphQL', 'gatographql');
 
 /**
