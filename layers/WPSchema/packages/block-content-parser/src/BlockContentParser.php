5ed0305d52688f01f03ce460cf72d21d598b4d63
leoloso <leo@getpop.org>
2024-03-15 17:17:54 +0800

    comment

--- layers/WPSchema/packages/block-content-parser/src/BlockContentParser.php
+++ layers/WPSchema/packages/block-content-parser/src/BlockContentParser.php
@@ -411,6 +411,9 @@ class BlockContentParser implements BlockContentParserInterface
 
             $attribute_value = $this->source_block_attribute($crawler, $block_attribute_definition);
         } elseif ('html' === $attribute_source || 'rich-text' === $attribute_source) {
+            // 'html' source was converted to 'rich-text' in WordPress 6.5.
+            // https://github.com/WordPress/gutenberg/commit/6a42225124e69276a2deec4597a855bb504b37cc
+
             $attribute_value = $this->source_block_html($crawler, $block_attribute_definition);
         } elseif ('text' === $attribute_source) {
             $attribute_value = $this->source_block_text($crawler, $block_attribute_definition);
