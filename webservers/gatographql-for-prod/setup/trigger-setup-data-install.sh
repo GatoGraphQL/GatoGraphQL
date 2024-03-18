24c65462c9901e139454736507d35c357c042937
leoloso <leo@getpop.org>
2024-03-18 13:30:00 +0800

    trigger-setup-data-install

---
+++ webservers/gatographql-for-prod/setup/trigger-setup-data-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Call any WordPress page on the site, to trigger the "Install Setup Data" process
+# and generate the entities
+curl -I https://gatographql-for-prod.lndo.site/wp-admin/
