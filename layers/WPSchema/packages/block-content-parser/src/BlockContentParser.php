39cced7868e7cb0dfa4deb8628937c5d8eecfb57
leoloso <leo@getpop.org>
2024-03-20 12:40:34 +0800

    Integrate upstream changes for `source_block_rich_text`

--- layers/WPSchema/packages/block-content-parser/src/BlockContentParser.php
+++ layers/WPSchema/packages/block-content-parser/src/BlockContentParser.php
@@ -410,10 +410,12 @@ class BlockContentParser implements BlockContentParserInterface
             // https://github.com/WordPress/gutenberg/pull/8276
 
             $attribute_value = $this->source_block_attribute($crawler, $block_attribute_definition);
-        } elseif ('html' === $attribute_source || 'rich-text' === $attribute_source) {
-            // 'html' source was converted to 'rich-text' in WordPress 6.5.
-            // https://github.com/WordPress/gutenberg/commit/6a42225124e69276a2deec4597a855bb504b37cc
+        } elseif ('rich-text' === $attribute_source) {
+            // Most 'html' sources were converted to 'rich-text' in WordPress 6.5.
+            // https://github.com/WordPress/gutenberg/pull/43204
 
+            $attribute_value = $this->source_block_rich_text($crawler, $block_attribute_definition);
+        } elseif ('html' === $attribute_source) {
             $attribute_value = $this->source_block_html($crawler, $block_attribute_definition);
         } elseif ('text' === $attribute_source) {
             $attribute_value = $this->source_block_text($crawler, $block_attribute_definition);
@@ -500,6 +502,36 @@ class BlockContentParser implements BlockContentParserInterface
 
         return $attribute_value;
     }
+
+	/**
+	 * Helper function to process the `rich-text` source attribute.
+	 * At present, the main difference from `html` is that `rich-text` does not support multiline selectors.
+	 *
+	 * @param Crawler $crawler Crawler instance.
+	 * @param array<string,mixed> $block_attribute_definition Definition of the block attribute.
+	 *
+     * @return string|null
+     * 
+	 * @access private
+	 */
+	protected function source_block_rich_text(Crawler $crawler, array $block_attribute_definition): ?string
+    {
+		// 'rich-text' sources:
+		// https://github.com/WordPress/gutenberg/blob/6a42225124e69276a2deec4597a855bb504b37cc/packages/blocks/src/api/parser/get-block-attributes.js#L228-L232
+
+		$attribute_value = null;
+		$selector        = $block_attribute_definition['selector'] ?? null;
+
+		if ( null !== $selector ) {
+			$crawler = $crawler->filter( $selector );
+		}
+
+		if ( $crawler->count() > 0 ) {
+			$attribute_value = $crawler->html();
+		}
+
+		return $attribute_value;
+	}
 
     /**
      * @param array<string,mixed> $block_attribute_definition
@@ -736,7 +768,7 @@ class BlockContentParser implements BlockContentParserInterface
      */
     protected function from_dom_node(DOMNode $node): array|string|null
     {
-		// phpcs:disable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase -- external API calls
+        // phpcs:disable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase -- external API calls
 
         if (XML_TEXT_NODE === $node->nodeType) {
             // For plain text nodes, return the text directly
@@ -760,7 +792,7 @@ class BlockContentParser implements BlockContentParserInterface
 
         return null;
 
-		// phpcs:enable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+        // phpcs:enable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
     }
 
     /**
