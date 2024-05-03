af98c9bcdc40d5326334af51915b4e019bd6ac67
leoloso <leo@getpop.org>
2024-05-03 15:04:47 -0300

    Add Integrations Module Type

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/IntegrationsModuleResolverTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\ModuleResolvers;
+
+use GatoGraphQL\GatoGraphQL\Services\ModuleTypeResolvers\ModuleTypeResolver;
+
+trait IntegrationsModuleResolverTrait
+{
+    /**
+     * The priority to display the modules from this resolver in the Modules page.
+     * The higher the number, the earlier it shows
+     */
+    public function getPriority(): int
+    {
+        return 88;
+    }
+
+    /**
+     * The type of the module
+     */
+    public function getModuleType(string $module): string
+    {
+        return ModuleTypeResolver::INTEGRATIONS;
+    }
+}
