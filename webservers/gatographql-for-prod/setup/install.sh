c416ade10a51a8c3a59b5c59375b5bad67a85551
leoloso <leo@getpop.org>
2024-05-10 11:07:00 -0300

    Gato GraphQL for PROD => demo site

--- webservers/gatographql-for-prod/setup/install.sh
+++ webservers/gatographql-for-prod/setup/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wp core install --url="gatographql-for-prod.lndo.site" --title="Gato GraphQL for PROD" --admin_user=admin --admin_password=admin --admin_email=admin@example.com 
+wp core install --url="gatographql-for-prod.lndo.site" --title="Gato GraphQL demo site" --admin_user=admin --admin_password=admin --admin_email=admin@example.com 
 wp option update siteurl "https://gatographql-for-prod.lndo.site" 
 wp option update home "https://gatographql-for-prod.lndo.site" 
 # This change is needed on InstaWP
