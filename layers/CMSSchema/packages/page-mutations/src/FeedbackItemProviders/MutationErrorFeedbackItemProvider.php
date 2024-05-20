1c9afb18bb8a1fa3d94b5f675c29e2d94eb12ec2
leoloso <leo@getpop.org>
2024-05-20 15:55:04 -0300

    removed commented

--- layers/CMSSchema/packages/page-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
+++ layers/CMSSchema/packages/page-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
@@ -9,12 +9,8 @@ use PoP\ComponentModel\Feedback\FeedbackCategories;
 
 class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
 {
-    // public final const E1 = 'e1';
     public final const E2 = 'e2';
     public final const E3 = 'e3';
-    // public final const E6 = 'e6';
-    // public final const E7 = 'e7';
-    // public final const E8 = 'e8';
 
     /**
      * @return string[]
@@ -22,24 +18,16 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     public function getCodes(): array
     {
         return [
-            // self::E1,
             self::E2,
             self::E3,
-            // self::E6,
-            // self::E7,
-            // self::E8,
         ];
     }
 
     public function getMessagePlaceholder(string $code): string
     {
         return match ($code) {
-            // self::E1 => $this->__('You must be logged in to create or update pages', 'custompost-mutations'),
-            self::E2 => $this->__('Your user doesn\'t have permission for editing pages.', 'custompost-mutations'),
-            self::E3 => $this->__('Your user doesn\'t have permission for publishing pages.', 'custompost-mutations'),
-            // self::E6 => $this->__('The page ID is missing', 'custompost-mutations'),
-            // self::E7 => $this->__('There is no page with ID \'%s\'', 'custompost-mutations'),
-            // self::E8 => $this->__('You don\'t have permission to edit page with ID \'%s\'', 'custompost-mutations'),
+            self::E2 => $this->__('Your user doesn\'t have permission for editing pages.', 'page-mutations'),
+            self::E3 => $this->__('Your user doesn\'t have permission for publishing pages.', 'page-mutations'),
             default => parent::getMessagePlaceholder($code),
         };
     }
