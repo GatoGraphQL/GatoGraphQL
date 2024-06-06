e8dc996f0a17afcbae388c1ff3ad88fe849861d7
leoloso <leo@getpop.org>
2024-06-06 12:59:34 -0300

    prepare release

--- layers/GatoGraphQLForWP/phpunit-plugins/gatographql-testing/gatographql-testing.php
+++ layers/GatoGraphQLForWP/phpunit-plugins/gatographql-testing/gatographql-testing.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gato GraphQL - PHPUnit & Testing Utilities
 Description: Utilities for testing Gato GraphQL
-Version: 2.6.0-dev
+Version: 2.5.2
 Requires at least: 5.4
 Requires PHP: 8.1
 Author: Gato GraphQL
