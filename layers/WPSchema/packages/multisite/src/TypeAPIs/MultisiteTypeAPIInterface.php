6105d117c28099052e647c4c0850a54aa8200d3d
leoloso <leo@getpop.org>
2024-06-24 15:43:56 +0800

    MultisiteTypeAPI

---
+++ layers/WPSchema/packages/multisite/src/TypeAPIs/MultisiteTypeAPIInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\Multisite\TypeAPIs;
+
+interface MultisiteTypeAPIInterface
+{
+    public function isInstanceOfNetworkSiteType(object $object): bool;
+
+    /**
+     * @return array<string|int>|object[]
+     * @param array<string,mixed> $query
+     * @param array<string,mixed> $options
+     */
+    public function getNetworkSites(array $query, array $options = []): array;
+    /**
+     * @param array<string,mixed> $query
+     * @param array<string,mixed> $options
+     */
+    public function getNetworkSiteCount(array $query, array $options = []): int;
+}
