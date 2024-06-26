f781b8f24acb2a18cabd3b4d0dc7d06672a690c0
leoloso <leo@getpop.org>
2024-06-26 16:36:58 +0800

    AbstractScalarValueJSONObjectScalarTypeResolver

---
+++ layers/Schema/packages/extended-schema-commons/src/TypeResolvers/ScalarType/AbstractScalarValueJSONObjectScalarTypeResolver.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\ExtendedSchemaCommons\TypeResolvers\ScalarType;
+
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
+use PoP\Engine\TypeResolvers\ScalarType\JSONObjectScalarTypeResolver;
+use PoP\GraphQLParser\Spec\Parser\Ast\AstInterface;
+use stdClass;
+
+/**
+ * GraphQL Custom Scalar representing a JSON Object on the client-side,
+ * handled via an stdClass object on the server-side
+ *
+ * @see https://spec.graphql.org/draft/#sec-Scalars.Custom-Scalars
+ */
+abstract class AbstractScalarValueJSONObjectScalarTypeResolver extends JSONObjectScalarTypeResolver
+{
+    public function getSpecifiedByURL(): ?string
+    {
+        return null;
+    }
+
+    public function coerceValue(
+        string|int|float|bool|stdClass $inputValue,
+        AstInterface $astNode,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): string|int|float|bool|object|null {
+        $inputValue = parent::coerceValue(
+            $inputValue,
+            $astNode,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+        if ($inputValue === null) {
+            return $inputValue;
+        }
+        /** @var stdClass $inputValue */
+
+        $inputValueArray = (array) $inputValue;
+        foreach ($inputValueArray as $key => $value) {
+            /**
+             * If the value is of any of the following types, it can't be
+             * coerced to string:
+             *
+             * - null
+             * - array
+             * - object
+             */
+            if ($value === null || is_array($value) || is_object($value)) {
+                $this->addDefaultError($inputValue, $astNode, $objectTypeFieldResolutionFeedbackStore);
+                return null;
+            }
+            /**
+             * Coerce to the specific scalar type
+             */
+            $inputValue->$key = $this->castJSONObjectPropertyValue($value);
+        }
+        return $inputValue;
+    }
+
+    abstract protected function castJSONObjectPropertyValue(
+        string|int|float|bool $value,
+    ): string|int|float|bool;
+}
