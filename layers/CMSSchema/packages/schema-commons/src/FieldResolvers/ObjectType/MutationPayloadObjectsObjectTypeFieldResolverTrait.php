1b317a5f5389dcd862aa1a76cee786e35bddf3bc
leoloso <leo@getpop.org>
2024-07-03 14:09:37 +0800

    Make sure the IDs really exist

--- layers/CMSSchema/packages/schema-commons/src/FieldResolvers/ObjectType/MutationPayloadObjectsObjectTypeFieldResolverTrait.php
+++ layers/CMSSchema/packages/schema-commons/src/FieldResolvers/ObjectType/MutationPayloadObjectsObjectTypeFieldResolverTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\SchemaCommons\FieldResolvers\ObjectType;
 
+use PoP\ComponentModel\Facades\Dictionaries\ObjectDictionaryFacade;
 use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
 use stdClass;
 
@@ -15,10 +16,23 @@ trait MutationPayloadObjectsObjectTypeFieldResolverTrait
      * @return array<string|int>
      */
     protected function resolveMutationPayloadObjectsValue(
+        string $objectClass,
         FieldDataAccessorInterface $fieldDataAccessor,
     ): array {
         /** @var stdClass */
         $mutationPayloadObjectsInput = $fieldDataAccessor->getValue('input');
-        return $mutationPayloadObjectsInput->ids;
+        $ids = $mutationPayloadObjectsInput->ids;
+        
+        /**
+         * Make sure the IDs really exist (i.e. they are IDs from transient
+         * payload objects, just created)
+         */
+        $objectDictionary = ObjectDictionaryFacade::getInstance();
+        $ids = array_filter(
+            $ids,
+            fn (string|int $id) => $objectDictionary->has($objectClass, $id)
+        );
+        
+        return $ids;
     }
 }
