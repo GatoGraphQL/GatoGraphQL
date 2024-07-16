abceda39b3aad73bd564a8cc3e7253f38f5c4458
leoloso <leo@getpop.org>
2024-07-16 13:42:57 +0800

    append tests: @ to __

--- layers/GatoGraphQLForWP/phpunit-packages/webserver-requests/tests/AbstractThirdPartyPluginDependencyWordPressAuthenticatedUserWebserverRequestTestCase.php
+++ layers/GatoGraphQLForWP/phpunit-packages/webserver-requests/tests/AbstractThirdPartyPluginDependencyWordPressAuthenticatedUserWebserverRequestTestCase.php
@@ -150,14 +150,14 @@ abstract class AbstractThirdPartyPluginDependencyWordPressAuthenticatedUserWebse
 
     /**
      * Support executing many enable/disable plugin tests:
-     * If the $dataName ends with "@{number}" (such as "@1",
-     * or "@2" or etc), strip them off, as they are
+     * If the $dataName ends with "__{number}" (such as "__1",
+     * or "__2" or etc), strip them off, as they are
      * "this is another test of the same plugin".
      *
      * Also, each of those tests can have variations via
      * additional .var.json files. In that case, the name
      * will end with ":{number}", like "test:1.var.json"
-     * or "test+1:1.var.json"
+     * or "test__1:1.var.json"
      */
     protected function getPluginNameFromDataName(string $dataName): string
     {
@@ -171,7 +171,7 @@ abstract class AbstractThirdPartyPluginDependencyWordPressAuthenticatedUserWebse
             break;
         }
         $matches = [];
-        if (preg_match('/(.*)@\d+(\:\d+)?/', $pluginName, $matches)) {
+        if (preg_match('/(.*)__\d+(\:\d+)?/', $pluginName, $matches)) {
             return $matches[1];
         }
         return $pluginName;
