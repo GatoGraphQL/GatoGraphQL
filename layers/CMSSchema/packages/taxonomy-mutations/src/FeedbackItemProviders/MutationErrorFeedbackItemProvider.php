42dca5e293c1a0356600f13c0e9a414e2e80833d
leoloso <leo@getpop.org>
2024-07-31 14:34:50 +0800

    error msg

--- layers/CMSSchema/packages/taxonomy-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
+++ layers/CMSSchema/packages/taxonomy-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
@@ -11,6 +11,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
 {
     public final const E1 = 'e1';
     public final const E2 = 'e2';
+    public final const E3 = 'e3';
     public final const E6 = 'e6';
     public final const E7 = 'e7';
     public final const E8 = 'e8';
@@ -23,6 +24,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
         return [
             self::E1,
             self::E2,
+            self::E3,
             self::E6,
             self::E7,
             self::E8,
@@ -33,7 +35,8 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     {
         return match ($code) {
             self::E1 => $this->__('You must be logged in to mutate taxonomy terms', 'taxonomy-mutations'),
-            self::E2 => $this->__('Your user doesn\'t have permission for editing taxonomies.', 'taxonomy-mutations'),
+            self::E2 => $this->__('Your user doesn\'t have permission for editing taxonomy terms.', 'taxonomy-mutations'),
+            self::E3 => $this->__('Your user doesn\'t have permission for deleting taxonomy term with ID \'%s\'.', 'taxonomy-mutations'),
             self::E6 => $this->__('The taxonomy ID is missing', 'taxonomy-mutations'),
             self::E7 => $this->__('There is no taxonomy with ID \'%s\'', 'taxonomy-mutations'),
             self::E8 => $this->__('There is no taxonomy with slug \'%s\'', 'taxonomy-mutations'),
