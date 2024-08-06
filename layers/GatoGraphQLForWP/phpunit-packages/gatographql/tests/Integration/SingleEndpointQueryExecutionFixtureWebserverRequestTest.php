1e727daba041d5858986c19e500fd7e4888b373b
leoloso <leo@getpop.org>
2024-08-06 14:59:35 +0800

    Migrated to AppPassword

--- layers/GatoGraphQLForWP/phpunit-packages/gatographql/tests/Integration/SingleEndpointQueryExecutionFixtureWebserverRequestTest.php
+++ layers/GatoGraphQLForWP/phpunit-packages/gatographql/tests/Integration/SingleEndpointQueryExecutionFixtureWebserverRequestTest.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace PHPUnitForGatoGraphQL\GatoGraphQL\Integration;
 
-class SingleEndpointQueryExecutionFixtureWebserverRequestTest extends AbstractAdminClientQueryExecutionFixtureWebserverRequestTestCase
+class SingleEndpointQueryExecutionFixtureWebserverRequestTest extends AbstractApplicationPasswordQueryExecutionFixtureWebserverRequestTestCase
 {
     protected static function getFixtureFolder(): string
     {
         return __DIR__ . '/fixture-single-endpoint';
+    }
+
+    protected static function getEndpoint(): string
+    {
+        return 'graphql/';
     }
 }
