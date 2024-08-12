02427f6cd0039a659c41a43ab6739f4021e4fe44
leoloso <leo@getpop.org>
2024-08-12 16:35:00 +0800

    Use getLogsFilePath in the logger

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Log/Logger.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Log/Logger.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\Log;
 
+use GatoGraphQL\GatoGraphQL\PluginEnvironment;
+
 class Logger implements LoggerInterface
 {
     public function logError(string $message): void
@@ -14,11 +16,14 @@ class Logger implements LoggerInterface
         ));
     }
 
+    /**
+     * @see https://stackoverflow.com/a/7655379
+     */
     public function logInfo(string $message): void
     {
-        \syslog(LOG_INFO, sprintf(
+        \error_log(sprintf(
             '[Gato GraphQL] %s',
             $message
-        ));
+        ), 3, PluginEnvironment::getLogsFilePath('logs.txt'));
     }
 }
