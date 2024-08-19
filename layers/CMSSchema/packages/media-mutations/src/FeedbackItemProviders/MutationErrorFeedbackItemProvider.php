5c6bded858b0067ab1dce423b43c68a6f67428aa
leoloso <leo@getpop.org>
2024-08-19 18:08:11 +0800

    replaced error

--- layers/CMSSchema/packages/media-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
+++ layers/CMSSchema/packages/media-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
@@ -16,6 +16,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     public final const E6 = 'e6';
     public final const E7 = 'e7';
     public final const E8 = 'e8';
+    public final const E9 = 'e9';
 
     /**
      * @return string[]
@@ -30,6 +31,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
             self::E6,
             self::E7,
             self::E8,
+            self::E9,
         ];
     }
 
@@ -43,6 +45,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
             self::E6 => $this->__('There is no media item with ID \'%s\'', 'media-mutations'),
             self::E7 => $this->__('There is no media item with slug \'%s\'', 'media-mutations'),
             self::E8 => $this->__('You don\'t have permission to edit media item with id \'%s\'', 'media-mutations'),
+            self::E9 => $this->__('You don\'t have permission to edit media items', 'polylang-mutations'),
             default => parent::getMessagePlaceholder($code),
         };
     }
