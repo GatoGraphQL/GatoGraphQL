df8bc21b96236fcdb302d973ceb8b0d520082461
leoloso <leo@getpop.org>
2024-08-19 08:47:17 +0800

    dataloader

---
+++ layers/CMSSchema/packages/media-mutations/src/RelationalTypeDataLoaders/UnionType/RootUpdateMediaItemMutationErrorPayloadUnionTypeDataLoader.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\MediaMutations\RelationalTypeDataLoaders\UnionType;
+
+use PoPCMSSchema\MediaMutations\TypeResolvers\UnionType\RootUpdateMediaItemMutationErrorPayloadUnionTypeResolver;
+use PoP\ComponentModel\RelationalTypeDataLoaders\UnionType\AbstractUnionTypeDataLoader;
+use PoP\ComponentModel\TypeResolvers\UnionType\UnionTypeResolverInterface;
+
+class RootUpdateMediaItemMutationErrorPayloadUnionTypeDataLoader extends AbstractUnionTypeDataLoader
+{
+    private ?RootUpdateMediaItemMutationErrorPayloadUnionTypeResolver $rootUpdateMediaItemMutationErrorPayloadUnionTypeResolver = null;
+
+    final public function setRootUpdateMediaItemMutationErrorPayloadUnionTypeResolver(RootUpdateMediaItemMutationErrorPayloadUnionTypeResolver $rootUpdateMediaItemMutationErrorPayloadUnionTypeResolver): void
+    {
+        $this->rootUpdateMediaItemMutationErrorPayloadUnionTypeResolver = $rootUpdateMediaItemMutationErrorPayloadUnionTypeResolver;
+    }
+    final protected function getRootUpdateMediaItemMutationErrorPayloadUnionTypeResolver(): RootUpdateMediaItemMutationErrorPayloadUnionTypeResolver
+    {
+        if ($this->rootUpdateMediaItemMutationErrorPayloadUnionTypeResolver === null) {
+            /** @var RootUpdateMediaItemMutationErrorPayloadUnionTypeResolver */
+            $rootUpdateMediaItemMutationErrorPayloadUnionTypeResolver = $this->instanceManager->getInstance(RootUpdateMediaItemMutationErrorPayloadUnionTypeResolver::class);
+            $this->rootUpdateMediaItemMutationErrorPayloadUnionTypeResolver = $rootUpdateMediaItemMutationErrorPayloadUnionTypeResolver;
+        }
+        return $this->rootUpdateMediaItemMutationErrorPayloadUnionTypeResolver;
+    }
+
+    protected function getUnionTypeResolver(): UnionTypeResolverInterface
+    {
+        return $this->getRootUpdateMediaItemMutationErrorPayloadUnionTypeResolver();
+    }
+}
