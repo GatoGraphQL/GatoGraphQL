e0b68f7fd36943a82e0582f567bba85520edfa44
leoloso <leo@getpop.org>
2024-08-20 18:01:45 +0800

    adaptResponseBody

--- layers/GatoGraphQLForWP/phpunit-packages/gatographql/tests/Integration/MyContentQueryExecutionFixtureWebserverRequestTest.php
+++ layers/GatoGraphQLForWP/phpunit-packages/gatographql/tests/Integration/MyContentQueryExecutionFixtureWebserverRequestTest.php
@@ -30,4 +30,22 @@ class MyContentQueryExecutionFixtureWebserverRequestTest extends AbstractFixture
     {
         return Environment::getIntegrationTestsAuthenticatedContributorUserPassword();
     }
+
+    /**
+     * As the response contains the URL of the endpoint,
+     * modify it so that it works for both "Integration Tests"
+     * and "PROD Integration Tests", always printing the domain
+     * from "Integration Tests" (as in the fixture .json file)
+     */
+    protected function adaptResponseBody(string $responseBody): string
+    {
+        return match (static::getDataName()) {
+            'media-items' => str_replace(
+                Environment::getIntegrationTestsWebserverDomain(),
+                'gatographql.lndo.site',
+                $responseBody
+            ),
+            default => parent::adaptResponseBody($responseBody),
+        };
+    }
 }
