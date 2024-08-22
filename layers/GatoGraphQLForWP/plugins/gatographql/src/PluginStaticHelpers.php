0f5ab07c297e2c9c554ea8fcffa5ce701352cdcf
leoloso <leo@getpop.org>
2024-08-22 13:50:01 +0800

    Reuse isWordPressPluginActive

--- layers/GatoGraphQLForWP/plugins/gatographql/src/PluginStaticHelpers.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/PluginStaticHelpers.php
@@ -7,20 +7,11 @@ namespace GatoGraphQL\GatoGraphQL;
 use GatoGraphQL\ExternalDependencyWrappers\Composer\Semver\SemverWrapper;
 use GatoGraphQL\GatoGraphQL\StaticHelpers\PluginVersionHelpers;
 
+use PoPWPSchema\SchemaCommons\StaticHelpers\WordPressStaticHelpers;
 use function get_file_data;
-use function is_multisite;
 
 class PluginStaticHelpers
 {
-    /**
-     * @var string[]|null
-     */
-    private static ?array $activeWordPressPluginFiles = null;
-    /**
-     * @var string[]|null
-     */
-    private static ?array $activeWordPressPluginSlugs = null;
-
     private static function getGitHubRepoDocsRootURL(): string
     {
         return sprintf(
@@ -49,51 +40,7 @@ class PluginStaticHelpers
      */
     public static function isWordPressPluginActive(string $pluginFileOrSlug): bool
     {
-        if (str_ends_with($pluginFileOrSlug, '.php')) {
-            $pluginFile = $pluginFileOrSlug;
-            return in_array($pluginFile, static::getActiveWordPressPluginFiles());
-        }
-        $pluginSlug = $pluginFileOrSlug;
-        return in_array($pluginSlug, static::getActiveWordPressPluginSlugs());
-    }
-
-    /**
-     * @return string[]
-     */
-    protected static function getActiveWordPressPluginFiles(): array
-    {
-        if (self::$activeWordPressPluginFiles === null) {
-            self::$activeWordPressPluginFiles = get_option('active_plugins', []);
-            if (is_multisite()) {
-                /** @var string[] */
-                $activeNetworkWordPressPluginFiles = array_keys(get_network_option(0, 'active_sitewide_plugins', []));
-                self::$activeWordPressPluginFiles = array_values(array_unique(array_merge(
-                    self::$activeWordPressPluginFiles,
-                    $activeNetworkWordPressPluginFiles
-                )));
-            }
-        }
-        return self::$activeWordPressPluginFiles;
-    }
-
-    /**
-     * @return string[]
-     */
-    protected static function getActiveWordPressPluginSlugs(): array
-    {
-        if (self::$activeWordPressPluginSlugs === null) {
-            self::$activeWordPressPluginSlugs = array_map(
-                function (string $pluginFile): string {
-                    $pos = strpos($pluginFile, '/');
-                    if ($pos === false) {
-                        return $pluginFile;
-                    }
-                    return substr($pluginFile, 0, $pos);
-                },
-                static::getActiveWordPressPluginFiles()
-            );
-        }
-        return self::$activeWordPressPluginSlugs;
+        return WordPressStaticHelpers::isWordPressPluginActive($pluginFileOrSlug);
     }
 
     public static function doesActivePluginSatisfyVersionConstraint(
