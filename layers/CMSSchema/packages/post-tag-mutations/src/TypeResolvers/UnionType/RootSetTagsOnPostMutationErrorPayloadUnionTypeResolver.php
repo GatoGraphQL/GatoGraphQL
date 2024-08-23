e9487ba262ef75b70d682e1002d4823dc0422da7
leoloso <leo@getpop.org>
2024-08-23 16:58:54 +0800

    desc

--- layers/CMSSchema/packages/post-tag-mutations/src/TypeResolvers/UnionType/RootSetTagsOnPostMutationErrorPayloadUnionTypeResolver.php
+++ layers/CMSSchema/packages/post-tag-mutations/src/TypeResolvers/UnionType/RootSetTagsOnPostMutationErrorPayloadUnionTypeResolver.php
@@ -32,7 +32,7 @@ class RootSetTagsOnPostMutationErrorPayloadUnionTypeResolver extends AbstractPos
 
     public function getTypeDescription(): ?string
     {
-        return $this->__('Union of \'Error Payload\' types when setting tags on a custom post', 'posttag-mutations');
+        return $this->__('Union of \'Error Payload\' types when setting tags on a post', 'posttag-mutations');
     }
 
     public function getRelationalTypeDataLoader(): RelationalTypeDataLoaderInterface
