a94a84e00f97970736040ed0991cc6ffac21baa5
leoloso <leo@getpop.org>
2024-08-23 19:59:07 +0800

    removed GenericCustomPostCategoryTypeMutationAPIInterface

--- layers/CMSSchema/packages/custompost-category-mutations/src/Hooks/MutationResolverHookSet.php
+++ layers/CMSSchema/packages/custompost-category-mutations/src/Hooks/MutationResolverHookSet.php
@@ -7,27 +7,11 @@ namespace PoPCMSSchema\CustomPostCategoryMutations\Hooks;
 use PoPCMSSchema\Categories\TypeAPIs\CategoryTypeAPIInterface;
 use PoPCMSSchema\Categories\TypeAPIs\QueryableCategoryTypeAPIInterface;
 use PoPCMSSchema\CustomPostCategoryMutations\Hooks\AbstractMutationResolverHookSet;
-use PoPCMSSchema\CustomPostCategoryMutations\TypeAPIs\CustomPostCategoryTypeMutationAPIInterface;
-use PoPCMSSchema\CustomPostCategoryMutations\TypeAPIs\GenericCustomPostCategoryTypeMutationAPIInterface;
 
 class MutationResolverHookSet extends AbstractMutationResolverHookSet
 {
-    private ?GenericCustomPostCategoryTypeMutationAPIInterface $genericCustomPostCategoryTypeMutationAPI = null;
     private ?QueryableCategoryTypeAPIInterface $queryableCategoryTypeAPI = null;
 
-    final public function setGenericCustomPostCategoryTypeMutationAPI(GenericCustomPostCategoryTypeMutationAPIInterface $genericCustomPostCategoryTypeMutationAPI): void
-    {
-        $this->genericCustomPostCategoryTypeMutationAPI = $genericCustomPostCategoryTypeMutationAPI;
-    }
-    final protected function getGenericCustomPostCategoryTypeMutationAPI(): GenericCustomPostCategoryTypeMutationAPIInterface
-    {
-        if ($this->genericCustomPostCategoryTypeMutationAPI === null) {
-            /** @var GenericCustomPostCategoryTypeMutationAPIInterface */
-            $genericCustomPostCategoryTypeMutationAPI = $this->instanceManager->getInstance(GenericCustomPostCategoryTypeMutationAPIInterface::class);
-            $this->genericCustomPostCategoryTypeMutationAPI = $genericCustomPostCategoryTypeMutationAPI;
-        }
-        return $this->genericCustomPostCategoryTypeMutationAPI;
-    }
     final public function setQueryableCategoryTypeAPI(QueryableCategoryTypeAPIInterface $queryableCategoryTypeAPI): void
     {
         $this->queryableCategoryTypeAPI = $queryableCategoryTypeAPI;
@@ -45,11 +29,6 @@ class MutationResolverHookSet extends AbstractMutationResolverHookSet
     protected function getCustomPostType(): string
     {
         return '';
-    }
-
-    protected function getCustomPostCategoryTypeMutationAPI(): CustomPostCategoryTypeMutationAPIInterface
-    {
-        return $this->getGenericCustomPostCategoryTypeMutationAPI();
     }
 
     protected function getCategoryTypeAPI(): CategoryTypeAPIInterface
