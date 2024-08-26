ccc50d81873860957a41b28fdc2eb83c7ab396cb
leoloso <leo@getpop.org>
2024-08-26 08:09:48 +0800

    removed unneeded

--- layers/CMSSchema/packages/custompost-tag-mutations/src/Hooks/MutationResolverHookSet.php
+++ layers/CMSSchema/packages/custompost-tag-mutations/src/Hooks/MutationResolverHookSet.php
@@ -7,26 +7,11 @@ namespace PoPCMSSchema\CustomPostTagMutations\Hooks;
 use PoPCMSSchema\Tags\TypeAPIs\TagTypeAPIInterface;
 use PoPCMSSchema\Tags\TypeAPIs\QueryableTagTypeAPIInterface;
 use PoPCMSSchema\CustomPostTagMutations\Hooks\AbstractMutationResolverHookSet;
-use PoPCMSSchema\CustomPostTagMutations\TypeAPIs\GenericCustomPostTagTypeMutationAPIInterface;
 
 class MutationResolverHookSet extends AbstractMutationResolverHookSet
 {
-    private ?GenericCustomPostTagTypeMutationAPIInterface $genericCustomPostTagTypeMutationAPI = null;
     private ?QueryableTagTypeAPIInterface $queryableTagTypeAPI = null;
 
-    final public function setGenericCustomPostTagTypeMutationAPI(GenericCustomPostTagTypeMutationAPIInterface $genericCustomPostTagTypeMutationAPI): void
-    {
-        $this->genericCustomPostTagTypeMutationAPI = $genericCustomPostTagTypeMutationAPI;
-    }
-    final protected function getGenericCustomPostTagTypeMutationAPI(): GenericCustomPostTagTypeMutationAPIInterface
-    {
-        if ($this->genericCustomPostTagTypeMutationAPI === null) {
-            /** @var GenericCustomPostTagTypeMutationAPIInterface */
-            $genericCustomPostTagTypeMutationAPI = $this->instanceManager->getInstance(GenericCustomPostTagTypeMutationAPIInterface::class);
-            $this->genericCustomPostTagTypeMutationAPI = $genericCustomPostTagTypeMutationAPI;
-        }
-        return $this->genericCustomPostTagTypeMutationAPI;
-    }
     final public function setQueryableTagTypeAPI(QueryableTagTypeAPIInterface $queryableTagTypeAPI): void
     {
         $this->queryableTagTypeAPI = $queryableTagTypeAPI;
