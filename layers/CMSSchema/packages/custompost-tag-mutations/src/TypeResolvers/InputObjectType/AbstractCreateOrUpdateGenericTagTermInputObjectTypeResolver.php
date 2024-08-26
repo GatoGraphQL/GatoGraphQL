2f7c7fe5b0c5d6638b8d6d68c1c441cc4a7cab1e
leoloso <leo@getpop.org>
2024-08-26 15:20:35 +0800

    add taxonomy to inputs

--- layers/CMSSchema/packages/custompost-tag-mutations/src/TypeResolvers/InputObjectType/AbstractCreateOrUpdateGenericTagTermInputObjectTypeResolver.php
+++ layers/CMSSchema/packages/custompost-tag-mutations/src/TypeResolvers/InputObjectType/AbstractCreateOrUpdateGenericTagTermInputObjectTypeResolver.php
@@ -5,7 +5,53 @@ declare(strict_types=1);
 namespace PoPCMSSchema\CustomPostTagMutations\TypeResolvers\InputObjectType;
 
 use PoPCMSSchema\TagMutations\TypeResolvers\InputObjectType\AbstractCreateOrUpdateTagTermInputObjectTypeResolver;
+use PoPCMSSchema\Tags\TypeResolvers\EnumType\TagTaxonomyEnumStringScalarTypeResolver;
+use PoPCMSSchema\TaxonomyMutations\TypeResolvers\InputObjectType\MutateGenericTaxonomyTermInputObjectTypeResolverTrait;
+use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
 
 abstract class AbstractCreateOrUpdateGenericTagTermInputObjectTypeResolver extends AbstractCreateOrUpdateTagTermInputObjectTypeResolver implements UpdateGenericTagTermInputObjectTypeResolverInterface, CreateGenericTagTermInputObjectTypeResolverInterface
 {
+    use MutateGenericTaxonomyTermInputObjectTypeResolverTrait;
+
+    private ?TagTaxonomyEnumStringScalarTypeResolver $tagTaxonomyEnumStringScalarTypeResolver = null;
+
+    final public function setTagTaxonomyEnumStringScalarTypeResolver(TagTaxonomyEnumStringScalarTypeResolver $tagTaxonomyEnumStringScalarTypeResolver): void
+    {
+        $this->tagTaxonomyEnumStringScalarTypeResolver = $tagTaxonomyEnumStringScalarTypeResolver;
+    }
+    final protected function getTagTaxonomyEnumStringScalarTypeResolver(): TagTaxonomyEnumStringScalarTypeResolver
+    {
+        if ($this->tagTaxonomyEnumStringScalarTypeResolver === null) {
+            /** @var TagTaxonomyEnumStringScalarTypeResolver */
+            $tagTaxonomyEnumStringScalarTypeResolver = $this->instanceManager->getInstance(TagTaxonomyEnumStringScalarTypeResolver::class);
+            $this->tagTaxonomyEnumStringScalarTypeResolver = $tagTaxonomyEnumStringScalarTypeResolver;
+        }
+        return $this->tagTaxonomyEnumStringScalarTypeResolver;
+    }
+
+    /**
+     * @return array<string,InputTypeResolverInterface>
+     */
+    public function getInputFieldNameTypeResolvers(): array
+    {
+        return array_merge(
+            parent::getInputFieldNameTypeResolvers(),
+            $this->getGenericTaxonomyTermInputFieldNameTypeResolvers()
+        );
+    }
+
+    public function getInputFieldDescription(string $inputFieldName): ?string
+    {
+        return $this->getGenericTaxonomyTermInputFieldDescription($inputFieldName) ?? parent::getInputFieldDescription($inputFieldName);
+    }
+
+    public function getInputFieldTypeModifiers(string $inputFieldName): int
+    {
+        return $this->getGenericTaxonomyTermInputFieldTypeModifiers($inputFieldName) ?? parent::getInputFieldTypeModifiers($inputFieldName);
+    }
+
+    protected function getGenericTaxonomyTermTaxonomyInputTypeResolver(): InputTypeResolverInterface
+    {
+        return $this->getTagTaxonomyEnumStringScalarTypeResolver();
+    }
 }
