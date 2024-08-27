d25d2ae440699c2e809625103a135507975f9723
leoloso <leo@getpop.org>
2024-08-27 18:08:41 +0800

    Remove unused Generic tag/cat CRUD interfaces

--- layers/CMSSchema/packages/tag-mutations/src/TypeResolvers/InputObjectType/UpdateGenericTagTermInputObjectTypeResolverInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace PoPCMSSchema\TagMutations\TypeResolvers\InputObjectType;
-
-interface UpdateGenericTagTermInputObjectTypeResolverInterface extends UpdateTagTermInputObjectTypeResolverInterface
-{
-}
