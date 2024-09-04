e04bf99a052c5ab69c663394843d089d596a3658
leoloso <leo@getpop.org>
2024-09-04 16:22:21 +0800

    getTagTaxonomies in trait

--- layers/CMSSchema/packages/tags/src/ObjectTypeResolverPickers/TagObjectTypeResolverPickerTrait.php
+++ layers/CMSSchema/packages/tags/src/ObjectTypeResolverPickers/TagObjectTypeResolverPickerTrait.php
@@ -10,14 +10,17 @@ use PoP\ComponentModel\App;
 
 trait TagObjectTypeResolverPickerTrait
 {
-    abstract public function getTagTaxonomy(): string;
+    /**
+     * @return string[]
+     */
+    abstract public function getTagTaxonomies(): array;
 
     public function isServiceEnabled(): bool
     {
         /** @var ModuleConfiguration */
         $moduleConfiguration = App::getModule(Module::class)->getConfiguration();
         return in_array(
-            $this->getTagTaxonomy(),
+            $this->getTagTaxonomies(),
             $moduleConfiguration->getQueryableTagTaxonomies()
         );
     }
