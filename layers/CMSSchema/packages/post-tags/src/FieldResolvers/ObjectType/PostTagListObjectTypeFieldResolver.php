ce500c22230de3959354debc3c61ed1a997608eb
leoloso <leo@getpop.org>
2024-09-05 13:42:14 +0800

    Fix passing taxonomy to query

--- layers/CMSSchema/packages/post-tags/src/FieldResolvers/ObjectType/PostTagListObjectTypeFieldResolver.php
+++ layers/CMSSchema/packages/post-tags/src/FieldResolvers/ObjectType/PostTagListObjectTypeFieldResolver.php
@@ -4,13 +4,30 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\PostTags\FieldResolvers\ObjectType;
 
+use PoPCMSSchema\PostTags\TypeResolvers\ObjectType\PostTagObjectTypeResolver;
+use PoPCMSSchema\Posts\FieldResolvers\ObjectType\AbstractPostObjectTypeFieldResolver;
+use PoPCMSSchema\Taxonomies\TypeAPIs\TaxonomyTermTypeAPIInterface;
 use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
 use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
-use PoPCMSSchema\Posts\FieldResolvers\ObjectType\AbstractPostObjectTypeFieldResolver;
-use PoPCMSSchema\PostTags\TypeResolvers\ObjectType\PostTagObjectTypeResolver;
 
 class PostTagListObjectTypeFieldResolver extends AbstractPostObjectTypeFieldResolver
 {
+    private ?TaxonomyTermTypeAPIInterface $taxonomyTermTypeAPI = null;
+    
+    final public function setTaxonomyTermTypeAPI(TaxonomyTermTypeAPIInterface $taxonomyTermTypeAPI): void
+    {
+        $this->taxonomyTermTypeAPI = $taxonomyTermTypeAPI;
+    }
+    final protected function getTaxonomyTermTypeAPI(): TaxonomyTermTypeAPIInterface
+    {
+        if ($this->taxonomyTermTypeAPI === null) {
+            /** @var TaxonomyTermTypeAPIInterface */
+            $taxonomyTermTypeAPI = $this->instanceManager->getInstance(TaxonomyTermTypeAPIInterface::class);
+            $this->taxonomyTermTypeAPI = $taxonomyTermTypeAPI;
+        }
+        return $this->taxonomyTermTypeAPI;
+    }
+    
     /**
      * @return array<class-string<ObjectTypeResolverInterface>>
      */
@@ -45,6 +62,7 @@ class PostTagListObjectTypeFieldResolver extends AbstractPostObjectTypeFieldReso
             case 'posts':
             case 'postCount':
                 $query['tag-ids'] = [$objectTypeResolver->getID($tag)];
+                $query['tag-taxonomy'] = $this->getTaxonomyTermTypeAPI()->getTermTaxonomyName($tag);
                 break;
         }
 
