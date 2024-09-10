5276860402c6c934eea723c5cbad167ab8c7e38f
leoloso <leo@getpop.org>
2024-09-10 20:49:41 +0800

    Migrated Services\EndpointAnnotators

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Services/EndpointAnnotators/CustomEndpointAnnotatorServiceTagInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace GatoGraphQL\GatoGraphQL\Services\EndpointAnnotators;
-
-interface CustomEndpointAnnotatorServiceTagInterface extends EndpointAnnotatorInterface
-{
-}
