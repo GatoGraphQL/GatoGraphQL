c13a47453f42d9a98f677469987ee2d70d0a3dd9
leoloso <leo@getpop.org>
2024-09-10 20:36:55 +0800

    Migrated Registries

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Registries/PersistedQueryEndpointAnnotatorRegistryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace GatoGraphQL\GatoGraphQL\Registries;
-
-interface PersistedQueryEndpointAnnotatorRegistryInterface extends EndpointAnnotatorRegistryInterface
-{
-}
