1c0d5deea727a0f14143302065c172b4f520a87c
leoloso <leo@getpop.org>
2024-09-10 17:53:45 +0800

    Migrated editor-scripts

--- layers/GatoGraphQLForWP/plugins/gatographql/editor-scripts/custom-endpoint-overview/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '0ce392b3b743a6b3ae2f');
