9f1cfc19534aae58462d15afffe2963c649b2188
leoloso <leo@getpop.org>
2024-09-11 17:47:00 +0800

    UpstreamPersistedQueryEndpointEditorBlockServiceTagInterface

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Services/Blocks/PersistedQueryEndpointEditorBlockServiceTagInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Services\Blocks;
+
+use GatoGraphQL\GatoGraphQL\Services\Blocks\EditorBlockInterface;
+
+interface PersistedQueryEndpointEditorBlockServiceTagInterface extends EditorBlockInterface
+{
+}
