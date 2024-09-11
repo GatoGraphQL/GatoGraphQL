390fe6d27d3fbf750ab95453d765d21e85ce88f3
leoloso <leo@getpop.org>
2024-09-11 18:09:03 +0800

    Migrated EndpointVoyagerBlock

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Services/Blocks/AbstractEndpointVoyagerBlock.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Services/Blocks/AbstractEndpointVoyagerBlock.php
@@ -5,49 +5,20 @@ declare(strict_types=1);
 namespace GatoGraphQL\GatoGraphQL\Services\Blocks;
 
 use GatoGraphQL\GatoGraphQL\Constants\BlockAttributeNames;
-use GatoGraphQL\GatoGraphQL\ModuleResolvers\ClientFunctionalityModuleResolver;
-use GatoGraphQL\GatoGraphQL\Services\BlockCategories\BlockCategoryInterface;
-use GatoGraphQL\GatoGraphQL\Services\BlockCategories\CustomEndpointBlockCategory;
 
-class EndpointVoyagerBlock extends AbstractBlock implements EndpointEditorBlockServiceTagInterface
+abstract class AbstractEndpointVoyagerBlock extends AbstractBlock implements EndpointEditorBlockServiceTagInterface
 {
     use MainPluginBlockTrait;
     use OptionsBlockTrait;
-
-    private ?CustomEndpointBlockCategory $customEndpointBlockCategory = null;
-
-    final public function setCustomEndpointBlockCategory(CustomEndpointBlockCategory $customEndpointBlockCategory): void
-    {
-        $this->customEndpointBlockCategory = $customEndpointBlockCategory;
-    }
-    final protected function getCustomEndpointBlockCategory(): CustomEndpointBlockCategory
-    {
-        if ($this->customEndpointBlockCategory === null) {
-            /** @var CustomEndpointBlockCategory */
-            $customEndpointBlockCategory = $this->instanceManager->getInstance(CustomEndpointBlockCategory::class);
-            $this->customEndpointBlockCategory = $customEndpointBlockCategory;
-        }
-        return $this->customEndpointBlockCategory;
-    }
 
     protected function getBlockName(): string
     {
         return 'endpoint-voyager';
     }
 
-    public function getEnablingModule(): ?string
-    {
-        return ClientFunctionalityModuleResolver::INTERACTIVE_SCHEMA_FOR_CUSTOM_ENDPOINTS;
-    }
-
     public function getBlockPriority(): int
     {
         return 120;
-    }
-
-    protected function getBlockCategory(): ?BlockCategoryInterface
-    {
-        return $this->getCustomEndpointBlockCategory();
     }
 
     protected function isDynamicBlock(): bool
