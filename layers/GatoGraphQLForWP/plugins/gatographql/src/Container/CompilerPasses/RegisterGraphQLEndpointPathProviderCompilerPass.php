4dcd144757363e212c23332a1564fe5c891fd9f1
leoloso <leo@getpop.org>
2024-09-12 16:02:21 +0800

    fixed bug

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Container/CompilerPasses/RegisterGraphQLEndpointPathProviderCompilerPass.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Container/CompilerPasses/RegisterGraphQLEndpointPathProviderCompilerPass.php
@@ -4,22 +4,22 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\Container\CompilerPasses;
 
-use GatoGraphQL\GatoGraphQL\Registries\TaxonomyRegistryInterface;
-use GatoGraphQL\GatoGraphQL\Services\Taxonomies\TaxonomyInterface;
+use GatoGraphQL\GatoGraphQL\Registries\GraphQLEndpointPathProviderRegistryInterface;
+use GatoGraphQL\GatoGraphQL\Services\GraphQLEndpointPathProviders\GraphQLEndpointPathProviderInterface;
 use PoP\Root\Container\CompilerPasses\AbstractInjectServiceIntoRegistryCompilerPass;
 
 class RegisterGraphQLEndpointPathProviderCompilerPass extends AbstractInjectServiceIntoRegistryCompilerPass
 {
     protected function getRegistryServiceDefinition(): string
     {
-        return TaxonomyRegistryInterface::class;
+        return GraphQLEndpointPathProviderRegistryInterface::class;
     }
     protected function getServiceClass(): string
     {
-        return TaxonomyInterface::class;
+        return GraphQLEndpointPathProviderInterface::class;
     }
     protected function getRegistryMethodCallName(): string
     {
-        return 'addTaxonomy';
+        return 'addGraphQLEndpointPathProvider';
     }
 }
