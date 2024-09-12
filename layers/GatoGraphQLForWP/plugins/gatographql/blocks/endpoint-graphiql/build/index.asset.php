210da54df87a359b568eb4f31fb73fd71d28e66d
leoloso <leo@getpop.org>
2024-09-12 10:43:34 +0800

    Migrated endpoint-graphiql/voyager blocks

--- layers/GatoGraphQLForWP/plugins/gatographql/blocks/endpoint-graphiql/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e761f8a93b786037efe4');
