93800984a73ba8eda9cc539245048aeb3b343e0f
leoloso <leo@getpop.org>
2024-09-15 11:18:18 +0800

    moved to trait

--- layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/SchemaConfigurationFunctionalityModuleResolver.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/SchemaConfigurationFunctionalityModuleResolver.php
@@ -12,7 +12,6 @@ use GatoGraphQL\GatoGraphQL\Module;
 use GatoGraphQL\GatoGraphQL\ModuleConfiguration;
 use GatoGraphQL\GatoGraphQL\ModuleSettings\Properties;
 use GatoGraphQL\GatoGraphQL\Plugin;
-use GatoGraphQL\GatoGraphQL\SettingsCategoryResolvers\SettingsCategoryResolver;
 use GatoGraphQL\GatoGraphQL\StaticHelpers\BehaviorHelpers;
 use GraphQLByPoP\GraphQLServer\Configuration\MutationPayloadTypeOptions;
 use GraphQLByPoP\GraphQLServer\Configuration\MutationSchemes;
@@ -74,11 +73,6 @@ class SchemaConfigurationFunctionalityModuleResolver extends AbstractFunctionali
             self::SCHEMA_SELF_FIELDS,
             self::GLOBAL_ID_FIELD,
         ];
-    }
-
-    public function getSettingsCategory(string $module): string
-    {
-        return SettingsCategoryResolver::SCHEMA_CONFIGURATION;
     }
 
     /**
