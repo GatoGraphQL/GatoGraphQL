8422d1bcafa2bede64094297104952efee48e9a3
leoloso <leo@getpop.org>
2024-09-17 15:01:07 +0800

    fix style

--- layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/Extensions/BundleExtensionModuleResolver.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/Extensions/BundleExtensionModuleResolver.php
@@ -107,10 +107,12 @@ class BundleExtensionModuleResolver extends AbstractBundleExtensionModuleResolve
 
     public function getLogoURL(string $module): string
     {
-        if (in_array($module, [
+        if (
+            in_array($module, [
             self::PRO,
             self::ALL_IN_ONE_TOOLBOX_FOR_WORDPRESS,
-        ])) {
+            ])
+        ) {
             return PluginApp::getMainPlugin()->getPluginURL() . 'assets/img/logos/GatoGraphQL-logo-face.png';
         }
         return parent::getLogoURL($module);
