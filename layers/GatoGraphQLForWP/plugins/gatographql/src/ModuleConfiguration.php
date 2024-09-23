94c7052eb9a7d1d1f8bdf62645e9d7356b219bbc
leoloso <leo@getpop.org>
2024-09-24 07:55:51 +0800

    Use extensions on getGatoGraphQLBundlesPageURL

--- layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleConfiguration.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleConfiguration.php
@@ -112,7 +112,9 @@ class ModuleConfiguration extends AbstractModuleConfiguration
     public function getGatoGraphQLBundlesPageURL(): string
     {
         $envVariable = Environment::GATOGRAPHQL_BUNDLES_PAGE_URL;
-        $defaultValue = 'https://gatographql.com/bundles/';
+        $defaultValue = PluginStaticModuleConfiguration::displayGatoGraphQLPROExtensionsOnExtensionsPage()
+            ? 'https://gatographql.com/bundles/'
+            : 'https://gatographql.com/extensions/';
 
         return $this->retrieveConfigurationValueOrUseDefault(
             $envVariable,
