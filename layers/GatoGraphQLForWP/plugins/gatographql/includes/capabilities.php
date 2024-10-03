74d02a2243e386f987421af32ccbe2accd61c25d
leoloso <leo@getpop.org>
2024-10-03 15:03:09 +0800

    no need to check if GATOGRAPHQL_CAPABILITY_MANAGE_GRAPHQL_SCHEMA defined

--- layers/GatoGraphQLForWP/plugins/gatographql/includes/capabilities.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/includes/capabilities.php
@@ -6,10 +6,5 @@ declare(strict_types=1);
  * Capabilities are defined via `define` instead of `const`,
  * because these are accessed before the vendor/ folder is
  * loaded, hence we can't reference the PSR4 file.
- *
- * Check if they are defined just in case 2 bundles, or a bundle
- * and an included extension, are loaded.
  */
-if (!defined('GATOGRAPHQL_CAPABILITY_MANAGE_GRAPHQL_SCHEMA')) {
-    define('GATOGRAPHQL_CAPABILITY_MANAGE_GRAPHQL_SCHEMA', 'gatographql_manage_graphql_schema');
-}
+define('GATOGRAPHQL_CAPABILITY_MANAGE_GRAPHQL_SCHEMA', 'gatographql_manage_graphql_schema');
