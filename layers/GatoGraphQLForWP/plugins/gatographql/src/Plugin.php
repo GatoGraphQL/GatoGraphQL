a0e16d371a84ce56e2eb16bdf603af592b7ec201
leoloso <leo@getpop.org>
2024-10-03 14:12:31 +0800

    showRatePluginBannerInAdminNotice

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Plugin.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Plugin.php
@@ -45,6 +45,8 @@ class Plugin extends AbstractMainPlugin
             return;
         }
 
+        $this->showRatePluginBannerInAdminNotice();
+
         // Admin notice: Check if it is enabled
         $userSettingsManager = UserSettingsManagerFacade::getInstance();
         if (
@@ -63,6 +65,31 @@ class Plugin extends AbstractMainPlugin
         }
         // All checks passed, show the release notes
         $this->showReleaseNotesInAdminNotice();
+    }
+
+    /**
+     * Add a banner asking users to rate the plugin
+     */
+    protected function showRatePluginBannerInAdminNotice(): void
+    {
+        // Add the admin notice
+        add_action('admin_notices', function (): void {
+            $adminNotice_safe = sprintf(
+                '<div class="notice notice-info is-dismissible">' .
+                    '<h3>%s</h3>' .
+                    '<p>%s</p>' .
+                    '<p>%s</p>' .
+                '</div>',
+                __('Rate Gato GraphQL', 'gatographql'),
+                __('We work really hard to deliver a plugin that enables accessing any piece of data on our WordPress sites. It takes plenty of time and effort to develop, test and maintain the free Gato GraphQL plugin. Therefore if you like what you see and appreciate our work, we ask you nothing more than to please rate the plugin in the directory. Thanks in advance!', 'gatographql'),
+                __('Rate Gato GraphQL', 'gatographql'),
+                sprintf(
+                    '<a class="rating-link" rel="noopener noreferrer" href="https://wordpress.org/plugins/gatographql/#reviews" target="_blank"><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span></a> <a class="button" rel="noopener noreferrer" href="https://wordpress.org/plugins/gatographql/#reviews" target="_blank">%s</a>',
+                    \__('Rate Plugin', 'gatographql')
+                ),
+            );
+            echo $adminNotice_safe;
+        });
     }
 
     /**
