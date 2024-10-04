e1e754f5e9c1ebe0a748d16803d940ce68cd9935
leoloso <leo@getpop.org>
2024-10-04 14:26:28 +0800

    phpdoc

--- src/Extensions/Symplify/MonorepoBuilder/ValueObject/OptionValues.php
+++ src/Extensions/Symplify/MonorepoBuilder/ValueObject/OptionValues.php
@@ -7,10 +7,15 @@ namespace PoP\PoP\Extensions\Symplify\MonorepoBuilder\ValueObject;
 final class OptionValues
 {
     /**
+     * Watch out! This const can be provided via a secret in GitHub Actions
+     *
      * @var string
      */
     public final const EXTENSION = 'extension';
+
     /**
+     * Watch out! This const can be provided via a secret in GitHub Actions
+     *
      * @var string
      */
     public final const BUNDLE = 'bundle';
