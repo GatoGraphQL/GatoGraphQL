48677cabb6c2a4fecb99a20d641d8f5ef6213099
leoloso <leo@getpop.org>
2024-10-15 13:50:37 +0800

    fix style

--- src/Extensions/Symplify/MonorepoBuilder/Command/RemoveComposerReplaceEntriesCommand.php
+++ src/Extensions/Symplify/MonorepoBuilder/Command/RemoveComposerReplaceEntriesCommand.php
@@ -9,7 +9,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symplify\MonorepoBuilder\FileSystem\ComposerJsonProvider;
 use Symplify\MonorepoBuilder\Validator\SourcesPresenceValidator;
 use Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
