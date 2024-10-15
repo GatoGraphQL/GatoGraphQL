e56a46f45005f79c8474b1ee061aa31fba372a64
leoloso <leo@getpop.org>
2024-10-15 09:18:38 +0800

    checkGatoGraphQLMemoryRequirements

--- layers/GatoGraphQLForWP/plugins/gatographql/gatographql.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/gatographql.php
@@ -60,33 +60,10 @@ $pluginName = __('Gato GraphQL', 'gatographql');
 //     return;
 // }
 
-/**
- * Validate that there is enough memory to run the plugin.
- *
- * > Note that to have no memory limit, set this directive to -1.
- *
- * @see https://www.php.net/manual/en/ini.core.php#ini.sect.resource-limits
- */
-$phpMemoryLimit = \ini_get('memory_limit');
-$phpMemoryLimitInBytes = \wp_convert_hr_to_bytes($phpMemoryLimit);
-if ($phpMemoryLimitInBytes !== -1) {
-    // Minimum: 64MB
-    $minRequiredPHPMemoryLimit = '64M';
-    $minRequiredPHPMemoryLimitInBytes = \wp_convert_hr_to_bytes($minRequiredPHPMemoryLimit);
-    if ($phpMemoryLimitInBytes < $minRequiredPHPMemoryLimitInBytes) {
-        \add_action('admin_notices', function () use ($minRequiredPHPMemoryLimit, $phpMemoryLimit) {
-            printf(
-                '<div class="notice notice-error"><p>%s</p></div>',
-                sprintf(
-                    __('Plugin <strong>%1$s</strong> requires at least <strong>%2$s</strong> of memory, however the server\'s PHP memory limit is set to <strong>%3$s</strong>. Please increase the memory limit to load %1$s.', 'gatographql'),
-                    __('Gato GraphQL', 'gatographql'),
-                    $minRequiredPHPMemoryLimit,
-                    $phpMemoryLimit
-                )
-            );
-        });
-        return;
-    }
+// Validate that there is enough memory to run the plugin
+require_once __DIR__ . '/includes/startup.php';
+if (!checkGatoGraphQLMemoryRequirements(__('Gato GraphQL', 'gatographql'))) {
+    return;
 }
 
 /**
