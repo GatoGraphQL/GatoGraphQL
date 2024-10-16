c6f01d552308e7790e5ca4fee04decf9e4cefdfc
leoloso <leo@getpop.org>
2024-10-16 13:19:09 +0800

    Add convenience class for standalone plugins

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/AbstractStandalonePluginModule.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL;
+
+use PoP\Root\Module\ModuleInterface;
+
+/**
+ * This class is not used by Gato GraphQL.
+ *
+ * Instead, it is a convenience class for standalone plugins,
+ * to supersede GatoGraphQL\GatoGraphQL\Plugin
+ */
+abstract class AbstractStandalonePluginModule extends Module
+{
+    /**
+     * Indicate if this Module must also be registered
+     * under the classes of other Modules. Needed by
+     * standalone plugins to supersede
+     * GatoGraphQL\GatoGraphQL\Module and yet be able to
+     * get configuration values from that module.
+     *
+     * @return array<class-string<ModuleInterface>>
+     */
+    public function registerAsModules(): array
+    {
+        return [
+            Module::class,
+        ];
+    }
+}
