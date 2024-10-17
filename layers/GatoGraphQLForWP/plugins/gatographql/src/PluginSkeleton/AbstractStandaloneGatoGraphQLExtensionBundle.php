2829b1fca7eb0dfe67af5b3ad081e4f2668e857c
leoloso <leo@getpop.org>
2024-10-17 08:21:46 +0800

    Removed using 'vendor/gatographql/gatographql' in StandaloneBundle

--- layers/GatoGraphQLForWP/plugins/gatographql/src/PluginSkeleton/AbstractStandaloneGatoGraphQLExtensionBundle.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/PluginSkeleton/AbstractStandaloneGatoGraphQLExtensionBundle.php
@@ -5,34 +5,10 @@ declare(strict_types=1);
 namespace GatoGraphQL\GatoGraphQL\PluginSkeleton;
 
 use GatoGraphQL\GatoGraphQL\PluginSkeleton\AbstractGatoGraphQLBundleExtension;
-use GatoGraphQL\GatoGraphQL\PluginSkeleton\ExtensionInitializationConfigurationInterface;
 use PoP\Root\Module\ModuleInterface;
 
 abstract class AbstractStandaloneGatoGraphQLExtensionBundle extends AbstractGatoGraphQLBundleExtension
 {
-    use StandalonePluginTrait;
-
-    public function __construct(
-        string $pluginFile, /** The main plugin file */
-        string $pluginVersion,
-        ?string $pluginName = null,
-        string $commitHash = null, /** Useful for development to regenerate the container when testing the generated plugin */
-        ?ExtensionInitializationConfigurationInterface $extensionInitializationConfiguration = null,
-    ) {
-        $pluginFolder = \dirname($pluginFile) . '/' . $this->getGatoGraphQLComposerRelativePath();
-        $pluginURL = \plugin_dir_url($pluginFile) . $this->getGatoGraphQLComposerRelativePath() . '/';
-
-        parent::__construct(
-            $pluginFile,
-            $pluginVersion,
-            $pluginName,
-            $commitHash,
-            $pluginFolder,
-            $pluginURL,
-            $extensionInitializationConfiguration,
-        );
-    }
-
     /**
      * Do not initialize any Module, as that is already
      * done by the corresponding StandalonePlugin
