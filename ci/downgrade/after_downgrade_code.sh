e5c82463165b750fb5409f7438f1af5908c826e8
leoloso <leo@getpop.org>
2024-10-18 11:32:34 +0800

    don't do getItem hack anymore

--- ci/downgrade/after_downgrade_code.sh
+++ ci/downgrade/after_downgrade_code.sh
@@ -8,7 +8,4 @@
 #
 ########################################################################
 
-# Fix the return type in Trait
-# @see https://github.com/rectorphp/rector/issues/6962
-# @see https://github.com/GatoGraphQL/GatoGraphQL/issues/1470
-sed -i 's/function getItem($key): CacheItem/function getItem($key): \\Psr\\Cache\\CacheItemInterface/' vendor/symfony/cache/Traits/AbstractAdapterTrait.php
+# Nothing here...
