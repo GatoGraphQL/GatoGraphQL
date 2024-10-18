c515644fe45724b60f0bd587a6541e9b39409308
leoloso <leo@getpop.org>
2024-10-18 18:15:22 +0800

    Standalone getModuleClasses

--- layers/GatoGraphQLForWP/plugins/gatographql/src/AbstractStandaloneModuleConfiguration.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/AbstractStandaloneModuleConfiguration.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL;
 
+use PoP\Root\Helpers\ClassHelpers;
+use PoP\Root\Module\ModuleInterface;
+
 abstract class AbstractStandaloneModuleConfiguration extends ModuleConfiguration
 {
+    /**
+     * @phpstan-return array<class-string<ModuleInterface>>
+     */
+    protected function getModuleClasses(): array
+    {
+        $classNamespace = ClassHelpers::getClassPSR4Namespace(\get_called_class());
+        /** @var array<class-string<ModuleInterface>> */
+        return [
+            Module::class,
+            $classNamespace . '\\StandaloneModule',
+        ];
+    }
 }
