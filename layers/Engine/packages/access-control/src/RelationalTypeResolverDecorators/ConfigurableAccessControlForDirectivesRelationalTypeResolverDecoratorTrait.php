8f441299a0ca5e2c98b6e4c51e545cb9b9dc011a
leoloso <leo@getpop.org>
2024-10-18 10:51:22 +0800

    Removed insteadof hacks for PHP 7.2

--- layers/Engine/packages/access-control/src/RelationalTypeResolverDecorators/ConfigurableAccessControlForDirectivesRelationalTypeResolverDecoratorTrait.php
+++ layers/Engine/packages/access-control/src/RelationalTypeResolverDecorators/ConfigurableAccessControlForDirectivesRelationalTypeResolverDecoratorTrait.php
@@ -13,13 +13,6 @@ trait ConfigurableAccessControlForDirectivesRelationalTypeResolverDecoratorTrait
 {
     use ConfigurableMandatoryDirectivesForDirectivesRelationalTypeResolverDecoratorTrait, AccessControlConfigurableMandatoryDirectivesForDirectivesTrait {
         AccessControlConfigurableMandatoryDirectivesForDirectivesTrait::getMatchingEntries insteadof ConfigurableMandatoryDirectivesForDirectivesRelationalTypeResolverDecoratorTrait;
-        // The conflict resolutions below should not be needed, because the functions are not repeated, but it is defined just once in the same source trait
-        // However, there is a bug about, still unresolved by PHP 7.2: https://bugs.php.net/bug.php?id=63911
-        // It was resolved by PHP 7.3.9, though, but handle to add compatibility up to PHP 7.1
-        AccessControlConfigurableMandatoryDirectivesForDirectivesTrait::getEntries insteadof ConfigurableMandatoryDirectivesForDirectivesRelationalTypeResolverDecoratorTrait;
-        AccessControlConfigurableMandatoryDirectivesForDirectivesTrait::getRequiredEntryValue insteadof ConfigurableMandatoryDirectivesForDirectivesRelationalTypeResolverDecoratorTrait;
-        AccessControlConfigurableMandatoryDirectivesForDirectivesTrait::getFieldDirectiveResolvers insteadof ConfigurableMandatoryDirectivesForDirectivesRelationalTypeResolverDecoratorTrait;
-        AccessControlConfigurableMandatoryDirectivesForDirectivesTrait::getFieldDirectiveResolverClasses insteadof ConfigurableMandatoryDirectivesForDirectivesRelationalTypeResolverDecoratorTrait;
     }
 
     /**
