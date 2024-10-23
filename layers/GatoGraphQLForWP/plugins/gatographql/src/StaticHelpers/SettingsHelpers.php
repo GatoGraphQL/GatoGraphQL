b28811e5ece03df6276904073424e98895aa8a63
leoloso <leo@getpop.org>
2024-10-23 17:02:14 +0800

    namespace options

--- layers/GatoGraphQLForWP/plugins/gatographql/src/StaticHelpers/SettingsHelpers.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/StaticHelpers/SettingsHelpers.php
@@ -6,7 +6,9 @@ namespace GatoGraphQL\GatoGraphQL\StaticHelpers;
 
 use GatoGraphQL\GatoGraphQL\Marketplace\Constants\LicenseProperties;
 use GatoGraphQL\GatoGraphQL\Marketplace\ObjectModels\CommercialExtensionActivatedLicenseObjectProperties;
+use GatoGraphQL\GatoGraphQL\Settings\OptionNamespacerInterface;
 use GatoGraphQL\GatoGraphQL\Settings\Options;
+use PoP\Root\Facades\Instances\InstanceManagerFacade;
 
 class SettingsHelpers
 {
@@ -23,8 +25,14 @@ class SettingsHelpers
     public static function getCommercialExtensionActivatedLicenseObjectProperties(): array
     {
         if (self::$commercialExtensionActivatedLicenseObjectProperties === null) {
+            $instanceManager = InstanceManagerFacade::getInstance();
+
+            /** @var OptionNamespacerInterface */
+            $optionNamespacer = $instanceManager->getInstance(OptionNamespacerInterface::class);
+            $option = $optionNamespacer->namespaceOption(Options::COMMERCIAL_EXTENSION_ACTIVATED_LICENSE_ENTRIES);
+
             /** @var array<string,mixed> */
-            $commercialExtensionActivatedLicenseEntries = get_option(Options::COMMERCIAL_EXTENSION_ACTIVATED_LICENSE_ENTRIES, []);
+            $commercialExtensionActivatedLicenseEntries = get_option($option, []);
             self::$commercialExtensionActivatedLicenseObjectProperties = [];
             foreach ($commercialExtensionActivatedLicenseEntries as $extensionSlug => $commercialExtensionActivatedLicenseEntry) {
                 self::$commercialExtensionActivatedLicenseObjectProperties[$extensionSlug] = new CommercialExtensionActivatedLicenseObjectProperties(
