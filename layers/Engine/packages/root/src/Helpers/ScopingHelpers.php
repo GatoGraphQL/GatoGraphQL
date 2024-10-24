c281bc26e37d1597b56055c5014bf86ba890624b
leoloso <leo@getpop.org>
2024-10-24 16:39:00 +0800

    phpdoc

--- layers/Engine/packages/root/src/Helpers/ScopingHelpers.php
+++ layers/Engine/packages/root/src/Helpers/ScopingHelpers.php
@@ -9,14 +9,26 @@ use PoP\Root\Constants\Scoping;
 class ScopingHelpers
 {
     /**
-     * If own classes have been prefixed, then the top-level
-     * domain will start with "InternallyPrefixed".
+     * If the plugin is prefixed using PHP-Scoper, use the
+     * top-level namespace name calculated here.
+     *
+     * This same name must be input in the scoper-internal.inc.php
+     * config file.
+     *
+     * For instance, plugin "Gato GraphQL" will have the top-level
+     * namespace "InternallyPrefixedByGatoGraphQL".
+     *
+     * @see ci/scoping/plugins/gatographql/scoper-internal.inc.php
      */
     public static function getPluginScopingTopLevelNamespace(string $pluginName): string
     {
         return Scoping::NAMESPACE_PREFIX . str_replace([' ', '-'], '', $pluginName);
     }
 
+    /**
+     * If own classes have been prefixed, then the top-level
+     * domain will start with "InternallyPrefixed".
+     */
     public static function isNamespaceInternallyScoped(string $class): bool
     {
         return str_starts_with($class, Scoping::NAMESPACE_PREFIX);
