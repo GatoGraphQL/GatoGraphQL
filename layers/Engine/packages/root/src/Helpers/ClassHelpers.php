e32a41ea060521232465f9f5d7cf9f9fb4e50351
leoloso <leo@getpop.org>
2024-10-24 15:50:29 +0800

    fix getClassPSR4Namespace for prefixed code

--- layers/Engine/packages/root/src/Helpers/ClassHelpers.php
+++ layers/Engine/packages/root/src/Helpers/ClassHelpers.php
@@ -7,11 +7,16 @@ namespace PoP\Root\Helpers;
 class ClassHelpers
 {
     /**
-     * The PSR-4 namespace, with format "Vendor\Project"
+     * The PSR-4 namespace, with format "Vendor\Project".
+     *
+     * If own classes have been prefixed, then format will be:
+     * "PrefixedByGatoStandalone\Vendor\Project"
+     *
+     * @see ci/scoping/plugins/gatographql/scoper-internal.inc.php
      */
     public static function getClassPSR4Namespace(string $class): string
     {
         $parts = explode('\\', $class);
-        return $parts[0] . (isset($parts[1]) ? '\\' . $parts[1] : '');
+        return $parts[0] . (isset($parts[1]) ? '\\' . $parts[1] : '') . ($parts[0] === 'PrefixedByGatoStandalone' && isset($parts[2]) ? '\\' . $parts[2] : '');
     }
 }
