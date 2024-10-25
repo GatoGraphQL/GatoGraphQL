a7e4db759f22dac5cee354770dd231106e3d1f3b
leoloso <leo@getpop.org>
2024-10-25 16:12:38 +0800

    phpdoc: Internal vs External

--- layers/Engine/packages/root/src/Helpers/ScopingHelpers.php
+++ layers/Engine/packages/root/src/Helpers/ScopingHelpers.php
@@ -6,6 +6,27 @@ namespace PoP\Root\Helpers;
 
 use PoP\Root\Constants\Scoping;
 
+/**
+ * The difference between "External" and "Internal" prefix is:
+ *
+ *   - External: Scope 3rd-party dependencies (eg: Symfony packages)
+ *   - Internal: Scope own dependencies (eg: all packages composing
+ *               the Gato GraphQL plugin)
+ *
+ * "External" is your typical scoping, needed to make sure that,
+ * if there's any other (WordPress) plugin installed in the same
+ * site with the same dependencies, these do not conflict.
+ *
+ * "Internal" is used within the Gato GraphQL project to also
+ * produce "standalone" plugins, which are plugins wrapping
+ * the Gato GraphQL plugin, and adding extra logic, such as
+ * "Gato Multilingual for Polylang".
+ *
+ * Internal scopes this project's own classes, so that, if the
+ * same site has 2 or more (WordPress) plugins from this project
+ * (eg: "Gato GraphQL" and "Gato Multilingual for WordPress"),
+ * these do not conflict with each other.
+ */
 class ScopingHelpers
 {
     /**
