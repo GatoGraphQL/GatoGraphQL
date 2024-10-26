348a1655fa038882e766bbfa966d8e62521173e1
leoloso <leo@getpop.org>
2024-10-26 11:16:00 +0800

    No need for `getGatoGraphQLScopingConfiguration`

--- ci/scoping/plugins/gatographql/scoper.inc.php
+++ ci/scoping/plugins/gatographql/scoper.inc.php
@@ -42,93 +42,87 @@ require_once dirname(__DIR__, 4) . '/layers/Engine/packages/root/src/Helpers/Sco
  *
  * @return array<string,mixed>
  */
-function getGatoGraphQLScopingConfiguration(
-    string $pluginName,
-): array {
-    return [
-        'prefix' => ScopingHelpers::getPluginExternalScopingTopLevelNamespace($pluginName),
-        'finders' => [
-            // Scope packages under vendor/, excluding local WordPress packages
-            Finder::create()
-                ->files()
-                ->ignoreVCS(true)
-                ->notName('/.*\\.md|.*\\.dist|composer\\.lock/')
-                ->exclude([
-                    'tests',
-                ])
-                ->notPath([
-                    // Exclude all libraries for WordPress
-                    // 1. Exclude libraries ending in "-wp" from general packages
-                    '#[getpop|graphql\-by\-pop|pop\-api|pop\-cms\-schema]/[a-zA-Z0-9_-]*-wp/#',
-                    // 2. Exclude all libraries from WPSchema
-                    '#pop-wp-schema/#',
-                    // Exclude all composer.json from own libraries (they get broken!)
-                    '#[getpop|gatographql|graphql\-by\-pop|pop\-api|pop\-backbone|pop\-cms\-schema|pop\-schema|pop\-wp\-schema]/*/composer.json#',
-                    // Exclude libraries
-                    '#symfony/deprecation-contracts/#',
-                    // Exclude tests from libraries
-                    /**
-                     * Gato GraphQL plugin: This code is commented out as it is
-                     * not needed anymore, because package `brain/cortex`
-                     * is not loaded
-                     *
-                     * @see layers/Engine/packages/root-wp/src/Module.php
-                     */
-                    // '#nikic/fast-route/test/#',
-                    '#psr/log/Psr/Log/Test/#',
-                    '#symfony/dom-crawler/Test/#',
-                    '#symfony/http-foundation/Test/#',
-                    '#symfony/service-contracts/Test/#',
-                    '#michelf/php-markdown/test/#',
-                ])
-                ->in(convertRelativeToFullPath('vendor')),
-            /**
-             * Gato GraphQL plugin: This code is commented out as it is
-             * not needed anymore, because package `brain/cortex`
-             * is not loaded
-             *
-             * @see layers/Engine/packages/root-wp/src/Module.php
-             */
-            // Finder::create()->append([
-            //     convertRelativeToFullPath('vendor/getpop/root-wp/src/Module.php'),
-            //     convertRelativeToFullPath('vendor/getpop/root-wp/src/Hooks/SetupCortexRoutingHookSet.php'),
-            // ])
-        ],
-        'exclude-namespaces' => [
-            // Own namespaces
-            // Watch out! Do NOT alter the order of PoPSchema, PoPWPSchema and PoP!
-            // If PoP comes first, then PoPSchema is still scoped!
-            'PoPAPI',
-            'PoPBackbone',
-            'PoPCMSSchema',
-            'PoPIncludes',
-            'PoPSchema',
-            'PoPWPSchema',
-            'PoP',
-            'GraphQLByPoP',
-            'GatoGraphQL',
-            
-            // No need to exclude the container, as its code is generated on runtime
-            // /**
-            //  * Own container cache namespace
-            //  * 
-            //  * Watch out! This value is being hardcoded!
-            //  * 
-            //  * In the application, it can be overridden via code:
-            //  * 
-            //  *   - ContainerBuilderFactory::getContainerNamespace()
-            //  *   - SystemContainerBuilderFactory::getContainerNamespace()
-            //  * 
-            //  * But can't reference these classes here, since they are not found
-            //  * (unless adding the files to the autoload path)
-            //  */
-            // 'PoPContainer',
-        ],
-        'patchers' => [
-            gatoGraphQLScopingConfigurationPatcher(...),
-        ],
-    ];
-}
-
 $pluginName = 'Gato GraphQL';
-return getGatoGraphQLScopingConfiguration($pluginName);
+return [
+    'prefix' => ScopingHelpers::getPluginExternalScopingTopLevelNamespace($pluginName),
+    'finders' => [
+        // Scope packages under vendor/, excluding local WordPress packages
+        Finder::create()
+            ->files()
+            ->ignoreVCS(true)
+            ->notName('/.*\\.md|.*\\.dist|composer\\.lock/')
+            ->exclude([
+                'tests',
+            ])
+            ->notPath([
+                // Exclude all libraries for WordPress
+                // 1. Exclude libraries ending in "-wp" from general packages
+                '#[getpop|graphql\-by\-pop|pop\-api|pop\-cms\-schema]/[a-zA-Z0-9_-]*-wp/#',
+                // 2. Exclude all libraries from WPSchema
+                '#pop-wp-schema/#',
+                // Exclude all composer.json from own libraries (they get broken!)
+                '#[getpop|gatographql|graphql\-by\-pop|pop\-api|pop\-backbone|pop\-cms\-schema|pop\-schema|pop\-wp\-schema]/*/composer.json#',
+                // Exclude libraries
+                '#symfony/deprecation-contracts/#',
+                // Exclude tests from libraries
+                /**
+                 * Gato GraphQL plugin: This code is commented out as it is
+                 * not needed anymore, because package `brain/cortex`
+                 * is not loaded
+                 *
+                 * @see layers/Engine/packages/root-wp/src/Module.php
+                 */
+                // '#nikic/fast-route/test/#',
+                '#psr/log/Psr/Log/Test/#',
+                '#symfony/dom-crawler/Test/#',
+                '#symfony/http-foundation/Test/#',
+                '#symfony/service-contracts/Test/#',
+                '#michelf/php-markdown/test/#',
+            ])
+            ->in(convertRelativeToFullPath('vendor')),
+        /**
+         * Gato GraphQL plugin: This code is commented out as it is
+         * not needed anymore, because package `brain/cortex`
+         * is not loaded
+         *
+         * @see layers/Engine/packages/root-wp/src/Module.php
+         */
+        // Finder::create()->append([
+        //     convertRelativeToFullPath('vendor/getpop/root-wp/src/Module.php'),
+        //     convertRelativeToFullPath('vendor/getpop/root-wp/src/Hooks/SetupCortexRoutingHookSet.php'),
+        // ])
+    ],
+    'exclude-namespaces' => [
+        // Own namespaces
+        // Watch out! Do NOT alter the order of PoPSchema, PoPWPSchema and PoP!
+        // If PoP comes first, then PoPSchema is still scoped!
+        'PoPAPI',
+        'PoPBackbone',
+        'PoPCMSSchema',
+        'PoPIncludes',
+        'PoPSchema',
+        'PoPWPSchema',
+        'PoP',
+        'GraphQLByPoP',
+        'GatoGraphQL',
+        
+        // No need to exclude the container, as its code is generated on runtime
+        // /**
+        //  * Own container cache namespace
+        //  * 
+        //  * Watch out! This value is being hardcoded!
+        //  * 
+        //  * In the application, it can be overridden via code:
+        //  * 
+        //  *   - ContainerBuilderFactory::getContainerNamespace()
+        //  *   - SystemContainerBuilderFactory::getContainerNamespace()
+        //  * 
+        //  * But can't reference these classes here, since they are not found
+        //  * (unless adding the files to the autoload path)
+        //  */
+        // 'PoPContainer',
+    ],
+    'patchers' => [
+        gatoGraphQLScopingConfigurationPatcher(...),
+    ],
+];
