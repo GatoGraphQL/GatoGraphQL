261dc452405d5e9dbc026e3c2858983042d82e21
leoloso <leo@getpop.org>
2024-10-28 22:07:33 +0800

    Moved to Standalone: Remove first item from menu

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Services/MenuPageAttachers/BottomMenuPageAttacher.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Services/MenuPageAttachers/BottomMenuPageAttacher.php
@@ -376,18 +376,6 @@ class BottomMenuPageAttacher extends AbstractPluginMenuPageAttacher
                 $aboutMenuPage->setHookName($hookName);
             }
         }
-
-        /**
-         * Standalone plugins: The Settings page needs capability
-         * "manage_options". If the current user does not fulfil it,
-         * and this page was added as the first item,
-         * then WordPress will still add a first entry with the same
-         * name as the plugin. Remove that entry from the menu!
-         */            
-        global $submenu;
-        if ($submenu[$menuName][0][0] === $this->getPluginMenu()->getMenuName()) {
-            unset($submenu[$menuName][0]);
-        }
     }
 
     /**
---
+++ layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/Overrides/Upstream/Services/MenuPageAttachers/BottomMenuPageAttacher.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQLStandalone\GatoGraphQL\Overrides\Upstream\Services\MenuPageAttachers;
+
+use GatoGraphQL\GatoGraphQL\Services\MenuPageAttachers\BottomMenuPageAttacher as UpstreamBottomMenuPageAttacher;
+
+class BottomMenuPageAttacher extends UpstreamBottomMenuPageAttacher
+{
+    /**
+     * Standalone plugins: The Settings page needs capability
+     * "manage_options". If the current user does not fulfil it,
+     * and this page was added as the first item,
+     * then WordPress will still add a first entry with the same
+     * name as the plugin. Remove that entry from the menu!
+     */            
+    public function addMenuPages(): void
+    {
+        parent::addMenuPages();
+
+        global $submenu;
+        $menuName = $this->getMenuName();
+        if ($submenu[$menuName][0][0] === $this->getPluginMenu()->getMenuName()) {
+            unset($submenu[$menuName][0]);
+        }
+    }
+}
