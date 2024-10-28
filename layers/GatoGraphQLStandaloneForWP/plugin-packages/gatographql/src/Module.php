4ba720eb5e9e56b60ff5a94fe795048ede44d797
leoloso <leo@getpop.org>
2024-10-28 15:30:52 +0800

    Added module to the Standalone package

---
+++ layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/Module.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQLStandalone\GatoGraphQL;
+
+use PoP\Root\Module\ModuleInterface;
+use PoP\Root\Module\AbstractModule;
+
+class Module extends AbstractModule
+{
+    /**
+     * @return array<class-string<ModuleInterface>>
+     */
+    public function getDependedModuleClasses(): array
+    {
+        return [];
+    }
+}
