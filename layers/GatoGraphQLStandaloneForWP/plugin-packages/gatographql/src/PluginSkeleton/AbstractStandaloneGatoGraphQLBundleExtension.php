64ceb2818e6cb2ecd2b38b780c5ed2856e73adcd
leoloso <leo@getpop.org>
2024-10-29 15:54:28 +0800

    Added AbstractStandaloneGatoGraphQLBundleExtension

---
+++ layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/PluginSkeleton/AbstractStandaloneGatoGraphQLBundleExtension.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQLStandalone\GatoGraphQL\PluginSkeleton;
+
+use GatoGraphQL\GatoGraphQL\PluginSkeleton\AbstractGatoGraphQLBundleExtension;
+
+abstract class AbstractStandaloneGatoGraphQLBundleExtension extends AbstractGatoGraphQLBundleExtension
+{
+    
+}
