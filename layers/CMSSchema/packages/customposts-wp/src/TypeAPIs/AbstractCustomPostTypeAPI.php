7e8ed4bd2ea213c9222416c2633d835247d56c83
leoloso <leo@getpop.org>
2024-10-30 10:22:37 +0800

    $query['post_status'] = 'any';

--- layers/CMSSchema/packages/customposts-wp/src/TypeAPIs/AbstractCustomPostTypeAPI.php
+++ layers/CMSSchema/packages/customposts-wp/src/TypeAPIs/AbstractCustomPostTypeAPI.php
@@ -184,6 +184,9 @@ abstract class AbstractCustomPostTypeAPI extends UpstreamAbstractCustomPostTypeA
             //     : $this->getStatusQueryArgValue($status);
             $query['post_status'] = $status;
         }
+        if (is_array($query['post_status']) && in_array('any', $query['post_status'])) {
+            $query['post_status'] = 'any';
+        }
         if (isset($query['include']) && is_array($query['include'])) {
             // It can be an array or a string
             $query['include'] = implode(',', $query['include']);
