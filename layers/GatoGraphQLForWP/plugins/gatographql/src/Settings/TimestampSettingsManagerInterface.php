0533393e0ddcb896248caeffd84cfa057f423abf
leoloso <leo@getpop.org>
2024-10-31 14:17:13 +0800

    redesign TimestampSettingsManagerInterface

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Settings/TimestampSettingsManagerInterface.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Settings/TimestampSettingsManagerInterface.php
@@ -6,32 +6,10 @@ namespace GatoGraphQL\GatoGraphQL\Settings;
 
 interface TimestampSettingsManagerInterface
 {
+    public function getTimestampValue(string $name): ?int;
+    public function storeTimestampValue(string $name, int $timestamp): void;
     /**
-     * Timestamp of latest executed write to DB, concerning plugin activation,
-     * module enabled/disabled, user settings updated, to refresh the Service
-     * Container
+     * @param array<string,int> $nameTimestamps Key: name, Value: timestamp
      */
-    public function getContainerUniqueTimestamp(): string;
-    /**
-     * Timestamp of latest executed write to DB, concerning CPT entity created
-     * or modified (such as Schema Configuration, ACL, etc), to refresh
-     * the GraphQL schema
-     */
-    public function getOperationalUniqueTimestamp(): string;
-    /**
-     * Store the current time to indicate the latest executed write to DB,
-     * concerning plugin activation, module enabled/disabled, user settings updated,
-     * to refresh the Service Container
-     */
-    public function storeContainerTimestamp(): void;
-    /**
-     * Store the current time to indicate the latest executed write to DB,
-     * concerning CPT entity created or modified (such as Schema Configuration,
-     * ACL, etc), to refresh the GraphQL schema
-     */
-    public function storeOperationalTimestamp(): void;
-    /**
-     * Remove the timestamp
-     */
-    public function removeTimestamps(): void;
+    public function storeTimestampValues(array $nameTimestamps): void;
 }
