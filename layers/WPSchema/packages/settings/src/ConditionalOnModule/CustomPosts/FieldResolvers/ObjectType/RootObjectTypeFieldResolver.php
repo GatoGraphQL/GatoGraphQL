c8daa1985e4a7586ee2ce3c56deb641563c1a580
leoloso <leo@getpop.org>
2024-11-04 15:26:49 +0800

    Moved to ConditionalOnModule\CustomPosts

--- layers/WPSchema/packages/settings/src/ConditionalOnModule/CustomPosts/FieldResolvers/ObjectType/RootObjectTypeFieldResolver.php
+++ layers/WPSchema/packages/settings/src/ConditionalOnModule/CustomPosts/FieldResolvers/ObjectType/RootObjectTypeFieldResolver.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
-namespace PoPWPSchema\Settings\FieldResolvers\ObjectType;
+namespace PoPWPSchema\Settings\ConditionalOnModule\CustomPosts\FieldResolvers\ObjectType;
 
+use PoPCMSSchema\CustomPosts\TypeResolvers\EnumType\CustomPostEnumStringScalarTypeResolver;
 use PoPWPSchema\Settings\TypeAPIs\SettingsTypeAPIInterface;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractQueryableObjectTypeFieldResolver;
@@ -15,10 +16,13 @@ use PoP\ComponentModel\TypeResolvers\ScalarType\BooleanScalarTypeResolver;
 use PoP\Engine\TypeResolvers\ObjectType\RootObjectTypeResolver;
 use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
 
+use function use_block_editor_for_post_type;
+
 class RootObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolver
 {
     private ?BooleanScalarTypeResolver $booleanScalarTypeResolver = null;
     private ?SettingsTypeAPIInterface $settingsTypeAPI = null;
+    private ?CustomPostEnumStringScalarTypeResolver $customPostEnumStringScalarTypeResolver = null;
 
     final protected function getBooleanScalarTypeResolver(): BooleanScalarTypeResolver
     {
@@ -38,6 +42,15 @@ class RootObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolv
         }
         return $this->settingsTypeAPI;
     }
+    final protected function getCustomPostEnumStringScalarTypeResolver(): CustomPostEnumStringScalarTypeResolver
+    {
+        if ($this->customPostEnumStringScalarTypeResolver === null) {
+            /** @var CustomPostEnumStringScalarTypeResolver */
+            $customPostEnumStringScalarTypeResolver = $this->instanceManager->getInstance(CustomPostEnumStringScalarTypeResolver::class);
+            $this->customPostEnumStringScalarTypeResolver = $customPostEnumStringScalarTypeResolver;
+        }
+        return $this->customPostEnumStringScalarTypeResolver;
+    }
 
     /**
      * @return array<class-string<ObjectTypeResolverInterface>>
@@ -55,14 +68,14 @@ class RootObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolv
     public function getFieldNamesToResolve(): array
     {
         return [
-            'isGutenbergEditorEnabled',
+            'isGutenbergEditorEnabledForCustomPostType',
         ];
     }
 
     public function getFieldDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
     {
         return match ($fieldName) {
-            'isGutenbergEditorEnabled' => $this->__('Is the Gutenberg editor enabled? (i.e. the "Classic Editor" plugin is not enabled)', 'settings'),
+            'isGutenbergEditorEnabledForCustomPostType' => $this->__('Is the Gutenberg editor enabled for the custom post type?', 'settings'),
             default => parent::getFieldDescription($objectTypeResolver, $fieldName),
         };
     }
@@ -70,7 +83,7 @@ class RootObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolv
     public function getFieldTypeResolver(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ConcreteTypeResolverInterface
     {
         return match ($fieldName) {
-            'isGutenbergEditorEnabled' => $this->getBooleanScalarTypeResolver(),
+            'isGutenbergEditorEnabledForCustomPostType' => $this->getBooleanScalarTypeResolver(),
             default => parent::getFieldTypeResolver($objectTypeResolver, $fieldName),
         };
     }
@@ -78,8 +91,34 @@ class RootObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolv
     public function getFieldTypeModifiers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): int
     {
         return match ($fieldName) {
-            'isGutenbergEditorEnabled' => SchemaTypeModifiers::NON_NULLABLE,
+            'isGutenbergEditorEnabledForCustomPostType' => SchemaTypeModifiers::NON_NULLABLE,
             default => parent::getFieldTypeModifiers($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldArgNameTypeResolvers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): array
+    {
+        return match ($fieldName) {
+            'isGutenbergEditorEnabledForCustomPostType' => [
+                'customPostType' => $this->getCustomPostEnumStringScalarTypeResolver(),
+            ],
+            default => parent::getFieldArgNameTypeResolvers($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldArgDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName, string $fieldArgName): ?string
+    {
+        return match ([$fieldName => $fieldArgName]) {
+            ['isGutenbergEditorEnabledForCustomPostType' => 'customPostType'] => $this->__('The custom post type', 'gatographql'),
+            default => parent::getFieldArgDescription($objectTypeResolver, $fieldName, $fieldArgName),
+        };
+    }
+
+    public function getFieldArgTypeModifiers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName, string $fieldArgName): int
+    {
+        return match ([$fieldName => $fieldArgName]) {
+            ['isGutenbergEditorEnabledForCustomPostType' => 'customPostType'] => SchemaTypeModifiers::MANDATORY,
+            default => parent::getFieldArgTypeModifiers($objectTypeResolver, $fieldName, $fieldArgName),
         };
     }
 
@@ -90,8 +129,13 @@ class RootObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolv
         ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
     ): mixed {
         switch ($fieldDataAccessor->getFieldName()) {
-            case 'isGutenbergEditorEnabled':
-                return $this->getSettingsTypeAPI()->isGutenbergEditorEnabled();
+            case 'isGutenbergEditorEnabledForCustomPostType':
+                if (!$this->getSettingsTypeAPI()->isGutenbergEditorEnabled()) {
+                    return false;
+                }
+                /** @var string */
+                $customPostType = $fieldDataAccessor->getValue('customPostType');
+                return use_block_editor_for_post_type($customPostType);
         }
 
         return parent::resolveValue($objectTypeResolver, $object, $fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
