07fec40683ae1ff984651551e5073d72582aeb13
leoloso <leo@getpop.org>
2024-11-22 16:46:31 +0800

    better flag

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/Constants/DBFlags.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/Constants/DBFlags.php
@@ -7,5 +7,5 @@ namespace GatoGraphQL\GatoGraphQL\Marketplace\Constants;
 class DBFlags
 {
     /** Flag that the license for an extension has just been activated */
-    public final const JUST_ACTIVATED_COMMERCIAL_EXTENSION_LICENSE = '_*_';
+    public final const JUST_ACTIVATED_COMMERCIAL_EXTENSION_LICENSE = '__*__';
 }
