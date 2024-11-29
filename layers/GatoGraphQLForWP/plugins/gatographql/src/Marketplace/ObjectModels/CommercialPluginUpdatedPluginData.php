bd44884d315fdb588ccb0dcd6f91ef9e0366744d
leoloso <leo@getpop.org>
2024-11-29 15:14:58 +0800

    fixed bug

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/ObjectModels/CommercialPluginUpdatedPluginData.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/ObjectModels/CommercialPluginUpdatedPluginData.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\Marketplace\ObjectModels;
 
-use GatoGraphQL\GatoGraphQL\Plugin;
+use GatoGraphQL\GatoGraphQL\PluginSkeleton\PluginInterface;
 
 class CommercialPluginUpdatedPluginData
 {
     public function __construct(
-        public readonly Plugin $plugin,
+        public readonly PluginInterface $plugin,
         public readonly string $licenseKey,
         public readonly string $cacheKey,
     ) {
