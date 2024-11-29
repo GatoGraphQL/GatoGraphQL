81e4d4c5e09e7b618e2b7408dba45e94053471d1
leoloso <leo@getpop.org>
2024-11-29 12:55:30 +0800

    phpdoc

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/AbstractMarketplaceProviderCommercialPluginUpdaterService.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/AbstractMarketplaceProviderCommercialPluginUpdaterService.php
@@ -6,6 +6,12 @@ namespace GatoGraphQL\GatoGraphQL\Marketplace;
 
 use PoP\Root\Services\BasicServiceTrait;
 
+/**
+ * Copied code from `Make-Lemonade/lemonsqueezy-wp-updater-example`
+ *
+ * @see https://github.com/Make-Lemonade/lemonsqueezy-wp-updater-example
+ * @see https://github.com/Make-Lemonade/lemonsqueezy-wp-updater-example/blob/7c0c71876309939b07d96e270f4db8568f3148cb/includes/class-plugin-updater.php
+ */
 abstract class AbstractMarketplaceProviderCommercialPluginUpdaterService implements MarketplaceProviderCommercialPluginUpdaterServiceInterface
 {
     use BasicServiceTrait;
--- layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/LemonSqueezyCommercialPluginUpdaterService.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/LemonSqueezyCommercialPluginUpdaterService.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\Marketplace;
 
+/**
+ * Copied code from `Make-Lemonade/lemonsqueezy-wp-updater-example`
+ *
+ * @see https://github.com/Make-Lemonade/lemonsqueezy-wp-updater-example
+ * @see https://github.com/Make-Lemonade/lemonsqueezy-wp-updater-example/blob/7c0c71876309939b07d96e270f4db8568f3148cb/includes/class-plugin-updater.php
+ */
 class LemonSqueezyCommercialPluginUpdaterService extends AbstractMarketplaceProviderCommercialPluginUpdaterService
 {
 }
