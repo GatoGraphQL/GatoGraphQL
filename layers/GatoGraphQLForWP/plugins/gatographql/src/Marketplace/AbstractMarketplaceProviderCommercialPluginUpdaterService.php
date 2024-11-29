15255c37df0909c95ef5788d8a818a1a28be25c2
leoloso <leo@getpop.org>
2024-11-29 12:52:39 +0800

    added draft service

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/AbstractMarketplaceProviderCommercialPluginUpdaterService.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Marketplace;
+
+use PoP\Root\Services\BasicServiceTrait;
+
+abstract class AbstractMarketplaceProviderCommercialPluginUpdaterService implements MarketplaceProviderCommercialPluginUpdaterServiceInterface
+{
+    use BasicServiceTrait;
+}
