5e9218a9f720a492990c1ece46d64ec07d779516
leoloso <leo@getpop.org>
2024-12-12 18:46:48 +0800

    added `SettingsValues` class

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Constants/SettingsValues.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Constants;
+
+class SettingsValues
+{
+    public final const ENABLED = 'enabled';
+    public final const DISABLED = 'disabled';
+}
