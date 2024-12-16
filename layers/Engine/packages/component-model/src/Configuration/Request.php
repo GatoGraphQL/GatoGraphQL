28894cbe049574f9af79727a37f640dfd6eee673
leoloso <leo@getpop.org>
2024-12-16 17:59:17 +0800

    phpdoc

--- layers/Engine/packages/component-model/src/Configuration/Request.php
+++ layers/Engine/packages/component-model/src/Configuration/Request.php
@@ -14,6 +14,12 @@ class Request
      */
     public static function getActions(): array
     {
+        /**
+         * Watch out! Other plugins can implement the same URL param name!
+         * Hence, make sure it's an array, to avoid bugs from conflicts.
+         *
+         * @see https://github.com/GatoGraphQL/GatoGraphQL/issues/2985
+         */
         return App::getRequest()->request->all()[Params::ACTIONS] ?? App::getRequest()->query->all()[Params::ACTIONS] ?? [];
     }
 
