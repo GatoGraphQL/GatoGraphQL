58d6af9565b2f99a911056ec7a34644e0930fd53
leoloso <leo@getpop.org>
2024-12-19 09:36:37 +0800

    added consts

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Constants/SettingsValues.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Constants/SettingsValues.php
@@ -8,4 +8,7 @@ class SettingsValues
 {
     public final const ENABLED = 'enabled';
     public final const DISABLED = 'disabled';
+
+    public final const DEFAULT = 'default';
+    public final const NONE = 'none';
 }
