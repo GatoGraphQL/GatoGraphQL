e19ffc45f38c240a08bc2b79936f6059cd408848
leoloso <leo@getpop.org>
2024-12-26 18:33:52 +0800

    added premium bundles

--- layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/Extensions/PremiumBundleExtensionModuleResolver.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/Extensions/PremiumBundleExtensionModuleResolver.php
@@ -12,6 +12,11 @@ class PremiumBundleExtensionModuleResolver extends AbstractBundleExtensionModule
 {
     use PremiumExtensionModuleResolverTrait;
 
+    public const AUTOMATION = Plugin::NAMESPACE . '\\bundle-extensions\\automation';
+    public const DEEPL = Plugin::NAMESPACE . '\\bundle-extensions\\deepl';
+    public const EVENTS_MANAGER = Plugin::NAMESPACE . '\\bundle-extensions\\events-manager';
+    public const GOOGLE_TRANSLATE = Plugin::NAMESPACE . '\\bundle-extensions\\google-translate';
+    public const MULTILINGUALPRESS = Plugin::NAMESPACE . '\\bundle-extensions\\multilingualpress';
     public const POLYLANG = Plugin::NAMESPACE . '\\bundle-extensions\\polylang';
 
     /**
@@ -19,17 +24,28 @@ class PremiumBundleExtensionModuleResolver extends AbstractBundleExtensionModule
      */
     public function getModulesToResolve(): array
     {
-        return PluginStaticModuleConfiguration::displayGatoGraphQLPROFeatureBundlesOnExtensionsPage()
-            ? [
-                self::POLYLANG,
-            ] : [];
+        if (!PluginStaticModuleConfiguration::displayGatoGraphQLPROFeatureBundlesOnExtensionsPage()) {
+            return [];
+        }
+        return [
+            self::AUTOMATION,
+            self::DEEPL,
+            self::EVENTS_MANAGER,
+            self::GOOGLE_TRANSLATE,
+            self::MULTILINGUALPRESS,
+            self::POLYLANG,
+        ];
     }
 
     public function getName(string $module): string
     {
-        $extensionPlaceholder = \__('%s', 'gatographql');
         return match ($module) {
-            self::POLYLANG => sprintf($extensionPlaceholder, \__('Polylang', 'gatographql')),
+            self::AUTOMATION => \__('Automation', 'gatographql'),
+            self::DEEPL => \__('DeepL', 'gatographql'),
+            self::EVENTS_MANAGER => \__('Events Manager', 'gatographql'),
+            self::GOOGLE_TRANSLATE => \__('Google Translate', 'gatographql'),
+            self::MULTILINGUALPRESS => \__('MultilingualPress', 'gatographql'),
+            self::POLYLANG => \__('Polylang', 'gatographql'),
             default => $module,
         };
     }
@@ -37,7 +53,12 @@ class PremiumBundleExtensionModuleResolver extends AbstractBundleExtensionModule
     public function getDescription(string $module): string
     {
         return match ($module) {
-            self::POLYLANG => \__('Integration with the Polylang plugin, providing fields to the GraphQL schema to fetch multilingual data', 'gatographql'),
+            self::AUTOMATION => \__('Use GraphQL to automate tasks in your app: Execute queries when some event happens, chain queries, and schedule and trigger queries via WP-Cron. (The Internal GraphQL Server extension is required).', 'gatographql'),
+            self::DEEPL => \__('Translate content to multiple languages using the DeepL API.', 'gatographql'),
+            self::EVENTS_MANAGER => \__('Integration with plugin "Events Manager", adding fields to the schema to fetch event data.', 'gatographql'),
+            self::GOOGLE_TRANSLATE => \__('Translate content to multiple languages using the Google Translate API.', 'gatographql'),
+            self::MULTILINGUALPRESS => \__('Integration with plugin "MultilingualPress", adding fields to the schema to fetch multilingual data.', 'gatographql'),
+            self::POLYLANG => \__('Integration with plugin "Polylang", adding fields to the schema to fetch multilingual data.', 'gatographql'),
             default => parent::getDescription($module),
         };
     }
@@ -51,8 +72,7 @@ class PremiumBundleExtensionModuleResolver extends AbstractBundleExtensionModule
     {
         $pluginURL = PluginApp::getMainPlugin()->getPluginURL();
         return match ($module) {
-            self::POLYLANG
-                => $pluginURL . 'assets/img/extension-logos/polylang.png',
+            self::POLYLANG => $pluginURL . 'assets/img/extension-logos/polylang.png',
             default => parent::getLogoURL($module),
         };
     }
