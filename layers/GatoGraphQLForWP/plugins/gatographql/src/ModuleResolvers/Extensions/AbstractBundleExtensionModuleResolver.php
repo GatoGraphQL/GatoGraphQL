687fb5f2ee6c01dc8bc8344abb251442b3086be1
leoloso <leo@getpop.org>
2025-02-07 12:56:06 +0800

    replaced path from .webp to .png

--- layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/Extensions/AbstractBundleExtensionModuleResolver.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/Extensions/AbstractBundleExtensionModuleResolver.php
@@ -43,7 +43,7 @@ abstract class AbstractBundleExtensionModuleResolver extends AbstractExtensionMo
     {
         $mainPlugin = PluginApp::getMainPlugin();
         $pluginURL = $mainPlugin->getPluginURL();
-        return $pluginURL . 'assets/img/logos/GatoGraphQL-logo-back-long.webp';
+        return $pluginURL . 'assets/img/logos/GatoGraphQL-logo-back-long.png';
     }
 
     public function getGatoGraphQLExtensionSlug(string $module): string
