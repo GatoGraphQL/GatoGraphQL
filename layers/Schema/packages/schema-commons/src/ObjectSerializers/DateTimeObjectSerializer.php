118bf7d9048269a92c8aa7ee2e1547bf00e98c6e
leoloso <leo@getpop.org>
2025-02-17 17:43:18 +0800

    remove reference to custom class

--- layers/Schema/packages/schema-commons/src/ObjectSerializers/DateTimeObjectSerializer.php
+++ layers/Schema/packages/schema-commons/src/ObjectSerializers/DateTimeObjectSerializer.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace PoPSchema\SchemaCommons\ObjectSerializers;
 
 use DateTime;
-// @todo Replace with \DateTimeInterface. See: https://github.com/GatoGraphQL/GatoGraphQL/issues/1282
-use PoPSchema\SchemaCommons\Polyfill\PHP72\DateTimeInterface;
+use DateTimeInterface;
 use PoP\ComponentModel\ObjectSerialization\AbstractObjectSerializer;
 use stdClass;
 
