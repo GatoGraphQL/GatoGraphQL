f92f83d1608aaa41edfd64544f767bffb5a6ed66
leoloso <leo@getpop.org>
2025-02-17 17:32:02 +0800

    DateTime => String

--- layers/CMSSchema/packages/custompost-mutations/src/MutationResolvers/AbstractCreateOrUpdateCustomPostMutationResolver.php
+++ layers/CMSSchema/packages/custompost-mutations/src/MutationResolvers/AbstractCreateOrUpdateCustomPostMutationResolver.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CustomPostMutations\MutationResolvers;
 
+use DateTime;
+use DateTimeInterface;
 use PoPCMSSchema\CustomPostMutations\Constants\CustomPostCRUDHookNames;
 use PoPCMSSchema\CustomPostMutations\Constants\MutationInputProperties;
 use PoPCMSSchema\CustomPostMutations\Exception\CustomPostCRUDMutationException;
@@ -270,7 +272,9 @@ abstract class AbstractCreateOrUpdateCustomPostMutationResolver extends Abstract
             $customPostData['custompost-type'] = $fieldDataAccessor->getValue(MutationInputProperties::CUSTOMPOST_TYPE);
         }
         if ($fieldDataAccessor->hasValue(MutationInputProperties::DATE)) {
-            $customPostData['date'] = $fieldDataAccessor->getValue(MutationInputProperties::DATE);
+            /** @var DateTime */
+            $dateTime = $fieldDataAccessor->getValue(MutationInputProperties::DATE);
+            $customPostData['date'] = $dateTime->format(DateTimeInterface::ATOM);
         }
 
         // Inject author, categories, tags, featured image, etc
