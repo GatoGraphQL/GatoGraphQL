b2f6a585ea55e08610e4fdc3c8ebb2faa372023a
leoloso <leo@getpop.org>
2025-03-25 15:37:39 +0800

    moved GenericCategoryTermDeleteInputObjectTypeResolver

--- layers/CMSSchema/packages/custompost-categorymeta-mutations/src/TypeResolvers/InputObjectType/GenericCategoryTermDeleteInputObjectTypeResolver.php
+++ layers/CMSSchema/packages/custompost-categorymeta-mutations/src/TypeResolvers/InputObjectType/GenericCategoryTermDeleteInputObjectTypeResolver.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace PoPCMSSchema\CustomPostCategoryMutations\TypeResolvers\InputObjectType;
+namespace PoPCMSSchema\CustomPostCategoryMetaMutations\TypeResolvers\InputObjectType;
 
 use PoPCMSSchema\CategoryMutations\TypeResolvers\InputObjectType\CategoryTermDeleteInputObjectTypeResolverTrait;
 
