7be941addaf78733934c3cf691159d36e4a8e759
leoloso <leo@getpop.org>
2025-03-25 14:49:27 +0800

    renamed to GenericCategorySetMeta

--- layers/CMSSchema/packages/custompost-categorymeta-mutations/src/TypeResolvers/InputObjectType/GenericCategorySetMetaInputObjectTypeResolver.php
+++ layers/CMSSchema/packages/custompost-categorymeta-mutations/src/TypeResolvers/InputObjectType/GenericCategorySetMetaInputObjectTypeResolver.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CustomPostCategoryMetaMutations\TypeResolvers\InputObjectType;
 
-class GenericCategorySetMetaInputObjectTypeResolver extends AbstractSetCategoriesOnGenericCustomPostInputObjectTypeResolver
+class GenericCategorySetMetaInputObjectTypeResolver extends AbstractSetMetaOnGenericCategoryInputObjectTypeResolver
 {
     public function getTypeName(): string
     {
