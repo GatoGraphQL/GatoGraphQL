2acf6a90a856c60820bcb84f83296bafd371e01e
leoloso <leo@getpop.org>
2025-03-26 17:02:50 +0800

    removed unneeded

--- layers/CMSSchema/packages/custompost-categorymeta-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
+++ layers/CMSSchema/packages/custompost-categorymeta-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
@@ -10,11 +10,6 @@ use PoP\ComponentModel\Feedback\FeedbackCategories;
 class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
 {
     public final const E1 = 'e1';
-    public final const E3 = 'e3';
-    public final const E6 = 'e6';
-    public final const E7 = 'e7';
-    public final const E8 = 'e8';
-    public final const E9 = 'e9';
 
     /**
      * @return string[]
@@ -23,23 +18,13 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     {
         return [
             self::E1,
-            self::E3,
-            self::E6,
-            self::E7,
-            self::E8,
-            self::E9,
         ];
     }
 
     public function getMessagePlaceholder(string $code): string
     {
         return match ($code) {
-            self::E1 => $this->__('You must be logged in to set category meta', 'custompost-categorymeta-mutations'),
-            self::E3 => $this->__('There is already a meta entry for key \'%s\'', 'custompost-categorymeta-mutations'),
-            self::E6 => $this->__('There is no category with ID \'%s\'', 'custompost-categorymeta-mutations'),
-            self::E7 => $this->__('On taxonomy \'%s\', there is no category with ID \'%s\'', 'custompost-categorymeta-mutations'),
-            self::E8 => $this->__('There is no category with slug \'%s\'', 'custompost-categorymeta-mutations'),
-            self::E9 => $this->__('On taxonomy \'%s\', there is no category with slug \'%s\'', 'custompost-categorymeta-mutations'),
+            self::E1 => $this->__('There is already a meta entry for key \'%s\'', 'custompost-categorymeta-mutations'),
             default => parent::getMessagePlaceholder($code),
         };
     }
--- layers/CMSSchema/packages/custompost-categorymeta-mutations/src/MutationResolvers/SetMetaOnCategoryMutationResolverTrait.php
+++ layers/CMSSchema/packages/custompost-categorymeta-mutations/src/MutationResolvers/SetMetaOnCategoryMutationResolverTrait.php
@@ -4,76 +4,24 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CustomPostCategoryMetaMutations\MutationResolvers;
 
-use PoPCMSSchema\CategoryMutations\ObjectModels\CategoryTermDoesNotExistErrorPayload;
-use PoPCMSSchema\CustomPostCategoryMetaMutations\Constants\MutationInputProperties;
 use PoPCMSSchema\CustomPostCategoryMetaMutations\FeedbackItemProviders\MutationErrorFeedbackItemProvider;
-use PoPCMSSchema\TaxonomyMetaMutations\FeedbackItemProviders\MutationErrorFeedbackItemProvider as TaxonomyMetaMutationErrorFeedbackItemProvider;
-use PoPCMSSchema\TaxonomyMetaMutations\MutationResolvers\SetTaxonomyTermsOnCustomPostMutationResolverTrait;
-use PoPCMSSchema\TaxonomyMetaMutations\ObjectModels\LoggedInUserHasNoAssigningTermsToTaxonomyCapabilityErrorPayload;
-use PoPCMSSchema\TaxonomyMetaMutations\ObjectModels\TaxonomyIsNotValidErrorPayload;
+use PoPCMSSchema\TaxonomyMetaMutations\MutationResolvers\MutateTaxonomyTermMetaMutationResolverTrait;
 use PoPCMSSchema\UserStateMutations\ObjectModels\UserIsNotLoggedInErrorPayload;
 use PoPSchema\SchemaCommons\ObjectModels\ErrorPayloadInterface;
-use PoP\ComponentModel\Feedback\FeedbackItemResolution;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackInterface;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
 use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
-use stdClass;
 
 trait SetMetaOnCategoryMutationResolverTrait
 {
-    use SetTaxonomyTermsOnCustomPostMutationResolverTrait;
-
-    /**
-     * @param array<string|int> $taxonomyTermIDs
-     */
-    protected function getTaxonomyIsNotRegisteredInCustomPostTypeFeedbackItemResolution(
-        string $customPostType,
-        string $taxonomyName,
-        array $taxonomyTermIDs,
-    ): FeedbackItemResolution {
-        return new FeedbackItemResolution(
-            MutationErrorFeedbackItemProvider::class,
-            MutationErrorFeedbackItemProvider::E4,
-            [
-                $taxonomyName,
-                implode('\', \'', $taxonomyTermIDs),
-                $customPostType,
-            ]
-        );
-    }
+    use MutateTaxonomyTermMetaMutationResolverTrait;
 
     protected function validateSetMetaOnCategory(
         string $customPostType,
         FieldDataAccessorInterface $fieldDataAccessor,
         ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
     ): void {
-        /** @var stdClass|null */
-        $categoriesBy = $fieldDataAccessor->getValue(MutationInputProperties::CATEGORY_BY);
-        if ($categoriesBy === null || ((array) $categoriesBy) === []) {
-            return;
-        }
-
-        // If `null` there was an error (already added to FeedbackStore)
-        $categoryTaxonomyToTaxonomyTerms = $this->getCategoryTaxonomyToTaxonomyTerms($fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
-        if ($categoryTaxonomyToTaxonomyTerms === null) {
-            return;
-        }
-
-        foreach ($categoryTaxonomyToTaxonomyTerms as $taxonomyName => $categoryIDs) {
-            $this->validateCanLoggedInUserAssignTermsToTaxonomy(
-                $taxonomyName,
-                $fieldDataAccessor,
-                $objectTypeFieldResolutionFeedbackStore,
-            );
-
-            $this->validateTaxonomyIsRegisteredForCustomPostType(
-                $customPostType,
-                $taxonomyName,
-                $categoryIDs,
-                $fieldDataAccessor,
-                $objectTypeFieldResolutionFeedbackStore,
-            );
-        }
+        // @todo Do something
     }
 
     protected function setMetaOnCategory(
@@ -82,48 +30,7 @@ trait SetMetaOnCategoryMutationResolverTrait
         FieldDataAccessorInterface $fieldDataAccessor,
         ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
     ): void {
-        /** @var array<string,array<string|int>> */
-        $categoryTaxonomyToTaxonomyTerms = $this->getCategoryTaxonomyToTaxonomyTerms($fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
-        foreach ($categoryTaxonomyToTaxonomyTerms as $taxonomyName => $categoryIDs) {
-            $this->getCustomPostCategoryMetaTypeMutationAPI()->setCategoriesByID(
-                $taxonomyName,
-                $customPostID,
-                $categoryIDs,
-                $append
-            );
-        }
-    }
-
-    /**
-     * Retrieve the taxonomy from the queried object's CPT,
-     * which works as long as it has only 1 category taxonomy registered.
-     *
-     * @return array<string,array<string|int>>|null
-     */
-    protected function getCategoryTaxonomyToTaxonomyTerms(
-        FieldDataAccessorInterface $fieldDataAccessor,
-        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
-    ): ?array {
-        $categoriesBy = $fieldDataAccessor->getValue(MutationInputProperties::CATEGORY_BY);
-        if (isset($categoriesBy->{MutationInputProperties::ID})) {
-            $categoryIDs = $categoriesBy->{MutationInputProperties::ID};
-            return $this->getTaxonomyToTaxonomyTermsByID(
-                true,
-                $categoryIDs,
-                $fieldDataAccessor,
-                $objectTypeFieldResolutionFeedbackStore,
-            );
-        }
-        if (isset($categoriesBy->{MutationInputProperties::SLUG})) {
-            $categorySlugs = $categoriesBy->{MutationInputProperties::SLUG};
-            return $this->getTaxonomyToTaxonomyTermsBySlug(
-                true,
-                $categorySlugs,
-                $fieldDataAccessor,
-                $objectTypeFieldResolutionFeedbackStore,
-            );
-        }
-        return null;
+        // @todo Do something
     }
 
     public function createSetMetaOnCategoryErrorPayloadFromObjectTypeFieldResolutionFeedback(
@@ -142,109 +49,7 @@ trait SetMetaOnCategoryMutationResolverTrait
             ] => new UserIsNotLoggedInErrorPayload(
                 $feedbackItemResolution->getMessage(),
             ),
-            [
-                MutationErrorFeedbackItemProvider::class,
-                MutationErrorFeedbackItemProvider::E4,
-            ] => new TaxonomyIsNotValidErrorPayload(
-                $feedbackItemResolution->getMessage(),
-            ),
-            [
-                MutationErrorFeedbackItemProvider::class,
-                MutationErrorFeedbackItemProvider::E6,
-            ] => new CategoryTermDoesNotExistErrorPayload(
-                $feedbackItemResolution->getMessage(),
-            ),
-            [
-                MutationErrorFeedbackItemProvider::class,
-                MutationErrorFeedbackItemProvider::E7,
-            ] => new CategoryTermDoesNotExistErrorPayload(
-                $feedbackItemResolution->getMessage(),
-            ),
-            [
-                MutationErrorFeedbackItemProvider::class,
-                MutationErrorFeedbackItemProvider::E8,
-            ] => new CategoryTermDoesNotExistErrorPayload(
-                $feedbackItemResolution->getMessage(),
-            ),
-            [
-                MutationErrorFeedbackItemProvider::class,
-                MutationErrorFeedbackItemProvider::E9,
-            ] => new CategoryTermDoesNotExistErrorPayload(
-                $feedbackItemResolution->getMessage(),
-            ),
-            [
-                TaxonomyMetaMutationErrorFeedbackItemProvider::class,
-                TaxonomyMetaMutationErrorFeedbackItemProvider::E10,
-            ] => new LoggedInUserHasNoAssigningTermsToTaxonomyCapabilityErrorPayload(
-                $feedbackItemResolution->getMessage(),
-            ),
-            [
-                TaxonomyMetaMutationErrorFeedbackItemProvider::class,
-                TaxonomyMetaMutationErrorFeedbackItemProvider::E11,
-            ] => new TaxonomyIsNotValidErrorPayload(
-                $feedbackItemResolution->getMessage(),
-            ),
-            [
-                TaxonomyMetaMutationErrorFeedbackItemProvider::class,
-                TaxonomyMetaMutationErrorFeedbackItemProvider::E12,
-            ] => new TaxonomyIsNotValidErrorPayload(
-                $feedbackItemResolution->getMessage(),
-            ),
             default => null,
         };
-    }
-
-    protected function getTaxonomyTermDoesNotExistError(
-        ?string $taxonomyName,
-        string|int $taxonomyTermID,
-    ): FeedbackItemResolution {
-        if ($taxonomyName !== null && $taxonomyName !== '') {
-            return new FeedbackItemResolution(
-                MutationErrorFeedbackItemProvider::class,
-                MutationErrorFeedbackItemProvider::E7,
-                [
-                    $taxonomyName,
-                    $taxonomyTermID,
-                ]
-            );
-        }
-        return new FeedbackItemResolution(
-            MutationErrorFeedbackItemProvider::class,
-            MutationErrorFeedbackItemProvider::E6,
-            [
-                $taxonomyTermID,
-            ]
-        );
-    }
-
-    protected function getTaxonomyTermBySlugDoesNotExistError(
-        ?string $taxonomyName,
-        string|int $taxonomyTermSlug,
-    ): FeedbackItemResolution {
-        if ($taxonomyName !== null && $taxonomyName !== '') {
-            return new FeedbackItemResolution(
-                MutationErrorFeedbackItemProvider::class,
-                MutationErrorFeedbackItemProvider::E9,
-                [
-                    $taxonomyName,
-                    $taxonomyTermSlug,
-                ]
-            );
-        }
-        return new FeedbackItemResolution(
-            MutationErrorFeedbackItemProvider::class,
-            MutationErrorFeedbackItemProvider::E8,
-            [
-                $taxonomyTermSlug,
-            ]
-        );
-    }
-
-    protected function getUserNotLoggedInError(): FeedbackItemResolution
-    {
-        return new FeedbackItemResolution(
-            MutationErrorFeedbackItemProvider::class,
-            MutationErrorFeedbackItemProvider::E1,
-        );
     }
 }
