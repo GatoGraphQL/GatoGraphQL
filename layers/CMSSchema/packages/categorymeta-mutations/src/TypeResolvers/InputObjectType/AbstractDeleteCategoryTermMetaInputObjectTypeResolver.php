cabab465c20f81096edc54a455fc7721df0a231d
leoloso <leo@getpop.org>
2025-03-26 14:47:26 +0800

    AbstractDeleteTaxonomyTermMetaInputObjectTypeResolver

--- layers/CMSSchema/packages/categorymeta-mutations/src/TypeResolvers/InputObjectType/AbstractDeleteCategoryTermMetaInputObjectTypeResolver.php
+++ layers/CMSSchema/packages/categorymeta-mutations/src/TypeResolvers/InputObjectType/AbstractDeleteCategoryTermMetaInputObjectTypeResolver.php
@@ -11,13 +11,13 @@ abstract class AbstractDeleteCategoryTermMetaInputObjectTypeResolver extends Abs
 {
     public function getTypeDescription(): ?string
     {
-        return $this->__('Input to delete a category term', 'categorymeta-mutations');
+        return $this->__('Input to delete a category term\'s meta entry', 'categorymeta-mutations');
     }
 
     public function getInputFieldDescription(string $inputFieldName): ?string
     {
         return match ($inputFieldName) {
-            MutationInputProperties::ID => $this->__('The ID of the category to delete', 'categorymeta-mutations'),
+            MutationInputProperties::ID => $this->__('The ID of the category', 'categorymeta-mutations'),
             default => parent::getInputFieldDescription($inputFieldName),
         };
     }
--- layers/CMSSchema/packages/taxonomymeta-mutations/src/TypeResolvers/InputObjectType/AbstractDeleteTaxonomyTermMetaInputObjectTypeResolver.php
+++ layers/CMSSchema/packages/taxonomymeta-mutations/src/TypeResolvers/InputObjectType/AbstractDeleteTaxonomyTermMetaInputObjectTypeResolver.php
@@ -9,10 +9,12 @@ use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoP\ComponentModel\TypeResolvers\InputObjectType\AbstractInputObjectTypeResolver;
 use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\ScalarType\IDScalarTypeResolver;
+use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
 
 abstract class AbstractDeleteTaxonomyTermMetaInputObjectTypeResolver extends AbstractInputObjectTypeResolver implements DeleteTaxonomyTermMetaInputObjectTypeResolverInterface
 {
     private ?IDScalarTypeResolver $idScalarTypeResolver = null;
+    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
 
     final protected function getIDScalarTypeResolver(): IDScalarTypeResolver
     {
@@ -23,10 +25,19 @@ abstract class AbstractDeleteTaxonomyTermMetaInputObjectTypeResolver extends Abs
         }
         return $this->idScalarTypeResolver;
     }
+    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
+    {
+        if ($this->stringScalarTypeResolver === null) {
+            /** @var StringScalarTypeResolver */
+            $stringScalarTypeResolver = $this->instanceManager->getInstance(StringScalarTypeResolver::class);
+            $this->stringScalarTypeResolver = $stringScalarTypeResolver;
+        }
+        return $this->stringScalarTypeResolver;
+    }
 
     public function getTypeDescription(): ?string
     {
-        return $this->__('Input to delete a taxonomy term', 'taxonomymeta-mutations');
+        return $this->__('Input to delete a taxonomy term\'s meta entry', 'taxonomymeta-mutations');
     }
 
     /**
@@ -39,18 +50,18 @@ abstract class AbstractDeleteTaxonomyTermMetaInputObjectTypeResolver extends Abs
                 MutationInputProperties::ID => $this->getIDScalarTypeResolver(),
             ] : [],
             [
-                MutationInputProperties::TAXONOMY => $this->getTaxonomyInputObjectTypeResolver(),
+                MutationInputProperties::KEY => $this->getStringScalarTypeResolver(),
             ]
         );
     }
 
     abstract protected function addIDInputField(): bool;
-    abstract protected function getTaxonomyInputObjectTypeResolver(): InputTypeResolverInterface;
 
     public function getInputFieldDescription(string $inputFieldName): ?string
     {
         return match ($inputFieldName) {
-            MutationInputProperties::ID => $this->__('The ID of the taxonomy to delete', 'taxonomymeta-mutations'),
+            MutationInputProperties::ID => $this->__('The ID of the taxonomy term', 'taxonomymeta-mutations'),
+            MutationInputProperties::KEY => $this->__('The meta key', 'taxonomymeta-mutations'),
             default => parent::getInputFieldDescription($inputFieldName),
         };
     }
@@ -58,8 +69,11 @@ abstract class AbstractDeleteTaxonomyTermMetaInputObjectTypeResolver extends Abs
     public function getInputFieldTypeModifiers(string $inputFieldName): int
     {
         return match ($inputFieldName) {
-            MutationInputProperties::ID => SchemaTypeModifiers::MANDATORY,
-            default => parent::getInputFieldTypeModifiers($inputFieldName),
+            MutationInputProperties::ID,
+            MutationInputProperties::KEY
+                => SchemaTypeModifiers::MANDATORY,
+            default
+                => parent::getInputFieldTypeModifiers($inputFieldName),
         };
     }
 }
