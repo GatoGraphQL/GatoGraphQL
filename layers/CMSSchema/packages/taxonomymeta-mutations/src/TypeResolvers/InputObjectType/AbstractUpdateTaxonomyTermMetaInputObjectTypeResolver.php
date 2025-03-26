317c7bb37868136dd156cd10732f0415146d34ed
leoloso <leo@getpop.org>
2025-03-26 14:45:36 +0800

    AbstractUpdateCategoryTermMetaInputObjectTypeResolver

--- layers/CMSSchema/packages/taxonomymeta-mutations/src/TypeResolvers/InputObjectType/AbstractUpdateTaxonomyTermMetaInputObjectTypeResolver.php
+++ layers/CMSSchema/packages/taxonomymeta-mutations/src/TypeResolvers/InputObjectType/AbstractUpdateTaxonomyTermMetaInputObjectTypeResolver.php
@@ -8,11 +8,15 @@ use PoPCMSSchema\TaxonomyMetaMutations\Constants\MutationInputProperties;
 use PoP\ComponentModel\Schema\SchemaTypeModifiers;
 use PoP\ComponentModel\TypeResolvers\InputObjectType\AbstractInputObjectTypeResolver;
 use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ScalarType\AnyScalarScalarTypeResolver;
 use PoP\ComponentModel\TypeResolvers\ScalarType\IDScalarTypeResolver;
+use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
 
 abstract class AbstractUpdateTaxonomyTermMetaInputObjectTypeResolver extends AbstractInputObjectTypeResolver implements UpdateTaxonomyTermMetaInputObjectTypeResolverInterface
 {
     private ?IDScalarTypeResolver $idScalarTypeResolver = null;
+    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
+    private ?AnyScalarScalarTypeResolver $anyScalarScalarTypeResolver = null;
 
     final protected function getIDScalarTypeResolver(): IDScalarTypeResolver
     {
@@ -23,10 +27,28 @@ abstract class AbstractUpdateTaxonomyTermMetaInputObjectTypeResolver extends Abs
         }
         return $this->idScalarTypeResolver;
     }
+    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
+    {
+        if ($this->stringScalarTypeResolver === null) {
+            /** @var StringScalarTypeResolver */
+            $stringScalarTypeResolver = $this->instanceManager->getInstance(StringScalarTypeResolver::class);
+            $this->stringScalarTypeResolver = $stringScalarTypeResolver;
+        }
+        return $this->stringScalarTypeResolver;
+    }
+    final protected function getAnyScalarScalarTypeResolver(): AnyScalarScalarTypeResolver
+    {
+        if ($this->anyScalarScalarTypeResolver === null) {
+            /** @var AnyScalarScalarTypeResolver */
+            $anyScalarScalarTypeResolver = $this->instanceManager->getInstance(AnyScalarScalarTypeResolver::class);
+            $this->anyScalarScalarTypeResolver = $anyScalarScalarTypeResolver;
+        }
+        return $this->anyScalarScalarTypeResolver;
+    }
 
     public function getTypeDescription(): ?string
     {
-        return $this->__('Input to update a taxonomy term', 'taxonomymeta-mutations');
+        return $this->__('Input to update a taxonomy term\'s meta', 'taxonomymeta-mutations');
     }
 
     /**
@@ -39,18 +61,20 @@ abstract class AbstractUpdateTaxonomyTermMetaInputObjectTypeResolver extends Abs
                 MutationInputProperties::ID => $this->getIDScalarTypeResolver(),
             ] : [],
             [
-                MutationInputProperties::TAXONOMY => $this->getTaxonomyInputObjectTypeResolver(),
+                MutationInputProperties::KEY => $this->getStringScalarTypeResolver(),
+                MutationInputProperties::VALUE => $this->getAnyScalarScalarTypeResolver(),
             ]
         );
     }
 
     abstract protected function addIDInputField(): bool;
-    abstract protected function getTaxonomyInputObjectTypeResolver(): InputTypeResolverInterface;
 
     public function getInputFieldDescription(string $inputFieldName): ?string
     {
         return match ($inputFieldName) {
-            MutationInputProperties::ID => $this->__('The ID of the taxonomy to delete', 'taxonomymeta-mutations'),
+            MutationInputProperties::ID => $this->__('The ID of the taxonomy term', 'taxonomymeta-mutations'),
+            MutationInputProperties::KEY => $this->__('The meta key', 'taxonomymeta-mutations'),
+            MutationInputProperties::VALUE => $this->__('The meta value', 'taxonomymeta-mutations'),
             default => parent::getInputFieldDescription($inputFieldName),
         };
     }
@@ -58,8 +82,12 @@ abstract class AbstractUpdateTaxonomyTermMetaInputObjectTypeResolver extends Abs
     public function getInputFieldTypeModifiers(string $inputFieldName): int
     {
         return match ($inputFieldName) {
-            MutationInputProperties::ID => SchemaTypeModifiers::MANDATORY,
-            default => parent::getInputFieldTypeModifiers($inputFieldName),
+            MutationInputProperties::ID,
+            MutationInputProperties::KEY,
+            MutationInputProperties::VALUE
+                => SchemaTypeModifiers::MANDATORY,
+            default
+                => parent::getInputFieldTypeModifiers($inputFieldName),
         };
     }
 }
