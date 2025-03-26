e446230f31b8d228d5fd082daa26e278d17c8195
leoloso <leo@getpop.org>
2025-03-26 14:23:28 +0800

    removed taxonomy from input

--- layers/CMSSchema/packages/taxonomymeta-mutations/src/Constants/MutationInputProperties.php
+++ layers/CMSSchema/packages/taxonomymeta-mutations/src/Constants/MutationInputProperties.php
@@ -7,10 +7,8 @@ namespace PoPCMSSchema\TaxonomyMetaMutations\Constants;
 class MutationInputProperties
 {
     public final const ID = 'id';
-    public final const TAXONOMY = 'taxonomy';
-    public final const SLUG = 'slug';
     public final const KEY = 'key';
     public final const VALUE = 'value';
-    public final const ENTRIES = 'entries';
     public final const SINGLE = 'single';
+    public final const ENTRIES = 'entries';
 }
