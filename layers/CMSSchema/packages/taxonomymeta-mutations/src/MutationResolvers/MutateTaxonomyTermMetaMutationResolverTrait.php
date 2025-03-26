1b9918460ae49a5dd5890ef1f75d40c3c876545e
leoloso <leo@getpop.org>
2025-03-26 11:59:29 +0800

    satisfy validateSingleMetaEntryDoesNotExist

--- layers/CMSSchema/packages/taxonomymeta-mutations/src/MutationResolvers/MutateTaxonomyTermMetaMutationResolverTrait.php
+++ layers/CMSSchema/packages/taxonomymeta-mutations/src/MutationResolvers/MutateTaxonomyTermMetaMutationResolverTrait.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\TaxonomyMetaMutations\MutationResolvers;
 
-use PoPCMSSchema\Taxonomies\TypeAPIs\TaxonomyTermTypeAPIInterface;
+use PoPCMSSchema\TaxonomyMeta\TypeAPIs\TaxonomyMetaTypeAPIInterface;
 use PoPCMSSchema\TaxonomyMetaMutations\FeedbackItemProviders\MutationErrorFeedbackItemProvider;
 use PoPCMSSchema\TaxonomyMutations\MutationResolvers\MutateTaxonomyTermMutationResolverTrait;
-use PoP\ComponentModel\App;
 use PoP\ComponentModel\Feedback\FeedbackItemResolution;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedback;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
@@ -17,7 +16,7 @@ trait MutateTaxonomyTermMetaMutationResolverTrait
 {
     use MutateTaxonomyTermMutationResolverTrait;
 
-    abstract protected function getTaxonomyTermTypeAPI(): TaxonomyTermTypeAPIInterface;
+    abstract protected function getTaxonomyMetaTypeAPI(): TaxonomyMetaTypeAPIInterface;
 
     protected function validateSingleMetaEntryDoesNotExist(
         string|int $taxonomyTermID,
@@ -25,11 +24,9 @@ trait MutateTaxonomyTermMetaMutationResolverTrait
         FieldDataAccessorInterface $fieldDataAccessor,
         ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
     ): void {
-        // Validate user permission
-        // @todo Validate single meta does not exist
-        // if ($this->getTaxonomyTermTypeAPI()->canUserEditTaxonomy($userID, $taxonomyName)) {
-        //     return;
-        // }
+        if ($this->getTaxonomyMetaTypeAPI()->getTaxonomyTermMeta($taxonomyTermID, $key, true) === null) {
+            return;
+        }
         $objectTypeFieldResolutionFeedbackStore->addError(
             new ObjectTypeFieldResolutionFeedback(
                 $this->getSingleMetaEntryAlreadyExistsError($taxonomyTermID, $key),
