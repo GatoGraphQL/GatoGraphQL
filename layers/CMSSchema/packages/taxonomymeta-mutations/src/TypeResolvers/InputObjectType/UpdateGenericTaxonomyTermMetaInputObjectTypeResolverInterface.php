837a3ddce82cb75e4a99c5f61c4ba7e33aceb0a3
leoloso <leo@getpop.org>
2025-03-26 17:34:35 +0800

    removed unneeded

--- layers/CMSSchema/packages/taxonomymeta-mutations/src/TypeResolvers/InputObjectType/UpdateGenericTaxonomyTermMetaInputObjectTypeResolverInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace PoPCMSSchema\TaxonomyMetaMutations\TypeResolvers\InputObjectType;
-
-interface UpdateGenericTaxonomyTermMetaInputObjectTypeResolverInterface extends UpdateTaxonomyTermMetaInputObjectTypeResolverInterface
-{
-}
