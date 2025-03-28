1422a07520ace8f1498fd1bd1d263a86aa3961d4
leoloso <leo@getpop.org>
2025-03-28 21:31:36 +0800

    fixed bug

--- layers/CMSSchema/packages/categorymeta-mutations/src/TypeResolvers/UnionType/AbstractCategorySetMetaMutationErrorPayloadUnionTypeResolver.php
+++ layers/CMSSchema/packages/categorymeta-mutations/src/TypeResolvers/UnionType/AbstractCategorySetMetaMutationErrorPayloadUnionTypeResolver.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CategoryMetaMutations\TypeResolvers\UnionType;
 
-abstract class AbstractCategoryTermMetaMutationErrorPayloadUnionTypeResolver extends AbstractCategoryMetaMutationErrorPayloadUnionTypeResolver
+abstract class AbstractCategorySetMetaMutationErrorPayloadUnionTypeResolver extends AbstractCategoryTermMetaMutationErrorPayloadUnionTypeResolver
 {
 }
