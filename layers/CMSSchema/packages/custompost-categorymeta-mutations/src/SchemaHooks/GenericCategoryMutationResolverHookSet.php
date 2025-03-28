be8f0d4666d86af56a0887d8e5eb4f8a4f241cc7
leoloso <leo@getpop.org>
2025-03-28 13:59:15 +0800

    SchemaHooks

--- layers/CMSSchema/packages/custompost-categorymeta-mutations/src/SchemaHooks/GenericCategoryMutationResolverHookSet.php
+++ layers/CMSSchema/packages/custompost-categorymeta-mutations/src/SchemaHooks/GenericCategoryMutationResolverHookSet.php
@@ -6,8 +6,7 @@ namespace PoPCMSSchema\CustomPostCategoryMetaMutations\SchemaHooks;
 
 use PoPCMSSchema\Categories\TypeResolvers\ObjectType\CategoryObjectTypeResolverInterface;
 use PoPCMSSchema\Categories\TypeResolvers\ObjectType\GenericCategoryObjectTypeResolver;
-use PoPCMSSchema\CustomPostCategoryMetaMutations\SchemaHooks\AbstractCategoryMutationResolverHookSet;
-use PoPCMSSchema\CategoryMutations\SchemaHooks\GenericCategoryMutationResolverHookSetTrait;
+use PoPCMSSchema\CategoryMetaMutations\SchemaHooks\AbstractCategoryMutationResolverHookSet;
 
 class GenericCategoryMutationResolverHookSet extends AbstractCategoryMutationResolverHookSet
 {
