b839bd437b81aecc8e029e5bb13e7e2e96ab82b6
leoloso <leo@getpop.org>
2025-03-28 14:27:40 +0800

    satisfy getMetaTypeAPI

--- layers/CMSSchema/packages/taxonomymeta-mutations/src/Hooks/AbstractTaxonomyMetaMutationResolverHookSet.php
+++ layers/CMSSchema/packages/taxonomymeta-mutations/src/Hooks/AbstractTaxonomyMetaMutationResolverHookSet.php
@@ -6,24 +6,42 @@ namespace PoPCMSSchema\TaxonomyMetaMutations\Hooks;
 
 use PoPCMSSchema\MetaMutations\Hooks\AbstractMetaMutationResolverHookSet;
 use PoPCMSSchema\MetaMutations\TypeAPIs\MetaTypeMutationAPIInterface;
+use PoPCMSSchema\Meta\TypeAPIs\MetaTypeAPIInterface;
 use PoPCMSSchema\TaxonomyMetaMutations\TypeAPIs\TaxonomyMetaTypeMutationAPIInterface;
+use PoPCMSSchema\TaxonomyMeta\TypeAPIs\TaxonomyMetaTypeAPIInterface;
 
 abstract class AbstractTaxonomyMetaMutationResolverHookSet extends AbstractMetaMutationResolverHookSet
 {
-    private ?TaxonomyMetaTypeMutationAPIInterface $taxonomyTypeMutationAPI = null;
+    private ?TaxonomyMetaTypeMutationAPIInterface $taxonomyMetaTypeMutationAPI = null;
+    private ?TaxonomyMetaTypeAPIInterface $taxonomyMetaTypeAPI = null;
 
     final protected function getTaxonomyMetaTypeMutationAPI(): TaxonomyMetaTypeMutationAPIInterface
     {
-        if ($this->taxonomyTypeMutationAPI === null) {
+        if ($this->taxonomyMetaTypeMutationAPI === null) {
             /** @var TaxonomyMetaTypeMutationAPIInterface */
-            $taxonomyTypeMutationAPI = $this->instanceManager->getInstance(TaxonomyMetaTypeMutationAPIInterface::class);
-            $this->taxonomyTypeMutationAPI = $taxonomyTypeMutationAPI;
+            $taxonomyMetaTypeMutationAPI = $this->instanceManager->getInstance(TaxonomyMetaTypeMutationAPIInterface::class);
+            $this->taxonomyMetaTypeMutationAPI = $taxonomyMetaTypeMutationAPI;
         }
-        return $this->taxonomyTypeMutationAPI;
+        return $this->taxonomyMetaTypeMutationAPI;
+    }
+    final protected function getTaxonomyMetaTypeAPI(): TaxonomyMetaTypeAPIInterface
+    {
+        if ($this->taxonomyMetaTypeAPI === null) {
+            /** @var TaxonomyMetaTypeAPIInterface */
+            $taxonomyMetaTypeAPI = $this->instanceManager->getInstance(TaxonomyMetaTypeAPIInterface::class);
+            $this->taxonomyMetaTypeAPI = $taxonomyMetaTypeAPI;
+        }
+        return $this->taxonomyMetaTypeAPI;
     }
 
     protected function getMetaTypeMutationAPI(): MetaTypeMutationAPIInterface
     {
         return $this->getTaxonomyMetaTypeMutationAPI();
     }
+
+    protected function getMetaTypeAPI(): MetaTypeAPIInterface
+    {
+        return $this->getTaxonomyMetaTypeAPI();
+    }
+
 }
