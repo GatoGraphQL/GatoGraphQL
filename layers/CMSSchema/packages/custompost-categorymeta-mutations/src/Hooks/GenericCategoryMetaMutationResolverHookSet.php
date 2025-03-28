d864248318e4bc1f240c4a885268ff80c8018763
leoloso <leo@getpop.org>
2025-03-28 14:34:05 +0800

    GenericCategoryMetaMutationResolverHookSet

--- layers/CMSSchema/packages/custompost-categorymeta-mutations/src/Hooks/GenericCategoryMetaMutationResolverHookSet.php
+++ layers/CMSSchema/packages/custompost-categorymeta-mutations/src/Hooks/GenericCategoryMetaMutationResolverHookSet.php
@@ -4,21 +4,21 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CustomPostCategoryMetaMutations\Hooks;
 
-use PoPCMSSchema\CustomPostCategoryMetaMutations\Hooks\AbstractMutationResolverHookSet;
+use PoPCMSSchema\CategoryMetaMutations\Hooks\AbstractCategoryMetaMutationResolverHookSet;
 use PoPCMSSchema\CustomPostCategoryMutations\Constants\GenericCategoryCRUDHookNames;
 
-class MutationResolverHookSet extends AbstractMutationResolverHookSet
+class GenericCategoryMetaMutationResolverHookSet extends AbstractCategoryMetaMutationResolverHookSet
 {
     protected function getValidateCreateHookName(): string
     {
-        return GenericCategoryCRUDHookNames::VALIDATE_ADD_META;
+        return GenericCategoryCRUDHookNames::VALIDATE_CREATE;
     }
     protected function getValidateUpdateHookName(): string
     {
-        return GenericCategoryCRUDHookNames::VALIDATE_UPDATE_META;
+        return GenericCategoryCRUDHookNames::VALIDATE_UPDATE;
     }
     protected function getExecuteCreateOrUpdateHookName(): string
     {
-        return GenericCategoryCRUDHookNames::EXECUTE_SET_META;
+        return GenericCategoryCRUDHookNames::EXECUTE_CREATE_OR_UPDATE;
     }
 }
