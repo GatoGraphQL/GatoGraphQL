a99e19257faa0e12d74f8c77ce6a9779ca8236e7
leoloso <leo@getpop.org>
2025-03-31 19:32:29 +0800

    CATEGORY_META => CUSTOMPOST_META

--- layers/CMSSchema/packages/custompostmeta-mutations/src/ModuleConfiguration.php
+++ layers/CMSSchema/packages/custompostmeta-mutations/src/ModuleConfiguration.php
@@ -15,7 +15,7 @@ class ModuleConfiguration extends AbstractModuleConfiguration
      */
     public function usePayloadableCustomPostMetaMutations(): bool
     {
-        $envVariable = Environment::USE_PAYLOADABLE_CATEGORY_META_MUTATIONS;
+        $envVariable = Environment::USE_PAYLOADABLE_CUSTOMPOST_META_MUTATIONS;
         $defaultValue = true;
         $callback = EnvironmentValueHelpers::toBool(...);
 
@@ -32,7 +32,7 @@ class ModuleConfiguration extends AbstractModuleConfiguration
             return false;
         }
 
-        $envVariable = Environment::ADD_FIELDS_TO_QUERY_PAYLOADABLE_CATEGORY_META_MUTATIONS;
+        $envVariable = Environment::ADD_FIELDS_TO_QUERY_PAYLOADABLE_CUSTOMPOST_META_MUTATIONS;
         $defaultValue = false;
         $callback = EnvironmentValueHelpers::toBool(...);
 
