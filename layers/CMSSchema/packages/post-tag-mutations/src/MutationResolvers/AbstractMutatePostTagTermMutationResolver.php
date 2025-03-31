affaf53257e223c75403c74fd89a888ee2021185
leoloso <leo@getpop.org>
2025-03-31 15:49:29 +0800

    replicate PostTagCRUDHookNames

--- layers/CMSSchema/packages/post-tag-mutations/src/MutationResolvers/AbstractMutatePostTagTermMutationResolver.php
+++ layers/CMSSchema/packages/post-tag-mutations/src/MutationResolvers/AbstractMutatePostTagTermMutationResolver.php
@@ -5,7 +5,131 @@ declare(strict_types=1);
 namespace PoPCMSSchema\PostTagMutations\MutationResolvers;
 
 use PoPCMSSchema\TagMutations\MutationResolvers\AbstractMutateTagTermMutationResolver;
+use PoPCMSSchema\PostTagMutations\Constants\PostTagCRUDHookNames;
+use PoPCMSSchema\TaxonomyMutations\Exception\TaxonomyTermCRUDMutationException;
+use PoP\ComponentModel\App;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
+use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
 
 abstract class AbstractMutatePostTagTermMutationResolver extends AbstractMutateTagTermMutationResolver
 {
+    protected function validateCreateErrors(
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): void {
+        App::doAction(
+            PostTagCRUDHookNames::VALIDATE_CREATE,
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+        App::doAction(
+            PostTagCRUDHookNames::VALIDATE_CREATE_OR_UPDATE,
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+
+        parent::validateCreateErrors(
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+    }
+
+    protected function validateUpdateErrors(
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): void {
+        App::doAction(
+            PostTagCRUDHookNames::VALIDATE_UPDATE,
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+        App::doAction(
+            PostTagCRUDHookNames::VALIDATE_CREATE_OR_UPDATE,
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+
+        parent::validateUpdateErrors(
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    protected function getCreateOrUpdateTaxonomyTermData(FieldDataAccessorInterface $fieldDataAccessor): array
+    {
+        $taxonomyData = parent::getCreateOrUpdateTaxonomyTermData($fieldDataAccessor);
+
+        $taxonomyData = App::applyFilters(PostTagCRUDHookNames::GET_CREATE_OR_UPDATE_DATA, $taxonomyData, $fieldDataAccessor);
+
+        return $taxonomyData;
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    protected function getUpdateTaxonomyTermData(FieldDataAccessorInterface $fieldDataAccessor): array
+    {
+        $taxonomyData = parent::getUpdateTaxonomyTermData($fieldDataAccessor);
+
+        $taxonomyData = App::applyFilters(PostTagCRUDHookNames::GET_UPDATE_DATA, $taxonomyData, $fieldDataAccessor);
+
+        return $taxonomyData;
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    protected function getCreateTaxonomyTermData(FieldDataAccessorInterface $fieldDataAccessor): array
+    {
+        $taxonomyData = parent::getCreateTaxonomyTermData($fieldDataAccessor);
+
+        $taxonomyData = App::applyFilters(PostTagCRUDHookNames::GET_CREATE_DATA, $taxonomyData, $fieldDataAccessor);
+
+        return $taxonomyData;
+    }
+
+    /**
+     * @return string|int The ID of the updated entity
+     * @throws TaxonomyTermCRUDMutationException If there was an error (eg: taxonomy term does not exist)
+     */
+    protected function update(
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): string|int {
+        $taxonomyTermID = parent::update($fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
+
+        App::doAction(
+            PostTagCRUDHookNames::EXECUTE_CREATE_OR_UPDATE,
+            $taxonomyTermID,
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+        App::doAction(
+            PostTagCRUDHookNames::EXECUTE_UPDATE,
+            $taxonomyTermID,
+            $fieldDataAccessor,
+            $objectTypeFieldResolutionFeedbackStore,
+        );
+
+        return $taxonomyTermID;
+    }
+
+    /**
+     * @return string|int The ID of the created entity
+     * @throws TaxonomyTermCRUDMutationException If there was an error (eg: some taxonomy term creation validation failed)
+     */
+    protected function create(
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): string|int {
+        $taxonomyTermID = parent::create($fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
+
+        App::doAction(PostTagCRUDHookNames::EXECUTE_CREATE_OR_UPDATE, $taxonomyTermID, $fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
+        App::doAction(PostTagCRUDHookNames::EXECUTE_CREATE, $taxonomyTermID, $fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
+
+        return $taxonomyTermID;
+    }
 }
