456e17da868ac9612ce973a5d9760c18e3a2e5a8
leoloso <leo@getpop.org>
2025-03-31 19:33:55 +0800

    removed unneeded

--- layers/CMSSchema/packages/custompostmeta-mutations-wp/src/TypeAPIs/CustomPostMetaTypeMutationAPI.php
+++ layers/CMSSchema/packages/custompostmeta-mutations-wp/src/TypeAPIs/CustomPostMetaTypeMutationAPI.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CustomPostMetaMutationsWP\TypeAPIs;
 
+use PoPCMSSchema\SchemaCommonsWP\TypeAPIs\TypeMutationAPITrait;
+use PoPCMSSchema\CustomPostMetaMutations\TypeAPIs\AbstractCustomPostMetaTypeMutationAPI;
 use PoPCMSSchema\CustomPostMetaMutations\Exception\CustomPostMetaCRUDMutationException;
-use PoPCMSSchema\CustomPostMetaMutations\TypeAPIs\CustomPostMetaTypeMutationAPIInterface;
-use PoPCMSSchema\CustomPostMetaMutationsWP\TypeAPIs\CustomPostMetaTypeMutationAPI;
-use PoPCMSSchema\CustomPostMetaMutations\Exception\CustomPostMetaCRUDMutationException;
+use WP_Error;
+
+use function add_post_meta;
+use function delete_post_meta;
+use function update_post_meta;
 
 /**
  * Methods to interact with the Type, to be implemented by the underlying CMS
  */
-class CustomPostMetaTypeMutationAPI extends CustomPostMetaTypeMutationAPI implements CustomPostMetaTypeMutationAPIInterface
+class CustomPostMetaTypeMutationAPI extends AbstractCustomPostMetaTypeMutationAPI
 {
+    use TypeMutationAPITrait;
+
     /**
      * @param array<string,mixed[]|null> $entries
      * @throws CustomPostMetaCRUDMutationException If there was an error
@@ -22,7 +28,37 @@ class CustomPostMetaTypeMutationAPI extends CustomPostMetaTypeMutationAPI implem
         string|int $customPostID,
         array $entries,
     ): void {
-        $this->setCustomPostMeta($customPostID, $entries);
+        foreach ($entries as $key => $values) {
+            if ($values === null) {
+                delete_post_meta((int) $customPostID, $key);
+                continue;
+            }
+
+            $numberItems = count($values);
+            if ($numberItems === 0) {
+                continue;
+            }
+
+            /**
+             * If there are 2 or more items, then use `add` to add them.
+             * If there is only 1 item, then use `update` to update it.
+             */
+            if ($numberItems === 1) {
+                $value = $values[0];
+                if ($value === null) {
+                    delete_post_meta((int) $customPostID, $key);
+                    continue;
+                }
+                update_post_meta((int) $customPostID, $key, $value);
+                continue;
+            }
+
+            // $numberItems > 1
+            delete_post_meta((int) $customPostID, $key);
+            foreach ($values as $value) {
+                add_post_meta((int) $customPostID, $key, $value, false);
+            }
+        }
     }
 
     /**
@@ -35,7 +71,42 @@ class CustomPostMetaTypeMutationAPI extends CustomPostMetaTypeMutationAPI implem
         mixed $value,
         bool $single = false,
     ): int {
-        return $this->addCustomPostMeta($customPostID, $key, $value, $single);
+        $result = add_post_meta((int) $customPostID, $key, $value, $single);
+        if ($result === false) {
+            throw $this->getCustomPostMetaCRUDMutationException(
+                \__('Error adding term meta', 'custompostmeta-mutations-wp')
+            );
+        }
+        $this->handleMaybeError($result);
+        /** @var int $result */
+        return $result;
+    }
+
+    protected function handleMaybeError(
+        int|bool|WP_Error $result,
+    ): void {
+        if (!($result instanceof WP_Error)) {
+            return;
+        }
+
+        /** @var WP_Error */
+        $wpError = $result;
+        throw $this->getCustomPostMetaCRUDMutationException($wpError);
+    }
+
+    protected function getCustomPostMetaCRUDMutationException(WP_Error|string $error): CustomPostMetaCRUDMutationException
+    {
+        $customPostMetaCRUDMutationExceptionClass = $this->getCustomPostMetaCRUDMutationExceptionClass();
+        if (is_string($error)) {
+            return new $customPostMetaCRUDMutationExceptionClass($error);
+        }
+        /** @var WP_Error */
+        $wpError = $error;
+        return new $customPostMetaCRUDMutationExceptionClass(
+            $wpError->get_error_message(),
+            $wpError->get_error_code() ? $wpError->get_error_code() : null,
+            $this->getWPErrorData($wpError),
+        );
     }
 
     /**
@@ -56,13 +127,30 @@ class CustomPostMetaTypeMutationAPI extends CustomPostMetaTypeMutationAPI implem
         mixed $value,
         mixed $prevValue = null,
     ): string|int|bool {
-        return $this->updateCustomPostMeta($customPostID, $key, $value, $prevValue);
+        $result = update_post_meta((int) $customPostID, $key, $value, $prevValue ?? '');
+        $this->handleMaybeError($result);
+        if ($result === false) {
+            throw $this->getCustomPostMetaCRUDMutationException(
+                \__('Error updating term meta', 'custompostmeta-mutations-wp')
+            );
+        }
+        /** @var int|bool $result */
+        return $result;
     }
 
+    /**
+     * @throws CustomPostMetaCRUDMutationException If there was an error (eg: custom post does not exist)
+     */
     public function deleteCustomPostMeta(
         string|int $customPostID,
         string $key,
     ): void {
-        $this->deleteCustomPostMeta($customPostID, $key);
+        $result = delete_post_meta((int) $customPostID, $key);
+        $this->handleMaybeError($result);
+        if ($result === false) {
+            throw $this->getCustomPostMetaCRUDMutationException(
+                \__('Error deleting term meta', 'custompostmeta-mutations-wp')
+            );
+        }
     }
 }
