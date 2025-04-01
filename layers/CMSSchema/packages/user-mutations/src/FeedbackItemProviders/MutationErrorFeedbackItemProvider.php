b0e3fef83b259a8a38f3b8551d96fe77d515a09e
leoloso <leo@getpop.org>
2025-04-01 20:05:50 +0800

    added PayloadableEditUserMutationResolverTrait

--- layers/CMSSchema/packages/user-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
+++ layers/CMSSchema/packages/user-mutations/src/FeedbackItemProviders/MutationErrorFeedbackItemProvider.php
@@ -12,6 +12,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
     public final const E1 = 'e1';
     public final const E2 = 'e2';
     public final const E3 = 'e3';
+    public final const E4 = 'e4';
 
     /**
      * @return string[]
@@ -22,6 +23,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
             self::E1,
             self::E2,
             self::E3,
+            self::E4,
         ];
     }
 
@@ -31,6 +33,7 @@ class MutationErrorFeedbackItemProvider extends AbstractFeedbackItemProvider
             self::E1 => $this->__('There is no user with ID \'%s\'', 'user-mutations'),
             self::E2 => $this->__('There is no user with username \'%s\'', 'user-mutations'),
             self::E3 => $this->__('There is no user with email \'%s\'', 'user-mutations'),
+            self::E4 => $this->__('The logged-in user doesn\'t have the ability to edit user with ID \'%s\'', 'user-mutations'),
             default => parent::getMessagePlaceholder($code),
         };
     }
