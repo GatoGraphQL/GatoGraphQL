42e8053c2079fbb38c931e3792e9f00d766e1baf
leoloso <leo@getpop.org>
2025-04-01 17:49:39 +0800

    added consts

--- layers/CMSSchema/packages/comment-mutations/src/Constants/CommentCRUDHookNames.php
+++ layers/CMSSchema/packages/comment-mutations/src/Constants/CommentCRUDHookNames.php
@@ -6,5 +6,7 @@ namespace PoPCMSSchema\CommentMutations\Constants;
 
 class CommentCRUDHookNames
 {
+    public const VALIDATE_ADD_COMMENT = __CLASS__ . ':validate-add-comment';
     public const EXECUTE_ADD_COMMENT = __CLASS__ . ':execute-add-comment';
+    public const GET_ADD_COMMENT_DATA = __CLASS__ . ':get-add-comment-data';
 }
