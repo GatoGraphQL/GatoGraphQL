90401514222c8dc69b1ab7003df5f38f2b0b3980
leoloso <leo@getpop.org>
2025-04-01 11:58:59 +0800

    phpstan

--- layers/CMSSchema/packages/taxonomymeta-mutations/src/TypeAPIs/AbstractTaxonomyMetaTypeMutationAPI.php
+++ layers/CMSSchema/packages/taxonomymeta-mutations/src/TypeAPIs/AbstractTaxonomyMetaTypeMutationAPI.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\TaxonomyMetaMutations\TypeAPIs;
 
+use PoPCMSSchema\MetaMutations\Exception\EntityMetaCRUDMutationException;
 use PoPCMSSchema\MetaMutations\TypeAPIs\AbstractEntityMetaTypeMutationAPI;
 use PoPCMSSchema\TaxonomyMetaMutations\Exception\TaxonomyTermMetaCRUDMutationException;
 use PoPCMSSchema\TaxonomyMetaMutations\TypeAPIs\TaxonomyMetaTypeMutationAPIInterface;
