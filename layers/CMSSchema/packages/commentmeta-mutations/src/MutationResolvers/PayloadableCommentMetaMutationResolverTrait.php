ca3022773a5a5ae72ada5e67af2c2a4a277d2fbc
leoloso <leo@getpop.org>
2025-04-01 18:27:49 +0800

    adapted PayloadableCommentMetaMutationResolverTrait

--- layers/CMSSchema/packages/commentmeta-mutations/src/MutationResolvers/PayloadableCommentMetaMutationResolverTrait.php
+++ layers/CMSSchema/packages/commentmeta-mutations/src/MutationResolvers/PayloadableCommentMetaMutationResolverTrait.php
@@ -4,32 +4,45 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\CommentMetaMutations\MutationResolvers;
 
-use PoPCMSSchema\MetaMutations\MutationResolvers\PayloadableMetaMutationResolverTrait;
 use PoPCMSSchema\CommentMetaMutations\Constants\CommentMetaCRUDHookNames;
-use PoPCMSSchema\CommentMutations\MutationResolvers\PayloadableCommentMutationResolverTrait as CommentMutationsPayloadableCommentMutationResolverTrait;
+use PoPCMSSchema\CommentMutations\FeedbackItemProviders\MutationErrorFeedbackItemProvider;
+use PoPCMSSchema\MetaMutations\MutationResolvers\PayloadableMetaMutationResolverTrait;
+use PoPCMSSchema\UserStateMutations\ObjectModels\UserIsNotLoggedInErrorPayload;
 use PoPSchema\SchemaCommons\ObjectModels\ErrorPayloadInterface;
+use PoPSchema\SchemaCommons\ObjectModels\GenericErrorPayload;
 use PoP\ComponentModel\App;
 use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackInterface;
 
 trait PayloadableCommentMetaMutationResolverTrait
 {
-    use CommentMutationsPayloadableCommentMutationResolverTrait {
-        CommentMutationsPayloadableCommentMutationResolverTrait::createErrorPayloadFromObjectTypeFieldResolutionFeedback as upstreamCreateErrorPayloadFromObjectTypeFieldResolutionFeedback;
-    }
     use PayloadableMetaMutationResolverTrait;
 
     protected function createErrorPayloadFromObjectTypeFieldResolutionFeedback(
         ObjectTypeFieldResolutionFeedbackInterface $objectTypeFieldResolutionFeedback
     ): ErrorPayloadInterface {
         $feedbackItemResolution = $objectTypeFieldResolutionFeedback->getFeedbackItemResolution();
-        return App::applyFilters(
-            CommentMetaCRUDHookNames::ERROR_PAYLOAD,
-            $this->createMetaMutationErrorPayloadFromObjectTypeFieldResolutionFeedback(
-                $objectTypeFieldResolutionFeedback,
-            ) ?? $this->upstreamCreateErrorPayloadFromObjectTypeFieldResolutionFeedback(
+        return match (
+            [
+            $feedbackItemResolution->getFeedbackProviderServiceClass(),
+            $feedbackItemResolution->getCode()
+            ]
+        ) {
+            [
+                MutationErrorFeedbackItemProvider::class,
+                MutationErrorFeedbackItemProvider::E1,
+            ] => new UserIsNotLoggedInErrorPayload(
+                $feedbackItemResolution->getMessage(),
+            ),
+            default => App::applyFilters(
+                CommentMetaCRUDHookNames::ERROR_PAYLOAD,
+                $this->createMetaMutationErrorPayloadFromObjectTypeFieldResolutionFeedback(
+                    $objectTypeFieldResolutionFeedback,
+                ) ?? new GenericErrorPayload(
+                    $feedbackItemResolution->getMessage(),
+                    $feedbackItemResolution->getNamespacedCode(),
+                ),
                 $objectTypeFieldResolutionFeedback,
             ),
-            $objectTypeFieldResolutionFeedback,
-        );
+        };
     }
 }
