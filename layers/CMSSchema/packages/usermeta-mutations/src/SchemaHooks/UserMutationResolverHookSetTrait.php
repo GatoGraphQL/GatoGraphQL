0910f36ad8999ffa0fc80a2b706a1b9a8145994b
leoloso <leo@getpop.org>
2025-04-01 20:27:57 +0800

    todo code

--- layers/CMSSchema/packages/usermeta-mutations/src/SchemaHooks/UserMutationResolverHookSetTrait.php
+++ layers/CMSSchema/packages/usermeta-mutations/src/SchemaHooks/UserMutationResolverHookSetTrait.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace PoPCMSSchema\UserMetaMutations\SchemaHooks;
 
-use PoPCMSSchema\UserMutations\TypeResolvers\InputObjectType\CreateUserInputObjectTypeResolverInterface;
-use PoPCMSSchema\UserMutations\TypeResolvers\InputObjectType\UpdateUserInputObjectTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\InputObjectType\InputObjectTypeResolverInterface;
 
 trait UserMutationResolverHookSetTrait
@@ -13,7 +11,10 @@ trait UserMutationResolverHookSetTrait
     protected function isInputObjectTypeResolver(
         InputObjectTypeResolverInterface $inputObjectTypeResolver,
     ): bool {
-        return $inputObjectTypeResolver instanceof CreateUserInputObjectTypeResolverInterface
-            || $inputObjectTypeResolver instanceof UpdateUserInputObjectTypeResolverInterface;
+        // @todo Remove temp code when adding User Mutations
+        return false;
+        // @todo Re-enable when adding User Mutations
+        // return $inputObjectTypeResolver instanceof CreateUserInputObjectTypeResolverInterface
+        //     || $inputObjectTypeResolver instanceof UpdateUserInputObjectTypeResolverInterface;
     }
 }
