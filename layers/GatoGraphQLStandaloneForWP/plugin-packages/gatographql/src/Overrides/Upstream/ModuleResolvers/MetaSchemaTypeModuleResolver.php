79177fc9a706a657606f7f4b2e996bef86e6e037
leoloso <leo@getpop.org>
2025-04-03 14:38:36 +0800

    No need to override OPTION_TREAT_META_KEYS_AS_SENSITIVE_DATA for Standalone

--- layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/Overrides/Upstream/ModuleResolvers/MetaSchemaTypeModuleResolver.php
+++ layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/Overrides/Upstream/ModuleResolvers/MetaSchemaTypeModuleResolver.php
@@ -9,23 +9,4 @@ use GatoGraphQL\GatoGraphQL\ModuleResolvers\MetaSchemaTypeModuleResolver as Upst
 class MetaSchemaTypeModuleResolver extends UpstreamMetaSchemaTypeModuleResolver
 {
     use OverrideToDisableModuleResolverTrait;
-
-    public function getSettingsDefaultValue(string $module, string $option): mixed
-    {
-        $defaultValues = [
-            self::SCHEMA_CUSTOMPOST_META => [
-                self::OPTION_TREAT_META_KEYS_AS_SENSITIVE_DATA => false,
-            ],
-            self::SCHEMA_USER_META => [
-                self::OPTION_TREAT_META_KEYS_AS_SENSITIVE_DATA => false,
-            ],
-            self::SCHEMA_COMMENT_META => [
-                self::OPTION_TREAT_META_KEYS_AS_SENSITIVE_DATA => false,
-            ],
-            self::SCHEMA_TAXONOMY_META => [
-                self::OPTION_TREAT_META_KEYS_AS_SENSITIVE_DATA => false,
-            ],
-        ];
-        return $defaultValues[$module][$option] ?? parent::getSettingsDefaultValue($module, $option);
-    }
 }
