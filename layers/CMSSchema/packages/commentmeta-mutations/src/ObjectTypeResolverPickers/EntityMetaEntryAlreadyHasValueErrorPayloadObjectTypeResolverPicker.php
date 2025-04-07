7ee9cae0ee4203eb26f0d3ca383c0c5289274a96
leoloso <leo@getpop.org>
2025-04-07 14:57:15 +0800

    added missing ObjectPickers

---
+++ layers/CMSSchema/packages/commentmeta-mutations/src/ObjectTypeResolverPickers/EntityMetaEntryAlreadyHasValueErrorPayloadObjectTypeResolverPicker.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\CommentMetaMutations\ObjectTypeResolverPickers;
+
+use PoPCMSSchema\CommentMetaMutations\TypeResolvers\UnionType\AbstractCommentUpdateMetaMutationErrorPayloadUnionTypeResolver;
+use PoPCMSSchema\CommentMetaMutations\TypeResolvers\UnionType\AbstractRootUpdateCommentTermMetaMutationErrorPayloadUnionTypeResolver;
+use PoPCMSSchema\MetaMutations\ObjectTypeResolverPickers\AbstractEntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker;
+use PoP\ComponentModel\TypeResolvers\UnionType\UnionTypeResolverInterface;
+
+class EntityMetaEntryAlreadyHasValueErrorPayloadObjectTypeResolverPicker extends AbstractEntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker
+{
+    /**
+     * @return array<class-string<UnionTypeResolverInterface>>
+     */
+    public function getUnionTypeResolverClassesToAttachTo(): array
+    {
+        return [
+            AbstractRootUpdateCommentTermMetaMutationErrorPayloadUnionTypeResolver::class,
+            AbstractCommentUpdateMetaMutationErrorPayloadUnionTypeResolver::class,
+        ];
+    }
+}
---
+++ layers/CMSSchema/packages/custompostmeta-mutations/src/ObjectTypeResolverPickers/EntityMetaEntryAlreadyHasValueErrorPayloadObjectTypeResolverPicker.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\CustomPostMetaMutations\ObjectTypeResolverPickers;
+
+use PoPCMSSchema\CustomPostMetaMutations\TypeResolvers\UnionType\AbstractCustomPostUpdateMetaMutationErrorPayloadUnionTypeResolver;
+use PoPCMSSchema\CustomPostMetaMutations\TypeResolvers\UnionType\AbstractRootUpdateCustomPostTermMetaMutationErrorPayloadUnionTypeResolver;
+use PoPCMSSchema\MetaMutations\ObjectTypeResolverPickers\AbstractEntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker;
+use PoP\ComponentModel\TypeResolvers\UnionType\UnionTypeResolverInterface;
+
+class EntityMetaEntryAlreadyHasValueErrorPayloadObjectTypeResolverPicker extends AbstractEntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker
+{
+    /**
+     * @return array<class-string<UnionTypeResolverInterface>>
+     */
+    public function getUnionTypeResolverClassesToAttachTo(): array
+    {
+        return [
+            AbstractRootUpdateCustomPostTermMetaMutationErrorPayloadUnionTypeResolver::class,
+            AbstractCustomPostUpdateMetaMutationErrorPayloadUnionTypeResolver::class,
+        ];
+    }
+}
