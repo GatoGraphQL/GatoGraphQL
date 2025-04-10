cff29a0727c0151cd675f3fa7f32ff4de870f08b
leoloso <leo@getpop.org>
2025-04-10 15:01:55 +0800

    Add entity classes in upstream

---
+++ layers/CMSSchema/packages/meta-mutations/src/TypeResolvers/InputObjectType/UpdateEntityMetaInputObjectTypeResolverInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\MetaMutations\TypeResolvers\InputObjectType;
+
+use PoP\ComponentModel\TypeResolvers\InputObjectType\InputObjectTypeResolverInterface;
+
+interface UpdateEntityMetaInputObjectTypeResolverInterface extends InputObjectTypeResolverInterface
+{
+}
