ef7d6b4062829594034fee7da87de502e43564b4
leoloso <leo@getpop.org>
2025-05-15 16:30:27 +0800

    order

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Constants/LoggerSeverity.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Constants/LoggerSeverity.php
@@ -6,18 +6,18 @@ namespace GatoGraphQL\GatoGraphQL\Constants;
 
 class LoggerSeverity
 {
+    public final const ERROR = 'ERROR';
+    public final const WARNING = 'WARNING';
     public final const INFO = 'INFO';
     public final const SUCCESS = 'SUCCESS';
-    public final const WARNING = 'WARNING';
-    public final const ERROR = 'ERROR';
 
     /**
      * @return string[]
      */
     public final const ALL = [
+        self::ERROR,
+        self::WARNING,
         self::INFO,
         self::SUCCESS,
-        self::WARNING,
-        self::ERROR,
     ];
 }
--- layers/GatoGraphQLForWP/plugins/gatographql/src/Constants/LoggerSigns.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Constants/LoggerSigns.php
@@ -6,8 +6,8 @@ namespace GatoGraphQL\GatoGraphQL\Constants;
 
 class LoggerSigns
 {
+    public final const ERROR = '🔴';
+    public final const WARNING = '🟡';
     public final const INFO = '🔵';
     public final const SUCCESS = '🟢';
-    public final const WARNING = '🟡';
-    public final const ERROR = '🔴';
 }
