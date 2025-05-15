e2006e746394ac4fe64d3fea05bac4167df38a2a
leoloso <leo@getpop.org>
2025-05-15 09:13:30 +0800

    added from woocommerce

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Controllers/MenuPages/Logs/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Controllers\MenuPages\Logs;
+
+use GatoGraphQL\GatoGraphQL\Services\MenuPages\AbstractPluginMenuPage;
+
+abstract class PageController extends AbstractPluginMenuPage
+{
+	
+        
+}
