110d3cac08010dab6510a9c221779cbee2f863d2
leoloso <leo@getpop.org>
2025-05-15 14:50:38 +0800

    fixed issues

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Controllers/MenuPages/Logs/PageController.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Controllers/MenuPages/Logs/PageController.php
@@ -8,6 +8,6 @@ use GatoGraphQL\GatoGraphQL\Services\MenuPages\AbstractPluginMenuPage;
 
 abstract class PageController extends AbstractPluginMenuPage
 {
-	
-        
+	abstract public function get_query_params( array $param_keys = array() ): array;
+    abstract public function get_logs_tab_url(): string;
 }
