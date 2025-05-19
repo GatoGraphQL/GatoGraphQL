5bee2461045077517b2f3b1acc854250c8be4728
leoloso <leo@getpop.org>
2025-05-19 16:54:46 +0800

    phpdoc

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Log/Controllers/PageController.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Log/Controllers/PageController.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\Log\Controllers;
 
+/**
+ * All code in this directory is based on WooCommerce.
+ *
+ * @see https://github.com/woocommerce/woocommerce/blob/9.8.5/plugins/woocommerce/src/Internal/Admin/Logging/PageController.php
+ */
 interface PageController
 {
 	public function get_query_params( array $param_keys = array() ): array;
