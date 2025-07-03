1c44ac391db4911a7b14808fa96c4aded0c50058
leoloso <leo@getpop.org>
2025-07-03 14:31:29 +0800

    Add `future` and `inherit` status to the `CustomPostStatus` enum

--- layers/WPSchema/packages/customposts/src/Overrides/TypeResolvers/EnumType/CustomPostStatusEnumTypeResolver.php
+++ layers/WPSchema/packages/customposts/src/Overrides/TypeResolvers/EnumType/CustomPostStatusEnumTypeResolver.php
@@ -8,7 +8,7 @@ use PoPWPSchema\CustomPosts\Enums\CustomPostStatus;
 use PoPCMSSchema\CustomPosts\TypeResolvers\EnumType\CustomPostStatusEnumTypeResolver as UpstreamCustomPostStatusEnumTypeResolver;
 
 /**
- * Add the "private" status
+ * Add the additional "WordPress"-specific statuses
  */
 class CustomPostStatusEnumTypeResolver extends UpstreamCustomPostStatusEnumTypeResolver
 {
@@ -19,14 +19,18 @@ class CustomPostStatusEnumTypeResolver extends UpstreamCustomPostStatusEnumTypeR
     {
         return [
             ...parent::getEnumValues(),
+            CustomPostStatus::FUTURE,
             CustomPostStatus::PRIVATE,
+            CustomPostStatus::INHERIT,
         ];
     }
 
     public function getEnumValueDescription(string $enumValue): ?string
     {
         return match ($enumValue) {
+            CustomPostStatus::FUTURE => $this->__('Future content', 'customposts'),
             CustomPostStatus::PRIVATE => $this->__('Private content', 'customposts'),
+            CustomPostStatus::INHERIT => $this->__('Inherit content', 'customposts'),
             default => parent::getEnumValueDescription($enumValue),
         };
     }
