77f8393027fb72fcab337a14ba8f4c2ba4f01861
leoloso <leo@getpop.org>
2025-07-24 16:16:24 +0800

    AbstractExecuteActionWithCustomSettingsMenuPage

---
+++ layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/Services/MenuPages/AbstractExecuteActionWithCustomSettingsMenuPage.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQLStandalone\GatoGraphQL\Services\MenuPages;
+
+use GatoGraphQL\GatoGraphQL\ModuleSettings\Properties;
+
+use GatoGraphQL\GatoGraphQL\Services\MenuPages\AbstractSettingsMenuPage;
+
+abstract class AbstractExecuteActionWithCustomSettingsMenuPage extends AbstractSettingsMenuPage
+{
+    /**
+     * Get the settings items which have this target
+     *
+     * @return array<array<string,mixed>>
+     */
+    protected function getSettingsItems(): array
+    {
+        $upstreamSettingsItems = parent::getSettingsItems();
+        $settingsItems = [];
+        foreach ($upstreamSettingsItems as $settingItem) {
+            $settingItem['settings'] = array_values(array_filter(
+                $settingItem['settings'] ?? [],
+                fn (array $item) => in_array(
+                    $this->getTarget(),
+                    $item[Properties::ADDITIONAL_TARGETS] ?? []
+                )
+            ));
+            if ($settingItem['settings'] === []) {
+                continue;
+            }
+            $settingsItems[] = $settingItem;
+        }
+        return $settingsItems;
+    }
+
+    abstract protected function getTarget(): string;
+}
