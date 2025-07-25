2d6f21d9fa60a7bb25d584bfe5801dc8368f9415
leoloso <leo@getpop.org>
2025-07-25 14:23:37 +0800

    PluginOptionsFormHandler

---
+++ layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/PluginManagement/PluginOptionsFormHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQLStandalone\GatoGraphQL\PluginManagement;
+
+use GatoGraphQL\GatoGraphQL\PluginManagement\PluginOptionsFormHandler as UpstreamPluginOptionsFormHandler;
+
+class PluginOptionsFormHandler extends UpstreamPluginOptionsFormHandler
+{
+    /**
+     * Also override the value from the form when doing
+     * "Execution action with custom settings".
+     *
+     * For that, it is enabled in all pages supporting bulk actions.
+     */
+    public function maybeOverrideValueFromForm(
+        mixed $value,
+        string $module,
+        string $option,
+    ): mixed {
+        global $pagenow;
+        if (!in_array($pagenow, [
+            'edit.php', // Posts
+            'upload.php', // Media
+            'edit-tags.php', // Taxonomies
+        ])) {
+            return parent::maybeOverrideValueFromForm($value, $module, $option);
+        }
+
+        return $this->doOverrideValueFromForm($value, $module, $option);
+    }
+}
