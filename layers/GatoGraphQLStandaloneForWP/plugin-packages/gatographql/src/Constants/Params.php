4a076554f2e6bf14fdcd443d540a65f22a2dd998
leoloso <leo@getpop.org>
2025-07-26 12:03:02 +0800

    change value of param

--- layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/Constants/Params.php
+++ layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/Constants/Params.php
@@ -9,5 +9,5 @@ class Params
     public const BULK_ACTION_SELECTED_IDS = 'bulk_action_selected_ids';
     public const BULK_ACTION_ORIGIN_URL = 'bulk_action_origin_url';
     public const BULK_ACTION_ORIGIN_SENDBACK_URL = 'bulk_action_origin_sendback_url';
-    public const BULK_ACTION_EXECUTE = 'execute_bulk_action';
+    public const BULK_ACTION_EXECUTE = 'bulk_action_execute';
 }
