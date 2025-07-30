7e7ab9a712febfa93d6d5c317eef66700df36e95
leoloso <leo@getpop.org>
2025-07-30 15:00:58 +0800

    DRY base class

---
+++ layers/CMSSchema/packages/customposts/src/FieldResolvers/ObjectType/AbstractWithParentCustomPostObjectTypeFieldResolver.php
@@ -0,0 +1,136 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPCMSSchema\CustomPosts\FieldResolvers\ObjectType;
+
+use PoPCMSSchema\CustomPosts\TypeAPIs\CustomPostTypeAPIInterface;
+use PoPCMSSchema\CustomPosts\TypeHelpers\CustomPostUnionTypeHelpers;
+use PoPCMSSchema\SchemaCommons\DataLoading\ReturnTypes;
+use PoPSchema\SchemaCommons\Constants\QueryOptions;
+use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
+use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractQueryableObjectTypeFieldResolver;
+use PoP\ComponentModel\QueryResolution\FieldDataAccessorInterface;
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\TypeResolvers\ConcreteTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
+use PoP\ComponentModel\TypeResolvers\ScalarType\IntScalarTypeResolver;
+use PoP\GraphQLParser\Spec\Parser\Ast\FieldInterface;
+
+abstract class AbstractWithParentCustomPostObjectTypeFieldResolver extends AbstractQueryableObjectTypeFieldResolver
+{
+    private ?CustomPostTypeAPIInterface $customPostTypeAPI = null;
+    private ?IntScalarTypeResolver $intScalarTypeResolver = null;
+
+    final protected function getCustomPostTypeAPI(): CustomPostTypeAPIInterface
+    {
+        if ($this->customPostTypeAPI === null) {
+            /** @var CustomPostTypeAPIInterface */
+            $customPostTypeAPI = $this->instanceManager->getInstance(CustomPostTypeAPIInterface::class);
+            $this->customPostTypeAPI = $customPostTypeAPI;
+        }
+        return $this->customPostTypeAPI;
+    }
+    final protected function getIntScalarTypeResolver(): IntScalarTypeResolver
+    {
+        if ($this->intScalarTypeResolver === null) {
+            /** @var IntScalarTypeResolver */
+            $intScalarTypeResolver = $this->instanceManager->getInstance(IntScalarTypeResolver::class);
+            $this->intScalarTypeResolver = $intScalarTypeResolver;
+        }
+        return $this->intScalarTypeResolver;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getFieldNamesToResolve(): array
+    {
+        return [
+            'parent',
+            'ancestors',
+            'children',
+            'childCount',
+        ];
+    }
+
+    public function getFieldTypeResolver(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ConcreteTypeResolverInterface
+    {
+        return match ($fieldName) {
+            'parent',
+            'ancestors',
+            'children' =>
+                $this->getCustomPostFieldTypeResolver(),
+            'childCount'
+                => $this->getIntScalarTypeResolver(),
+            default
+                => parent::getFieldTypeResolver($objectTypeResolver, $fieldName),
+        };
+    }
+
+    abstract protected function getCustomPostFieldTypeResolver(): ConcreteTypeResolverInterface;
+
+    public function getFieldTypeModifiers(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): int
+    {
+        return match ($fieldName) {
+            'ancestors' => SchemaTypeModifiers::IS_ARRAY | SchemaTypeModifiers::IS_NON_NULLABLE_ITEMS_IN_ARRAY,
+            'childCount' => SchemaTypeModifiers::NON_NULLABLE,
+            'children' => SchemaTypeModifiers::NON_NULLABLE | SchemaTypeModifiers::IS_ARRAY | SchemaTypeModifiers::IS_NON_NULLABLE_ITEMS_IN_ARRAY,
+            default => parent::getFieldTypeModifiers($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function getFieldDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
+    {
+        return match ($fieldName) {
+            'parent' => $this->__('Parent custom post', 'customposts'),
+            'ancestors' => $this->__('List of all ancestor custom posts (parent, grandparent, etc.)', 'customposts'),
+            'children' => $this->__('Child custom posts', 'customposts'),
+            'childCount' => $this->__('Number of child custom posts', 'customposts'),
+            default => parent::getFieldDescription($objectTypeResolver, $fieldName),
+        };
+    }
+
+    public function resolveValue(
+        ObjectTypeResolverInterface $objectTypeResolver,
+        object $object,
+        FieldDataAccessorInterface $fieldDataAccessor,
+        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
+    ): mixed {
+        $customPostTypeAPI = $this->getCustomPostTypeAPI();
+        $customPost = $object;
+        switch ($fieldDataAccessor->getFieldName()) {
+            case 'parent':
+                return $customPostTypeAPI->getParentCustomPostID($customPost);
+            case 'ancestors':
+                /** @var array<int|string> */
+                return $customPostTypeAPI->getAncestorCustomPostIDs($customPost);
+        }
+
+        $query = array_merge(
+            $this->convertFieldArgsToFilteringQueryArgs($objectTypeResolver, $fieldDataAccessor),
+            [
+                'parent-id' => $objectTypeResolver->getID($customPost),
+            ]
+        );
+        switch ($fieldDataAccessor->getFieldName()) {
+            case 'children':
+                return $customPostTypeAPI->getCustomPosts($query, [QueryOptions::RETURN_TYPE => ReturnTypes::IDS]);
+            case 'childCount':
+                return $customPostTypeAPI->getCustomPostCount($query);
+        }
+
+        return parent::resolveValue($objectTypeResolver, $object, $fieldDataAccessor, $objectTypeFieldResolutionFeedbackStore);
+    }
+
+    /**
+     * Since the return type is known for all the fields in this
+     * FieldResolver, there's no need to validate them
+     */
+    public function validateResolvedFieldType(
+        ObjectTypeResolverInterface $objectTypeResolver,
+        FieldInterface $field,
+    ): bool {
+        return false;
+    }
+}
