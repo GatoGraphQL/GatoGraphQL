4486a41bb6147b0bcf26493b3cce8cc5b205db05
leoloso <leo@getpop.org>
2025-08-05 11:14:40 +0800

    read env var

--- layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/FeedbackItemProviders/ErrorFeedbackItemProvider.php
+++ layers/GatoGraphQLStandaloneForWP/plugin-packages/gatographql/src/FeedbackItemProviders/ErrorFeedbackItemProvider.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 
 namespace GatoGraphQLStandalone\GatoGraphQL\FeedbackItemProviders;
 
+use GatoGraphQLStandalone\GatoGraphQL\Module;
+use GatoGraphQLStandalone\GatoGraphQL\ModuleConfiguration;
+use PoP\ComponentModel\App;
 use PoP\ComponentModel\FeedbackItemProviders\ErrorFeedbackItemProvider as UpstreamErrorFeedbackItemProvider;
 
 class ErrorFeedbackItemProvider extends UpstreamErrorFeedbackItemProvider
 {
     public function getMessagePlaceholder(string $code): string
     {
+        /** @var ModuleConfiguration */
+        $moduleConfiguration = App::getModule(Module::class)->getConfiguration();
         return match ($code) {
-            self::E9 => $this->__('Object with ID \'%2$s\' (of type \'%1$s\') cannot be loaded. Please check if referencing this ID is stale data, still stored on the WordPress database, but pointing to a non-existing object.', 'component-model'),
+            self::E9 => $moduleConfiguration->overrideErrorFeedbackItemProviderObjectNotFoundErrorMessage()
+                ? $this->__('Object with ID \'%2$s\' (of type \'%1$s\') cannot be loaded. Please check if referencing this ID is stale data, still stored on the WordPress database, but pointing to a non-existing object.', 'component-model')
+                : parent::getMessagePlaceholder($code),
             default => parent::getMessagePlaceholder($code),
         };
     }
