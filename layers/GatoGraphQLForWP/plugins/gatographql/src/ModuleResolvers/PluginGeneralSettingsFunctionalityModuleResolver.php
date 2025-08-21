674d8e0755d82e28f455e3c84640c87d22b204de
leoloso <leo@getpop.org>
2025-08-21 10:47:06 +0800

    Enable log notifications for warnings by default

--- layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/PluginGeneralSettingsFunctionalityModuleResolver.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/PluginGeneralSettingsFunctionalityModuleResolver.php
@@ -151,7 +151,7 @@ class PluginGeneralSettingsFunctionalityModuleResolver extends AbstractFunctiona
                 self::OPTION_ENABLE_LOG_COUNT_BADGES => true,
                 self::OPTION_ENABLE_LOG_COUNT_BADGES_BY_SEVERITY => [
                     LoggerSeverity::ERROR => true,
-                    LoggerSeverity::WARNING => false,
+                    LoggerSeverity::WARNING => true,
                     LoggerSeverity::INFO => false,
                     LoggerSeverity::DEBUG => false,
                 ],
