676d8e03f83da838d42e86975c6cd7a7c4e1268f
leoloso <leo@getpop.org>
2025-08-25 20:58:23 +0800

    phpdoc

--- layers/Engine/packages/root-wp/src/AppHooks.php
+++ layers/Engine/packages/root-wp/src/AppHooks.php
@@ -16,6 +16,13 @@ class AppHooks
     public final const BOOT_APP_IN_FRONTEND = 'wp';
     /**
      * WordPress filter hook to boot the App when in the WP REST API
+     *
+     * Watch out! Can't use `rest_api_init` because it is called several
+     * times, including before the user is authenticated. Then,
+     * the user would not be logged in for the execution of the GraphQL query.
+     *
+     * So we use `rest_jsonp_enabled` instead, which is called after the user
+     * is authenticated.
      */
     public final const BOOT_APP_IN_REST = 'rest_jsonp_enabled';
 }
