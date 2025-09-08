0e10954e35137b4bca74807a84c0a73790b07c16
leoloso <leo@getpop.org>
2025-09-09 07:54:13 +0800

    use wp_doing_cron

--- layers/GatoGraphQLForWP/plugins/gatographql/src/StaticHelpers/WordPressHelpers.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/StaticHelpers/WordPressHelpers.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\StaticHelpers;
 
+use function wp_doing_cron;
+
 class WordPressHelpers
 {
     public static function doingCron(): bool
     {
-        return defined('DOING_CRON') && DOING_CRON;
+        return wp_doing_cron();
+        // return defined('DOING_CRON') && DOING_CRON;
     }
 }
