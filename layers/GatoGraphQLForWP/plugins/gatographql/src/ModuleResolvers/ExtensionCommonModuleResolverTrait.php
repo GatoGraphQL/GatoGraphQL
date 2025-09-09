398e96eff50c36d52ec916bda6189f9508279220
leoloso <leo@getpop.org>
2025-09-09 08:17:19 +0800

    ignore traits

--- layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/ExtensionCommonModuleResolverTrait.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/ModuleResolvers/ExtensionCommonModuleResolverTrait.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\ModuleResolvers;
 
+/**
+ * @phpstan-ignore-next-line
+ */
 trait ExtensionCommonModuleResolverTrait
 {
     use CommonModuleResolverTrait;
