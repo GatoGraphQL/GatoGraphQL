2114103c7c964b71620725f3a26706e234e1c9aa
leoloso <leo@getpop.org>
2025-10-08 15:46:46 +0800

    Cast post ID to int

--- layers/CMSSchema/packages/customposts-wp/src/TypeAPIs/AbstractCustomPostTypeAPI.php
+++ layers/CMSSchema/packages/customposts-wp/src/TypeAPIs/AbstractCustomPostTypeAPI.php
@@ -469,7 +469,7 @@ abstract class AbstractCustomPostTypeAPI extends UpstreamAbstractCustomPostTypeA
         if (is_object($customPostObjectOrID)) {
             /** @var WP_Post */
             $customPost = $customPostObjectOrID;
-            return $customPost->ID;
+            return (int) $customPost->ID;
         }
         return (int)$customPostObjectOrID;
     }
