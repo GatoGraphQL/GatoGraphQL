9808e7b4df8d1d011c0e0afceaaf3117b333e96b
leoloso <leo@getpop.org>
2025-10-08 13:03:48 +0800

    added AbstractJSONObjectScalarTypeResolver

--- layers/Engine/packages/engine/src/TypeResolvers/ScalarType/JSONObjectScalarTypeResolver.php
+++ layers/Engine/packages/engine/src/TypeResolvers/ScalarType/JSONObjectScalarTypeResolver.php
@@ -4,18 +4,13 @@ declare(strict_types=1);
 
 namespace PoP\Engine\TypeResolvers\ScalarType;
 
-use PoP\ComponentModel\Feedback\ObjectTypeFieldResolutionFeedbackStore;
-use PoP\ComponentModel\TypeResolvers\ScalarType\AbstractScalarTypeResolver;
-use PoP\GraphQLParser\Spec\Parser\Ast\AstInterface;
-use stdClass;
-
 /**
  * GraphQL Custom Scalar representing a JSON Object on the client-side,
  * handled via an stdClass object on the server-side
  *
  * @see https://spec.graphql.org/draft/#sec-Scalars.Custom-Scalars
  */
-class JSONObjectScalarTypeResolver extends AbstractScalarTypeResolver
+class JSONObjectScalarTypeResolver extends AbstractJSONObjectScalarTypeResolver
 {
     public function getTypeName(): string
     {
@@ -30,17 +25,5 @@ class JSONObjectScalarTypeResolver extends AbstractScalarTypeResolver
     public function getSpecifiedByURL(): ?string
     {
         return 'https://datatracker.ietf.org/doc/html/rfc7159';
-    }
-
-    public function coerceValue(
-        string|int|float|bool|stdClass $inputValue,
-        AstInterface $astNode,
-        ObjectTypeFieldResolutionFeedbackStore $objectTypeFieldResolutionFeedbackStore,
-    ): string|int|float|bool|object|null {
-        if (!($inputValue instanceof stdClass)) {
-            $this->addDefaultError($inputValue, $astNode, $objectTypeFieldResolutionFeedbackStore);
-            return null;
-        }
-        return $inputValue;
     }
 }
