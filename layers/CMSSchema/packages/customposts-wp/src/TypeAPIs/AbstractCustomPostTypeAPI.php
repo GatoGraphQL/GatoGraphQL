4cca2054714a89baa0aacc73558b24fb279a6c29
leoloso <leo@getpop.org>
2025-11-07 14:08:49 +0800

    phpstan

--- layers/CMSSchema/packages/customposts-wp/src/TypeAPIs/AbstractCustomPostTypeAPI.php
+++ layers/CMSSchema/packages/customposts-wp/src/TypeAPIs/AbstractCustomPostTypeAPI.php
@@ -426,7 +426,6 @@ abstract class AbstractCustomPostTypeAPI extends UpstreamAbstractCustomPostTypeA
 
         // // Function get_sample_permalink comes from the file below, so it must be included
         // // Code below copied from `function get_sample_permalink_html`
-        // // @phpstan-ignore-next-line
         // include_once ABSPATH . 'wp-admin/includes/post.php';
         // list($permalink, $post_name) = get_sample_permalink((int)$customPostID, null, null);
         // return $post_name;
