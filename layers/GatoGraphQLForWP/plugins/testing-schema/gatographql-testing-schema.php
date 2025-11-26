f0148606ed2d943ea3741f8d2375670e6fcf2a2c
leoloso <leo@getpop.org>
2025-11-26 15:45:51 +0800

    prepare release

--- layers/GatoGraphQLForWP/plugins/testing-schema/gatographql-testing-schema.php
+++ layers/GatoGraphQLForWP/plugins/testing-schema/gatographql-testing-schema.php
@@ -3,7 +3,7 @@
 Plugin Name: Gato GraphQL - Testing Schema
 Plugin URI: https://github.com/GatoGraphQL/GatoGraphQL
 Description: Addition of elements to the GraphQL schema to test the Gato GraphQL plugin
-Version: 15.2.0-dev
+Version: 15.2.0
 Requires at least: 6.1
 Requires PHP: 8.1
 Author: Gato GraphQL
@@ -52,7 +52,7 @@ add_action(
          *
          * @gatographql-readonly-code
          */
-        $extensionVersion = '15.2.0-dev';
+        $extensionVersion = '15.2.0';
         $extensionName = \__('Gato GraphQL - Testing Schema');
         /**
          * Important: Do not modify the formatting of this PHP code!
