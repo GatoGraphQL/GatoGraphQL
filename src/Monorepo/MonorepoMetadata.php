a7c46b546f6364e044d7046678e00545a764da3f
leoloso <leo@getpop.org>
2026-01-14 11:12:59 +0800

    open 16.2-dev

--- src/Monorepo/MonorepoMetadata.php
+++ src/Monorepo/MonorepoMetadata.php
@@ -16,7 +16,7 @@ final class MonorepoMetadata
      *
      * @gatographql-readonly-code
      */
-    final public const VERSION = '16.1.1';
+    final public const VERSION = '16.2.0-dev';
     /**
      * This const will reflect the latest published tag in GitHub.
      *
