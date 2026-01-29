c775d262f3c5932d3726b07b799b1a9f75cd68b2
leoloso <leo@getpop.org>
2026-01-29 11:53:36 +0800

    better desc

--- layers/GatoGraphQLForWP/plugins/gatographql/includes/startup.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/includes/startup.php
@@ -32,7 +32,7 @@ class Startup {
                     printf(
                         '<div class="notice notice-error"><p>%s</p>%s</div>',
                         sprintf(
-                            __('Plugin <strong>%1$s</strong> requires at least <strong>%2$s</strong> of memory, however the server\'s PHP memory limit is set to <strong>%3$s</strong>. Please increase the memory limit to load %1$s.', 'gatographql'),
+                            __('Plugin <strong>%1$s</strong> requires at least <strong>%2$s</strong> of memory, however the server\'s PHP memory limit is set to <strong>%3$s</strong>. Please increase the memory limit to load the plugin.', 'gatographql'),
                             $pluginName,
                             $minRequiredPHPMemoryLimit,
                             $phpMemoryLimit
