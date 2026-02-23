5bce9ecb159bf7c9b1a135dab7f1eb4a908a4a1d
leoloso <leo@getpop.org>
2026-02-23 14:04:15 +0800

    renamed class

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Overrides/Clients/GraphiQLClient.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Overrides/Clients/GraphiQLClient.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace GatoGraphQL\GatoGraphQL\Overrides\Clients;
 
 use GatoGraphQL\GatoGraphQL\PluginApp;
-use GraphQLByPoP\GraphQLClientsForWP\Clients\GraphiQLClient;
+use GraphQLByPoP\GraphQLClientsForWP\Clients\GraphiQLClient as UpstreamGraphiQLClient;
 
 /**
  * Supplies the graphiql-app build base URL from the plugin's vendor path
  * so the public single endpoint at /graphiql/ serves GraphiQL v5.
  */
-class SingleEndpointGraphiQLClient extends GraphiQLClient
+class GraphiQLClient extends UpstreamGraphiQLClient
 {
     protected function getGraphiQLAppBuildBaseURL(): ?string
     {
