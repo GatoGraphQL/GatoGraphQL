81e0fcb69b94665c788a5dc63744c47aae55dbc6
leoloso <leo@getpop.org>
2026-02-26 17:01:09 +0800

    moved classes

--- layers/GatoGraphQLForWP/plugins/gatographql/src/MarketplaceProviders/LemonSqueezyCommercialPluginUpdaterService.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/MarketplaceProviders/LemonSqueezyCommercialPluginUpdaterService.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace GatoGraphQL\GatoGraphQL\Marketplace;
+namespace GatoGraphQL\GatoGraphQL\MarketplaceProviders;
 
 use GatoGraphQL\GatoGraphQL\Marketplace\ObjectModels\CommercialPluginUpdatedPluginData;
 use GatoGraphQL\GatoGraphQL\Module;
