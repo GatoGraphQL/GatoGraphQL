2a20720d65e3ac2589005a9cf2d6a8f9b4305dcf
leoloso <leo@getpop.org>
2026-02-26 17:06:51 +0800

    missing import

--- layers/GatoGraphQLForWP/plugins/gatographql/src/MarketplaceProviders/MarketplaceProviderCommercialPluginUpdaterServiceInterface.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/MarketplaceProviders/MarketplaceProviderCommercialPluginUpdaterServiceInterface.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace GatoGraphQL\GatoGraphQL\MarketplaceProviders;
 
+use GatoGraphQL\GatoGraphQL\Marketplace\CommercialPluginUpdaterServiceInterface;
+
 /**
  * Provider interface for plugin updater services that can be
  * registered and selected by license key (e.g. LemonSqueezy, FluentCart).
