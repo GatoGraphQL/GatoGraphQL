cdc41b4bca791fdc3265e549836fa25721230651
leoloso <leo@getpop.org>
2026-02-27 15:42:58 +0800

    MarketplaceVersion

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/Enums/MarketplaceVersion.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\Marketplace\Enums;
+
+enum MarketplaceVersion: string
+{
+    case V1_LemonSqueezy = 'v1-lemon-squeezy';
+    case V2_FluentCart = 'v2-fluent-cart';
+}
+
