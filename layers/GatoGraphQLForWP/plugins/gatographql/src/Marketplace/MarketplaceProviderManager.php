d9c4f974c4d2553288d9660e468467e610723b2f
leoloso <leo@getpop.org>
2026-02-27 17:13:04 +0800

    use const

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/MarketplaceProviderManager.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Marketplace/MarketplaceProviderManager.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace GatoGraphQL\GatoGraphQL\Marketplace;
 
 use GatoGraphQL\GatoGraphQL\Marketplace\Enums\MarketplaceVersion;
+use GatoGraphQL\GatoGraphQL\Marketplace\Constants\LicensePrefixes;
 use PoP\Root\Services\AbstractBasicService;
 
 class MarketplaceProviderManager extends AbstractBasicService implements MarketplaceProviderManagerInterface
 {
     public function getMarketplaceProviderFromLicenseKey(string $licenseKey): MarketplaceVersion
     {
-        if (str_starts_with($licenseKey, 'gatov2-')) {
+        if (str_starts_with($licenseKey, LicensePrefixes::GATOV2_FLUENTCART)) {
             return MarketplaceVersion::V2_FluentCart;
         }
 
