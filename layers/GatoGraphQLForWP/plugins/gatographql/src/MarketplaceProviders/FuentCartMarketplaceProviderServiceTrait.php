a7a78f32940fa36240a55c517bccf45ea29cc502
leoloso <leo@getpop.org>
2026-02-27 14:40:20 +0800

    FluentCart trait

---
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/MarketplaceProviders/FuentCartMarketplaceProviderServiceTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GatoGraphQL\GatoGraphQL\MarketplaceProviders;
+
+use GatoGraphQL\GatoGraphQL\Marketplace\Constants\LicensePrefixes;
+
+trait FuentCartMarketplaceProviderServiceTrait
+{
+    public function getPriority(): int
+    {
+        return 100;
+    }
+
+    public function canProcessLicense(string $licenseKey): bool
+    {
+        return str_starts_with($licenseKey, LicensePrefixes::GATOV2_FLUENTCART);
+    }
+}
