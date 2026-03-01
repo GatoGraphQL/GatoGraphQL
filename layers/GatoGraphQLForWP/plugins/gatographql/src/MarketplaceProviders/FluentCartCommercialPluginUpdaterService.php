706df4ec1c1853789df10fa7f3acb00aac7bb05c
leoloso <leo@getpop.org>
2026-03-01 22:59:27 +0800

    document API response

--- layers/GatoGraphQLForWP/plugins/gatographql/src/MarketplaceProviders/FluentCartCommercialPluginUpdaterService.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/MarketplaceProviders/FluentCartCommercialPluginUpdaterService.php
@@ -14,6 +14,37 @@ use WP_Error;
  * Based on code from FluentCart's `PluginUpdater` class
  *
  * @see wp-content/plugins/fluent-cart-pro/app/Services/PluginManager/PluginUpdater.php
+ *
+ * Response by FluentCart API:
+ * 
+ * ```json
+ * {
+ *     "new_version": "16.1.0",
+ *     "stable_version": "16.1.0",
+ *     "name": "Gato AI Translations for Polylang",
+ *     "slug": "gato-ai-translations-for-polylang",
+ *     "url": "https:\/\/gatoplugins.com\/changelog\/ai-translations-for-polylang",
+ *     "last_updated": "2026-02-25 06:57:17",
+ *     "homepage": "https:\/\/store.gatoplugins.com\/item\/gato-ai-translations-for-polylang\/",
+ *     "package": "https:\/\/store.gatoplugins.com\/?fluent-cart=download_license_package&fct_package=...",
+ *     "download_link": "https:\/\/store.gatoplugins.com\/?fluent-cart=download_license_package&fct_package=...",
+ *     "sections": {
+ *         "description": "Automatically translate your content",
+ *         "changelog": "<div style=\"color: #e4e4e4;background-color: #181818;font-family: Menlo, Monaco, 'Courier New', monospace;font-weight: normal;font-size: 12px;line-height: 18px;white-space: pre\">\n<div><span style=\"color: #aaa0fa\">##<\/span> <span style=\"color: #aaa0fa\">Added<\/span><\/div>\n<br \/>\n<div><span style=\"color: #d6d6dd\">-<\/span><span style=\"color: #e4e4e4\"> Translate user descriptions (<\/span><span style=\"color: #e394dc\">#2280<\/span><span style=\"color: #e4e4e4\">)<\/span><\/div>\n<div><span style=\"color: #d6d6dd\">-<\/span><span style=\"color: #e4e4e4\"> Translate user descriptions via WP-CLI (<\/span><span style=\"color: #e394dc\">#2281<\/span><span style=\"color: #e4e4e4\">)<\/span><\/div>\n<div><span style=\"color: #d6d6dd\">-<\/span><span style=\"color: #e4e4e4\"> Support Claude Opus 4.6 (<\/span><span style=\"color: #e394dc\">#2297<\/span><span style=\"color: #e4e4e4\">)<\/span><\/div>\n<br \/>\n<div><span style=\"color: #aaa0fa\">##<\/span> <span style=\"color: #aaa0fa\">Improvements<\/span><\/div>\n<br \/>\n<div><span style=\"color: #d6d6dd\">-<\/span><span style=\"color: #e4e4e4\"> Allow printing logs in reverse order (<\/span><span style=\"color: #e394dc\">#2279<\/span><span style=\"color: #e4e4e4\">)<\/span><\/div>\n<div><span style=\"color: #d6d6dd\">-<\/span><span style=\"color: #e4e4e4\"> Pass <\/span><span style=\"color: #e394dc\">`<\/span><span style=\"color: #e4e4e4\">--default-provider<\/span><span style=\"color: #e394dc\">`<\/span><span style=\"color: #e4e4e4\"> param to WP-CLI for menus (<\/span><span style=\"color: #e394dc\">#2282<\/span><span style=\"color: #e4e4e4\">)<\/span><\/div>\n<div><span style=\"color: #d6d6dd\">-<\/span><span style=\"color: #e4e4e4\"> Validate the PHP server has 1GB of memory available (<\/span><span style=\"color: #e394dc\">#2283<\/span><span style=\"color: #e4e4e4\">)<\/span><\/div>\n<div><span style=\"color: #d6d6dd\">-<\/span><span style=\"color: #e4e4e4\"> Link to documentation URL to explain how to increase memory for plugin (<\/span><span style=\"color: #e394dc\">#2284<\/span><span style=\"color: #e4e4e4\">)<\/span><\/div>\n<br \/>\n<div><span style=\"color: #aaa0fa\">##<\/span> <span style=\"color: #aaa0fa\">Fixed<\/span><\/div>\n<br \/>\n<div><span style=\"color: #d6d6dd\">-<\/span><span style=\"color: #e4e4e4\"> Do not show input for custom posts on the \"Gato Translate (Custom)\" form for taxonomies (#45b09e9f)<\/span><\/div>\n<div><span style=\"color: #d6d6dd\">-<\/span><span style=\"color: #e4e4e4\"> Execute menu translation when automatic translation for CPTs is disabled (#271a08c5)<\/span><\/div>\n<br \/><br \/><\/div>\n<p>\u00a0<\/p>"
+ *     },
+ *     "banners": {
+ *         "low": "https:\/\/gatoplugins.com\/images\/thumbnails\/plugins-logo\/ai-translations-for-polylang-1544x500.jpg",
+ *         "high": "https:\/\/gatoplugins.com\/images\/thumbnails\/plugins-logo\/ai-translations-for-polylang-1544x500.jpg"
+ *     },
+ *     "icons": {
+ *         "2x": "https:\/\/gatoplugins.com\/assets\/Gato-logo-suki-256x256.png",
+ *         "1x": "https:\/\/gatoplugins.com\/assets\/Gato-logo-suki-256x256.png"
+ *     },
+ *     "license_status": "valid",
+ *     "trunk": "https:\/\/store.gatoplugins.com\/?fluent-cart=download_license_package&fct_package=...",
+ *     "success": true
+ * }
+ * ```
  */
 class FluentCartCommercialPluginUpdaterService extends AbstractMarketplaceProviderCommercialPluginUpdaterService
 {
--- layers/GatoGraphQLForWP/plugins/gatographql/src/MarketplaceProviders/LemonSqueezyCommercialPluginUpdaterService.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/MarketplaceProviders/LemonSqueezyCommercialPluginUpdaterService.php
@@ -14,6 +14,31 @@ use function wp_remote_get;
  *
  * @see https://github.com/Make-Lemonade/lemonsqueezy-wp-updater-example
  * @see https://github.com/Make-Lemonade/lemonsqueezy-wp-updater-example/blob/7c0c71876309939b07d96e270f4db8568f3148cb/includes/class-plugin-updater.php
+ *
+ * Response by LemonSqueezy API:
+ * 
+ * ```json
+ * {
+ *     "success": true,
+ *     "error": "",
+ *     "error_code": "",
+ *     "update": {
+ *         "version": "16.1.0",
+ *         "tested": null,
+ *         "requires": null,
+ *         "author": "Gato Shop",
+ *         "author_profile": "https:\/\/shop.gatoplugins.com",
+ *         "download_link": "https:\/\/app.lemonsqueezy.com\/download\/{license_key}?expires=1772371546&signature=...",
+ *         "trunk": "https:\/\/app.lemonsqueezy.com\/download\/{license_key}?expires=1772371546&signature=...",
+ *         "requires_php": null,
+ *         "last_updated": null,
+ *         "sections": {
+ *             "description": "<p>Automatically translate your content.<\/p>",
+ *             "changelog": "16.1.0"
+ *         }
+ *     }
+ * }
+ * ```
  */
 class LemonSqueezyCommercialPluginUpdaterService extends AbstractMarketplaceProviderCommercialPluginUpdaterService
 {
