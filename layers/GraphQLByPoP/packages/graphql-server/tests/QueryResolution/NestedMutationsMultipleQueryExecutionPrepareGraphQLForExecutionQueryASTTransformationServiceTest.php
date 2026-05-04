330152148a4f64105bfec3ebfe22de90c3153bb0
leoloso <leo@getpop.org>
2026-05-04 08:58:33 +0800

    removed test non-needed for sequential mode

--- layers/GraphQLByPoP/packages/graphql-server/tests/QueryResolution/NestedMutationsMultipleQueryExecutionPrepareGraphQLForExecutionQueryASTTransformationServiceTest.php
+++ layers/GraphQLByPoP/packages/graphql-server/tests/QueryResolution/NestedMutationsMultipleQueryExecutionPrepareGraphQLForExecutionQueryASTTransformationServiceTest.php
@@ -6,11 +6,6 @@ namespace GraphQLByPoP\GraphQLServer\QueryResolution;
 
 class NestedMutationsMultipleQueryExecutionPrepareGraphQLForExecutionQueryASTTransformationServiceTest extends AbstractPrepareGraphQLForExecutionQueryASTTransformationServiceTestCase
 {
-    protected static function isMultipleQueryExecutionEnabled(): bool
-    {
-        return true;
-    }
-
     protected static function isNestedMutationsEnabled(): bool
     {
         return true;
