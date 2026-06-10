81587afa184f9732c40aefcbbea4850e036ab48e
leoloso <leo@getpop.org>
2026-06-10 10:57:30 +0800

    Link to extensions-reference page for locale

--- layers/GatoGraphQLForWP/plugins/gatographql/src/Services/MenuPages/AbstractExtensionModuleDocumentationMenuPage.php
+++ layers/GatoGraphQLForWP/plugins/gatographql/src/Services/MenuPages/AbstractExtensionModuleDocumentationMenuPage.php
@@ -6,10 +6,57 @@ namespace GatoGraphQL\GatoGraphQL\Services\MenuPages;
 
 use GatoGraphQL\GatoGraphQL\App;
 use GatoGraphQL\GatoGraphQL\Constants\RequestParams;
+use GatoGraphQL\GatoGraphQL\ContentProcessors\ContentParserOptions;
+use GatoGraphQL\GatoGraphQL\ModuleResolvers\Extensions\ExtensionModuleResolverInterface;
 use GatoGraphQL\GatoGraphQL\ModuleResolvers\ModuleResolverInterface;
 
 abstract class AbstractExtensionModuleDocumentationMenuPage extends AbstractModuleDocsMenuPage
 {
+    /**
+     * @return array<string,mixed>
+     */
+    protected function getMarkdownContentOptions(): array
+    {
+        $options = parent::getMarkdownContentOptions();
+
+        $websiteURL = $this->getDocParamWebsiteURL();
+        if ($websiteURL !== null) {
+            $options[ContentParserOptions::WEBSITE_DOC_URL] = $websiteURL;
+        }
+
+        return $options;
+    }
+
+    /**
+     * When opening a bundled extension doc via ?doc=..., link the English-doc
+     * notice to that extension's page on the localized website.
+     */
+    protected function getDocParamWebsiteURL(): ?string
+    {
+        $doc = App::query(RequestParams::DOC, '');
+        if ($doc === '') {
+            return null;
+        }
+
+        if (!preg_match('#extensions/([^/]+)/docs/modules/#', $doc, $matches)) {
+            return null;
+        }
+
+        $extensionSlug = $matches[1];
+        foreach ($this->getModuleRegistry()->getAllModules(true, false, false) as $module) {
+            $moduleResolver = $this->getModuleRegistry()->getModuleResolver($module);
+            if (!($moduleResolver instanceof ExtensionModuleResolverInterface)) {
+                continue;
+            }
+            if ($moduleResolver->getSlug($module) !== $extensionSlug) {
+                continue;
+            }
+            return $moduleResolver->getWebsiteURL($module);
+        }
+
+        return null;
+    }
+
     /**
      * If opening a Tutorial lesson doc in the iframe, do not use tabpanels
      */
