306644f9c5b27bf2d91e9b78a967541aaa313c47
leoloso <leo@getpop.org>
2026-07-08 11:30:12 +0800

    run imports as admin user

--- webservers/_shared/setup/import-data.sh
+++ webservers/_shared/setup/import-data.sh
@@ -9,8 +9,8 @@ if [ -d "$UPLOADS_DIR" ]; then
     rm -rf $UPLOADS_DIR
 fi
 
-wp import /app/_shared-webserver/assets/gatographql-data.xml --url="$(echo $WEBSERVER_DOMAIN)" --authors=create
-wp import /app/_shared-webserver/assets/gatographql-testing-data.xml --url="$(echo $WEBSERVER_DOMAIN)" --authors=create
+wp import /app/_shared-webserver/assets/gatographql-data.xml --url="$WEBSERVER_DOMAIN" --authors=create --user=admin
+wp import /app/_shared-webserver/assets/gatographql-testing-data.xml --url="$WEBSERVER_DOMAIN" --authors=create --user=admin
 
 # Importing a JSON object via gatographql-data.xml doesn't work, then do here
 wp post meta update 1116 meta_with_object_value '{"_edit_lock":{"to":{"111":["1743667747:1"]}},"_pingme":{"to":{"111":["1"]}},"some_property_to_translate":{"to":{"111":["Messi won another cup for more than 4 times by now, bleh\r\nAnd with another sentence"]}},"some_property":{"to":{"111":["Nothing about Diego?"]}}}' --format=json
