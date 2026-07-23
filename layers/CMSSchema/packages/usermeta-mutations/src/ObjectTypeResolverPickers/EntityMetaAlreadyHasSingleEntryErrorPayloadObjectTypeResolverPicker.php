ae1d814eadf349e156f6d15db6821a6e951fac9a
leoloso <leo@getpop.org>
2026-07-23 17:11:04 +0800

    [UserMeta] Keep only the reachable meta error on user create/update
    The injected `meta` on createUser/updateUser uses set-meta semantics, whose
    only validation error is AccessToMetaKeyIsNotAllowed. Remove
    EntityMetaAlreadyHasSingleEntry from the user create/update error-payload unions
    (it can only arise from the standalone addUserMeta mutation, never from the
    injected meta).
    
    Also add example queries to the User Mutations module docs.

--- layers/CMSSchema/packages/usermeta-mutations/src/ObjectTypeResolverPickers/EntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker.php
+++ layers/CMSSchema/packages/usermeta-mutations/src/ObjectTypeResolverPickers/EntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker.php
@@ -7,9 +7,6 @@ namespace PoPCMSSchema\UserMetaMutations\ObjectTypeResolverPickers;
 use PoPCMSSchema\UserMetaMutations\TypeResolvers\UnionType\AbstractUserAddMetaMutationErrorPayloadUnionTypeResolver;
 use PoPCMSSchema\UserMetaMutations\TypeResolvers\UnionType\AbstractRootAddUserMetaMutationErrorPayloadUnionTypeResolver;
 use PoPCMSSchema\MetaMutations\ObjectTypeResolverPickers\AbstractEntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker;
-use PoPCMSSchema\UserMutations\TypeResolvers\UnionType\RootCreateUserMutationErrorPayloadUnionTypeResolver;
-use PoPCMSSchema\UserMutations\TypeResolvers\UnionType\RootUpdateUserMutationErrorPayloadUnionTypeResolver;
-use PoPCMSSchema\UserMutations\TypeResolvers\UnionType\UserUpdateMutationErrorPayloadUnionTypeResolver;
 use PoP\ComponentModel\TypeResolvers\UnionType\UnionTypeResolverInterface;
 
 class EntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker extends AbstractEntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker
@@ -22,9 +19,6 @@ class EntityMetaAlreadyHasSingleEntryErrorPayloadObjectTypeResolverPicker extend
         return [
             AbstractRootAddUserMetaMutationErrorPayloadUnionTypeResolver::class,
             AbstractUserAddMetaMutationErrorPayloadUnionTypeResolver::class,
-            RootCreateUserMutationErrorPayloadUnionTypeResolver::class,
-            RootUpdateUserMutationErrorPayloadUnionTypeResolver::class,
-            UserUpdateMutationErrorPayloadUnionTypeResolver::class,
         ];
     }
 }
